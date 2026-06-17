c621f659ab8ed67823c3bf30bbf9add5f561be7c
Alexander Sieg <alex@xanderio.de>
2026-06-17 14:28:42 +0200

    dependency-track: 4.14.1 -> 5.0.1

--- pkgs/by-name/de/dependency-track/package.nix
+++ pkgs/by-name/de/dependency-track/package.nix
@@ -2,31 +2,28 @@
   lib,
   buildNpmPackage,
   fetchFromGitHub,
-  nodejs_20,
-  jre_headless,
+  jdk25_headless,
   protobuf_30,
   xmlstarlet,
-  cyclonedx-cli,
   makeWrapper,
   maven,
   nix-update-script,
   nixosTests,
 }:
 let
-  version = "4.14.1";
+  version = "5.0.1";
+
+  jdk = jdk25_headless;
 
   frontend = buildNpmPackage {
     pname = "dependency-track-frontend";
     inherit version;
 
-    # TODO: pinned due to build error on node 22
-    nodejs = nodejs_20;
-
     src = fetchFromGitHub {
       owner = "DependencyTrack";
       repo = "frontend";
       rev = version;
-      hash = "sha256-xjIRkffmXYMAfZ8wJehnPRfTThJjTgNL8ONl9N9ZJ+M=";
+      hash = "sha256-1M4z4kLY5kQW00Y9aSZkkayUzQGS0oFgCKApl/veQYM=";
     };
 
     installPhase = ''
@@ -34,7 +31,7 @@ let
       cp -R ./dist $out/
     '';
 
-    npmDepsHash = "sha256-CW9LOur8N3obrOeHgYFH2OO/vg8XihUspuXS5Zrix8I=";
+    npmDepsHash = "sha256-XxERLmAoRJUOlMcC9zRueS+ZlTXiVM87irXeZ8BIFNY=";
     forceGitDeps = true;
     makeCacheWritable = true;
 
@@ -51,13 +48,12 @@ maven.buildMavenPackage rec {
     owner = "DependencyTrack";
     repo = "dependency-track";
     rev = version;
-    hash = "sha256-pIZM8FQ0IFqRbTQT5VIlCmS+fCCXULJJ6bdEv6xfjbc=";
+    hash = "sha256-txH7n77FBwtyJ0W+ecKyChFulDiMZUC/KMCCxziHPEg=";
   };
 
   postPatch = ''
-    # update to version 5.1.3 to fix NullPointer and specify protoc path
+    # specify protoc path
     xmlstarlet ed --inplace -N x=http://maven.apache.org/POM/4.0.0 \
-    --update '//x:plugin[x:artifactId="protobuf-maven-plugin"]/x:version' -v "5.1.3" \
     --delete '//x:plugin[x:artifactId="protobuf-maven-plugin"]/x:configuration/x:protoc' \
     --subnode '//x:plugin[x:artifactId="protobuf-maven-plugin"]/x:configuration' -t elem -n protoc -v "" \
     --var protoc '$prev' \
@@ -65,12 +61,10 @@ maven.buildMavenPackage rec {
     --subnode '$protoc' -t elem -n name -v "protoc" \
     pom.xml
 
-    # remove frontend related tasks
+    # remove bom related tasks, this requieres maven online mode
     xmlstarlet ed --inplace -N x=http://maven.apache.org/POM/4.0.0 \
-    --delete '//x:execution[x:id="frontend-download"]' \
-    --delete '//x:execution[x:id="frontend-extract"]' \
-    --delete '//x:execution[x:id="frontend-resource-deploy"]' \
-    pom.xml
+    --delete '//x:execution[x:id="deploy-bom"]' \
+    apiserver/pom.xml
 
     # add junixsocket to enable unixsocket connection to postgres
     xmlstarlet ed --inplace -N x=http://maven.apache.org/POM/4.0.0 \
@@ -83,40 +77,26 @@ maven.buildMavenPackage rec {
     pom.xml
   '';
 
-  mvnJdk = jre_headless;
-  mvnHash = "sha256-4N4KuJBF/RFZwpp3dIgXntxSEfKHyfvrShKQoUqY5bE=";
+  mvnJdk = jdk;
+  mvnHash = "sha256-kYKj6VLLAAMqqe74Ca6qkS2j0f9dU6OtHueenFOOvPc=";
   manualMvnArtifacts = [
-    "com.coderplus.maven.plugins:copy-rename-maven-plugin:1.0.1"
-    # added to saticfy protobuf compiler plugin dependency resolving
-    "jakarta.el:jakarta.el-api:5.0.1"
-    "com.fasterxml.jackson.module:jackson-module-jakarta-xmlbind-annotations:2.19.1"
-    "com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.21.0"
-    "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.3"
-    "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.21.0"
-    "io.micrometer:micrometer-core:1.16.0"
-    "io.micrometer:micrometer-observation:1.16.0"
+    "org.apache.maven.plugins:maven-antrun-plugin:3.1.0"
+    "org.apache.maven.plugins:maven-assembly-plugin:3.7.1"
+    "org.apache.maven.plugins:maven-surefire-report-plugin:3.5.5"
+    "org.apache.maven.plugins:maven-release-plugin:3.3.1"
+    "org.eclipse.microprofile.config:microprofile-config-api:3.1.1-RC1"
   ];
   buildOffline = true;
 
   mvnDepsParameters = lib.escapeShellArgs [
     "-Dmaven.test.skip=true"
-    "-P enhance"
-    "-P embedded-jetty"
   ];
 
   mvnParameters = lib.escapeShellArgs [
     "-Dmaven.test.skip=true"
-    "-P enhance"
-    "-P embedded-jetty"
-    "-Dservices.bom.merge.skip=false"
-    "-Dlogback.configuration.file=${src}/src/main/docker/logback.xml"
-    "-Dcyclonedx-cli.path=${lib.getExe cyclonedx-cli}"
+    # BOM generation requieres maven online mode
+    "-Dservices.bom.merge.skip=true"
   ];
-
-  afterDepsSetup = ''
-    mvn cyclonedx:makeBom -Dmaven.repo.local=$mvnDeps/.m2 \
-      org.codehaus.mojo:exec-maven-plugin:exec@merge-services-bom
-  '';
 
   doCheck = false;
 
@@ -129,8 +109,8 @@ maven.buildMavenPackage rec {
   installPhase = ''
     runHook preInstall
 
-    install -Dm644 target/dependency-track-*.jar $out/share/dependency-track/dependency-track.jar
-    makeWrapper ${jre_headless}/bin/java $out/bin/dependency-track \
+    install -Dm644 apiserver/target/dependency-track-apiserver.jar $out/share/dependency-track/dependency-track.jar
+    makeWrapper ${jdk}/bin/java $out/bin/dependency-track \
       --add-flags "-jar $out/share/dependency-track/dependency-track.jar"
 
     runHook postInstall
@@ -158,6 +138,6 @@ maven.buildMavenPackage rec {
       xanderio
     ];
     mainProgram = "dependency-track";
-    inherit (jre_headless.meta) platforms;
+    inherit (jdk.meta) platforms;
   };
 }
