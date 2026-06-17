NixOS/nixpkgs

commit 1dc27975d1c106ccefcce2b9d7f3f75f16c3970f

octavePackages.optim: mark as broken

pkgs/development/octave-modules/optim/default.nix
@@ -36,5 +36,7 @@ buildOctavePackage rec {
     # Modified BSD code seems removed
     maintainers = with lib.maintainers; [ KarlJoad ];
     description = "Non-linear optimization toolkit";
+    # Hasn't been updated since 2022, and fails to build with octave >= 10.1.0
+    broken = true;
   };
 }

commit cbbc21f9f84099f5c00b4e1e67a64ade87ec7083

octave.pkgs.optim: 1.6.2 -> 1.6.3, unbreak
It was marked broken in 1dc27975d1c106ccefcce2b9d7f3f75f16c3970f (May
2025).
But the bug was fixed in October 2025 with:
https://sourceforge.net/p/octave/optim/ci/d8c28ab3f3f37d439bc2d961d79f4a8caa9830d1/

Since there were new commits, but no release. I asked the upstream
maintainer to push the 1.6.3 tag.

I tested it works by importing the library:
nix-shell -p "octave.withPackages (ps: [ ps.optim ])" --run 'octave
--eval "pkg load optim; disp(\"Success!\")"'

pkgs/development/octave-modules/optim/default.nix
@@ -10,11 +10,11 @@
 
 buildOctavePackage rec {
   pname = "optim";
-  version = "1.6.2";
+  version = "1.6.3";
 
   src = fetchurl {
     url = "mirror://sourceforge/octave/${pname}-${version}.tar.gz";
-    sha256 = "sha256-VUqOGLtxla6GH1BZwU8aVXhEJlwa3bW/vzq5iFUkeH4=";
+    sha256 = "sha256-Wfs3caLSojE0R1MsWaLgAKanu3pnfz74GD+6qrVJOhQ=";
   };
 
   buildInputs = [
@@ -34,9 +34,10 @@ buildOctavePackage rec {
       publicDomain
     ];
     # Modified BSD code seems removed
-    maintainers = with lib.maintainers; [ ravenjoad ];
+    maintainers = with lib.maintainers; [
+      ravenjoad
+      lnk3
+    ];
     description = "Non-linear optimization toolkit";
-    # Hasn't been updated since 2022, and fails to build with octave >= 10.1.0
-    broken = true;
   };
 }