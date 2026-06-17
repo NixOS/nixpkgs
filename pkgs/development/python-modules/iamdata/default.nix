0e9972a461167e1d5dcca02575e0e8d36a99a420
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-17 08:44:12 +0200

    python3Packages.iamdata: 0.1.202606161 -> 0.1.202606171
    Diff: https://github.com/cloud-copilot/iam-data-python/compare/v0.1.202606161...v0.1.202606171
    
    Changelog: https://github.com/cloud-copilot/iam-data-python/releases/tag/v0.1.202606171

--- pkgs/development/python-modules/iamdata/default.nix
+++ pkgs/development/python-modules/iamdata/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "iamdata";
-  version = "0.1.202606161";
+  version = "0.1.202606171";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "cloud-copilot";
     repo = "iam-data-python";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-B5xXUgF709FCmiLD8t0nNhDhCIid1IDW1cTgJ6UR7c0=";
+    hash = "sha256-/ObglIjoGPf96vD6BCweXvwHE7yvqSSdqZPSUmn5jwg=";
   };
 
   __darwinAllowLocalNetworking = true;
