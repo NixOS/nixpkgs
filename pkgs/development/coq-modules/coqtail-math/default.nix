d496653b3d9f8ba48b04b8dc1c1dfdd363ba9ba0
Yifei Sun <ysun@hey.com>
2026-06-17 21:33:59 +0200

    coqPackages.coqtail-math: modernize mkCoqDerivation release hash format

--- pkgs/development/coq-modules/coqtail-math/default.nix
+++ pkgs/development/coq-modules/coqtail-math/default.nix
@@ -22,9 +22,9 @@ mkCoqDerivation {
       }
     ] null;
 
-  release."8.14".sha256 = "sha256:1k8f8idjnx0mf4z479vcx55iz42rjxrbplbznv80m2famxakq03c";
+  release."8.14".hash = "sha256:1k8f8idjnx0mf4z479vcx55iz42rjxrbplbznv80m2famxakq03c";
   release."20201124".rev = "5c22c3d7dcd8cf4c47cf84a281780f5915488e9e";
-  release."20201124".sha256 = "sha256-wd+Lh7dpAD4zfpyKuztDmSFEZo5ZiFrR8ti2jUCVvoQ=";
+  release."20201124".hash = "sha256-wd+Lh7dpAD4zfpyKuztDmSFEZo5ZiFrR8ti2jUCVvoQ=";
   mlPlugin = true;
   meta = {
     license = lib.licenses.lgpl3Only;
