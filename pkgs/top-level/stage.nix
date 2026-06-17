3c4f976c69ef38d87fccac361121d1399aff64ef
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2026-06-17 15:26:57 +0200

    config: Introduce attrPathsDisallowedForInternalUse
    This is very similar to how the aliases.nix overlay can be removed to
    prevent usage of them in Nixpkgs itself, but this config attribute
    allows preventing use of other existing attributes in Nixpkgs.
    
    Needed for a follow-up commit.
    
    (cherry picked from commit 7cc4d7dea8843ae0176e465f333587e0b657b042)

--- pkgs/top-level/stage.nix
+++ pkgs/top-level/stage.nix
@@ -316,6 +316,31 @@ let
     };
   };
 
+  # Replaces the attributes in config.attrPathsDisallowedForInternalUse with aborts.
+  # Not throws because those would be ignored by nix-env, which is what CI uses to evaluate everything
+  # See also ./default.nix, where these attributes are added back again so they're still checked by CI
+  internallyDisallowedAttrPathsOverlay =
+    final: prev:
+    if config.attrPathsDisallowedForInternalUse == [ ] then
+      { }
+    else
+      {
+        # So that ./default.nix can add them back again outside the fixed point
+        # Don't use this in packages!
+        __internalBeforeInternallyDisallowedAttrPathsOverlay = prev;
+      }
+      // lib.updateManyAttrsByPath (map (
+        { attrPath, reason }:
+        {
+          path = attrPath;
+          update =
+            _:
+            abort "${lib.concatStringsSep "." attrPath} is disallowed from being used within Nixpkgs${
+              lib.optionalString (reason != null) ", because ${reason}"
+            }";
+        }
+      ) config.attrPathsDisallowedForInternalUse) prev;
+
   # The complete chain of package set builders, applied from top to bottom.
   # stdenvOverlays must be last as it brings package forward from the
   # previous bootstrapping phases which have already been overlaid.
@@ -331,6 +356,7 @@ let
       aliases
       variants
       configOverrides
+      internallyDisallowedAttrPathsOverlay
     ]
     ++ overlays
     ++ [
