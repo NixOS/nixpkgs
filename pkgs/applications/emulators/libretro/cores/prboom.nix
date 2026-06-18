b0f0dbae3be4423cdf8b7f6be313b3b9a4dd92d7
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-18 21:55:05 +0000

    libretro.prboom: 0-unstable-2026-05-20 -> 0-unstable-2026-06-15

--- pkgs/applications/emulators/libretro/cores/prboom.nix
+++ pkgs/applications/emulators/libretro/cores/prboom.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "prboom";
-  version = "0-unstable-2026-05-20";
+  version = "0-unstable-2026-06-15";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "libretro-prboom";
-    rev = "648223372e24773821a2041e0c39728f723badf4";
-    hash = "sha256-LyITrBmL5me5zWOfMRZAeCM9rDS19aV2TAFuU8QU/Kw=";
+    rev = "b760de6a1eb1bfcd7833c5b677ff9170d104302f";
+    hash = "sha256-JZrQbhntCGV4WIktUbtVXCSGAEmh9gTPn+tLiMyt6ZY=";
   };
 
   makefile = "Makefile";
