db5b29f2e8246f31f5c7f919694362e83915f221
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2026-06-19 00:59:48 +0300

    patchance: 1.1.0 → 1.4.0

--- pkgs/by-name/pa/patchance/package.nix
+++ pkgs/by-name/pa/patchance/package.nix
@@ -1,46 +1,83 @@
 {
   lib,
   fetchurl,
-  python3Packages,
-  libjack2,
-  qt5,
-  which,
+  python3,
+  qt6,
   bash,
 }:
 
-python3Packages.buildPythonApplication (finalAttrs: {
+let
+  # Executable dependencies
+  exe = {
+    bash = lib.getExe bash;
+    python3 = lib.getExe python3;
+    lrelease = lib.getExe' qt6.qttools "lrelease";
+    rcc = "${qt6.qtbase}/libexec/rcc";
+  };
+in
+
+python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "patchance";
-  version = "1.1.0";
+  version = "1.4.0";
 
   src = fetchurl {
     url = "https://github.com/Houston4444/Patchance/releases/download/v${finalAttrs.version}/Patchance-${finalAttrs.version}-source.tar.gz";
-    hash = "sha256-wlkEKkPH2C/y7TQicIVycWbtLUdX2hICcUWi7nFN51w=";
+    hash = "sha256-U7/l+zz8T1cmEEYwr68N+J6G/K0rbJFmKUOveeVv7kw=";
   };
 
   pyproject = false;
 
   nativeBuildInputs = [
-    python3Packages.pyqt5 # pyuic5 and pyrcc5 to build resources.
-    qt5.qttools # lrelease to build translations.
-    which # which to find lrelease.
-    qt5.wrapQtAppsHook
+    python3.pkgs.pyqt6 # “pyuic6” executable
+    qt6.wrapQtAppsHook
   ];
+
   buildInputs = [
-    libjack2
-    bash
+    qt6.qtsvg
   ];
-  propagatedBuildInputs = [ python3Packages.pyqt5 ];
+
+  propagatedBuildInputs = [
+    python3.pkgs.pyqt6
+    python3.pkgs.qtpy
+    python3.pkgs.jack-client
+  ];
 
   dontWrapQtApps = true; # The program is a python script.
 
   installFlags = [ "PREFIX=$(out)" ];
 
-  makeWrapperArgs = [
-    "--suffix"
-    "LD_LIBRARY_PATH"
-    ":"
-    (lib.makeLibraryPath [ libjack2 ])
+  makeFlags = [
+    "QT_VERSION=6"
+    "QT_API=PyQt6"
+    "PYUIC=pyuic6"
+    "SHELL=${exe.bash}"
+    "RCC=${exe.rcc}"
+    "LRELEASE=${exe.lrelease}"
   ];
+
+  postPatch = ''
+    # Fix paths in Makefile and avoid invoking “which”.
+    #
+    # Note that `rcc -g python` thing seems to be already fixed in `master`
+    # but not in the latest stable 1.4.0 release.
+    # See https://github.com/Houston4444/Patchance/blob/v1.4.0/Makefile#L85C2-L85C15
+    # and https://github.com/Houston4444/Patchance/commit/1986cc5
+    # TODO: Remove it for the next version update when it releases.
+    substituteInPlace Makefile \
+      --replace-fail '$(shell which $(PYTHON))' ${lib.escapeShellArg exe.python3} \
+      --replace-fail '$(shell which $(LRELEASE))' ${lib.escapeShellArg exe.lrelease} \
+      --replace-fail '$(shell which $(RCC))' ${lib.escapeShellArg exe.rcc} \
+      --replace-fail 'rcc -g python' ${lib.escapeShellArg "${exe.rcc} -g python"}
+    substituteInPlace HoustonPatchbay/Makefile \
+      --replace-fail '$(shell which $(LRELEASE))' ${lib.escapeShellArg exe.lrelease}
+
+    # Fix a Python syntax bug.
+    # See https://github.com/Houston4444/HoustonPatchbay/issues/27
+    substituteInPlace HoustonPatchbay/source/patch_engine/patch_engine.py \
+      --replace-fail \
+        '"JACK seems to be compiled without Metadatas support, ",' \
+        '"JACK seems to be compiled without Metadatas support, "'
+  '';
 
   preFixup = ''
     makeWrapperArgs+=("''${qtWrapperArgs[@]}")
@@ -58,7 +95,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
     description = "JACK Patchbay GUI";
     mainProgram = "patchance";
     license = lib.licenses.gpl2;
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.unclechu ];
     platforms = lib.platforms.linux;
   };
 })
