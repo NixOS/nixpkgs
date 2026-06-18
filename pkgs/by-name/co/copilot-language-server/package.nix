bb4d3cc7e609d90a7cf122c678969c38b31294d0
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-18 20:54:56 +0000

    copilot-language-server: 1.498.0 -> 1.509.0

--- pkgs/by-name/co/copilot-language-server/package.nix
+++ pkgs/by-name/co/copilot-language-server/package.nix
@@ -10,11 +10,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "copilot-language-server";
-  version = "1.498.0";
+  version = "1.509.0";
 
   src = fetchzip {
     url = "https://github.com/github/copilot-language-server-release/releases/download/${finalAttrs.version}/copilot-language-server-js-${finalAttrs.version}.zip";
-    hash = "sha256-WpZKsi8OgF72cuAxSD4AHJBZkvRsPtveiG5AmaQH320=";
+    hash = "sha256-PY4pr9FHKHSJjNctq7bGlsZ1yvDM6IqbmfobI1WsDP0=";
     stripRoot = false;
   };
 
