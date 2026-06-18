13522aacab7ffdbfc32789d1cdd1e1bf25a9c915
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-18 03:20:53 +0000

    python3Packages.cxxheaderparser: 1.7.3 -> 1.8.1

--- pkgs/development/python-modules/cxxheaderparser/default.nix
+++ pkgs/development/python-modules/cxxheaderparser/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "cxxheaderparser";
-  version = "1.7.3";
+  version = "1.8.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "robotpy";
     repo = "cxxheaderparser";
     tag = version;
-    hash = "sha256-1i8HbVq1dEXktKCZg/ZGX0n5J8LrZx64flMCP/c6ltI=";
+    hash = "sha256-l81b/M6ir3aWnycrl8RfuN3JQYkcRcTFRhejBWXGgb4=";
   };
 
   postPatch = ''
