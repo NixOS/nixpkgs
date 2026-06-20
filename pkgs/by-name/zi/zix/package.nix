ff85c2b93b8e5cea4a224c58a422dd61c6ab7dd8
Aliaksandr <grubian2@gmail.com>
2026-06-20 05:15:15 +0300

    zix: 0.6.2 -> 0.8.2

--- pkgs/by-name/zi/zix/package.nix
+++ pkgs/by-name/zi/zix/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zix";
-  version = "0.6.2";
+  version = "0.8.2";
 
   src = fetchFromGitLab {
     owner = "drobilla";
     repo = "zix";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-1fdW014QKvTYHaEmDsivUVPzF/vZgnW3Srk6edp6G1o=";
+    hash = "sha256-EfljHq73FA5llX2nnr6pn75nB/FbKnWvr3/Phl5716M=";
   };
 
   nativeBuildInputs = [
