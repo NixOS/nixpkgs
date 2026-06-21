f15419211a12508df17323c281ac662c72e4b71e
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-21 11:36:44 +0000

    buildbox: 1.4.8 -> 1.4.10
    (cherry picked from commit 4132d37c4d7b94b0951fd2a03d3db12805bca481)

--- pkgs/by-name/bu/buildbox/package.nix
+++ pkgs/by-name/bu/buildbox/package.nix
@@ -23,13 +23,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "buildbox";
-  version = "1.4.8";
+  version = "1.4.10";
 
   src = fetchFromGitLab {
     owner = "BuildGrid";
     repo = "buildbox/buildbox";
     tag = finalAttrs.version;
-    hash = "sha256-P7CFgK5CkHAf5gddmm0SBGIkC/AAdCO5pkya/XNuby0=";
+    hash = "sha256-WWvaOPOckqyZ1ehEbPLcsP8qET9cq/3PkG0ep37ypn0=";
   };
 
   nativeBuildInputs = [
