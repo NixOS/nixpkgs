38ce74ebc3b4b63d94d841d7beefe911aaaaa2af
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-21 05:28:09 +0000

    python3Packages.semchunk: 4.0.0 -> 4.1.0

--- pkgs/development/python-modules/semchunk/default.nix
+++ pkgs/development/python-modules/semchunk/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "semchunk";
-  version = "4.0.0";
+  version = "4.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "isaacus-dev";
     repo = "semchunk";
     tag = "v${version}";
-    hash = "sha256-8bceOMMnQ4JsbX7zU5zAoyP8esTm83m/a3VwwnUzCAA=";
+    hash = "sha256-jQQNb5E/EarsN9OwlF6l8huX06kM2EChfUYW+MM5uxA=";
   };
 
   build-system = [
