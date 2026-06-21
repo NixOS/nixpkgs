4a5e236d3e697b574a2bcedab30f4686e0ad73b8
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-21 07:12:40 +0000

    python3Packages.conda: 26.5.2 -> 26.5.3

--- pkgs/development/python-modules/conda/default.nix
+++ pkgs/development/python-modules/conda/default.nix
@@ -28,7 +28,7 @@
 buildPythonPackage rec {
   __structuredAttrs = true;
   pname = "conda";
-  version = "26.5.2";
+  version = "26.5.3";
   pyproject = true;
 
   src = fetchFromGitHub {
@@ -36,7 +36,7 @@ buildPythonPackage rec {
     owner = "conda";
     repo = "conda";
     tag = version;
-    hash = "sha256-hiH25EcybtyEuks496VgiP4TPwNKI3x1URfwuefJRls=";
+    hash = "sha256-XpgLznbIPPHtmniZWdCWflcvmi3NdOrkpLlxmerlHlA=";
   };
 
   build-system = [
