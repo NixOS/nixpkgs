9b4956d2f728fb5cefb78e869687f22317d75aa4
Xavier Araque <franciscoxa.araquearguello@colaborador.grupoveci.es>
2026-06-21 11:07:59 +0200

    shellbar: fix derivation for sandboxed builds and address review
    - Switch from rec to finalAttrs pattern
    - Add __structuredAttrs and strictDeps
    - Use tag instead of rev in fetchFromGitHub
    - Add missing transitive build inputs (expat, libxdmcp, libsysprof-capture, pcre2)
    - Provide ghostty source and zig dependencies for offline/sandboxed builds
    - Add rendergraf as maintainer
    - Fix formatting (trailing commas)

--- pkgs/by-name/sh/shellbar/package.nix
+++ pkgs/by-name/sh/shellbar/package.nix
@@ -1,6 +1,13 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchzip
+, fetchurl
+, fetchgit
+, linkFarm
+, runCommand
+, runCommandLocal
+, zstd
 , cmake
 , ninja
 , pkg-config
@@ -15,18 +22,48 @@
 , vulkan-headers
 , vulkan-loader
 , glib
+, expat
+, libxdmcp
+, libsysprof-capture
+, pcre2
 , wrapGAppsHook4
 }:
 
-stdenv.mkDerivation rec {
+let
+  ghosttySrc = fetchFromGitHub {
+    owner = "ghostty-org";
+    repo = "ghostty";
+    rev = "b0f8276658fbcc75318d2125d40146074a3fc505";
+    hash = lib.fakeHash;
+  };
+
+  zigDepsRaw = import "${ghosttySrc}/build.zig.zon.nix" {
+    inherit lib linkFarm fetchzip fetchurl fetchgit zstd;
+    inherit runCommandLocal;
+    zig_0_15 = zig;
+  };
+
+  zigDeps = runCommand "zig-global-cache" { } ''
+    mkdir -p "$out/p"
+    for dep in "${zigDepsRaw}/"*; do
+      if [ -e "$dep" ]; then
+        ln -s "$(readlink -f "$dep")" "$out/p/$(basename "$dep")"
+      fi
+    done
+  '';
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "shellbar";
   version = "1.9.0";
+
+  __structuredAttrs = true;
+  strictDeps = true;
 
   src = fetchFromGitHub {
     owner = "rendergraf";
     repo = "shellbar";
-    rev = "v${version}";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-2gVUEVS2Kw3HokJwifztcDE7Nhp8E4+exAK9p7X+1FQ=";
   };
 
   nativeBuildInputs = [
@@ -48,17 +85,32 @@ stdenv.mkDerivation rec {
     vulkan-headers
     vulkan-loader
     glib
+    expat
+    libxdmcp
+    libsysprof-capture
+    pcre2
   ];
+
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail 'set(ZIG_NEED_DOWNLOAD TRUE)' 'set(ZIG_NEED_DOWNLOAD TRUE CACHE BOOL "Whether to download Zig")'
+  '';
 
   cmakeFlags = [
     "-DCMAKE_BUILD_TYPE=Release"
+    "-DFETCHCONTENT_SOURCE_DIR_GHOSTTY=${ghosttySrc}"
+    "-DZIG_EXECUTABLE=${zig}/bin/zig"
+    "-DZIG_NEED_DOWNLOAD=FALSE"
   ];
+
+  ZIG_GLOBAL_CACHE_DIR = "${zigDeps}";
 
   meta = with lib; {
     description = "A Ghostty-like terminal emulator with a configurable command toolbar";
     homepage = "https://github.com/rendergraf/shellbar";
     license = licenses.mit;
-    platforms = platforms.linux;
     mainProgram = "shellbar";
+    maintainers = with maintainers; [ rendergraf ];
+    platforms = platforms.linux;
   };
-}
+})
