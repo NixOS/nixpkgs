38f1515a365ec9cfb8ffc7b0d781456fb0def3cd
Niklas Hambüchen <mail@nh2.me>
2026-06-22 20:17:49 +0000

    plausible: Add new maintainers

--- pkgs/by-name/pl/plausible/package.nix
+++ pkgs/by-name/pl/plausible/package.nix
@@ -215,7 +215,10 @@ beamPackages.mixRelease rec {
     changelog = "https://github.com/plausible/analytics/blob/${src.rev}/CHANGELOG.md";
     description = "Simple, open-source, lightweight (< 1 KB) and privacy-friendly web analytics alternative to Google Analytics";
     mainProgram = "plausible";
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [
+      stepbrobd
+      nh2
+    ];
     platforms = lib.platforms.unix;
   };
 }
