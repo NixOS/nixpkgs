b8aa1711b05fe38ef74695265cc159c414fd6bc9
jcvega <jcvega0b1@gmail.com>
2026-06-22 13:48:59 -0700

    hypr-relay: 0.2.1 -> 0.3.0

--- pkgs/by-name/hy/hypr-relay/package.nix
+++ pkgs/by-name/hy/hypr-relay/package.nix
@@ -8,16 +8,16 @@ rustPlatform.buildRustPackage {
   __structuredAttrs = true;
 
   pname = "hypr-relay";
-  version = "0.2.1";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "Vega-0b1";
     repo = "hypr-relay";
-    rev = "v0.2.1";
-    hash = "sha256-EoZMMTj6ob+Jeeg4DpZirVi2yWMLPVwcmPK1xIHQf/Y=";
+    rev = "v0.3.0";
+    hash = "sha256-IYiUwekQS6cSZ/S/jy6IOmPQOhMRlJpPF5aDS7autXg=";
   };
 
-  cargoHash = "sha256-Bl2J2ti0+CpSq9tNlB+3eJd+mwN6JJF7aNnuW/nJde0=";
+  cargoHash = "sha256-IsTS6OZmGCWFIwl70HOa1f+dEiqriWbAKmvHJ4fSRYg=";
 
   meta = {
     description = "Lightweight daemon for Hyprland that bridges system events to desktop notifications";
