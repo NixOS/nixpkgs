0e1f4fd8d9e95f9f6e35f56ccbc882879d264db4
Colin <colin@uninsane.org>
2026-06-22 22:16:13 +0000

    rdma-core: 62.0 -> 63.0

--- pkgs/by-name/rd/rdma-core/package.nix
+++ pkgs/by-name/rd/rdma-core/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchurl,
   gitUpdater,
   cmake,
   pkg-config,
@@ -16,13 +17,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "rdma-core";
-  version = "62.0";
+  version = "63.0";
 
   src = fetchFromGitHub {
     owner = "linux-rdma";
     repo = "rdma-core";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-1n33KH8HTyZ0jHtDanopxwABiLjAvt+V7lgaeabJs8s=";
+    hash = "sha256-YW6BJS6acj9S8wFXUhC1vrJSm9YowGGuwWEBzQRVyPM=";
   };
 
   strictDeps = true;
@@ -48,9 +49,20 @@ stdenv.mkDerivation (finalAttrs: {
     udev
   ];
 
+  patches = [
+    (fetchurl {
+      # remove when rdma-core 64.0 is released
+      # https://github.com/linux-rdma/rdma-core/pull/1737
+      name = "cmake-allow-overriding-sysusers.d-install-directory";
+      url = "https://github.com/linux-rdma/rdma-core/commit/8b186b5d932701e94bbced83d2f3899ee53f041a.patch?full_index=1";
+      hash = "sha256-Rjknu7mmJL2Sx+Ypq9SRXU4LUiHERs9j5/qMIZaiRTI=";
+    })
+  ];
+
   cmakeFlags = [
     "-DCMAKE_INSTALL_RUNDIR=/run"
     "-DCMAKE_INSTALL_SHAREDSTATEDIR=/var/lib"
+    "-DSYSUSERS_DIR=${placeholder "out"}/lib/sysusers.d"
   ];
 
   postPatch = ''
