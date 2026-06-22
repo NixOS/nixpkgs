NixOS/nixpkgs

commit 4f72341536ca5fdb7520328e835802476a897f1b

valgrind: 3.26.0 -> 3.27.0
Changes: https://valgrind.org/docs/manual/dist.news.html

pkgs/by-name/va/valgrind/package.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation rec {
   pname = "valgrind";
-  version = "3.26.0";
+  version = "3.27.0";
 
   src = fetchurl {
     url = "https://sourceware.org/pub/${pname}/${pname}-${version}.tar.bz2";
-    hash = "sha256-jVTHFwKRBvFkSq2vgCq5aS5T2T3QFcvRnnQZDrpha9c=";
+    hash = "sha256-W1k33oJX7o9RaY6nG5cRrc6YBhqgfapKaF78OvkhW+8=";
   };
 
   patches = [

commit 7ac506e79fd11a772a8326c4cd40478508bb032e

valgrind: backport x86_64-unknown-linux-musl fix
Fixes: 4f72341536ca ("valgrind: 3.26.0 -> 3.27.0")

pkgs/by-name/va/valgrind/package.nix
@@ -30,6 +30,11 @@ stdenv.mkDerivation rec {
       url = "https://bugsfiles.kde.org/attachment.cgi?id=186451";
       hash = "sha256-IGmyHwwGoy00hcz3XxQSDcwcU8zHLBJ9dfqTvWDQ520=";
     })
+    (fetchpatch {
+      name = "reallocarray-test-musl.patch";
+      url = "https://sourceware.org/git/?p=valgrind.git;a=patch;h=991961ece87e4cdc0771a05c956c55baa437bb07";
+      hash = "sha256-U16384rLXMhLE5Em9z8FKYbshPlnq8l9ejC2+epL7M4=";
+    })
 
     # Fix build on armv7l.
     # see also https://bugs.kde.org/show_bug.cgi?id=454346