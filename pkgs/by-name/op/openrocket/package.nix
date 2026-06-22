798fef05542d6880ccd644d99ab2ceae35859d2f
Sapi <144389458+tree-sapii@users.noreply.github.com>
2026-06-22 01:44:22 -0400

    openrocket: 23.09 -> 24.12

--- pkgs/by-name/op/openrocket/package.nix
+++ pkgs/by-name/op/openrocket/package.nix
@@ -2,47 +2,54 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
-  ant,
   jdk17,
   makeWrapper,
-  stripJavaArchivesHook,
+  gradle,
 }:
 
-let
-  jdk = jdk17; # Only java 17 is supported as of 23.09
-in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "openrocket";
-  version = "23.09";
+  version = "24.12";
 
   src = fetchFromGitHub {
     owner = "openrocket";
     repo = "openrocket";
     tag = "release-${finalAttrs.version}";
-    hash = "sha256-Dg/v72N9cDG9Ko5JIcZxGxh+ClRDgf5Jq5DvQyCiYOs=";
+    hash = "sha256-Vb1NkhEkMvotyGzswq3Lq0RbG1rTmtfzRD+MHbsYFWM=";
     fetchSubmodules = true;
   };
 
+  # The mitmCache update script expects core dependency to be built before it pulls the swing dependency
+  # first so we place a dunny jar file, jogamp repositories may fallback to HTTP, we substitute in a arg that allows the fallback
+  postPatch = ''
+    # This will satisfy the ExtraJavaModuleInfoTransform plugin during gradle.fetchDeps
+    # by generating dummy JAR files before Gradle resolves the classpath.
+    mkdir -p dummy_dir
+
+    mkdir -p core/build/libs
+    jar cf core/build/libs/core-${finalAttrs.version}.jar -C dummy_dir .
+
+    mkdir -p swing/build/libs
+    jar cf swing/build/libs/swing-${finalAttrs.version}.jar -C dummy_dir .
+
+    # patch the jogamp repository to allow HTTP fallback redirects
+    substituteInPlace build.gradle \
+      --replace 'url "https://jogamp.org/deployment/maven/"' \
+      'url "https://www.jogamp.org/deployment/maven/"; allowInsecureProtocol = true'
+  '';
+
   nativeBuildInputs = [
-    ant
-    jdk
+    jdk17 # Only java 17 is supported as of 23.09
     makeWrapper
-    stripJavaArchivesHook
+    gradle
   ];
 
-  buildPhase = ''
-    runHook preBuild
-    ant
-    runHook postBuild
-  '';
+  mitmCache = gradle.fetchDeps {
+    pkg = finalAttrs.finalPackage;
+    data = ./deps.json;
+  };
 
   doCheck = true;
-
-  checkPhase = ''
-    runHook preCheck
-    ant unittest
-    runHook postCheck
-  '';
 
   installPhase = ''
     runHook preInstall
@@ -50,10 +57,10 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     sed -i "s|Icon=.*|Icon=openrocket|g" snap/gui/openrocket.desktop
     install -Dm644 snap/gui/openrocket.desktop -t $out/share/applications
     install -Dm644 snap/gui/openrocket.png -t $out/share/icons/hicolor/256x256/apps
-    install -Dm644 swing/build/jar/OpenRocket.jar -t $out/share/openrocket
+    install -Dm644 build/libs/OpenRocket-${finalAttrs.version}.jar -t $out/share/openrocket
 
-    makeWrapper ${jdk}/bin/java $out/bin/openrocket \
-        --add-flags "-jar $out/share/openrocket/OpenRocket.jar"
+    makeWrapper ${lib.getExe jdk17} $out/bin/openrocket \
+        --add-flags "-jar $out/share/openrocket/OpenRocket-${finalAttrs.version}.jar"
 
     runHook postInstall
   '';
@@ -65,10 +72,10 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     license = lib.licenses.gpl3Plus;
     mainProgram = "openrocket";
     maintainers = with lib.maintainers; [ tomasajt ];
-    platforms = jdk.meta.platforms;
+    platforms = jdk17.meta.platforms;
     sourceProvenance = with lib.sourceTypes; [
       fromSource
-      binaryBytecode # source bundles dependencies as jars
+      binaryBytecode
     ];
   };
 })
