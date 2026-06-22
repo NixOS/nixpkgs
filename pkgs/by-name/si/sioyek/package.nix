21d55ae372c16ecd4c4cbb22db9eb92f9b10d688
Stephan <stephan.siegl@me.com>
2026-06-22 17:50:28 +0200

    sioyek: 2.0.0-unstable-2026-06-06 -> 2.0.0-unstable-2026-06-13

--- pkgs/by-name/si/sioyek/package.nix
+++ pkgs/by-name/si/sioyek/package.nix
@@ -15,13 +15,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "sioyek";
-  version = "2.0.0-unstable-2026-06-06";
+  version = "2.0.0-unstable-2026-06-13";
 
   src = fetchFromGitHub {
     owner = "ahrm";
     repo = "sioyek";
-    rev = "96a5b0aebbacffdd9c8ddefd6efd3413828d0f37";
-    hash = "sha256-kCblPkBZETNmkX1RCQRR/zLBbAMhlB2svNCSltSeNlg=";
+    rev = "552008ace47614dbc21a0d41060b25f08767a4b6";
+    hash = "sha256-yzisnz/7pl7IgUK24CqivPoau17rIiu7n0Z20q7UXy8=";
   };
 
   buildInputs = [
@@ -91,6 +91,7 @@ stdenv.mkDerivation (finalAttrs: {
       podocarp
       stephen-huan
       xyven1
+      stephsi
     ];
     platforms = lib.platforms.unix;
   };
