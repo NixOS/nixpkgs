06c60b8e0e2147ce6c28236e080802ac1e3be1c7
Doron Behar <doron.behar@gmail.com>
2026-06-23 01:23:11 +0300

    python3Packages.scippnexus: init at 26.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17782,6 +17782,8 @@ self: super: with self; {
 
   scipp = callPackage ../development/python-modules/scipp { };
 
+  scippnexus = callPackage ../development/python-modules/scippnexus { };
+
   scipy = callPackage ../development/python-modules/scipy { };
 
   scipy-stubs = callPackage ../development/python-modules/scipy-stubs { };
