53e656a2415676f0b44c49fc6df71a6b8c71ad04
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-22 18:38:17 +0000

    go-audit: 1.2.1 -> 1.2.2

--- pkgs/by-name/go/go-audit/package.nix
+++ pkgs/by-name/go/go-audit/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "go-audit";
-  version = "1.2.1";
+  version = "1.2.2";
 
   src = fetchFromGitHub {
     owner = "slackhq";
     repo = "go-audit";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-VzxFhaeETmhjYWBLQil10OhV4k8w6EHfV0qnun73gb0=";
+    sha256 = "sha256-Si8OuvQOyRN17DJC3mvFS7xkpbID8zcAD8n44VSLsTA=";
   };
 
-  vendorHash = "sha256-g5NP5QY8kNPQLLT9GGqHIQXkaBoZ+Wqna7KknCIwBNM=";
+  vendorHash = "sha256-eUuLLpF8p7nTiddRy0hlqZ+n+OyvyJ1D20X1jvqKVC8=";
 
   # Tests need network access
   doCheck = false;
