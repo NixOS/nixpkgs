92dfbbfc3bca6792afd5e1163eea784b410b5c3b
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-23 18:24:59 +0000

    railway: 4.64.0 -> 5.20.0

--- pkgs/by-name/ra/railway/package.nix
+++ pkgs/by-name/ra/railway/package.nix
@@ -8,16 +8,16 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "railway";
-  version = "4.64.0";
+  version = "5.20.0";
 
   src = fetchFromGitHub {
     owner = "railwayapp";
     repo = "cli";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-0Zgdazs6AANKa3QtRHuYmudLpkvMHAzsaTCg3/cSN2o=";
+    hash = "sha256-Di0L9+DT55XYEGsy1rVJSc8BV0mC7R+SLFtyY1SkMBQ=";
   };
 
-  cargoHash = "sha256-K1W8tn3Nr/PqWHHHF6mGsNqoAzekjEjqAoa6/4MZKfI=";
+  cargoHash = "sha256-fHULX+ZqzgawqnL7Su2aUXFo8JwvmI57gEp0aRvHrNA=";
 
   nativeBuildInputs = [
     pkg-config
