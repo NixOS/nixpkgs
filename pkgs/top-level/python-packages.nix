8fb3a890e51ce8442616966bf676ca3489b75b02
Doron Behar <doron.behar@gmail.com>
2026-06-23 04:29:12 +0300

    python3Packages.adbc-driver-manager: init at 1.11.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -83,6 +83,8 @@ self: super: with self; {
 
   adb-shell = callPackage ../development/python-modules/adb-shell { };
 
+  adbc-driver-manager = callPackage ../development/python-modules/adbc-driver-manager { };
+
   adblock = callPackage ../development/python-modules/adblock { };
 
   adbutils = callPackage ../development/python-modules/adbutils { };
