768226adc78862b417e55117ba77b497b2fc2727
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-23 12:23:59 +0000

    weaviate: 1.38.0 -> 1.38.1

--- pkgs/by-name/we/weaviate/package.nix
+++ pkgs/by-name/we/weaviate/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "weaviate";
-  version = "1.38.0";
+  version = "1.38.1";
 
   src = fetchFromGitHub {
     owner = "weaviate";
     repo = "weaviate";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-dYApKcDW8Cs/T3uszohy0PQqKar9lkiNBMY0mnPr5xs=";
+    hash = "sha256-JPhyO03aDbz0DHJYgu/wx32eiEy9G5nsK8ryF+Ao55g=";
   };
 
   vendorHash = "sha256-1uOPet4HywajcPbZuT2ePRUrcT9mwNx0n2vFUkxUn6s=";
