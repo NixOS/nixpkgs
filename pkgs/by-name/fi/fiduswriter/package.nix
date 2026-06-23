7b92f19f85a023649a53e8005bfd2ccf4e21e6dc
eljamm <fedi.jamoussi@protonmail.ch>
2026-06-23 17:32:34 +0200

    wip: fiduswriter: vendor pnpm lockfile; build still fails

--- pkgs/by-name/fi/fiduswriter/package.nix
+++ pkgs/by-name/fi/fiduswriter/package.nix
@@ -8,6 +8,7 @@
   pnpm_11,
   fetchPnpmDeps,
   makeWrapper,
+  rsync,
 }:
 
 python3Packages.buildPythonApplication (finalAttrs: {
@@ -23,24 +24,43 @@ python3Packages.buildPythonApplication (finalAttrs: {
     hash = "sha256-NkFJehSgwYwYUZaznZW63KEXR1wTf+Hpa+8ZM71aZ84=";
   };
 
-  # pnpmDeps = fetchPnpmDeps {
-  #   inherit (finalAttrs) pname version src;
-  #   pnpm = pnpm_11;
-  #   fetcherVersion = 4;
-  #   hash = "";
-  # };
+  pnpmDeps = fetchPnpmDeps {
+    inherit (finalAttrs)
+      pname
+      version
+      src
+      ;
+    postPatch = finalAttrs.postPatch + "";
+    pnpm = pnpm_11;
+    fetcherVersion = 4;
+    hash = "sha256-8JqolPCb9HtfCIgRfM5a3BGozh4alYmEWuWSBUykAZg=";
+
+    nativeBuildInputs = [
+      nodejs
+      rsync
+    ];
+
+    env.PYTHONPATH = "${finalAttrs.passthru.pythonPath}";
+  };
 
   postPatch = ''
     substituteInPlace pyproject.toml \
       --replace-fail "setuptools>=82.0.1" "setuptools"
+
+    # Upstream uses a `package.json5` file for multiple components, each with
+    # its own dependencies. To merge all of them into a single `package.json`
+    # and get the associated lockfile, run: `python manage.py npm_install`
+    mkdir -p .transpile
+    cp ${./pnpm-lock.yaml} ./pnpm-lock.yaml
+    cp ${./package.json} ./package.json
   '';
 
   nativeBuildInputs = [
     gettext
     makeWrapper
     nodejs
-    # pnpmConfigHook
-    # pnpm_11
+    pnpmConfigHook
+    pnpm_11
   ];
 
   build-system = with python3Packages; [
@@ -136,6 +156,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
   '';
 
   env.FIDUS_OUT_DIR = "${placeholder "out"}/${python3Packages.python.sitePackages}";
+  env.PYTHONPATH = "${finalAttrs.passthru.pythonPath}";
 
   makeWrapperArgs = [
     "--prefix"
