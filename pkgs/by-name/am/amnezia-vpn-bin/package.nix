a4f72b0445ba7ab6e75bb22c8993fdf5f0366ec6
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-23 03:50:04 +0000

    amnezia-vpn-bin: 4.8.18.0 -> 4.8.19.0

--- pkgs/by-name/am/amnezia-vpn-bin/package.nix
+++ pkgs/by-name/am/amnezia-vpn-bin/package.nix
@@ -29,13 +29,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "amnezia-vpn-bin";
-  version = "4.8.18.0";
+  version = "4.8.19.0";
 
   __structuredAttrs = true;
 
   src = fetchzip {
     url = "https://github.com/amnezia-vpn/amnezia-client/releases/download/${finalAttrs.version}/AmneziaVPN_${finalAttrs.version}_linux_x64.tar";
-    hash = "sha256-Mypzq/Lm+JI0HljRJ6/0ZDFlwOeVbZB+Lmuy33xE06M=";
+    hash = "sha256-2bCP5yeaH0yQcjgwnrCgSnF1vafhrf+AlGyVPPgbK0k=";
   };
 
   strictDeps = true;
