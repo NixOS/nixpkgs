NixOS/nixpkgs

commit 6d8c12b2b36e3d5993c522516e533a4c740c27fd

lttng-ust: 2.14.0 -> 2.15.0
Changes: https://lore.kernel.org/lkml/9592e7ac-2ff5-4fc4-94cc-ee3de89fa541@efficios.com/

pkgs/by-name/lt/lttng-ust/package.nix
@@ -26,13 +26,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lttng-ust";
-  version = "2.14.0";
+  version = "2.15.0";
 
   src = fetchFromGitHub {
     owner = "lttng";
     repo = "lttng-ust";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-9WZDjOGfflEc6BUUO3W70KeLcZnTaePkF8eg8Ns/lQc=";
+    hash = "sha256-LOZo/sVb9eG39Gg1PBs2FoLHTeOD5us9wW4kUwNo8o0=";
   };
 
   outputs = [

commit f4caa95ddef0d1e67621f20a82f28ed639dc6f6c

lttng-ust: fix musl crashes
Caught by the tests.  I've linked to my submitted upstream patch, but
I'm not confident enough in it to apply it here without upstream
review, so for now just build with an increased stack size across the
board.  We can remove it if upstream does something to address the
stack size issue.

Fixes: 6d8c12b2b36e ("lttng-ust: 2.14.0 -> 2.15.0")

pkgs/by-name/lt/lttng-ust/package.nix
@@ -67,7 +67,15 @@ stdenv.mkDerivation (finalAttrs: {
     patchShebangs .
   '';
 
-  configureFlags = [ "--disable-examples" ];
+  configureFlags = [
+    "--disable-examples"
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isMusl [
+    # lttng-ust puts a single array on the stack that's the size of
+    # musl's whole default stack.
+    # https://review.lttng.org/c/lttng-ust/+/18160
+    "CFLAGS=-Wl,-z,stack-size=2097152"
+  ];
 
   doCheck = true;
 