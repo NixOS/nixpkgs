3d4eab4c41992e02a59e8ee47a8eed889472eeca
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-23 17:30:22 +0200

    baddns: 1.12.294 -> 2.4.1
    Changelog: https://github.com/blacklanternsecurity/baddns/releases/tag/2.4.1

--- pkgs/by-name/ba/baddns/package.nix
+++ pkgs/by-name/ba/baddns/package.nix
@@ -7,26 +7,24 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "baddns";
-  version = "1.12.294";
+  version = "2.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "blacklanternsecurity";
     repo = "baddns";
     tag = finalAttrs.version;
-    hash = "sha256-HAVoCyI7yxCdAR4qq7yXJz3YxkPnhBdeXLJZmzZpwF4=";
+    hash = "sha256-3SKR94/KBjTxk7swPKaIn2zzAjYMSEqqLALeCBjwMFg=";
   };
 
-  pythonRelaxDeps = true;
-
-  build-system = with python3.pkgs; [
-    poetry-core
-    poetry-dynamic-versioning
-  ];
+  build-system = with python3.pkgs; [ hatchling ];
 
   dependencies = with python3.pkgs; [
     colorama
+    cloudcheck
     dnspython
+    blastdns
+    blasthttp
     httpx
     python-dateutil
     python-whois
@@ -49,15 +47,26 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
   disabledTests = [
     # Tests require network access
     "test_cli_cname_http"
+    "test_cli_cname_nxdomain"
     "test_cli_direct"
     "test_cli_validation_customnameservers_valid"
     "test_cname_http_bigcartel_match"
     "test_cname_whois_unregistered_baddata"
     "test_cname_whois_unregistered_match"
     "test_cname_whois_unregistered_missingdata"
+    "test_custom_signatures_dir"
+    "test_debug_mode"
+    "test_default_resolver"
+    "test_dmarc"
+    "test_min_confidence_confirmed_excludes_high"
+    "test_min_confidence_filters_findings"
+    "test_min_severity_critical_excludes_medium"
+    "test_min_severity_low_includes_medium"
     "test_modules_customnameservers"
     "test_references_cname_css"
     "test_references_cname_js"
+    "test_silent_mode"
+    "test_spf"
   ];
 
   meta = {
