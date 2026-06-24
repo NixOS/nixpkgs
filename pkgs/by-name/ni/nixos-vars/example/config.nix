7f054121de0fd4a1de8e6c8a4dab0d10586922f1
prescientmoon <git@moonythm.dev>
2026-06-24 13:01:45 +0200

    nixos-vars: Implement rebuild order computation & checking whether files exist according to the backend
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- pkgs/by-name/ni/nixos-vars/example/config.nix
+++ pkgs/by-name/ni/nixos-vars/example/config.nix
@@ -7,9 +7,15 @@ in
     generatorBackends.example = {
       get = noop;
       set = noop;
-      exists = noop;
       deploy = noop;
       fixup = noop; # This one's optional, but I wanted to make sure that works
+      exists =
+        pkgs:
+        pkgs.writeShellScript "exists" ''
+          if [[ ! -f ./generators/$1/files/$2 ]]; then 
+            exit 1
+          fi
+        '';
     };
 
     generators.example = {
@@ -18,6 +24,16 @@ in
         pkgs:
         pkgs.writeShellScript "gen-example" ''
           echo "Hewwo!" > $out/example
+        '';
+    };
+
+    generators.derived = {
+      dependencies = [ "example" ];
+      files.derived = { };
+      script =
+        pkgs:
+        pkgs.writeShellScript "gen-derived" ''
+          cp $in/example $out/derived
         '';
     };
   };
