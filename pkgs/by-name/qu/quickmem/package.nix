d84e08681c2b49155152ff98dbc393e8a828d8c1
toonn <toonn@toonn.io>
2026-06-24 20:57:10 +0200

    quickmem: 0.3.0 -> 1.0.0

--- pkgs/by-name/qu/quickmem/package.nix
+++ pkgs/by-name/qu/quickmem/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "quickmem";
-  version = "0.3.0";
+  version = "1.0.0";
 
   src = fetchFromGitLab {
     owner = "arpa2";
     repo = "Quick-MEM";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-cqg8QN4/I+zql7lVDDAgFA05Dmg4ylBTvPSPP7WATdc=";
+    hash = "sha256-I6WTBzY275a82YM+qsH9k2Zbb8SqAIF3qboxv9o3f4Q=";
   };
 
   nativeBuildInputs = [
