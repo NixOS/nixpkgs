6175f9c52533fc18a2da3f4d3bbc13beebe38bdc
prescientmoon <git@moonythm.dev>
2026-06-24 16:49:38 +0200

    nixos-vars: Add rudimentary error handling to generation logic
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- pkgs/by-name/ni/nixos-vars/src/generate.py
+++ pkgs/by-name/ni/nixos-vars/src/generate.py
@@ -11,40 +11,47 @@ from .error import VarsError
 def generate_vars(args: VarsArgs, config: VarsConfig):
 	order = rebuild_order(config)
 
-	try:
-		# NOTE: we are going to run the scripts inside bubblewrap, thus sharing a
-		# single temporary directory is not a concern.
-		with tempfile.TemporaryDirectory() as temp:
-			temp = Path(temp)
-			for entry in order:
-				in_dir = temp / entry / "in"
-				out_dir = temp / entry / "out"
-				os.makedirs(in_dir)
-				os.makedirs(out_dir)
+	# NOTE: we are going to run the scripts inside bubblewrap, thus sharing a
+	# single temporary directory is not a concern.
+	with tempfile.TemporaryDirectory() as temp:
+		temp = Path(temp)
+		for entry in order:
+			in_dir = temp / entry / "in"
+			out_dir = temp / entry / "out"
+			os.makedirs(in_dir)
+			os.makedirs(out_dir)
 
-				generator = config.generators[entry]
-				binary = build_binary(generator.script)
+			generator = config.generators[entry]
+			binary = build_binary(generator.script)
 
-				for dep_name in generator.dependencies:
-					dep = config.generators[dep_name]
-					os.makedirs(in_dir / dep_name)
-					for file in dep.files:
+			for dep_name in generator.dependencies:
+				dep = config.generators[dep_name]
+				os.makedirs(in_dir / dep_name)
+				for file in dep.files:
+					try:
 						get_secret(
 							config, dep, file, in_dir / dep_name / file.name
 						)
+					except subprocess.CalledProcessError as e:
+						raise VarsError(
+							f"Error getting '{entry}/{file.name}': {e.stderr}"
+						)
 
-				print(binary, in_dir, out_dir)
-				result = subprocess.run(
+			try:
+				subprocess.run(
 					[binary],
 					env={"in": in_dir, "out": out_dir},
 					capture_output=True,
 					check=True,
 					text=True,
 				)
+			except subprocess.CalledProcessError as e:
+				raise VarsError(f"Error generating '{entry}': {e.stderr}")
 
-				for file in generator.files:
+			for file in generator.files:
+				try:
 					set_secret(config, generator, file, out_dir / file.name)
-
-				print(result)
-	except subprocess.CalledProcessError as e:
-		raise VarsError(f"Error building:\n{e.stderr}")
+				except subprocess.CalledProcessError as e:
+					raise VarsError(
+						f"Error setting '{entry}/{file.name}': {e.stderr}"
+					)
