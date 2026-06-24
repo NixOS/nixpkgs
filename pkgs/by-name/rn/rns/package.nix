d94065c5dd1280a15f63eb75a80740c2097bbbe5
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-06-24 22:55:48 +0200

    rns: use `passthru.withLxmf`

--- pkgs/by-name/rn/rns/package.nix
+++ pkgs/by-name/rn/rns/package.nix
@@ -2,19 +2,4 @@
   python3Packages,
 }:
 
-let
-  pythonPackages = python3Packages.overrideScope (
-    self: super: {
-      lxmf = super.lxmf.override {
-        propagateRns = false;
-      };
-    }
-  );
-in
-pythonPackages.toPythonApplication (
-  pythonPackages.rns.overridePythonAttrs (old: {
-    dependencies = old.dependencies ++ [
-      pythonPackages.lxmf
-    ];
-  })
-)
+python3Packages.toPythonApplication python3Packages.rns.withLxmf
