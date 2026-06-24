d5b54160f035d2b5e7e1a8cc8274f70ae04d262a
eljamm <fedi.jamoussi@protonmail.ch>
2026-06-24 11:00:01 +0200

    wip: fiduswriter: compile frontend successfully

---
+++ pkgs/by-name/fi/fiduswriter/frontend.nix
@@ -0,0 +1,100 @@
+{
+  lib,
+  stdenv,
+  python3Packages,
+  fiduswriter,
+  fetchPnpmDeps,
+
+  gettext,
+  nodejs,
+  pnpmConfigHook,
+  pnpm_11,
+  makeWrapper,
+  rsync,
+
+  python3,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "fiduswriter-frontend";
+  inherit (fiduswriter)
+    version
+    src
+    ;
+  __structuredAttrs = true;
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "setuptools>=82.0.1" "setuptools"
+
+    # Upstream uses a `package.json5` file for multiple components, each with
+    # its own dependencies. To merge all of them into a single `package.json`
+    # and get the associated lockfile, run: `python manage.py npm_install`
+    mkdir -p fiduswriter/.transpile
+    cp --no-preserve=mode ${./pnpm-lock.yaml} fiduswriter/.transpile/pnpm-lock.yaml
+    cp --no-preserve=mode ${./package.json} fiduswriter/.transpile/package.json
+    # (for fetchPnpmDeps)
+    cp fiduswriter/.transpile/* .
+  '';
+
+  outputs = [
+    "out"
+    "node_modules"
+  ];
+
+  pnpmRoot = "fiduswriter/.transpile";
+  pnpmDeps = fetchPnpmDeps {
+    inherit (finalAttrs)
+      pname
+      version
+      src
+      postPatch
+      ;
+    pnpm = pnpm_11;
+    fetcherVersion = 4;
+    hash = "sha256-8JqolPCb9HtfCIgRfM5a3BGozh4alYmEWuWSBUykAZg=";
+  };
+
+  nativeBuildInputs = [
+    gettext
+    nodejs
+    pnpmConfigHook
+    pnpm_11
+    python3
+    rsync
+  ];
+
+  preBuild = ''
+    pushd fiduswriter || true
+    cp configuration-default.py configuration.py
+    python manage.py setup # migrations, npm install, transpilation
+    python manage.py collectstatic
+    popd
+  '';
+
+  env.FIDUS_OUT_DIR = "${placeholder "out"}/${python3Packages.python.sitePackages}";
+  env.PYTHONPATH = "${fiduswriter.passthru.pythonPath}";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r fiduswriter/static-* $out
+
+    mkdir -p $node_modules
+    cp -r fiduswriter/.transpile/node_modules $node_modules
+
+    runHook postInstall
+  '';
+
+  meta = {
+    inherit (fiduswriter.meta)
+      description
+      homepage
+      changelog
+      license
+      maintainers
+      teams
+      ;
+  };
+})
--- pkgs/by-name/fi/fiduswriter/package.nix
+++ pkgs/by-name/fi/fiduswriter/package.nix
@@ -2,13 +2,9 @@
   lib,
   python3Packages,
   fetchFromGitHub,
+  callPackage,
   gettext,
-  nodejs,
-  pnpmConfigHook,
-  pnpm_11,
-  fetchPnpmDeps,
   makeWrapper,
-  rsync,
 }:
 
 python3Packages.buildPythonApplication (finalAttrs: {
@@ -24,43 +20,14 @@ python3Packages.buildPythonApplication (finalAttrs: {
     hash = "sha256-NkFJehSgwYwYUZaznZW63KEXR1wTf+Hpa+8ZM71aZ84=";
   };
 
-  pnpmDeps = fetchPnpmDeps {
-    inherit (finalAttrs)
-      pname
-      version
-      src
-      ;
-    postPatch = finalAttrs.postPatch + "";
-    pnpm = pnpm_11;
-    fetcherVersion = 4;
-    hash = "sha256-8JqolPCb9HtfCIgRfM5a3BGozh4alYmEWuWSBUykAZg=";
-
-    nativeBuildInputs = [
-      nodejs
-      rsync
-    ];
-
-    env.PYTHONPATH = "${finalAttrs.passthru.pythonPath}";
-  };
-
   postPatch = ''
     substituteInPlace pyproject.toml \
       --replace-fail "setuptools>=82.0.1" "setuptools"
-
-    # Upstream uses a `package.json5` file for multiple components, each with
-    # its own dependencies. To merge all of them into a single `package.json`
-    # and get the associated lockfile, run: `python manage.py npm_install`
-    mkdir -p .transpile
-    cp ${./pnpm-lock.yaml} ./pnpm-lock.yaml
-    cp ${./package.json} ./package.json
   '';
 
   nativeBuildInputs = [
     gettext
     makeWrapper
-    nodejs
-    pnpmConfigHook
-    pnpm_11
   ];
 
   build-system = with python3Packages; [
@@ -147,14 +114,6 @@ python3Packages.buildPythonApplication (finalAttrs: {
     "pyotp"
   ];
 
-  preBuild = ''
-    pushd fiduswriter || true
-    cp configuration-default.py configuration.py
-    python manage.py setup # migrations, npm install, transpilation
-    python manage.py collectstatic
-    popd
-  '';
-
   env.FIDUS_OUT_DIR = "${placeholder "out"}/${python3Packages.python.sitePackages}";
   env.PYTHONPATH = "${finalAttrs.passthru.pythonPath}";
 
@@ -167,6 +126,8 @@ python3Packages.buildPythonApplication (finalAttrs: {
 
   postFixup = ''
     mkdir -p $out/bin
+
+    cp -r ${finalAttrs.passthru.frontend}/static-* $FIDUS_OUT_DIR/fiduswriter
 
     makeWrapper ${lib.getExe python3Packages.granian} $out/bin/granian \
       "''${makeWrapperArgs[@]}"
@@ -188,6 +149,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
 
   passthru = {
     pythonPath = python3Packages.makePythonPath finalAttrs.passthru.dependencies;
+    frontend = callPackage ./frontend.nix { fiduswriter = finalAttrs.finalPackage; };
   };
 
   meta = {
