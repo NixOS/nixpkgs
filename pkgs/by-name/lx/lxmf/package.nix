4f83cf346539972226b81ed4b3b6ebadc90c3b73
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-06-24 22:53:30 +0200

    lxmf: simplify

--- pkgs/by-name/lx/lxmf/package.nix
+++ pkgs/by-name/lx/lxmf/package.nix
@@ -2,13 +2,4 @@
   python3Packages,
 }:
 
-let
-  pythonPackages = python3Packages.overrideScope (
-    self: super: {
-      lxmf = super.lxmf.override {
-        propagateRns = true;
-      };
-    }
-  );
-in
-pythonPackages.toPythonApplication pythonPackages.lxmf
+python3Packages.toPythonApplication python3Packages.lxmf
