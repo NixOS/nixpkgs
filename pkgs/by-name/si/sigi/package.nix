fe9dea3b390a8c462186e6073e3ccbb9a799bd31
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-24 02:55:04 +0000

    sigi: 3.7.1 -> 3.8.0

--- pkgs/by-name/si/sigi/package.nix
+++ pkgs/by-name/si/sigi/package.nix
@@ -9,14 +9,14 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sigi";
-  version = "3.7.1";
+  version = "3.8.0";
 
   src = fetchCrate {
     inherit (finalAttrs) pname version;
-    hash = "sha256-Tsrfan7aejP2oy9x9VoTIq0ba0s0tnx1RTlAB0v6eis=";
+    hash = "sha256-O/M0NBES215xLwktgOTVIKeXpDXQHDJcJKV3ej5ILEw=";
   };
 
-  cargoHash = "sha256-SFT0quq5e37tFa07sCFjb8u8scZLjhICBvKdQOR8s14=";
+  cargoHash = "sha256-0jB/eMXEMNEapqwSeFD6aHsYhzHTEYxL3usFrCCZ4uI=";
   nativeBuildInputs = [ installShellFiles ];
 
   # In case anything goes wrong.
