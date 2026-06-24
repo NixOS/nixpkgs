9c0e8f33c055c5095472b4a0478617f8be8089ea
prescientmoon <git@moonythm.dev>
2026-06-24 16:43:35 +0200

    nixos-vars: Implement generation logic for secrets
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- pkgs/by-name/ni/nixos-vars/example/config.nix
+++ pkgs/by-name/ni/nixos-vars/example/config.nix
@@ -1,21 +1,31 @@
 let
   noop = pkgs: pkgs.writeShellScript "noop" "echo 'Unimplemented!'";
+  root = "/tmp/vars-demo";
 in
 {
   vars = {
     defaultGeneratorBackend = "example";
     generatorBackends.example = {
-      get = noop;
-      set = noop;
-      deploy = noop;
-      fixup = noop; # This one's optional, but I wanted to make sure that works
+      get =
+        pkgs:
+        pkgs.writeShellScript "get" ''
+          ${pkgs.coreutils}/bin/cat ${root}/generators/"$1"/files/"$2" > $out
+        '';
+      set =
+        pkgs:
+        pkgs.writeShellScript "set" ''
+          ${pkgs.coreutils}/bin/mkdir -p ${root}/generators/"$1"/files/
+          ${pkgs.coreutils}/bin/cat "$in" > ${root}/generators/"$1"/files/"$2"
+        '';
       exists =
         pkgs:
         pkgs.writeShellScript "exists" ''
-          if [[ ! -f ./generators/$1/files/$2 ]]; then 
+          if [[ ! -f ${root}/generators/"$1"/files/"$2" ]]; then
             exit 1
           fi
         '';
+      deploy = noop;
+      fixup = noop; # This one's optional, but I wanted to make sure that works
     };
 
     generators.example = {
@@ -33,7 +43,8 @@ in
       script =
         pkgs:
         pkgs.writeShellScript "gen-derived" ''
-          cp $in/example $out/derived
+          ${pkgs.coreutils}/bin/cat $in/example/example \
+            | ${pkgs.lib.getExe pkgs.cowsay} > $out/derived
         '';
     };
   };
