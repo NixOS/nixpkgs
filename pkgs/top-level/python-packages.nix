3a219654c50ddd71e40c2ce5a45bced41a6128c3
LodWKobku <lodwkobku@gmail.com>
2026-06-25 12:28:55 +0200

    python3Packages.slackify-markdown: init at 0.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19142,6 +19142,8 @@ self: super: with self; {
 
   slack-sdk = callPackage ../development/python-modules/slack-sdk { };
 
+  slackify-markdown = callPackage ../development/python-modules/slackify-markdown { };
+
   slapd = callPackage ../development/python-modules/slapd { };
 
   sleekxmppfs = callPackage ../development/python-modules/sleekxmppfs { };
