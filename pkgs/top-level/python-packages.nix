813e41bdc69d8cf9abf9a84ed9070633dc13a370
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-25 23:59:47 +0200

    python3Packages.alibabacloud-darabonba-array: init at 0.1.0
    Alibaba Cloud Darabonba Array SDK Library for Python
    
    https://github.com/aliyun/darabonba-array

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -666,6 +666,10 @@ self: super: with self; {
 
   alibabacloud-cs20151215 = callPackage ../development/python-modules/alibabacloud-cs20151215 { };
 
+  alibabacloud-darabonba-array =
+    callPackage ../development/python-modules/alibabacloud-darabonba-array
+      { };
+
   alibabacloud-darabonba-encode-util =
     callPackage ../development/python-modules/alibabacloud-darabonba-encode-util
       { };
