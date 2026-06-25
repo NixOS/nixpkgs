d97cfb358909d889cb01b4cc49e767769ea19c62
Marcel <me@m4rc3l.de>
2026-06-25 12:07:09 +0200

    motioneye: 0.43.1 -> 0.44.0
    Diff: https://github.com/motioneye-project/motioneye/compare/0.43.1...0.44.0
    (cherry picked from commit 6a12e4797665ab59aa79d9bd05d1df7d843ec00a)

--- pkgs/by-name/mo/motioneye/package.nix
+++ pkgs/by-name/mo/motioneye/package.nix
@@ -2,30 +2,19 @@
   lib,
   python3Packages,
   fetchFromGitHub,
-  fetchpatch,
 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "motioneye";
-  version = "0.43.1";
+  version = "0.44.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "motioneye-project";
     repo = "motioneye";
     tag = version;
-    hash = "sha256-ckOgYmOP5irjNutcC3FMZPBexn/CldG0UtFZ+tPYNJ4=";
+    hash = "sha256-4sXttSSkmMgsoZb7PXEXXh8KNORTSmqq4lYp3JBDmPo=";
   };
-
-  patches = [
-    # fix pytest
-    # https://github.com/motioneye-project/motioneye/pull/3271
-    (fetchpatch {
-      url = "https://github.com/motioneye-project/motioneye/commit/41c0727e2872af1b758743c41b529e76dcac6f84.patch";
-      hash = "sha256-0zDveoAN1T0SuCob0U/9GEGTh7pj2CXH/j4YrjO0VE0=";
-      includes = [ "conftest.py" ];
-    })
-  ];
 
   build-system = with python3Packages; [
     setuptools
@@ -38,6 +27,7 @@ python3Packages.buildPythonApplication rec {
     pillow
     pycurl
     tornado
+    argon2-cffi
   ];
 
   nativeCheckInputs = with python3Packages; [
