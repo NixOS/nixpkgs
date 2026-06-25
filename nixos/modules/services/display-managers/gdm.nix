557ffb2ffaa12f5ac1ea77266db875970b84570a
Grimmauld <Grimmauld@grimmauld.de>
2026-06-25 22:57:37 +0200

    nixos/treewide: use security.pam.pam_unixModulePath

--- nixos/modules/services/display-managers/gdm.nix
+++ nixos/modules/services/display-managers/gdm.nix
@@ -421,7 +421,7 @@ in
             {
               name = "unix";
               control = "sufficient";
-              modulePath = "${config.security.pam.package}/lib/security/pam_unix.so";
+              modulePath = config.security.pam.pam_unixModulePath;
             }
           ];
 
@@ -567,7 +567,7 @@ in
             {
               name = "unix";
               control = "sufficient";
-              modulePath = "${config.security.pam.package}/lib/security/pam_unix.so";
+              modulePath = config.security.pam.pam_unixModulePath;
             }
           ];
 
@@ -575,7 +575,7 @@ in
             {
               name = "unix";
               control = "requisite";
-              modulePath = "${config.security.pam.package}/lib/security/pam_unix.so";
+              modulePath = config.security.pam.pam_unixModulePath;
               settings.nullok = true;
               settings.yescrypt = true;
             }
