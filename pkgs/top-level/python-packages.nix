e711e03b861cac530b74f460a01ce8b6d7aea7a9
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-25 23:54:39 +0200

    python3Packages.alibabacloud-darabonba-encode-util: init at 0.0.2
    Darabonba Encode Util Library for Alibaba Cloud Python SDK
    
    https://github.com/aliyun/darabonba-crypto-util

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -666,6 +666,10 @@ self: super: with self; {
 
   alibabacloud-cs20151215 = callPackage ../development/python-modules/alibabacloud-cs20151215 { };
 
+  alibabacloud-darabonba-encode-util =
+    callPackage ../development/python-modules/alibabacloud-darabonba-encode-util
+      { };
+
   alibabacloud-darabonba-map =
     callPackage ../development/python-modules/alibabacloud-darabonba-map
       { };
