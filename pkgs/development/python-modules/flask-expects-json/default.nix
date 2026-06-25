e073b0a69649a44a096c4878244f5e0a67e1f09d
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-25 15:15:18 +0200

    python3Packages.flask-expects-json: use finalAttrs

--- pkgs/development/python-modules/flask-expects-json/default.nix
+++ pkgs/development/python-modules/flask-expects-json/default.nix
@@ -7,7 +7,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "flask-expects-json";
   version = "1.7.0";
   format = "setuptools";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "Fischerfredl";
     repo = "flask-expects-json";
-    rev = version;
+    rev = finalAttrs.version;
     hash = "sha256-CUxuwqjjAb9Fy6xWtX1WtSANYaYr5//vY8k89KghYoQ=";
   };
 
@@ -48,4 +48,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
