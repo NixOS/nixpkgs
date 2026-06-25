43b25165d6763df80dd0cc2e2f2bcc940b66bb51
LodWKobku <lodwkobku@gmail.com>
2026-06-25 11:57:32 +0200

    python3Packages.lark-oapi: added package tests, nix-update-script, optional deps and alias

--- pkgs/top-level/python-aliases.nix
+++ pkgs/top-level/python-aliases.nix
@@ -454,6 +454,7 @@ mapAliases {
   nonbloat-db = throw "nonbloat-db has been removed because the upstream project was archived."; # added 2025-05-16
   nptyping = throw "nptyping has been removed because it was broken, unmaintained upstream, and unused."; # added 2025-08-29
   Nuitka = throw "'Nuitka' has been renamed to/replaced by 'nuitka'"; # Converted to throw 2025-10-29
+  oapi-sdk-python = throw "This project have inconsistent naming, use pypi name 'lark-oapi'"; # added 2026-06-25
   oauth2 = throw "oauth2 has been removed as it is unmaintained"; # added 2025-05-16
   oauth = throw "oauth has been removed as it is unmaintained"; # added 2025-05-16
   objax = throw "objax has been removed because the upstream project was archived."; # Added 2025-10-04
