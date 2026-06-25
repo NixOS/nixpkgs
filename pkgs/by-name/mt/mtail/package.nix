b16254b39d3367ee0adddf40d50ea51ab131a729
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-25 07:19:49 +0000

    mtail: 3.3.2 -> 3.4.0

--- pkgs/by-name/mt/mtail/package.nix
+++ pkgs/by-name/mt/mtail/package.nix
@@ -8,17 +8,17 @@
 
 buildGoModule (finalAttrs: {
   pname = "mtail";
-  version = "3.3.2";
+  version = "3.4.0";
 
   src = fetchFromGitHub {
     owner = "jaqx0r";
     repo = "mtail";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Cn6Ssj5ik0G/MORbx+YwY7ekheXim64zUx7oKH5JXD0=";
+    hash = "sha256-iNTj8bXtJl1H0rNiFGITgIxD9oAZ7zKUAoyDaxvcR6o=";
   };
 
   proxyVendor = true;
-  vendorHash = "sha256-v00wX7uz3z/ldwppQd9aJLN/bnhoN/wweY3nbO/fu2I=";
+  vendorHash = "sha256-NKNCpTCfc2U5fqdhXu30w7QlUjCwSX0l+t5ivWtEgdU=";
 
   nativeBuildInputs = [
     gotools # goyacc
