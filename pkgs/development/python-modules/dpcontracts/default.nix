6e409a04d47d3dcd8f6ecfb0b9fbaadd1c36f057
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-25 14:33:30 +0200

    python3Packages.dpcontracts: migrate to pyproject

--- pkgs/development/python-modules/dpcontracts/default.nix
+++ pkgs/development/python-modules/dpcontracts/default.nix
@@ -2,12 +2,13 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
 }:
 
 buildPythonPackage {
   pname = "dpcontracts";
   version = "unstable-2018-11-20";
-  format = "setuptools";
+  pyproject = true;
 
   __structuredAttrs = true;
 
@@ -17,6 +18,10 @@ buildPythonPackage {
     rev = "45cb8542272c2ebe095c6efb97aa9407ddc8bf3c";
     hash = "sha256-FygJPXo7lZ9tlfqY6KmPJ3PLIilMGLBr3013uj9hCEs=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   # package does not have any tests
   doCheck = false;
