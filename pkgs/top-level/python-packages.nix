fd3288a0c01fc43bcff1c5b4b6de9854fd13e422
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-25 23:48:27 +0200

    python3Packages.alibabacloud-darabonba-signature-util: init at 0.0.4
    Darabonba Signature Util Library for Alibaba Cloud Python SDK
    
    https://github.com/aliyun/darabonba-crypto-util

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -666,6 +666,10 @@ self: super: with self; {
 
   alibabacloud-cs20151215 = callPackage ../development/python-modules/alibabacloud-cs20151215 { };
 
+  alibabacloud-darabonba-signature-util =
+    callPackage ../development/python-modules/alibabacloud-darabonba-signature-util
+      { };
+
   alibabacloud-darabonba-string =
     callPackage ../development/python-modules/alibabacloud-darabonba-string
       { };
