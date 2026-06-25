eaa1851d424fd344072bf62cce4fd9abc2f12390
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-06-25 08:13:08 +0200

    ocamlPackages.batteries: update for OCaml ≥ 5.5

--- pkgs/development/ocaml-modules/batteries/default.nix
+++ pkgs/development/ocaml-modules/batteries/default.nix
@@ -3,19 +3,18 @@
   fetchFromGitHub,
   buildDunePackage,
   ocaml,
+  fetchpatch,
   ounit,
   qtest,
   qcheck,
   num,
   camlp-streams,
-  doCheck ? lib.versionAtLeast ocaml.version "4.08",
+  doCheck ? true,
 }:
 
 buildDunePackage (finalAttrs: {
   pname = "batteries";
   version = "3.10.0";
-
-  minimalOCamlVersion = "4.05";
 
   src = fetchFromGitHub {
     owner = "ocaml-batteries-team";
@@ -23,6 +22,11 @@ buildDunePackage (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-cD0O4kEDE58yCYnUuS83O1CJNHJuCGVhvKJSKQeQGkc=";
   };
+
+  patches = lib.optional (lib.versionAtLeast ocaml.version "5.5") (fetchpatch {
+    url = "https://github.com/ocaml-batteries-team/batteries-included/commit/f6e091266599aea93c8a94115cf08f50e88c5dd0.patch";
+    hash = "sha256-6CUcq24x4fnumduK4BJ5cVQ5DHPbVLyLUxl57q2JVtw=";
+  });
 
   nativeCheckInputs = [ qtest ];
   checkInputs = [
