edfbea219fc7a00d32abd09cc7aca57342c1bf5c
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-26 22:06:06 +0000

    git-msgraph: 5.6.2 -> 5.9.3

--- pkgs/by-name/gi/git-credential-email/package.nix
+++ pkgs/by-name/gi/git-credential-email/package.nix
@@ -26,13 +26,13 @@ let
 in
 python3Packages.buildPythonApplication (finalAttrs: {
   inherit pname;
-  version = "5.6.2";
+  version = "5.9.3";
 
   src = fetchFromGitHub {
     owner = "AdityaGarg8";
     repo = "git-credential-email";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-pW13tOPOyS5EorR1C/WEpJpu2ilCA4s8N7GkXoyPv7U=";
+    hash = "sha256-UtCe4buur2E6MUaLN5UH8oXn1JF0NTDycg190NHkJYA=";
   };
 
   dependencies =
