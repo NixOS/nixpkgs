amansakhuja/nixpkgs5

nixos/cmk-agent: init module
Co-authored-by: Chaostheorie <cobalt@cobalt.rocks>

nixos/modules/module-list.nix
@@ -939,6 +939,7 @@
   ./services/monitoring/bosun.nix
   ./services/monitoring/cadvisor.nix
   ./services/monitoring/certspotter.nix
+  ./services/monitoring/cmk-agent.nix
   ./services/monitoring/cockpit.nix
   ./services/monitoring/collectd.nix
   ./services/monitoring/das_watchdog.nix

NixOS/nixpkgs

nixos/modules/module-list.nix
@@ -1019,6 +1019,7 @@
   ./services/monitoring/bosun.nix
   ./services/monitoring/cadvisor.nix
   ./services/monitoring/certspotter.nix
+  ./services/monitoring/checkmk-agent.nix
   ./services/monitoring/cockpit.nix
   ./services/monitoring/collectd.nix
   ./services/monitoring/das_watchdog.nix