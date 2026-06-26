8995a537ffa6bf55dd5d9e1601f021010d08c40d
Robert Rose <robert.rose@mailbox.org>
2026-06-26 15:44:11 +0200

    k3s_1_36: 1.36.1+k3s1 -> 1.36.2+k3s1
    https://github.com/k3s-io/k3s/releases/tag/v1.36.2%2Bk3s1

--- pkgs/applications/networking/cluster/k3s/1_36/versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_36/versions.nix
@@ -1,21 +1,21 @@
 {
-  k3sVersion = "1.36.1+k3s1";
-  k3sCommit = "a9663261a7ff40522542485a6b2f81916b6d72f9";
-  k3sRepoSha256 = "0788034bw5pl8ikfb16fvdhl8a3dhhfasrbafir6s9fb8q9h3z4z";
-  k3sVendorHash = "sha256-jX/qoRhVLZy/25fdhp5NOiRSGEatV/acBbSpjhutAzU=";
+  k3sVersion = "1.36.2+k3s1";
+  k3sCommit = "01b6f04aaa69e8b09303f0393d4b4f1811da23aa";
+  k3sRepoSha256 = "0iqh1hkqfgm9df3bnwi79zxcdk0a9621q451yibr19j58pb76pxv";
+  k3sVendorHash = "sha256-rwFC0bzUacl5kiQrnpRqqIam+9ADqaPuxLxNBX0wWXY=";
   chartVersions = import ./chart-versions.nix;
   imagesVersions = builtins.fromJSON (builtins.readFile ./images-versions.json);
-  k3sRootVersion = "0.15.0";
-  k3sRootSha256 = "008n8xx7x36y9y4r24hx39xagf1dxbp3pqq2j53s9zkaiqc62hd0";
+  k3sRootVersion = "0.15.2";
+  k3sRootSha256 = "0yxq2jqqb7flm4rs9dl7fqxba3mmwkmjbc8rx7pgai4qa1lzyigy";
   k3sCNIVersion = "1.9.1-k3s1";
   k3sCNISha256 = "1ggaz0p1c2k94car9d89a05smz3zx32sxn197b1l5kmjcnzdwadh";
-  containerdVersion = "2.2.3-k3s1";
-  containerdSha256 = "0fn252icn082822r754i2bqd8rivhvjwkfk031a8g0vvw8rz46vj";
+  containerdVersion = "2.3.2-k3s2";
+  containerdSha256 = "1jzhkh0zg1s2922fkr4r5v1680apafkjqba8ic6br8nc8bk7j4xq";
   containerdPackage = "github.com/k3s-io/containerd/v2";
   criCtlVersion = "1.36.0-k3s1";
   flannelVersion = "v0.28.4";
   flannelPluginVersion = "v1.9.0-flannel1";
   kubeRouterVersion = "v2.6.3-k3s1";
   criDockerdVersion = "v0.3.19-k3s5";
-  helmJobVersion = "v0.10.0-build20260513";
+  helmJobVersion = "v0.11.1-build20260615";
 }
