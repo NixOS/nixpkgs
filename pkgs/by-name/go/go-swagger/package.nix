572b90a6bdbecdab5a129dec7f295d0471a7277a
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-26 19:26:20 +0000

    go-swagger: 0.34.1 -> 0.35.0

--- pkgs/by-name/go/go-swagger/package.nix
+++ pkgs/by-name/go/go-swagger/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "go-swagger";
-  version = "0.34.1";
+  version = "0.35.0";
 
   src = fetchFromGitHub {
     owner = "go-swagger";
     repo = "go-swagger";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-2kPB/E8/qr4j/2rJS6fbgH2CKTZE43L/7ERr2BzOESk=";
+    hash = "sha256-+08mPU3R8IF8vooNkHDLeS2q1VakPKIce3WY+sTjDm4=";
   };
 
-  vendorHash = "sha256-FILITZAcRHcZs3sICuWVmLA53/zhegQcZTAua6m4HVI=";
+  vendorHash = "sha256-K2Ca9s2FV+KYuJhO6BA/5gCXb7nXK68RO8+QwcKgpAM=";
 
   doCheck = false;
 
