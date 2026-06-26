7d443ad53695c87d601e4f3a6f4a4a8c4353f394
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-26 18:50:49 +0000

    postgresqlPackages.pgvector: 0.8.2 -> 0.8.3

--- pkgs/servers/sql/postgresql/ext/pgvector.nix
+++ pkgs/servers/sql/postgresql/ext/pgvector.nix
@@ -7,13 +7,13 @@
 
 postgresqlBuildExtension (finalAttrs: {
   pname = "pgvector";
-  version = "0.8.2";
+  version = "0.8.3";
 
   src = fetchFromGitHub {
     owner = "pgvector";
     repo = "pgvector";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-TLPlH+amFdeI2pEsUZuvoQ1JA0sCMiIAWdkgqGBo4mI=";
+    hash = "sha256-Tpa+OGau8RRMVgWQiAwjL3ez72hx6J+01yVvuCj7zVU=";
   };
 
   meta = {
