dc78f986bb5306327fa8f7eb83e3dd88deb5823a
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-27 00:57:21 +0000

    python3Packages.mcstatus: 13.1.0 -> 14.0.0

--- pkgs/development/python-modules/mcstatus/default.nix
+++ pkgs/development/python-modules/mcstatus/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "mcstatus";
-  version = "13.1.0";
+  version = "14.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "py-mine";
     repo = "mcstatus";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Fn2i9CCO5wESKSpeM8YlqrzF6RCwBbYQs2wSEgOYAcE=";
+    hash = "sha256-EyBaBC1Ly2549oFohtEulQMXIeRhQjtzvO+XPmwl8Zs=";
   };
 
   build-system = [
