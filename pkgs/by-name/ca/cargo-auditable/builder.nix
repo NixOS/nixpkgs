5c672c1409daa03f29e0141391068e755edfeb64
Emily <vcs@emily.moe>
2026-06-27 22:52:43 +0100

    cargo-auditable: disable `test_bare_linker`
    This tries to use `linker = "rust-lld"` on `aarch64-darwin`, Linux
    Musl and ARMv7 targets, and MSVC Windows, but we deliberately don’t
    provide that.
    
    (cherry picked from commit 35ebf72241e43fc7bff59f6244b3bca0299d0820)

--- pkgs/by-name/ca/cargo-auditable/builder.nix
+++ pkgs/by-name/ca/cargo-auditable/builder.nix
@@ -42,7 +42,20 @@ lib.extendMkDerivation {
         # https://github.com/rust-secure-code/cargo-auditable/issues/235
         "--skip=test_proc_macro"
         "--skip=test_self_hosting"
-      ];
+      ]
+      # TODO: Clean up on `staging`.
+      ++
+        lib.optionals
+          (
+            stdenv.hostPlatform.isMusl
+            || stdenv.hostPlatform.isAarch32
+            || stdenv.hostPlatform.isDarwin
+            || stdenv.hostPlatform.isMsvc
+          )
+          [
+            # Expects `linker = "rust-lld"` to work.
+            "--skip=test_bare_linker"
+          ];
 
       postInstall = ''
         installManPage cargo-auditable/cargo-auditable.1
