c2dba0508705052b7cdade77b2da6bb1b947c310
sophiebsw <sophiebsw@gmail.com>
2026-06-27 16:07:16 -0700

    archon-lite: add desktop entry
    Co-authored-by: sophiebsw <sophiebsw@gmail.com>
    Co-authored-by: Henri Peurasaari <fargate+github@posteo.eu>
    (cherry picked from commit 47eed53c87239e83a69e76e4868f0c96c4a2e940)

--- pkgs/by-name/ar/archon-lite/package.nix
+++ pkgs/by-name/ar/archon-lite/package.nix
@@ -11,9 +11,23 @@ let
     url = "https://github.com/RPGLogs/Uploaders-archon-lite/releases/download/v${version}/archon-lite-v${version}.AppImage";
     hash = "sha256-ooNvgbtV6HKgzRLgHZul92NLnEB8oX6fHL6iwfHajVA=";
   };
+
+  extracted = appimageTools.extractType2 { inherit pname version src; };
 in
 appimageTools.wrapType2 {
   inherit pname version src;
+
+  extraInstallCommands = ''
+    mkdir -p $out/share/applications
+    mkdir -p $out/share/icons/hicolor/512x512/apps
+    cp -r ${extracted}/usr/share/icons/hicolor/512x512/apps/'Archon App Lite.png' $out/share/icons/hicolor/512x512/apps/archon-lite.png
+    chmod -R +w $out/share/
+    test ! -e $out/share/icons/hicolor/0x0 # check for regression of https://github.com/electron-userland/electron-builder/issues/5294
+    cp ${extracted}/'Archon App Lite.desktop' $out/share/applications/archon-lite.desktop
+    substituteInPlace $out/share/applications/archon-lite.desktop \
+      --replace-fail "Exec=AppRun --no-sandbox" "Exec=archon-lite" \
+      --replace-fail "Icon=Archon App Lite" "Icon=archon-lite"
+  '';
 
   passthru.updateScript = nix-update-script { };
 
