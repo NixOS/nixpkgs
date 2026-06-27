9d197b6f88596205cbeddc08df863de4b7ff0e65
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-27 03:05:53 +0000

    amazon-cloudwatch-agent: 1.300069.0 -> 1.300070.0

--- pkgs/by-name/am/amazon-cloudwatch-agent/package.nix
+++ pkgs/by-name/am/amazon-cloudwatch-agent/package.nix
@@ -11,16 +11,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "amazon-cloudwatch-agent";
-  version = "1.300069.0";
+  version = "1.300070.0";
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = "amazon-cloudwatch-agent";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-A9UASdKERo/vg3K8EDu//r6SqQjskhmVKeBlbqqpdDM=";
+    hash = "sha256-w0/y4qW109MjgO1/HWcBfPQ3zhIQb8lzZLCnoDMM99c=";
   };
 
-  vendorHash = "sha256-Qlwy0wz79TgYlBcsdHLzZA3OWbSIg6reK6KGSKsMlzI=";
+  vendorHash = "sha256-3KRNKueNl5KAAuAFRo04OMU/f78HmjKuSpbGwGBD8D4=";
 
   # See the list in https://github.com/aws/amazon-cloudwatch-agent/blob/v1.300049.1/Makefile#L68-L77.
   subPackages = [
