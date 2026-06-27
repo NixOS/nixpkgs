0a69d76548af9c7efc4068c8ee1219acb6842cc9
Bart Oostveen <bart@bartoostveen.nl>
2026-06-27 14:37:28 +0200

    bind: enable structuredAttrs
    (cherry picked from commit 3af077a023c39694c0c72d75f3c4e1922873c149)

--- pkgs/by-name/bi/bind/package.nix
+++ pkgs/by-name/bi/bind/package.nix
@@ -109,6 +109,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   enableParallelBuilding = true;
   strictDeps = true;
+  __structuredAttrs = true;
 
   doCheck = with stdenv.hostPlatform; !isStatic && isLinux;
   checkTarget = "unit";
