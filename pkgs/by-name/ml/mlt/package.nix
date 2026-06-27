079a6f481f4e42e8c3ff670f7582403a8b47e84a
nick-linux8 <nick.stationengineer@proton.me>
2026-06-27 01:46:53 -0400

    mlt: update --replace and match opencv to PR in staging

--- pkgs/by-name/ml/mlt/package.nix
+++ pkgs/by-name/ml/mlt/package.nix
@@ -152,7 +152,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   postFixup = ''
     substituteInPlace "$dev"/lib/pkgconfig/mlt-framework-7.pc \
-      --replace '=''${prefix}//' '=/'
+      --replace-fail '=''${prefix}//' '=/'
   '';
 
   passthru = {
