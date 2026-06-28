060bb0f04d4cbd9e6a77fbeda137cffa87fe3bc9
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-06-28 17:35:56 +0200

    python3Packages.opentelemetry-instrumentation: 0.61b0 -> 0.64b0
    https://github.com/open-telemetry/opentelemetry-python-contrib/releases/tag/vv0.64b0

--- pkgs/development/python-modules/opentelemetry-instrumentation-requests/default.nix
+++ pkgs/development/python-modules/opentelemetry-instrumentation-requests/default.nix
@@ -1,6 +1,5 @@
 {
   buildPythonPackage,
-  fetchpatch,
   requests,
   hatchling,
   opentelemetry-api,
@@ -8,7 +7,9 @@
   opentelemetry-semantic-conventions,
   opentelemetry-util-http,
   httpretty,
+  mocket,
   opentelemetry-test-utils,
+  pytest-benchmark,
   pytestCheckHook,
 }:
 
@@ -18,16 +19,6 @@ buildPythonPackage {
   pyproject = true;
 
   sourceRoot = "${opentelemetry-instrumentation.src.name}/instrumentation/opentelemetry-instrumentation-requests";
-
-  patches = [
-    # remove hardcoded requests version from fixtures
-    (fetchpatch {
-      url = "https://github.com/open-telemetry/opentelemetry-python-contrib/commit/69a94e0c3b25edfdc4abeb18a4d26f5b7532e7ba.patch";
-      stripLen = 2;
-      includes = [ "tests/test_requests_integration.py" ];
-      hash = "sha256-JGWJVHR6lAg8bG1fpfJ4BJbqipnVFRLV7i/bKwOmtPk=";
-    })
-  ];
 
   build-system = [ hatchling ];
 
@@ -41,8 +32,14 @@ buildPythonPackage {
 
   nativeCheckInputs = [
     httpretty
+    mocket
     opentelemetry-test-utils
+    pytest-benchmark
     pytestCheckHook
+  ];
+
+  pytestFlags = [
+    "--benchmark-disable"
   ];
 
   pythonImportsCheck = [ "opentelemetry.instrumentation.requests" ];
