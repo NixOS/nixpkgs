c65a0501f07cb720e14f40a34dac09a44988e3e4
Tom Hunze <dev@thunze.de>
2026-06-28 09:09:35 +0200

    calamares-nixos-extensions: suggest wireplumber instead of media-session
    The `services.pipewire.media-session` option was removed and wireplumber
    is now the officially recommended session manager [1].
    
    [1] https://gitlab.freedesktop.org/pipewire/media-session/-/blob/7376c167f1ad20b3968d7a5d6c395a8b698d31d8/README.md#pipewire-media-session
    
    (cherry picked from commit c6b4612a163873d3ff8ad063e4853985298b6f9e)

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
@@ -234,9 +234,8 @@ cfgmisc = """  # Enable CUPS to print documents.
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Use the WirePlumber session manager
+    #wireplumber.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
