defd9e6097dc8c5d4056aec288c860c199d9cb5c
Tom Hunze <dev@thunze.de>
2026-06-28 08:52:39 +0200

    calamares-nixos-extensions: don't enable X11 for GNOME
    GNOME 50 removed the X11 backend.
    
    (cherry picked from commit d637ac4c84f0a60c293bd77b96a22dd4a22c6fb7)

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
@@ -117,10 +117,7 @@ cfglocaleextra = """  i18n.extraLocaleSettings = {
 
 """
 
-cfggnome = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
+cfggnome = """  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
