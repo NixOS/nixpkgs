6c1d4e5d2746eff58d643ffa601ff3df946ad5fa
Tom Hunze <dev@thunze.de>
2026-06-28 10:01:01 +0200

    calamares-nixos-extensions: sync some config with nixos-generate-config
    See `nixos/modules/installer/tools/tools.nix` and
    `nixos/modules/installer/tools/nixos-generate-config.pl`

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
@@ -22,8 +22,8 @@ _ = gettext.translation(
 
 cfghead = """\
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, pkgs, ... }:
 
@@ -35,14 +35,14 @@ cfghead = """\
 
 """
 cfgbootefi = """\
-  # Bootloader.
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
 """
 
 cfgbootbios = """\
-  # Bootloader.
+  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "@@bootdev@@";
   boot.loader.grub.useOSProber = true;
@@ -50,7 +50,7 @@ cfgbootbios = """\
 """
 
 cfgbootbiosbtrfs = """\
-  # Bootloader.
+  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "@@bootdev@@";
   boot.loader.grub.useOSProber = true;
@@ -305,12 +305,12 @@ cfgunfree = """\
 """
 
 cfgpkgs = """\
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
 
 """
 
@@ -334,12 +334,28 @@ cfgtail = """\
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "@@nixosversion@@"; # Did you read the comment?
 
 }
