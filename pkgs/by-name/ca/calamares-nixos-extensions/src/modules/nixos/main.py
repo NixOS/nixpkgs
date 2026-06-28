505042a43725350dec24809b5808d68f9b06e33d
Tom Hunze <dev@thunze.de>
2026-06-28 08:56:24 +0200

    calamares-nixos-extensions: use renamed GNOME options
    Use the newer `services.displayManager.gdm` and
    `services.desktopManager.gnome` options instead of the deprecated
    `services.xserver.*` options.

--- pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
+++ pkgs/by-name/ca/calamares-nixos-extensions/src/modules/nixos/main.py
@@ -118,8 +118,8 @@ cfglocaleextra = """  i18n.extraLocaleSettings = {
 """
 
 cfggnome = """  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
 """
 
