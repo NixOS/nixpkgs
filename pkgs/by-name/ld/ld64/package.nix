NixOS/nixpkgs

commit 23c05205023464c322a0df9ac0834c00f4da9fa8

ld64: drop `x86_64-darwin` support

pkgs/by-name/ld/ld64/package.nix
@@ -138,9 +138,6 @@ stdenv.mkDerivation (finalAttrs: {
     xar
   ];
 
-  # ld built with this fails to link glib's gio on x86_64 darwin
-  hardeningDisable = [ "libcxxhardeningfast" ];
-
   dontUseCmakeConfigure = true; # CMake is only needed because it’s used by Meson to find LLVM.
 
   # Note for overrides: ld64 cannot be built as a debug build because of UB in its iteration implementations,

commit 5089f46917e468a60e024601fe75266ce9c39618

ld64: disable hardening again
Turn out this happens on `aarch64-darwin` too.

Fixes: 23c05205023464c322a0df9ac0834c00f4da9fa8

pkgs/by-name/ld/ld64/package.nix
@@ -138,6 +138,9 @@ stdenv.mkDerivation (finalAttrs: {
     xar
   ];
 
+  # ld built with this fails to link glib's gio
+  hardeningDisable = [ "libcxxhardeningfast" ];
+
   dontUseCmakeConfigure = true; # CMake is only needed because it’s used by Meson to find LLVM.
 
   # Note for overrides: ld64 cannot be built as a debug build because of UB in its iteration implementations,