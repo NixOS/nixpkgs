NixOS/nixpkgs

commit 1f4c50ab8116b151fb9a007b2dbf46decd7a1483

lib/cli: deprecate toGNUCommandLine

nixos/modules/hardware/printers.nix
@@ -10,7 +10,7 @@ let
   ensurePrinter =
     p:
     let
-      args = lib.cli.toGNUCommandLineShell { } (
+      args = lib.cli.toCommandLineShellGNU { } (
         {
           p = p.name;
           v = p.deviceUri;

lib/cli.nix
@@ -40,7 +40,9 @@
     :::
   */
   toGNUCommandLineShell =
-    options: attrs: lib.escapeShellArgs (lib.cli.toGNUCommandLine options attrs);
+    lib.warnIf (lib.oldestSupportedReleaseIsAtLeast 2511)
+      "lib.cli.toGNUCommandLineShell is deprecated, please use lib.cli.toCommandLineShell or lib.cli.toCommandLineShellGNU instead."
+      (options: attrs: lib.escapeShellArgs (lib.cli.toGNUCommandLine options attrs));
 
   /**
     Automatically convert an attribute set to a list of command-line options.
@@ -114,40 +116,44 @@
     :::
   */
   toGNUCommandLine =
-    {
-      mkOptionName ? k: if builtins.stringLength k == 1 then "-${k}" else "--${k}",
+    lib.warnIf (lib.oldestSupportedReleaseIsAtLeast 2511)
+      "lib.cli.toGNUCommandLine is deprecated, please use lib.cli.toCommandLine or lib.cli.toCommandLineShellGNU instead."
+      (
+        {
+          mkOptionName ? k: if builtins.stringLength k == 1 then "-${k}" else "--${k}",
 
-      mkBool ? k: v: lib.optional v (mkOptionName k),
+          mkBool ? k: v: lib.optional v (mkOptionName k),
 
-      mkList ? k: v: lib.concatMap (mkOption k) v,
+          mkList ? k: v: lib.concatMap (mkOption k) v,
 
-      mkOption ?
-        k: v:
-        if v == null then
-          [ ]
-        else if optionValueSeparator == null then
-          [
-            (mkOptionName k)
-            (lib.generators.mkValueStringDefault { } v)
-          ]
-        else
-          [ "${mkOptionName k}${optionValueSeparator}${lib.generators.mkValueStringDefault { } v}" ],
+          mkOption ?
+            k: v:
+            if v == null then
+              [ ]
+            else if optionValueSeparator == null then
+              [
+                (mkOptionName k)
+                (lib.generators.mkValueStringDefault { } v)
+              ]
+            else
+              [ "${mkOptionName k}${optionValueSeparator}${lib.generators.mkValueStringDefault { } v}" ],
 
-      optionValueSeparator ? null,
-    }:
-    options:
-    let
-      render =
-        k: v:
-        if builtins.isBool v then
-          mkBool k v
-        else if builtins.isList v then
-          mkList k v
-        else
-          mkOption k v;
+          optionValueSeparator ? null,
+        }:
+        options:
+        let
+          render =
+            k: v:
+            if builtins.isBool v then
+              mkBool k v
+            else if builtins.isList v then
+              mkList k v
+            else
+              mkOption k v;
 
-    in
-    builtins.concatLists (lib.mapAttrsToList render options);
+        in
+        builtins.concatLists (lib.mapAttrsToList render options)
+      );
 
   /**
     Converts the given attributes into a single shell-escaped command-line string.

nixos/modules/services/finance/libeufin/common.nix
@@ -48,7 +48,7 @@ libeufinComponent:
             DynamicUser = true;
             ExecStart =
               let
-                args = lib.cli.toGNUCommandLineShell { } {
+                args = lib.cli.toCommandLineShellGNU { } {
                   c = configFile;
                   L = if cfg.debug then "debug" else null;
                 };
@@ -80,7 +80,7 @@ libeufinComponent:
             initialAccountRegistration = lib.concatMapStringsSep "\n" (
               account:
               let
-                args = lib.cli.toGNUCommandLineShell { } {
+                args = lib.cli.toCommandLineShellGNU { } {
                   c = configFile;
                   inherit (account) username password name;
                   payto_uri = "payto://x-taler-bank/${bankHost}/${account.username}?receiver-name=${account.name}";
@@ -90,7 +90,7 @@ libeufinComponent:
               "${lib.getExe' cfg.package "libeufin-bank"} create-account ${args}"
             ) cfg.initialAccounts;
 
-            args = lib.cli.toGNUCommandLineShell { } {
+            args = lib.cli.toCommandLineShellGNU { } {
               c = configFile;
               L = if cfg.debug then "debug" else null;
             };

nixos/modules/services/hardware/undervolt.nix
@@ -16,7 +16,7 @@ let
         limit != null && window != null
       ) "Both power limit and window must be set";
       "${toString limit} ${toString window}";
-  cliArgs = lib.cli.toGNUCommandLine { } {
+  cliArgs = lib.cli.toCommandLineGNU { } {
     inherit (cfg)
       verbose
       temp

nixos/modules/services/home-automation/ebusd.nix
@@ -170,7 +170,7 @@ in
         serviceConfig = {
           ExecStart =
             let
-              args = lib.cli.toGNUCommandLineShell { optionValueSeparator = "="; } (
+              args = lib.cli.toCommandLineShellGNU { } (
                 lib.foldr (a: b: a // b) { } [
                   {
                     inherit (cfg)

nixos/modules/services/mail/mailpit.nix
@@ -22,7 +22,7 @@ let
 
   isNonNull = v: v != null;
   genCliFlags =
-    settings: concatStringsSep " " (cli.toGNUCommandLine { } (filterAttrs (const isNonNull) settings));
+    settings: concatStringsSep " " (cli.toCommandLineGNU { } (filterAttrs (const isNonNull) settings));
 in
 {
   options.services.mailpit.instances = mkOption {

nixos/modules/services/misc/paperless.nix
@@ -686,7 +686,7 @@ in
           path = [ manage ];
           script = ''
             paperless-manage document_exporter ${cfg.exporter.directory} ${
-              lib.cli.toGNUCommandLineShell { } cfg.exporter.settings
+              lib.cli.toCommandLineShellGNU { } cfg.exporter.settings
             }
           '';
         };

nixos/modules/services/networking/gns3-server.nix
@@ -187,7 +187,7 @@ in
 
       systemd.services.gns3-server =
         let
-          commandArgs = lib.cli.toGNUCommandLineShell { } {
+          commandArgs = lib.cli.toCommandLineShellGNU { } {
             config = "/etc/gns3/gns3_server.conf";
             pid = "/run/gns3/server.pid";
             log = cfg.log.file;

commit 9272dc83c78644d8abaf5ca113228cf9c23aa691

nixos/printers: fix empty value handling in ensurePrinters
E.g. this configuration:

    hardware.printers.ensurePrinters = [
      {
        name = "test";
        deviceUri = "ipp://somewhere";
        location = "";
        model = "everywhere";
      }
    ];

Resulted in the following command being generated:

    /nix/store/...-cups-2.4.19/bin/lpadmin -L -meverywhere -ptest -vipp://somewhere -E

Note the missing '' after -L which then caused /etc/cups/printers.conf to contain:

    Info test
    Location -meverywhere
    DeviceURI ipp://somewhere

This bug was introduced in 1f4c50ab8116b151fb9a007b2dbf46decd7a1483 with the
change from the (now deprecated) toGNUCommandLineShell to toCommandLineShellGNU.

nixos/modules/hardware/printers.nix
@@ -10,22 +10,30 @@ let
   ensurePrinter =
     p:
     let
-      args = lib.cli.toCommandLineShellGNU { } (
-        {
-          p = p.name;
-          v = p.deviceUri;
-          m = p.model;
-        }
-        // lib.optionalAttrs (p.location != null) {
-          L = p.location;
-        }
-        // lib.optionalAttrs (p.description != null) {
-          D = p.description;
-        }
-        // lib.optionalAttrs (p.ppdOptions != { }) {
-          o = lib.mapAttrsToList (name: value: "${name}=${value}") p.ppdOptions;
-        }
-      );
+      args =
+        # Not using toCommandLineShellGNU since it omits empty strings for short options.
+        lib.cli.toCommandLineShell
+          (optionName: {
+            option = "-${optionName}";
+            sep = null;
+            explicitBool = false;
+          })
+          (
+            {
+              p = p.name;
+              v = p.deviceUri;
+              m = p.model;
+            }
+            // lib.optionalAttrs (p.location != null) {
+              L = p.location;
+            }
+            // lib.optionalAttrs (p.description != null) {
+              D = p.description;
+            }
+            // lib.optionalAttrs (p.ppdOptions != { }) {
+              o = lib.mapAttrsToList (name: value: "${name}=${value}") p.ppdOptions;
+            }
+          );
     in
     ''
       # shellcheck disable=SC2016