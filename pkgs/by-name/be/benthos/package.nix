802d89153799f8b31989f273577d824aa40e2a8a
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-29 05:05:46 +0000

    benthos: 4.63.1 -> 4.76.0

--- pkgs/by-name/be/benthos/package.nix
+++ pkgs/by-name/be/benthos/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "benthos";
-  version = "4.63.1";
+  version = "4.76.0";
 
   src = fetchFromGitHub {
     owner = "redpanda-data";
     repo = "benthos";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dcVPTLzRVhyWEkzXfzQKOv7bfjzsxV7odcdPzLP64bQ=";
+    hash = "sha256-BSXK5ylRcYgp88kpkMLD3aWzWGGG0deqJ5suaoxDedw=";
   };
 
   proxyVendor = true;
@@ -22,7 +22,7 @@ buildGoModule (finalAttrs: {
     "cmd/benthos"
   ];
 
-  vendorHash = "sha256-WMnhjGgkIG+yz2SgKoibWSPdNbET7NxY87v5WtMDl8I=";
+  vendorHash = "sha256-PhFEbIO/XwkYruRd4o+DQG4XvdcWMQr9Bn08Pb2R7VI=";
 
   #  doCheck = false;
 
