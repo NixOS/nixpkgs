de3c59ca011a61403f2b0fbe01acd027b22a3e03
Dråfølin <66629792+drafolin@users.noreply.github.com>
2026-06-29 16:40:29 +0200

    tonearm: add passthru.updateScript

--- pkgs/by-name/to/tonearm/package.nix
+++ pkgs/by-name/to/tonearm/package.nix
@@ -1,5 +1,5 @@
 {
-  buildGo126Module,
+  buildGoModule,
   cairo,
   copyDesktopItems,
   fetchFromGitea,
@@ -15,7 +15,8 @@
   libsecret,
   librsvg,
   makeDesktopItem,
-  makeWrapper,
+  makeBinaryWrapper,
+  nix-update-script,
   pango,
   pkg-config,
   symlinkJoin,
@@ -38,7 +39,8 @@ let
     ];
   };
 in
-buildGo126Module (finalAttrs: {
+buildGoModule (finalAttrs: {
+  __structuredAttrs = true;
   pname = "tonearm";
   version = "1.4.0";
   src = fetchFromGitea {
@@ -67,7 +69,7 @@ buildGo126Module (finalAttrs: {
   nativeBuildInputs = [
     pkg-config
     copyDesktopItems
-    makeWrapper
+    makeBinaryWrapper
     wrapGAppsHook4
   ];
 
@@ -107,6 +109,8 @@ buildGo126Module (finalAttrs: {
     glib-compile-schemas $out/share/glib-2.0/schemas
   '';
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "GTK client for TIDAL written in Golang";
     homepage = "https://codeberg.org/dergs/Tonearm";
@@ -116,5 +120,6 @@ buildGo126Module (finalAttrs: {
       nilathedragon
     ];
     mainProgram = "tonearm";
+    platforms = lib.platforms.unix;
   };
 })
