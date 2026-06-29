c37a440151469dee5af08b745e37d210e9d3d912
Oleksii Filonenko <github@brightone.cloud>
2026-06-29 14:13:29 +0100

    stakk: 1.15.0 -> 1.17.1

--- pkgs/by-name/st/stakk/package.nix
+++ pkgs/by-name/st/stakk/package.nix
@@ -2,23 +2,27 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  versionCheckHook,
   nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "stakk";
-  version = "1.15.0";
+  version = "1.17.1";
 
   src = fetchFromGitHub {
     owner = "glennib";
     repo = "stakk";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-rui/Gc21tvy9Kv56UOoErwe0b73YOtP+ggAuKpDY90o=";
+    hash = "sha256-5wEZOxXg30F0MqeszDRYrOBnsOzfXxld8iQkZSPqUIY=";
   };
 
-  cargoHash = "sha256-zypOfxYFfrp3u2+pLNFQNtqXMn01MX4Q2HlBjkZH3r8=";
+  cargoHash = "sha256-iEGrGSia/Da1bpXVAOI9M7QNNVHUAB/RFRgzl+IrFlU=";
 
   useNextest = true;
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
 
   passthru.updateScript = nix-update-script { };
   __structuredAttrs = true;
@@ -26,12 +30,15 @@ rustPlatform.buildRustPackage (finalAttrs: {
   meta = {
     description = "Bridge Jujutsu (jj) bookmarks to GitHub stacked pull requests";
     homepage = "https://github.com/glennib/stakk";
-    changelog = "https://github.com/glennib/stakk/blob/${finalAttrs.src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/glennib/stakk/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = with lib.licenses; [
       asl20
       mit
     ];
-    maintainers = with lib.maintainers; [ voidlily ];
+    maintainers = with lib.maintainers; [
+      voidlily
+      Br1ght0ne
+    ];
     mainProgram = "stakk";
   };
 })
