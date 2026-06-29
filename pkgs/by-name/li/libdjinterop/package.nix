321fd718b7f79d392a99521cbbc0aa2d7d807bc9
Anton Tetov Johansson <anton@tetov.se>
2026-06-29 15:53:16 +0200

    libdjinterop: use nixpkgs howard-hinnant-date
    fixes https://github.com/xsco/libdjinterop/issues/185

--- pkgs/by-name/li/libdjinterop/package.nix
+++ pkgs/by-name/li/libdjinterop/package.nix
@@ -2,8 +2,10 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch2,
   boost,
   cmake,
+  howard-hinnant-date,
   ninja,
   pkg-config,
   sqlite,
@@ -22,6 +24,21 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-Uiz2VZef0+H9NfPgOPT1XVBKCiTXC/n8VKVveXEy40c=";
   };
 
+  patches = [
+    # https://github.com/xsco/libdjinterop/pull/188
+    # adds options to use system howard-hinnant-date and sqlite_modern_cpp
+    # additionally bumps version in CMakeLists.txt, fixed in postPatch
+    (fetchpatch2 {
+      url = "https://github.com/xsco/libdjinterop/commit/d21f713440633e41d37df14c8e5cc16be7595a7a.patch";
+      hash = "sha256-QPgSU1BYl+RAb3sJn5LkbqovhBgCLYw44+d8pr7Tx3M=";
+    })
+  ];
+
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+    --replace-fail "VERSION 0.27.2" "VERSION 0.27.1"
+  '';
+
   nativeBuildInputs = [
     cmake
     ninja
@@ -35,8 +52,13 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = [
     boost
+    howard-hinnant-date
     sqlite
     zlib
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "SYSTEM_DATE_H" true)
   ];
 
   meta = {
