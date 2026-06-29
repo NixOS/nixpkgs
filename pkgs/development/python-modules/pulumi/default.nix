9ca5a1b103a16cee72d4cad95ef2a2a5825cc8d1
Robin Kneepkens <robin@skunk.team>
2026-06-29 15:26:06 +0200

    pulumiPackages.pulumi-python: 3.192.0 -> 3.248.0
    - Update postInstall: pulumi-analyzer-policy not built anymore
    - Skip new failing tests
    - Skip pulumi python module langhost tests

--- pkgs/development/python-modules/pulumi/default.nix
+++ pkgs/development/python-modules/pulumi/default.nix
@@ -12,6 +12,10 @@
   pyyaml,
   debugpy,
   pip,
+  opentelemetry-api,
+  opentelemetry-sdk,
+  opentelemetry-instrumentation-grpc,
+  opentelemetry-exporter-otlp-proto-grpc,
   pytest,
   pytest-asyncio,
   pytest-timeout,
@@ -48,6 +52,10 @@ buildPythonPackage {
     pyyaml
     debugpy
     pip
+    opentelemetry-api
+    opentelemetry-sdk
+    opentelemetry-instrumentation-grpc
+    opentelemetry-exporter-otlp-proto-grpc
   ];
 
   pythonRelaxDeps = [
@@ -55,6 +63,10 @@ buildPythonPackage {
     "grpcio"
     "pip"
     "semver"
+    "opentelemetry-api"
+    "opentelemetry-sdk"
+    "opentelemetry-instrumentation-grpc"
+    "opentelemetry-exporter-otlp-proto-grpc"
   ];
 
   nativeCheckInputs = [
@@ -64,19 +76,20 @@ buildPythonPackage {
     pulumi-python
   ];
 
-  disabledTestPaths = [
-    # TODO: remove disabledTestPaths once the test is fixed upstream.
-    # https://github.com/pulumi/pulumi/pull/19080#discussion_r2309611222
-    "lib/test/provider/experimental/test_property_value.py::test_nesting"
-  ];
-
+  # CheckPhase script based on:
   # https://github.com/pulumi/pulumi/blob/0acaf8060640fdd892abccf1ce7435cd6aae69fe/sdk/python/scripts/test_fast.sh#L10-L11
   # https://github.com/pulumi/pulumi/blob/0acaf8060640fdd892abccf1ce7435cd6aae69fe/sdk/python/scripts/test_fast.sh#L16
+  # Script updated in https://github.com/pulumi/pulumi/pull/21365
+  #
+  # Ignore lib/test/langhost because tests require `uv` / virtualenv which is not supported in the Nix sandbox.
   installCheckPhase = ''
     runHook preInstallCheck
     declare -a _disabledTestPathsArray
     concatTo _disabledTestPathsArray disabledTestPaths
-    ${python.executable} -m pytest --junit-xml= --ignore=lib/test/automation lib/test \
+    ${python.executable} -m pytest --junit-xml= \
+      --ignore=lib/test/automation \
+      --ignore=lib/test/langhost \
+      lib/test \
       "''${_disabledTestPathsArray[@]/#/--deselect=}"
     pushd lib/test_with_mocks
     ${python.executable} -m pytest --junit-xml=
@@ -95,6 +108,7 @@ buildPythonPackage {
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [
       tie
+      untio11
     ];
   };
 }
