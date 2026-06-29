26830a86ca63a39c8cbd73c7de45f3e0f7950c9c
Wolfgang Walther <walther@technowledgy.de>
2026-06-29 20:30:39 +0200

    postgresql: return wrapper from `withoutJIT` passthru
    Before this change, we would return:
    - The postgresql derivation itself from `postgresql.withoutJIT`,
    - A wrapper without any extensions at all, from
    `postgresql_jit.withoutJIT`.
    
    This changes makes it that every call to `.withJIT` and `.withoutJIT`
    returns a wrapper and never the base derivation. More consistent and
    easier to reason about.
    
    (cherry picked from commit 5cf857ed827857ee6ade47a759985fd8dbc83ad7)

--- pkgs/servers/sql/postgresql/generic.nix
+++ pkgs/servers/sql/postgresql/generic.nix
@@ -561,7 +561,7 @@ let
           psqlSchema = lib.versions.major version;
 
           withJIT = if jitSupport then this.withPackages (_: [ this.jit ]) else null;
-          withoutJIT = this;
+          withoutJIT = this.withPackages (_: [ ]);
 
           pkgs =
             let
