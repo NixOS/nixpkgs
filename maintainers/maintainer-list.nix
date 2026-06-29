54aa862749215377af12a5f00f49bbd47a1940aa
Cole <65095161+zeusec@users.noreply.github.com>
2026-06-29 10:49:10 -0500

    maintainers: add zeusec

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -31006,6 +31006,11 @@
     githubId = 39456023;
     name = "Mike Yim";
   };
+  zeusec = {
+    name = "Cole";
+    github = "zeusec";
+    githubId = 65095161;
+  };
   zevisert = {
     email = "dev@zevisert.ca";
     github = "zevisert";
