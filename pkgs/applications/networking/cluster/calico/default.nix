e39a9de70cc66fa046260a71e178068800bb3592
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-30 13:26:17 +0000

    calico-kube-controllers: 3.31.5 -> 3.32.1

--- pkgs/applications/networking/cluster/calico/default.nix
+++ pkgs/applications/networking/cluster/calico/default.nix
@@ -14,16 +14,16 @@ builtins.mapAttrs
     }:
     buildGoModule rec {
       inherit pname;
-      version = "3.31.5";
+      version = "3.32.1";
 
       src = fetchFromGitHub {
         owner = "projectcalico";
         repo = "calico";
         rev = "v${version}";
-        hash = "sha256-HdTnERSuqJ2ORijvI8AbzLGTwXaRl349p9onKgH4HE8=";
+        hash = "sha256-5xC/QleaI1UhtKkDVOydb2QAhE3kCV54pOIxqU6Valw=";
       };
 
-      vendorHash = "sha256-LwOEshx4VU+a0oBlq/881RC/ViQVDnZZlZs/U3BMJ7M=";
+      vendorHash = "sha256-XDf3DaHa18E7OZPrgr7+uCbbumcjG7Yj4umll0BnifY=";
 
       inherit doCheck subPackages;
 
