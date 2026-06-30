69a0dc9488c7b8b1e3cf21c85271ddcf0d927903
Muhammad Talal Anwar <noreply@talal.ch>
2026-06-30 21:49:55 +0200

    remnote: add talal to maintainers

--- pkgs/by-name/re/remnote/package.nix
+++ pkgs/by-name/re/remnote/package.nix
@@ -39,7 +39,7 @@ appimageTools.wrapType2 {
   meta = {
     description = "Note-taking application focused on learning and productivity";
     homepage = "https://remnote.com/";
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ talal ];
     license = lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
     mainProgram = "remnote";
