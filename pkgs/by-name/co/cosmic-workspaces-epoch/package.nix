55dfd3d7c59ed4ee96d2e60370470ec0200a3cb2
Salva <salva.hg01@gmail.com>
2026-06-30 23:32:15 +0000

    cosmic-workspaces-epoch: 1.1.0 -> 1.2.0

--- pkgs/by-name/co/cosmic-workspaces-epoch/package.nix
+++ pkgs/by-name/co/cosmic-workspaces-epoch/package.nix
@@ -14,7 +14,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-workspaces-epoch";
-  version = "1.1.0";
+  version = "1.2.0";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
