8a1a74f461af1d32afbb509673093e1d2e1538e1
Daniel Hwang <whadmia@gmail.com>
2026-07-01 17:22:13 -0500

    gopodder: init at 1.2.1

---
+++ pkgs/by-name/go/gopodder/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  gitUpdater,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "gopodder";
+  version = "1.2.1";
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "cbrgm";
+    repo = "gopodder";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-o/iQnr8WLArecRyMttCluuEYwKirKsOJyj5a7tdulVo=";
+  };
+
+  vendorHash = "sha256-iG2IUfBVLQ7P0W4HOiGShVyD4mGUQ0dfGjG4XIYVtWU=";
+
+  passthru.updateScript = gitUpdater {
+    rev-prefix = "v";
+  };
+
+  meta = {
+    description = "Self-hostable podcast synchronization server compatible with the gPodder API";
+    homepage = "https://github.com/cbrgm/gopodder";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ nielmin ];
+    mainProgram = "gopodder";
+  };
+})
