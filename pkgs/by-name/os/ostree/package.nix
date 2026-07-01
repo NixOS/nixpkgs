6a94b9238fa7a2ea78143562e933bcddc52fb16c
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-01 23:25:10 +0000

    ostree-full: 2026.1 -> 2026.2

--- pkgs/by-name/os/ostree/package.nix
+++ pkgs/by-name/os/ostree/package.nix
@@ -55,7 +55,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "ostree";
-  version = "2026.1";
+  version = "2026.2";
 
   outputs = [
     "out"
@@ -66,7 +66,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://github.com/ostreedev/ostree/releases/download/v${finalAttrs.version}/libostree-${finalAttrs.version}.tar.xz";
-    hash = "sha256-jnfChd1vpexfsGMTA5CXe+cn/hEQczXth3ikA4UGnpU=";
+    hash = "sha256-ooHy22MfNyHs1Lnid5oer1bi0D8sxHYpqfARfxIBaoM=";
   };
 
   patches = [
