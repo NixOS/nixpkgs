bc9d617dc7511c9e30fef1d0c506aa8a101f5e35
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-01 19:23:39 +0000

    python3Packages.django-health-check: 4.4.2 -> 4.4.3

--- pkgs/development/python-modules/django-health-check/default.nix
+++ pkgs/development/python-modules/django-health-check/default.nix
@@ -22,14 +22,14 @@
 
 buildPythonPackage rec {
   pname = "django-health-check";
-  version = "4.4.2";
+  version = "4.4.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "codingjoe";
     repo = "django-health-check";
     tag = version;
-    hash = "sha256-O/s++NN07B6I8YVi2HetIRY9IPtnh6Br5QzSH61NQy0=";
+    hash = "sha256-brC/gMqxo6BsfMA+4u9alOtIH4js4EgdExT1LL0QXxU=";
   };
 
   build-system = [
