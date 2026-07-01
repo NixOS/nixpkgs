b46f0e19984c43eacebee83e4a48d756bf87bdaf
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-01 07:31:49 +0000

    python3Packages.weblate-schemas: 2026.4 -> 2026.6
    Diff: https://github.com/WeblateOrg/weblate_schemas/compare/2026.4...2026.8
    
    Changelog: https://github.com/WeblateOrg/weblate_schemas/blob/2026.8/CHANGES.rst

--- pkgs/development/python-modules/weblate-schemas/default.nix
+++ pkgs/development/python-modules/weblate-schemas/default.nix
@@ -3,19 +3,18 @@
   buildPythonPackage,
   fetchFromGitHub,
   setuptools,
-  fqdn,
   jsonschema,
-  rfc3987,
-  strict-rfc3339,
   fedora-messaging,
   pytestCheckHook,
+  pytest-cov-stub,
 }:
 
 buildPythonPackage (finalAttrs: {
   pname = "weblate-schemas";
-  version = "2026.4";
+  version = "2026.8";
 
   pyproject = true;
+  __structuredAttrs = true;
 
   # nixpkgs-update: no auto update
   # Only weblate uses this and we want to follow its version constraints
@@ -23,7 +22,7 @@ buildPythonPackage (finalAttrs: {
     owner = "WeblateOrg";
     repo = "weblate_schemas";
     tag = finalAttrs.version;
-    hash = "sha256-OPuhRsUmVte54UPNna76N5Kbg1Tl7p8OdKbE6VHWcvg=";
+    hash = "sha256-0RiRD5CGzVSDE+83VYpZBQ4L6axmRrhnNQvQ1VChOLE=";
   };
 
   build-system = [ setuptools ];
@@ -34,6 +33,7 @@ buildPythonPackage (finalAttrs: {
 
   nativeCheckInputs = [
     pytestCheckHook
+    pytest-cov-stub
     fedora-messaging
   ]
   ++ jsonschema.optional-dependencies.format;
