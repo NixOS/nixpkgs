4e434407c46c87dc17201a8211d8c44c828c0032
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-01 15:10:39 +0000

    gdevelop: 5.6.269 -> 5.6.273

--- pkgs/by-name/gd/gdevelop/darwin.nix
+++ pkgs/by-name/gd/gdevelop/darwin.nix
@@ -18,7 +18,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://github.com/4ian/GDevelop/releases/download/v${version}/GDevelop-5-${version}-universal-mac.zip";
-    hash = "sha256-B/QyB6ZdyjIBBOOZG1nnVcXyqbPGyf56AndELzi3IZY=";
+    hash = "sha256-pW+2vcKMe5d104nPhTPnNXVkOpGNTwbwiHxCBCGmuj8=";
   };
 
   sourceRoot = ".";
