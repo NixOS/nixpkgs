35946ed3e64161e8e853f9532ecfff2e9202340e
Kerstin Humm <kerstin@erictapen.name>
2026-07-01 17:39:12 +0200

    python3Packages.translate-toolkit: 3.19.11 -> 3.19.19
    Diff: https://github.com/translate/translate/compare/3.19.11...3.19.19
    
    Changelog: https://docs.translatehouse.org/projects/translate-toolkit/en/latest/releases/3.19.19.html

--- pkgs/development/python-modules/translate-toolkit/default.nix
+++ pkgs/development/python-modules/translate-toolkit/default.nix
@@ -34,8 +34,9 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "translate-toolkit";
-  version = "3.19.11";
+  version = "3.19.19";
   pyproject = true;
+  __structuredAttrs = true;
 
   # nixpkgs-update: no auto update
   # Only weblate uses this and we want to follow its version constraints
@@ -43,7 +44,7 @@ buildPythonPackage (finalAttrs: {
     owner = "translate";
     repo = "translate";
     tag = finalAttrs.version;
-    hash = "sha256-+94oo6IYnRR4jnR60C3WNjesK6Tk6jND3xsYyx6sw0U=";
+    hash = "sha256-TWEVrZHtMgXn4oG3Wfx3LMbAoX1S770PCdRoq/qKzZE=";
   };
 
   build-system = [ setuptools-scm ];
@@ -92,7 +93,7 @@ buildPythonPackage (finalAttrs: {
     description = "Useful localization tools for building localization & translation systems";
     homepage = "https://toolkit.translatehouse.org/";
     changelog = "https://docs.translatehouse.org/projects/translate-toolkit/en/latest/releases/${finalAttrs.src.tag}.html";
-    license = lib.licenses.gpl2Plus;
+    license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ erictapen ];
   };
 })
