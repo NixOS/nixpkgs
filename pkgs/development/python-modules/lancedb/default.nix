2a0b3b5281728f0a439d243276bf52e86c802b44
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-02 22:32:08 +0000

    python3Packages.lancedb: 0.32.0 -> 0.34.0

--- pkgs/development/python-modules/lancedb/default.nix
+++ pkgs/development/python-modules/lancedb/default.nix
@@ -42,7 +42,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "lancedb";
-  version = "0.32.0";
+  version = "0.34.0";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -50,14 +50,14 @@ buildPythonPackage (finalAttrs: {
     owner = "lancedb";
     repo = "lancedb";
     tag = "python-v${finalAttrs.version}";
-    hash = "sha256-OIoQCk0YlWpaaau4AiWxarvH4oy1rAjaS9yvs3mIzzo=";
+    hash = "sha256-QCHh0ap3VAY+pUo4FquCxk+qXo4sXR7Nshrptg+dkBY=";
   };
 
   buildAndTestSubdir = "python";
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-rfAhvC6byg134NF21CR5n0A0DL42CLGy7VvHi9aZUrw=";
+    hash = "sha256-wfChrpkMATp7WfvgkSOzl6GXZMBMfv5ZAAGdFXNlQBw=";
   };
 
   build-system = [ rustPlatform.maturinBuildHook ];
