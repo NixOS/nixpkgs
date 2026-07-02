eaa544a9f93bbc1433ce230d83cb41d452ef9fe3
Harinn <prinn.dev@pm.me>
2026-07-03 02:16:03 +0700

    python3Packages.simanneal: modernize

--- pkgs/development/python-modules/simanneal/default.nix
+++ pkgs/development/python-modules/simanneal/default.nix
@@ -6,15 +6,17 @@
   pytest,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "simanneal";
   version = "0.5.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "perrygeo";
     repo = "simanneal";
-    rev = version;
+    tag = finalAttrs.version;
     hash = "sha256-yKZHkrf6fM0WsHczIEK5Kxusz5dSBgydK3fLu1nDyvk=";
   };
 
@@ -23,10 +25,12 @@ buildPythonPackage rec {
   nativeCheckInputs = [ pytest ];
   checkPhase = "pytest tests";
 
+  pythonImportsCheck = [ "simanneal" ];
+
   meta = {
     description = "Python implementation of the simulated annealing optimization technique";
     homepage = "https://github.com/perrygeo/simanneal";
     license = lib.licenses.isc;
     maintainers = with lib.maintainers; [ veprbl ];
   };
-}
+})
