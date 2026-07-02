f5b2ea87407ebd1ddc1d3fb51e7b0cce13aa1418
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2026-07-03 08:40:35 +1000

    mediamtx: 1.18.2 -> 1.19.2
    Release notes: https://github.com/bluenviron/mediamtx/releases/tag/v1.19.2
    Compare diff: https://github.com/bluenviron/mediamtx/compare/v1.18.2...v1.19.2

--- pkgs/by-name/me/mediamtx/package.nix
+++ pkgs/by-name/me/mediamtx/package.nix
@@ -15,32 +15,27 @@ in
 buildGo126Module (finalAttrs: {
   pname = "mediamtx";
   # check for hls.js version updates in internal/servers/hls/hlsjsdownloader/VERSION
-  version = "1.18.2";
+  version = "1.19.2";
 
   src = fetchFromGitHub {
     owner = "bluenviron";
     repo = "mediamtx";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-qhW2q3PoOXBRtW4YXKMIYbQG4LypVLcSeV/uQ66hOgg=";
+    hash = "sha256-jUyA0XjR92I6RNTXtFKqrUG0v7P3DDnoThWHSxTQE2I=";
   };
 
-  vendorHash = "sha256-dWMwD1jG7+69d00T/T+7jY6MgodGuBpSBDwEHSGOKLQ=";
+  vendorHash = "sha256-i3J91K0aQ6/vC+HsJjjjWNjl9vX9uOSEzmNjB6cMU6Q=";
 
   postPatch = ''
     cp ${hlsJs} internal/servers/hls/hls.min.js
     echo "v${finalAttrs.version}" > internal/core/VERSION
 
     # disable binary-only rpi camera support
-    substituteInPlace internal/staticsources/rpicamera/camera_other.go \
+    substituteInPlace internal/staticsources/rpicamera/source_other.go \
       --replace-fail '!linux || (!arm && !arm64)' 'linux || !linux'
-    substituteInPlace internal/staticsources/rpicamera/{params_serialize,pipe}.go \
+    substituteInPlace internal/staticsources/rpicamera/*_arm_.go \
       --replace-fail '(linux && arm) || (linux && arm64)' 'linux && !linux'
-    substituteInPlace internal/staticsources/rpicamera/camera_arm32_.go \
-      --replace-fail 'linux && arm' 'linux && !linux'
-    substituteInPlace internal/staticsources/rpicamera/camera_arm64_.go \
-      --replace-fail 'linux && arm64' 'linux && !linux'
-    substituteInPlace internal/staticsources/rpicamera/camera_arm_.go \
-      --replace-fail '(linux && arm) || (linux && arm64)' 'linux && !linux'
+    rm internal/staticsources/rpicamera/camera_linux_arm*.go
   '';
 
   subPackages = [ "." ];
