9e2158d29d485ec6c73ccb823b2989fba79d2adf
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-03 21:43:45 +0200

    python3Packages.pymisp: init at 2.5.34.1
    Python library using the MISP Rest API
    
    https://github.com/MISP/PyMISP

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14765,6 +14765,8 @@ self: super: with self; {
 
   pymilvus = callPackage ../development/python-modules/pymilvus { };
 
+  pymisp = callPackage ../development/python-modules/pymisp { };
+
   pymitsubishi = callPackage ../development/python-modules/pymitsubishi { };
 
   pymitv = callPackage ../development/python-modules/pymitv { };
