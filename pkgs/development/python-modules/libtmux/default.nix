9e8564fb057f5cf3e8171653c099eaf33c96c5fa
Alexis Hildebrandt <afh@surryhill.net>
2026-07-03 20:01:07 +0200

    python3Packages.libtmux: 0.59.0 -> 0.60.0
    Diff: https://github.com/tmux-python/libtmux/compare/v0.59.0...v0.60.0
    
    Changelog: https://github.com/tmux-python/libtmux/raw/v0.60.0/CHANGES

--- pkgs/development/python-modules/libtmux/default.nix
+++ pkgs/development/python-modules/libtmux/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "libtmux";
-  version = "0.59.0";
+  version = "0.60.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tmux-python";
     repo = "libtmux";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-RsK3nVGpgNX05tCc5kK5GFLUS5vVoe8NRKgg7Y/DzwM=";
+    hash = "sha256-1h+qkQDYRwP2peMOvKC1kk4DFcG4cwuBApsF8MmkWbo=";
   };
 
   patches = [ ./0001-fix-test_control_mode_stdout_preserves_non_ascii_out.patch ];
