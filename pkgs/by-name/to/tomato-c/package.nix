419e6a416f8aa13632270186744a6dca81c08f40
3JlOy_PYCCKUI <3jl0y_pycckui@riseup.net>
2026-07-03 05:47:22 +0300

    tomato-c: adopt

--- pkgs/by-name/to/tomato-c/package.nix
+++ pkgs/by-name/to/tomato-c/package.nix
@@ -58,7 +58,7 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://github.com/gabrielzschmitz/Tomato.C";
     description = "Pomodoro timer written in pure C";
     license = with lib.licenses; [ gpl3Plus ];
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ _3JlOy-PYCCKUi ];
     mainProgram = "tomato";
     platforms = lib.platforms.unix;
   };
