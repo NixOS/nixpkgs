3f4242c1d80a474b6cc40d9cf2a94f1d7032a84a
Gliczy <129636582+Gliczy@users.noreply.github.com>
2026-07-04 12:37:17 +0200

    nixosTests.scx: remove cscheds
    They are pretty much unmaintained, and only serves as examples, it makes
    little sense to test them.
    
    (cherry picked from commit 9bb5c308eb06d0f5e03d0d055a6fa8cc75d0596a)

--- nixos/tests/scx/default.nix
+++ nixos/tests/scx/default.nix
@@ -15,14 +15,11 @@
       bpfland.configuration.services.scx.scheduler = "scx_bpfland";
       cosmos.configuration.services.scx.scheduler = "scx_cosmos";
       flash.configuration.services.scx.scheduler = "scx_flash";
-      flatcg.configuration.services.scx.scheduler = "scx_flatcg";
       lavd.configuration.services.scx.scheduler = "scx_lavd";
-      nest.configuration.services.scx.scheduler = "scx_nest";
       p2dq.configuration.services.scx.scheduler = "scx_p2dq";
       rlfifo.configuration.services.scx.scheduler = "scx_rlfifo";
       rustland.configuration.services.scx.scheduler = "scx_rustland";
       rusty.configuration.services.scx.scheduler = "scx_rusty";
-      simple.configuration.services.scx.scheduler = "scx_simple";
     };
   };
 
@@ -32,14 +29,11 @@
       "bpfland",
       "cosmos",
       "flash",
-      "flatcg",
       "lavd",
-      "nest",
       "p2dq",
       "rlfifo",
       "rustland",
       "rusty",
-      "simple"
     ]
 
     def activate_specialisation(name: str):
