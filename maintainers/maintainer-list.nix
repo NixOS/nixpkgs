353f6dd33165764d715a70f18fe1a4d6d629daa8
bornav <borna.vincek1@gmail.com>
2026-07-04 16:53:13 +0200

    maintainers: add bornav

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3944,6 +3944,12 @@
     githubId = 1743184;
     name = "Boris Babić";
   };
+  bornav = {
+    email = "borna.vincek1@gmail.com";
+    github = "bornav";
+    githubId = 51048565;
+    name = "Borna Vincek";
+  };
   bosu = {
     email = "boriss@gmail.com";
     github = "bosu";
