f1aa936fdc319b5533da4816cc7711272cbeabf9
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-04 20:13:51 +0200

    python3Packages.infrared-protocols: 5.8.1 -> 6.3.1
    https://github.com/home-assistant-libs/infrared-protocols/releases/tag/6.3.1

--- pkgs/development/python-modules/infrared-protocols/default.nix
+++ pkgs/development/python-modules/infrared-protocols/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "infrared-protocols";
-  version = "5.8.1";
+  version = "6.3.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "home-assistant-libs";
     repo = "infrared-protocols";
     tag = finalAttrs.version;
-    hash = "sha256-CSVnH+U/dqp5vjA4eWEJEFT0LZgaAG3OC1rcgyKIcJE=";
+    hash = "sha256-6kyb0a0cCwVSS4evDGg0Z7wLGhDUHnLeXUJ9PW+fhHk=";
   };
 
   build-system = [ setuptools ];
