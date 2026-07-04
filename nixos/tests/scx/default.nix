3c57a1d1c6a2180bc2d67668348f912b6d070ddd
Gliczy <129636582+Gliczy@users.noreply.github.com>
2026-07-04 14:11:26 +0200

    nixosTests.scx: add missing rustscheds
    `scx_layered` and `scx_mitosis` are excluded as they both require a
    config to be run.
    
    (cherry picked from commit 22ded46682c17e2a9b1091b3fa2252e0143897cd)

--- nixos/tests/scx/default.nix
+++ nixos/tests/scx/default.nix
@@ -13,13 +13,19 @@
     specialisation = {
       beerland.configuration.services.scx.scheduler = "scx_beerland";
       bpfland.configuration.services.scx.scheduler = "scx_bpfland";
+      cake.configuration.services.scx.scheduler = "scx_cake";
+      chaos.configuration.services.scx.scheduler = "scx_chaos";
       cosmos.configuration.services.scx.scheduler = "scx_cosmos";
       flash.configuration.services.scx.scheduler = "scx_flash";
+      flow.configuration.services.scx.scheduler = "scx_flow";
+      forge.configuration.services.scx.scheduler = "scx_forge";
       lavd.configuration.services.scx.scheduler = "scx_lavd";
       p2dq.configuration.services.scx.scheduler = "scx_p2dq";
+      pandemonium.configuration.services.scx.scheduler = "scx_pandemonium";
       rlfifo.configuration.services.scx.scheduler = "scx_rlfifo";
       rustland.configuration.services.scx.scheduler = "scx_rustland";
       rusty.configuration.services.scx.scheduler = "scx_rusty";
+      tickless.configuration.services.scx.scheduler = "scx_tickless";
     };
   };
 
@@ -27,13 +33,19 @@
     specialisation = [
       "beerland",
       "bpfland",
+      "cake",
+      "chaos",
       "cosmos",
       "flash",
+      "flow",
+      "forge",
       "lavd",
       "p2dq",
+      "pandemonium",
       "rlfifo",
       "rustland",
       "rusty",
+      "tickless",
     ]
 
     def activate_specialisation(name: str):
