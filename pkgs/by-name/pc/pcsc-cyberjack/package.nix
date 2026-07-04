1096b0e7b7cf15b03ee514f2d7824c1d1038fc98
Benno Fünfstück <git@benno.five.name>
2026-07-04 15:36:40 +0200

    pcsc-cyberjack: add udev rules
    pcscd runs as user/group pcscd, so those udev rules are needed for it to
    work. With these rules, pkgs.pcsc-cyberjack can be added to
    services.udev.packages to get the necessary rules.

--- pkgs/by-name/pc/pcsc-cyberjack/package.nix
+++ pkgs/by-name/pc/pcsc-cyberjack/package.nix
@@ -6,12 +6,43 @@
   pkg-config,
   libusb1,
   pcsclite,
+  writeText,
 }:
 
 let
   version = "3.99.5";
   suffix = "SP15";
   tarBall = "${version}final.${suffix}";
+  # source: https://salsa.debian.org/debian/pcsc-cyberjack/-/blob/master/debian/libifd-cyberjack6.udev?ref_type=heads
+  udevRules = writeText "pcsc-cyberjack.rules" ''
+    # If not adding the device, go away
+    ACTION!="add", GOTO="cyberjack_rules_end"
+    SUBSYSTEM!="usb", GOTO="cyberjack_rules_end"
+    ENV{DEVTYPE}!="usb_device", GOTO="cyberjack_rules_end"
+
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0100", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0300", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0400", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0401", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0412", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0485", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0500", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0501", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0502", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0503", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0504", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0505", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0506", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0507", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0525", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0580", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="2000", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="0551", MODE="660", GROUP="pcscd"
+    ATTR{idVendor}=="0c4b", ATTR{idProduct}=="2002", MODE="660", GROUP="pcscd"
+
+    # All done
+    LABEL="cyberjack_rules_end"
+  '';
 
 in
 stdenv.mkDerivation rec {
@@ -47,7 +78,12 @@ stdenv.mkDerivation rec {
     "--bindir=${placeholder "tools"}/bin"
   ];
 
-  postInstall = "make -C tools/cjflash install";
+  postInstall = ''
+    make -C tools/cjflash install
+
+    mkdir -p $out/lib/udev/rules.d
+    ln -s "${udevRules}" $out/lib/udev/rules.d/90-pcsc-cyberjack.rules
+  '';
 
   meta = {
     description = "REINER SCT cyberJack USB chipcard reader user space driver";
