9a58143dca7e9a08df14b522ad65556e2d03f53e
Gaetan Lepage <gaetan@glepage.com>
2026-07-04 20:33:25 +0000

    python3Packages.cuda-core: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3526,6 +3526,8 @@ self: super: with self; {
 
   cuda-bindings = callPackage ../development/python-modules/cuda-bindings { };
 
+  cuda-core = callPackage ../development/python-modules/cuda-core { };
+
   cuda-pathfinder = callPackage ../development/python-modules/cuda-pathfinder { };
 
   cuda-tile = callPackage ../development/python-modules/cuda-tile { };
