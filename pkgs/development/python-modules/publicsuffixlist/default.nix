75393e01daa043b08885c5abceba83f3925a2256
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-04 16:03:01 +0200

    python3Packages.publicsuffixlist: 1.0.2.20260702 -> 1.0.2.20260703
    Changelog: https://github.com/ko-zu/psl/blob/v1.0.2.20260703-gha/CHANGES.md

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "publicsuffixlist";
-  version = "1.0.2.20260702";
+  version = "1.0.2.20260703";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-DqicazZHXh8ZzxIpYC8NuvsuGIp4xfrsxk0/AbBLgNA=";
+    hash = "sha256-NTZ4oalz09w74HpiWHRJP95yartWAHc8baWeQPBnZ3Q=";
   };
 
   postPatch = ''
