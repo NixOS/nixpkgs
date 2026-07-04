2a01b7314e3a486cfe6180592757e08628822494
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-04 13:02:13 +0000

    koffan: 2.11.0 -> 2.12.0

--- pkgs/by-name/ko/koffan/package.nix
+++ pkgs/by-name/ko/koffan/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "koffan";
-  version = "2.11.0";
+  version = "2.12.0";
 
   src = fetchFromGitHub {
     owner = "PanSalut";
     repo = "Koffan";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-TaZ65WE6EfrENpB/c3oTDgMEsnBkiEt62B4jDEWCf98=";
+    hash = "sha256-E4R2FrL6pqwz1yuF+0TuSIE33/ksrvE0U9XO4z11AfE=";
   };
 
   vendorHash = "sha256-BYehi5LQQ0MIsKG/fN3DHaQwKVmxUFrvWGrKZeKj+ow=";
