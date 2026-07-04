8e5b3d3b3c98ae7c2cdc7f8b4a1b21deaeadc168
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-04 19:40:55 +0200

    python3Packages.django-rq: 4.1-unstable-2026-05-28 -> 4.1.1
    https://github.com/rq/django-rq/releases/tag/v4.1.1

--- pkgs/development/python-modules/django-rq/default.nix
+++ pkgs/development/python-modules/django-rq/default.nix
@@ -16,19 +16,15 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "django-rq";
-  version = "4.1-unstable-2026-05-28";
+  version = "4.1.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "rq";
     repo = "django-rq";
-    rev = "39539ab680cf6a23073f4907b5e7332226494784";
-    hash = "sha256-Yrc0HQmCsrdTs66RZwwmZQraxB5/eJG8dOPcVn53rjA=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-pp8/7pMG4CHEe+jsmZ9euAV8eEMW0Hh4ecTTHnP6DiE=";
   };
-
-  patches = [
-    ./redis-py-8.0-compat.patch
-  ];
 
   build-system = [ hatchling ];
 
@@ -65,7 +61,7 @@ buildPythonPackage (finalAttrs: {
   meta = {
     description = "Simple app that provides django integration for RQ (Redis Queue)";
     homepage = "https://github.com/rq/django-rq";
-    # changelog = "https://github.com/rq/django-rq/releases/tag/${finalAttrs.src.tag}";
+    changelog = "https://github.com/rq/django-rq/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ hexa ];
   };
