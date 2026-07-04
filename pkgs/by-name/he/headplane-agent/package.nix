63f30c1c7d0e323d190246667746c6dfb0148240
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-04 20:07:50 +0000

    headplane-agent: 0.6.3 -> 0.7.0

--- pkgs/by-name/he/headplane-agent/package.nix
+++ pkgs/by-name/he/headplane-agent/package.nix
@@ -7,13 +7,13 @@ buildGoModule (finalAttrs: {
   pname = "headplane-agent";
   __structuredAttrs = true;
   # Note, if you are upgrading this, you should upgrade headplane at the same time
-  version = "0.6.3";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "tale";
     repo = "headplane";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zvJUTKRIlHyPMq80teVXBSb7K9Zz44Kuuj2PPi6qIOw=";
+    hash = "sha256-UMAGsrG2xfpgWlsDhf4aWJKoOrUbruucDNOhCJcYmQQ=";
   };
 
   vendorHash = "sha256-MvrqKMD+A+qBZmzQv+T9920U5uJop+pjfJpZdm2ZqEA=";
