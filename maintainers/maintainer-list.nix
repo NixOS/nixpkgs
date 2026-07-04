26a56f96fc83a89d7074bb510e6dcfaedac44f11
Ali Abrar <ali.abrar@obsidian.systems>
2026-07-04 15:46:03 -0400

    waynav: init at 1.0.1, maintainers: add ali-abrar
    waynav is a keyboard-driven grid mouse navigator for Wayland, a
    from-scratch rewrite of keynav for wlroots-compatible compositors.
    Tested on niri.
    
    repo: https://github.com/kovetskiy/waynav

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1416,6 +1416,11 @@
       }
     ];
   };
+  ali-abrar = {
+    name = "Ali Abrar";
+    github = "ali-abrar";
+    githubId = 7432518;
+  };
   alinnow = {
     name = "Alin";
     email = "alin@alin.ovh";
