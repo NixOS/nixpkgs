1b766cb64e524468827b9e05bdd51993768731e2
alliumm <33610620+alliumm@users.noreply.github.com>
2026-07-05 09:49:50 -0700

    lms: 3.74.0 -> 3.78.0

--- pkgs/by-name/lm/lms/package.nix
+++ pkgs/by-name/lm/lms/package.nix
@@ -19,17 +19,18 @@
   openssl,
   xxHash,
   pugixml,
+  onnxruntime,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lms";
-  version = "3.74.0";
+  version = "3.78.0";
 
   src = fetchFromGitHub {
     owner = "epoupon";
     repo = "lms";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-D1Sg6XzZ8t/dFKrVh7k+KGLg2r6LeLGJk4FweVb4L1A=";
+    hash = "sha256-uOijIipay4ncE8hP6vJG9vOGiD/Ad6WJHEQ7P1HKi/Y=";
   };
 
   strictDeps = true;
@@ -53,6 +54,7 @@ stdenv.mkDerivation (finalAttrs: {
     openssl
     xxHash
     pugixml
+    onnxruntime
   ];
 
   postPatch = ''
@@ -62,8 +64,7 @@ stdenv.mkDerivation (finalAttrs: {
   postInstall = ''
     substituteInPlace $out/share/lms/lms.conf --replace-fail "/usr/bin/ffmpeg" "${lib.getExe ffmpeg}"
     substituteInPlace $out/share/lms/lms.conf --replace-fail "/usr/share/Wt/resources" "${wt}/share/Wt/resources"
-    substituteInPlace $out/share/lms/lms.conf --replace-fail "/usr/share/lms/docroot" "$out/share/lms/docroot"
-    substituteInPlace $out/share/lms/lms.conf --replace-fail "/usr/share/lms/approot" "$out/share/lms/approot"
+    substituteInPlace $out/share/lms/lms.conf --replace-fail "/usr/share/lms" "$out/share/lms"
     substituteInPlace $out/share/lms/default.service --replace-fail "/usr/bin/lms" "$out/bin/lms"
     install -Dm444 $out/share/lms/default.service -T $out/lib/systemd/system/lmsd.service
   '';
