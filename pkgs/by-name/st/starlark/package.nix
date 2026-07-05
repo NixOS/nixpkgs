4917a4a5baaed0ffde9f09ce0e5851e5500b45ca
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-05 12:04:02 +0000

    starlark: 0-unstable-2026-06-13 -> 0-unstable-2026-06-30

--- pkgs/by-name/st/starlark/package.nix
+++ pkgs/by-name/st/starlark/package.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule {
   pname = "starlark";
-  version = "0-unstable-2026-06-13";
+  version = "0-unstable-2026-06-30";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "starlark-go";
-    rev = "8ba36ccb83fb02b223182e27808a6d5d0636afb9";
-    hash = "sha256-TH97v0LZt2W/0P9jPuKBmBCa079o3FIcX2hHBidhC3Y=";
+    rev = "529d8e869a14da46efe75b7e904a6183dd26ae29";
+    hash = "sha256-/DTLgF5S0lzftLVF2XTk0k5auuGBWnZqjN092i0eOKA=";
   };
 
   vendorHash = "sha256-Ejw5f5ulEcLHm4WYKatwA7FZ9lfdqZTOE3SdkaK6jYE=";
