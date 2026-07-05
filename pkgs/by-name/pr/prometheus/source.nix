8e748c7ca37698818ca82a91ccd64c914dfadfb5
Jonathan Davies <jpds@protonmail.com>
2026-07-05 09:05:38 +0000

    prometheus: 3.12.0 → 3.13.0
    Upstream migrated `web/ui` from `npm` to `pnpm` in this release

--- pkgs/by-name/pr/prometheus/source.nix
+++ pkgs/by-name/pr/prometheus/source.nix
@@ -1,6 +1,6 @@
 {
-  version = "3.12.0";
-  hash = "sha256-xeENUVmG9tbIF+7i2u9zuvo7RXI9iNWFVDNUfNpF6/4=";
-  npmDepsHash = "sha256-cHMI5DqSRpIanrgk/H3aFUHLrGXH1v796PH1qDrCnbE=";
-  vendorHash = "sha256-caSI9uzbH93j06sJus9jSqo6qHKbP8D9DuDkiAlnfF4=";
+  version = "3.13.0";
+  hash = "sha256-v6jk4MqoxcfK+yj+T31Ovqj1tyh3mc4aEr8BD0vjBOc=";
+  pnpmDepsHash = "sha256-Z1TYYZhELi+rIiuleN8xR/WiMn9TF4KotFMTOsR2e6Y=";
+  vendorHash = "sha256-yvzQHfe7yd6Sjh1Vd2VxTp3jK8OWoKTmJ2uMyXX3+xs=";
 }
