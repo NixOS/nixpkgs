a625f44f426aab6118e25643cee2416a71e47f67
Gavin John <gavinnjohn@gmail.com>
2026-07-04 23:50:25 -0400

    stardust-xr-*: enable strictDeps and __structuredAttrs where not already enabled

--- pkgs/by-name/st/stardust-xr-gravity/package.nix
+++ pkgs/by-name/st/stardust-xr-gravity/package.nix
@@ -16,6 +16,9 @@ rustPlatform.buildRustPackage {
     hash = "sha256-upw0MjGccSI1B10wabKPMGrEo7ATfg4a7Hzaucbf99w=";
   };
 
+  __structuredAttrs = true;
+  strictDeps = true;
+
   cargoHash = "sha256-tkWY+dLFDnyir6d0supR3Z202p5i4UewY+J66mL1x/o=";
 
   passthru.updateScript = nix-update-script { };
