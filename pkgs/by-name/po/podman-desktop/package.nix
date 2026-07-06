a55147f91bfad8dbe3dc144249ae10a70c053184
Kenichi Kamiya <kachick1@gmail.com>
2026-07-06 15:50:22 +0900

    podman-desktop: remove obsolete workaround
    This was only needed before version 1.28.0.
    
    (cherry picked from commit 6af20830bc1b172db6c8181b76ce00c820102479)

--- pkgs/by-name/po/podman-desktop/package.nix
+++ pkgs/by-name/po/podman-desktop/package.nix
@@ -138,8 +138,6 @@ stdenv.mkDerivation (finalAttrs: {
         wrapProgram "$out/Applications/${appName}.app/Contents/MacOS/${appName}" \
           ${commonWrapperArgs}
       ''
-      # Enforce X11 to avoid the Wayland dashboard issue.
-      # Revisit this once issue https://github.com/podman-desktop/podman-desktop/issues/14388 is resolved.
       + lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
         mkdir -p "$out/share/lib/podman-desktop"
         cp -r dist/*-unpacked/{locales,resources{,.pak}} "$out/share/lib/podman-desktop"
@@ -155,7 +153,7 @@ stdenv.mkDerivation (finalAttrs: {
 
         makeWrapper '${electron}/bin/electron' "$out/bin/podman-desktop" \
           --add-flags "$out/share/lib/podman-desktop/resources/app.asar" \
-          --set XDG_SESSION_TYPE 'x11' \
+          --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
           ${commonWrapperArgs} \
           --inherit-argv0
       ''
