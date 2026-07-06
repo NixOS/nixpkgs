a5dd3c2ea5c4550b352a06c888cff301c44abd38
lukas-sgx <lukas.soigneux@epitech.eu>
2026-07-06 21:05:58 +0200

    maintainers: add KirCK

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14586,6 +14586,12 @@
     github = "kiranshila";
     githubId = 6305359;
   };
+  KirCK = {
+    email = "Arikkenebas09@gmail.com";
+    github = "AristarhKenebas";
+    githubId = 142233116;
+    name = "Aristarh Kenebas";
+  };
   kirelagin = {
     email = "kirelagin@gmail.com";
     matrix = "@kirelagin:matrix.org";
