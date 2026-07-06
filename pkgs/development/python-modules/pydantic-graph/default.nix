378b589b69f12445bdacb280842a64590763496b
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-07 00:22:46 +0200

    python3Packages.pydantic-graph: 2.0.0 -> 2.5.0
    https://github.com/pydantic/pydantic-ai/releases/tag/v2.5.0

--- pkgs/development/python-modules/pydantic-graph/default.nix
+++ pkgs/development/python-modules/pydantic-graph/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pydantic-graph";
-  version = "2.0.0";
+  version = "2.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pydantic";
     repo = "pydantic-ai";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-4mqGRKFcQAexgccvUoZP0oDsmgfK9jzFPM1cIvWUOUA=";
+    hash = "sha256-SpWiFEN9wwBtKSRDKZ8buPvBiRU3cqjwTCLFkGKpeOs=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/pydantic_graph";
