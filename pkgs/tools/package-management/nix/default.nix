909c958b3ee87771cf9058b44f44bb3c8d9d4f29
Sergei Zimmerman <sergei@zimmerman.foo>
2026-07-06 23:53:46 +0300

    nixVersions.nix_2_34: 2.34.7 -> 2.34.8
    Notably, this has the fix for libcurl 8.21 and wakeup consumption.
    
    Diff: https://github.com/NixOS/nix/compare/2.34.7...2.34.8
    (cherry picked from commit 88b362d9770356c0170dcc01db2e6d56612be0b7)

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -197,14 +197,14 @@ lib.makeExtensible (
 
       nixComponents_2_34 =
         (nixDependencies.callPackage ./modular/packages.nix rec {
-          version = "2.34.7";
+          version = "2.34.8";
           inherit teams;
           otherSplices = generateSplicesForNixComponents "nixComponents_2_34";
           src = fetchFromGitHub {
             owner = "NixOS";
             repo = "nix";
             tag = version;
-            hash = "sha256-uj5KNW8Vdm60FCUxD2KsrCVH/WwoemvczWmmrb3Gvlo=";
+            hash = "sha256-Rvy1PmIUMGI0IS/kwDwmf/VrorU8v1iZYejssSVu1rY=";
           };
         }).appendPatches
           patches_common;
