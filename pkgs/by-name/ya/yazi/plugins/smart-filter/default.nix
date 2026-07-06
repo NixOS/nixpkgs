b0b6799f9f78511a6c67ccdf261558c091a45aa4
Austin Horstman <khaneliman12@gmail.com>
2026-07-06 12:23:21 -0500

    yaziPlugins.smart-filter: 0-unstable-2025-12-29 → 0-unstable-2026-07-04
    Compare: https://github.com/yazi-rs/plugins/compare/517619af126f25f3da096ff156ce46b561b54be3...5d461d85908338371b9433ab6c29707bee3a813b

--- pkgs/by-name/ya/yazi/plugins/smart-filter/default.nix
+++ pkgs/by-name/ya/yazi/plugins/smart-filter/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "smart-filter.yazi";
-  version = "0-unstable-2025-12-29";
+  version = "0-unstable-2026-07-04";
 
   src = fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "517619af126f25f3da096ff156ce46b561b54be3";
-    hash = "sha256-j7fsUmx2nK4Tyj5KCamcCmfs99K6duV+okf8NvzccsI=";
+    rev = "5d461d85908338371b9433ab6c29707bee3a813b";
+    hash = "sha256-vRElXxEe2Am7p6SoxJ7g8GSaka7/4/yZ2zfiOOqUR2I=";
   };
 
   meta = {
