1b71a7409a05209aad70144c8ed65308de16c4d2
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-07 10:54:25 +0000

    tutanota-desktop: 350.260608.0 -> 353.260630.0

--- pkgs/by-name/tu/tutanota-desktop/package.nix
+++ pkgs/by-name/tu/tutanota-desktop/package.nix
@@ -8,11 +8,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "tutanota-desktop";
-  version = "350.260608.0";
+  version = "353.260630.0";
 
   src = fetchurl {
     url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/tutanota-desktop-linux.AppImage";
-    hash = "sha256-y/wUe8kNlA4XmbJrjjAYSnNAMAB8OgVmCrum6bYAyV4=";
+    hash = "sha256-ZJdiufoyZQxZncxyJZd1rhVyBMlkep+8uvchO/D/Krs=";
   };
 
   extraPkgs = pkgs: [ pkgs.libsecret ];
