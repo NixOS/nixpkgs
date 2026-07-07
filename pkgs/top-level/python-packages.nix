2dbac4242beff4db79afd531316756a439bbf3f4
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-07-07 22:04:34 +0200

    python3Packages.comfyui-workflow-templates-media-video: init at 0.3.101

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3491,6 +3491,10 @@ self: super: with self; {
     callPackage ../development/python-modules/comfyui-workflow-templates-media-other
       { };
 
+  comfyui-workflow-templates-media-video =
+    callPackage ../development/python-modules/comfyui-workflow-templates-media-video
+      { };
+
   comicapi = callPackage ../development/python-modules/comicapi { };
 
   comicon = callPackage ../development/python-modules/comicon { };
