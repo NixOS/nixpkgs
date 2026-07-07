d033813d9b8e4652115f5cdafefb043933a8031e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-07 16:47:28 +0200

    python3Packages.django_6: 6.0.6 -> 6.0.7
    https://docs.djangoproject.com/en/6.0/releases/6.0.7/
    https://www.djangoproject.com/weblog/2026/jul/07/security-releases/
    
    Fixes: CVE-2026-48588, CVE-2026-53877, CVE-2026-53878

--- pkgs/development/python-modules/django/6.nix
+++ pkgs/development/python-modules/django/6.nix
@@ -42,7 +42,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "django";
-  version = "6.0.6";
+  version = "6.0.7";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -51,7 +51,7 @@ buildPythonPackage (finalAttrs: {
     owner = "django";
     repo = "django";
     tag = finalAttrs.version;
-    hash = "sha256-hLnTqY64PfaGJ1JJccrxYms41Jp4E4pVq6rmrtFpESE=";
+    hash = "sha256-B28twwEGLcXV0TlQxgRhNBiKhwJd+5f7sL35SkHAkRY=";
   };
 
   patches = [
