0326a768e77103c0d8d6dccfdc3c907df1a16d7b
Sigmanificient <edhyjox@gmail.com>
2026-07-08 00:50:16 +0200

    python3Packages.inotifyrecursive: migration to pyproject

--- pkgs/development/python-modules/inotifyrecursive/default.nix
+++ pkgs/development/python-modules/inotifyrecursive/default.nix
@@ -2,20 +2,23 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   inotify-simple,
 }:
 
 buildPythonPackage rec {
   pname = "inotifyrecursive";
   version = "0.3.5";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "osRQsxdpPkU4QW+Q6x14WFBtr+a4uIUDe9LdmuLa+h4=";
   };
 
-  propagatedBuildInputs = [ inotify-simple ];
+  build-system = [ setuptools ];
+
+  dependencies = [ inotify-simple ];
 
   # No tests included
   doCheck = false;
