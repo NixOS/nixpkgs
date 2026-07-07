138998ca74d541ca9410f5823c07d25dc44b9267
zraexy <zraexy@gmail.com>
2026-07-07 15:44:07 -0800

    orca-slicer: add GStreamer gtksink plugin as dependency

--- pkgs/by-name/or/orca-slicer/package.nix
+++ pkgs/by-name/or/orca-slicer/package.nix
@@ -111,7 +111,7 @@ clangStdenv.mkDerivation (finalAttrs: {
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-good
+    (gst_all_1.gst-plugins-good.override { gtkSupport = true; })
     gtk3
     hicolor-icon-theme
     libsecret
