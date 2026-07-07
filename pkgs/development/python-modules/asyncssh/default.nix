d5ae2397c709cd6c4e72181f53ff8798efce9f48
Sandro Jäckel <sandro.jaeckel@sap.com>
2026-07-07 13:30:20 +0200

    python314Packages.asyncssh: disable another network test

--- pkgs/development/python-modules/asyncssh/default.nix
+++ pkgs/development/python-modules/asyncssh/default.nix
@@ -16,6 +16,7 @@
   pytestCheckHook,
   python-pkcs11,
   setuptools,
+  stdenv,
   typing-extensions,
 }:
 
@@ -84,6 +85,9 @@ buildPythonPackage rec {
     "test_put_name_too_long"
     # SFTP copy ends up with an empty file on ZFS
     "test_copy_max_requests"
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    "test_canonicalize_failure"
   ];
 
   pythonImportsCheck = [ "asyncssh" ];
