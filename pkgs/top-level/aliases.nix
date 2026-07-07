020298927184473939dbb61b2628ea3082e15d99
Sefa Eyeoglu <contact@scrumplex.net>
2026-07-07 12:17:31 +0200

    pnpm_8: drop
    Insecure and unmaintained. No consumers in nixpkgs anymore so it can go.
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1843,6 +1843,7 @@ mapAliases {
   pltScheme = throw "'pltScheme' has been renamed to/replaced by 'racket'"; # Converted to throw 2025-10-27
   plv8 = throw "'plv8' has been removed. Use 'postgresqlPackages.plv8' instead."; # Added 2025-07-19
   pn = throw "'pn' has been removed as upstream was archived in 2020"; # Added 2025-10-17
+  pnpm_8 = throw "'pnpm_8' has been removed as it hasn't been updated since 2024. Please use 'pnpm'"; # Added 2026-07-07
   poac = throw "'poac' has been renamed to/replaced by 'cabinpkg'"; # Converted to throw 2025-10-27
   pocket-updater-utility = throw "'pocket-updater-utility' has been renamed to/replaced by 'pupdate'"; # Converted to throw 2025-10-27
   podofo010 = throw "'podofo010' has been renamed to 'podofo0'"; # Converted to throw 2025-10-27
