f26edb08cecc8d20f092261b6ef7fa8b31010ef2
Guy Chronister <guychronister@outlook.com>
2026-07-07 16:20:19 -0500

    system-config-printer: move entry out of toplevel, use finalAttrs, strictDeps, and structuredAttrs

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2927,9 +2927,7 @@ with pkgs;
 
   subzerod = with python3Packages; toPythonApplication subzerod;
 
-  system-config-printer = callPackage ../tools/misc/system-config-printer {
-    libxml2 = libxml2Python;
-  };
+  system-config-printer = callPackage ../tools/misc/system-config-printer { };
 
   tartube-yt-dlp = tartube.override {
     youtube-dl = yt-dlp;
