5a539604c668ad6b4924fae94fd7502a72a5f287
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-08 07:50:38 +0000

    bfs: 4.1 -> 4.1.4

--- pkgs/by-name/bf/bfs/package.nix
+++ pkgs/by-name/bf/bfs/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bfs";
-  version = "4.1";
+  version = "4.1.4";
 
   src = fetchFromGitHub {
     repo = "bfs";
     owner = "tavianator";
     tag = finalAttrs.version;
-    hash = "sha256-+hGxdsk9MU5MVvvx3C2cqomboNxD0UZ5y7t84fAwfqs=";
+    hash = "sha256-gENWuxZh4dOaKUyrb53dn/ai7F2L3gyYj8kuha0JaLo=";
   };
 
   buildInputs = [
