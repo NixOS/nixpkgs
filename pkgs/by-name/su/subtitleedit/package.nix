3eb8315849946764f19395f9a161210622d0759e
pancaek <20342389+pancaek@users.noreply.github.com>
2026-07-07 18:46:43 -0700

    subtitleedit: [WIP] try to build from source

--- pkgs/by-name/su/subtitleedit/package.nix
+++ pkgs/by-name/su/subtitleedit/package.nix
@@ -1,82 +1,73 @@
 {
   lib,
-  stdenv,
-  fetchzip,
+  buildDotnetModule,
+  fetchFromGitHub,
   makeDesktopItem,
   nix-update-script,
 
   copyDesktopItems,
-  icoutils,
   makeWrapper,
-
   ffmpeg,
-  gtk2,
   hunspell,
-  mono,
   mpv,
   tesseract4,
+  dotnetCorePackages,
+  libxcursor,
+  libGL,
+
 }:
 
-stdenv.mkDerivation rec {
+buildDotnetModule (finalAttrs: {
   pname = "subtitleedit";
-  version = "4.0.16";
+  version = "5.0.0";
 
-  src = fetchzip {
-    url = "https://github.com/SubtitleEdit/subtitleedit/releases/download/${version}/SE${
-      lib.replaceStrings [ "." ] [ "" ] version
-    }.zip";
-    hash = "sha256-SXM5aMBNXI/ClrhvoXDeo86KUntU2Ad3fxx8O3dr+j0=";
-    stripRoot = false;
+  src = fetchFromGitHub {
+    owner = "SubtitleEdit";
+    repo = "subtitleedit";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-07dEThNWkAvxFoojDVfGGHqfL/EnM0xqjfjaKhPq6nU=";
   };
 
   nativeBuildInputs = [
     copyDesktopItems
-    icoutils
     makeWrapper
   ];
 
-  runtimeLibs = lib.makeLibraryPath [
-    gtk2
+  dotnet-sdk = dotnetCorePackages.sdk_10_0;
+  dotnet-runtime = dotnetCorePackages.runtime_10_0;
+  nugetDeps = ./deps.json;
+  runtimeDeps = [
     hunspell
     mpv
     tesseract4
+    libGL
+    libxcursor
   ];
 
-  runtimeBins = lib.makeBinPath [
-    ffmpeg
-    hunspell
-    tesseract4
-  ];
+  projectFile = "src/ui/UI.csproj";
+  enableParallelBuilding = false;
+  executables = [ "SubtitleEdit" ];
 
-  installPhase = ''
-    runHook preInstall
+  preFixup = ''
+    install -D src/libse/Icon.png $out/share/icons/hicolor/256x256/apps/subtitleedit.png
 
-    mkdir -p $out/bin
-    mkdir -p $out/share/icons/hicolor/{16x16,32x32,48x48,256x256}/apps
-
-    cp -r * $out/bin/
-    ln -s ${hunspell.out}/lib/libhunspell*.so $out/bin/libhunspell.so
-    makeWrapper "${mono}/bin/mono" $out/bin/subtitleedit \
-      --add-flags "$out/bin/SubtitleEdit.exe" \
-      --prefix LD_LIBRARY_PATH : ${runtimeLibs} \
-      --prefix PATH : ${runtimeBins}
-
-    wrestool -x -t 14 SubtitleEdit.exe > subtitleedit.ico
-    icotool -x -i 3 -o $out/share/icons/hicolor/16x16/apps/subtitleedit.png subtitleedit.ico
-    icotool -x -i 6 -o $out/share/icons/hicolor/32x32/apps/subtitleedit.png subtitleedit.ico
-    icotool -x -i 9 -o $out/share/icons/hicolor/48x48/apps/subtitleedit.png subtitleedit.ico
-    icotool -x -i 10 -o $out/share/icons/hicolor/256x256/apps/subtitleedit.png subtitleedit.ico
-
-    runHook postInstall
+    wrapProgram $out/bin/SubtitleEdit \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          ffmpeg
+          hunspell
+          tesseract4
+        ]
+      }
   '';
 
   desktopItems = [
     (makeDesktopItem {
-      name = pname;
+      name = "subtitleedit";
       desktopName = "Subtitle Edit";
       exec = "subtitleedit";
       icon = "subtitleedit";
-      comment = meta.description;
+      comment = "Subtitle editor";
       categories = [ "AudioVideo" ];
     })
   ];
@@ -97,4 +88,4 @@ stdenv.mkDerivation rec {
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     maintainers = [ ];
   };
-}
+})
