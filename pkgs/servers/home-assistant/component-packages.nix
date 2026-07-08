f65cd64e41e64b312745aaa2f1fb2d973d259303
Jamie Magee <jamie.magee@gmail.com>
2026-07-08 14:07:45 -0700

    home-assistant: update component packages

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -416,8 +416,9 @@
     "arwn" =
       ps: with ps; [
         aiohasupervisor
+        arwn-client
         paho-mqtt
-      ]; # missing inputs: arwn-client
+      ];
     "aseko_pool_live" =
       ps: with ps; [
         aioaseko
@@ -7845,6 +7846,7 @@
     "aranet"
     "arcam_fmj"
     "arve"
+    "arwn"
     "aseko_pool_live"
     "assist_pipeline"
     "assist_satellite"
