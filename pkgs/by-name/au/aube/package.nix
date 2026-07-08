d278f3dd4db11e30dd102d42b7753056d576c396
Oleksii Filonenko <github@brightone.cloud>
2026-07-08 18:48:08 +0100

    aube: 1.9.1 -> 1.26.0

--- pkgs/by-name/au/aube/package.nix
+++ pkgs/by-name/au/aube/package.nix
@@ -10,16 +10,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "aube";
-  version = "1.9.1";
+  version = "1.26.0";
 
   src = fetchFromGitHub {
     owner = "jdx";
     repo = "aube";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-uwOEou6DH+bePNupYKmTc82xQV9T08bDmSPG9RU9yBk=";
+    hash = "sha256-bQDDLgO5dG9kMF9VDnHGwuMZjWrbNT5Ia90rJrERDaE=";
   };
 
-  cargoHash = "sha256-CBI44O2iMwdMym+ZOO9MvJQ73n+12J6FjzIXAOQTGT0=";
+  cargoHash = "sha256-L9UiSO9UL8kBOebFXrZqbIJ/V4tobl1NYAdlktmX2lY=";
 
   nativeBuildInputs = [ cmake ]; # libz-ng-sys
 
@@ -41,9 +41,12 @@ rustPlatform.buildRustPackage (finalAttrs: {
   meta = {
     description = "Fast Node.js package manager";
     homepage = "https://github.com/jdx/aube";
-    changelog = "https://github.com/jdx/aube/blob/v${finalAttrs.version}/CHANGELOG.md";
+    changelog = "https://github.com/jdx/aube/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ chillcicada ];
+    maintainers = with lib.maintainers; [
+      chillcicada
+      Br1ght0ne
+    ];
     mainProgram = "aube";
   };
 })
