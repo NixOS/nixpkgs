c774789fb2b9c0cb4e408f796f136dc9ccd07da3
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-08 09:13:03 +0200

    python3Packages.std-uritemplate: migrate to finalAttrs

--- pkgs/development/python-modules/std-uritemplate/default.nix
+++ pkgs/development/python-modules/std-uritemplate/default.nix
@@ -5,14 +5,14 @@
   poetry-core,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "std-uritemplate";
   version = "2.0.11";
   pyproject = true;
 
   src = fetchPypi {
     pname = "std_uritemplate";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-afqeUkc41RG7S5Sz4jk8oFJKrRGOVJJZxU2zZ+BdmFI=";
   };
 
@@ -26,8 +26,8 @@ buildPythonPackage rec {
   meta = {
     description = "Std-uritemplate implementation for Python";
     homepage = "https://github.com/std-uritemplate/std-uritemplate";
-    changelog = "https://github.com/std-uritemplate/std-uritemplate/releases/tag/${version}";
+    changelog = "https://github.com/std-uritemplate/std-uritemplate/releases/tag/${finalAttrs.version}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
