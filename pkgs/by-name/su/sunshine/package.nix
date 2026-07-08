fb6e97a15d0e5e4f981e918d150e1761e53d508f
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-07-08 01:32:47 -0400

    sunshine: remove cudaPackages.cudatoolkit
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>
    (cherry picked from commit 1423ce6c16ce9c4b12998956a3d4584736c5b126)

--- pkgs/by-name/su/sunshine/package.nix
+++ pkgs/by-name/su/sunshine/package.nix
@@ -228,7 +228,6 @@ stdenv'.mkDerivation (finalAttrs: {
     libnotify
   ]
   ++ lib.optionals cudaSupport [
-    cudaPackages.cudatoolkit
     cudaPackages.cuda_cudart
   ]
   ++ lib.optionals isDarwin [
