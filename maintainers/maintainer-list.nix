8752d836ffa19096b4baa52f27d5eb938ed2530a
Johannes Jeising <jeising@magazino.eu>
2026-07-09 12:28:54 +0200

    maintainers: add jsqu4re

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13757,6 +13757,12 @@
     name = "John Soo";
     githubId = 10039785;
   };
+  jsqu4re = {
+    email = "johannes.jeising@gmail.com";
+    github = "jsqu4re";
+    githubId = 35706792;
+    name = "Johannes Jeising";
+  };
   jsusk = {
     email = "joshua@suskalo.org";
     github = "IGJoshua";
