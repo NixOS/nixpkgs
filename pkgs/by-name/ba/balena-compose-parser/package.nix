ee3d936b51e926a6a498de91df4cc15720947f64
Willy Hille <willy.hille@intenta.de>
2026-07-09 16:11:29 +0200

    balena-compose-parser: init at 0.2.6

---
+++ pkgs/by-name/ba/balena-compose-parser/package.nix
@@ -0,0 +1,31 @@
+{
+  buildGoModule,
+  fetchFromGitHub,
+  lib,
+}:
+buildGoModule rec {
+  __structuredAttrs = true;
+  pname = "balena-compose-parser";
+  version = "0.2.6";
+
+  src = fetchFromGitHub {
+    owner = "balena-io-modules";
+    repo = "balena-compose-parser";
+    rev = "v${version}";
+    hash = "sha256-Bpcj3a689CjXmcjccyBzg30bj4+YT2FCvDvnpIy+EdY=";
+  };
+
+  modRoot = "lib";
+  vendorHash = "sha256-t3snmAQe+rbclrvNjmFl9O9KAc5OFU9zH3r+1+B+JI4=";
+
+  meta = {
+    description = "compose-go wrapper for parsing balena-compatible docker-compose.yml files ";
+    homepage = "https://github.com/balena-io-modules/balena-compose-parser";
+    changelog = "https://github.com/balena-io-modules/balena-compose-parser/blob/v${version}/CHANGELOG.md";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
+      kalebpace
+    ];
+    mainProgram = "balena-compose-parser";
+  };
+}
