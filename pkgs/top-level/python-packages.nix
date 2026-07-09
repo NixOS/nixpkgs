bbad0eca6e3c3589e00849978ec0eae23188f06e
Jamie Magee <jamie.magee@gmail.com>
2026-07-09 08:24:56 -0700

    python3Packages.energieleser: init at 0.1.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5434,6 +5434,8 @@ self: super: with self; {
 
   encutils = callPackage ../development/python-modules/encutils { };
 
+  energieleser = callPackage ../development/python-modules/energieleser { };
+
   energyflip-client = callPackage ../development/python-modules/energyflip-client { };
 
   energyflow = callPackage ../development/python-modules/energyflow { };
