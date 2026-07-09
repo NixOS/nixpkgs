c59f12bb999ff51c84b1daa0825b2eb89a2f6c1b
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-07-09 13:53:01 +0200

    rPackages.SuperGauss: fix dependencies

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -659,6 +659,7 @@ let
     ];
     rsbml = [ pkgs.pkg-config ];
     rvg = [ pkgs.libpng.dev ];
+    SuperGauss = [ pkgs.pkg-config ];
     MAGEE = [
       pkgs.zlib.dev
       pkgs.bzip2.dev
@@ -1231,10 +1232,7 @@ let
       netcdf
     ];
     rsbml = [ pkgs.libsbml ];
-    SuperGauss = [
-      pkgs.pkg-config
-      pkgs.fftw.dev
-    ];
+    SuperGauss = [ pkgs.fftw ];
     ravetools = with pkgs; [
       pkg-config
       fftw.dev
