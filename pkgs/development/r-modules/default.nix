708563e3423b838d304ba7ea4519cc23bf9adc30
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-07-09 14:06:34 +0200

    rPackages.units: fix dependencies

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -951,7 +951,6 @@ let
       udunits
       expat
     ];
-    units = [ pkgs.udunits ];
     unigd = [ pkgs.pkg-config ];
     unsum = with pkgs; [
       cargo
@@ -1191,6 +1190,7 @@ let
     ];
     Rpoppler = [ pkgs.poppler ];
     RPostgres = with pkgs; [ libpq ];
+    units = [ pkgs.udunits ];
     XML = with pkgs; [
       libtool
       libxml2
