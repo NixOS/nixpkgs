d03dba9c80ffe42b5d716d0f4d18f9b3cdbc8236
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-09 09:46:47 +0000

    croc: 10.4.6 -> 10.4.12

--- pkgs/by-name/cr/croc/package.nix
+++ pkgs/by-name/cr/croc/package.nix
@@ -11,13 +11,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "croc";
-  version = "10.4.6";
+  version = "10.4.12";
 
   src = fetchFromGitHub {
     owner = "schollz";
     repo = "croc";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-+KG1PHUymeoAj92UAn/sitQF6xC1xwl+cdisxy2ZtPs=";
+    hash = "sha256-ThMwPbQcxRBVvL/jxTluBc4gKbEIl1Wm69c/3r976s0=";
   };
 
   vendorHash = "sha256-rwGunSDIgetBsk97LxQz0WHpzMDMMESHC1OhBWRuVjI=";
