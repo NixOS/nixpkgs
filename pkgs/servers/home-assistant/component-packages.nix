9a70fe1f1281cd070b0cde9d1e6941487b53d814
Jamie Magee <jamie.magee@gmail.com>
2026-07-09 08:26:20 -0700

    home-assistant: update component packages

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -1690,7 +1690,8 @@
       ];
     "energieleser" =
       ps: with ps; [
-      ]; # missing inputs: energieleser
+        energieleser
+      ];
     "energy" =
       ps: with ps; [
         fnv-hash-fast
@@ -8022,6 +8023,7 @@
     "emulated_kasa"
     "emulated_roku"
     "energenie_power_sockets"
+    "energieleser"
     "energy"
     "energyid"
     "energyzero"
