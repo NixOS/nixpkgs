27ff9bb305a9ecb37db114a9607243bb7c9f376e
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-09 12:07:50 +0000

    poethepoet: 0.46.0 -> 0.48.0

--- pkgs/by-name/po/poethepoet/package.nix
+++ pkgs/by-name/po/poethepoet/package.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "poethepoet";
-  version = "0.46.0";
+  version = "0.48.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "nat-n";
     repo = "poethepoet";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-K2ARb70vTEYdnNOKtUES6n5FPapdq6BFMVg25dTb12U=";
+    hash = "sha256-vxbdNzjPp7Jhfd2XDfjMYQ8iDiaIn6DfL/mCOLdjRtA=";
   };
 
   nativeBuildInputs = [
