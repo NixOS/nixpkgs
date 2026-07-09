4be860560b3bdbf06967d2158b1a41aa7e79dfd6
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-07-09 12:26:24 +0200

    rPackages.qpdf: fix dependencies

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -707,7 +707,7 @@ let
       pkgs.pkg-config
       pkgs.clp
     ];
-    pdftools = [ pkgs.poppler.dev ];
+    pdftools = [ pkgs.pkg-config ];
     PEPBVS = [ pkgs.gsl ];
     phytools = [ pkgs.which ];
     PKI = [ pkgs.openssl.dev ];
@@ -1294,7 +1294,7 @@ let
       protobuf
       zlib.dev
     ];
-    pdftools = [ pkgs.pkg-config ];
+    pdftools = [ pkgs.poppler ];
     qckitfastq = [ pkgs.zlib.dev ];
     raer = with pkgs; [
       zlib.dev
