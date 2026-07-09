4484ce1dd201d63ea10a4735aba0d81e2415237a
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-07-09 12:21:16 -0400

    supertuxkart: move assets and bundled libs into passthru
    personal preference, like to avoid the let block to keep it all into
    finalattrs for overriding.

--- pkgs/by-name/su/supertuxkart/package.nix
+++ pkgs/by-name/su/supertuxkart/package.nix
@@ -24,48 +24,11 @@
   sqlite,
   libsamplerate,
   shaderc,
+  nix-update-script,
   serverOnly ? false,
   nameSuffix ? "",
 }:
-let
-  assets = fetchsvn {
-    url = "https://svn.code.sf.net/p/supertuxkart/code/stk-assets";
-    rev = "18621";
-    sha256 = "sha256-iqQSezGu0tecA53qhrtYA77SLj28WUUCcL4ZCJbK5C8=";
-    name = "stk-assets";
-  };
 
-  # List of bundled libraries in stk-code/lib to keep
-  # Those are the libraries that cannot be replaced
-  # with system packages.
-  bundledLibraries = [
-    # Bullet 2.87 is incompatible (bullet 2.79 needed whereas 2.87 is packaged)
-    # The api changed in a lot of classes, too much work to adapt
-    "bullet"
-    # Upstream Libenet doesn't yet support IPv6,
-    # So we will use the bundled libenet which
-    # has been fixed to support it.
-    "enet"
-    # Internal library of STK, nothing to do about it
-    "graphics_engine"
-    # Internal library of STK, nothing to do about it
-    "graphics_utils"
-    # Internal library.
-    "simd_wrapper"
-    # This irrlicht is bundled with cmake
-    # whereas upstream irrlicht still uses
-    # archaic Makefiles, too complicated to switch to.
-    "irrlicht"
-    # Not packaged to this date
-    "libsquish"
-    # Not packaged to this date
-    "sheenbidi"
-    # Not packaged to this date
-    "tinygettext"
-    # Not packaged to this date (needed on Darwin)
-    "mojoal"
-  ];
-in
 stdenv.mkDerivation (finalAttrs: {
 
   pname = "supertuxkart${nameSuffix}";
@@ -81,7 +44,7 @@ stdenv.mkDerivation (finalAttrs: {
   postPatch = ''
     # Deletes all bundled libs in stk-code/lib except those
     # That couldn't be replaced with system packages
-    find lib -maxdepth 1 -type d | egrep -v "^lib$|${(lib.concatStringsSep "|" bundledLibraries)}" | xargs -n1 -L1 -r -I{} rm -rf {}
+    find lib -maxdepth 1 -type d | egrep -v "^lib$|${(lib.concatStringsSep "|" finalAttrs.passthru.bundledLibraries)}" | xargs -n1 -L1 -r -I{} rm -rf {}
 
     # Allow building with system-installed wiiuse on Darwin
     substituteInPlace CMakeLists.txt \
@@ -152,7 +115,7 @@ stdenv.mkDerivation (finalAttrs: {
   # Obtain the assets directly from the fetched store path, to avoid duplicating assets across multiple engine builds
   preFixup = ''
     wrapProgram $out/bin/supertuxkart \
-      --set-default SUPERTUXKART_ASSETS_DIR "${assets}" \
+      --set-default SUPERTUXKART_ASSETS_DIR "${finalAttrs.passthru.assets}" \
       --set-default SUPERTUXKART_DATADIR "$out/share/supertuxkart"
   ''
   + lib.optionalString stdenv.hostPlatform.isDarwin ''
@@ -160,6 +123,57 @@ stdenv.mkDerivation (finalAttrs: {
     cp $out/bin/supertuxkart \
       $out/Applications/SuperTuxKart.app/Contents/MacOS/supertuxkart
   '';
+
+  passthru = {
+    updateScript = nix-update-script { };
+
+    assets = fetchsvn {
+      url = "https://svn.code.sf.net/p/supertuxkart/code/stk-assets";
+      rev = "18621";
+      sha256 = "sha256-iqQSezGu0tecA53qhrtYA77SLj28WUUCcL4ZCJbK5C8=";
+      name = "stk-assets";
+    };
+
+    # List of bundled libraries in stk-code/lib to keep
+    # Those are the libraries that cannot be replaced
+    # with system packages.
+    bundledLibraries = [
+      # Bullet 2.87 is incompatible (bullet 2.79 needed whereas 2.87 is packaged)
+      # The api changed in a lot of classes, too much work to adapt
+      "bullet"
+
+      # Upstream Libenet doesn't yet support IPv6,
+      # So we will use the bundled libenet which
+      # has been fixed to support it.
+      "enet"
+
+      # Internal library of STK, nothing to do about it
+      "graphics_engine"
+
+      # Internal library of STK, nothing to do about it
+      "graphics_utils"
+
+      # Internal library.
+      "simd_wrapper"
+
+      # This irrlicht is bundled with cmake
+      # whereas upstream irrlicht still uses
+      # archaic Makefiles, too complicated to switch to.
+      "irrlicht"
+
+      # Not packaged to this date
+      "libsquish"
+
+      # Not packaged to this date
+      "sheenbidi"
+
+      # Not packaged to this date
+      "tinygettext"
+
+      # Not packaged to this date (needed on Darwin)
+      "mojoal"
+    ];
+  };
 
   meta = {
     description = "3D open-source arcade racer";
