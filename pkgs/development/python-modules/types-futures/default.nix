089ea70ba8c3d30d455913f0aec6acb6426c4e8f
Harinn <prinn.dev@pm.me>
2026-07-10 14:44:00 +0700

    python3Packages.types-futures: migrate to pyproject

--- pkgs/development/python-modules/types-futures/default.nix
+++ pkgs/development/python-modules/types-futures/default.nix
@@ -2,17 +2,20 @@
   buildPythonPackage,
   fetchPypi,
   lib,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "types-futures";
   version = "3.3.8";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "6fe8ccc2c2af7ef2fdd9bf73eab6d617074f09f30ad7d373510b4043d39c42de";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     description = "Typing stubs for futures";
