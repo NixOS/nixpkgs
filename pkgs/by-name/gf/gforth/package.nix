3f000cb56494275a8f6632f45f87bf4836b378ee
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-10 14:13:19 +0000

    gforth: 0.7.9_20260610 -> 0.7.9_20260708

--- pkgs/by-name/gf/gforth/package.nix
+++ pkgs/by-name/gf/gforth/package.nix
@@ -17,13 +17,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "gforth";
-  version = "0.7.9_20260610";
+  version = "0.7.9_20260708";
 
   src = fetchFromGitHub {
     owner = "forthy42";
     repo = "gforth";
     rev = finalAttrs.version;
-    hash = "sha256-gaP3Mmcp0NueRfqh62XlvtWuHN6fAnMTa1uSm7Bj+Rk=";
+    hash = "sha256-3lKd50Rlhk9OlKb5ATHH4vTWlo40h3iCz+VQfuo/6ys=";
   };
 
   patches = [ ./use-nproc-instead-of-fhs.patch ];
