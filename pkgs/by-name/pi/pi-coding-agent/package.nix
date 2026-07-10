21c1bad36368bcbd3dbc8712d09825dfb49e4568
Adam C. Stephens <adam@valkor.net>
2026-07-09 22:21:20 -0400

    pi-coding-agent: 0.80.3 -> 0.80.6
    Changelog: https://github.com/earendil-works/pi/blob/main/packages/coding-agent/CHANGELOG.md

--- pkgs/by-name/pi/pi-coding-agent/package.nix
+++ pkgs/by-name/pi/pi-coding-agent/package.nix
@@ -12,16 +12,16 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "pi-coding-agent";
-  version = "0.80.3";
+  version = "0.80.6";
 
   src = fetchFromGitHub {
     owner = "earendil-works";
     repo = "pi";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-wQTrWKsb2HCGwzSAFEk8NWSDpqxSY/lv1/R6ghcmbaA=";
+    hash = "sha256-e/wcHruEcBAHDF5tKvwew7LXjVp0eraHh2k+QaL2sCA=";
   };
 
-  npmDepsHash = "sha256-geh8LH88OZybFXkR/jDeTdew6TNMdFM6jhCSYKn//dU=";
+  npmDepsHash = "sha256-xXEOR0epZcfbXayYGyJdBiFVliamBexqA+1Sd7wlGhU=";
 
   npmWorkspace = "packages/coding-agent";
 
