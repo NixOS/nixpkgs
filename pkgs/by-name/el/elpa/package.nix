359dd19dc32a15034ccd86892366b6a4486af373
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-10 12:41:19 +0000

    elpa: 2026.02.001 -> 2026.02.002

--- pkgs/by-name/el/elpa/package.nix
+++ pkgs/by-name/el/elpa/package.nix
@@ -28,13 +28,13 @@ assert blas.isILP64 == scalapack.isILP64;
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "elpa";
-  version = "2026.02.001";
+  version = "2026.02.002";
 
   passthru = { inherit (blas) isILP64; };
 
   src = fetchurl {
     url = "https://elpa.mpcdf.mpg.de/software/tarball-archive/Releases/${finalAttrs.version}/elpa-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-o3nyf029J7LuRQF6/sZW0GQwHpcVDIdGSb39ZJV7de0=";
+    sha256 = "sha256-AuPFn+xTzY62akzBX6T78ZDPllQiciP7itVXE+lCeTI=";
   };
 
   patches = [
