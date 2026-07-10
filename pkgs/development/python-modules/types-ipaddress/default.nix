101e1a43d8b3ec57a6e873db9b46fbad8d4d0b1f
Harinn <prinn.dev@pm.me>
2026-07-10 14:45:00 +0700

    python3Packages.types-ipaddress: migrate to pyproject

--- pkgs/development/python-modules/types-ipaddress/default.nix
+++ pkgs/development/python-modules/types-ipaddress/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "types-ipaddress";
   version = "1.0.8";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "0h9q9pjvw1ap5k70ygp750d096jkzymxlhx87yh0pr9mb6zg6gd0";
   };
+
+  build-system = [ setuptools ];
 
   pythonImportsCheck = [ "ipaddress-python2-stubs" ];
 
