56cdf6d9a5a299af46d789a9e4f033c24fe3a9ee
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-10 03:51:08 +0000

    vacuum-tube: 1.8.0 -> 1.8.1

--- pkgs/by-name/va/vacuum-tube/package.nix
+++ pkgs/by-name/va/vacuum-tube/package.nix
@@ -10,16 +10,16 @@
 
 buildNpmPackage rec {
   pname = "vacuum-tube";
-  version = "1.8.0";
+  version = "1.8.1";
 
   src = fetchFromGitHub {
     owner = "shy1132";
     repo = "VacuumTube";
     tag = "v${version}";
-    hash = "sha256-LVsNCf0rvgWuyKEzt5b2AcwK6JdDe3ghiSOUXg7nSaA=";
+    hash = "sha256-jLktsUza5cEPIry+LyP8lB4A0ivxdGtE5y1i6UHap30=";
   };
 
-  npmDepsHash = "sha256-hbSN5I3LG4+y0Ggkv5C1zsaLYbrFjGXiwURfc51d0Kk=";
+  npmDepsHash = "sha256-Q/E+Rc9CuaN2cN6AKKT3EHiR97sdLAAnks5DYBQK24I=";
   makeCacheWritable = true;
 
   env = {
