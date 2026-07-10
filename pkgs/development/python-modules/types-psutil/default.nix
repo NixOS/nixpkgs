c8d9369eb749f72fddb37dd69a9b6b54a8c47350
Harinn <prinn.dev@pm.me>
2026-07-10 14:44:59 +0700

    python3Packages.types-psutil: migrate to pyproject

--- pkgs/development/python-modules/types-psutil/default.nix
+++ pkgs/development/python-modules/types-psutil/default.nix
@@ -2,18 +2,21 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "types-psutil";
   version = "7.2.1.20260116";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "types_psutil";
     inherit version;
     hash = "sha256-RmG+XV16zV2K+wKpLQUWCmy7LOdHIyRbUfe6ff25+YE=";
   };
+
+  build-system = [ setuptools ];
 
   # Module doesn't have tests
   doCheck = false;
