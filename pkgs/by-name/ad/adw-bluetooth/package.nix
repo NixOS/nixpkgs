6e2e4934077ee77e10c4d40a217c3f764a0c46a9
ezratweaver <ezratweaver@gmail.com>
2026-07-10 01:14:51 -0400

    adw-bluetooth: 1.1.0 -> 1.1.2

--- pkgs/by-name/ad/adw-bluetooth/package.nix
+++ pkgs/by-name/ad/adw-bluetooth/package.nix
@@ -15,13 +15,13 @@
 }:
 
 let
-  version = "1.1.0";
+  version = "1.1.2";
 
   src = fetchFromGitHub {
     owner = "ezratweaver";
     repo = "adw-bluetooth";
     tag = version;
-    hash = "sha256-h3cHtecwBsx3j33qXVn/zaq4FZext71P7flzunCHqHg=";
+    hash = "sha256-0rySzx03KeKeqtl0yrbnj/tVbpVPBAKDz+1qLQ5kZRc=";
   };
 
   daemon = buildGoModule {
