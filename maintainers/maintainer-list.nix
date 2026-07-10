7b20113f4d87b216e4537d908e99959b7644b92a
Mikilio <official.mikilio@gmail.com>
2026-07-10 23:11:48 +0200

    maintainers: add mikilio

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18777,6 +18777,12 @@
     githubId = 1387206;
     name = "Mike Sperber";
   };
+  mikilio = {
+    email = "kilian.mio@mikilio.com";
+    github = "Mikilio";
+    githubId = 86004375;
+    name = "Kilian Mio";
+  };
   mikoim = {
     email = "ek@esh.ink";
     github = "mikoim";
