384a342a1565ab8300ad2a298db46861c59330a5
Sefa Eyeoglu <contact@scrumplex.net>
2026-07-10 19:01:02 +0200

    pnpm: 11.10.0 -> 11.11.0
    https://github.com/pnpm/pnpm/releases/tag/v11.11.0
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    (cherry picked from commit dd85f021f5042e5e19c229b8503fa93cd983bb61)

--- pkgs/development/tools/pnpm/default.nix
+++ pkgs/development/tools/pnpm/default.nix
@@ -53,8 +53,8 @@ let
       hash = "sha256-WOFDJYhx31FYm2UcBiBdq+xIdmpdu6PCWZm2m1C+WY4=";
     };
     "11" = {
-      version = "11.10.0";
-      hash = "sha256-YgtmBepPYvxWptCphzP0eQcdAyHgPkhrUix+mnRhdDE=";
+      version = "11.11.0";
+      hash = "sha256-he8u/yFqGukIBMAMjfv6ZoU1NkRlDRCQaok8Ba7c2IQ=";
     };
   };
 
