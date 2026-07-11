387aa88f9d0c112c3f1201748d704e9a8ae8a77e
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-11 19:28:21 +0000

    sabiql: 1.13.0 -> 1.14.0

--- pkgs/by-name/sa/sabiql/package.nix
+++ pkgs/by-name/sa/sabiql/package.nix
@@ -13,16 +13,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sabiql";
-  version = "1.13.0";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "riii111";
     repo = "sabiql";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-HDaiCLu1L2aQ+9swQWWPzb9MWqI44ECf71KyhI43emk=";
+    hash = "sha256-Fm9AINbCoazT2OHPJUO7YHpLt1KpQ8jDfXAkX3Karl0=";
   };
 
-  cargoHash = "sha256-5o68x2pOe4ArDzPJmGI9ooqRgs8rEReSFnpT8TKItl4=";
+  cargoHash = "sha256-915W5zpavggfPN7artvgxkErWsx9eZ6953RX/eLQagg=";
 
   # Upstream use latest rust version need to patch use nixpkgs version
   postPatch = ''
