25fb57e937e54849721d8bc3b8b89cfa08346078
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2026-07-11 19:52:32 -0400

    sage: disable ntl threading on all platforms
    Configure ntl dependency with NTL_THREADS=off, following how official sage builds configure it:
    
    https://github.com/sagemath/sage/blob/80e56eaeb7089cfc5e4e0cc495ba8da43ee0a573/build/pkgs/ntl/spkg-install.in#L102

--- pkgs/by-name/sa/sage/package.nix
+++ pkgs/by-name/sa/sage/package.nix
@@ -12,12 +12,19 @@
 let
   inherit (pkgs) symlinkJoin callPackage mathjax;
 
+  ntl = pkgs.ntl.overrideAttrs (old: {
+    configureFlags = (old.configureFlags or [ ]) ++ [
+      "NTL_THREADS=off"
+    ];
+  });
+
   python3 = pkgs.python3 // {
     pkgs = pkgs.python3.pkgs.overrideScope (
       self: super: {
         # `sagelib`, i.e. all of sage except some wrappers and runtime dependencies
         sagelib = self.callPackage ./sagelib.nix {
           inherit flint;
+          inherit ntl;
           inherit sage-src env-locations singular;
           inherit (maxima) lisp-compiler;
           linbox = pkgs.linbox;
@@ -80,6 +87,7 @@ let
       singular
       palp
       flint
+      ntl
       pythonEnv
       maxima
       ;
@@ -94,6 +102,7 @@ let
   # sagelib with added wrappers and a dependency on sage-tests to make sure thet tests were run.
   sage-with-env = callPackage ./sage-with-env.nix {
     inherit python3 pythonEnv;
+    inherit ntl;
     inherit sage-env;
     inherit singular maxima;
     inherit three;
