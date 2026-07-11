ec4624a491adccf42a12b1e26c5b5b6f6cb9b925
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-11 08:54:36 +0200

    python3Packages.aio-geojson-generic-client: migrate to finalAttrs

--- pkgs/development/python-modules/aio-geojson-generic-client/default.nix
+++ pkgs/development/python-modules/aio-geojson-generic-client/default.nix
@@ -13,7 +13,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aio-geojson-generic-client";
   version = "2026.6.0";
   pyproject = true;
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "exxamalte";
     repo = "python-aio-geojson-generic-client";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-ZRPagyzFAa7f6liT1hWVf6FtabxPKfOzMS/Id14Jpv0=";
   };
 
@@ -48,8 +48,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python library for accessing GeoJSON feeds";
     homepage = "https://github.com/exxamalte/python-aio-geojson-generic-client";
-    changelog = "https://github.com/exxamalte/python-aio-geojson-generic-client/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/exxamalte/python-aio-geojson-generic-client/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
