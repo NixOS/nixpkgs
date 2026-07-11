742592884856865f00138491846f8d18bda0ddf2
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-11 08:59:33 +0200

    python3Packages.aio-georss-client: migrate to finalAttrs

--- pkgs/development/python-modules/aio-georss-client/default.nix
+++ pkgs/development/python-modules/aio-georss-client/default.nix
@@ -15,7 +15,7 @@
   xmltodict,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aio-georss-client";
   version = "2026.6.0";
   pyproject = true;
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "exxamalte";
     repo = "python-aio-georss-client";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-HstN/X3fJHJHLtduMyeUCK8epqY6yfXi8LlF75mn8+g=";
   };
 
@@ -56,4 +56,4 @@ buildPythonPackage rec {
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
