e5893d79fe94f0c57c6c02e687be95efcc5150ff
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-11 10:45:41 +0000

    wasmedge: 0.16.3 -> 0.17.1

--- pkgs/by-name/wa/wasmedge/package.nix
+++ pkgs/by-name/wa/wasmedge/package.nix
@@ -24,13 +24,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "wasmedge";
-  version = "0.16.3";
+  version = "0.17.1";
 
   src = fetchFromGitHub {
     owner = "WasmEdge";
     repo = "WasmEdge";
     rev = finalAttrs.version;
-    sha256 = "sha256-Z6SnTKLW1nBa9gCSDO3d+CmwfWpGRAb2D9ZCoqqqMjk=";
+    sha256 = "sha256-+Z9mhIoRc8iJoCPafbkX3lB9nQAysKB5gjLmq8AUk/I=";
   };
 
   nativeBuildInputs = [
