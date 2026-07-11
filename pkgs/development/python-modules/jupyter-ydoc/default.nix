a733d00acc1594e12786daba0c1078e8ac2bb98f
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-11 19:17:45 +0000

    python3Packages.jupyter-ydoc: 3.4.1 -> 4.1.1

--- pkgs/development/python-modules/jupyter-ydoc/default.nix
+++ pkgs/development/python-modules/jupyter-ydoc/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "jupyter-ydoc";
-  version = "3.4.1";
+  version = "4.1.1";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -27,7 +27,7 @@ buildPythonPackage (finalAttrs: {
     owner = "jupyter-server";
     repo = "jupyter_ydoc";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-HlYSPlYiHyVwJhsRY10SgotKa9ejlj0hlxbS+chtaBI=";
+    hash = "sha256-8hGe8a4kA8fAqsR9hN5NK7BKhtw0HnsN14uWcc1239M=";
   };
 
   build-system = [
