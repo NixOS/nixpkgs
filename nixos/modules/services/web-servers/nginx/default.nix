323b76b8230825bcd74dae526a3cea72d6c46fe1
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-07-12 01:33:42 +0200

    Revert "nixos/nginx: disable MemoryDenyWriteExecute when lua with luajit is enabled"
    This reverts commit 04b527753e405d48b62e168488f373d19723e88d.

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -1617,7 +1617,6 @@ in
           MemoryDenyWriteExecute =
             !(
               (builtins.any (mod: (mod.allowMemoryWriteExecute or false)) cfg.package.modules)
-              || cfg.lua.enable
               || (cfg.package == pkgs.openresty)
             );
           RestrictRealtime = true;
