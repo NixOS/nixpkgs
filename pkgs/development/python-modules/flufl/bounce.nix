5a9a5ab1b6537f53271ea0f3a596c6c70e6c7973
Robert Schütz <nix@dotlambda.de>
2026-07-10 19:45:50 -0700

    python3Packages.flufl-bounce: 4.0 -> 5.0.1
    Diff: https://gitlab.com/flufl/flufl.bounce/-/compare/4.0...5.0.1
    
    Changelog: https://gitlab.com/warsaw/flufl.bounce/-/blob/5.0.1/docs/NEWS.rst

--- pkgs/development/python-modules/flufl/bounce.nix
+++ pkgs/development/python-modules/flufl/bounce.nix
@@ -1,41 +1,35 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
-  fetchpatch2,
-  setuptools,
+  fetchFromGitLab,
+  hatchling,
   atpublic,
-  zope-interface,
   pytestCheckHook,
+  sybil,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "flufl-bounce";
-  version = "4.0";
+  version = "5.0.1";
   pyproject = true;
 
-  src = fetchPypi {
-    pname = "flufl.bounce";
-    inherit version;
-    hash = "sha256-JVBK65duwP5aGc1sQTo0EMtRT9zb3Kn5tdjTQ6hgODE=";
+  src = fetchFromGitLab {
+    owner = "flufl";
+    repo = "flufl.bounce";
+    tag = finalAttrs.version;
+    hash = "sha256-NWDh8vqHCAjhQYyOUkURuFhDa2xdtEukppCBCjhxhfg=";
   };
 
-  patches = [
-    (fetchpatch2 {
-      # Replace deprecated failIf with assertFalse for Python 3.12 compatibility.
-      url = "https://gitlab.com/warsaw/flufl.bounce/-/commit/e0b9fd0f24572e024a8d0484a3c9fb4542337d18.patch";
-      hash = "sha256-HJHEbRVjiiP5Z7W0sQCj6elUMyaWOTqQw6UpYOYCVZM=";
-    })
-  ];
+  build-system = [ hatchling ];
 
-  nativeBuildInputs = [ setuptools ];
-
-  propagatedBuildInputs = [
+  dependencies = [
     atpublic
-    zope-interface
   ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  nativeCheckInputs = [
+    pytestCheckHook
+    sybil
+  ];
 
   pythonImportsCheck = [ "flufl.bounce" ];
 
@@ -44,8 +38,8 @@ buildPythonPackage rec {
   meta = {
     description = "Email bounce detectors";
     homepage = "https://gitlab.com/warsaw/flufl.bounce";
-    changelog = "https://gitlab.com/warsaw/flufl.bounce/-/blob/${version}/flufl/bounce/NEWS.rst";
+    changelog = "https://gitlab.com/warsaw/flufl.bounce/-/blob/${finalAttrs.src.tag}/docs/NEWS.rst";
     maintainers = [ ];
     license = lib.licenses.asl20;
   };
-}
+})
