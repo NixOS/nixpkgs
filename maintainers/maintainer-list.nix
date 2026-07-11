5d7a5c6af56a5f29ec7b3414bc4ceb63b484ac12
4evy <git@evy.pink>
2026-07-11 17:05:09 +0300

    maintainer-list: fix `github` & `name` for `_4evy`

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -257,9 +257,9 @@
     name = "Eske Nielsen";
   };
   _4evy = {
-    name = "_4evy";
+    name = "4evy";
     email = "git@ps1.sh";
-    github = "_4evy";
+    github = "4evy";
     githubId = 57304299;
     matrix = "@donteatoreo:matrix.org";
   };
