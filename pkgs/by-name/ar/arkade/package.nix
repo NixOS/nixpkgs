03bdb82e7d1b4f08114265bfbda811183b4530a6
Holiu <165534185+Holiu618@users.noreply.github.com>
2026-07-11 19:32:54 +0800

    arkade: 0.11.106 -> 0.11.113

--- pkgs/by-name/ar/arkade/package.nix
+++ pkgs/by-name/ar/arkade/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "arkade";
-  version = "0.11.106";
+  version = "0.11.113";
 
   src = fetchFromGitHub {
     owner = "alexellis";
     repo = "arkade";
     tag = finalAttrs.version;
-    hash = "sha256-D6Vq263mo77CSTOwLLPqBbM31bb+d0I8DiuQElx1z70=";
+    hash = "sha256-8T7gYaT52L4Xnbuxvi9GayQ1qfI5U2cphSIkRGqx5Go=";
   };
 
   env.CGO_ENABLED = 0;
