3b4d9199edfa503509060e2529128f198b8f0197
Robert Schütz <nix@dotlambda.de>
2026-07-12 09:59:26 -0700

    python3Packages.trove-classifiers: 2026.5.20.19 -> 2026.6.1.19
    Changelog: https://github.com/pypa/trove-classifiers/releases/tag/2026.6.1.19

--- pkgs/development/python-modules/trove-classifiers/default.nix
+++ pkgs/development/python-modules/trove-classifiers/default.nix
@@ -10,13 +10,13 @@
 let
   self = buildPythonPackage rec {
     pname = "trove-classifiers";
-    version = "2026.5.20.19";
+    version = "2026.6.1.19";
     pyproject = true;
 
     src = fetchPypi {
       pname = "trove_classifiers";
       inherit version;
-      hash = "sha256-bmEZk5h8qTJpaK1wRScz2t0xRxWZ05iWBFsolwqbuB4=";
+      hash = "sha256-xRMrS2GoKdEc+9LXLpfyCkXtbtuV5Fxe/eteAINrJ0U=";
     };
 
     postPatch = ''
