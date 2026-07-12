ecd8f32bcd8010c9ad7f79111200befa81b45d7a
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-12 02:57:37 +0200

    python3Packages.flask: 3.1.2 -> 3.1.3
    https://flask.palletsprojects.com/en/stable/changes/#version-3-1-3

--- pkgs/development/python-modules/flask/default.nix
+++ pkgs/development/python-modules/flask/default.nix
@@ -29,13 +29,18 @@
 
 buildPythonPackage rec {
   pname = "flask";
-  version = "3.1.2";
+  version = "3.1.3";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-v2VsFcgBkO1iitCM39Oqo1vrCHhV4vSUkQqjd0zE/Yc=";
+    hash = "sha256-DvDlK4qc2TKFU3kZfdj5QEezWcoKeGlRRDBMtF+Hyes=";
   };
+
+  patches = [
+    # https://github.com/pallets/flask/issues/6071
+    ./pytest-9.1-compat.patch
+  ];
 
   build-system = [ flit-core ];
 
