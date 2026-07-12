d3b1e8c674014d3bda029469b093c07e100338e5
Emily <vcs@emily.moe>
2026-07-12 17:15:52 +0100

    ripcord: clean up after `x86_64-darwin` drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9321,11 +9321,7 @@ with pkgs;
 
   ringboard-wayland = callPackage ../by-name/ri/ringboard/package.nix { displayServer = "wayland"; };
 
-  ripcord =
-    if stdenv.hostPlatform.isLinux then
-      qt5.callPackage ../applications/networking/instant-messengers/ripcord { }
-    else
-      callPackage ../applications/networking/instant-messengers/ripcord/darwin.nix { };
+  ripcord = qt5.callPackage ../applications/networking/instant-messengers/ripcord { };
 
   inherit (callPackage ../applications/networking/cluster/rke2 { })
     rke2_1_33
