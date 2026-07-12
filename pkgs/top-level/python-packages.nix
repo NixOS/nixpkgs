a1ed61b57cf277d50af0bf74f128e1dddab0041a
Bruno BELANYI <bruno@belanyi.fr>
2026-07-12 20:31:14 +0100

    {python3Packages.,}autobean-format: init at 0.1.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1603,6 +1603,8 @@ self: super: with self; {
 
   autobean = callPackage ../development/python-modules/autobean { };
 
+  autobean-format = callPackage ../development/python-modules/autobean-format { };
+
   autobean-refactor = callPackage ../development/python-modules/autobean-refactor { };
 
   autocommand = callPackage ../development/python-modules/autocommand { };
