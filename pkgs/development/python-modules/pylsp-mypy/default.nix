2bfb5dc0decfa9c00ccceaa341477cf3af544383
Andrew Marshall <andrew@johnandrewmarshall.com>
2026-07-12 13:12:16 -0400

    python3Packages.pylsp-mypy: switch to PyPI for newest versions
    See https://github.com/python-lsp/pylsp-mypy/issues/113 for context

--- pkgs/development/python-modules/pylsp-mypy/default.nix
+++ pkgs/development/python-modules/pylsp-mypy/default.nix
@@ -1,7 +1,7 @@
 {
   lib,
   buildPythonPackage,
-  fetchFromGitHub,
+  fetchPypi,
 
   # build-system
   setuptools,
@@ -17,11 +17,10 @@ buildPythonPackage (finalAttrs: {
   version = "0.7.0";
   pyproject = true;
 
-  src = fetchFromGitHub {
-    owner = "python-lsp";
-    repo = "pylsp-mypy";
-    tag = finalAttrs.version;
-    hash = "sha256-rS0toZaAygNJ3oe3vfP9rKJ1A0avIdp5yjNx7oGOB4o=";
+  src = fetchPypi {
+    pname = "pylsp_mypy";
+    inherit (finalAttrs) version;
+    hash = "sha256-6U9THUzlIyIsKvdHGr45bP60zDxLGB1URi+21VPh4LM=";
   };
 
   build-system = [ setuptools ];
@@ -42,8 +41,7 @@ buildPythonPackage (finalAttrs: {
 
   meta = {
     description = "Mypy plugin for the Python LSP Server";
-    homepage = "https://github.com/python-lsp/pylsp-mypy";
-    changelog = "https://github.com/python-lsp/pylsp-mypy/releases/tag/${finalAttrs.version}";
+    homepage = "https://pypi.org/project/pylsp-mypy/";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ cpcloud ];
   };
