6cbba166f0107db29b2a17116fcb115f9ca4868a
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-13 15:54:03 +0000

    gajim: 2.4.7 -> 2.4.7.1+win

--- pkgs/applications/networking/instant-messengers/gajim/default.nix
+++ pkgs/applications/networking/instant-messengers/gajim/default.nix
@@ -38,14 +38,14 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "gajim";
-  version = "2.4.7";
+  version = "2.4.7.1+win";
 
   src = fetchFromGitLab {
     domain = "dev.gajim.org";
     owner = "gajim";
     repo = "gajim";
     tag = finalAttrs.version;
-    hash = "sha256-tZ1+DRVCzwaWeur9mwc/zE34H2xdqk96upqWfqNTl3g=";
+    hash = "sha256-/X2Xp1ZnPLTZc1Hf4Kp6R/+mezU6qoUhaT9OskYlnOY=";
   };
 
   pyproject = true;
