51fb0c374320b733df11933a09a71e643ddb1d8e
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-13 12:46:25 +0200

    python3Packages.mypy-boto3-cloudwatch: 1.43.38 -> 1.43.46

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -255,8 +255,8 @@ in
       "sha256-PVskBSuwqSfNybHDtLLfVpDG0dwR/Q1LhrHz1imsR8A=";
 
   mypy-boto3-cloudwatch =
-    buildMypyBoto3Package "cloudwatch" "1.43.38"
-      "sha256-3STwHJJpbVZa1ACznoyPLvYMpBGL7xxS1+5h0bx40+Y=";
+    buildMypyBoto3Package "cloudwatch" "1.43.46"
+      "sha256-xUMPa6CSx3wYLfGZMV05ulf2yX+p3ToM7aOd55U7AUE=";
 
   mypy-boto3-codeartifact =
     buildMypyBoto3Package "codeartifact" "1.43.0"
