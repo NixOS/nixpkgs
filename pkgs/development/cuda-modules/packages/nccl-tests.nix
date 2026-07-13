02cdacd8266bc012b1b740635c11d73071a13b37
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-13 13:36:42 +0000

    cudaPackages.nccl-tests: 2.19.1 -> 2.19.6

--- pkgs/development/cuda-modules/packages/nccl-tests.nix
+++ pkgs/development/cuda-modules/packages/nccl-tests.nix
@@ -30,13 +30,13 @@ backendStdenv.mkDerivation (finalAttrs: {
   # NOTE: Depends on the CUDA package set, so use cudaNamePrefix.
   name = "${cudaNamePrefix}-${finalAttrs.pname}-${finalAttrs.version}";
   pname = "nccl-tests";
-  version = "2.19.1";
+  version = "2.19.6";
 
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = "nccl-tests";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-eRwIl646ndISFttjG5nPqsXfPVmInABNIsphhh5I0wM=";
+    hash = "sha256-Dvqg5wRI/1ehZdLje5i7Yz0lhGdILUSIsEK7k9Cl46Q=";
   };
 
   postPatch = ''
