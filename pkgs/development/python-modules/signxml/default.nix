535be22990e09bbc0175676204bc3664a75402f0
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-14 04:34:18 +0000

    python3Packages.signxml: 4.5.1 -> 5.1.0

--- pkgs/development/python-modules/signxml/default.nix
+++ pkgs/development/python-modules/signxml/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "signxml";
-  version = "4.5.1";
+  version = "5.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "XML-Security";
     repo = "signxml";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-0DzHw7E2sAJE3O7io++zjsi07FbkBD24EjGDOVo8/9s=";
+    hash = "sha256-SvM+wqlJd2n3gg1ROzWTuUvwSlOrMQIPDO39IyVNrRA=";
   };
 
   build-system = [
