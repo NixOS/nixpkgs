9b02cdbe94925172542e4f7b74d36e2492f8f462
Harinn <prinn.dev@pm.me>
2026-07-15 01:43:29 +0700

    python3Packages.varint: modernize

--- pkgs/development/python-modules/varint/default.nix
+++ pkgs/development/python-modules/varint/default.nix
@@ -4,14 +4,17 @@
   lib,
   setuptools,
 }:
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "varint";
   version = "1.0.2";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "a6ecc02377ac5ee9d65a6a8ad45c9ff1dac8ccee19400a5950fb51d594214ca5";
+    pname = "varint";
+    inherit (finalAttrs) version;
+    hash = "sha256-puzAI3esXunWWmqK1Fyf8drIzO4ZQApZUPtR1ZQhTKU=";
   };
 
   build-system = [ setuptools ];
@@ -27,4 +30,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ rakesh4g ];
   };
-}
+})
