e7dc8626a94add24c81f94c7c1276d923abbcad9
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-14 06:04:50 +0000

    rustywind: 0.24.3 -> 0.25.2

--- pkgs/by-name/ru/rustywind/package.nix
+++ pkgs/by-name/ru/rustywind/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rustywind";
-  version = "0.24.3";
+  version = "0.25.2";
 
   src = fetchFromGitHub {
     owner = "avencera";
     repo = "rustywind";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-qbOlU7kqVbB/sQg4b78CohOwQbraulZ8dRxeT+39rFk=";
+    hash = "sha256-PeYKBLTQ7/fmNuWtIQiqC47omrdGuIlB55OPxBQJQiM=";
   };
 
-  cargoHash = "sha256-eXTdPtcsWhsABZU6kRzZ6eF1VaabouZwLAFI9KpAx98=";
+  cargoHash = "sha256-76gC+nw/eV4j68O74XsJDaDFYAEdqZB9EzsRj5vdOvs=";
 
   doInstallCheck = true;
   nativeInstallCheckInputs = [ versionCheckHook ];
