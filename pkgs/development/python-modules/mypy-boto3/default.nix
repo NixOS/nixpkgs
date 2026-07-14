578f2e195bd727a4332e00688a123a5b734ea612
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-14 11:00:57 +0200

    python3Packages.mypy-boto3-lambda: 1.43.46 -> 1.43.47

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -766,8 +766,8 @@ in
       "sha256-gYTCgaRwH3zKi6gg4MC8DUwXQT+jZO6lqc/vi+JUahU=";
 
   mypy-boto3-lambda =
-    buildMypyBoto3Package "lambda" "1.43.46"
-      "sha256-mM1VwviZXBkkjd5PBEpJKZSDCoyV6VH26erz78gJKSk=";
+    buildMypyBoto3Package "lambda" "1.43.47"
+      "sha256-IaFTzlMYq0bEBdIy58yCl76Z1g839YRxlFs4E2N6emg=";
 
   mypy-boto3-lex-models =
     buildMypyBoto3Package "lex-models" "1.43.3"
