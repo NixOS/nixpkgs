d0a3cb8903937c21b8f35d0ae639248457b58ee8
torstenboettjer <torsten.boettjer@gmail.com>
2026-07-14 14:00:29 +0200

    rescile-ce: allow unfree evaluation in check-meta

--- pkgs/by-name/re/rescile-ce/package.nix
+++ pkgs/by-name/re/rescile-ce/package.nix
@@ -3,57 +3,69 @@
   stdenv,
   fetchurl,
   patchelf,
+  testers,
 }:
-let
-  version = "0.1.185";
 
+let
+  version = "0.1.187";
+
+  # Map Nix host platforms to the upstream pre-built binaries
   platform =
-    if stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isx86_64 then
-      {
-        url = "https://updates.rescile.com/v${version}/rescile-ce-linux-amd64";
-        sha256 = "a6bfe6230e7ef6d62dfa1217274a05ed9ac95228df52a3bdfeece64746fa6839"; # Adjust if 0.1.185 hash differs
-        metaPlatforms = [ "x86_64-linux" ];
-      }
-    else if stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isAarch64 then
-      {
-        url = "https://updates.rescile.com/v${version}/rescile-ce-darwin-arm64";
-        sha256 = "5546c884b4b5200936d35eb6a5ad8634ace93c01d67229a955011678ce5d773a"; # Adjust if 0.1.185 hash differs
-        metaPlatforms = [ "aarch64-darwin" ];
-      }
-    else
-      throw "rescile-ce: unsupported platform ${stdenv.hostPlatform.system}";
+    if stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isx86_64 then {
+      url = "https://updates.rescile.com/v${version}/rescile-ce-linux-amd64";
+      hash = "sha256-NJyCGTFhNk2rm04WyVKxZ/1n/P+61BU/BkW56gR50zc=";
+    } else if stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isAarch64 then {
+      url = "https://updates.rescile.com/v${version}/rescile-ce-darwin-arm64";
+      hash = "sha256-Pwjlajyl+YJ2X1fgYgbJOvV4hAdH7LUDEXkakYmFReE=";
+    } else throw "Unsupported platform: ${stdenv.hostPlatform.system}";
+
 in
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "rescile-ce";
   inherit version;
 
   src = fetchurl {
-    inherit (platform) url sha256;
+    inherit (platform) url hash;
   };
 
+  # Since we are downloading a bare binary, we prevent Nix from trying to unpack it like a tarball
   dontUnpack = true;
 
-  # Mandatory modern Nixpkgs standards flags
-  strictDeps = true;
-  __structuredAttrs = true;
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ patchelf ];
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
-    install -Dm755 $src $out/bin/rescile-ce
+    cp $src $out/bin/rescile-ce
+    chmod +x $out/bin/rescile-ce
+
+    runHook postInstall
   '';
 
-  fixupPhase = lib.optionalString stdenv.hostPlatform.isLinux ''
-    ${patchelf}/bin/patchelf \
+  # For Linux, pre-compiled binaries need their interpreter and runpaths patched to find Nix libraries
+  postFixup = lib.optionalString stdenv.hostPlatform.isLinux ''
+    patchelf \
       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${lib.makeLibraryPath [ stdenv.cc.cc.lib ]}" \
+      --set-rpath "${lib.makeLibraryPath [ stdenv.cc.cc ]}" \
       $out/bin/rescile-ce
   '';
 
+  # Providing a test makes ofBorg happy and ensures your binary actually executes!
+  passthru.tests = {
+    version = testers.testVersion {
+      package = finalAttrs.finalPackage;
+      command = "rescile-ce --version"; # Adjust if the help/version command is different
+    };
+  };
+
   meta = {
-    description = "Rescile Community Edition";
-    homepage = "https://www.rescile.com";
+    description = "Rescile Collaborative Engine";
+    homepage = "https://rescile.com";
     license = lib.licenses.unfreeRedistributable;
-    platforms = platform.metaPlatforms;
+    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
+    maintainers = with lib.maintainers; [ ]; # Add your GitHub handle here if you are maintaining it!
+    platforms = [ "x86_64-linux" "aarch64-darwin" ];
     mainProgram = "rescile-ce";
   };
-}
+})
