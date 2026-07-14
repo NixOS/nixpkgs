523c0c68674c758b88907e7a343798c63e2e537c
Anish Pallati <i@anish.land>
2026-07-14 05:28:52 -0400

    keycloak.plugins.keycloak-2fa-email-authenticator: init at 26.6.5
    Signed-off-by: Anish Pallati <i@anish.land>

--- pkgs/by-name/ke/keycloak/all-plugins.nix
+++ pkgs/by-name/ke/keycloak/all-plugins.nix
@@ -9,6 +9,7 @@
 {
   apple-identity-provider-keycloak = callPackage ./apple-identity-provider-keycloak { };
   keycloak-2fa-app-authenticator = callPackage ./keycloak-2fa-app-authenticator { };
+  keycloak-2fa-email-authenticator = callPackage ./keycloak-2fa-email-authenticator { };
   keycloak-2fa-sms-authenticator = callPackage ./keycloak-2fa-sms-authenticator { };
   keycloak-discord = callPackage ./keycloak-discord { };
   keycloak-enforce-mfa-authenticator = callPackage ./keycloak-enforce-mfa-authenticator { };
