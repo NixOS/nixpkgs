01b7cf17b69264828ef290800beed8e6cf8515b9
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-14 23:27:44 +0000

    basicswap: 0.14.4 -> 0.17.2

--- pkgs/by-name/ba/basicswap/package.nix
+++ pkgs/by-name/ba/basicswap/package.nix
@@ -58,14 +58,14 @@ let
 in
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "basicswap";
-  version = "0.14.4";
+  version = "0.17.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "basicswap";
     repo = "basicswap";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-UhuBTbGULImqRSsbg0QNb3yvnN7rnSzycweDLbqrW+8=";
+    hash = "sha256-xuyibB1JqdXS8GQ67XYDFxv/NMxgQIaaKqX9TLrCxkc=";
   };
 
   postPatch = ''
