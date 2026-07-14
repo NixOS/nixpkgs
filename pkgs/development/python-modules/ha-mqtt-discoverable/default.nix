f81aca1f36e2a9a7b3de821e14d30eadf8286930
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-14 23:01:16 +0200

    python3Packages.ha-mqtt-discoverable: migrate to finalAttrs

--- pkgs/development/python-modules/ha-mqtt-discoverable/default.nix
+++ pkgs/development/python-modules/ha-mqtt-discoverable/default.nix
@@ -9,7 +9,7 @@
   pydantic,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "ha-mqtt-discoverable";
   version = "0.25.2";
   pyproject = true;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "unixorn";
     repo = "ha-mqtt-discoverable";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-7MZDzaGJExEBjNjqjIyYWrnoGbt/dVbWlNkU33jvvU0=";
   };
 
@@ -44,8 +44,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python module to create MQTT entities that are automatically discovered by Home Assistant";
     homepage = "https://github.com/unixorn/ha-mqtt-discoverable";
-    changelog = "https://github.com/unixorn/ha-mqtt-discoverable/releases/tag/${src.tag}";
+    changelog = "https://github.com/unixorn/ha-mqtt-discoverable/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
