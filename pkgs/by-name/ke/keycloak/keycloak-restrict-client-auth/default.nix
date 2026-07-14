a7dff89c8a7054043087c0c0b7d1843be98360b9
Anish Pallati <i@anish.land>
2026-07-14 05:27:43 -0400

    keycloak.plugins.keycloak-restrict-client-auth: fix build with maven 3.9.16
    Signed-off-by: Anish Pallati <i@anish.land>

--- pkgs/by-name/ke/keycloak/keycloak-restrict-client-auth/default.nix
+++ pkgs/by-name/ke/keycloak/keycloak-restrict-client-auth/default.nix
@@ -2,20 +2,24 @@
   maven,
   lib,
   fetchFromGitHub,
+  nix-update-script,
 }:
 
-maven.buildMavenPackage rec {
+maven.buildMavenPackage (finalAttrs: {
   pname = "keycloak-restrict-client-auth";
   version = "26.1.0";
 
   src = fetchFromGitHub {
     owner = "sventorben";
     repo = "keycloak-restrict-client-auth";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-nQ2AwXhSUu5RY/BbxXE2OXgEb7Zf6FfrGP5tfbgAXk8=";
   };
 
-  mvnHash = "sha256-32un0gcpFI5wU9eShASzVnXmdhu3e+55iC3GBX/2yko=";
+  mvnHash = "sha256-Q/UKZ4oe7T2pVRb8U0SoyvxUMgUn9grlDJPxlU9wLg4=";
+
+  strictDeps = true;
+  __structuredAttrs = true;
 
   installPhase = ''
     runHook preInstall
@@ -23,10 +27,13 @@ maven.buildMavenPackage rec {
     runHook postInstall
   '';
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     homepage = "https://github.com/sventorben/keycloak-restrict-client-auth";
+    changelog = "https://github.com/sventorben/keycloak-restrict-client-auth/releases/tag/v${finalAttrs.version}";
     description = "Keycloak authenticator to restrict authorization on clients";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ leona ];
   };
-}
+})
