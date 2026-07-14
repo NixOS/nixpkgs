e8ce0d1e2c22133cf70d4b61d046600949e77882
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-14 01:47:58 +0000

    pmbootstrap: 3.10.3 -> 3.11.1

--- pkgs/by-name/pm/pmbootstrap/package.nix
+++ pkgs/by-name/pm/pmbootstrap/package.nix
@@ -15,14 +15,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "pmbootstrap";
-  version = "3.10.3";
+  version = "3.11.1";
   pyproject = true;
 
   src = fetchFromGitLab {
     owner = "postmarketOS";
     repo = "pmbootstrap";
     tag = version;
-    hash = "sha256-Zl7Ti0HwMQSjMeW4GjdEKIRoCNjV15Qiv8bzhktNoyQ=";
+    hash = "sha256-8i2CUJL/FALS3B/q39OlpQbhrY4ar2qdn0gnbFuJjUk=";
     domain = "gitlab.postmarketos.org";
   };
 
