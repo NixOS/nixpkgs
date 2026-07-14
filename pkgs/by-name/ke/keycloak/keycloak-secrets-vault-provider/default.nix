bd2940cc0313f090127956e008a748abc24aa1fc
Anish Pallati <i@anish.land>
2026-07-14 05:27:44 -0400

    keycloak.plugins.keycloak-secrets-vault-provider: modernize
    Signed-off-by: Anish Pallati <i@anish.land>

--- pkgs/by-name/ke/keycloak/keycloak-secrets-vault-provider/default.nix
+++ pkgs/by-name/ke/keycloak/keycloak-secrets-vault-provider/default.nix
@@ -2,28 +2,35 @@
   maven,
   lib,
   fetchFromGitHub,
+  nix-update-script,
 }:
-maven.buildMavenPackage rec {
+maven.buildMavenPackage (finalAttrs: {
   pname = "keycloak-secrets-vault-provider";
   version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "Nordix";
     repo = "keycloak-secrets-vault-provider";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-7ttjTm3D+dDiw+00pK4yvDFNCuXFmapPKnOY7vfa2Ac=";
   };
 
   mvnHash = "sha256-AJwt6JnNAffXzazWI2fIUMp5j/Fm5LRhO31bOUOl7g0=";
 
+  strictDeps = true;
+  __structuredAttrs = true;
+
   installPhase = ''
     runHook preInstall
-    install -Dm644 -t "$out/keycloak-secrets-vault-provider.jar" target/secrets-provider-${version}.jar
+    install -Dm644 -t "$out/keycloak-secrets-vault-provider.jar" target/secrets-provider-${finalAttrs.version}.jar
     runHook postInstall
   '';
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     homepage = "https://github.com/Nordix/keycloak-secrets-vault-provider";
+    changelog = "https://github.com/Nordix/keycloak-secrets-vault-provider/releases/tag/v${finalAttrs.version}";
     description = "Keycloak Vault SPI provider for OpenBao and HashiCorp Vault";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [
@@ -31,4 +38,4 @@ maven.buildMavenPackage rec {
       anish
     ];
   };
-}
+})
