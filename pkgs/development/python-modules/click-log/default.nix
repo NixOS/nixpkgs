d929d3e011535e24f562edb445caf7252acc4b20
Harinn <prinn.dev@pm.me>
2026-07-15 12:24:55 +0700

    python3Packages.click-log: migrate to pyproject

--- pkgs/development/python-modules/click-log/default.nix
+++ pkgs/development/python-modules/click-log/default.nix
@@ -2,20 +2,23 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   click,
 }:
 
 buildPythonPackage rec {
   pname = "click-log";
   version = "0.4.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-OXD4VwrFRJEje82z2KtePu9sBX3yn4w9EVGlGpwjuXU=";
   };
 
-  propagatedBuildInputs = [ click ];
+  build-system = [ setuptools ];
+
+  dependencies = [ click ];
 
   meta = {
     homepage = "https://github.com/click-contrib/click-log/";
