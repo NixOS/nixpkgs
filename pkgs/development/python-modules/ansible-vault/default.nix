f7b42cafc617a6a20794d0030b027f2fbc935694
kilyanni <github@nkilian.org>
2026-07-15 11:39:55 +0200

    python3Packages.ansible-vault: 2.1.0 -> 4.1.0

--- pkgs/development/python-modules/ansible-vault/default.nix
+++ pkgs/development/python-modules/ansible-vault/default.nix
@@ -1,49 +1,71 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
 
   # build-system
   setuptools,
 
   # dependencies
   ansible-core,
+  pyyaml,
 
   # tests
   pytestCheckHook,
+  writableTmpDirAsHomeHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "ansible-vault";
   version = "4.1.0";
   pyproject = true;
 
-  src = fetchPypi {
-    pname = "ansible-vault";
-    inherit version;
-    hash = "sha256-XOj9tUcPFEm3a/B64qvFZIDa1INWrkBchbaG77ZNvV4";
+  # Fetched from GitHub because PyPI doesn't ship the test suite.
+  src = fetchFromGitHub {
+    owner = "tomoh1r";
+    repo = "ansible-vault";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-JSFZavafUByn4u+WkPo01lPCzjv/ekCM8nvWSy4QrAs=";
   };
+
+  # The test helper resolves the ansible-vault CLI as a sibling of sys.executable,
+  # which holds in a venv but not under nix's split store; point it at ansible-core.
+  postPatch = ''
+    substituteInPlace test/lib/testing/__init__.py \
+      --replace-fail 'os.path.join(os.path.dirname(sys.executable), "ansible-vault")' \
+                     '"${lib.getExe' ansible-core "ansible-vault"}"'
+  '';
 
   nativeBuildInputs = [ setuptools ];
 
-  propagatedBuildInputs = [ ansible-core ];
+  propagatedBuildInputs = [
+    ansible-core
+    pyyaml
+  ];
 
-  # Otherwise tests will fail to create directory
-  # Permission denied: '/homeless-shelter'
-  preCheck = ''
-    export HOME=$(mktemp -d)
-  '';
+  nativeCheckInputs = [
+    pytestCheckHook
+    # importing ansible writes to HOME, without this import check and checkPhase would both fail
+    writableTmpDirAsHomeHook
+  ];
 
-  # no tests in sdist, no 2.1.0 tag on git
-  doCheck = false;
+  disabledTestPaths = [
+    # dev-only lint suite (black/flake8/isort/pylint), not useful for nixpkgs
+    "test/linter"
+  ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  disabledTests = [
+    # This fails on a benign reformatting of ansible-core's error message
+    "TestCannotLoadWithInvalidPassword"
+  ];
+
+  pythonImportsCheck = [ "ansible_vault" ];
 
   meta = {
-    description = "This project aim to R/W an ansible-vault yaml file";
+    description = "R/W an ansible-vault yaml file";
     homepage = "https://github.com/tomoh1r/ansible-vault";
     changelog = "https://github.com/tomoh1r/ansible-vault/blob/master/CHANGES.txt";
     license = lib.licenses.gpl3;
     maintainers = with lib.maintainers; [ StillerHarpo ];
   };
-}
+})
