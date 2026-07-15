7395f010425c7010d56a51bfb2d00c70ffa7b236
Harinn <prinn.dev@pm.me>
2026-07-15 12:27:34 +0700

    python3Packages.click-help-colors: modernize

--- pkgs/development/python-modules/click-help-colors/default.nix
+++ pkgs/development/python-modules/click-help-colors/default.nix
@@ -7,13 +7,16 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "click-help-colors";
   version = "0.9.4";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    pname = "click-help-colors";
+    inherit (finalAttrs) version;
     hash = "sha256-9Mq+Us9VApm4iI9PLuTF81msJ+M7z+TWHbR3haXMk2w=";
   };
 
@@ -28,8 +31,8 @@ buildPythonPackage rec {
   meta = {
     description = "Colorization of help messages in Click";
     homepage = "https://github.com/click-contrib/click-help-colors";
-    changelog = "https://github.com/click-contrib/click-help-colors/blob/${version}/CHANGES.rst";
+    changelog = "https://github.com/click-contrib/click-help-colors/blob/${finalAttrs.version}/CHANGES.rst";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
