94c985c4ed5e0bbe7634e4a628b63639077146e7
Anntóin Wilkinson <anntoin@gmail.com>
2026-07-15 22:49:10 +0100

    keymapper: 5.3.1 -> 5.6.0
    Add anntoin as maintainer.
    
    https://github.com/houmain/keymapper/blob/5.6.0/CHANGELOG.md
    
    Assisted-by: Hermes Agent (glm-5.2 via ollama-cloud)

--- pkgs/by-name/ke/keymapper/package.nix
+++ pkgs/by-name/ke/keymapper/package.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "keymapper";
-  version = "5.3.1";
+  version = "5.6.0";
 
   src = fetchFromGitHub {
     owner = "houmain";
     repo = "keymapper";
     tag = finalAttrs.version;
-    hash = "sha256-YKfKgsrjDrskLEoYCSRMYco7+7E/sgXFAMEwwm7rs7w=";
+    hash = "sha256-0GadjBGgawn0V+PV04R6ULmanNUF7R14N/jHhObcTzM=";
   };
 
   # all the following must be in nativeBuildInputs
@@ -47,8 +47,7 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://github.com/houmain/keymapper";
     license = lib.licenses.gpl3Only;
     mainProgram = "keymapper";
-    maintainers = [
-    ];
+    maintainers = with lib.maintainers; [ anntoin ];
     platforms = lib.platforms.linux;
   };
 })
