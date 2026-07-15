8dc9585a18452de38a7cd8b858664cd662eb8a6f
Jakob Klepp <jakob.klepp@moonvision.io>
2026-07-15 12:33:30 +0200

    python3Packages.pandas-ta: remove
    The unfree package source has disappeared.
    https://discourse.nixos.org/t/how-to-track-down-what-depends-on-a-broken-dependency/78833/12

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12747,8 +12747,6 @@ self: super: with self; {
 
   pandas-stubs = callPackage ../development/python-modules/pandas-stubs { };
 
-  pandas-ta = callPackage ../development/python-modules/pandas-ta { };
-
   pandera = callPackage ../development/python-modules/pandera { };
 
   pandoc-attributes = callPackage ../development/python-modules/pandoc-attributes { };
