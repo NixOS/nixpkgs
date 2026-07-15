125f236f8f5cb49d4d2089f53ea8e8d653022930
Harinn <prinn.dev@pm.me>
2026-07-15 12:27:41 +0700

    python3Packages.click-configfile: modernize

--- pkgs/development/python-modules/click-configfile/default.nix
+++ pkgs/development/python-modules/click-configfile/default.nix
@@ -8,13 +8,16 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "click-configfile";
   version = "0.2.3";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    pname = "click-configfile";
+    inherit (finalAttrs) version;
     hash = "sha256-lb7sE77pUOmPQ8gdzavvT2RAkVWepmKY+drfWTUdkNE=";
   };
 
@@ -29,7 +32,7 @@ buildPythonPackage rec {
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace "install_requires=install_requires," 'install_requires=["click >= 6.6", "six >= 1.10"],'
+      --replace-fail "install_requires=install_requires," 'install_requires=["click >= 6.6", "six >= 1.10"],'
   '';
 
   pythonImportsCheck = [ "click_configfile" ];
@@ -45,4 +48,4 @@ buildPythonPackage rec {
     license = lib.licenses.bsd3;
     maintainers = [ ];
   };
-}
+})
