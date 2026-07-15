67c199eae839c0a8bb0f45377337e5bb140922e3
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-15 15:56:12 +0000

    linuxKernel.kernels.linux_zen: 7.1.2 -> 7.1.3

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -12,13 +12,13 @@ let
   # override options if they need using lib.mkForce (that has 50 priority)
   mkKernelOverride = lib.mkOverride 90;
 
-  suffix = "zen1";
+  suffix = "zen2";
 in
 
 buildLinux (
   args
   // rec {
-    version = "7.1.2";
+    version = "7.1.3";
     pname = "linux-zen";
     modDirVersion = lib.versions.pad 3 "${version}-${suffix}";
     isZen = true;
@@ -27,7 +27,7 @@ buildLinux (
       owner = "zen-kernel";
       repo = "zen-kernel";
       rev = "v${version}-${suffix}";
-      sha256 = "0d02lgwl0x415xrbznsldd7j1krpzlnbd2ciis3shz8zhmbhfg1f";
+      sha256 = "0af939wq8gfgwrb4s3jjbkqh2pn3zkgk1w48m0hf5fc0qi8ify8x";
     };
 
     # This is based on the following source:
