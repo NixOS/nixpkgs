d97b86f1e0cc4f35f6ab90d4cef1780eaf8bd560
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-15 08:52:06 +0000

    sqls: 0.2.45 -> 0.2.48

--- pkgs/by-name/sq/sqls/package.nix
+++ pkgs/by-name/sq/sqls/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "sqls";
-  version = "0.2.45";
+  version = "0.2.48";
 
   src = fetchFromGitHub {
     owner = "sqls-server";
     repo = "sqls";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-r9D3YtUk/hv3ex/PSGPTNIBTW3s3a3KdIBtxDry30Zc=";
+    hash = "sha256-TjGu8QcwYIPoW2v61fXpq/oZKoksOUZ2/dnleJhPjFM=";
   };
 
-  vendorHash = "sha256-eh43G0fR+NRRXRPCfxjlwzzw3yg/ZRb1GpWwHGqyRrE=";
+  vendorHash = "sha256-VVa77h0mgWLEuL2+Q3qre5V71kbBaWaugNN9TcTC8y0=";
 
   ldflags = [
     "-s"
