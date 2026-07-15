3b7b78262a8eb035f7e8f24d7ce5e3e51f6057d3
OPNA2608 <opna2608@protonmail.com>
2026-07-15 18:49:52 +0200

    libresidfp: init at 1.1.1

---
+++ pkgs/by-name/li/libresidfp/package.nix
@@ -0,0 +1,105 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  gitUpdater,
+  testers,
+  withDocumentation ? true,
+  autoreconfHook,
+  doxygen,
+  graphviz,
+  makeFontsConf,
+  writableTmpDirAsHomeHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libresidfp";
+  version = "1.1.1";
+
+  src = fetchFromGitHub {
+    owner = "libsidplayfp";
+    repo = "libresidfp";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-xPIyJZWL5+tW3xk9b4dlL4klxXM6+cy38cALuhsH1zk=";
+  };
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  outputs = [
+    "out"
+    "dev"
+  ]
+  ++ lib.optionals withDocumentation [
+    "doc"
+  ];
+
+  # https://github.com/libsidplayfp/libresidfp/pull/41
+  postPatch = ''
+    substituteInPlace configure.ac \
+      --replace-fail 'AC_SUBST([LIB_MAJOR])NEWS.md' 'AC_SUBST([LIB_MAJOR])'
+  '';
+
+  nativeBuildInputs = [
+    autoreconfHook
+  ]
+  ++ lib.optionals withDocumentation [
+    doxygen
+    graphviz
+    writableTmpDirAsHomeHook
+  ];
+
+  configureFlags = [
+    # Supposedly runtime detection only supported on GCC
+    # __builtin_cpu_supports on GCC's list of x86 built-in functions
+    (lib.strings.withFeatureAs true "simd" (
+      if (stdenv.cc.isGNU && stdenv.hostPlatform.isx86) then "runtime" else "none"
+    ))
+    (lib.strings.enableFeature finalAttrs.finalPackage.doCheck "tests")
+  ];
+
+  enableParallelBuilding = true;
+
+  buildFlags = [
+    "all"
+  ]
+  ++ lib.optionals withDocumentation [
+    "doc"
+  ];
+
+  doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
+
+  postInstall = lib.optionalString withDocumentation ''
+    mkdir -p $doc/share/doc/
+    mv docs/html $doc/share/doc/libresidfp
+  '';
+
+  env = lib.optionalAttrs withDocumentation {
+    FONTCONFIG_FILE = makeFontsConf {
+      fontDirectories = [ ];
+    };
+  };
+
+  passthru = {
+    tests.pkg-config = testers.hasPkgConfigModules {
+      package = finalAttrs.finalPackage;
+      versionCheck = true;
+    };
+    updateScript = gitUpdater {
+      rev-prefix = "v";
+      ignoredVersions = "[a-zA-Z]";
+    };
+  };
+
+  meta = {
+    description = "Cycle exact SID emulation";
+    homepage = "https://github.com/libsidplayfp/libresidfp";
+    changelog = "https://github.com/libsidplayfp/libresidfp/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ lib.maintainers.OPNA2608 ];
+    platforms = lib.platforms.all;
+    pkgConfigModules = [
+      "libresidfp"
+    ];
+  };
+})
