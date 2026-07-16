4e31f1cf140476ac73e316d091ecc442582923d0
klea <klea@iwnp.org>
2026-07-16 16:13:18 +0000

    scroll-reverser: Use direct link
    Avoid the WBM indirection, since the link is live.

--- pkgs/by-name/sc/scroll-reverser/package.nix
+++ pkgs/by-name/sc/scroll-reverser/package.nix
@@ -9,7 +9,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   version = "1.9";
 
   src = fetchurl {
-    url = "https://web.archive.org/web/20250427052440/https://pilotmoon.com/downloads/ScrollReverser-${finalAttrs.version}.zip";
+    url = "https://pilotmoon.com/downloads/ScrollReverser-${finalAttrs.version}.zip";
     hash = "sha256-CWHbtvjvTl7dQyvw3W583UIZ2LrIs7qj9XavmkK79YU=";
   };
 
