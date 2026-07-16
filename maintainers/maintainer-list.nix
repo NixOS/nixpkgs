5e60253a5ec77128a1b728f257744c3c5e9995d5
hddq <git@hddq.org>
2026-07-16 02:41:18 +0200

    maintainers: add hddq

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11053,6 +11053,12 @@
     githubId = 10136407;
     name = "Harsh Chokshi";
   };
+  hddq = {
+    name = "hddq";
+    email = "git@hddq.org";
+    github = "hddq";
+    githubId = 125512521;
+  };
   hdhog = {
     name = "Serg Larchenko";
     email = "hdhog@hdhog.ru";
