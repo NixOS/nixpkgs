cf67ef06f4d8e2bbb735d2b9453f8fb470730833
Romana <science.zip@protonmail.com>
2026-07-16 05:20:36 +0200

    llvm-mos: Fixed package to include all outputs, use CMake setup hook
    Co-authored-by: Piyush Raj <hello@piyushraj.org>

--- pkgs/by-name/ll/llvm-mos/package.nix
+++ pkgs/by-name/ll/llvm-mos/package.nix
@@ -16,6 +16,7 @@
   libbacktrace,
   ncurses,
 }:
+
 stdenv.mkDerivation {
   pname = "llvm-mos";
   version = "0-unstable-2026-04-23";
@@ -51,82 +52,28 @@ stdenv.mkDerivation {
     ncurses
   ];
 
-  configurePhase = ''
-    runHook preConfigure
+  cmakeDir = "../llvm";
+  cmakeFlags = [
+    "-C"
+    "../clang/cmake/caches/MOS.cmake"
 
-    cmake \
-      -G Ninja \
-      -S llvm \
-      -B build \
-      -C clang/cmake/caches/MOS.cmake \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DLLVM_ENABLE_PROJECTS="clang;lld" \
-      -DLLVM_ENABLE_RUNTIMES="" \
-      -DLLVM_BUILD_RUNTIME=OFF \
-      -DLLVM_INCLUDE_TESTS=OFF \
-      -DLLVM_INCLUDE_BENCHMARKS=OFF \
-      -DLLVM_INCLUDE_EXAMPLES=OFF \
-      -DLLVM_INCLUDE_DOCS=OFF \
-      -DLLVM_ENABLE_ASSERTIONS=OFF \
-      -DLLVM_ENABLE_DISTRIBUTION=OFF \
-      -DLLVM_DISTRIBUTION_COMPONENTS="" \
-      -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON
-
-    runHook postConfigure
-  '';
-
-  buildPhase = ''
-    runHook preBuild
-
-    cmake --build build \
-      --target \
-        clang \
-        lld \
-        llvm-ar \
-        llvm-ranlib \
-        llvm-objcopy \
-        llvm-objdump \
-        llvm-strip \
-      --parallel $NIX_BUILD_CORES
-
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin
-
-    install -Dm755 build/bin/clang \
-      $out/bin/mos-clang
-
-    install -Dm755 build/bin/clang++ \
-      $out/bin/mos-clang++
-
-    install -Dm755 build/bin/ld.lld \
-      $out/bin/mos-ld.lld
-
-    install -Dm755 build/bin/llvm-ar \
-      $out/bin/mos-ar
-
-    install -Dm755 build/bin/llvm-ranlib \
-      $out/bin/mos-ranlib
-
-    install -Dm755 build/bin/llvm-objcopy \
-      $out/bin/mos-objcopy
-
-    install -Dm755 build/bin/llvm-objdump \
-      $out/bin/mos-objdump
-
-    install -Dm755 build/bin/llvm-strip \
-      $out/bin/mos-strip
-
-    runHook postInstall
-  '';
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DLLVM_ENABLE_PROJECTS=clang;lld"
+    "-DLLVM_ENABLE_RUNTIMES="
+    "-DLLVM_BUILD_RUNTIME=OFF"
+    "-DLLVM_INCLUDE_TESTS=OFF"
+    "-DLLVM_INCLUDE_BENCHMARKS=OFF"
+    "-DLLVM_INCLUDE_EXAMPLES=OFF"
+    "-DLLVM_INCLUDE_DOCS=OFF"
+    "-DLLVM_ENABLE_ASSERTIONS=OFF"
+    "-DLLVM_ENABLE_DISTRIBUTION=OFF"
+    "-DLLVM_DISTRIBUTION_COMPONENTS="
+    "-DLLVM_INSTALL_TOOLCHAIN_ONLY=ON"
+  ];
 
   meta = {
     description = "LLVM-MOS C compiler for 6502-based systems";
-    homepage = "https://llvm-mos.org";
+    homepage = "https://llvm-mos.org/";
     license = lib.licenses.gpl3;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ llamato ];
