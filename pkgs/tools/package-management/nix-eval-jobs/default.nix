12814ca04809d0082b07bb5c1271c9f3563588bb
zowoq <59103226+zowoq@users.noreply.github.com>
2026-07-16 23:13:23 +1000

    nix-eval-jobs: 2.34.3 -> 2.35.0
    Diff: https://github.com/NixOS/nix-eval-jobs/compare/v2.34.3...v2.35.0

--- pkgs/tools/package-management/nix-eval-jobs/default.nix
+++ pkgs/tools/package-management/nix-eval-jobs/default.nix
@@ -12,13 +12,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "nix-eval-jobs";
-  version = "2.34.3";
+  version = "2.35.0";
 
   src = fetchFromGitHub {
     owner = "NixOS";
     repo = "nix-eval-jobs";
     tag = "v${version}";
-    hash = "sha256-YaVQAgBxWbUBFHXLBLzdUyVvuA/DDw80SEnn9iq0Veo=";
+    hash = "sha256-/C5wyGYe4uMKKH26vy3knpwP/hvjOHO/58cySL8ADC4=";
   };
 
   buildInputs = [
