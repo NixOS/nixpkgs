38e6d10918ab37e6fd30b29a354bfce72697045e
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-07-17 19:21:40 +0200

    R: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7104,8 +7104,6 @@ with pkgs;
 
   ### DEVELOPMENT / R MODULES
 
-  R = callPackage ../applications/science/math/R { };
-
   rWrapper = callPackage ../development/r-modules/wrapper.nix {
     recommendedPackages = with rPackages; [
       boot
