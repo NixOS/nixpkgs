eb98c3378fcb1b84b396b1ed47601206be0cbcad
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-17 13:26:09 +0000

    daktari: 0.0.341 -> 0.0.342

--- pkgs/by-name/da/daktari/package.nix
+++ pkgs/by-name/da/daktari/package.nix
@@ -7,7 +7,7 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "daktari";
-  version = "0.0.341";
+  version = "0.0.342";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -15,7 +15,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
     owner = "genio-learn";
     repo = "daktari";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Z2NJ59APuw3AwJ4d3Ux8aaLugbzxjs7CnGQHyfIHiyk=";
+    hash = "sha256-g2uFcXV2v5ik5u2Gvysea8K1WeIIzDGJLmslW4awmNc=";
   };
 
   patches = [ ./optional-pyclip.patch ];
