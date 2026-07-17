NixOS/nixpkgs

commit 3a7deefec3200122e71ab50a0f9ed4f9849a91e1

cargo: Add fish completions, generate others when possible

pkgs/development/compilers/rust/cargo.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   pkgsHostHost,
+  pkgsBuildBuild,
   file,
   curl,
   pkg-config,
@@ -74,11 +75,23 @@ rustPlatform.buildRustPackage.override
 
       installManPage src/tools/cargo/src/etc/man/*
 
-      installShellCompletion --bash --name cargo \
-        src/tools/cargo/src/etc/cargo.bashcomp.sh
-
-      installShellCompletion --zsh src/tools/cargo/src/etc/_cargo
-    '';
+    ''
+    + (
+      if stdenv.buildPlatform.canExecute stdenv.hostPlatform then
+        ''
+          installShellCompletion --cmd cargo \
+            --bash <(CARGO_COMPLETE=bash cargo) \
+            --fish <(CARGO_COMPLETE=fish cargo) \
+            --zsh <(CARGO_COMPLETE=zsh cargo)
+        ''
+      else
+        ''
+          installShellCompletion --cmd cargo \
+            --bash src/tools/cargo/src/etc/cargo.bashcomp.sh \
+            --fish ${pkgsBuildBuild.cargo}/share/fish/vendor_completions.d/*.fish \
+            --zsh src/tools/cargo/src/etc/_cargo
+        ''
+    );
 
     checkPhase = ''
       # Disable cross compilation tests

commit 89327c805c0795fc2ae3135eb9911825bb89fb4f

cargo: don't use bootrstrap cargo for completions
Regression introduced in 3a7deefec3200122e71ab50a0f9ed4f9849a91e1

pkgs/development/compilers/rust/cargo.nix
@@ -80,9 +80,9 @@ rustPlatform.buildRustPackage.override
       if stdenv.buildPlatform.canExecute stdenv.hostPlatform then
         ''
           installShellCompletion --cmd cargo \
-            --bash <(CARGO_COMPLETE=bash cargo) \
-            --fish <(CARGO_COMPLETE=fish cargo) \
-            --zsh <(CARGO_COMPLETE=zsh cargo)
+            --bash <(CARGO_COMPLETE=bash $out/bin/cargo) \
+            --fish <(CARGO_COMPLETE=fish $out/bin/cargo) \
+            --zsh <(CARGO_COMPLETE=zsh $out/bin/cargo)
         ''
       else
         ''