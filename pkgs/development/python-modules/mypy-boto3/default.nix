474caf93934076b54430283f9a9d14d1f5090b48
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-17 09:48:00 +0200

    python3Packages.mypy-boto3-redshift: 1.43.7 -> 1.43.50

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1090,8 +1090,8 @@ in
       "sha256-11kfuBIlPjrL6NdBeo+23xYQfL9eeu+C7Gfi2aorNDI=";
 
   mypy-boto3-redshift =
-    buildMypyBoto3Package "redshift" "1.43.7"
-      "sha256-2YZ1Q6A1NWv1TeigA9Rs7a/szHz0QrbCnQX5+XyavcE=";
+    buildMypyBoto3Package "redshift" "1.43.50"
+      "sha256-S122NUgI3c1ne3XjPlP3SUWVSPJoV4sHhE4V4HS145o=";
 
   mypy-boto3-redshift-data =
     buildMypyBoto3Package "redshift-data" "1.43.0"
