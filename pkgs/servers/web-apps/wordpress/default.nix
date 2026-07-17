991846196c54b9efc03cc263b9fda36a3bff4f96
Bart Oostveen <bart@bartoostveen.nl>
2026-07-17 21:44:05 +0200

    wordpress_6_8: 6.8.5 -> 6.8.6
    Fixes (high-severity) security vulnerabilities, of which a SQL injection into RCE.
    Announcement: https://wordpress.org/news/2026/07/wordpress-7-0-2-release/
    
    (cherry picked from commit 1228b953c8c9306ba21699cdc41def104551d55b)

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -6,8 +6,8 @@ builtins.mapAttrs (_: callPackage ./generic.nix) rec {
     hash = "sha256-IS9B6kwjWLYLybmWG2Ym5+JAgFExMqhcEQsJJF2puXg=";
   };
   wordpress_6_8 = {
-    version = "6.8.5";
-    hash = "sha256-N/WVUQxI0W3t4L+lr6KcuK8S2/Dj00WyXElMFfjIHYE=";
+    version = "6.8.6";
+    hash = "sha256-ID5C12xn+GprUynX8WLS5dl9iJyv13yDxYbqMDPB8P4=";
   };
   wordpress_6_9 = {
     version = "6.9.5";
