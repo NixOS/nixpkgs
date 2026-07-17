5ab3461dfefa9a25b87ef8cbb05b7ede8d899055
Guy Chronister <guychronister@outlook.com>
2026-07-17 17:07:07 -0500

    your-editor: refactor derivation, move arg out of top-level, use strictDeps and structuredAttrs

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9778,7 +9778,7 @@ with pkgs;
 
   ydiff = with python3.pkgs; toPythonApplication ydiff;
 
-  your-editor = callPackage ../applications/editors/your-editor { stdenv = gccStdenv; };
+  your-editor = callPackage ../applications/editors/your-editor { };
 
   youtube-dl = with python3Packages; toPythonApplication youtube-dl;
 
