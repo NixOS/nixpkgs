eb6577f5d5e988227ddf049b45a6964374ba19d5
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-17 05:39:42 +0000

    pvz-portable-unwrapped: 0.1.26 -> 0.1.27

--- pkgs/by-name/pv/pvz-portable-unwrapped/package.nix
+++ pkgs/by-name/pv/pvz-portable-unwrapped/package.nix
@@ -23,13 +23,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pvz-portable-unwrapped";
-  version = "0.1.26";
+  version = "0.1.27";
 
   src = fetchFromGitHub {
     owner = "wszqkzqk";
     repo = "PvZ-Portable";
     tag = finalAttrs.version;
-    hash = "sha256-IsU/l7k7F79nfM6TYdbcofOmJKm6v55HR7HHAZc7sH0=";
+    hash = "sha256-kebgKQRju2CRboKRytkTEIjm7Hl+1KkIOdrvbwG/ufs=";
   };
 
   nativeBuildInputs = [
