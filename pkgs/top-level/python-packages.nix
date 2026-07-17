45f64fe8518432da2a783f98d243da360a5e4b31
Dennis Wuitz <dennish@wuitz.de>
2026-07-17 15:46:05 +0200

    python3Packages.daft: remove package

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3557,8 +3557,6 @@ self: super: with self; {
 
   daff = callPackage ../development/python-modules/daff { };
 
-  daft = callPackage ../development/python-modules/daft { };
-
   dahlia = callPackage ../development/python-modules/dahlia { };
 
   daiquiri = callPackage ../development/python-modules/daiquiri { };
