dce865327e584956b365cb14da32c2d270e119a7
Biswapriyo Nath <nathbappai@gmail.com>
2026-07-17 14:26:12 +0000

    nixos/spice-vdagent: Fix desktop resize with user service
    The service options are same as unit files provided by upstream project.
    Fixes: https://github.com/NixOS/nixpkgs/issues/481078

--- nixos/modules/services/misc/spice-vdagentd.nix
+++ nixos/modules/services/misc/spice-vdagentd.nix
@@ -29,5 +29,18 @@ in
         ExecStart = "${pkgs.spice-vdagent}/bin/spice-vdagentd";
       };
     };
+
+    systemd.user.services.spice-vdagent = {
+      description = "Spice guest session agent";
+      documentation = [ "man:spice-vdagent(1)" ];
+      unitConfig.ConditionPathExists = "/run/spice-vdagentd/spice-vdagent-sock";
+      after = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.spice-vdagent}/bin/spice-vdagent -x";
+        StandardError = "null";
+      };
+    };
   };
 }
