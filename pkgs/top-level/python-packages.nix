747c1123b6686f9ea55478fd6f89370733fc9023
aaravrav <37036762+aaravrav@users.noreply.github.com>
2026-07-17 09:27:48 +0530

    python3Packages.fastnanoid: init at 0.4.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5942,6 +5942,8 @@ self: super: with self; {
 
   fastmri = callPackage ../development/python-modules/fastmri { };
 
+  fastnanoid = callPackage ../development/python-modules/fastnanoid { };
+
   fastnlo-toolkit = toPythonModule (
     pkgs.fastnlo-toolkit.override {
       withPython = true;
