b92cea929aec7c1e94f29b8e686ce1730b1cda32
Harinn <prinn.dev@pm.me>
2026-07-19 00:24:10 +0700

    python3Packages.grpcio-gcp: migrate to pyproject

--- pkgs/development/python-modules/grpcio-gcp/default.nix
+++ pkgs/development/python-modules/grpcio-gcp/default.nix
@@ -2,20 +2,23 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   grpcio,
 }:
 
 buildPythonPackage rec {
   pname = "grpcio-gcp";
   version = "0.2.2";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "e292605effc7da39b7a8734c719afb12ec4b5362add3528d8afad3aa3aa9057c";
   };
 
-  propagatedBuildInputs = [ grpcio ];
+  build-system = [ setuptools ];
+
+  dependencies = [ grpcio ];
 
   meta = {
     description = "gRPC extensions for Google Cloud Platform";
