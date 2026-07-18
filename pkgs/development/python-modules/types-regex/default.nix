9abf76a77dd4eb7c96490dab08b9099a04ece59e
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-18 23:27:42 +0000

    python3Packages.types-regex: 2026.6.28.20260630 -> 2026.7.10.20260711

--- pkgs/development/python-modules/types-regex/default.nix
+++ pkgs/development/python-modules/types-regex/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "types-regex";
-  version = "2026.6.28.20260630";
+  version = "2026.7.10.20260711";
   pyproject = true;
 
   src = fetchPypi {
     pname = "types_regex";
     inherit (finalAttrs) version;
-    hash = "sha256-X6/kNLV409d2QKq5SKPr/J7+pyNBK/rlVRs/VQhqtqc=";
+    hash = "sha256-mqGxQgYK3V1KT0IUbry3DgHnsniefORzJVMj88Xu3e0=";
   };
 
   build-system = [ setuptools ];
