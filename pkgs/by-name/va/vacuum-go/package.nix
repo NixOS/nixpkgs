c00f6004ac8f64c844a3e93fb13d74dd5cf31db5
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-18 10:34:30 +0000

    vacuum-go: 0.29.9 -> 0.29.10

--- pkgs/by-name/va/vacuum-go/package.nix
+++ pkgs/by-name/va/vacuum-go/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "vacuum-go";
-  version = "0.29.9";
+  version = "0.29.10";
 
   src = fetchFromGitHub {
     owner = "daveshanley";
     repo = "vacuum";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dcc29hbllJqEI9Qv3UnLVEHfzWF0CQYalKCyUlCBsEA=";
+    hash = "sha256-LhPHHtQSIOeRsS/Mx3xIOyP2tMV6avR3cR3uQoX76RU=";
   };
 
-  vendorHash = "sha256-/0dAwcg3s5dTu97msrnwuOWjeCbmYJqxcldC2xLm3y0=";
+  vendorHash = "sha256-dexp++8oB8zIxkD5Evf02qaehiLxCy/4oVnU3ndn+FA=";
 
   env.CGO_ENABLED = 0;
   ldflags = [
