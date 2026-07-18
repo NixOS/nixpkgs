3bf09b228355fef9e6955a8a8af342e5b0554420
Dima <uzhdimka@gmail.com>
2026-07-18 12:56:12 +0200

    mitm6: fix build failure

--- pkgs/by-name/mi/mitm6/package.nix
+++ pkgs/by-name/mi/mitm6/package.nix
@@ -17,7 +17,6 @@ python3.pkgs.buildPythonApplication rec {
   build-system = with python3.pkgs; [ setuptools ];
 
   dependencies = with python3.pkgs; [
-    future
     netifaces
     scapy
     twisted
