e1585d63fc3ac7f2853a916cd239dcd76f14e742
Harinn <prinn.dev@pm.me>
2026-07-19 00:46:37 +0700

    python3Packages.grpcio-status: modernize

--- pkgs/development/python-modules/grpcio-status/default.nix
+++ pkgs/development/python-modules/grpcio-status/default.nix
@@ -11,20 +11,22 @@
 # This package should be updated together with the main grpc package and other
 # related python grpc packages.
 # nixpkgs-update: no auto update
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "grpcio-status";
   version = "1.83.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
     pname = "grpcio_status";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-g3IZxt6a/cy29vcrNLxx4VGiAR7wQEDj+qynRqV+VK4=";
   };
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace 'protobuf>=4.21.6' 'protobuf'
+      --replace-warn 'protobuf>=4.21.6' 'protobuf'
   '';
 
   build-system = [ setuptools ];
@@ -46,4 +48,4 @@ buildPythonPackage rec {
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
