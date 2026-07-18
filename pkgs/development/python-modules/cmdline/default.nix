8023aeef2136238722aac81015a1f90830a74ae7
Harinn <prinn.dev@pm.me>
2026-07-18 15:42:03 +0700

    python3Packages.cmdline: migrate to pyproject

--- pkgs/development/python-modules/cmdline/default.nix
+++ pkgs/development/python-modules/cmdline/default.nix
@@ -2,22 +2,25 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   pyyaml,
 }:
 
 buildPythonPackage rec {
   pname = "cmdline";
   version = "0.2.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "7cf6af53549892b2218c2f56a199dff54a733be5c5515c0fd626812070b0a86a";
   };
 
+  build-system = [ setuptools ];
+
   # No tests, https://github.com/rca/cmdline/issues/1
   doCheck = false;
-  propagatedBuildInputs = [ pyyaml ];
+  dependencies = [ pyyaml ];
 
   meta = {
     description = "Utilities for consistent command line tools";
