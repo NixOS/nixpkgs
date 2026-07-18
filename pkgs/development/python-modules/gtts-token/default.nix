9ce95861997fcc437a0d1b061813a3931fc37619
Harinn <prinn.dev@pm.me>
2026-07-19 00:43:57 +0700

    python3Packages.gtts-token: modernize

--- pkgs/development/python-modules/gtts-token/default.nix
+++ pkgs/development/python-modules/gtts-token/default.nix
@@ -7,16 +7,18 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "gtts-token";
   version = "1.1.4";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "boudewijn26";
     repo = "gTTS-token";
-    rev = "v${version}";
-    sha256 = "0vr52zc0jqyfvsccl67j1baims3cdx2is1y2lpx2kav9gadkn8hp";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-FyI7m3ppqyn6pcIHHUVvbOga1QryGMqY3s5jCdgXJW8=";
   };
 
   build-system = [ setuptools ];
@@ -28,10 +30,12 @@ buildPythonPackage rec {
   # requires internet access
   disabledTests = [ "test_real" ];
 
+  pythonImportsCheck = [ "gtts_token" ];
+
   meta = {
     description = "Calculates a token to run the Google Translate text to speech";
     homepage = "https://github.com/boudewijn26/gTTS-token";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ makefu ];
   };
-}
+})
