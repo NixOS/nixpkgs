b75c2d3c4843d1c9a6eb1026a2d4fb26546d2acb
Harinn <prinn.dev@pm.me>
2026-07-19 00:23:17 +0700

    python3Packages.greek-accentuation: migrate to pyproject

--- pkgs/development/python-modules/greek-accentuation/default.nix
+++ pkgs/development/python-modules/greek-accentuation/default.nix
@@ -2,15 +2,18 @@
   buildPythonPackage,
   lib,
   fetchPypi,
+  setuptools,
 }:
 buildPythonPackage rec {
   pname = "greek-accentuation";
   version = "1.2.0";
-  format = "setuptools";
+  pyproject = true;
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-l2HZXdqlLubvy2bWhhZVYGMpF0DXVKTDFehkcGF5xdk=";
   };
+
+  build-system = [ setuptools ];
   meta = {
     description = "Python 3 library for accenting (and analyzing the accentuation of) Ancient Greek words";
     homepage = "https://github.com/jtauber/greek-accentuation";
