5193877807fe4b3003d22e628ad266fe44aac31c
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-19 11:22:49 +0000

    jj-pre-push: 0.5.0 -> 0.5.1

--- pkgs/by-name/jj/jj-pre-push/package.nix
+++ pkgs/by-name/jj/jj-pre-push/package.nix
@@ -11,14 +11,14 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "jj-pre-push";
-  version = "0.5.0";
+  version = "0.5.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "acarapetis";
     repo = "jj-pre-push";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-AIsLUI42ewYqmgOP9livXE0tGAVWTfSh9K7h+PMObJg=";
+    hash = "sha256-Vi50t+AMUgSlUjwBRZPJ+yxfJMQWKDAr21trkqjcuQM=";
   };
 
   postPatch = ''
