d4944e4ff853d70daf51a21421c71905f5cf8b37
ItzEmoji <no-reply@itzemoji.com>
2026-07-19 17:03:34 +0000

    aeroflare: init at 1.11.0

---
+++ pkgs/by-name/ae/aeroflare/package.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  pkgs,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "aeroflare";
+  version = "1.11.0";
+
+  src = fetchFromGitHub {
+    owner = "itzemoji";
+    repo = "aeroflare";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-4l0QVJAgKqS/tnMD6GncQdbefWPncmS4QDIz7QEiPrk=";
+  };
+
+  vendorHash = "sha256-H4jgc08mklolpHQNlcQx5JzpCDBYpujgoKFR2Ct8xR8=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/itzemoji/aeroflare/internal/build.Version=${finalAttrs.version}"
+  ];
+
+  subPackages = [
+    "cmd/aeroflare"
+    "cmd/aeroflare-ci"
+  ];
+  passthru.tests.version =
+    pkgs.runCommand "aeroflare-version"
+      {
+        nativeBuildInputs = [ finalAttrs.finalPackage ];
+      }
+      ''
+        aeroflare version | grep "^aeroflare version ${finalAttrs.version}$"
+        touch "$out"
+      '';
+
+  meta = with lib; {
+    description = "The OCI-based Nix-Binary-Cache written in Go";
+    homepage = "https://github.com/itzemoji/aeroflare";
+    changelog = "https://github.com/itzemoji/aeroflare/blob/${finalAttrs.version}/CHANGELOG.md";
+    platforms = platforms.unix;
+    license = licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ itzemoji ];
+    mainProgram = "aeroflare";
+  };
+})
