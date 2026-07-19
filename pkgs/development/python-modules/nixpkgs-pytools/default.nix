1721a4af200e5b6ddd6819814bb45ce44187950e
bepz <bepz@bepz.dev>
2026-07-19 10:25:56 +0200

    python3Packages.nixpkgs-pytools: use finalAttrs and dependencies

--- pkgs/development/python-modules/nixpkgs-pytools/default.nix
+++ pkgs/development/python-modules/nixpkgs-pytools/default.nix
@@ -7,19 +7,19 @@
   rope,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "nixpkgs-pytools";
   version = "1.3.0";
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     sha256 = "11skcbi1lf9qcv9j5ikifb4pakhbbygqpcmv3390j7gxsa85cn19";
   };
 
   build-system = [ setuptools ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     jinja2
     setuptools
     rope
@@ -34,4 +34,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
