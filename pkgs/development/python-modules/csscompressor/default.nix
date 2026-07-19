a7e2853c9538f694e502248ffcef302780c9f50c
Sigmanificient <edhyjox@gmail.com>
2026-07-20 01:11:29 +0200

    python3Packages.csscompressor: migrate to pyproject

--- pkgs/development/python-modules/csscompressor/default.nix
+++ pkgs/development/python-modules/csscompressor/default.nix
@@ -3,17 +3,20 @@
   buildPythonPackage,
   fetchPypi,
   pytestCheckHook,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "csscompressor";
   version = "0.9.5";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "afa22badbcf3120a4f392e4d22f9fff485c044a1feda4a950ecc5eba9dd31a05";
   };
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
