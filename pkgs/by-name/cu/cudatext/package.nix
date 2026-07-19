8ed12904eb0127ff55e9a1e355ef9c7e8c254f45
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-19 15:28:18 +0000

    cudatext: 1.235.0.3 -> 1.235.1.0

--- pkgs/by-name/cu/cudatext/package.nix
+++ pkgs/by-name/cu/cudatext/package.nix
@@ -42,13 +42,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "cudatext";
-  version = "1.235.0.3";
+  version = "1.235.1.0";
 
   src = fetchFromGitHub {
     owner = "Alexey-T";
     repo = "CudaText";
     tag = finalAttrs.version;
-    hash = "sha256-J8Ht+VPoRrtR9TcNEgsX/3Ci1Z7+SAbeSh9KzGIENvw=";
+    hash = "sha256-9W0DlQfPbSJPFz7+ExbfFeYWc7IKsvvcjY0/3X6j0ys=";
   };
 
   patches = [ ./proc_globdata.patch ];
