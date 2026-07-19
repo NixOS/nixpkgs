40467322ea8be8b1632527da6213d826d4561215
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-19 04:55:05 +0000

    v2ray-rules-dat: 202607092306 -> 202607182241

--- pkgs/by-name/v2/v2ray-rules-dat/package.nix
+++ pkgs/by-name/v2/v2ray-rules-dat/package.nix
@@ -5,7 +5,7 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "v2ray-rules-dat";
-  version = "202607092306";
+  version = "202607182241";
 
   __structuredAttrs = true;
   strictDeps = true;
@@ -25,11 +25,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   passthru = {
     geoipDat = fetchurl {
       url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${finalAttrs.version}/geoip.dat";
-      hash = "sha256-g3l3GfrMCS4hD4+ODl4LC9/gaskKOko9amqy14GpF64=";
+      hash = "sha256-rzMquI60uxXjzRDwP1VC6QZV7kvVvw4jlJz70eRrwg8=";
     };
     geositeDat = fetchurl {
       url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${finalAttrs.version}/geosite.dat";
-      hash = "sha256-E1Vs8u0vkDtgXQNHJejL0Xenl/fYn5IE9npD2M0Js8o=";
+      hash = "sha256-wfz4r43we+6qI4gxMwmHQXIZoPXUV+foqMFVC7GVnj8=";
     };
     updateScript = ./update.sh;
   };
