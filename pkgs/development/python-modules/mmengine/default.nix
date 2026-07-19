1ccbdf7797a6d3fcb7508f3438f54845ad1fc420
Sarah Clark <seclark@nextquestion.net>
2026-07-19 13:20:15 -0700

    python3Packages.mmengine: pin setuptools at 80 to work around pkg_resources error

--- pkgs/development/python-modules/mmengine/default.nix
+++ pkgs/development/python-modules/mmengine/default.nix
@@ -6,7 +6,7 @@
   fetchpatch,
 
   # build-system
-  setuptools,
+  setuptools_80,
 
   # dependencies
   addict,
@@ -71,7 +71,8 @@ buildPythonPackage (finalAttrs: {
         --replace-fail "import numpy.compat" ""
     '';
 
-  build-system = [ setuptools ];
+  # https://github.com/open-mmlab/mmengine/issues/1616
+  build-system = [ setuptools_80 ];
 
   dependencies = [
     addict
