NixOS/nixpkgs

commit 4f1b1fac3d3d16854a998e3f2171f2f42e18dbae

duckdb: work around the excessive logging
https://hydra.nixos.org/build/337747651#tabs-buildsteps

pkgs/by-name/du/duckdb/package.nix
@@ -152,11 +152,13 @@ stdenv.mkDerivation (finalAttrs: {
       );
       LD_LIBRARY_PATH = lib.optionalString stdenv.hostPlatform.isDarwin "DY" + "LD_LIBRARY_PATH";
     in
+    # FIXME: do something about the excessive logging on x86_64-linux (> 250 MiB).
     ''
       runHook preInstallCheck
       (($(ulimit -n) < 1024)) && ulimit -n 1024
 
-      HOME="$(mktemp -d)" ${LD_LIBRARY_PATH}="$lib/lib" ./test/unittest ${toString excludes}
+      HOME="$(mktemp -d)" ${LD_LIBRARY_PATH}="$lib/lib" ./test/unittest ${toString excludes}${
+        lib.optionalString stdenv.hostPlatform.isx86_64 " >/dev/null" }
 
       runHook postInstallCheck
     '';

commit 7197da8faaf7a3bf729a787e7f5c65c3f5ccd789

duckdb: fix treefmt
regressed in 4f1b1fac3d3d16854a998e3f2171f2f42e18dbae.

pkgs/by-name/du/duckdb/package.nix
@@ -157,8 +157,7 @@ stdenv.mkDerivation (finalAttrs: {
       runHook preInstallCheck
       (($(ulimit -n) < 1024)) && ulimit -n 1024
 
-      HOME="$(mktemp -d)" ${LD_LIBRARY_PATH}="$lib/lib" ./test/unittest ${toString excludes}${
-        lib.optionalString stdenv.hostPlatform.isx86_64 " >/dev/null" }
+      HOME="$(mktemp -d)" ${LD_LIBRARY_PATH}="$lib/lib" ./test/unittest ${toString excludes}${lib.optionalString stdenv.hostPlatform.isx86_64 " >/dev/null"}
 
       runHook postInstallCheck
     '';