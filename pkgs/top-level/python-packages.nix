619132a43cd9dbb07555818932f8a27280497662
Stefan Frijters <sfrijters@gmail.com>
2026-07-21 00:54:56 +0200

    python3Packages.ttconv, ttconv: init at 1.2.2
    This package can be used as a library (https://github.com/sandflow/ttconv#library),
    but it also contains an executable for standalone use.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20774,6 +20774,8 @@ self: super: with self; {
 
   ttach = callPackage ../development/python-modules/ttach { };
 
+  ttconv = callPackage ../development/python-modules/ttconv { };
+
   ttfautohint-py = callPackage ../development/python-modules/ttfautohint-py { };
 
   ttkbootstrap = callPackage ../development/python-modules/ttkbootstrap { };
