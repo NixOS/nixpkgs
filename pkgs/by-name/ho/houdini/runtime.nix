c12a02d1036cb23375f717c3bbc8abf718f6a93b
permahorse <13556996+permahorse@users.noreply.github.com>
2026-07-20 09:58:25 +0200

    houdini: 21.0.559 -> 22.0.368

--- pkgs/by-name/ho/houdini/runtime.nix
+++ pkgs/by-name/ho/houdini/runtime.nix
@@ -1,12 +1,12 @@
 { requireFile, callPackage }:
 
 callPackage ./runtime-build.nix rec {
-  version = "21.0.559";
+  version = "22.0.368";
   eulaDate = "2021-10-13";
   src = requireFile {
-    name = "houdini-${version}-linux_x86_64_gcc11.2.tar.gz";
-    hash = "sha256-bZmoH1NKQhhMAhIl3pTL7irUZ7HrOhS8R7GApLD5514=";
+    name = "houdini-${version}-linux_x86_64_gcc14.2.tar.gz";
+    hash = "sha256-h2UzXwkKgyl2i0FbZLyfuAoNmWOxP2NFWtBC4y01NhY=";
     url = "https://www.sidefx.com/download/daily-builds/?show_launcher=false&production=true&linux=true";
   };
-  outputHash = "sha256-/7ctlMUoyJdPdBQV7rRO9pWcg9bXcnMJsB9TN/Jo8QQ=";
+  outputHash = "sha256-sJwKbkmHykgyP6yio5W8FVxLWm2S0PEQz5RVRF3cbfI=";
 }
