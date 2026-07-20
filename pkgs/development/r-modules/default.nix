9bc8a2ef64432e0b4ac53cc61d12d4fec1725058
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-07-20 22:58:00 +0200

    rPackages.{automerge,libipldr}: fix build

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -555,6 +555,11 @@ let
       cargo
       rustc
     ];
+    automerge = with pkgs; [
+      cargo
+      cmake
+      rustc
+    ];
     awdb = with pkgs; [
       cargo
       rustc
@@ -709,6 +714,10 @@ let
     libdeflate = with pkgs; [
       cmake
       pkg-config
+    ];
+    libipldr = with pkgs; [
+      cargo
+      rustc
     ];
     lnmixsurv = [ pkgs.gsl ]; # for gsl-config
     lpsymphony = with pkgs; [
