c3576a51da92def325b8188c1071990644cdf450
klea <klea@iwnp.org>
2026-07-20 20:59:25 +0000

    pleroma-bot: 0.8.6 -> 1.2.0

--- pkgs/development/python-modules/pleroma-bot/default.nix
+++ pkgs/development/python-modules/pleroma-bot/default.nix
@@ -9,11 +9,15 @@
   requests,
   pyaml,
   setuptools,
+  feedparser,
+  beautifulsoup4,
+  tqdm,
+  python,
 }:
 
 buildPythonPackage (finalAttrs: {
   pname = "pleroma-bot";
-  version = "0.8.6";
+  version = "1.2.0";
   pyproject = true;
   build-system = [ setuptools ];
   __structuredAttrs = true;
@@ -22,7 +26,7 @@ buildPythonPackage (finalAttrs: {
     owner = "robertoszek";
     repo = "pleroma-bot";
     tag = finalAttrs.version;
-    hash = "sha256-vJxblpf3NMSyYMHeWG7vHP5AeluTtMtVxOsHgvGDHeA=";
+    hash = "sha256-my42szvkiUg6vGG6yGjDqN5LlDaertj+yA/pIcjdaLk=";
   };
 
   dependencies = [
@@ -30,11 +34,25 @@ buildPythonPackage (finalAttrs: {
     requests
     requests-oauthlib
     oauthlib
+    feedparser
+    beautifulsoup4
+    tqdm
   ];
+
+  postPatch = ''
+    substituteInPlace pleroma_bot/tests/test_{exceptions,logger}.py --replace-fail 'return' 'assert'
+  '';
 
   nativeCheckInputs = [
     pytestCheckHook
     requests-mock
+  ];
+
+  disabledTestPaths = [
+    # Tries to connect to api.twitter.com
+    "pleroma_bot/tests/test_run.py::test_tweet_order"
+    # Tries to connect to api.twitter.com and pbs.twimg.com
+    "pleroma_bot/tests/test_run.py::test_main"
   ];
 
   pythonImportsCheck = [ "pleroma_bot" ];
@@ -45,5 +63,6 @@ buildPythonPackage (finalAttrs: {
     homepage = "https://robertoszek.github.io/pleroma-bot/";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ robertoszek ];
+    broken = python.pythonAtLeast "3.15";
   };
 })
