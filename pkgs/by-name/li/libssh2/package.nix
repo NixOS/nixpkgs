f7d9866fbe4f46048aaab0761a20b49aa6023e6d
whispers <whispersofthedawn@duck.com>
2026-07-21 09:39:16 -0400

    libssh2: remove SuperSandro2000 as maintainer
    https://github.com/NixOS/nixpkgs/pull/543937#pullrequestreview-4745170669
    (cherry picked from commit 19c05bc5b549b436bd25fcd136d3af9d0bc964dc)

--- pkgs/by-name/li/libssh2/package.nix
+++ pkgs/by-name/li/libssh2/package.nix
@@ -95,6 +95,5 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://www.libssh2.org";
     platforms = lib.platforms.all;
     license = with lib.licenses; [ bsd3 ];
-    maintainers = with lib.maintainers; [ SuperSandro2000 ];
   };
 })
