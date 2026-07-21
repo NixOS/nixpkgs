693a17c70b430fad75ad1b1d1dadf5769c4b516a
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-21 16:03:23 +0000

    gvm-libs: 23.8.0 -> 23.9.0

--- pkgs/by-name/gv/gvm-libs/package.nix
+++ pkgs/by-name/gv/gvm-libs/package.nix
@@ -26,13 +26,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gvm-libs";
-  version = "23.8.0";
+  version = "23.9.0";
 
   src = fetchFromGitHub {
     owner = "greenbone";
     repo = "gvm-libs";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+fd/f5bYjKaeDGj0cpe4vNqm6TvIcWxJ3pfy90oQkbI=";
+    hash = "sha256-oGV4Brb+LgxiflzAvv7HqQ0p9+Whe2J6du9wBKAkr5Q=";
   };
 
   postPatch = ''
