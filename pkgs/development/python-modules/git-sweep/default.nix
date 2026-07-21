afb5ec1ff1458d4cda8834c957c19dd28bb4a4b6
Sigmanificient <edhyjox@gmail.com>
2026-07-22 00:22:53 +0200

    python3Package.git-sweep: migrate to pyproject

--- pkgs/development/python-modules/git-sweep/default.nix
+++ pkgs/development/python-modules/git-sweep/default.nix
@@ -3,19 +3,22 @@
   buildPythonPackage,
   fetchPypi,
   gitpython,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "git-sweep";
   version = "0.1.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-zSnxw3JHsFru9fOZSJZX+XOu144uJ0DaIKYlAtoHV7M=";
   };
 
-  propagatedBuildInputs = [ gitpython ];
+  build-system = [ setuptools ];
+
+  dependencies = [ gitpython ];
 
   # Module has no tests
   doCheck = false;
