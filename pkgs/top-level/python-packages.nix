0a3fbc757fb12f15177c8faf2ac2f0c2640e340c
Gautier DI FOLCO <foss@difolco.dev>
2026-07-21 21:02:43 +0200

    python3Packages.hindsight-api: init at 0.8.4
    Adds the hindsight-api meta-distribution, which propagates hindsight-api-slim (core) and exposes the hindsight-api server command. The upstream [all] extra pin is relaxed since the local-ML/embedded-DB extras are not packaged.
    
    Assisted-by: Z.Ai/GLM

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7647,6 +7647,8 @@ self: super: with self; {
 
   hikvision = callPackage ../development/python-modules/hikvision { };
 
+  hindsight-api = callPackage ../development/python-modules/hindsight-api { };
+
   hindsight-api-slim = callPackage ../development/python-modules/hindsight-api-slim { };
 
   hiredis = callPackage ../development/python-modules/hiredis { };
