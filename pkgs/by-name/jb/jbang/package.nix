45b440cd7b9899a112932398397d3e34b7d841ea
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-21 17:45:04 +0000

    jbang: 0.136.0 -> 0.141.0

--- pkgs/by-name/jb/jbang/package.nix
+++ pkgs/by-name/jb/jbang/package.nix
@@ -10,7 +10,7 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "0.136.0";
+  version = "0.141.0";
   pname = "jbang";
 
   __structuredAttrs = true;
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
 
   src = fetchzip {
     url = "https://github.com/jbangdev/jbang/releases/download/v${version}/${pname}-${version}.tar";
-    sha256 = "sha256-MsP4iLquOwJWlV7EPxSuAPWuyTv1PPSyQCrVdq4lPlM=";
+    sha256 = "sha256-fw87WrmqdVH+NJ+rAnuMukz7qMTRg6CMgLVg/+JPahI=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
