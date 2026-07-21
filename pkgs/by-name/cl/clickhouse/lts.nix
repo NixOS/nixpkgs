136d2fb64f952dc532a8cc700fea5b3eb4d57fa3
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-21 06:48:14 +0000

    clickhouse-lts: 26.3.17.4-lts -> 26.3.17.56-lts

--- pkgs/by-name/cl/clickhouse/lts.nix
+++ pkgs/by-name/cl/clickhouse/lts.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
-  version = "26.3.17.4-lts";
-  rev = "00f5ad7bc29d8bb65e1f7eb24208da15db3cf642";
-  hash = "sha256-RJSqcsn3ZOL145RTUs4QujPm6DHjDSYiL9jI1C1YCVE=";
+  version = "26.3.17.56-lts";
+  rev = "c57540de480d8a501b601163471d3843674378cf";
+  hash = "sha256-fcRxZbcp7t28bC9vlmgVAV9gB63cQwFWsc0gyKxGhvQ=";
   lts = true;
 }
