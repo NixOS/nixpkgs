43498fa58957a64b509fb90d291cb80754a77ed9
Infinidoge <infinidoge@inx.moe>
2026-07-22 17:25:56 -0400

    xonsh.xontribs.xontrib-fish-completer: 0.0.1 -> 0.0.3

--- pkgs/by-name/xo/xonsh/xontribs/xontrib-fish-completer/default.nix
+++ pkgs/by-name/xo/xonsh/xontribs/xontrib-fish-completer/default.nix
@@ -13,20 +13,15 @@
 
 buildPythonPackage rec {
   pname = "xontrib-fish-completer";
-  version = "0.0.1";
+  version = "0.0.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "xonsh";
     repo = "xontrib-fish-completer";
     tag = version;
-    hash = "sha256-PhhdZ3iLPDEIG9uDeR5ctJ9zz2+YORHBhbsiLrJckyA=";
+    hash = "sha256-9S0Gj1CQxuX1mGL1+4Xyyld/NHUYUi7DJ424lUlExVc=";
   };
-
-  prePatch = ''
-    substituteInPlace pyproject.toml \
-      --replace-fail '"xonsh>=0.12.5"' ""
-  '';
 
   build-system = [
     setuptools
