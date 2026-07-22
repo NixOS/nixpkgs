5d4dccf4149116534ee0ffec82c07239e8142506
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-22 11:27:22 +0000

    rattler-build: 0.68.0 -> 0.70.0

--- pkgs/by-name/ra/rattler-build/package.nix
+++ pkgs/by-name/ra/rattler-build/package.nix
@@ -14,16 +14,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rattler-build";
-  version = "0.68.0";
+  version = "0.70.0";
 
   src = fetchFromGitHub {
     owner = "prefix-dev";
     repo = "rattler-build";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dS+PLxG5HfORy3hCEipS1rCoqCttHcqwsvIXwE5lQ/w=";
+    hash = "sha256-kvcxBrP6vzbhtzgFqMfTZwQlCJ6wnaAYbCNLotGQnK4=";
   };
 
-  cargoHash = "sha256-uJjS0dE89XRy1irTZv1piH2Mm9/CqeNuUJl8Ntc4V8c=";
+  cargoHash = "sha256-UoZqxpcLZqXxZYTfLIaqMa/1gkIn9Ntmk2qsnaTmV7s=";
 
   doCheck = false; # test requires network access
 
