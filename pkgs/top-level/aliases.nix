6597b98a22794a5101eda1c81706670beab37ea7
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-22 18:45:58 +0200

    zuki-themes: remove
    Depends on gtk-engine-murrine, which is being removed from nixpkgs.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2943,6 +2943,7 @@ mapAliases {
   zq = throw "zq has been replaced by zed"; # Converted to throw 2025-10-26
   zscroll = throw "zscroll has been removed due to lack of maintenance."; # Added 2026-05-29
   zsh-git-prompt = throw "zsh-git-prompt was removed as it is unmaintained upstream"; # Added 2025-08-28
+  zuki-themes = throw "'zuki-themes' has been removed because it depended on 'gtk-engine-murrine', which was removed because it was unmaintained upstream and depended on GTK 2."; # Added 2026-07-22
   zulu23 = throw "Zulu OpenJDK 23 was removed as it has reached its end of life"; # Added 2025-11-14
   zulu24 = throw "Zulu OpenJDK 24 was removed as it has reached its end of life"; # Added 2025-11-14
   zyn-fusion = throw "'zyn-fusion' has been renamed to/replaced by 'zynaddsubfx'"; # Converted to throw 2025-10-27
