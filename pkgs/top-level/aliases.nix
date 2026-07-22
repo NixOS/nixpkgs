4d4196eae193366141dab2fc99331df07aeb8267
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-22 18:44:32 +0200

    ant-theme: remove
    Depends on gtk-engine-murrine, which is being removed from nixpkgs.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -311,6 +311,7 @@ mapAliases {
   ansible_2_19 = throw "ansible_2_19 has been removed; use overridePythonAttrs if you need a specific version"; # Added 2025-11-10
   ant-bloody-theme = throw "'ant-bloody-theme' has been removed because it depended on 'gtk-engine-murrine', which was removed because it was unmaintained upstream and depended on GTK 2."; # Added 2026-07-22
   ant-nebula-theme = throw "'ant-nebula-theme' has been removed because it depended on 'gtk-engine-murrine', which was removed because it was unmaintained upstream and depended on GTK 2."; # Added 2026-07-22
+  ant-theme = throw "'ant-theme' has been removed because it depended on 'gtk-engine-murrine', which was removed because it was unmaintained upstream and depended on GTK 2."; # Added 2026-07-22
   antibody = throw "antibody has been removed because it was deprecated and archived upstream. Consider using antidote instead"; # Added 2026-01-16
   antigravity = warnAlias "'antigravity' has been renamed to 'antigravity-ide'" antigravity-ide; # Added 2026-05-20
   antigravity-fhs = warnAlias "'antigravity-fhs' has been renamed to 'antigravity-ide-fhs'" antigravity-ide-fhs; # Added 2026-05-20
