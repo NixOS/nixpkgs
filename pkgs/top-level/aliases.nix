9d8d039fa42b408dd8c2eaad2732a8bf2a0ef175
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-22 18:45:25 +0200

    numix-gtk-theme: remove
    Depends on gtk-engine-murrine, which is being removed from nixpkgs.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1768,6 +1768,7 @@ mapAliases {
   nrpl = throw "'nrpl' has been removed as it depends on pcre, which is deprecated"; # Added 2026-06-25
   NSPlist = throw "'NSPlist' has been renamed to/replaced by 'nsplist'"; # Converted to throw 2025-10-27
   nuget-to-nix = throw "nuget-to-nix has been removed as it was deprecated in favor of nuget-to-json. Please use nuget-to-json instead"; # Added 2025-08-28
+  numix-gtk-theme = throw "'numix-gtk-theme' has been removed because it depended on 'gtk-engine-murrine', which was removed because it was unmaintained upstream and depended on GTK 2."; # Added 2026-07-22
   nushellFull = throw "'nushellFull' has been renamed to/replaced by 'nushell'"; # Converted to throw 2025-10-27
   nuXmv = warnAlias "'nuXmv' has been renamed to 'nuxmv'" nuxmv; # Added 2026-02-08
   o = throw "'o' has been renamed to/replaced by 'orbiton'"; # Converted to throw 2025-10-27
