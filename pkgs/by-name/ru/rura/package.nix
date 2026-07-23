951ecec0424533530b31dbc4140f14e00946eff0
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-23 16:29:08 +0000

    rura: 1.9.0 -> 1.10.0

--- pkgs/by-name/ru/rura/package.nix
+++ pkgs/by-name/ru/rura/package.nix
@@ -8,17 +8,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rura";
-  version = "1.9.0";
+  version = "1.10.0";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "tlipinski";
     repo = "rura";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+XfJc9FH9El36AL7s7wMy9TNILKvtYflRDZjeW9J3fg=";
+    hash = "sha256-yxYTBXGFX2PdtzgYexnXyjNMcDTnfwsy2s8S0y4hp+M=";
   };
 
-  cargoHash = "sha256-HBcKuQjRqVBItgIVUyNQiqfZxNVtyQgwr+5mTrlV3eM=";
+  cargoHash = "sha256-FnLeyUUwQFWLNLLQX538LHIlzwNRg+JfAW9r3IiBnWg=";
 
   passthru = {
     updateScript = nix-update-script { };
