542f87fb4b7fd09a28d1e80da472e39a37fc5d28
whispers <whispersofthedawn@duck.com>
2026-07-23 08:40:57 -0400

    gnome2.ORBit2: drop
    ORBit2 was archived upstream after being last touched in July 2016. It
    is also now unused in Nixpkgs.

--- pkgs/desktops/gnome-2/default.nix
+++ pkgs/desktops/gnome-2/default.nix
@@ -14,8 +14,6 @@ lib.makeScope pkgs.newScope (
       gettext = if stdenv.hostPlatform.isDarwin then pkgs.gettext else null;
     };
 
-    ORBit2 = callPackage ./platform/ORBit2 { };
-
     libart_lgpl = callPackage ./platform/libart_lgpl { };
 
     libglade = callPackage ./platform/libglade { };
@@ -75,5 +73,6 @@ lib.makeScope pkgs.newScope (
   libgnome = throw "gnome2.libgnome has been removed"; # 2024-06-27
   libgnomeui = throw "gnome2.libgnomeui has been removed"; # 2024-06-27
   libgtkhtml = throw "gnome2.libgtkhtml has been removed"; # 2023-01-15
+  ORBit2 = throw "gnome2.ORBit2 has been removed as it has been archived upstream since July 2016"; # 2026-07-23
   python_rsvg = throw "gnome2.python_rsvg has been removed"; # 2023-01-14
 }
