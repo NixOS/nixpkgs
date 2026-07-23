9d0df86e63e581f9db4fa307484a59a5e609c6a7
Jan van Brügge <supermanitu@gmail.com>
2026-07-23 11:33:30 +0200

    python3Packages.audible: 0.10.0 -> 0.12.0

--- pkgs/development/python-modules/audible/default.nix
+++ pkgs/development/python-modules/audible/default.nix
@@ -4,7 +4,7 @@
   buildPythonPackage,
 
   # build-system
-  poetry-core,
+  hatchling,
 
   # dependencies
   beautifulsoup4,
@@ -13,26 +13,33 @@
   pillow,
   pyaes,
   rsa,
+  cryptography,
+  pycryptodome,
+  orjson,
+  ujson,
+  python-rapidjson,
 
   # test dependencies
   pytestCheckHook,
+  git,
+  git-cliff,
 }:
 
 buildPythonPackage rec {
   pname = "audible";
-  version = "0.10.0";
+  version = "0.12.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mkb79";
     repo = "Audible";
     tag = "v${version}";
-    hash = "sha256-ILGhjuPIxpRxu/dVDmz531FUgMWosk4P+onPJltuPIs=";
+    hash = "sha256-N13R6HQyfd3Q2cBvxjnVzCXtoW9JE54IMqBX5qgeteI=";
   };
 
-  nativeBuildInputs = [ poetry-core ];
+  build-system = [ hatchling ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     pillow
     beautifulsoup4
     httpx
@@ -41,7 +48,24 @@ buildPythonPackage rec {
     rsa
   ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  optional-dependencies = {
+    cryptography = [ cryptography ];
+    pycryptodome = [ pycryptodome ];
+    orjson = [ orjson ];
+    ujson = [ ujson ];
+    rapidjson = [ python-rapidjson ];
+    json-fast = [ orjson ];
+    json-full = [
+      orjson
+      ujson
+    ];
+  };
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    git
+    git-cliff
+  ];
 
   pythonImportsCheck = [ "audible" ];
 
