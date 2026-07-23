7aae1ad573f7ccd1457aa64b67aa6b05c49c6cb5
Harinn <prinn.dev@pm.me>
2026-07-24 04:21:14 +0700

    python3Packages.translationstring: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/translationstring/default.nix
+++ pkgs/development/python-modules/translationstring/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "translationstring";
   version = "1.4";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "bf947538d76e69ba12ab17283b10355a9ecfbc078e6123443f43f2107f6376f3";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     homepage = "https://pylonsproject.org/";
