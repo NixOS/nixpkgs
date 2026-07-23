0ac490ec71cea78f77f76d8b9d84c67e57c34455
whispers <whispersofthedawn@duck.com>
2026-07-23 08:40:57 -0400

    gnome2.libgnomecanvas: drop
    libgnomecanvas has been archived upstram as it was last touched in April
    2012. It also depends on the deprecated GTK2 engine, which is being
    removed from Nixpkgs.

--- pkgs/desktops/gnome-2/default.nix
+++ pkgs/desktops/gnome-2/default.nix
@@ -14,8 +14,6 @@ lib.makeScope pkgs.newScope (
 
     libglade = callPackage ./platform/libglade { };
 
-    libgnomecanvas = callPackage ./platform/libgnomecanvas { };
-
     gtkglext = callPackage ./platform/gtkglext { };
 
     #### DESKTOP
@@ -62,6 +60,7 @@ lib.makeScope pkgs.newScope (
   libbonobo = throw "gnome2.libbonobo has been removed"; # 2024-06-27
   libbonoboui = throw "gnome2.libbonoboui has been removed"; # 2024-06-27
   libglademm = throw "gnome2.libglademm has been removed"; # 2022-01-15
+  libgnomecanvas = throw "gnome2.libgnomecanvas has been removed as it has been archived upstream since April 2012"; # 2026-07-23
   libgnomecanvasmm = throw "gnome2.libgnomecanvasmm has been removed"; # 2022-01-15
   libgnomecups = throw "gnome2.libgnomecups has been removed"; # 2023-01-15
   libgnomeprint = throw "gnome2.libgnomeprint has been removed"; # 2023-01-15
