91143144f9fe5a647b15030aa70f1e3675a9e7c9
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-23 20:36:57 +0000

    android-studio: 2026.1.2.10 -> 2026.1.2.11

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -16,9 +16,9 @@ let
       inherit tiling_wm;
     };
   stableVersion = {
-    version = "2026.1.2.10"; # "Android Studio Quail 2 | 2026.1.2"
-    sha256Hash = "sha256-ZERaVAkucFbG638aia0RbQ/uwu9flluOWU1iq9tYWQ8=";
-    url = "https://edgedl.me.gvt1.com/android/studio/ide-zips/2026.1.2.10/android-studio-quail2-linux.tar.gz";
+    version = "2026.1.2.11"; # "Android Studio Quail 2 | 2026.1.2 Patch 1"
+    sha256Hash = "sha256-HMnNVPQA27DQeITw98DKtSTbXOCl73CyOu1v3TzURbk=";
+    url = "https://edgedl.me.gvt1.com/android/studio/ide-zips/2026.1.2.11/android-studio-quail2-patch1-linux.tar.gz";
   };
   betaVersion = {
     version = "2026.1.3.5"; # "Android Studio Quail 3 | 2026.1.3 RC 1"
