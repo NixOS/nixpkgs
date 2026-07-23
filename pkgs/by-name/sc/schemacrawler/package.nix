74e04cbee62e1bd20b0e3a6f218b4211f7d511f0
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-23 06:46:35 +0000

    schemacrawler: 17.11.4 -> 17.12.2

--- pkgs/by-name/sc/schemacrawler/package.nix
+++ pkgs/by-name/sc/schemacrawler/package.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "schemacrawler";
-  version = "17.11.4";
+  version = "17.12.2";
 
   src = fetchzip {
     url = "https://github.com/schemacrawler/SchemaCrawler/releases/download/v${finalAttrs.version}/schemacrawler-${finalAttrs.version}-bin.zip";
-    hash = "sha256-BiTGq5tGQZTglSQlssdHw7AuawwNdO1qNDPvYHLb2aw=";
+    hash = "sha256-njv/jhB9yc761MTH9aTjsQslwkqJUH417VhTYVPbpnk=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
