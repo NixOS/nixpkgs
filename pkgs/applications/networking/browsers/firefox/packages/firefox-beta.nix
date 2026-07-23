9e95daf299eb43606dbbbec62b93b6e5bfbf0944
Tol Wassman <tol@tol.sh>
2026-07-23 21:52:37 +0000

    firefox-beta-unwrapped: 153.0b13 -> 154.0b1

--- pkgs/applications/networking/browsers/firefox/packages/firefox-beta.nix
+++ pkgs/applications/networking/browsers/firefox/packages/firefox-beta.nix
@@ -10,11 +10,11 @@
 buildMozillaMach rec {
   pname = "firefox-beta";
   binaryName = "firefox-beta";
-  version = "153.0b13";
+  version = "154.0b1";
   applicationName = "Firefox Beta";
   src = fetchurl {
     url = "mirror://mozilla/firefox/releases/${version}/source/firefox-${version}.source.tar.xz";
-    sha512 = "fecfb1837d1907d54ed52bc40113804b6637352c2cbe6eb906f950d7749cbece32446b42c392102fa9e16591949619237dc2dbeb13bec18697ddd42e9ce8d4dd";
+    sha512 = "1139638ee19ddd3a91c94c3bf256e80304f098a7ca3b016ba43c3d490588d5ab79d9a7c50988c3b04269f1a7832e02a556b194cd86a77c4eaefa61a9b152bdf2";
   };
 
   meta = {
