7f3dbb5e3ffe846bc9d7584318203052fc822f0a
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-23 15:59:06 +0000

    git-credential-gmail: 5.6.2 -> 5.9.4

--- pkgs/by-name/gi/git-credential-email/package.nix
+++ pkgs/by-name/gi/git-credential-email/package.nix
@@ -26,13 +26,13 @@ let
 in
 python3Packages.buildPythonApplication (finalAttrs: {
   inherit pname;
-  version = "5.6.2";
+  version = "5.9.4";
 
   src = fetchFromGitHub {
     owner = "AdityaGarg8";
     repo = "git-credential-email";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-pW13tOPOyS5EorR1C/WEpJpu2ilCA4s8N7GkXoyPv7U=";
+    hash = "sha256-HIWqhPhI9SM0lTzqUXpPC6V9+nVFWK/p04gp8zuIEVk=";
   };
 
   dependencies =
