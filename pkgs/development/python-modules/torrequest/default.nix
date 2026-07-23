3758549ff129927cf457e67d23c9f651d3a0b515
Harinn <prinn.dev@pm.me>
2026-07-24 04:15:54 +0700

    python3Packages.torrequest: modernize

--- pkgs/development/python-modules/torrequest/default.nix
+++ pkgs/development/python-modules/torrequest/default.nix
@@ -8,13 +8,15 @@
   stem,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "torrequest";
   version = "0.1.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-N0XU6j/9qY16A0Njx4ets3qrd72rQAlKTZNzks1NroI=";
   };
 
@@ -37,4 +39,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ applePrincess ];
   };
-}
+})
