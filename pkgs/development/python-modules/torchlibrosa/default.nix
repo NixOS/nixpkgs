b90835d2ed44c50d8f7580579568f7e44b1e27ad
Harinn <prinn.dev@pm.me>
2026-07-24 04:17:31 +0700

    python3Packages.torchlibrosa: modernize

--- pkgs/development/python-modules/torchlibrosa/default.nix
+++ pkgs/development/python-modules/torchlibrosa/default.nix
@@ -8,13 +8,15 @@
   torch,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "torchlibrosa";
   version = "0.1.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-Yqi+7fnJtBQaBiNN8/ECKfe6huZ2eMzuAkiexO8EQCg=";
   };
 
@@ -41,4 +43,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ azuwis ];
   };
-}
+})
