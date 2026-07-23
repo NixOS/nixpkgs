cb4304f71d1d9013df0feda5cbcbce0aaf52cbfb
Pierre Roux <pierre.roux@onera.fr>
2026-07-23 09:07:23 +0200

    rocq-core_9_3: init at 9.3+rc1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10393,6 +10393,8 @@ with pkgs;
     rocq-core_9_1
     rocqPackages_9_2
     rocq-core_9_2
+    rocqPackages_9_3
+    rocq-core_9_3
     rocqPackages
     rocq-core
     ;
@@ -10410,6 +10412,7 @@ with pkgs;
         rocqPackages_9_0
         rocqPackages_9_1
         rocqPackages_9_2
+        rocqPackages_9_3
         rocqPackages
         ;
     })
@@ -10448,6 +10451,8 @@ with pkgs;
     coq_9_1
     coqPackages_9_2
     coq_9_2
+    coqPackages_9_3
+    coq_9_3
     coqPackages
     coq
     ;
