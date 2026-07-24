f06fb2a37347cb020e561f76602262f47c79a104
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-07-24 16:04:46 +0200

    easycrypt: 2026.06 → 2026.07

--- pkgs/applications/science/logic/easycrypt/default.nix
+++ pkgs/applications/science/logic/easycrypt/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "easycrypt";
-  version = "2026.06";
+  version = "2026.07";
 
   src = fetchFromGitHub {
     owner = "easycrypt";
     repo = "easycrypt";
     tag = "r${finalAttrs.version}";
-    hash = "sha256-+exP4UWfNGZauznLZTA/NkMOHJNstz4oaTqI0bSnkH8=";
+    hash = "sha256-ZJRvMdIv75BcU9r8kJdOF7XtTL5dFNycDMSnZjuSp3I=";
   };
 
   nativeBuildInputs =
@@ -33,6 +33,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = with ocamlPackages; [
     batteries
+    bitwuzla-cxx
     dune-build-info
     dune-site
     markdown
