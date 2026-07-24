c7d3cb32ce19a6c1202e43ca6933c06090cbf247
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-24 05:50:05 +0000

    armadillo: 15.2.3 -> 15.4.2

--- pkgs/by-name/ar/armadillo/package.nix
+++ pkgs/by-name/ar/armadillo/package.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "armadillo";
-  version = "15.2.3";
+  version = "15.4.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/arma/armadillo-${finalAttrs.version}.tar.xz";
-    hash = "sha256-AYLWfWlJ5DR6C8YvyMJ5O+frIDxx8Z7f+T+MRf1KgZA=";
+    hash = "sha256-WPoljQ4fkwdMmvp5ICf8JZuFy5dMzAuTKwYdStpb2Do=";
   };
 
   nativeBuildInputs = [ cmake ];
