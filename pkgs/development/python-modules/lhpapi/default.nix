6b38ccd02926b80e3377702707bdf115bc70e81f
9R <git@9-r.net>
2026-07-24 18:41:50 +0200

    lhpapi: 1.0.10 -> 1.0.11 release note: https://github.com/stephan192/lhpapi/releases/tag/v1.0.11

--- pkgs/development/python-modules/lhpapi/default.nix
+++ pkgs/development/python-modules/lhpapi/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "lhpapi";
-  version = "1.0.10";
+  version = "1.0.11";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "stephan192";
     repo = "lhpapi";
     tag = "v${version}";
-    hash = "sha256-Q9X1STaWWbWWy8wmCQ3Cx+z19+X3EcGl0u/Mmc49rAM=";
+    hash = "sha256-tz1/+NTg8wI0mWcPfM1zk7EheUwShv8eCoYA7wAb/lM=";
   };
 
   dependencies = [
