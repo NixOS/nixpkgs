c8b84918f0febda9437d07d17451d83a66da000f
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-24 11:13:26 +0000

    python3Packages.quixote: 3.7 -> 3.8

--- pkgs/development/python-modules/quixote/default.nix
+++ pkgs/development/python-modules/quixote/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "quixote";
-  version = "3.7";
+  version = "3.8";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-F4u50xz6sNwBIzgEglVnwKTKxguE6f1m9Y2DAUEJsGQ=";
+    hash = "sha256-+7RMcIG4J8CwyqY37BCaQNJlXgHTAYCda64IAMEdRjQ=";
   };
 
   build-system = [ setuptools ];
