63a0f8d361551d3c4d2dedf40694cb52e2da3830
Doron Behar <doron.behar@gmail.com>
2026-07-24 09:30:55 +0300

    gnuradioPackages.osmosdr: add airspyhf package

--- pkgs/development/gnuradio-modules/osmosdr/default.nix
+++ pkgs/development/gnuradio-modules/osmosdr/default.nix
@@ -22,6 +22,7 @@
   uhd,
   icu,
   airspy,
+  airspyhf,
   hackrf,
   libbladeRF,
   rtl-sdr,
@@ -95,6 +96,7 @@ mkDerivation (finalAttrs: {
       # Other features don't have dependencies but can still be disabled in the
       # `features` argument.
       airspy = [ airspy ];
+      airspyhf = [ airspyhf ];
       bladerf = [ libbladeRF ];
       hackrf = [ hackrf ];
       rtl = [ rtl-sdr ];
