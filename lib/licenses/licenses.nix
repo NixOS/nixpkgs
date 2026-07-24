eae033c26edfbea68a980db78ce304f660a4731e
Frozen Moon <120668955+FrozenOverTheMoon@users.noreply.github.com>
2026-07-24 15:38:30 -0400

    Update licenses.nix polyFormShield100 name fix

--- lib/licenses/licenses.nix
+++ lib/licenses/licenses.nix
@@ -1309,7 +1309,7 @@ lib.mapAttrs mkLicense (
       fullName = "PHP License v3.01";
     };
 
-    polyFormShieldd100 = {
+    polyFormShield100 = {
       shortName = "polyform-shield-1.0.0";
       fullName = "PolyForm Shield License 1.0.0";
       url = "https://raw.githubusercontent.com/mak-kirkland/chronicler/v0.56.3-alpha/LICENSE";
