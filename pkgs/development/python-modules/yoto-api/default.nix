109e168b031f5f327f984bd6106389ec85f9a27f
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-25 00:59:10 +0200

    python3Packages.yoto-api: 4.3.1 -> 4.3.2
    https://github.com/cdnninja/yoto_api/releases/tag/v4.3.2

--- pkgs/development/python-modules/yoto-api/default.nix
+++ pkgs/development/python-modules/yoto-api/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "yoto-api";
-  version = "4.3.1";
+  version = "4.3.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "cdnninja";
     repo = "yoto_api";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Hy2OE8jol/ttZ1MsIC4EzkYa72DINwcjsHflo8+a7xo=";
+    hash = "sha256-BJK1kTdiEfBoMW6WhpWc1S52VFIrffU9M0aYtJfvIXs=";
   };
 
   build-system = [ setuptools ];
