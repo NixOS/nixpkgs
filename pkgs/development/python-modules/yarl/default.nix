7f6b809aa5cda12b67d54bfcbfbc2a7c278d6f85
Robert Schütz <nix@dotlambda.de>
2026-07-24 11:50:11 -0700

    python3Packages.yarl: 1.24.2 -> 1.24.5
    Diff: https://github.com/aio-libs/yarl/compare/v1.24.2...v1.24.5
    
    Changelog: https://github.com/aio-libs/yarl/blob/v1.24.5/CHANGES.rst

--- pkgs/development/python-modules/yarl/default.nix
+++ pkgs/development/python-modules/yarl/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "yarl";
-  version = "1.24.2";
+  version = "1.24.5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "aio-libs";
     repo = "yarl";
     tag = "v${version}";
-    hash = "sha256-GEe2GDXmqsQgWB0UxPZVMdSco3j2JYHg9BU9M6oqynw=";
+    hash = "sha256-2Uqn1TwfH375CBIveEpsco4dDNrhxHwX8wIP8dKhh/M=";
   };
 
   build-system = [
