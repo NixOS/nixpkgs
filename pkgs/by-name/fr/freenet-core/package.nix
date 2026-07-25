c17d04395692737753992bbda9f5f517b17cf500
Skye Soss <skye@soss.website>
2026-07-25 09:19:27 -0500

    freenet-core: init at 0.2.106

---
+++ pkgs/by-name/fr/freenet-core/package.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  rustPlatform,
+  pkg-config,
+  openssl,
+  versionCheckHook,
+  rustc,
+  nix-update-script,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "freenet-core";
+  version = "0.2.106";
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "freenet";
+    repo = "freenet-core";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-mYGBsWgju2DVACk+usSCAXJ14PzAUqabiOkoW1dN6mM=";
+  };
+
+  cargoHash = "sha256-n+Tj1zrCzAZ/7k+ZK9Wbw1L9xXZhkrOtGudZFvcDdPo=";
+
+  cargoBuildFlags = [ "--package=freenet" ];
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ openssl ];
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+
+  # So many of the tests require network connectivity that it isn't
+  # worth trying to skip specific ones
+  doCheck = false;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Peer-to-peer platform for decentralized applications";
+    homepage = "https://freenet.org/";
+    donationPage = "https://freenet.org/donate/";
+    changelog = "https://github.com/freenet/freenet-core/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.agpl3Only;
+    maintainers = [ lib.maintainers.skyesoss ];
+    mainProgram = "freenet";
+    inherit (rustc.meta) platforms;
+  };
+})
