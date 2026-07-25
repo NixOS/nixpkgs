a06da98c107f30e1b6bbb10323a6ae5e70d94a38
Martin Joerg <martin.joerg@gmail.com>
2026-07-25 17:05:17 +0000

    parquet-tools: drop patch for disabled test

--- pkgs/by-name/pa/parquet-tools/package.nix
+++ pkgs/by-name/pa/parquet-tools/package.nix
@@ -24,13 +24,6 @@ python3Packages.buildPythonApplication rec {
     ./moto5.patch
   ];
 
-  postPatch = ''
-    substituteInPlace tests/test_inspect.py \
-      --replace "parquet-cpp-arrow version 5.0.0" "parquet-cpp-arrow version ${python3Packages.pyarrow.version}" \
-      --replace "serialized_size: 2222" "serialized_size: 2221" \
-      --replace "format_version: 1.0" "format_version: 2.6"
-  '';
-
   pythonRelaxDeps = [
     "pandas"
     "tabulate"
