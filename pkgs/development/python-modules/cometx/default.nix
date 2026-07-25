546836ae849346cbbcae5405f6512b3ea38d55db
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-25 17:26:49 +0000

    python3Packages.cometx: 3.6.7 -> 3.6.8

--- pkgs/development/python-modules/cometx/default.nix
+++ pkgs/development/python-modules/cometx/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "cometx";
-  version = "3.6.7";
+  version = "3.6.8";
 
   pyproject = true;
   build-system = [ setuptools ];
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "comet-ml";
     repo = "cometx";
     tag = version;
-    hash = "sha256-7jq/W1IEu6RZhKHMWdGC9b3xCGnS4hrZIPcQZ2NpIt4=";
+    hash = "sha256-Pcu4KEh3EXXrmyTysxtl2aFj3G4dcqBfrKT1pkPQ4Oo=";
   };
 
   dependencies = [
