cb93ffd5fafe27cfcc9c2fdf3109aec853ee140e
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-07-26 17:44:56 -0400

    pqrs: modernize
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/by-name/pq/pqrs/package.nix
+++ pkgs/by-name/pq/pqrs/package.nix
@@ -2,20 +2,26 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  versionCheckHook,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "pqrs";
   version = "0.3.2";
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "manojkarthick";
     repo = "pqrs";
-    rev = "v${finalAttrs.version}";
-    sha256 = "sha256-0oSSoGZga0OGAKUNsLmKkUl8N1l0pVi4KIqrKJbeVVU=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-0oSSoGZga0OGAKUNsLmKkUl8N1l0pVi4KIqrKJbeVVU=";
   };
 
   cargoHash = "sha256-P3yTmECj0K0mjWUiWlQCwuQVbnbVR1xFV5cE8Uo3U90=";
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
 
   meta = {
     description = "CLI tool to inspect Parquet files";
