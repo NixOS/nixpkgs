491bb995d773c15d998a87d83c7de5dff3370891
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-26 15:20:05 +0000

    c2patool: 0.26.72 -> 0.27.3

--- pkgs/by-name/c2/c2patool/package.nix
+++ pkgs/by-name/c2/c2patool/package.nix
@@ -10,16 +10,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "c2patool";
-  version = "0.26.72";
+  version = "0.27.3";
 
   src = fetchFromGitHub {
     owner = "contentauth";
     repo = "c2pa-rs";
     tag = "c2patool-v${finalAttrs.version}";
-    hash = "sha256-miK2AphIhpnbIsWa8eWrEPrdngEn6VVeq7k2m9zOGzk=";
+    hash = "sha256-eowUxpxWS3ur/fFDz5V3zhrbZNwEE33PYesoUZk3GXE=";
   };
 
-  cargoHash = "sha256-/bJjrfvWNowFsf2d7vWAi7+3I/0TFMnQw7DKMyYOnNY=";
+  cargoHash = "sha256-FAdz/NBpSXrpLFbQlpk7MWJkB/gUz4Ib5uRs+eS2Cd0=";
 
   # use the non-vendored openssl
   env.OPENSSL_NO_VENDOR = 1;
