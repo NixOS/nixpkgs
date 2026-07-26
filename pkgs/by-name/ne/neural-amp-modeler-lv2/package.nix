25e320c7427370864f1e26b6a6f9e38068177b6c
Gabriel Nützi <gnuetzi@gmail.com>
2026-07-26 11:55:55 +0200

    neural-amp-modeler-lv2: 0.1.4 -> 0.2.3

--- pkgs/by-name/ne/neural-amp-modeler-lv2/package.nix
+++ pkgs/by-name/ne/neural-amp-modeler-lv2/package.nix
@@ -7,14 +7,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "neural-amp-modeler-lv2";
-  version = "0.1.4";
+  version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "mikeoliphant";
     repo = "neural-amp-modeler-lv2";
-    tag = finalAttrs.version;
+    tag = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-5BOZOocZWWSWawXJFMAgM0NR0s0CbkzDVr6fnvZMvd0=";
+    hash = "sha256-TkynGmhwnkTqieJNuC+H2rDgxYZ9IFvqukfmtbSj790=";
   };
 
   nativeBuildInputs = [
