NixOS/nixpkgs

commit fefc9af5ddf3997b27f9e60b83a8a799145ca6c3

xdg-desktop-portal: move to by-name
Co-authored-by: Jan Tojnar <jtojnar@gmail.com>

pkgs/by-name/xd/xdg-desktop-portal/package.nix
@@ -28,6 +28,7 @@
   gst_all_1,
   libgudev,
   umockdev,
+  zynaddsubfx,
   replaceVars,
   enableGeoLocation ? true,
   enableSystemd ? true,
@@ -193,7 +194,7 @@ stdenv.mkDerivation (finalAttrs: {
       installedTests = nixosTests.installed-tests.xdg-desktop-portal;
 
       validate-icon = runCommand "test-icon-validation" { } ''
-        ${finalAttrs.finalPackage}/libexec/xdg-desktop-portal-validate-icon --ruleset=desktop --sandbox --path=${../../../by-name/zy/zynaddsubfx/ZynLogo.svg} > "$out"
+        ${finalAttrs.finalPackage}/libexec/xdg-desktop-portal-validate-icon --ruleset=desktop --sandbox --path=${finalAttrs.src}/doc/website/favicon.svg > "$out"
         grep format=svg "$out"
       '';
     };

pkgs/top-level/all-packages.nix
@@ -10292,8 +10292,6 @@ with pkgs;
     gbmSupport = true;
   };
 
-  xdg-desktop-portal = callPackage ../development/libraries/xdg-desktop-portal { };
-
   xdg-desktop-portal-hyprland =
     callPackage ../applications/window-managers/hyprwm/xdg-desktop-portal-hyprland
       {

commit fb44a14549664658bc53b25be0f4a73d7689a573

xdg-desktop-portal: remove unused input
Unused since its introduction; presumably a mistake.

Fixes: fefc9af5ddf3 ("xdg-desktop-portal: move to by-name")

pkgs/by-name/xd/xdg-desktop-portal/package.nix
@@ -28,7 +28,6 @@
   gst_all_1,
   libgudev,
   umockdev,
-  zynaddsubfx,
   replaceVars,
   enableGeoLocation ? true,
   enableSystemd ? true,