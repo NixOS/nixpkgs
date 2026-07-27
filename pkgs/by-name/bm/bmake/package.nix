808039d60020f21ce9397130eb8c48f5e6eda92b
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-27 09:51:34 +0000

    bmake: 20260313 -> 20260714

--- pkgs/by-name/bm/bmake/package.nix
+++ pkgs/by-name/bm/bmake/package.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bmake";
-  version = "20260313";
+  version = "20260714";
 
   src = fetchurl {
     url = "https://www.crufty.net/ftp/pub/sjg/bmake-${finalAttrs.version}.tar.gz";
-    hash = "sha256-dsjzzULuBc/7R7zIElbj1edCb00I5zN4i0WYXe30+XU=";
+    hash = "sha256-CBB4HSStbv4BCozpHFxSncjdlaVh1sk7MOVrjWeczmU=";
   };
 
   patches = [
