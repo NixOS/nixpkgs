be04cfe2ec5c60c2f26800e4b443c1d512312d76
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-27 22:09:20 +0000

    tdarr-node: 2.81.01 -> 2.85.01

--- pkgs/tools/misc/tdarr/common.nix
+++ pkgs/tools/misc/tdarr/common.nix
@@ -101,7 +101,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   inherit pname;
-  version = "2.81.01";
+  version = "2.85.01";
 
   src = fetchzip {
     url = "https://storage.tdarr.io/versions/${finalAttrs.version}/${platform}/${componentName}.zip";
