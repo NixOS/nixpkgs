fd980c76a60ac5cce320e1efa290dca35246e8e1
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-27 13:31:45 -0400

    navidrome.plugins: to extendMkDerivation

--- pkgs/by-name/na/navidrome/plugins/build-plugin.nix
+++ pkgs/by-name/na/navidrome/plugins/build-plugin.nix
@@ -4,64 +4,61 @@
   navidrome,
   zip,
 }:
+lib.extendMkDerivation {
+  constructDrv = buildGoModule;
 
-{
-  pname,
-  src,
-  version,
-  vendorHash,
-  meta,
-  ...
-}@args:
-
-buildGoModule (
-  finalAttrs:
-  {
-    inherit
-      pname
-      version
-      src
-      vendorHash
-      ;
-
-    env = {
-      CGO_ENABLED = "0";
-    }
-    // (args.env or { });
-
-    postBuild = ''
-      GOOS=wasip1 \
-      GOARCH=wasm \
-      go build \
-        -buildmode=c-shared \
-        -o $GOPATH/bin/plugin.wasm .
-    '';
-
-    postInstall = ''
-      mkdir $out/share
-      pushd $(mktemp -d)
-      cp $GOPATH/bin/plugin.wasm .
-      cp ${finalAttrs.src}/manifest.json .
-      ${lib.getExe zip} \
-        $out/share/${finalAttrs.pname}.ndp \
-        plugin.wasm \
-        manifest.json
-      popd
-      rm -r $out/bin
-    '';
-
-    passthru = {
-      isNavidromePlugin = true;
-    }
-    // args.passthru or { };
-
-    meta = {
-      inherit (navidrome.meta) platforms maintainers;
-    }
-    // args.meta or { };
-  }
-  // removeAttrs args [
+  excludeDrvArgNames = [
     "meta"
     "passthru"
-  ]
-)
+  ];
+
+  extendDrvArgs =
+    finalAttrs:
+    {
+      pname,
+      version,
+      src,
+      vendorHash,
+      meta,
+      env ? { },
+      passthru ? { },
+      ...
+    }@args:
+    {
+      env = {
+        CGO_ENABLED = "0";
+      }
+      // env;
+
+      postBuild = ''
+        GOOS=wasip1 \
+        GOARCH=wasm \
+        go build \
+          -buildmode=c-shared \
+          -o $GOPATH/bin/plugin.wasm .
+      '';
+
+      postInstall = ''
+        mkdir $out/share
+        pushd $(mktemp -d)
+        cp $GOPATH/bin/plugin.wasm .
+        cp ${finalAttrs.src}/manifest.json .
+        ${lib.getExe zip} \
+          $out/share/${finalAttrs.pname}.ndp \
+          plugin.wasm \
+          manifest.json
+        popd
+        rm -r $out/bin
+      '';
+
+      passthru = {
+        isNavidromePlugin = true;
+      }
+      // passthru;
+
+      meta = meta // {
+        platforms = navidrome.meta.platforms;
+        maintainers = navidrome.meta.maintainers ++ meta.maintainers or [ ];
+      };
+    };
+}
