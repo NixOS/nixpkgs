ee155a962ed5a3cea8073b235221e0d8cab9b85e
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-27 21:40:28 +0000

    vivaldi: 8.1.4087.55 -> 8.1.4087.58
    (cherry picked from commit 95592cab2e6ac2d1c59f0df04c01ccf69ff61580)

--- pkgs/by-name/vi/vivaldi/package.nix
+++ pkgs/by-name/vi/vivaldi/package.nix
@@ -67,7 +67,7 @@
 
 stdenv.mkDerivation rec {
   pname = "vivaldi";
-  version = "8.1.4087.55";
+  version = "8.1.4087.58";
 
   suffix =
     {
@@ -80,8 +80,8 @@ stdenv.mkDerivation rec {
     url = "https://downloads.vivaldi.com/stable/vivaldi-stable_${version}-1_${suffix}.deb";
     hash =
       {
-        aarch64-linux = "sha256-cSDu1cjfCIkn0wWm6cWaBrfgGZH4+qs3JnXYx+JfC7k=";
-        x86_64-linux = "sha256-GicxDYZJxerEEDu08ZlyegLLZSknZtuRZrCUYT+Q1N0=";
+        aarch64-linux = "sha256-Yvs3HRbsrgTeYXAKrRliLArXtK96jnjjf7x/sdpwvig=";
+        x86_64-linux = "sha256-/2GdNvj9+O6LDfqJdjNNdNWJymNAtnpTnzznW/4jINE=";
       }
       .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
   };
