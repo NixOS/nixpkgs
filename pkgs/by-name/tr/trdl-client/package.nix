52a4f7250c443f21d1d7d661a2b9ca10906bf15c
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-28 18:48:34 +0000

    trdl-client: 0.12.3 -> 0.13.0

--- pkgs/by-name/tr/trdl-client/package.nix
+++ pkgs/by-name/tr/trdl-client/package.nix
@@ -6,18 +6,18 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "trdl-client";
-  version = "0.12.3";
+  version = "0.13.0";
 
   src = fetchFromGitHub {
     owner = "werf";
     repo = "trdl";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-RKFfljYRVEmfGTX3kqmSm6SDz5i3v0mX/bnSSPj8/ZI=";
+    hash = "sha256-h7L8oe8SbXa0PSAizxpUfkyXW9pm9AX1A7+OkyDZugg=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/client";
 
-  vendorHash = "sha256-veSgWyk1ytHRNHuuZJBV+1rqGDsdEb01CImm+EexFCk=";
+  vendorHash = "sha256-x4P9/BKS+tSHs2/DzeJ3qixs+nsJZOw9dXlQd3LmDNM=";
 
   subPackages = [ "cmd/trdl" ];
 
