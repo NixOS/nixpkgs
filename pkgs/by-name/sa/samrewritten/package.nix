947d13f0f0a195b5585b4f510b1048382ad17122
KeksNino <KeksNino@proton.me>
2026-07-28 22:52:41 +0200

    samrewritten: format with nixfmt

--- pkgs/by-name/sa/samrewritten/package.nix
+++ pkgs/by-name/sa/samrewritten/package.nix
@@ -64,7 +64,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
     homepage = "https://github.com/PaulCombal/SamRewritten";
     changelog = "https://github.com/PaulCombal/SamRewritten/releases";
     license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [ ludovicopiero keksnino ];
+    maintainers = with lib.maintainers; [
+      ludovicopiero
+      keksnino
+    ];
     platforms = [ "x86_64-linux" ];
   };
 })
