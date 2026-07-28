d94239d56bbcf98e0407144bd06f540020a85bcc
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-28 13:00:56 +0000

    python3Packages.holidays: 0.100 -> 0.101

--- pkgs/development/python-modules/holidays/default.nix
+++ pkgs/development/python-modules/holidays/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "holidays";
-  version = "0.100";
+  version = "0.101";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "vacanza";
     repo = "python-holidays";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-PY2N/UysRcz8AWQQ8cA4WlY0jVV6mpxhzVE65uLuWPg=";
+    hash = "sha256-QptU47swuqlcYwW+g+arF9nfBvCBaxvP+9i1vGMPmV8=";
   };
 
   build-system = [
