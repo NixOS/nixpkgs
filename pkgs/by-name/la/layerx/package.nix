920eb62e834d27cab3bdcaeef0ff6fdc1c319403
Holiu618 <165534185+Holiu618@users.noreply.github.com>
2026-07-28 15:26:23 +0000

    layerx: 1.5.3 -> 1.6.0

--- pkgs/by-name/la/layerx/package.nix
+++ pkgs/by-name/la/layerx/package.nix
@@ -10,14 +10,14 @@
 
 buildGoModule (finalAttrs: {
   pname = "layerx";
-  version = "1.5.3";
+  version = "1.6.0";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "deveshctl";
     repo = "layerx";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-HboWzfDiiVMwMXkrOyxUvZ+V4Hi11cEqODfWT7b5+dw=";
+    hash = "sha256-6D/c2RDfvoVXikQ1TLq1dl6MJdxD0562tl6LgFOrSds=";
   };
 
   vendorHash = "sha256-7wbyz6fKB3HMFhKJVIWrOIczLfqF4yInyszdh2Ky8WU=";
