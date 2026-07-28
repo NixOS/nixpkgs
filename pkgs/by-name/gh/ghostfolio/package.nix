b8d2a65c576d8a3444e140aa77374a3d27a13b15
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-28 08:17:25 +0000

    ghostfolio: 3.29.0 -> 3.35.0
    (cherry picked from commit 4ba0ebacc98183f33b556ecbf450793e1ab58b6c)

--- pkgs/by-name/gh/ghostfolio/package.nix
+++ pkgs/by-name/gh/ghostfolio/package.nix
@@ -12,13 +12,13 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "ghostfolio";
-  version = "3.29.0";
+  version = "3.35.0";
 
   src = fetchFromGitHub {
     owner = "ghostfolio";
     repo = "ghostfolio";
     tag = finalAttrs.version;
-    hash = "sha256-fmYE942D91beGxCnJ5Kc8fdLbsgu9/VWe+9Gd/dMYIs=";
+    hash = "sha256-2HaEee3NZ24ZR+NGodlMgMspC5lhZLXY6jfwiGgJG3A=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -28,7 +28,7 @@ buildNpmPackage (finalAttrs: {
     '';
   };
 
-  npmDepsHash = "sha256-DqPCKW5axDToe0iboe9MF6ea4TNif3Oo5jOzWlMnQas=";
+  npmDepsHash = "sha256-5afjokIgi7ofNRG013irUtpmcUcScFUI+Uy0igjpCmw=";
 
   postPatch = ''
     substituteInPlace replace.build.mjs \
