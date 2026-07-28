5f4fc6f6044327b4df3ec0d74582ecbbb0e3d2d9
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-28 20:38:51 +0000

    aws-vault: 7.13.0 -> 7.13.1

--- pkgs/by-name/aw/aws-vault/package.nix
+++ pkgs/by-name/aw/aws-vault/package.nix
@@ -10,17 +10,17 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "aws-vault";
-  version = "7.13.0";
+  version = "7.13.1";
 
   src = fetchFromGitHub {
     owner = "ByteNess";
     repo = "aws-vault";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-afmApbIydrouoXmu3inLE9EvwFqOJxVkNcVSDjZUcXY=";
+    hash = "sha256-J9soEr5QhHZZLthWyNCr3ofCJD01gVLqT+8XVXgd/oA=";
   };
 
   proxyVendor = true;
-  vendorHash = "sha256-I+yKJO+MOCd+wZn4uQt+Xg18b/PWYPm6qxzGEOsCACo=";
+  vendorHash = "sha256-5GYKo2bs0JBe7aiqv/HVerfJKYf1Ocen7xkQvHQfm44=";
 
   nativeBuildInputs = [
     installShellFiles
