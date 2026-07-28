30fa960d4819d97b0b546ed3dcecddc8ac9c596a
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-28 17:36:52 +0000

    home-assistant-custom-components.frigidaire: 0.1.33 -> 0.1.37

--- pkgs/servers/home-assistant/custom-components/frigidaire/package.nix
+++ pkgs/servers/home-assistant/custom-components/frigidaire/package.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent rec {
   owner = "bm1549";
   domain = "frigidaire";
-  version = "0.1.33";
+  version = "0.1.37";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "home-assistant-frigidaire";
     tag = version;
-    hash = "sha256-P89EBB5JkdkSDoDvBaQgJiBMgu3cU9erkvtAPZS4KL8=";
+    hash = "sha256-q3rF7PtvlOL2CJCWZpahsOkJCmKwyTg9Bantobu9cdE=";
   };
 
   dependencies = [ frigidaire ];
