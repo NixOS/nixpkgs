58d77c70c0d2830e4f611ab7ed90d99a6cc04540
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-28 07:21:46 +0000

    python3Packages.reolink-aio: 0.21.4 -> 0.21.7

--- pkgs/development/python-modules/reolink-aio/default.nix
+++ pkgs/development/python-modules/reolink-aio/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "reolink-aio";
-  version = "0.21.4";
+  version = "0.21.7";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "starkillerOG";
     repo = "reolink_aio";
     tag = finalAttrs.version;
-    hash = "sha256-qBGUhqjHur3vKhpHmOHIQUxeBn+YAIjECdm33o1c3jg=";
+    hash = "sha256-nxcx0FADiIQun7eVs+SfJNuPCbwWacQidcR0+hX8KAc=";
   };
 
   build-system = [ setuptools ];
