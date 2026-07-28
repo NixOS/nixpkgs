73ca237bedad38ef50af6189991050c5dc7d9bc9
scraptux <git@thomasjasny.de>
2026-07-28 16:01:42 +0200

    python3Packages.grpcio-health-checking: 1.82.1 -> 1.83.0

--- pkgs/development/python-modules/grpcio-health-checking/default.nix
+++ pkgs/development/python-modules/grpcio-health-checking/default.nix
@@ -11,13 +11,13 @@
 # nixpkgs-update: no auto update
 buildPythonPackage rec {
   pname = "grpcio-health-checking";
-  version = "1.82.1";
+  version = "1.83.0";
   format = "setuptools";
 
   src = fetchPypi {
     pname = "grpcio_health_checking";
     inherit version;
-    hash = "sha256-hiVeBOHTnxyXpmMtQbYySTUUCN5cWOhe7eh6/X2YKN0=";
+    hash = "sha256-rWvE1aEQOtcE0lzNgt7zAN+ieZaxhcqz5Mzu7yxoZ9Q=";
   };
 
   propagatedBuildInputs = [
