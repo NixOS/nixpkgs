355d0830a922d68d4ca2de899ea3b9051ce45bcd
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-28 07:22:25 +0000

    kokkos: 5.1.1 -> 5.2.0

--- pkgs/by-name/ko/kokkos/package.nix
+++ pkgs/by-name/ko/kokkos/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "kokkos";
-  version = "5.1.1";
+  version = "5.2.0";
 
   src = fetchFromGitHub {
     owner = "kokkos";
     repo = "kokkos";
     rev = finalAttrs.version;
-    hash = "sha256-F/UNDIsEjRvM9RkTPg0wJtRdoTPpNPqyJYbgtvkntNk=";
+    hash = "sha256-+henisuI6Nzww3SIbfuB/23qiNSeYGOt4Ww5IGD9cO8=";
   };
 
   nativeBuildInputs = [
