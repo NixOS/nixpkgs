759525454cb2f92bc5cb27a35d032378811a32ad
whispers <whispersofthedawn@duck.com>
2026-07-28 10:47:01 -0400

    ldc: 1.41.0 -> 1.42.0
    release notes: https://github.com/ldc-developers/ldc/releases/tag/v1.42.0

--- pkgs/by-name/ld/ldc/package.nix
+++ pkgs/by-name/ld/ldc/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   callPackage,
   makeWrapper,
   removeReferencesTo,
@@ -11,7 +10,7 @@
   targetPackages,
   cmake,
   ninja,
-  llvm_18,
+  llvm_21,
   curl,
   tzdata,
   lit,
@@ -32,13 +31,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ldc";
-  version = "1.41.0";
+  version = "1.42.0";
 
   src = fetchFromGitHub {
     owner = "ldc-developers";
     repo = "ldc";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-6LcpY3LSFK4KgEiGrFp/LONu5Vr+/+vI04wEEpF3s+s=";
+    hash = "sha256-adA14tx/bruGvHVoODz13f8h/U2ol1lK0ytxnypsLv8=";
     fetchSubmodules = true;
   };
 
@@ -70,7 +69,7 @@ stdenv.mkDerivation (finalAttrs: {
     ldcBootstrap
     lit
     lit.python
-    llvm_18.dev
+    llvm_21.dev
     makeWrapper
     ninja
     unzip
