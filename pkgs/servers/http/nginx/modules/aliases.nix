053f157bd9a435fa4b20df60244f2a6c0f357bd7
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-28 22:03:59 +0200

    nginxModules.statsd: drop
    The repository is down and the source can't be retrieved any longer.
    
    Last update in nixpkgs was in 2018, over 8 years ago.

--- pkgs/servers/http/nginx/modules/aliases.nix
+++ pkgs/servers/http/nginx/modules/aliases.nix
@@ -5,4 +5,5 @@ self: {
   fastcgi-cache-purge = throw "fastcgi-cache-purge was renamed to cache-purge";
   ngx_aws_auth = throw "ngx_aws_auth was renamed to aws-auth";
   opentracing = throw "opentracing-cpp was removed because opentracing as been archived upstream"; # Added 2025-10-19
+  statsd = throw "statsd was removed because its upstream source vanished"; # Added 2026-07-28
 }
