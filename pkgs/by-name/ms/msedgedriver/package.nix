48a49e25e0bed7560987827a254072d0e7ac210a
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-29 04:16:58 +0000

    microsoft-edge: 150.0.4078.83 -> 150.0.4078.105
    (cherry picked from commit 08d869e30b1371994f30f9cfd693d36816c1ea2c)

--- pkgs/by-name/ms/msedgedriver/package.nix
+++ pkgs/by-name/ms/msedgedriver/package.nix
@@ -11,11 +11,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "msedgedriver";
-  version = "150.0.4078.83";
+  version = "150.0.4078.105";
 
   src = fetchzip {
     url = "https://msedgedriver.microsoft.com/${finalAttrs.version}/edgedriver_linux64.zip";
-    hash = "sha256-hmzL3ucHgKxPT1/WwqfWUWndSMUEf7n0Z0Wh9Rgm/MU=";
+    hash = "sha256-dFXj22lTP3lBN8gLooHFn6OzBWb7ha+dEHl/+jFjctI=";
     stripRoot = false;
   };
 
