80a258587cad5bb7a62c458e37075ac58c17ce2c
tincotema <tincotema@tincotema.org>
2026-07-29 22:29:55 +0200

    python3Packages.pytango: init at 10.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15990,6 +15990,8 @@ self: super: with self; {
 
   pytaglib = callPackage ../development/python-modules/pytaglib { };
 
+  pytango = callPackage ../development/python-modules/pytango { };
+
   pytankerkoenig = callPackage ../development/python-modules/pytankerkoenig { };
 
   pytap2 = callPackage ../development/python-modules/pytap2 { };
