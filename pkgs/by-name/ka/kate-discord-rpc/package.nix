c9fead457fdf7e8b2ee3bed95567e3f309bae185
solarfire <xatra169@gmail.com>
2026-07-29 08:17:23 -0400

    kate-discord-rpc: init at 0-unstable-2026-03-17

---
+++ pkgs/by-name/ka/kate-discord-rpc/package.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  fetchFromGitHub,
+  kdePackages,
+  qt6,
+  rapidjson,
+  unstableGitUpdater,
+  ...
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "kate-discord-rpc";
+  version = "0-unstable-2026-03-17";
+
+  src = fetchFromGitHub {
+    owner = "leia-uwu";
+    repo = "kate-discord-rpc";
+    rev = "93a14a03887540f819b3b4885fd8c789aee05b19";
+    hash = "sha256-TWMYy6oeFJZ1WTS9tNQLk9RcRBwkvVrZy9kVU2Kr90s=";
+    fetchSubmodules = true;
+  };
+
+  enableParallelBuilding = true;
+
+  nativeBuildInputs = [
+    cmake
+    kdePackages.extra-cmake-modules
+    kdePackages.kcoreaddons
+    kdePackages.kconfig
+    kdePackages.ktexteditor
+    kdePackages.kwidgetsaddons
+    kdePackages.qtbase
+    qt6.wrapQtAppsHook
+    rapidjson
+  ];
+
+  passthru.updateScript = unstableGitUpdater { };
+
+  meta = {
+    description = "Discord Rich Presence plugin for the KDE Plasma text editor Kate";
+    homepage = "https://github.com/leia-uwu/kate-discord-rpc";
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ Xatra1 ];
+  };
+})
