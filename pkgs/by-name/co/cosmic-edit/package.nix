17f219918e9f4136514da01007c06fcffd9bc477
Pratham Patel <prathampatel@thefossguy.com>
2026-07-29 21:43:51 +0530

    cosmic-edit: 1.4.0 -> 1.5.0

--- pkgs/by-name/co/cosmic-edit/package.nix
+++ pkgs/by-name/co/cosmic-edit/package.nix
@@ -16,21 +16,21 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-edit";
-  version = "1.4.0";
+  version = "1.5.0";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-edit";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-cONn5AksnZeqFKDEb5oMWspWn62rRqzeBKZtnvEpbng=";
+    hash = "sha256-/wuEpdjDzJScjxzJkAtUFOgcn/zh3ZmL0X5cH2kAhts=";
   };
 
   postPatch = ''
     substituteInPlace justfile --replace-fail '#!/usr/bin/env' "#!$(command -v env)"
   '';
 
-  cargoHash = "sha256-aIPvetY9ezCv7RgoyC8x7O8mTrnUqth+YuSYHWiPQVU=";
+  cargoHash = "sha256-pbmeef2dtFfkbVq8l6kUnH1by/rDJecdfxQL1tS1UMk=";
 
   separateDebugInfo = true;
   __structuredAttrs = true;
