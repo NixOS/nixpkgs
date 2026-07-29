bc93deee131821a9782225f4739484ff13af9aa3
Bart Oostveen <bart@bartoostveen.nl>
2026-07-29 15:15:22 +0200

    samba: 4.23.8 -> 4.23.10
    Changelog: https://www.samba.org/samba/history/samba-4.23.9.html, https://www.samba.org/samba/history/samba-4.23.10.html
    Fixes: CVE-2026-6949, CVE-2026-58216, CVE-2026-58218, CVE-2026-58221, CVE-2026-58222, CVE-2026-58224
    See also: https://www.openwall.com/lists/oss-security/2026/07/29/2

--- pkgs/servers/samba/4.x.nix
+++ pkgs/servers/samba/4.x.nix
@@ -81,11 +81,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "samba";
-  version = "4.23.8";
+  version = "4.23.10";
 
   src = fetchurl {
     url = "https://download.samba.org/pub/samba/stable/samba-${finalAttrs.version}.tar.gz";
-    hash = "sha256-l2EphHRW3Ft4wA+P+3ncYFxJ1qrKiyqncv0i27afrgE=";
+    hash = "sha256-50s/SV1+SOZxC1Aas4HIDd88uZxmBHMtNgQCZdnK9FI=";
   };
 
   outputs = [
