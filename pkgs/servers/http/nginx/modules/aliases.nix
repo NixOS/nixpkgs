33b28330e73b75d3f02b73074ea3b28e53521be7
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-29 15:03:49 +0200

    nginxModules.http_proxy_connect_module_v24: drop
    No compatible nginx version remains.

--- pkgs/servers/http/nginx/modules/aliases.nix
+++ pkgs/servers/http/nginx/modules/aliases.nix
@@ -1,8 +1,10 @@
 # Removed or renamed nginx modules
 
 self: {
+  
   modsecurity-nginx = self.modsecurity;
   fastcgi-cache-purge = throw "fastcgi-cache-purge was renamed to cache-purge";
+  http_proxy_connect_module_v24 = throw "http_proxy_connect_module_v24 was removed because it was not compatible with any supported nginx version"; # Added 2026-07-29
   ngx_aws_auth = throw "ngx_aws_auth was renamed to aws-auth";
   opentracing = throw "opentracing-cpp was removed because opentracing as been archived upstream"; # Added 2025-10-19
   statsd = throw "statsd was removed because its upstream source vanished"; # Added 2026-07-28
