0b0f327d02ac1a3f26236c48036e7e07b8bec052
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-30 19:09:35 +0200

    python3Packages.mypy-boto3-guardduty: 1.43.47 -> 1.43.54

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -590,8 +590,8 @@ in
       "sha256-+DDeD9YWo98meLZU2Mzu5AE0S7HFg6kfxeUWUh9XcQA=";
 
   mypy-boto3-guardduty =
-    buildMypyBoto3Package "guardduty" "1.43.47"
-      "sha256-0+dEQ+FP/r1o+8XTKS/wG67RLb9euBEaU3dz4m4r4GI=";
+    buildMypyBoto3Package "guardduty" "1.43.54"
+      "sha256-dTsVgQ0NBaczxQsmK7Lnm3XKlSv8/2PU2tRCfh+yLJ8=";
 
   mypy-boto3-health =
     buildMypyBoto3Package "health" "1.43.0"
