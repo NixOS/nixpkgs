8896acaf08ac58c7b541c715802c88e5cc00f5c0
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-30 00:18:56 +0000

    segger-ozone: 3.40e -> 3.50a

--- pkgs/by-name/se/segger-ozone/package.nix
+++ pkgs/by-name/se/segger-ozone/package.nix
@@ -18,13 +18,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "segger-ozone";
-  version = "3.40e";
+  version = "3.50a";
 
   src = fetchurl {
     url = "https://www.segger.com/downloads/jlink/Ozone_Linux_V${
       lib.replaceString "." "" finalAttrs.version
     }_x86_64.tgz";
-    hash = "sha256-UX7ZGWVtVphBMcQ0R0iiNYaDfSjYXjSpK9YMBHw2Pss=";
+    hash = "sha256-pE9uaYwE0df/bOE5kgiBEiVZjUol3lvLKHMj1cPskDc=";
   };
 
   nativeBuildInputs = [
