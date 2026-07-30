b64875861ae98e2a0444db2d3e4167a99aa8a793
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-07-30 14:34:02 +0200

    music-assistant-desktop: 0.6.0 -> 0.6.2
    Diff: https://github.com/music-assistant/desktop-app/compare/0.6.0...0.6.2
    
    Changelog: https://github.com/music-assistant/desktop-app/releases/tag/0.6.2

--- pkgs/by-name/mu/music-assistant-desktop/package.nix
+++ pkgs/by-name/mu/music-assistant-desktop/package.nix
@@ -32,13 +32,13 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "music-assistant-desktop";
-  version = "0.6.0";
+  version = "0.6.2";
 
   src = fetchFromGitHub {
     owner = "music-assistant";
     repo = "desktop-app";
     tag = finalAttrs.version;
-    hash = "sha256-GFe+2+/JPuDb11LGDOqYBu4+J1qkRZUzL+g9HSzxoOs=";
+    hash = "sha256-d9dC6YiF4xIM2hVA2lX5B8qt5dnDFSG/MAZhQdZf8q4=";
   };
 
   patches = [
