1585b75db783f50a0a53c01c24447fb0d4a4675e
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-30 19:09:28 +0200

    python3Packages.mypy-boto3-glue: 1.43.37 -> 1.43.59

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -571,8 +571,8 @@ in
       "sha256-vMz4YKm78XMavlPUNiSVAYmAbyUBrJhUXbFrhxIvUJA=";
 
   mypy-boto3-glue =
-    buildMypyBoto3Package "glue" "1.43.37"
-      "sha256-sXLxhWKQbaIyTv2/a3GWyyboYabCGcYfSD7AOtr7n2Y=";
+    buildMypyBoto3Package "glue" "1.43.59"
+      "sha256-TdCAQMk1awgIFti1BgdHyb427zO1m/Tk8NavsBfW/mE=";
   mypy-boto3-grafana =
     buildMypyBoto3Package "grafana" "1.43.11"
       "sha256-XJOSLyL1+uEweZ9zER7IhH3DFLaLtpJKvuRIn8Ri+P4=";
