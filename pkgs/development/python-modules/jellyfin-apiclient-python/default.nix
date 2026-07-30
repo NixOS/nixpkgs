f00d8712a1e3ad389f03249eda600ccc3bb27e87
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-30 08:04:31 +0000

    python3Packages.jellyfin-apiclient-python: 1.15.0 -> 1.17.0

--- pkgs/development/python-modules/jellyfin-apiclient-python/default.nix
+++ pkgs/development/python-modules/jellyfin-apiclient-python/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "jellyfin-apiclient-python";
-  version = "1.15.0";
+  version = "1.17.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jellyfin";
     repo = "jellyfin-apiclient-python";
     tag = "v${version}";
-    hash = "sha256-+ZlJK1wbu/taOydgSOF7JNKcsLzqXph6m8PHww04QjI=";
+    hash = "sha256-dSofOLzW/7YCUVjIBQxEPUu+TjNbjPzBlZ3I+yQ6Rw8=";
   };
 
   build-system = [ setuptools ];
