b9fa23fd2c35f479475403219c46e66799eb05b5
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-30 18:19:00 +0000

    dropbear: 2026.93 -> 2026.94

--- pkgs/by-name/dr/dropbear/package.nix
+++ pkgs/by-name/dr/dropbear/package.nix
@@ -18,13 +18,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "dropbear";
-  version = "2026.93";
+  version = "2026.94";
 
   src = fetchFromGitHub {
     owner = "mkj";
     repo = "dropbear";
     tag = "DROPBEAR_${finalAttrs.version}";
-    hash = "sha256-Xs5LTVaNdfRKx0cFTQknaHXka6QhxBS2JNTQ8RoHG80=";
+    hash = "sha256-AJ6JvTPhd+Y4xnIIVqbJiU1rkmRmojoZX3pwKV0qrxA=";
   };
 
   patches = [
