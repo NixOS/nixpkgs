95044abdad28fe82829149c1a1dd501d8bb90929
Fabian Affolter <mail@fabian-affolter.ch>
2026-07-30 18:22:47 +0200

    python3Packages.filedepot: migrate to finalAttrs

--- pkgs/development/python-modules/filedepot/default.nix
+++ pkgs/development/python-modules/filedepot/default.nix
@@ -16,7 +16,7 @@
   sqlalchemy,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "filedepot";
   version = "0.12.0";
   pyproject = true;
@@ -24,7 +24,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "amol-";
     repo = "depot";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-oDnGA2prxYUkC90ewryeJXTXED59vcZGHU9D0QiopFM=";
   };
 
@@ -63,8 +63,8 @@ buildPythonPackage rec {
   meta = {
     description = "Toolkit for storing files and attachments in web applications";
     homepage = "https://github.com/amol-/depot";
-    changelog = "https://github.com/amol-/depot/releases/tag/${version}";
+    changelog = "https://github.com/amol-/depot/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
