7134f7833162492b54ded68294b7305fad28ca65
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-30 04:08:58 +0000

    snip: 0.22.0 -> 0.24.1

--- pkgs/by-name/sn/snip/package.nix
+++ pkgs/by-name/sn/snip/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "snip";
-  version = "0.22.0";
+  version = "0.24.1";
 
   src = fetchFromGitHub {
     owner = "edouard-claude";
     repo = "snip";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-y88ZHg29Z/QBZk8YWXp287Rel7DpPESYAlbqt5hj1nc=";
+    hash = "sha256-17vAgwuOrDN81+XKa2vn60T9RyZktOoF2xfF/RE+BNw=";
   };
 
   vendorHash = "sha256-2MxFZqjNuLzcuu+bsLyOyHIakCxh7j0FUx8LsjZRhrY=";
