ecd882c3487405c28e597ecbe520ebc82746d522
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-30 07:42:02 +0000

    python3Packages.led-ble: 1.1.11 -> 1.1.12

--- pkgs/development/python-modules/led-ble/default.nix
+++ pkgs/development/python-modules/led-ble/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "led-ble";
-  version = "1.1.11";
+  version = "1.1.12";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Bluetooth-Devices";
     repo = "led-ble";
     tag = "v${version}";
-    hash = "sha256-YPOjbmHR6WpmAEpYFl/ajzojgiIYEk+6H5LFjl1yo1c=";
+    hash = "sha256-NPxDcdrcscvqfsJAqq4ZJEWedsWsPKBYEsiUambU7rA=";
   };
 
   build-system = [ poetry-core ];
