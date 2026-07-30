b99daae83a5cfa6a51aedae5bacacf561695392e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-07-30 23:35:11 +0200

    python3Packages.pydantic-ai-slim: 1.107.0 -> 1.107.1
    https://github.com/pydantic/pydantic-ai/security/advisories/GHSA-jpr8-2v3g-wgf9
    
    Fixes: CVE-2026-65975
    Closes: #547550
    Not-cherry-picked-because: master went for 2.0.0

--- pkgs/development/python-modules/pydantic-ai-slim/default.nix
+++ pkgs/development/python-modules/pydantic-ai-slim/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pydantic-ai-slim";
-  version = "1.107.0";
+  version = "1.107.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pydantic";
     repo = "pydantic-ai";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-++tAI+KRCscDWqPQx5oswrjDq2txpzdI1ls/O/aAClE=";
+    hash = "sha256-jXYcGywwiC+SYpecrcjROtzNduToj2iM6OUBClnwUfo=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/pydantic_ai_slim";
