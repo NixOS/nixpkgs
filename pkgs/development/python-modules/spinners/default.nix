bb90d8ed014b7794aed79a5a93d3bf178df674e6
Harinn <prinn.dev@pm.me>
2026-07-31 23:40:23 +0700

    python3Packages.spinners: migrate to pyproject

--- pkgs/development/python-modules/spinners/default.nix
+++ pkgs/development/python-modules/spinners/default.nix
@@ -2,17 +2,20 @@
   buildPythonPackage,
   fetchPypi,
   lib,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "spinners";
   version = "0.0.24";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "0zz2z6dpdjdq5z8m8w8dfi8by0ih1zrdq0caxm1anwhxg2saxdhy";
   };
+
+  build-system = [ setuptools ];
 
   # Tests are not included in the PyPI distribution and the git repo does not have tagged releases
   doCheck = false;
