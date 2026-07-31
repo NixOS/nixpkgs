f12cafcef895dab35cc7095369f6862730df7057
Harinn <prinn.dev@pm.me>
2026-07-31 23:19:35 +0700

    python3Packages.stringcase: modernize

--- pkgs/development/python-modules/stringcase/default.nix
+++ pkgs/development/python-modules/stringcase/default.nix
@@ -5,14 +5,16 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "stringcase";
   version = "1.2.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "023hv3gknblhf9lx5kmkcchzmbhkdhmsnknkv7lfy20rcs06k828";
+    inherit (finalAttrs) pname version;
+    hash = "sha256-SKBpgGYZCO/o2dNOqytsE676IWOzztJpcpAuO9/YcAg=";
   };
 
   build-system = [ setuptools ];
@@ -20,10 +22,12 @@ buildPythonPackage rec {
   # PyPi package does not include tests.
   doCheck = false;
 
+  pythonImportsCheck = [ "stringcase" ];
+
   meta = {
     homepage = "https://github.com/okunishinishi/python-stringcase";
     description = "Convert string cases between camel case, pascal case, snake case etc…";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ alunduil ];
   };
-}
+})
