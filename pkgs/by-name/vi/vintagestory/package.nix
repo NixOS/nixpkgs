13f693e65bc8de342c2679a16be76cf5a3b6da13
Nicholas Malcolm <bubylou@pm.me>
2026-07-31 11:01:21 -0400

    vintagestory: 1.22.5 → 1.22.6

--- pkgs/by-name/vi/vintagestory/package.nix
+++ pkgs/by-name/vi/vintagestory/package.nix
@@ -27,11 +27,11 @@ assert waylandSupport -> libxkbcommon != null;
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vintagestory";
-  version = "1.22.5";
+  version = "1.22.6";
 
   src = fetchurl {
     url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${finalAttrs.version}.tar.gz";
-    hash = "sha256-ozpjsnqJKFdzKeJuOSuurYN8UEvLrBw17quGZeMN8PY=";
+    hash = "sha256-xJPyaYklEq2BeUX9+7hNPI6WC6ibcQDS+sKeqKYSh3A=";
   };
 
   __structuredAttrs = true;
