b0ff0cc1970b5bcc7d835dc08f9d53a17dba2c84
Harinn <prinn.dev@pm.me>
2026-07-31 23:16:03 +0700

    python3Packages.stringcase: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/stringcase/default.nix
+++ pkgs/development/python-modules/stringcase/default.nix
@@ -2,17 +2,20 @@
   buildPythonPackage,
   fetchPypi,
   lib,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "stringcase";
   version = "1.2.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "023hv3gknblhf9lx5kmkcchzmbhkdhmsnknkv7lfy20rcs06k828";
   };
+
+  build-system = [ setuptools ];
 
   # PyPi package does not include tests.
   doCheck = false;
