ce63c5d497a18f74176fc12a63f559e837a00520
Harinn <prinn.dev@pm.me>
2026-07-31 23:21:24 +0700

    python3Packages.sumtypes: modernize

--- pkgs/development/python-modules/sumtypes/default.nix
+++ pkgs/development/python-modules/sumtypes/default.nix
@@ -7,15 +7,17 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "sumtypes";
   version = "0.1a6";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "radix";
     repo = "sumtypes";
-    rev = version;
+    tag = finalAttrs.version;
     hash = "sha256-qwQyFKVnGEqHUqFmUSnHVvedsp2peM6rJZcS90paLOo=";
   };
 
@@ -25,10 +27,12 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [ pytestCheckHook ];
 
+  pythonImportsCheck = [ "sumtypes" ];
+
   meta = {
     description = "Algebraic data types for Python";
     homepage = "https://github.com/radix/sumtypes";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ NieDzejkob ];
   };
-}
+})
