d5fa1aa335e90d4fe78d0881e3b7409a28039af2
Harinn <prinn.dev@pm.me>
2026-07-31 23:44:48 +0700

    python3Packages.sqlalchemy-jsonfield: modernize

--- pkgs/development/python-modules/sqlalchemy-jsonfield/default.nix
+++ pkgs/development/python-modules/sqlalchemy-jsonfield/default.nix
@@ -11,15 +11,17 @@
   psycopg2,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "sqlalchemy-jsonfield";
   version = "1.0.2";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "penguinolog";
     repo = "sqlalchemy_jsonfield";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-4zLXB3UQh6pgQ80KrxkLeC5yiv1R8t2+JmSukmGXr7I=";
   };
 
@@ -45,8 +47,8 @@ buildPythonPackage rec {
   meta = {
     description = "SQLALchemy JSONField implementation for storing dicts at SQL independently from JSON type support";
     homepage = "https://github.com/penguinolog/sqlalchemy_jsonfield";
-    changelog = "https://github.com/penguinolog/sqlalchemy_jsonfield/releases/tag/${version}";
+    changelog = "https://github.com/penguinolog/sqlalchemy_jsonfield/releases/tag/${finalAttrs.version}";
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
-}
+})
