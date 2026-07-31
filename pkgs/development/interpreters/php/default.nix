a61a28c52f28ff3e7d125f1ead3ef21447572bbc
Maximilian Bosch <maximilian@mbosch.me>
2026-08-01 00:59:11 +0200

    php84: 8.4.23 -> 8.4.24
    ChangeLog: https://www.php.net/ChangeLog-8.php#8.4.24
    (cherry picked from commit 48830883bbb39302c0bb9f579259ae46850d01b2)

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -77,8 +77,8 @@ in
     hash = "sha256-+cn00M12ksbj2jY81MpyqCakmuGPXtQwtvyue8KIHhg=";
   };
   php84 = mkPhp {
-    version = "8.4.23";
-    hash = "sha256-wULAY7EM/2jQcnZuP/v7NlSgibk4ZosIMDVkN+6V4Po=";
+    version = "8.4.24";
+    hash = "sha256-KGiQRHWIUQ3xEO3p6c4J8etPmFZjC0f9xA7OcOIDc88=";
   };
   php85 = mkPhp {
     version = "8.5.9";
