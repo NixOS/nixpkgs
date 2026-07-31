f800d9d728625df49fcdbe1a7097fd6e4d371758
Harinn <prinn.dev@pm.me>
2026-07-31 23:09:37 +0700

    python3Packages.streamcontroller-plugin-tools: modernize

--- pkgs/development/python-modules/streamcontroller-plugin-tools/default.nix
+++ pkgs/development/python-modules/streamcontroller-plugin-tools/default.nix
@@ -6,15 +6,17 @@
   loguru,
   rpyc,
 }:
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "streamcontroller-plugin-tools";
   version = "2.0.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "StreamController";
     repo = "streamcontroller-plugin-tools";
-    rev = version;
+    tag = finalAttrs.version;
     hash = "sha256-dQZPRSzHhI3X+Pf7miwJlECGFgUfp68PtvwXAmpq5/s=";
   };
 
@@ -34,4 +36,4 @@ buildPythonPackage rec {
     maintainers = with lib.maintainers; [ sifmelcara ];
     platforms = lib.platforms.linux;
   };
-}
+})
