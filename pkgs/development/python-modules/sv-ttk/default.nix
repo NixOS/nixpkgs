529ecd23f3fb207b622cce123024480fbfe9acb6
Harinn <prinn.dev@pm.me>
2026-07-31 23:20:41 +0700

    python3Packages.sv-ttk: modernize

--- pkgs/development/python-modules/sv-ttk/default.nix
+++ pkgs/development/python-modules/sv-ttk/default.nix
@@ -6,13 +6,15 @@
   tkinter,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "sv-ttk";
   version = "2.6.1";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit version;
+    inherit (finalAttrs) version;
     pname = "sv_ttk";
     hash = "sha256-R1idXiA5jPQE6DYvJPPtSPODDNCs4FbYM1T6Jdjk/kg=";
   };
@@ -29,8 +31,8 @@ buildPythonPackage rec {
   meta = {
     description = "Gorgeous theme for Tkinter/ttk, based on the Sun Valley visual style";
     homepage = "https://github.com/rdbende/Sun-Valley-ttk-theme";
-    changelog = "https://github.com/rdbende/Sun-Valley-ttk-theme/releases/tag/v${version}";
+    changelog = "https://github.com/rdbende/Sun-Valley-ttk-theme/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ AngryAnt ];
   };
-}
+})
