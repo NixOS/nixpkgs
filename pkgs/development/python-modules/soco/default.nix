ca6f733dddc95bcad28b76f9185cc84de0de3e61
R. Ryantm <ryantm-bot@ryantm.com>
2026-07-31 16:28:15 +0000

    python3Packages.soco: 0.31.1 -> 0.31.2

--- pkgs/development/python-modules/soco/default.nix
+++ pkgs/development/python-modules/soco/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "soco";
-  version = "0.31.1";
+  version = "0.31.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "SoCo";
     repo = "SoCo";
     tag = "v${version}";
-    hash = "sha256-YxIpTFGB82aBvRxfntou2dUZm7T0KSj7yhX3gcdAO7U=";
+    hash = "sha256-TCnKzAOrpQxh8JaBkoPs2e81xUS/iQ8D/Qtt3WU9J1k=";
   };
 
   build-system = [ setuptools ];
