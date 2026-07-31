115953e9d353361e413ee616aec6b65253fa80c8
Makuru <Makuru@Makuru.org>
2026-07-31 02:36:31 +0200

    proton-ge-bin: GE-Proton11-1 -> GE-Proton11-3

--- pkgs/by-name/pr/proton-ge-bin/package.nix
+++ pkgs/by-name/pr/proton-ge-bin/package.nix
@@ -9,11 +9,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "proton-ge-bin";
-  version = "GE-Proton11-1";
+  version = "GE-Proton11-3";
 
   src = fetchzip {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-    hash = "sha256-I7SSvzQQ/NqdvwjpJ9IFFtAaTS+rgHUyXx0us1vIOnw=";
+    hash = "sha256-RiCmnUKeZRhPUCgm7fsROKFkAl37+/tYkA47tQtkIF4=";
   };
 
   dontUnpack = true;
