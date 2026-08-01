cf032e0607fc0a695804e83b0d33b980928d117e
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-01 19:14:01 +0200

    python3Packages.pytrydan: 1.0.2 -> 1.0.4
    Diff: https://github.com/dgomes/pytrydan/compare/v1.0.2...v1.0.4
    
    Changelog: https://github.com/dgomes/pytrydan/blob/v1.0.4/CHANGELOG.md

--- pkgs/development/python-modules/pytrydan/default.nix
+++ pkgs/development/python-modules/pytrydan/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pytrydan";
-  version = "1.0.2";
+  version = "1.0.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dgomes";
     repo = "pytrydan";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-vxIY+BCo3o4FBI1otiMx3swWTxtmEgYfVCWQAq2OuUM=";
+    hash = "sha256-4+bTRdU8XSxs9Lnh2tSp5i9px+2F80PnnULHYJVeEfE=";
   };
 
   pythonRelaxDeps = [ "tenacity" ];
