a355fd17fc9c28357773e87d74ae7f3f75ad7309
lavecat <lirus12345.lave@gmail.com>
2026-08-01 23:49:20 +0200

    spotiflac: 7.1.7 -> 7.2.0

--- pkgs/by-name/sp/spotiflac/package.nix
+++ pkgs/by-name/sp/spotiflac/package.nix
@@ -18,14 +18,14 @@
 
 buildGoModule (finalAttrs: {
   pname = "spotiflac";
-  version = "7.1.8";
+  version = "7.2.0";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
-    owner = "afkarxyz";
+    owner = "spotbye";
     repo = "SpotiFLAC";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Uz+9vsneP8xLGi5pgwYIufCPUzKSH4CRAdt0o+FifAM=";
+    hash = "sha256-T73zf/ji9wqUytVQbn8GswBAlZ/RUu8+xg6B6bhqxhU=";
   };
 
   nativeBuildInputs = [
@@ -53,7 +53,7 @@ buildGoModule (finalAttrs: {
       sourceRoot = "${finalAttrs.src.name}/frontend";
       pnpm = pnpm_10;
       fetcherVersion = 3;
-      hash = "sha256-mecNGWbUATjNl1uWByxE1W1b8tfNyPIRMndcZSBl+XM=";
+      hash = "sha256-fAb+8IgtxfDRZeUJaEq0SMvfoUAkoMXQWfQSIEaTwy4=";
     };
     pnpmRoot = "frontend";
   };
@@ -88,7 +88,7 @@ buildGoModule (finalAttrs: {
     runHook postInstall;
   '';
 
-  vendorHash = "sha256-dTrfLnuo7W3m3mg32wBDv8IbmQA44KXsazRLdanIi/Y=";
+  vendorHash = "sha256-NRpuPFymdNeG1Wzeyis9k2V/GqbIu1M+nfag8GG7ILc=";
 
   desktopItems = [
     (makeDesktopItem {
@@ -103,8 +103,8 @@ buildGoModule (finalAttrs: {
 
   meta = {
     description = "Get Spotify tracks in true FLAC from Tidal, Qobuz & Amazon Music — no account required";
-    homepage = "https://github.com/afkarxyz/SpotiFLAC/";
-    changelog = "https://github.com/afkarxyz/SpotiFLAC/releases/tag/v${finalAttrs.version}";
+    homepage = "https://github.com/spotbye/SpotiFLAC/";
+    changelog = "https://github.com/spotbye/SpotiFLAC/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     platforms = lib.platforms.linux;
     mainProgram = "spotiflac";
