082815cacad35ad0855811af0a345bc88e01c8c8
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-01 23:35:07 +0200

    python3Packages.mypy-boto3-amp: 1.43.54 -> 1.43.62

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -59,8 +59,8 @@ in
       "sha256-o5hZAxSZklg7hzL7MWCBYnnYTRoBSSAKPLpub1NVO74=";
 
   mypy-boto3-amp =
-    buildMypyBoto3Package "amp" "1.43.54"
-      "sha256-i/iC+OeJWJMOqBlegdYokbH7NJ/H8ahKLhctUZAarBs=";
+    buildMypyBoto3Package "amp" "1.43.62"
+      "sha256-h42x1hJbjLtZrYis16d9HkcASfrFAkboeedwrwOthZk=";
 
   mypy-boto3-amplify =
     buildMypyBoto3Package "amplify" "1.43.0"
