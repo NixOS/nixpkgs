972f5c17d36da7c0b82ad30d989474d2bae9275d
Gaetan Lepage <gaetan@glepage.com>
2026-08-01 12:17:26 +0000

    python3Packages.checksumdir: cleanup

--- pkgs/development/python-modules/checksumdir/default.nix
+++ pkgs/development/python-modules/checksumdir/default.nix
@@ -2,22 +2,27 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+
+  # build-system
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "checksumdir";
   version = "1.3.0";
   pyproject = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "to-mc";
     repo = "checksumdir";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-rOHRJAK+Or8bwAtzpbINdnEjK3WQcU+4sEZI91tMvAk=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [
+    setuptools
+  ];
 
   doCheck = false; # Package does not contain tests
   pythonImportsCheck = [ "checksumdir" ];
@@ -25,7 +30,9 @@ buildPythonPackage rec {
   meta = {
     description = "Simple package to compute a single deterministic hash of the file contents of a directory";
     homepage = "https://github.com/to-mc/checksumdir";
+    changelog = "https://github.com/to-mc/checksumdir/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = [ ];
+    mainProgram = "checksumdir";
   };
-}
+})
