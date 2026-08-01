b42f595f950915c989e5d58c0f197b31733cc6a4
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-01 23:36:27 +0200

    python3Packages.mypy-boto3-rds: 1.43.51 -> 1.43.62

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1082,8 +1082,8 @@ in
       "sha256-ZmYC3YQujmfSUK0pOGCGFwT7LrSa3oJfo9juUb+2Xpk=";
 
   mypy-boto3-rds =
-    buildMypyBoto3Package "rds" "1.43.51"
-      "sha256-YFCezjQ5tIk0JdeOajx3jg59D9KiB7opjoaBOomNq6c=";
+    buildMypyBoto3Package "rds" "1.43.62"
+      "sha256-2DJ8h1PXMiIf5KIvqdEx17ZS44Hz8MXVcM5xbNkc1VI=";
 
   mypy-boto3-rds-data =
     buildMypyBoto3Package "rds-data" "1.43.37"
