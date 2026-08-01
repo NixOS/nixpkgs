e18bf92ca417c42d0c99046b13d8267f5c975aee
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-01 23:06:15 +0200

    python3Packages.meraki: migrate to finalAttrs

--- pkgs/development/python-modules/meraki/default.nix
+++ pkgs/development/python-modules/meraki/default.nix
@@ -6,7 +6,7 @@
   httpx,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "meraki";
   version = "4.5.0";
   pyproject = true;
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "meraki";
     repo = "dashboard-api-python";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-SYILd5epeB/V/fa+yxZvT49eHapZnqxcTW1s8gH3fAY=";
   };
 
@@ -30,8 +30,8 @@ buildPythonPackage rec {
   meta = {
     description = "Cisco Meraki cloud-managed platform dashboard API python library";
     homepage = "https://github.com/meraki/dashboard-api-python";
-    changelog = "https://github.com/meraki/dashboard-api-python/releases/tag/${src.tag}";
+    changelog = "https://github.com/meraki/dashboard-api-python/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ dylanmtaylor ];
   };
-}
+})
