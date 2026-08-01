54cfddd7c621ddf51ed42772d0d52acbeda684ed
Jonathan Stucklen <stuckj@gmail.com>
2026-07-31 22:36:17 -0400

    maintainers: add stuckj

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -27246,6 +27246,12 @@
     githubId = 77596767;
     name = "Scott Teague";
   };
+  stuckj = {
+    email = "stuckj@gmail.com";
+    github = "stuckj";
+    githubId = 2205578;
+    name = "Jonathan Stucklen";
+  };
   stumoss = {
     email = "samoss@gmail.com";
     github = "stumoss";
