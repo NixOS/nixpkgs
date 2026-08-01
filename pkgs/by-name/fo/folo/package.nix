d9ac8c451fbdbb47a4436d885148963b55aef0b8
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-08-01 16:10:41 +0200

    folo: 0.11.0 -> 0.12.0

--- pkgs/by-name/fo/folo/package.nix
+++ pkgs/by-name/fo/folo/package.nix
@@ -15,13 +15,13 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "folo";
 
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchFromGitHub {
     owner = "RSSNext";
     repo = "Folo";
     tag = "desktop/v${finalAttrs.version}";
-    hash = "sha256-kJ5FX6whBPTPJoWBmgaMeDSJAF7pGl5WkGmDzQSKLpo=";
+    hash = "sha256-Nq0EKH9Em5U+iNoWG/aZP2eiDYcJOGQGScZIlbrQvTw=";
   };
 
   nativeBuildInputs = [
@@ -41,7 +41,7 @@ stdenv.mkDerivation (finalAttrs: {
       ;
     pnpm = pnpm_11;
     fetcherVersion = 4;
-    hash = "sha256-W8vzjbBBK3wvr6abIniW5oRpSJU45Gua6EjBTTA326I=";
+    hash = "sha256-HnDXcofw8xUM2W0hoFbjYbCV269XixVA/4hFjoGEVLA=";
   };
 
   __structuredAttrs = true;
