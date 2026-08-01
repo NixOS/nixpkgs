ca17ba08bc6a47a32306279845c6a76815c4290a
KangaZero <samuelyongw@gmail.com>
2026-08-01 21:14:56 +0900

    menumaker: 0.99.13 -> 0.99.14 `versionCheckHook` added

--- pkgs/by-name/me/menumaker/package.nix
+++ pkgs/by-name/me/menumaker/package.nix
@@ -2,18 +2,22 @@
   lib,
   fetchurl,
   python3Packages,
+  versionCheckHook,
 }:
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "menumaker";
-  version = "0.99.13";
+  version = "0.99.14";
 
   src = fetchurl {
     url = "mirror://sourceforge/menumaker/menumaker-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-JBXs5hnt1snbnB1hi7q7HBI7rNp0OoalLeIM0uJCdkE=";
+    sha256 = "sha256-EeldDnajuFD1ffgbxVYCIr1gFBxXUbbpvQXDzVVg1lo=";
   };
 
   pyproject = false;
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
 
   meta = {
     description = "Heuristics-driven menu generator for several window managers";
