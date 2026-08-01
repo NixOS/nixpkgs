53f730689f367a25f0793a3b3aaebd75e77cfacf
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-01 19:25:01 +0200

    vunnel: 0.63.0 -> 0.63.1
    Diff: https://github.com/anchore/vunnel/compare/v0.63.0...v0.63.1
    
    Changelog: https://github.com/anchore/vunnel/releases/tag/v0.63.1

--- pkgs/by-name/vu/vunnel/package.nix
+++ pkgs/by-name/vu/vunnel/package.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "vunnel";
-  version = "0.63.0";
+  version = "0.63.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "anchore";
     repo = "vunnel";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-FU8N4LTdfxpcC47OphM0GqTobYFKyl2AJS9Jv24fT/E=";
+    hash = "sha256-HdMpJRFwTK5iiIuAogoDD7V5lbdMkMx1jYmmIKNb9iE=";
     leaveDotGit = true;
   };
 
