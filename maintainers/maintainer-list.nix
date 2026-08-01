db2812a771cdafab82fb9720d34f47301cdbc015
millia ampora <198710911+dappermint@users.noreply.github.com>
2026-08-02 03:58:11 +0400

    maintainers: add dappermint

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6288,6 +6288,11 @@
     githubId = 28959268;
     keys = [ { fingerprint = "4779 D1D5 3C97 2EAE 34A5  ED3D D8AF C4BF 0567 0F9D"; } ];
   };
+  dappermint = {
+    github = "dappermint";
+    githubId = 198710911;
+    name = "millia ampora";
+  };
   dariof4 = {
     name = "dariof4";
     email = "dazedtank@gmail.com";
