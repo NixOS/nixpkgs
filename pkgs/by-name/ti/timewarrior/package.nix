72005daff0c821a2d81b3052a0a95c26cea09d70
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-02 18:49:14 +0000

    timewarrior: 1.9.1 -> 1.10.0

--- pkgs/by-name/ti/timewarrior/package.nix
+++ pkgs/by-name/ti/timewarrior/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "timewarrior";
-  version = "1.9.1";
+  version = "1.10.0";
 
   src = fetchFromGitHub {
     owner = "GothenburgBitFactory";
     repo = "timewarrior";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-wwuyXcLCrNvpDIQvoBt/OQlwoTMhPzGZ+WrZdTo6IPo=";
+    hash = "sha256-kJdPnjQ2ZXLx5/o6CmKnlrxadhYMpQlbkJv8tHUVcCM=";
     fetchSubmodules = true;
   };
 
