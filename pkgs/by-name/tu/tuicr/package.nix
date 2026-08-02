f62da7c25721cb1c057af26f1c1e3f21c16a3ed1
Gaetan Lepage <gaetan@glepage.com>
2026-08-02 17:05:39 +0000

    tuicr: 0.19.1 -> 0.20.0
    Diff: https://github.com/agavra/tuicr/compare/v0.19.1...v0.20.0
    
    Changelog: https://github.com/agavra/tuicr/blob/v0.20.0/CHANGELOG.md

--- pkgs/by-name/tu/tuicr/package.nix
+++ pkgs/by-name/tu/tuicr/package.nix
@@ -2,14 +2,14 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
-  openssl,
-  git,
+  gitMinimal,
+  versionCheckHook,
   nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "tuicr";
-  version = "0.19.1";
+  version = "0.20.0";
 
   __structuredAttrs = true;
 
@@ -17,26 +17,33 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "agavra";
     repo = "tuicr";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-uLtwpieKBTbLLDmgE4LLNljvv69i0cBRvU1WEgy09Xo=";
+    hash = "sha256-ByeWD4mC1DiH6L971mC7Qv6zUVmaf2lneewK2ZWXxg8=";
   };
 
-  cargoHash = "sha256-jEPgXXlqTgVX+GutQX8JCwtLS0J3cx7RV76NdM5m6QE=";
+  cargoHash = "sha256-MVDq+78HIwjqQiO7R/ZAXd16Bye0fktxbiRIzEOH7vQ=";
 
   strictDeps = true;
 
-  nativeCheckInputs = [ git ];
+  nativeCheckInputs = [
+    gitMinimal
+  ];
 
   checkFlags = [
     # expects to be run inside the upstream git repository
     "--skip=should_return_no_changes_for_clean_repo"
   ];
 
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+  doInstallCheck = true;
+
   passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Review AI-generated diffs like a GitHub pull request, right from your terminal";
     homepage = "https://tuicr.dev";
-    changelog = "https://github.com/agavra/tuicr/blob/v${finalAttrs.version}/CHANGELOG.md";
+    changelog = "https://github.com/agavra/tuicr/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ Br1ght0ne ];
     mainProgram = "tuicr";
