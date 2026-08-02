8c7d14b4958b6711aad63df5b25a9da749068588
cnst <adam@cnst.dev>
2026-08-02 10:39:25 +0200

    app2unit: 1.4.2 -> 1.4.4

--- pkgs/by-name/ap/app2unit/package.nix
+++ pkgs/by-name/ap/app2unit/package.nix
@@ -11,13 +11,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "app2unit";
-  version = "1.4.2";
+  version = "1.4.4";
 
   src = fetchFromGitHub {
     owner = "Vladimir-csp";
     repo = "app2unit";
     tag = "v${finalAttrs.version}";
-    sha256 = "sha256-jUAjcpR4IszvmqWAIjZo0rWZt9yydCe3xH4X+mJ5O8k=";
+    sha256 = "sha256-TIY+/9ekGub+10uyqXy5aYU+2NLysMtaQnD1PIjBCFA=";
   };
 
   passthru.updateScript = nix-update-script { };
