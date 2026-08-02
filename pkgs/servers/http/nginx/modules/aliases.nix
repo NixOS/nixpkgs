0b7688d3e00c60914ff06062133e25d0f332702e
Maximilian Bosch <maximilian@mbosch.me>
2026-08-02 12:05:55 +0200

    nginxModules.pagespeed: remove
    Repo was archived last year & build is broken.

--- pkgs/servers/http/nginx/modules/aliases.nix
+++ pkgs/servers/http/nginx/modules/aliases.nix
@@ -9,6 +9,7 @@ self: {
   modsecurity-nginx = self.modsecurity;
   ngx_aws_auth = throw "ngx_aws_auth was renamed to aws-auth";
   opentracing = throw "opentracing-cpp was removed because opentracing as been archived upstream"; # Added 2025-10-19
+  pagespeed = throw "pagespeed was removed because the upstream repo is archived & the build was broken"; # Added 2026-08-02
   statsd = throw "statsd was removed because its upstream source vanished"; # Added 2026-07-28
   # keep-sorted end
 }
