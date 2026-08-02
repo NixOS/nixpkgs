5a05d096dbd57272bdd22280f8e1d1cfa7cc8000
Sizhe Zhao <prc.zhao@outlook.com>
2026-08-02 10:12:46 +0800

    luau: 0.726 -> 0.732

--- pkgs/by-name/lu/luau/package.nix
+++ pkgs/by-name/lu/luau/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "luau";
-  version = "0.726";
+  version = "0.732";
 
   src = fetchFromGitHub {
     owner = "luau-lang";
     repo = "luau";
     tag = finalAttrs.version;
-    hash = "sha256-DAKyA8/ERX2AJ/XQDABOoI+cQCSW3PKnBpJkwS8IBdI=";
+    hash = "sha256-EyRYmH6iABKsjezOCBudv4z20zW0YRD7GpdkLcfJQoI=";
   };
 
   nativeBuildInputs = [ cmake ];
