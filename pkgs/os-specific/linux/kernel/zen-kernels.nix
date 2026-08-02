de7c4f3621806ecc4bae441811f190e82bd9d6ad
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-02 21:31:53 +0000

    linuxKernel.kernels.linux_zen: 7.1.4 -> 7.1.5

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -18,7 +18,7 @@ in
 buildLinux (
   args
   // rec {
-    version = "7.1.4";
+    version = "7.1.5";
     pname = "linux-zen";
     modDirVersion = lib.versions.pad 3 "${version}-${suffix}";
     isZen = true;
@@ -27,7 +27,7 @@ buildLinux (
       owner = "zen-kernel";
       repo = "zen-kernel";
       rev = "v${version}-${suffix}";
-      sha256 = "1k27xhzd390krk60kay6cabl0jlpb26m6cwvdh3r8izbwgx1r0ig";
+      sha256 = "11d3c2hx2py6fy6qj0dqk2w392m144wjrfg5ldfh93x9mp4ffhjd";
     };
 
     # This is based on the following source:
