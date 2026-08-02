e4f54e30793a5bb16638840c1601ec4331f6cc31
Bobby Rong <rjl931189261@126.com>
2026-08-02 13:26:58 +0800

    qogir-theme: Revert removal and remove GTK2
    This mostly reverts commit 196b94d22f265af1b9f0a19543348e7e48974c0c.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2044,7 +2044,6 @@ mapAliases {
   qMasterPassword-wayland = warnAlias "'qMasterPassword-wayland' has been renamed to/replaced by 'qmasterpassword-wayland'" qmasterpassword-wayland; # Added 2026-02-01
   qmenumodel = warnAlias "'qmenumodel' has been renamed to 'libsForQt5.qmenumodel'" libsForQt5.qmenumodel; # Added 2026-03-26
   qnial = throw "'qnial' has been removed due to failing to build and being unmaintained"; # Added 2025-06-26
-  qogir-theme = throw "'qogir-theme' has been removed because it depended on 'gtk-engine-murrine', which was removed because it was unmaintained upstream and depended on GTK 2."; # Added 2026-07-22
   qrscan = throw "qrscan has been removed, as it does not build with supported LLVM versions"; # Added 2025-08-19
   qscintilla = throw "'qscintilla' has been renamed to/replaced by 'libsForQt5.qscintilla'"; # Converted to throw 2025-10-27
   qscintilla-qt6 = throw "'qscintilla-qt6' has been renamed to/replaced by 'qt6Packages.qscintilla'"; # Converted to throw 2025-10-27
