7d5aed1a4a20530df669215803d3ed8258025ae1
Sigmanificient <edhyjox@gmail.com>
2026-08-02 23:24:10 +0200

    python3Packages.pygments-style-github: migrate to pyproject

--- pkgs/development/python-modules/pygments-style-github/default.nix
+++ pkgs/development/python-modules/pygments-style-github/default.nix
@@ -3,22 +3,25 @@
   buildPythonPackage,
   fetchPypi,
   pygments,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "pygments-style-github";
   version = "0.4";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-D8q9IxR9VMhiQPYhZ4xTyZin3vqg0naRHB8t7wpF9Kc=";
   };
 
+  build-system = [ setuptools ];
+
+  dependencies = [ pygments ];
+
   # no tests exist on upstream repo
   doCheck = false;
-
-  propagatedBuildInputs = [ pygments ];
 
   pythonImportsCheck = [ "pygments_style_github" ];
 
