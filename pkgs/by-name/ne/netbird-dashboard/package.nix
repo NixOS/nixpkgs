f17828a716f585694b6ee004a71a4c6eb08113b5
K900 <me@0upti.me>
2026-08-02 18:45:09 +0300

    netbird-dashboard: 2.90.8 -> 2.90.9
    Diff: https://github.com/netbirdio/dashboard/compare/v2.90.8...v2.90.9

--- pkgs/by-name/ne/netbird-dashboard/package.nix
+++ pkgs/by-name/ne/netbird-dashboard/package.nix
@@ -6,13 +6,13 @@
 
 buildNpmPackage rec {
   pname = "netbird-dashboard";
-  version = "2.90.8";
+  version = "2.90.9";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "dashboard";
     rev = "v${version}";
-    hash = "sha256-783g44brRVBU7b4YAaZ6Qk3RVf5zHKmN1eYU2Oe1aGo=";
+    hash = "sha256-cDA1Tbw3yiaeYHN9Pnbz1vxxCK45W6V7k3Bh/Lz/VLA=";
   };
 
   npmDepsHash = "sha256-A6zXrOPdxLepi7XPn67YsY673iFOAgJqCEynn4SYco8=";
