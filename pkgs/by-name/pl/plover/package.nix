2534dc8d69aebfa4df5d22c5a47a72e9b281c0f5
Yueh-Shun Li <shamrocklee@posteo.net>
2026-08-03 17:51:50 +0800

    plover: add passthru.withPlugins

--- pkgs/by-name/pl/plover/package.nix
+++ pkgs/by-name/pl/plover/package.nix
@@ -1,11 +1,40 @@
 {
   lib,
   config,
+  # For wrapper
+  callPackage,
   python3Packages,
   # For aliases
   plover_4,
 }:
-python3Packages.toPythonApplication python3Packages.plover
+
+(python3Packages.toPythonApplication python3Packages.plover).overrideAttrs (
+  finalAttrs: previousAttrs: {
+    passthru = previousAttrs.passthru or { } // {
+      /**
+        Shorthand of
+        ```nix
+        python3.plover.wrapPloverExes python3.withPlugins (ps: [ ps.plover ] ++ selectPackages ps)
+        ```
+        ::: {.note}
+        The result package is unrelated to the main package,
+        and is not affected by overriding applying to the main package.
+        Its `<pkg>.override` interface consists of only the `python3` argument,
+        whose `packageOverrides` can be overridden.
+        :::
+      */
+      withPlugins = callPackage ./with-plugins.nix { };
+      tests = {
+        plover-with-lapwing = finalAttrs.passthru.withPlugins (
+          ps: with ps; [
+            plover-lapwing-aio
+          ]
+        );
+      }
+      // previousAttrs.passthru.tests or { };
+    };
+  }
+)
 # Aliases to now-dropped plover.stable and plover.dev
 # Added 2026-04-22
 # TODO(@ShamrockLee): remove after Nixpkgs 25.11 EOL
