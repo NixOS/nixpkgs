147d8d43709bb11c27e83a290a3454cf65b9ad83
Euclid Projects <e.hasanov.250194@gmail.com>
2026-08-03 16:47:49 +0400

    Configure Budgie desktop with GDM and Wayland support

--- euclid-linux-3d/modules/desktop/budgie.nix
+++ euclid-linux-3d/modules/desktop/budgie.nix
@@ -1,33 +1,65 @@
-{ config, pkgs, lib, ... }: {
-  services.xserver.enable = true;
+{ config, pkgs, lib, ... }:
+
+{
+  # Budgie 10.10 uses Wayland.
   services.desktopManager.budgie.enable = true;
 
-  services.displayManager.sessionPackages = [
-    (pkgs.stdenv.mkDerivation {
-      name = "euclid-budgie-compiz-session";
-      src = pkgs.writeTextFile {
-        name = "euclid-budgie-compiz.desktop";
-        destination = "/share/xsessions/euclid-budgie-compiz.desktop";
-        text = ''
-          [Desktop Entry]
-          Name=Euclid Linux 3D Budgie+Compiz
-          Comment=Budgie Desktop with Compiz Reloaded
-          Exec=env BUDGIE_WM=compiz budgie-desktop
-          TryExec=budgie-desktop
-          Icon=euclid-session-budgie-compiz
-          Type=Application
-          DesktopNames=Budgie;GNOME
-        '';
-      };
-      passthru.providedSessions = [ "euclid-budgie-compiz" ];
-      phases = [ "installPhase" ];
-      installPhase = ''
-        mkdir -p $out/share/xsessions
-        cp $src/share/xsessions/euclid-budgie-compiz.desktop $out/share/xsessions/
-      '';
-    })
+  # LightDM is primarily intended for X11 sessions.
+  # Use GDM for the Budgie Wayland session.
+  services.displayManager.gdm.enable = true;
+
+  # Budgie's package advertises this session name.
+  services.displayManager.defaultSession = "budgie-desktop";
+
+  # Automatically enter the live ISO desktop.
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "nixos";
+  };
+
+  # Keep XWayland enabled so ordinary X11 applications can run
+  # inside the Budgie Wayland session.
+  programs.xwayland.enable = true;
+
+  # Useful desktop packages.
+  environment.systemPackages = with pkgs; [
+    budgie-desktop
+    budgie-control-center
+    gnome-terminal
+    nautilus
+    networkmanagerapplet
+    pavucontrol
   ];
 
-  # Set default session to the new one
-  services.displayManager.defaultSession = "euclid-budgie-compiz";
+  # NetworkManager is appropriate for a desktop/live ISO.
+  networking.networkmanager.enable = true;
+
+  # Audio.
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Common desktop services.
+  services.dbus.enable = true;
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+  services.udisks2.enable = true;
+  services.upower.enable = true;
+
+  # Fonts.
+  fonts.packages = with pkgs; [
+    dejavu_fonts
+    liberation_ttf
+    noto-fonts
+    noto-fonts-color-emoji
+  ];
+
+  # Avoid competing display-manager/session definitions.
+  services.xserver.displayManager.lightdm.enable =
+    lib.mkForce false;
 }
