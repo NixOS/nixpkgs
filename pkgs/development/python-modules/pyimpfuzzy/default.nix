a0963d351dca162aeb657e1b9a908600b9048741
Sigmanificient <edhyjox@gmail.com>
2026-08-03 23:44:50 +0200

    python3Packages.pyimpfuzzy: migrate to pyproject

--- pkgs/development/python-modules/pyimpfuzzy/default.nix
+++ pkgs/development/python-modules/pyimpfuzzy/default.nix
@@ -4,20 +4,23 @@
   fetchPypi,
   ssdeep,
   pefile,
+  setuptools,
 }:
 buildPythonPackage rec {
   pname = "pyimpfuzzy";
   version = "0.5";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "da9796df302db4b04a197128637f84988f1882f1e08fdd69bbf9fdc6cfbaf349";
   };
 
+  build-system = [ setuptools ];
+
   buildInputs = [ ssdeep ];
 
-  propagatedBuildInputs = [ pefile ];
+  dependencies = [ pefile ];
 
   # no tests
   doCheck = false;
