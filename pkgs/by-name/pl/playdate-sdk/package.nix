634617b8ab2563c8b87e75fdd806cc61dc61cc1d
Christopher Kaster <me@atomicptr.de>
2026-08-03 15:35:55 +0200

    playdate-sdk: 3.1.0 -> 3.1.1

--- pkgs/by-name/pl/playdate-sdk/package.nix
+++ pkgs/by-name/pl/playdate-sdk/package.nix
@@ -14,11 +14,11 @@
 
 stdenv.mkDerivation rec {
   pname = "playdate-sdk";
-  version = "3.1.0";
+  version = "3.1.1";
 
   src = fetchurl {
     url = "https://download.panic.com/playdate_sdk/Linux/PlaydateSDK-${version}.tar.gz";
-    sha256 = "sha256-yKEl3UpVY3lB9LxAsx0RotTsNHv8WS76ZqpzRi6BSZg=";
+    sha256 = "sha256-K2RXTlZAoT/lZFlE+LUWnrX05MzgQ0gt3B1sRzXhFWs=";
   };
 
   srcScript = ./playdate-simulator-wrapper.sh;
