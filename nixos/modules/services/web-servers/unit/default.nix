0ba91d58c4014960c52d4c4de144a27f73ef2661
Maximilian Bosch <maximilian@mbosch.me>
2026-08-04 00:24:35 +0200

    nixos/unit: rm MemoryDenyWriteExecute
    This package has a direct dependency on pcre2. By removing the sealloc
    allocator, which allows MemoryDenyWriteExecute, but is experimental and
    not recommended for production, this option needs to be turned off
    again.

--- nixos/modules/services/web-servers/unit/default.nix
+++ nixos/modules/services/web-servers/unit/default.nix
@@ -139,7 +139,7 @@ in
           "AF_INET6"
         ];
         LockPersonality = true;
-        MemoryDenyWriteExecute = true;
+        MemoryDenyWriteExecute = false; # pcre2 jit
         RestrictRealtime = true;
         RestrictSUIDSGID = true;
         PrivateMounts = true;
