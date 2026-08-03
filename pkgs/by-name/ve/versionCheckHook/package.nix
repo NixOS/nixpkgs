a2659c802750377e3fe9d1cf9f31aaa2478aaf8a
h7x4 <h7x4@nani.wtf>
2026-08-03 14:25:32 +0900

    versionCheckHook: enable `__structuredAttrs`

--- pkgs/by-name/ve/versionCheckHook/package.nix
+++ pkgs/by-name/ve/versionCheckHook/package.nix
@@ -10,6 +10,7 @@ makeSetupHook {
     storeDir = builtins.storeDir;
     envCommand = lib.getExe' coreutils "env"; # Cannot call it env, because it isn't an attrset of environment variables!
   };
+  __structuredAttrs = true;
   meta = {
     description = "Lookup for $version in the output of --help and --version";
     maintainers = with lib.maintainers; [ doronbehar ];
