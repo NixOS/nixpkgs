6592fd266b04aaa0405caee985cca09bb1eaec9b
Kerstin Humm <kerstin@erictapen.name>
2026-08-03 15:48:22 +0200

    python3Packages.weblate-language-data: 2026.8 -> 2026.10
    Diff: https://github.com/WeblateOrg/language-data/compare/2026.8...2026.10
    
    Changelog: https://github.com/WeblateOrg/language-data/releases/tag/2026.10

--- pkgs/development/python-modules/weblate-language-data/default.nix
+++ pkgs/development/python-modules/weblate-language-data/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "weblate-language-data";
-  version = "2026.8";
+  version = "2026.10";
   pyproject = true;
 
   # nixpkgs-update: no auto update
@@ -17,7 +17,7 @@ buildPythonPackage (finalAttrs: {
     owner = "WeblateOrg";
     repo = "language-data";
     tag = finalAttrs.version;
-    hash = "sha256-7wNm6bu2L1/eF5D49wSYu1qfVC5Fl5MbaSbXO/az4F4=";
+    hash = "sha256-/kfdgsOT+6RulyzJuUsXK2gpUebaqNN58N66EzshwS0=";
   };
 
   build-system = [ setuptools ];
