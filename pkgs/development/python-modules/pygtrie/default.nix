2c04506687df0dd08963911b12f842ae6999879d
Sigmanificient <edhyjox@gmail.com>
2026-08-03 23:25:01 +0200

    python3Packages.pygtrie: migrate to pyproject

--- pkgs/development/python-modules/pygtrie/default.nix
+++ pkgs/development/python-modules/pygtrie/default.nix
@@ -2,15 +2,21 @@
   lib,
   fetchPypi,
   buildPythonPackage,
+  setuptools,
 }:
 buildPythonPackage rec {
   pname = "pygtrie";
   version = "2.5.0";
-  format = "setuptools";
+
+  pyproject = true;
+
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-IDUUrYJutAPasdLi3dA04NFTS75NvgITuwWT9mvrpOI=";
   };
+
+  build-system = [ setuptools ];
+
   meta = {
     homepage = "https://github.com/mina86/pygtrie";
     description = "Trie data structure implementation";
