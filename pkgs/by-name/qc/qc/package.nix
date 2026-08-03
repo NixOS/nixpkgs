ff84cb43ad488016ea71940d4fdcf930a3876607
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-03 19:10:10 +0000

    qc: 0.6.2 -> 0.6.3

--- pkgs/by-name/qc/qc/package.nix
+++ pkgs/by-name/qc/qc/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "qc";
-  version = "0.6.2";
+  version = "0.6.3";
 
   src = fetchFromGitHub {
     owner = "qownnotes";
     repo = "qc";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Y7SjlVNiZjWDTRPNZfyoFjI5qyo2SHgTPurNJzGmN0k=";
+    hash = "sha256-Z96wEb9jTf3zeIxgHJMBl7OQHeEIrP/uIcJncXggA/g=";
   };
 
-  vendorHash = "sha256-ad4IuGv2y4L9cS7pf/fEVJ3wXwy9pEIegMTbUoJHPmg=";
+  vendorHash = "sha256-/nRPv6SlvWV8mHlQstV19BLou9iwGt/VvJbrpVwiTCU=";
 
   ldflags = [
     "-s"
