6a89b5f910749d24a078598b956e706dbff2d439
ZariTen <zariten0@outlook.com>
2026-08-03 11:03:01 -0300

    maintainers: add ZariTen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -31955,6 +31955,12 @@
     githubId = 3449926;
     name = "David Costa";
   };
+  ZariTen = {
+    email = "ZaritenProt@proton.me";
+    github = "ZariTen";
+    githubId = 48529745;
+    name = "ZariTen";
+  };
   zatm8 = {
     email = "maxis1191@gmail.com";
     github = "mourogurt";
