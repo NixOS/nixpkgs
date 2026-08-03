62d666d341ad9fcbb4cc0efcccc63a8da804982d
Stefan Frijters <sfrijters@gmail.com>
2026-08-03 12:21:29 +0200

    json-c: enable structuredAttrs, use tag

--- pkgs/by-name/js/json_c/package.nix
+++ pkgs/by-name/js/json_c/package.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "json-c";
     repo = "json-c";
-    rev = "json-c-0.18-20240915";
+    tag = "json-c-0.18-20240915";
     hash = "sha256-UyMXr8Vc6kDOx1/lD2YKPiHdaTotXAF9ak0yQuwrSUA=";
   };
 
@@ -28,6 +28,8 @@ stdenv.mkDerivation (finalAttrs: {
   cmakeFlags = [
     (lib.cmakeBool "BUILD_APPS" false)
   ];
+
+  __structuredAttrs = true;
 
   meta = {
     description = "JSON implementation in C";
