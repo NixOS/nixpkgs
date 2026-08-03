0400ed18d5eaa6066c49553a395f231bd8590344
cktiel <tiel@tuta.io>
2026-08-03 10:42:50 +0200

    maintainers: add cktiel

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5283,6 +5283,12 @@
     name = "Christian Kruse";
     keys = [ { fingerprint = "BC5D 9F4E F7FB 4382 6056  E834 B8E0 F342 A99A 9D73"; } ];
   };
+  cktiel = {
+    email = "tiel@tuta.io";
+    github = "cktiel";
+    githubId = 310042450;
+    name = "Artur Cierocki";
+  };
   clacke = {
     email = "claes.wallin@greatsinodevelopment.com";
     github = "clacke";
