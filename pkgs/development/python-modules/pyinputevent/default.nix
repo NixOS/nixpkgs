ee1c953397de0f684497f9651f525365efefa1e2
Sigmanificient <edhyjox@gmail.com>
2026-08-04 00:01:51 +0200

    python3Packages.pyinputevent: set __structuredAttrs, use sri-hash

--- pkgs/development/python-modules/pyinputevent/default.nix
+++ pkgs/development/python-modules/pyinputevent/default.nix
@@ -9,13 +9,14 @@ buildPythonPackage {
   pname = "pyinputevent";
   version = "0.1-unstable-2015-10-18";
 
+  __structuredAttrs = true;
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ntzrmtthihu777";
     repo = "pyinputevent";
     rev = "d2075fa5db5d8a402735fe788bb33cf9fe272a5b";
-    sha256 = "0rkis0xp8f9jc00x7jb9kbvhdla24z1vl30djqa6wy6fx0cr6sib";
+    hash = "sha256-K2qTGejOeG4Ulg0MusMnQtEG95ppydMBYDI5dDvQcWY=";
   };
 
   build-system = [ setuptools ];
