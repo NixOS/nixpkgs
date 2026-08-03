7ab5a3bc6bd2d3dcd0193ded0a2411dd1f409553
whispers <whispersofthedawn@duck.com>
2026-08-03 11:59:49 -0400

    ssh-askpass-fullscreen: drop
    ssh-askpass-fullscreen provides an askpass GUI using GTK 2, and does not
    support any newer versions. It has received very few changes in many
    years (https://github.com/atj/ssh-askpass-fullscreen/blob/master/ChangeLog),
    and in an issue about migrating to GTK 3 or 4 upstream, the author said
    that they did not use the application any longer and there has been no
    activity on the issue since 2022. Thus, it seems unlikely that it will
    migrate away in the forseeable future.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2244,6 +2244,7 @@ mapAliases {
   sqlbag = throw "sqlbag has been removed because it has been marked as broken since May 2024."; # Added 2025-10-11
   squirreldisk = throw "'squirreldisk' has been removed as it depended on webkitgtk 4.0"; # Added 2026-06-07
   src = throw "The \"src\" package has been renamed to \"simple-revision-control\". If you encounter this error and did not intend to use that package you may have a falsely constructed overlay."; # Added 2025-11-19
+  ssh-ashkpass-fullscreen = throw "'ssh-askpass-fullscreen' has been removed as it depended on the deprecated GTK2 engine."; # Added 2026-08-03
   ssm-agent = throw "'ssm-agent' has been renamed to/replaced by 'amazon-ssm-agent'"; # Converted to throw 2025-10-27
   stacer = throw "'stacer' has been removed because it was abandoned upstream and relied upon vulnerable software"; # Added 2025-11-08
   stalwart = warnAlias "`stalwart` is currently pinned to `0.15.5`. If `0.16.x` is needed, it is available as `stalwart_0_16`. Note: `stalwart_0_16` is not compatible with `services.stalwart` at this time." stalwart_0_15;
