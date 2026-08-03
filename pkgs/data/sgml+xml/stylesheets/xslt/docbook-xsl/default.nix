7e696cf8d9e59e452d0e683f93596fe63f6e37b8
Stefan Frijters <sfrijters@gmail.com>
2026-08-03 14:29:18 +0200

    docbook-xsl-*: enable structuredAttrs, use finalAttrs

--- pkgs/data/sgml+xml/stylesheets/xslt/docbook-xsl/default.nix
+++ pkgs/data/sgml+xml/stylesheets/xslt/docbook-xsl/default.nix
@@ -21,12 +21,12 @@ let
     }:
     let
       legacySuffix = lib.optionalString (suffix != "-nons") "-ns";
-      self = stdenv.mkDerivation rec {
+      self = stdenv.mkDerivation (finalAttrs: {
         inherit pname;
         version = "1.79.2";
 
         src = fetchurl {
-          url = "https://github.com/docbook/xslt10-stylesheets/releases/download/release%2F${version}/docbook-xsl${suffix}-${version}.tar.bz2";
+          url = "https://github.com/docbook/xslt10-stylesheets/releases/download/release%2F${finalAttrs.version}/docbook-xsl${suffix}-${finalAttrs.version}.tar.bz2";
           inherit sha256;
         };
 
@@ -50,7 +50,8 @@ let
 
           # Add legacy sourceforge.net URIs to the catalog
           (replaceVars ./catalog-legacy-uris.patch {
-            inherit legacySuffix suffix version;
+            inherit legacySuffix suffix;
+            inherit (finalAttrs) version;
           })
         ]
         ++ lib.optionals withManOptDedupPatch [
@@ -66,7 +67,7 @@ let
         dontBuild = true;
 
         installPhase = ''
-          dst=$out/share/xml/${pname}
+          dst=$out/share/xml/${finalAttrs.pname}
           mkdir -p $dst
           rm -rf RELEASE* README* INSTALL TODO NEWS* BUGS install.sh tools Makefile tests extensions webhelp
           mv * $dst/
@@ -81,8 +82,10 @@ let
 
         passthru.dbtoepub = writeScriptBin "dbtoepub" ''
           #!${bash}/bin/bash
-          exec -a dbtoepub ${ruby}/bin/ruby ${self}/share/xml/${pname}/epub/bin/dbtoepub "$@"
+          exec -a dbtoepub ${ruby}/bin/ruby ${self}/share/xml/${finalAttrs.pname}/epub/bin/dbtoepub "$@"
         '';
+
+        __structuredAttrs = true;
 
         meta = {
           homepage = "https://github.com/docbook/wiki/wiki/DocBookXslStylesheets";
@@ -91,7 +94,7 @@ let
           maintainers = [ ];
           platforms = lib.platforms.all;
         };
-      };
+      });
     in
     self;
 
