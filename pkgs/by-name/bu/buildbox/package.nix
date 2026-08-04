04e06ccb8441b18c98b53b99221564f5bcf1dbaf
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-04 19:05:26 +0000

    buildbox: 1.4.13 -> 1.4.15

--- pkgs/by-name/bu/buildbox/package.nix
+++ pkgs/by-name/bu/buildbox/package.nix
@@ -23,13 +23,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "buildbox";
-  version = "1.4.13";
+  version = "1.4.15";
 
   src = fetchFromGitLab {
     owner = "BuildGrid";
     repo = "buildbox/buildbox";
     tag = finalAttrs.version;
-    hash = "sha256-W/QV65A7zm2vUdvJRrR+bxe8O3Zogzrx86RmqFVCY84=";
+    hash = "sha256-V/dKo/ynKL37NNCY36D5NvepeIbE470qg3qPKH1jLoY=";
   };
 
   nativeBuildInputs = [
