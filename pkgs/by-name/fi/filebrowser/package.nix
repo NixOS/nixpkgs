0217af142fd76aee2dbe28ae4b6773ffdbfaac58
Hritwik <git@thorin.theoakenshield.com>
2026-08-04 12:51:18 +0530

    filebrowser: 2.63.18 -> 2.63.23
    Final upstream release: File Browser is winding down and its repository
    will be archived on 2026-09-01, after which there will be no further
    releases, bug fixes, or security fixes.
    
    https://github.com/filebrowser/filebrowser/releases/tag/v2.63.23
    
    Not-cherry-picked-because: master builds the frontend with pnpmBuildHook, which does not exist on release-26.05

--- pkgs/by-name/fi/filebrowser/package.nix
+++ pkgs/by-name/fi/filebrowser/package.nix
@@ -12,13 +12,13 @@
 }:
 
 let
-  version = "2.63.18";
+  version = "2.63.23";
 
   src = fetchFromGitHub {
     owner = "filebrowser";
     repo = "filebrowser";
     rev = "v${version}";
-    hash = "sha256-0j0i6bKKbyUi4O0wBT+xYjvywjRzAGd0/13Yh/dG5GA=";
+    hash = "sha256-G0TIQE+Rru4JWBJIi8kdxSaP0CDPo2DyWLmcU2AX7Fs=";
   };
 
   frontend = buildNpmPackage rec {
@@ -41,7 +41,7 @@ let
         ;
       fetcherVersion = 3;
       pnpm = pnpm_10;
-      hash = "sha256-UwTA7Eogp2GrvmXDbdfGBTJS3DuOTJ42e6fHlQxSHoA=";
+      hash = "sha256-XZdHaXnIfjA1wO6KQihj6PwXQ5LEcMrLRe2Md65nQ38=";
     };
 
     installPhase = ''
@@ -59,7 +59,7 @@ buildGoModule {
   pname = "filebrowser";
   inherit version src;
 
-  vendorHash = "sha256-BXw+fURCh1qNlwWo49aXIpSM339bV3Gwn9Ov8HLEVF0=";
+  vendorHash = "sha256-CuYi2PfR0F0lppFiRFzFj0yLms7VFNxzKpzlmEaCWWs=";
 
   excludedPackages = [ "tools" ];
 
