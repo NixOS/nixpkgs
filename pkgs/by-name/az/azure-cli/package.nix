2afbd79b44e3436cff6e4a28ff48bb97936353b6
Paul Meyer <katexochen0@gmail.com>
2026-08-04 08:52:33 +0200

    azure-cli: 2.88.0 -> 2.89.0
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- pkgs/by-name/az/azure-cli/package.nix
+++ pkgs/by-name/az/azure-cli/package.nix
@@ -26,14 +26,14 @@
 }:
 
 let
-  version = "2.88.0";
+  version = "2.89.0";
 
   src = fetchFromGitHub {
     name = "azure-cli-${version}-src";
     owner = "Azure";
     repo = "azure-cli";
     tag = "azure-cli-${version}";
-    hash = "sha256-9lDDzUuON1fkZzvV6oAzUOZcf+t7biDzPFufZIYQrAY=";
+    hash = "sha256-bNKPp/O5YOhne9OOEjextcXVNtGl1TP+vyFUBmOXt7I=";
   };
 
   # put packages that needs to be overridden in the py package scope
