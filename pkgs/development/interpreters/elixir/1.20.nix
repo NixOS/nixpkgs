cb112c1524c4c1714db97aec7feed642452b424c
Adam C. Stephens <adam@valkor.net>
2026-08-04 19:18:07 -0400

    beamPackages.elixir_1_20: 1.20.2 -> 1.20.3
    Changelog: https://github.com/elixir-lang/elixir/releases/tag/v1.20.3
    (cherry picked from commit 34996e1398d80618eb5e4a1062eb3205afa6f4e0)

--- pkgs/development/interpreters/elixir/1.20.nix
+++ pkgs/development/interpreters/elixir/1.20.nix
@@ -1,7 +1,7 @@
 import ./generic-builder.nix {
-  version = "1.20.2";
-  hash = "sha256-KSRsXQhh3PX7SUNhuw/POg74XfjkPiZDsv9wdNwFrwA=";
-  # https://hexdocs.pm/elixir/1.20.0/compatibility-and-deprecations.html#between-elixir-and-erlang-otp
+  version = "1.20.3";
+  hash = "sha256-60DlK+yocWcKnxcOtUOkRO69scaY35AADoKFCCF6QfQ=";
+  # https://hexdocs.pm/elixir/1.20.3/compatibility-and-deprecations.html#between-elixir-and-erlang-otp
   minimumOTPVersion = "27";
   maximumOTPVersion = "29";
 }
