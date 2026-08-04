ebd16f67dbb2f4c4ad58518da484903c959b9232
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-04 18:12:21 +0000

    shader-slang: 2026.14 -> 2026.14.1

--- pkgs/by-name/sh/shader-slang/package.nix
+++ pkgs/by-name/sh/shader-slang/package.nix
@@ -21,13 +21,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "shader-slang";
-  version = "2026.14";
+  version = "2026.14.1";
 
   src = fetchFromGitHub {
     owner = "shader-slang";
     repo = "slang";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-D/D0V7960g3AD9XM9FA1H/NP2sapFLO4UyH1MJ7O5Fo=";
+    hash = "sha256-D4n2WsZMzHS6cLpaKlPvoz/JPBEzAMff1LOxNbpHPgg=";
     fetchSubmodules = true;
   };
 
