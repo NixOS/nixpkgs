e5fd3bb86bd990368a5f2e1cee4c13137bfe43a1
Anthony Roussel <anthony@roussel.dev>
2026-08-04 22:23:20 +0200

    python3Packages.reno: use stestrCheckHook

--- pkgs/development/python-modules/reno/default.nix
+++ pkgs/development/python-modules/reno/default.nix
@@ -10,9 +10,10 @@
   pyyaml,
   setuptools,
   sphinx,
-  stestr,
+  stestrCheckHook,
   testtools,
   testscenarios,
+  writableTmpDirAsHomeHook,
 }:
 
 buildPythonPackage (finalAttrs: {
@@ -44,35 +45,31 @@ buildPythonPackage (finalAttrs: {
     # Python packages
     docutils
     sphinx
-    stestr
+    stestrCheckHook
     testtools
     testscenarios
+    writableTmpDirAsHomeHook
 
     # Required programs to run all tests
     git
     gnupg
   ];
 
-  checkPhase = ''
-    runHook preCheck
-    export HOME=$(mktemp -d)
-    stestr run -e <(echo "
-      # Expects to be run from a git repository
-      reno.tests.test_cache.TestCache.test_build_cache_db
-      reno.tests.test_semver.TestSemVer.test_major_post_release
-      reno.tests.test_semver.TestSemVer.test_major_working_and_post_release
-      reno.tests.test_semver.TestSemVer.test_major_working_copy
-      reno.tests.test_semver.TestSemVer.test_minor_post_release
-      reno.tests.test_semver.TestSemVer.test_minor_working_and_post_release
-      reno.tests.test_semver.TestSemVer.test_minor_working_copy
-      reno.tests.test_semver.TestSemVer.test_patch_post_release
-      reno.tests.test_semver.TestSemVer.test_patch_working_and_post_release
-      reno.tests.test_semver.TestSemVer.test_patch_working_copy
-      reno.tests.test_semver.TestSemVer.test_same
-      reno.tests.test_semver.TestSemVer.test_same_with_note
-    ")
-    runHook postCheck
-  '';
+  disabledTests = [
+    # Expects to be run from a git repository
+    "reno.tests.test_cache.TestCache.test_build_cache_db"
+    "reno.tests.test_semver.TestSemVer.test_major_post_release"
+    "reno.tests.test_semver.TestSemVer.test_major_working_and_post_release"
+    "reno.tests.test_semver.TestSemVer.test_major_working_copy"
+    "reno.tests.test_semver.TestSemVer.test_minor_post_release"
+    "reno.tests.test_semver.TestSemVer.test_minor_working_and_post_release"
+    "reno.tests.test_semver.TestSemVer.test_minor_working_copy"
+    "reno.tests.test_semver.TestSemVer.test_patch_post_release"
+    "reno.tests.test_semver.TestSemVer.test_patch_working_and_post_release"
+    "reno.tests.test_semver.TestSemVer.test_patch_working_copy"
+    "reno.tests.test_semver.TestSemVer.test_same"
+    "reno.tests.test_semver.TestSemVer.test_same_with_note"
+  ];
 
   pythonImportsCheck = [ "reno" ];
 
