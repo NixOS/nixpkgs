c2c2e67f864dfc9ea9de3f9cd887dcd93601084f
weriomat <engel@weriomat.com>
2026-08-05 15:45:49 +0200

    syncstorage-rs: add weriomat as maintainer

--- pkgs/by-name/sy/syncstorage-rs/package.nix
+++ pkgs/by-name/sy/syncstorage-rs/package.nix
@@ -98,7 +98,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
     homepage = "https://github.com/mozilla-services/syncstorage-rs";
     changelog = "https://github.com/mozilla-services/syncstorage-rs/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.mpl20;
-    maintainers = with lib.maintainers; [ titaniumtown ];
+    maintainers = with lib.maintainers; [
+      titaniumtown
+      weriomat
+    ];
     platforms = lib.platforms.linux;
     mainProgram = "syncserver";
   };
