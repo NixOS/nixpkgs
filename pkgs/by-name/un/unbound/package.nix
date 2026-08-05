6a988575ed7bea91518fea2d5bff843e41e17b16
Stefan Frijters <sfrijters@gmail.com>
2026-08-05 15:35:01 +0200

    unbound: enable structuredAttrs

--- pkgs/by-name/un/unbound/package.nix
+++ pkgs/by-name/un/unbound/package.nix
@@ -234,6 +234,8 @@ stdenv.mkDerivation (finalAttrs: {
     };
   };
 
+  __structuredAttrs = true;
+
   meta = {
     description = "Validating, recursive, and caching DNS resolver";
     license = lib.licenses.bsd3;
