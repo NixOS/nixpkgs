e6356e1c28f6a07c47a87fbf917df386cc0f3974
h7x4 <h7x4@nani.wtf>
2026-08-05 18:47:33 +0900

    doc: add section about `nukeReferences`

--- pkgs/build-support/nuke-references/default.nix
+++ pkgs/build-support/nuke-references/default.nix
@@ -1,7 +1,4 @@
-# The program `nuke-refs' created by this derivation replaces all
-# references to the Nix store in the specified files by a non-existent
-# path (/nix/store/eeee...).  This is useful for getting rid of
-# dependencies that you know are not actually needed at runtime.
+# See https://nixos.org/manual/nixpkgs/unstable/#pkgs-nukereferences
 
 {
   lib,
