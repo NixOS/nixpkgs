7ccd9165edcbdbe3ab9830fdcc0edd8c2379bad5
Stefan Frijters <sfrijters@gmail.com>
2026-08-05 11:28:10 +0200

    musl-fts: enable structuredAttrs, use tag

--- pkgs/by-name/mu/musl-fts/package.nix
+++ pkgs/by-name/mu/musl-fts/package.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "void-linux";
     repo = "musl-fts";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     sha256 = "Azw5qrz6OKDcpYydE6jXzVxSM5A8oYWAztrHr+O/DOE=";
   };
 
@@ -25,6 +25,8 @@ stdenv.mkDerivation (finalAttrs: {
   strictDeps = true;
 
   enableParallelBuilding = true;
+
+  __structuredAttrs = true;
 
   meta = {
     homepage = "https://github.com/void-linux/musl-fts";
