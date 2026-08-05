f9f2dfb7e19cb2145dbb27bea75b2f42bafe2868
Anthony Roussel <anthony@roussel.dev>
2026-08-05 11:23:46 +0200

    octodns-providers.transip: fix build
    https://github.com/octodns/octodns-transip/pull/71

--- pkgs/by-name/oc/octodns/providers/transip/package.nix
+++ pkgs/by-name/oc/octodns/providers/transip/package.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   octodns,
   pytestCheckHook,
   setuptools,
@@ -18,6 +19,15 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-O9KhHjCdRt5lejwEqpv0OCwIXaqWVc2/u4ghzbYMiBA=";
   };
+
+  patches = [
+    # Update hex comparison to be case insensitive in tests
+    (fetchpatch {
+      name = "octodns-transip-pull-71.patch";
+      url = "https://github.com/octodns/octodns-transip/commit/100fe61c5ff20a8fce319a48bfc7bbfddc995a48.patch";
+      hash = "sha256-RfLAZSDVjLKUw/2g25vSySGrJdlb0I+HfBZE6hLtc6Q=";
+    })
+  ];
 
   build-system = [
     setuptools
@@ -36,6 +46,12 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [
     pytestCheckHook
+  ];
+
+  pytestFlags = [
+    # Ignore octoDNS 2.0 deprecation warnings
+    "-W"
+    "ignore::DeprecationWarning"
   ];
 
   meta = {
