ad9c4fac527c77ccb83d2af14db1ace5f60ca97e
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-05 13:01:02 +0000

    tektoncd-cli: 0.45.1 -> 0.46.0

--- pkgs/by-name/te/tektoncd-cli/package.nix
+++ pkgs/by-name/te/tektoncd-cli/package.nix
@@ -14,13 +14,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "tektoncd-cli";
-  version = "0.45.1";
+  version = "0.46.0";
 
   src = fetchFromGitHub {
     owner = "tektoncd";
     repo = "cli";
     tag = "v${finalAttrs.version}";
-    sha256 = "sha256-KkxNX0wIteW0DQkoEGshIITumMqoHTcJtFZUwdzTm4k=";
+    sha256 = "sha256-BEYwvGi/Mt/DkHRR2TU42ItLC57CVKnb7ZBy3TKBoZs=";
   };
 
   vendorHash = null;
