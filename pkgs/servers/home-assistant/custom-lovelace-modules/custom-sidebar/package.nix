0c59e27562f654a01e1fd285aab6845cfff8f489
Robert Schütz <nix@dotlambda.de>
2026-08-05 12:04:16 -0700

    home-assistant-custom-lovelace-modules.custom-sidebar: 16.1.0 -> 17.0.0
    Diff: https://github.com/elchininet/custom-sidebar/compare/v16.1.0...v17.0.0
    
    Changelog: https://github.com/elchininet/custom-sidebar/releases/tag/v17.0.0

--- pkgs/servers/home-assistant/custom-lovelace-modules/custom-sidebar/package.nix
+++ pkgs/servers/home-assistant/custom-lovelace-modules/custom-sidebar/package.nix
@@ -12,20 +12,20 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "custom-sidebar";
-  version = "16.1.0";
+  version = "17.0.0";
 
   src = fetchFromGitHub {
     owner = "elchininet";
     repo = "custom-sidebar";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-41q0GxwX8jQEX9TOjSqLw9VU4wA/7D7tM0CTgKBYNcg=";
+    hash = "sha256-ggUF1C9Evq87boXpxJzeDAkKRAXf/8qZLDHJO0ZHswI=";
   };
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     inherit pnpm;
     fetcherVersion = 3;
-    hash = "sha256-cwyhL2fC4j7XDayS/kaVCTOTdQ0BqfUgC7kp+l1fk8M=";
+    hash = "sha256-xHGWqtv6rAp1+1DT18IegUdfBsJDR/WBNeNffjcIt00=";
   };
 
   nativeBuildInputs = [
