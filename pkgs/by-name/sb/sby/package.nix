6f0bb85d3c6c6b55c2920028b0f061199af975ab
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-06 17:42:05 +0000

    sby: 0.61 -> 0.68

--- pkgs/by-name/sb/sby/package.nix
+++ pkgs/by-name/sb/sby/package.nix
@@ -18,13 +18,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sby";
-  version = "0.61";
+  version = "0.68";
 
   src = fetchFromGitHub {
     owner = "YosysHQ";
     repo = "sby";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-pFtSXg8DiN//jkZJyAIJ/jpVvu1OwwfAAXSrrmCZ3SQ=";
+    hash = "sha256-WRZp4+gwUgDKCWAdBK/36ArM2KFGyLBZ20S32k7YN+8=";
   };
 
   postPatch = ''
