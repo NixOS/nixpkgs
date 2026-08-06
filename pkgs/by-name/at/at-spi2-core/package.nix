325dd5145b490d7fdfe9d1a92aea7b9e2f18ef20
Jan Tojnar <jtojnar@gmail.com>
2026-08-06 00:15:10 +0000

    at-spi2-core: 2.60.5 → 2.60.6
    https://gitlab.gnome.org/GNOME/at-spi2-core/-/compare/2.60.5...2.60.6
    (cherry picked from commit 59cd134da0474d5361ca9fa8486bc7a49b67d39f)

--- pkgs/by-name/at/at-spi2-core/package.nix
+++ pkgs/by-name/at/at-spi2-core/package.nix
@@ -29,7 +29,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "at-spi2-core";
-  version = "2.60.5";
+  version = "2.60.6";
 
   outputs = [
     "out"
@@ -39,7 +39,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/at-spi2-core/${lib.versions.majorMinor finalAttrs.version}/at-spi2-core-${finalAttrs.version}.tar.xz";
-    hash = "sha256-YFmnfVB0OP9sjW0GAl+Pn1d0+g+Oq+nJsFmxzEHhu8A=";
+    hash = "sha256-qJtkqLIXqAQr3w41y/q2Kc7uNWQNunXfV4r96ap4nVc=";
   };
 
   nativeBuildInputs = [
