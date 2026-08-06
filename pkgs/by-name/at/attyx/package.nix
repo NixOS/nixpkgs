c6c84c94094d0315c41016d6823e5e3a91273393
Aaron Jheng <wentworth@outlook.com>
2026-08-06 14:28:53 +0800

    attyx: 0.4.15 -> 0.4.16

--- pkgs/by-name/at/attyx/package.nix
+++ pkgs/by-name/at/attyx/package.nix
@@ -18,13 +18,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "attyx";
-  version = "0.4.15";
+  version = "0.4.16";
 
   src = fetchFromGitHub {
     owner = "semos-labs";
     repo = "attyx";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-WB3qMFmVmv/FkZX60MTKFewk3kEA3yDEoaUFL2LY1bQ=";
+    hash = "sha256-j6VahzpBZFd6lOtBlqrjK5oZ7sPwet6+FZcZ2fn3STE=";
   };
 
   deps = callPackage ./build.zig.zon.nix { };
