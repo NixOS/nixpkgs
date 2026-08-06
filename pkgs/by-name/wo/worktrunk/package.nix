b234d5ff7c6422ed11b29bc71bda2844055963b0
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-06 17:45:12 +0000

    worktrunk: 0.68.0 -> 0.71.0

--- pkgs/by-name/wo/worktrunk/package.nix
+++ pkgs/by-name/wo/worktrunk/package.nix
@@ -11,16 +11,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "worktrunk";
-  version = "0.68.0";
+  version = "0.71.0";
 
   src = fetchFromGitHub {
     owner = "max-sixty";
     repo = "worktrunk";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-4mxWRNNrpM5Fo49Xm8ypzBS15Y8kPPFd1iPod1RwxjA=";
+    hash = "sha256-TBna2iFzrVyfa4D/rDysQdZPS5yHEv/mTY8ixc4GilE=";
   };
 
-  cargoHash = "sha256-ZEv3peP/mjDDWYw4LNuhIt8I806W/yfUKtEA7e3t7rA=";
+  cargoHash = "sha256-UdSWEP9m/b9xtEtyX5JWvF4LU2uJdbk4juYrKwrx4gA=";
 
   cargoBuildFlags = [ "--package=worktrunk" ];
 
