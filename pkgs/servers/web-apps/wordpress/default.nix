72d3f61444118a9eae8861be3d413ba104b218e1
Bart Oostveen <bart@bartoostveen.nl>
2026-08-06 21:36:49 +0200

    wordpress_6_8: 6.8.6 -> 6.8.7
    Fixes (high-severity) security vulnerabilities, of which a priviledge escalation and a multitude of XSS vulnerabilities.
    
    Fixes: GHSA-52p2-r8wf-jcrf
    Fixes: CVE-2026-64638
    Announcement: https://wordpress.org/news/2026/08/wordpress-7-0-3-release/

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -2,8 +2,8 @@
 builtins.mapAttrs (_: callPackage ./generic.nix) rec {
   wordpress = wordpress_7_0;
   wordpress_6_8 = {
-    version = "6.8.6";
-    hash = "sha256-ID5C12xn+GprUynX8WLS5dl9iJyv13yDxYbqMDPB8P4=";
+    version = "6.8.7";
+    hash = "sha256-Fu0P83henBEdNbXXVb7cKc69zcLrGyRMx7LcTea+if0=";
   };
   wordpress_6_9 = {
     version = "6.9.5";
