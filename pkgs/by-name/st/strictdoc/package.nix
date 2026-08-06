cf140204da377c4f3959bfb09822ab08acec99b7
Tim Schubert <dadada@dadada.li>
2026-08-06 15:00:25 +0200

    strictdoc: add dadada as to maintainers

--- pkgs/by-name/st/strictdoc/package.nix
+++ pkgs/by-name/st/strictdoc/package.nix
@@ -81,7 +81,10 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
     homepage = "https://github.com/strictdoc-project/strictdoc";
     changelog = "https://github.com/strictdoc-project/strictdoc/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
-    maintainers = [ lib.maintainers.puzzlewolf ];
+    maintainers = with lib.maintainers; [
+      dadada
+      puzzlewolf
+    ];
     mainProgram = "strictdoc";
   };
 })
