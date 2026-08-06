41e26579fc314b224627eb6e8f0721b1555410e8
D3vil0p3r <crazysnob@live.it>
2026-08-06 16:10:16 +0200

    hayabusa-sec: 3.7.0-unstable-2025-12-02 -> 4.0.0

--- pkgs/by-name/ha/hayabusa-sec/package.nix
+++ pkgs/by-name/ha/hayabusa-sec/package.nix
@@ -3,26 +3,26 @@
   rustPlatform,
   fetchFromGitHub,
   makeWrapper,
-  unstableGitUpdater,
+  nix-update-script,
   pkg-config,
   openssl,
   rust-jemalloc-sys,
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "hayabusa-sec";
-  version = "3.7.0-unstable-2025-12-02";
+  version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = "Yamato-Security";
     repo = "hayabusa";
-    rev = "1c4f332b446f20af154257b2e9b581f7bcb4b1a2";
-    hash = "sha256-JWb54yudfB6pOMZca8sFeoRqNA7M//xJ3IBKfIcGBnM=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-MabwaHKbbC8fbnICkVMA+bu7zBasIztMR4m0ro8vhYA=";
     # Include the hayabusa-rules
     fetchSubmodules = true;
   };
 
-  cargoHash = "sha256-JIHkFokaZ+nt1hW+gRxFrb1DVZcm4jsZKT12gx/BRCA=";
+  cargoHash = "sha256-PbzMVJPyBOfpS9j3d0RHOlFNJLApU1Gc5O1ro2LROYY=";
 
   nativeBuildInputs = [
     makeWrapper
@@ -40,6 +40,7 @@ rustPlatform.buildRustPackage {
   # Skipping individual checks causes failure as `--skip` flags
   # end up passed to executing `hayabusa`
   # > error: unexpected argument '--skip' found
+
   doCheck = false;
 
   postInstall = ''
@@ -49,7 +50,7 @@ rustPlatform.buildRustPackage {
     makeWrapper $out/share/hayabusa-sec/hayabusa $out/bin/hayabusa
   '';
 
-  passthru.updateScript = unstableGitUpdater { };
+  passthru.updateScript = nix-update-script { rev-prefix = "v"; };
 
   meta = {
     description = "Sigma-based threat hunting and fast forensics timeline generator for Windows event logs";
@@ -60,4 +61,4 @@ rustPlatform.buildRustPackage {
     ];
     mainProgram = "hayabusa";
   };
-}
+})
