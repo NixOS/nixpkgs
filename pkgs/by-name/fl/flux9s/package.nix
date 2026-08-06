404da34e7a81eac1a69c000dce2ea6800a7a9529
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-06 12:14:03 +0000

    flux9s: 1.0.0 -> 1.0.1

--- pkgs/by-name/fl/flux9s/package.nix
+++ pkgs/by-name/fl/flux9s/package.nix
@@ -9,16 +9,16 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "flux9s";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "dgunzy";
     repo = "flux9s";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-DpXTWnFjQ023cWEe46Qu+m200vVNnraSq7RNVcr9GxM=";
+    hash = "sha256-7ZxGzhbEuNZA2eBGOVil6PqbZa4GawBjl0qj4Jeh+18=";
   };
 
-  cargoHash = "sha256-R3dEHn6XY+6q6kZcp43T/ASGiEIqvtlORRJ9VOgW9f0=";
+  cargoHash = "sha256-go1HfGDufV/XDhsgHbvTThaHQSlfdLQXx/i6ZqG3h/s=";
 
   nativeBuildInputs = [ pkg-config ];
 
