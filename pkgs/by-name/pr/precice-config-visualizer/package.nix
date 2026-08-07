6bb0ed77a70b2f7add9569941cd56f0268de377c
Sigmanificient <edhyjox@gmail.com>
2026-08-07 03:23:52 +0200

    precice-config-visualizer: fix version date
    https://github.com/precice/config-visualizer/commit/60f2165f25352c8261f370dc4ceb64a8b422d4ec was made at 2022-01-31

--- pkgs/by-name/pr/precice-config-visualizer/package.nix
+++ pkgs/by-name/pr/precice-config-visualizer/package.nix
@@ -6,7 +6,7 @@
 
 python3Packages.buildPythonApplication {
   pname = "config-visualizer";
-  version = "unstable-2022-02-23";
+  version = "unstable-2022-01-31";
   pyproject = true;
 
   src = fetchFromGitHub {
