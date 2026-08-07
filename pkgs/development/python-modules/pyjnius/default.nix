ca5fcedc5d6eeb2d1e941a3f889a5d57f291d979
Sigmanificient <edhyjox@gmail.com>
2026-08-08 00:49:53 +0200

    python3Packages.pyjnius: moernize

--- pkgs/development/python-modules/pyjnius/default.nix
+++ pkgs/development/python-modules/pyjnius/default.nix
@@ -7,14 +7,15 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pyjnius";
   version = "1.7.0";
 
+  __structuredAttrs = true;
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-n4FwhISwqE6tPrC6hOU6xXnkxDyhDHRvmJip891Q9U0=";
   };
 
@@ -35,8 +36,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python module to access Java classes as Python classes using the Java Native Interface (JNI)";
     homepage = "https://github.com/kivy/pyjnius";
-    changelog = "https://github.com/kivy/pyjnius/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/kivy/pyjnius/blob/${finalAttrs.version}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ifurther ];
   };
-}
+})
