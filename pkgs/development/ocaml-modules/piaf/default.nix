9b9ba3931c0c6d808ac39966bfe4755c09f5db71
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-08-07 09:04:20 +0200

    ocamlPackages.piaf: fix build

--- pkgs/development/ocaml-modules/piaf/default.nix
+++ pkgs/development/ocaml-modules/piaf/default.nix
@@ -2,6 +2,7 @@
   alcotest,
   buildDunePackage,
   fetchurl,
+  fetchpatch,
   eio-ssl,
   faraday,
   h2-eio,
@@ -29,6 +30,21 @@ buildDunePackage (finalAttrs: {
     url = "https://github.com/anmonteiro/piaf/releases/download/${finalAttrs.version}/piaf-${finalAttrs.version}.tbz";
     hash = "sha256-B/qQCaUvrqrm2GEW51AH9SebGFx7x8laq5RV8hBzcPs=";
   };
+
+  patches =
+    # Compatibility with eio 1.4
+    lib.optionals (lib.versionAtLeast eio_main.version "1.4") [
+      (fetchpatch {
+        url = "https://github.com/anmonteiro/piaf/commit/5b2f44683fb7eabdb78f43848c9e0a448e741fc3.patch";
+        includes = [ "lib/*.ml" ];
+        hash = "sha256-/Y3OQtqoA4DCgs3Ai2EPCipMb/xSwDAN7+6MTFoHKXI=";
+      })
+      (fetchpatch {
+        url = "https://github.com/anmonteiro/piaf/commit/b8f5e94deea6e653025c37131bbf6833dc4c4c85.patch";
+        includes = [ "lib/*.ml" ];
+        hash = "sha256-gazYJFlM9OeuixpJnLwANZj/DVqdc8vacd1a6v35SpM=";
+      })
+    ];
 
   propagatedBuildInputs = [
     eio-ssl
