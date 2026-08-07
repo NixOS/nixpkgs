b3f2fbe6578f6072a2691136df53271b5bcea9cc
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-07 23:41:52 +0000

    cargo-arc: 0.3.0 -> 0.3.1

--- pkgs/by-name/ca/cargo-arc/package.nix
+++ pkgs/by-name/ca/cargo-arc/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cargo-arc";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "seflue";
     repo = "cargo-arc";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Jf23dACQW2LbfINWq98wNf1rwqueM4bPFFwQIdDT+3Y=";
+    hash = "sha256-tAES0CP7kGS9RYDsXqiVc/Q8q/APci5m55kTjpIAXNg=";
   };
 
-  cargoHash = "sha256-kKAL5ZgOZOt7Lmu2Epe7B9/FrayGAUguUcb9HPi3gj0=";
+  cargoHash = "sha256-kqFHu1+BYQQZKCr1WVAWoTzuwkPNfxY34jhtffN9MVE=";
 
   checkFlags = [
     # Tries to create temp dir
