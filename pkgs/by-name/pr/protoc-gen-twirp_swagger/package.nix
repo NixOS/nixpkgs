a170efc55d89f7db4a43830ac0e22e0a8c5611ce
Sigmanificient <edhyjox@gmail.com>
2026-08-07 03:23:42 +0200

    protoc-gen-twirp_swagger: fix version date
    https://github.com/elliots/protoc-gen-twirp_swagger/commit/f21ef47d69e37c1602a7fb26146de05c092d30b6 was made at 2020-05-02

--- pkgs/by-name/pr/protoc-gen-twirp_swagger/package.nix
+++ pkgs/by-name/pr/protoc-gen-twirp_swagger/package.nix
@@ -6,7 +6,7 @@
 
 buildGoModule {
   pname = "protoc-gen-twirp_swagger";
-  version = "0-unstable-2021-03-29";
+  version = "0-unstable-2020-05-02";
 
   src = fetchFromGitHub {
     owner = "elliots";
