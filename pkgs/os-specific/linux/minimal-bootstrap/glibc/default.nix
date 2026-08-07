27e645c60c68b79db0131b9e0dfc337b7bedfac6
Aleksi Hannula <ahannula4+nixgit@gmail.com>
2026-08-07 19:49:24 +0300

    minimal-bootstrap.glibc: fix for cross-compilation

--- pkgs/os-specific/linux/minimal-bootstrap/glibc/default.nix
+++ pkgs/os-specific/linux/minimal-bootstrap/glibc/default.nix
@@ -27,14 +27,8 @@ let
     url = "mirror://gnu/libc/glibc-${version}.tar.xz";
     hash = "sha256-N/YA8r7zxegwAUcFlWiyouQKetbMxlzpQlVtSUKcxmc=";
   };
-
-  linkerFile =
-    {
-      x86_64-linux = "ld-linux-x86-64";
-      i686-linux = "ld-linux";
-    }
-    .${buildPlatform.system};
-
+  # FIXME: eventually, prefix unconditionally
+  binutilsTargetPrefix = "";
 in
 bash.runCommand "${pname}-${version}"
   {
@@ -71,10 +65,17 @@ bash.runCommand "${pname}-${version}"
     # Configure
     mkdir build
     cd build
+
+    export CFLAGS="-B${libgcc}/lib -L${libgcc}/lib -Wl,-rpath,${gcc}/lib,-rpath,${libgcc} -O2 -Wno-error=attribute-alias -Wno-error=maybe-uninitialized -fexceptions"
+    export AR=${binutils}/bin/${binutilsTargetPrefix}ar
+    export OBJCOPY=${binutils}/bin/${binutilsTargetPrefix}objcopy
+    export OBJDUMP=${binutils}/bin/${binutilsTargetPrefix}objdump
+    export NM=${binutils}/bin/${binutilsTargetPrefix}nm
+    export READELF=${binutils}/bin/${binutilsTargetPrefix}readelf
+    export STRIP=${binutils}/bin/${binutilsTargetPrefix}strip
+    export LDFLAGS="-L${libgcc}/lib -L${libgcc}/lib/gcc/${hostPlatform.config}/${libgcc.version} -B${libgcc}/lib -B${libgcc}/lib/gcc/${hostPlatform.config}/${libgcc.version}"
     # libstdc++.so is built against musl and fails to link
     export CXX=false
-    export CFLAGS="-B${libgcc}/lib -L${libgcc}/lib -Wl,-rpath,${gcc}/lib,-rpath,${libgcc} -O2 -Wno-error=attribute-alias -Wno-error=maybe-uninitialized -fexceptions"
-    export LDFLAGS="-L${libgcc}/lib -L${libgcc}/lib/gcc/${hostPlatform.config}/${libgcc.version} -B${libgcc}/lib -B${libgcc}/lib/gcc/${hostPlatform.config}/${libgcc.version}"
     bash ../configure \
       --prefix=$out \
       --build=${buildPlatform.config} \
@@ -85,7 +86,8 @@ bash.runCommand "${pname}-${version}"
       --disable-build-nscd \
       --disable-profile \
       --disable-timezone-tools \
-      --disable-mathvec
+      --disable-mathvec \
+      --with-binutils=${binutils}/bin
 
     # Build
     make -j $NIX_BUILD_CORES sysdep-LDFLAGS="$LDFLAGS"
@@ -93,7 +95,7 @@ bash.runCommand "${pname}-${version}"
     # Install
     make -j $NIX_BUILD_CORES INSTALL_UNCOMPRESSED=yes install
     ln -s $(ls -d ${linux-headers}/include/* | grep -v scsi\$) $out/include/
-    find $out/{bin,sbin,lib,libexec} -type f -exec strip --strip-unneeded {} + || true
+    find $out/{bin,sbin,lib,libexec} -type f -exec ${binutilsTargetPrefix}strip --strip-unneeded {} + || true
 
     # localedef + iconv() are never invoked downstream of this glibc
     rm -rf $out/share/i18n $out/lib/gconv $out/share/locale
