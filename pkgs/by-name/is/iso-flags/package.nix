775d27b0e64002fb1ec6f974dff86be698cd0852
Sigmanificient <edhyjox@gmail.com>
2026-08-07 03:23:39 +0200

    iso-flags-png-320x240: fix version date
    https://github.com/joielechong/iso-country-flags-svg-collection/commit/9ebbd577b9a70fbfd9a1931be80c66e0d2f31a9d was made at 2018-01-18

--- pkgs/by-name/is/iso-flags/package.nix
+++ pkgs/by-name/is/iso-flags/package.nix
@@ -10,7 +10,7 @@
 
 stdenvNoCC.mkDerivation {
   pname = "iso-flags";
-  version = "0-unstable-2020-01-18";
+  version = "0-unstable-2018-01-18";
 
   src = fetchFromGitHub {
     owner = "joielechong";
