bf53a061f95fcaebe3d02851c54e77f943999dc8
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 23:04:51 +0200

    qt6.qtwayland: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtwayland.nix
+++ pkgs/development/libraries/qt-6/modules/qtwayland.nix
@@ -17,9 +17,14 @@ qtModule {
   ];
   buildInputs = [ libdrm ];
 
-  cmakeFlags = lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
-    "-DQt6WaylandScannerTools_DIR=${pkgsBuildBuild.qt6.qtbase}/lib/cmake/Qt6WaylandScannerTools"
-  ];
+  # Conditional is required to prevent infinite recursion during a cross build
+  cmakeFlags =
+    lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+      "-DQt6WaylandScannerTools_DIR=${pkgsBuildBuild.qt6.qtbase}/lib/cmake/Qt6WaylandScannerTools"
+    ]
+    ++ [
+      "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+    ];
 
   meta = {
     platforms = lib.platforms.unix;
