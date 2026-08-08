f2f539845c48ec1bcb1f081c3f88b1d7fa8ff20b
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-08 21:45:59 +0000

    python3Packages.somajo: 2.4.3 -> 2.5.0

--- pkgs/development/python-modules/somajo/default.nix
+++ pkgs/development/python-modules/somajo/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "somajo";
-  version = "2.4.3";
+  version = "2.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tsproisl";
     repo = "SoMaJo";
     tag = "v${version}";
-    hash = "sha256-fq891LX6PukUEfrXplulhnisuPX/RqLAQ/5ty/Fvm9k=";
+    hash = "sha256-2ddFfwTZGAWBnZprkD5qTBezAOl9DaraNwwWWVGQz8I=";
   };
 
   build-system = [ setuptools ];
