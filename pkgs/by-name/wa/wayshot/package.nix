7faf61649280bcf5510978f1b7f682ecd0af76c4
id3v1669 <git@id3v1669.com>
2026-08-08 22:22:14 +0800

    wayshot: 1.5.0 -> 1.6.0 & fix manpages & update packaging & update meta

--- pkgs/by-name/wa/wayshot/package.nix
+++ pkgs/by-name/wa/wayshot/package.nix
@@ -1,42 +1,63 @@
 {
   lib,
-  libgbm,
-  libjxl,
-  libGL,
   stdenv,
   fetchFromGitHub,
   nix-update-script,
   pango,
+  scdoc,
   pkg-config,
   installShellFiles,
   rustPlatform,
   wayland,
+  jpegSupport ? true,
+  pnmSupport ? true,
+  qoiSupport ? true,
+  webpSupport ? true,
+  avifSupport ? true,
+  jxlSupport ? true,
+  clipboardSupport ? true,
+  colorPickerSupport ? true,
+  completionsSupport ? true,
+  loggerSupport ? true,
+  notificationsSupport ? true,
+  selectorSupport ? true,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "wayshot";
-  version = "1.5.0";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "waycrate";
     repo = "wayshot";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-sbY3h3FoWxDmxSng9YvYpt3kyasVJGsykYC/7tblFn8=";
+    hash = "sha256-MihZAAZ+K95jBE1gp0kne/+Z3rBUDqlphmyBI0FI1jE=";
   };
   nativeBuildInputs = [
     pkg-config
     installShellFiles
+    scdoc
   ];
-  buildInputs = [
-    pango
-    libgbm
-    libjxl
-    libGL
-    wayland
-  ];
-  cargoHash = "sha256-J7ZKWx258bBCNBd061aCeKgTdcWMUF4yzAiIa9l8ZRA=";
+  buildInputs = [ wayland ] ++ lib.optional (selectorSupport || colorPickerSupport) pango;
+
+  cargoHash = "sha256-SAI+KXRV4E130ROmNyXraaO31D341CRWXGuIPV8depA=";
+
+  buildNoDefaultFeatures = true;
+  buildFeatures =
+    lib.optional jpegSupport "jpeg"
+    ++ lib.optional pnmSupport "pnm"
+    ++ lib.optional qoiSupport "qoi"
+    ++ lib.optional webpSupport "webp"
+    ++ lib.optional avifSupport "avif"
+    ++ lib.optional jxlSupport "jxl"
+    ++ lib.optional clipboardSupport "clipboard"
+    ++ lib.optional colorPickerSupport "color_picker"
+    ++ lib.optional completionsSupport "completions"
+    ++ lib.optional loggerSupport "logger"
+    ++ lib.optional notificationsSupport "notifications"
+    ++ lib.optional selectorSupport "selector";
 
   postInstall = ''
-    installManPage docs/wayshot.1.scd docs/wayshot.5.scd docs/wayshot.7.scd
+    installManPage docs/wayshot.1.gz docs/wayshot.5.gz docs/wayshot.7.gz
   ''
   + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
     installShellCompletion --cmd wayshot \
@@ -48,9 +69,9 @@ rustPlatform.buildRustPackage (finalAttrs: {
   passthru.updateScript = nix-update-script { };
 
   meta = {
-    description = "Native, blazing-fast screenshot tool for wlroots based compositors such as sway and river";
-    homepage = "https://github.com/waycrate/wayshot";
-    license = lib.licenses.bsd2;
+    description = "Screenshot crate for wlroots based compositors implementing the zwlr_screencopy_v1 protocol.";
+    homepage = "https://crates.io/crates/wayshot";
+    license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [
       id3v1669
       Subserial
