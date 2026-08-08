aa8c9c3134f9bb8f545b422ba316c2c65fb82b38
BatteredBunny <ayes2022@protonmail.com>
2026-08-08 23:05:46 +0300

    kdePackages.kdegraphics-mobipocket: add darwin support

--- pkgs/kde/gear/kdegraphics-mobipocket/default.nix
+++ pkgs/kde/gear/kdegraphics-mobipocket/default.nix
@@ -1,9 +1,12 @@
 {
   mkKdeDerivation,
   qt5compat,
+  lib,
 }:
 mkKdeDerivation {
   pname = "kdegraphics-mobipocket";
 
   extraBuildInputs = [ qt5compat ];
+
+  meta.platforms = lib.platforms.unix;
 }
