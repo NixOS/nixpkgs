1f477a7edab6bd1c481c53a4d81b445bbad8af66
Sigmanificient <edhyjox@gmail.com>
2026-08-08 20:12:08 +0200

    python3Packages.pytweening: migrate to pyproject

--- pkgs/development/python-modules/pytweening/default.nix
+++ pkgs/development/python-modules/pytweening/default.nix
@@ -2,16 +2,19 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 buildPythonPackage rec {
   pname = "pytweening";
   version = "1.2.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-JDMYt3NmmAZsXzYuxcK2Q07PQpfDyOfKqKv+avTKxxs=";
   };
+
+  build-system = [ setuptools ];
 
   pythonImportsCheck = [ "pytweening" ];
   checkPhase = ''
