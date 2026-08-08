5138046cc9e153977631db8d51394499b376bf56
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 23:06:47 +0200

    python3Packages.pyside6: enable strictDeps, structuredAttrs, fix cross

--- pkgs/development/python-modules/pyside6/default.nix
+++ pkgs/development/python-modules/pyside6/default.nix
@@ -10,6 +10,8 @@
   shiboken6,
   llvmPackages,
   symlinkJoin,
+  buildPackages,
+  pkgsBuildBuild,
 }:
 let
   packages = with python.pkgs.qt6; [
@@ -36,7 +38,6 @@ let
     qtsvg
     qtwebchannel
     qtwebsockets
-    qtwebview
     qtpositioning
     qtlocation
     qtshadertools
@@ -44,6 +45,10 @@ let
     qtserialbus
     qtgraphs
     qttools
+  ]
+  # qtwebview is broken in cross
+  ++ lib.optionals (stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    qtwebview
   ];
   qt_linked = symlinkJoin {
     name = "qt_linked";
@@ -90,6 +95,8 @@ stdenv.mkDerivation (finalAttrs: {
     cmake
     ninja
     python
+  ]
+  ++ lib.optionals (stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
     pythonImportsCheckHook
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [ moveBuildTree ];
@@ -98,7 +105,11 @@ stdenv.mkDerivation (finalAttrs: {
     if stdenv.hostPlatform.isLinux then
       # qtwebengine fails under darwin
       # see https://github.com/NixOS/nixpkgs/pull/312987
-      packages ++ [ python.pkgs.qt6.qtwebengine ]
+      packages
+      # qtwebengine is broken in cross
+      ++ lib.optionals (stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+        python.pkgs.qt6.qtwebengine
+      ]
     else
       python.pkgs.qt6.darwinVersionInputs
       ++ [
@@ -109,9 +120,18 @@ stdenv.mkDerivation (finalAttrs: {
 
   propagatedBuildInputs = [ shiboken6 ];
 
+  strictDeps = true;
+
   cmakeFlags = [
     "-DBUILD_TESTS=OFF"
     "-Dis_pyside6_superproject_build=1"
+    # Needed for cross, similarly to the way the indivudual Qt6 modules are built
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+    "-DQt6Quick3DTools_DIR=${pkgsBuildBuild.qt6.qtquick3d}/lib/cmake/Qt6Quick3DTools"
+    "-DQt6RemoteObjectsTools_DIR=${pkgsBuildBuild.qt6.qtremoteobjects}/lib/cmake/Qt6RemoteObjectsTools"
+    "-DQt6ScxmlTools_DIR=${pkgsBuildBuild.qt6.qtscxml}/lib/cmake/Qt6ScxmlTools"
+    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderToolsTools"
   ];
 
   dontWrapQtApps = true;
@@ -119,7 +139,7 @@ stdenv.mkDerivation (finalAttrs: {
   postInstall = ''
     cd ../../..
     chmod +w .
-    ${python.pythonOnBuildForHost.interpreter} setup.py egg_info --build-type=pyside6
+    python3 setup.py egg_info --build-type=pyside6 --qtpaths=${lib.getExe' buildPackages.python3.pkgs.qt6.qtbase "qtpaths"}
     cp -r PySide6.egg-info $out/${python.sitePackages}/
 
     mkdir -p "$devtools"
@@ -127,6 +147,8 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   pythonImportsCheck = [ "PySide6" ];
+
+  __structuredAttrs = true;
 
   meta = {
     description = "Python bindings for Qt";
