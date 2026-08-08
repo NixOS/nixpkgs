e4e3eee6706990068023df590dd12edf8162145d
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-08 08:50:29 +0200

    python3Packages.mypy-boto3-mediapackagev2: 1.43.55 -> 1.43.67

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -874,8 +874,8 @@ in
       "sha256-5AqWiNGz9jemWb8dZkuGQXxPXIruMdDWcoRzbT+ZGro=";
 
   mypy-boto3-mediapackagev2 =
-    buildMypyBoto3Package "mediapackagev2" "1.43.55"
-      "sha256-ZMHmbq4kd8J7N30nfRUoPS5+tlBMvOGzq7FkmSOS7wk=";
+    buildMypyBoto3Package "mediapackagev2" "1.43.67"
+      "sha256-zgO/4TSdGI7TDa1diqAQ7S56T/G7vON5HoTcIxSv/sE=";
 
   mypy-boto3-mediastore =
     buildMypyBoto3Package "mediastore" "1.43.0"
