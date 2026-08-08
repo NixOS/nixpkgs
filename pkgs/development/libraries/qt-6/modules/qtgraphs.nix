5185472e04d7811444d34a03b0448fa850ff490f
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 22:54:51 +0200

    qt6.qtgraphs: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtgraphs.nix
+++ pkgs/development/libraries/qt-6/modules/qtgraphs.nix
@@ -4,6 +4,7 @@
   qtdeclarative,
   qtquick3d,
   qtquicktimeline,
+  pkgsBuildBuild,
 }:
 
 qtModule {
@@ -13,5 +14,11 @@ qtModule {
     qtdeclarative
     qtquick3d
     qtquicktimeline
+  ];
+
+  cmakeFlags = [
+    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderToolsTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+    "-DQt6Quick3DTools_DIR=${pkgsBuildBuild.qt6.qtquick3d}/lib/cmake/Qt6Quick3DTools"
   ];
 }
