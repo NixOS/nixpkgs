8b2208be46ecc9842aba7c90e9150807e9ec06c4
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 22:57:34 +0200

    qt6.qtpositioning: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtpositioning.nix
+++ pkgs/development/libraries/qt-6/modules/qtpositioning.nix
@@ -4,6 +4,7 @@
   qtdeclarative,
   qtserialport,
   openssl,
+  pkgsBuildBuild,
 }:
 
 qtModule {
@@ -14,4 +15,10 @@ qtModule {
     qtserialport
   ];
   buildInputs = [ openssl ];
+
+  cmakeFlags = [
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderToolsTools"
+  ];
 }
