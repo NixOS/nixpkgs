9bbbb90e881b52184d435c2b10d7c822316f6396
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 22:55:04 +0200

    qt6.qtgrpc: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtgrpc.nix
+++ pkgs/development/libraries/qt-6/modules/qtgrpc.nix
@@ -1,9 +1,12 @@
 {
   qtModule,
+  lib,
+  stdenv,
   qtbase,
   qtdeclarative,
   protobuf,
   grpc,
+  pkgsBuildBuild,
 }:
 
 qtModule {
@@ -17,4 +20,20 @@ qtModule {
     protobuf
     grpc
   ];
+
+  nativeBuildInputs = [
+    protobuf # for protoc executable
+  ];
+
+  # Conditional is required to prevent infinite recursion during a cross build
+  cmakeFlags =
+    lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+      "-DQt6GrpcTools_DIR=${pkgsBuildBuild.qt6.qtgrpc}/lib/cmake/Qt6GrpcTools"
+      "-DQt6ProtobufTools_DIR=${pkgsBuildBuild.qt6.qtgrpc}/lib/cmake/Qt6ProtobufTools"
+    ]
+    ++ [
+      "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+      "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    ];
+
 }
