3234d295e5cea60ef3c92c4d3dde1e77af5bd119
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 22:54:28 +0200

    qt6.qtdeclarative: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtdeclarative/default.nix
+++ pkgs/development/libraries/qt-6/modules/qtdeclarative/default.nix
@@ -59,12 +59,13 @@ qtModule {
   ];
 
   cmakeFlags = [
-    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderTools"
+    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderToolsTools"
     # for some reason doesn't get found automatically on Darwin
     "-DPython_EXECUTABLE=${lib.getExe pkgsBuildBuild.python3}"
   ]
   # Conditional is required to prevent infinite recursion during a cross build
   ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
     "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
   ];
 
