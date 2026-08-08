0c379a546144dbbecdc65db2a588f28184d73759
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 23:01:56 +0200

    qt6.qtshadertools: add comment for consistency

--- pkgs/development/libraries/qt-6/modules/qtshadertools.nix
+++ pkgs/development/libraries/qt-6/modules/qtshadertools.nix
@@ -9,6 +9,7 @@
 qtModule {
   pname = "qtshadertools";
   propagatedBuildInputs = [ qtbase ];
+  # Conditional is required to prevent infinite recursion during a cross build
   cmakeFlags = lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
     "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderToolsTools"
   ];
