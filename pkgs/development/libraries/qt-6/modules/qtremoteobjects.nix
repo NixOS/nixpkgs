631ecd648e2e6d13d6793df044c9369effe954ad
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 23:00:24 +0200

    qt6.qtremoteobjects: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtremoteobjects.nix
+++ pkgs/development/libraries/qt-6/modules/qtremoteobjects.nix
@@ -1,7 +1,10 @@
 {
+  lib,
+  stdenv,
   qtModule,
   qtbase,
   qtdeclarative,
+  pkgsBuildBuild,
 }:
 
 qtModule {
@@ -10,4 +13,13 @@ qtModule {
     qtbase
     qtdeclarative
   ];
+
+  # Conditional is required to prevent infinite recursion during a cross build
+  cmakeFlags =
+    lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+      "-DQt6RemoteObjectsTools_DIR=${pkgsBuildBuild.qt6.qtremoteobjects}/lib/cmake/Qt6RemoteObjectsTools"
+    ]
+    ++ [
+      "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+    ];
 }
