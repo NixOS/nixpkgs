cb742086e8d1121bf2ad651a5f2798e95054e3d3
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-08 19:38:37 +0000

    metabigor: 2.1.0 -> 2.2.0

--- pkgs/by-name/me/metabigor/package.nix
+++ pkgs/by-name/me/metabigor/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "metabigor";
-  version = "2.1.0";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     owner = "j3ssie";
     repo = "metabigor";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-sFrcre4S2VTmKFzBLCkFXLr+HW/WZ0XhMEFl7N1WgOA=";
+    hash = "sha256-zlgPlCNNE6y4L4+Urw/EbNMwzSnOajILDHaT7HPVRqM=";
   };
 
-  vendorHash = "sha256-GN069be+ZUXBA0d8Mq4ffgTM8JHlDhlVZCw2AyAxVHw=";
+  vendorHash = "sha256-hqFu2sUh2M0vO7/Zm46IW0Zlbx9Q8uwEnD8WXL8SC14=";
 
   ldflags = [
     "-w"
