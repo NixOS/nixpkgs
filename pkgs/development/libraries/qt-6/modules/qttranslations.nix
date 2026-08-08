ed6d18287747b569bd4a54762add10eea0c9485c
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 23:03:40 +0200

    qt6.qttranslations: fix cross build

--- pkgs/development/libraries/qt-6/modules/qttranslations.nix
+++ pkgs/development/libraries/qt-6/modules/qttranslations.nix
@@ -1,12 +1,24 @@
 {
   qtModule,
+  lib,
+  stdenv,
+  python3,
   qttools,
+  pkgsBuildBuild,
 }:
 
 qtModule {
   pname = "qttranslations";
-  nativeBuildInputs = [ qttools ];
+  nativeBuildInputs = [
+    qttools
+    python3
+  ];
   separateDebugInfo = false;
   outputs = [ "out" ];
   allowedReferences = [ "out" ];
+
+  # Conditional is required to prevent infinite recursion during a cross build
+  cmakeFlags = lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6LinguistTools_DIR=${pkgsBuildBuild.qt6.qttools}/lib/cmake/Qt6LinguistTools"
+  ];
 }
