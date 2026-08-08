63747bb4adc95e96803ee723791e97532c22506d
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 22:58:15 +0200

    qt6.qtquick3d: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtquick3d.nix
+++ pkgs/development/libraries/qt-6/modules/qtquick3d.nix
@@ -1,8 +1,11 @@
 {
   qtModule,
+  stdenv,
+  lib,
   qtbase,
   qtdeclarative,
   openssl,
+  pkgsBuildBuild,
 }:
 
 qtModule {
@@ -12,4 +15,14 @@ qtModule {
     qtdeclarative
   ];
   buildInputs = [ openssl ];
+
+  cmakeFlags = [
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+    # "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderToolsTools"
+  ]
+  # Conditional is required to prevent infinite recursion during a cross build
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6Quick3DTools_DIR=${pkgsBuildBuild.qt6.qtquick3d}/lib/cmake/Qt6Quick3DTools"
+  ];
 }
