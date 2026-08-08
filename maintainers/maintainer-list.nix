632fb0774d671cc72b781b0df21cc76e5a8864f9
Benedikt Weyer <bw.development@pm.me>
2026-08-09 00:18:07 +0200

    openwhispr: init at 1.8.1

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3429,6 +3429,12 @@
     githubId = 32903896;
     keys = [ { fingerprint = "CB5C 7B3C 3E6F 2A59 A583  A90A 8A60 0376 7BE9 5976"; } ];
   };
+  benediktweyer = {
+    name = "Benedikt Weyer";
+    email = "bw.development@pm.me";
+    github = "benedikt-weyer";
+    githubId = 105204092;
+  };
   bengsparks = {
     email = "benjamin.sparks@protonmail.com";
     github = "bengsparks";
