eea09d29f25f5d4d9bf27bc7a89eca597d3fd44c
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 22:56:18 +0200

    qt6.qtmqtt: fix cross build

--- pkgs/development/libraries/qt-6/modules/qtmqtt.nix
+++ pkgs/development/libraries/qt-6/modules/qtmqtt.nix
@@ -2,6 +2,9 @@
   qtModule,
   fetchFromGitHub,
   qtbase,
+  qtdeclarative,
+  qtwebsockets,
+  pkgsBuildBuild,
 }:
 
 qtModule rec {
@@ -15,5 +18,13 @@ qtModule rec {
     hash = "sha256-GWaF4iCPtATL1mJkPHVY0rom8R2FMNWGahE3KWBlfV8=";
   };
 
-  propagatedBuildInputs = [ qtbase ];
+  propagatedBuildInputs = [
+    qtbase
+    qtdeclarative
+    qtwebsockets
+  ];
+
+  cmakeFlags = [
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+  ];
 }
