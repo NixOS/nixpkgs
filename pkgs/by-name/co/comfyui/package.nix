371a9edc5a2d15cf6ffdf5d81b784255c594cfdb
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-08-08 20:30:04 +0200

    comfyui: 0.30.2 -> 0.31.1
    Diff: https://github.com/Comfy-Org/ComfyUI/compare/v0.30.2...v0.31.1
    
    Changelog: https://github.com/Comfy-Org/ComfyUI/releases/tag/v0.31.1

--- pkgs/by-name/co/comfyui/package.nix
+++ pkgs/by-name/co/comfyui/package.nix
@@ -74,7 +74,7 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "comfyui";
-  version = "0.30.2";
+  version = "0.31.1";
 
   strictDeps = true;
   __structuredAttrs = true;
@@ -83,7 +83,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "Comfy-Org";
     repo = "ComfyUI";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-fLtgJpK8hEuYcfu1vKGDJtszPfED4mmyKHZYrrq/uiA=";
+    hash = "sha256-zo5XPU10liF5SvVQ4WpRDmx0QfuZ0CNh6EjCa60/9hc=";
   };
 
   nativeBuildInputs = [ makeBinaryWrapper ];
