99f66da263fab1e08ab7f6626466f3e7d74d489d
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-09 19:42:53 +0000

    spruce: 1.35.14 -> 1.35.15

--- pkgs/by-name/sp/spruce/package.nix
+++ pkgs/by-name/sp/spruce/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "spruce";
-  version = "1.35.14";
+  version = "1.35.15";
 
   src = fetchFromGitHub {
     owner = "geofffranks";
     repo = "spruce";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-EKhspLsuovnzJVFyp8+0cUSA+kRzPwJ0YHN5iUe5NRM=";
+    hash = "sha256-JzHLdKafvqcmolDO6To4R3xtvyctxVTe1h65GUEsO/o=";
   };
 
   vendorHash = null;
