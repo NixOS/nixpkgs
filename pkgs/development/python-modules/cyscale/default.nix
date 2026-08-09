1cb7763fa8130de9066210adef86f4d9f80ede08
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-09 23:35:00 +0000

    python3Packages.cyscale: 0.7.0 -> 0.8.0

--- pkgs/development/python-modules/cyscale/default.nix
+++ pkgs/development/python-modules/cyscale/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "cyscale";
-  version = "0.7.0";
+  version = "0.8.0";
   pyproject = true;
 
   __structuredAttrs = true;
@@ -19,7 +19,7 @@ buildPythonPackage (finalAttrs: {
     owner = "latent-to";
     repo = "cyscale";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-vsicGsl5Mcpb3zXdPDLX6RdGeCn/fPwT2S5k1SdLDWk=";
+    hash = "sha256-UIInrmNiW/3KkHoVO1WhxLvSHBhycjvhkmXjGUGSCQw=";
   };
 
   build-system = [ setuptools ];
