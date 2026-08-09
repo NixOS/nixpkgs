233fa5ec76fca2e3c99cfa251c09a748cc5f1edc
Tom Hunze <dev@thunze.de>
2026-08-09 15:32:30 +0200

    ahoviewer: drop
    Depends on the deprecated gtk2 via gtkmm2 and the last upstream (beta)
    release was in 2021.
    
    Tracking: https://github.com/NixOS/nixpkgs/issues/410814
    
    Closes https://github.com/NixOS/nixpkgs/issues/353067

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -281,6 +281,7 @@ mapAliases {
   afterstep = throw "'afterstep' has been removed as it was unmaintained upstream and depends on the deprecated GTK2 engine."; # Added 2026-07-23
   afuse = throw "'afuse' has been removed as it is unmaintained upstream, and depends on fuse2, which is deprecated"; # Added 2026-05-05
   agrep = throw "'agrep' has been removed due to lack of upstream maintenance. Consider using 'tre' or 'ugrep' instead."; # Added 2025-12-28
+  ahoviewer = throw "'ahoviewer' has been removed as it was unmaintained upstream and the packaged version depended on the deprecated gtk2 via gtkmm2"; # Added 2026-08-09
   akkoma-emoji = throw "'akkoma-emoji' has been renamed to/replaced by 'blobs_gg'"; # Converted to throw 2025-10-27
   akkoma-frontends.admin-fe = throw "'akkoma-frontends.admin-fe' has been renamed to/replaced by 'akkoma-admin-fe'"; # Converted to throw 2025-10-27
   akkoma-frontends.akkoma-fe = throw "'akkoma-frontends.akkoma-fe' has been renamed to/replaced by 'akkoma-fe'"; # Converted to throw 2025-10-27
