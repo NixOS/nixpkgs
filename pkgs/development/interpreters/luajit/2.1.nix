9f5c1c70dfff141ae6ce3194078c743b0916cd3d
robin <me@robinwobin.dev>
2026-08-09 13:52:17 +0200

    luajit_2_1: 2.1.1774638290 -> 2.1.1785577137
    closes https://github.com/NixOS/nixpkgs/issues/548383
    
    Co-authored-by: june <me@koi.rip>

--- pkgs/development/interpreters/luajit/2.1.nix
+++ pkgs/development/interpreters/luajit/2.1.nix
@@ -8,13 +8,13 @@
 callPackage ./default.nix {
   # The patch version is the timestamp of the git commit,
   # obtain via `cat $(nix-build -A luajit_2_1.src)/.relver`
-  version = "2.1.1774638290";
+  version = "2.1.1785577137";
 
   src = fetchFromGitHub {
     owner = "LuaJIT";
     repo = "LuaJIT";
-    rev = "fbb36bb6bfa88716a47c58bcf9ce9f2ef752abac";
-    hash = "sha256-BqH66q38mJpIYJgPiSPt7I0B3VLBvuDRRTiMJ7ldkBI=";
+    rev = "4886b676a698acc4bbdf54adfabb3e33a8c020e8";
+    hash = "sha256-3nTyPcphBQN2segb5bxBgvrWHodN/ckRy4AGS8lDL44=";
   };
 
   inherit self passthruFun;
