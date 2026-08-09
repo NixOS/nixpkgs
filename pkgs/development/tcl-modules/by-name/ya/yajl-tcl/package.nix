e8214e523088b9070e6cae5e3cd0f10964d3f414
Stefan Frijters <sfrijters@gmail.com>
2026-08-09 20:15:17 +0200

    tclPackages.yajl-tcl: use finalAttrs

--- pkgs/development/tcl-modules/by-name/ya/yajl-tcl/package.nix
+++ pkgs/development/tcl-modules/by-name/ya/yajl-tcl/package.nix
@@ -7,14 +7,14 @@
   yajl,
 }:
 
-mkTclDerivation rec {
+mkTclDerivation (finalAttrs: {
   pname = "yajl-tcl";
   version = "1.8.1";
 
   src = fetchFromGitHub {
     owner = "flightaware";
     repo = "yajl-tcl";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-MKm/cfZxPoxpsHuZf9qSXZXzdFbDb7IGeJgMHGh9bcE=";
   };
 
@@ -37,8 +37,8 @@ mkTclDerivation rec {
   meta = {
     description = "Tcl bindings for Yet Another JSON Library";
     homepage = "https://github.com/flightaware/yajl-tcl";
-    changelog = "https://github.com/flightaware/yajl-tcl/blob/${src.tag}/ChangeLog";
+    changelog = "https://github.com/flightaware/yajl-tcl/blob/${finalAttrs.src.tag}/ChangeLog";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ fgaz ];
   };
-}
+})
