21be8b4d1f924de5edb3e1400305f9dcb50103aa
Stefan Frijters <sfrijters@gmail.com>
2026-08-09 20:06:19 +0200

    scid: use finalAttrs, tag, --replace-fail

--- pkgs/by-name/sc/scid/package.nix
+++ pkgs/by-name/sc/scid/package.nix
@@ -9,20 +9,20 @@
   zlib,
 }:
 
-tcl.mkTclDerivation rec {
+tcl.mkTclDerivation (finalAttrs: {
   pname = "scid";
   version = "5.0.2";
 
   src = fetchFromGitHub {
     owner = "benini";
     repo = "scid";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-5WGZm7EwhZAMKJKxj/OOIFOJIgPBcc6/Bh4xVAlia4Y=";
   };
 
   postPatch = ''
     substituteInPlace configure \
-      --replace "set var(INSTALL) {install_mac}" ""
+      --replace-fail "set var(INSTALL) {install_mac}" ""
   '';
 
   nativeBuildInputs = [
@@ -55,4 +55,4 @@ tcl.mkTclDerivation rec {
     license = lib.licenses.gpl2Only;
     platforms = lib.platforms.all;
   };
-}
+})
