dbb7ee46adffe9fd1622de034b39cdd5d28144bf
Randy Eckenrode <randy@largeandhighquality.com>
2026-08-09 08:38:12 -0400

    llvm: vendor upstream TextAPI patch
    While this patch applies cleanly to upstream LLVM and the fork of LLVM
    used by Swift 6.2, it fails to apply to the fork used by Swift 6.3
    because that version already includes the changes in this patch.
    
    Moving the patch to `getVersionFile` will allow Swift to override it or
    drop it (by replacing it with an empty patch).

--- pkgs/development/compilers/llvm/common/llvm/default.nix
+++ pkgs/development/compilers/llvm/common/llvm/default.nix
@@ -266,12 +266,7 @@ stdenv.mkDerivation (
       ]
       ++ lib.optionals (lib.versionOlder release_version "22") [
         # Needed to add arm64e.x1 support, as the enum name differs from the architecture name
-        (fetchpatch {
-          name = "llvm-textapi-separate-arch-name-enum-label.patch";
-          url = "https://github.com/llvm/llvm-project/commit/477a65a051ce151895193f8dede1262fdc251132.patch";
-          stripLen = 1;
-          hash = "sha256-XXteX2zK5TzFQX+XhLzUtikY4PkCWF1f8l5MshelzX4=";
-        })
+        (getVersionFile "llvm/llvm-textapi-separate-arch-name-enum-label.patch")
       ]
       ++ lib.optionals (lib.versionOlder release_version "23") [
         # Needed to link with the macOS 27 sdk, as it has libraries linked for arm64e.x1
