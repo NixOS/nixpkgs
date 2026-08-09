e19f2e213b76c43ee375de40920ab561309020bc
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-09 17:30:50 +0000

    cudaPackages.cudnn-frontend: 1.25.0 -> 1.27.0

--- pkgs/development/cuda-modules/packages/cudnn-frontend/package.nix
+++ pkgs/development/cuda-modules/packages/cudnn-frontend/package.nix
@@ -35,13 +35,13 @@ backendStdenv.mkDerivation (finalAttrs: {
   name = "${cudaNamePrefix}-${finalAttrs.pname}-${finalAttrs.version}";
 
   pname = "cudnn-frontend";
-  version = "1.25.0";
+  version = "1.27.0";
 
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = "cudnn-frontend";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zUCrLJvkqw2FUgBR2mDwaqnmyQ21xuexNJb1omgbJRw=";
+    hash = "sha256-4KDxORr6vJRgJKYrooIILaWMWkGF6ZDuti1QtrzYTfA=";
   };
 
   # nlohmann_json should be the only vendored dependency.
