7cf9549304f835ef3901815bb6e842645d50e599
Michael Daniels <mdaniels5757@gmail.com>
2026-08-09 10:43:49 -0400

    python3Packages.setuptools-git-versioning: modernize

--- pkgs/development/python-modules/setuptools-git-versioning/default.nix
+++ pkgs/development/python-modules/setuptools-git-versioning/default.nix
@@ -2,17 +2,19 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  addBinToPathHook,
   build,
   coverage,
   git,
   packaging,
+  pyprojectVersionPatchHook,
   pytestCheckHook,
   pytest-rerunfailures,
   setuptools,
   tomli-w,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "setuptools-git-versioning";
   version = "3.0.1";
   pyproject = true;
@@ -20,14 +22,11 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "dolfinus";
     repo = "setuptools-git-versioning";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-rAJ9OvSKhQ3sMN5DlUg2tfR42Ae7jjz9en3gfRnXb3I=";
   };
 
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace-fail 'dynamic = ["version"]' 'version = "${version}"'
-  '';
+  nativeBuildInputs = [ pyprojectVersionPatchHook ];
 
   build-system = [
     packaging
@@ -42,6 +41,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "setuptools_git_versioning" ];
 
   nativeCheckInputs = [
+    addBinToPathHook
     build
     coverage
     git
@@ -49,11 +49,6 @@ buildPythonPackage rec {
     pytest-rerunfailures
     tomli-w
   ];
-
-  preCheck = ''
-    # so that its built binary is accessible by tests
-    export PATH="$out/bin:$PATH"
-  '';
 
   # limit tests because the full suite takes several minutes to run
   enabledTestMarks = [
@@ -69,7 +64,7 @@ buildPythonPackage rec {
     description = "Use git repo data (latest tag, current commit hash, etc) for building a version number according PEP-440";
     mainProgram = "setuptools-git-versioning";
     homepage = "https://github.com/dolfinus/setuptools-git-versioning";
-    changelog = "https://github.com/dolfinus/setuptools-git-versioning/blob/${src.tag}/CHANGELOG.rst";
+    changelog = "https://github.com/dolfinus/setuptools-git-versioning/blob/${finalAttrs.src.tag}/CHANGELOG.rst";
     license = lib.licenses.mit;
   };
-}
+})
