b792f62e04fbc11527958ff6a41165c17d6d111d
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-08-09 00:10:07 -0600

    maintainers: shackra -> elzorrorebelde
    I changed the name of my GitHub account, also renamed any references
    used.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8233,6 +8233,12 @@
     githubId = 1365692;
     name = "Will Fancher";
   };
+  elzorrorebelde = {
+    name = "Jorge Javier Araya Navarro";
+    email = "jorge@esavara.cr";
+    github = "elzorrorebelde";
+    githubId = 1055216;
+  };
   emaiax = {
     email = "github@emaiax.dev";
     github = "emaiax";
@@ -25912,12 +25918,6 @@
     github = "sguimmara";
     githubId = 5512096;
     name = "Sébastien Guimmara";
-  };
-  shackra = {
-    name = "Jorge Javier Araya Navarro";
-    email = "jorge@esavara.cr";
-    github = "shackra";
-    githubId = 1055216;
   };
   shadaj = {
     github = "shadaj";
