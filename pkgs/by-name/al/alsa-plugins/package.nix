99e5aff3f0c20e7fbe6904a7014ea236e33642f1
whispers <whispersofthedawn@duck.com>
2026-08-08 20:31:01 -0400

    alsa-plugins: fix build with ffmpeg 9
    failing build logs: https://hydra.nixos.org/build/341407667

--- pkgs/by-name/al/alsa-plugins/package.nix
+++ pkgs/by-name/al/alsa-plugins/package.nix
@@ -1,6 +1,7 @@
 {
   stdenv,
   fetchurl,
+  fetchpatch,
   lib,
   pkg-config,
   alsa-lib,
@@ -20,6 +21,14 @@ stdenv.mkDerivation (finalAttrs: {
     url = "mirror://alsa/plugins/alsa-plugins-${finalAttrs.version}.tar.bz2";
     hash = "sha256-e9ioPTBOji2GoliV2Nyw7wJFqN8y4nGVnNvcavObZvI=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "ffmpeg-9-compatibility.patch";
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/alsa-plugins/-/raw/7f250af93b76e9b3d552af509beb8ea1356114d1/ffmpeg9.patch";
+      hash = "sha256-heAl6Ym3iYI8mhuuQpwBpc0FeFYsQZRHx4UUvqiVtBo=";
+    })
+  ];
 
   nativeBuildInputs = [ pkg-config ];
 
