353a70a0714c67fbe3bbe5de4aeedaa828032127
Randy Eckenrode <randy@largeandhighquality.com>
2026-08-09 08:38:12 -0400

    swiftPackages: delete Swift 5.10.1 package set
    The old package set will be replaced with the Swift 6.2 package set.
    Going forward, it is expected that updates will be done in that package
    set since it is hoped that another rewrite will not be required.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4101,7 +4101,6 @@ with pkgs;
     ];
   };
 
-  swiftPackages = recurseIntoAttrs (callPackage ../development/compilers/swift { });
   swiftPackages_ng = recurseIntoAttrs (callPackage ./swift-packages.nix { });
   inherit (swiftPackages_ng)
     fetchSwiftPMDeps
