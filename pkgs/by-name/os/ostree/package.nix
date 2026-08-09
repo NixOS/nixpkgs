40dc05597a3db6449b048e2fb89f32755f6d2f14
Aaron Andersen <aaron@fosslib.net>
2026-08-08 21:50:19 -0400

    ostree: replace systemd dependency with systemdLibs

--- pkgs/by-name/os/ostree/package.nix
+++ pkgs/by-name/os/ostree/package.nix
@@ -38,8 +38,8 @@
     lib.meta.availableOn stdenv.hostPlatform gobject-introspection
     && stdenv.hostPlatform.emulatorAvailable buildPackages,
   gobject-introspection,
-  withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemd,
-  systemd,
+  withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemdLibs,
+  systemdLibs,
   replaceVars,
   openssl,
   ostree-full,
@@ -124,7 +124,7 @@ stdenv.mkDerivation (finalAttrs: {
     gjs
   ]
   ++ lib.optionals withSystemd [
-    systemd
+    systemdLibs
   ];
 
   enableParallelBuilding = true;
