0ebd02170cebcc8c28bec3af7d2576599ddb32d1
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-09 18:50:49 +0000

    media-downloader: 5.4.9 -> 5.6.5

--- pkgs/by-name/me/media-downloader/package.nix
+++ pkgs/by-name/me/media-downloader/package.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "media-downloader";
-  version = "5.4.9";
+  version = "5.6.5";
 
   src = fetchFromGitHub {
     owner = "mhogomchungu";
     repo = "media-downloader";
     rev = finalAttrs.version;
-    hash = "sha256-afQ3Tra7hUjrG3vs4XBfmvSOBrhG7k5fkEMDr6WF+Fo=";
+    hash = "sha256-2Nn/XBU5mnrbITtwygAD1saZXRm1/2Y1mOL0SZkV6bA=";
   };
 
   nativeBuildInputs = [
