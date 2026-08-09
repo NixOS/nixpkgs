4f223f927411b2659d6ad8abe4a196fac1a39335
Randy Eckenrode <randy@largeandhighquality.com>
2026-08-09 08:38:12 -0400

    swift: 5.10.1 -> 6.2.4
    https://www.swift.org/blog/announcing-swift-6/
    https://www.swift.org/blog/swift-6.1-released/
    https://www.swift.org/blog/swift-6.2-released/
    
    Switch the top-level Swift implementation to use the new packaging,
    which also upgrades the version from 5.10.1 to 6.2.4.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4107,11 +4107,12 @@ with pkgs;
 
   swiftPackages = recurseIntoAttrs (callPackage ../development/compilers/swift { });
   swiftPackages_ng = recurseIntoAttrs (callPackage ./swift-packages.nix { });
-  inherit (swiftPackages)
-    swift
-    swiftpm
+  inherit (swiftPackages_ng)
     sourcekit-lsp
+    swift
+    swift-corelibs-libdispatch
     swift-format
+    swiftpm
     swiftpm2nix
     ;
 
@@ -10827,8 +10828,6 @@ with pkgs;
   wfuzz = with python3Packages; toPythonApplication wfuzz;
 
   sieveshell = with python3.pkgs; toPythonApplication managesieve;
-
-  swift-corelibs-libdispatch = swiftPackages.Dispatch;
 
   duden = python3Packages.toPythonApplication python3Packages.duden;
 
