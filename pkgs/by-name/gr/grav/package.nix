c44860ebbaaf3016f544b2d3edf4990848727246
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-09 15:08:46 +0000

    grav: 1.7.53.2 -> 2.0.17

--- pkgs/by-name/gr/grav/package.nix
+++ pkgs/by-name/gr/grav/package.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "1.7.53.2";
+  version = "2.0.17";
 in
 stdenvNoCC.mkDerivation {
   pname = "grav";
@@ -14,7 +14,7 @@ stdenvNoCC.mkDerivation {
 
   src = fetchzip {
     url = "https://github.com/getgrav/grav/releases/download/${version}/grav-admin-v${version}.zip";
-    hash = "sha256-6cQotHwIwWFR5phFQI9r79jpd+iYA1HpFBbYIzEVBsc=";
+    hash = "sha256-Z+T9Pl8d2VirYT9g05hmHzNzuxpnb7KTw8ynqJ0NO20=";
   };
 
   patches = [
