185005b130b8f944eae2859e5469f3844f1105d6
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-10 11:31:38 +0000

    oscavmgr: 25.2 -> 26.8.0

--- pkgs/by-name/os/oscavmgr/package.nix
+++ pkgs/by-name/os/oscavmgr/package.nix
@@ -12,16 +12,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "oscavmgr";
-  version = "25.2";
+  version = "26.8.0";
 
   src = fetchFromGitHub {
     owner = "galister";
     repo = "oscavmgr";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-592qj0dHn0fbIFt4Y+1TESIOUpwXcJ2tnlKNcYuxriQ=";
+    hash = "sha256-BYS/1rsXlP67MlkStm45Dm8aZllaVVOmdq8Yf0jB0iM=";
   };
 
-  cargoHash = "sha256-1/jjZ1jkLvE/L1lHFL3RCx3ox2w15WWDp6aQJOtFkcU=";
+  cargoHash = "sha256-R8UzXy44fli54zHEIZ+MdpHEggvoD06YXm5gpfMNfK0=";
 
   nativeBuildInputs = [
     pkg-config
