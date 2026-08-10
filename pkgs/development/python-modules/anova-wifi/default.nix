d3af7c189414d4d34a06a2d1241827f7c3b8c33b
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-10 19:24:20 +0200

    python3Packages.anova-wifi: enable previously failing test

--- pkgs/development/python-modules/anova-wifi/default.nix
+++ pkgs/development/python-modules/anova-wifi/default.nix
@@ -38,8 +38,6 @@ buildPythonPackage (finalAttrs: {
   disabledTests = [
     # Makes network calls
     "test_async_data_1"
-    # async def functions are not natively supported.
-    "test_can_create"
   ];
 
   pythonImportsCheck = [ "anova_wifi" ];
