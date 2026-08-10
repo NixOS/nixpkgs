41502fb2f85cad2e1d30f4f4ef05265d62002be5
ShouviT <showhy@proton.me>
2026-08-11 01:40:00 +0300

    essentia-extractor: 2.1_beta2 -> 2.1_beta5

--- pkgs/by-name/es/essentia-extractor/package.nix
+++ pkgs/by-name/es/essentia-extractor/package.nix
@@ -1,40 +1,92 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
+  python312Packages,
+  libyaml,
+  waf,
+  pkg-config,
+  ffmpeg_4,
+  eigen,
+  fftwSinglePrec,
+  libsamplerate,
+  taglib,
+  wafHook,
+  libresample,
+  chromaprint,
+  zlib,
+  gitUpdater,
 }:
-let
-  arch_table = {
-    "x86_64-linux" = "linux-x86_64";
-    "i686-linux" = "linux-i686";
-  };
 
-  sha_table = {
-    "x86_64-linux" = "d9902aadac4f442992877945da2a6fe8d6ea6b0de314ca8ac0c28dc5f253f7d8";
-    "i686-linux" = "46deb0a053b4910c4e68737a7b6556ff5360260c8f86652f91a0130445f5c949";
-  };
-
-  throwSystem = throw "Unsupported system: ${stdenv.system}";
-  arch = arch_table.${stdenv.system} or throwSystem;
-in
 stdenv.mkDerivation (finalAttrs: {
   pname = "essentia-extractor";
-  version = "2.1_beta2";
+  version = "2.1_beta5";
 
-  src = fetchurl {
-    url = "https://ftp.acousticbrainz.org/pub/acousticbrainz/essentia-extractor-v${finalAttrs.version}-${arch}.tar.gz";
-    sha256 = sha_table.${stdenv.system} or throwSystem;
+  src = fetchFromGitHub {
+    owner = "MTG";
+    repo = "essentia";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-nPw3KxN2vXgAGnQIC5pMxZ35hbveERmvzMLn7vgx4kU=";
   };
 
-  unpackPhase = "unpackFile $src ; export sourceRoot=.";
+  nativeBuildInputs = [
+    libyaml
+    waf
+    wafHook
+    pkg-config
+  ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp streaming_extractor_music $out/bin
+  buildInputs = [
+    libyaml
+    python312Packages.setuptools
+    python312Packages.six
+    python312Packages.pyyaml
+    python312Packages.distutils
+    ffmpeg_4
+    eigen
+    fftwSinglePrec
+    libsamplerate
+    taglib
+    chromaprint
+    libresample
+    zlib
+  ];
+
+  preConfigure = ''
+    # Fixing error with python-utils
+    export PYTHONPATH="$PWD:$PWD/src:$PYTHONPATH"
+    rm waf
   '';
 
+  wafConfigureFlags = [
+    "--build-static"
+    "--with-cpptests"
+    "--with-examples"
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    waf build
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out/bin" "$out/include" "$out/lib"
+    waf install --prefix="$out"
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    # Remove another files
+    mv $out/bin/essentia_streaming_extractor_music $out/bin/streaming_extractor_music
+    rm -f $out/bin/essentia_*
+  '';
+
+  passthru.updateScript = gitUpdater { rev-prefix = "v"; };
+
   meta = {
-    homepage = "https://acousticbrainz.org/download";
+    homepage = "https://github.com/MTG/essentia";
     description = "AcousticBrainz audio feature extractor";
     license = lib.licenses.agpl3Plus;
     maintainers = with lib.maintainers; [ lovesegfault ];
