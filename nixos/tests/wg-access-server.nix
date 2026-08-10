7a3544a4214124e4a75dbf21d77d47dbbe0f9e83
Alexander Sieg <alex@xanderio.de>
2026-08-10 15:16:11 +0200

    nixosTests/wg-access-server: check for version string in metrics

--- nixos/tests/wg-access-server.nix
+++ nixos/tests/wg-access-server.nix
@@ -1,34 +1,42 @@
 {
-  pkgs,
   lib,
-  kernelPackages ? null,
+  config,
   ...
 }:
+let
+  keys = import ./wireguard/snakeoil-keys.nix;
+in
 {
   name = "wg-access-server";
-  meta = with pkgs.lib.maintainers; {
+  meta = with lib.maintainers; {
     maintainers = [ xanderio ];
   };
 
   nodes = {
-    server = {
+    server = { pkgs, ... }: {
       services.wg-access-server = {
         enable = true;
         settings = {
           adminUsername = "admin";
+          enableMetadata = true;
         };
         secretsFile = (
           pkgs.writers.writeYAML "secrets.yaml" {
             adminPassword = "hunter2";
+            wireguard.privateKey = keys.peer0.privateKey;
           }
         );
       };
     };
   };
 
-  testScript = ''
-    start_all()
+  testScript =
+    # python
+    ''
+      start_all()
 
-    server.wait_for_unit("wg-access-server.service")
-  '';
+      server.wait_for_unit("wg-access-server.service")
+      server.wait_for_open_port(8000)
+      assert "${config.nodes.server.services.wg-access-server.package.version}" in server.succeed("curl -L http://localhost:8000/metrics | grep wg_access_server_build_info")
+    '';
 }
