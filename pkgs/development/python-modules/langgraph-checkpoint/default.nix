7d9551f6ceb6e05d8fd26c966a42688665470567
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-10 20:35:31 +0000

    python3Packages.langgraph-checkpoint: 4.1.1 -> 4.2.0

--- pkgs/development/python-modules/langgraph-checkpoint/default.nix
+++ pkgs/development/python-modules/langgraph-checkpoint/default.nix
@@ -27,14 +27,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "langgraph-checkpoint";
-  version = "4.1.1";
+  version = "4.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langgraph";
     tag = "checkpoint==${finalAttrs.version}";
-    hash = "sha256-P4SbQK6lFG572WKxisnNn/ZiHcMYBBM/vcBB9N6xpfo=";
+    hash = "sha256-Odn44pOTMyEvEDv3s/hfV+CKG7FS8sjTjA+bUDNLS2M=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/libs/checkpoint";
