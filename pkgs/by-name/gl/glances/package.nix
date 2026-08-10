85190f870a5c132c42a93efed3e783fdf3558c34
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-10 07:07:33 +0000

    glances: 4.5.5 -> 4.5.6

--- pkgs/by-name/gl/glances/package.nix
+++ pkgs/by-name/gl/glances/package.nix
@@ -11,7 +11,7 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "glances";
-  version = "4.5.5";
+  version = "4.5.6";
   pyproject = true;
 
   disabled = python3Packages.isPyPy;
@@ -20,7 +20,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
     owner = "nicolargo";
     repo = "glances";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-RiAt797YS468lmwH68O9/KlbV46DAqd25O8J0wNIDsU=";
+    hash = "sha256-ygXPInfs3jw0Uw3G8DK9llyCpzrtK2/szmKersxCTSI=";
   };
 
   build-system = with python3Packages; [ setuptools ];
