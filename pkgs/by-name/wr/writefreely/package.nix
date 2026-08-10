cc90abbb84a3c38c2027b0774353a641460450cc
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-10 09:08:36 +0000

    writefreely: 0.17.1 -> 0.17.2

--- pkgs/by-name/wr/writefreely/package.nix
+++ pkgs/by-name/wr/writefreely/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "writefreely";
-  version = "0.17.1";
+  version = "0.17.2";
 
   src = fetchFromGitHub {
     owner = "writefreely";
     repo = "writefreely";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-N6AKDNGeUhJOUPHK3eXhrNi1hC3Wl2YXPawBeFPhVw0=";
+    sha256 = "sha256-CzIlXy1StXuK8fY3+uZd2pu5hB/MWdJrN+WEdEZEzfk=";
   };
 
-  vendorHash = "sha256-e9usNyJHwmRNMjovhuL7Z4Ll7f58DgA1v1/hfJTZ4pg=";
+  vendorHash = "sha256-RrwcY2DNO90cG8YtTQ0nAkUMNnehd1JByHCw/QtGRNs=";
 
   ldflags = [
     "-s"
