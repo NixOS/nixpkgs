576b59c8ad9291d506dc6c84e590b03e2f6b0c7d
Austin Horstman <khaneliman12@gmail.com>
2026-08-10 18:30:40 -0500

    sketchybar-app-font: 2.0.71 -> 2.0.72
    Update to the latest stable upstream release.

--- pkgs/by-name/sk/sketchybar-app-font/package.nix
+++ pkgs/by-name/sk/sketchybar-app-font/package.nix
@@ -10,13 +10,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "sketchybar-app-font";
-  version = "2.0.71";
+  version = "2.0.72";
 
   src = fetchFromGitHub {
     owner = "kvndrsslr";
     repo = "sketchybar-app-font";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Xyj2yPU7Sd6WTWHrVAmx2JGGvUihM5jq79yXbLUz9Qk=";
+    hash = "sha256-FTyElxbUs8Vu7dvjv9dyAWWq+wuOUn8MjK8QgCUnuOA=";
   };
 
   pnpmDeps = fetchPnpmDeps {
