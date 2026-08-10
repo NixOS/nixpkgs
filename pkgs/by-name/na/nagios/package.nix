d2c59f4e96c395bf5f2fe18f5db10abed23ace10
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-10 03:57:55 +0000

    nagios: 4.5.13 -> 4.5.14

--- pkgs/by-name/na/nagios/package.nix
+++ pkgs/by-name/na/nagios/package.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "nagios";
-  version = "4.5.13";
+  version = "4.5.14";
 
   src = fetchFromGitHub {
     owner = "NagiosEnterprises";
     repo = "nagioscore";
     tag = "nagios-${finalAttrs.version}";
-    hash = "sha256-6d49LhnerArXM2tTjyEe0/PU/THqxxptaSaBCKJzkiU=";
+    hash = "sha256-hU+Q/GiT99WXL2r+7KRhypAp/Rvo2/ydEVtX6GlY7vU=";
   };
 
   patches = [ ./nagios.patch ];
