310e43976c09658e48a9fe56a3a365d6f9dbc904
plrigaux <491172+plrigaux@users.noreply.github.com>
2026-08-11 14:05:18 -0400

    maintainers: add plrigaux

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22623,6 +22623,11 @@
     github = "plebhash";
     githubId = 147345153;
   };
+  plrigaux = {
+    name = "Pierre-Luc Rigaux";
+    github = "plrigaux";
+    githubId = 491172;
+  };
   pluiedev = {
     email = "hi@pluie.me";
     github = "pluiedev";
