1f8065414744281b0471ea4e07a7787c24774b3c
Robert Schütz <nix@dotlambda.de>
2026-08-11 16:21:59 -0700

    python3Packages.rocketchat-api: use finalAttrs

--- pkgs/development/python-modules/rocketchat-api/default.nix
+++ pkgs/development/python-modules/rocketchat-api/default.nix
@@ -8,7 +8,7 @@
   requests,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "rocketchat-api";
   version = "3.7.1";
   pyproject = true;
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "jadolg";
     repo = "rocketchat_API";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-hAm67dEHXQy3huW9hFxxdVuEdqpgZpjuZMYM3KVq3GA=";
   };
 
@@ -40,10 +40,10 @@ buildPythonPackage rec {
   doCheck = false;
 
   meta = {
-    changelog = "https://github.com/jadolg/rocketchat_API/releases/tag/${src.tag}";
+    changelog = "https://github.com/jadolg/rocketchat_API/releases/tag/${finalAttrs.src.tag}";
     description = "Python API wrapper for Rocket.Chat";
     homepage = "https://github.com/jadolg/rocketchat_API";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ dotlambda ];
   };
-}
+})
