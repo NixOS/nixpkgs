c2ce292182ca0c5486034cfe2ba91fabb5502927
Marco Köpcke <hello@capypara.de>
2026-08-11 08:32:07 +0200

    jetbrains.*: move to by-name

--- pkgs/by-name/in/intellij-idea/package.nix
+++ pkgs/by-name/in/intellij-idea/package.nix
@@ -34,7 +34,7 @@ in
 jetbrains.mkJetBrainsProduct {
   inherit jetbrains-libdbm fsnotifier;
 
-  pname = "idea";
+  pname = "intellij-idea";
 
   wmClass = "jetbrains-idea";
   product = "IntelliJ IDEA";
