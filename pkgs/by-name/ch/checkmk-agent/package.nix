amansakhuja/nixpkgs5

nixos/tests/cmk-agent: init tests
Co-authored-by: Chaostheorie <cobalt@cobalt.rocks>

pkgs/by-name/ch/checkmk-agent/package.nix
@@ -5,6 +5,7 @@
   makeWrapper,
   gzip,
   lib,
+  nixosTests,
   # derivations for `plugins`:
   # All executables in PLUGINSDIR will simply be executed and their
   # ouput appended to the output of the agent. Plugins define their own
@@ -213,6 +214,10 @@ stdenv.mkDerivation {
         )
       }:$out/bin
   '';
+
+  passthru.tests = {
+    inherit (nixosTests) cmk-agent;
+  };
 
   meta = {
     description = "The checkmk agent to monitor *nix style systems.";

NixOS/nixpkgs

pkgs/by-name/ch/checkmk-agent/package.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   makeWrapper,
   callPackage,
+  nixosTests,
 
   # Derivations for `plugins`:
   # All executables in PLUGINSDIR will simply be executed and their
@@ -289,6 +290,10 @@ stdenv.mkDerivation {
 
     runHook postInstall
   '';
+
+  passthru.tests = {
+    inherit (nixosTests) checkmk-agent;
+  };
 
   inherit (cmk-agent-ctl) meta;
 }