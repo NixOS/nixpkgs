4c7b7dd16f69d676b16a6eb5b0f0916fd4e4003a
Sigmanificient <edhyjox@gmail.com>
2026-08-11 23:25:44 +0200

    python3Packages.diffimg: set __structuredAttrs

--- pkgs/development/python-modules/diffimg/default.nix
+++ pkgs/development/python-modules/diffimg/default.nix
@@ -11,6 +11,8 @@
 buildPythonPackage {
   pname = "diffimg";
   version = "0.3.0"; # github recognized 0.1.3, there's a v0.1.5 tag and setup.py says 0.3.0
+
+  __structureAttrs = true;
   pyproject = true;
 
   src = fetchFromGitHub {
