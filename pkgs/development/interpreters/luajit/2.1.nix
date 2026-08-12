acadbe18bc04952da37a806806639fce272c1081
Vladimír Čunát <v@cunat.cz>
2026-08-12 09:54:05 +0200

    luajit_2_1: 2.1.1785577137 -> 2.1.1785763465
    I saw some fixes for changes done in the parent commit in nixpkgs.

--- pkgs/development/interpreters/luajit/2.1.nix
+++ pkgs/development/interpreters/luajit/2.1.nix
@@ -8,13 +8,13 @@
 callPackage ./default.nix {
   # The patch version is the timestamp of the git commit,
   # obtain via `cat $(nix-build -A luajit_2_1.src)/.relver`
-  version = "2.1.1785577137";
+  version = "2.1.1785763465";
 
   src = fetchFromGitHub {
     owner = "LuaJIT";
     repo = "LuaJIT";
-    rev = "4886b676a698acc4bbdf54adfabb3e33a8c020e8";
-    hash = "sha256-3nTyPcphBQN2segb5bxBgvrWHodN/ckRy4AGS8lDL44=";
+    rev = "1edc3e52b67eaf6ce5f809be8e17d6862594b8bc";
+    hash = "sha256-mcOvVJ7AaoHrbEXxznpOkFoY7Kbd2aWMoOmyx5B4FIg=";
   };
 
   inherit self passthruFun;
