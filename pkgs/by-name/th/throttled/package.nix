80c4bf77e5b722cbbc169a3abcb7120ae95fff9e
cktiel <tiel@tuta.io>
2026-08-12 08:58:20 +0200

    throttled: 0.11 -> 0.12.2

--- pkgs/by-name/th/throttled/package.nix
+++ pkgs/by-name/th/throttled/package.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "throttled";
-  version = "0.11";
+  version = "0.12.2";
 
   src = fetchFromGitHub {
     owner = "erpalma";
     repo = "throttled";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-+3ktDkr5hvOfHcch4+mjgJqcuw24UgWTkJqTyDQumyk=";
+    sha256 = "sha256-hwnJO9KEDOizpGcb9NYHYHoEEHKa3PkLt76cKpwgEUs=";
   };
 
   nativeBuildInputs = [
