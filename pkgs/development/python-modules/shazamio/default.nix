de92fbc6613ebeb4c4d1979b753f6758409f958d
Tomás Rivera <tomas.riveral@icloud.com>
2026-08-12 11:36:31 +0200

    python3Packages.shazamio: migrate rec to finalAttrs

--- pkgs/development/python-modules/shazamio/default.nix
+++ pkgs/development/python-modules/shazamio/default.nix
@@ -15,7 +15,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "shazamio";
   version = "0.8.1";
   pyproject = true;
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "dotX12";
     repo = "ShazamIO";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-beEEr9Y8w0XlC/0+mNL/oWscmnfwt9KChlZ7Ullyk3E=";
   };
 
@@ -59,8 +59,8 @@ buildPythonPackage rec {
   meta = {
     description = "Free asynchronous library from reverse engineered Shazam API";
     homepage = "https://github.com/dotX12/ShazamIO";
-    changelog = "https://github.com/dotX12/ShazamIO/releases/tag/${src.tag}";
+    changelog = "https://github.com/dotX12/ShazamIO/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
