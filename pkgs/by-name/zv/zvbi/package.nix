9d11f34e02c616190da22b82d271064bf0350b83
Sergei Trofimovich <slyich@gmail.com>
2026-08-12 22:20:24 +0100

    zvbi: 0.2.44 -> 0.2.45
    Changes: https://github.com/zapping-vbi/zvbi/compare/v0.2.44...v0.2.45

--- pkgs/by-name/zv/zvbi/package.nix
+++ pkgs/by-name/zv/zvbi/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zvbi";
-  version = "0.2.44";
+  version = "0.2.45";
 
   src = fetchFromGitHub {
     owner = "zapping-vbi";
     repo = "zvbi";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-knc9PejugU6K4EQflfz91keZr3ZJqZu2TKFQFFJrxiI=";
+    hash = "sha256-Nkg/Y7tHYAEi3ndbiJwwutVrGCOIE5RUCNQW3j12BkM=";
   };
 
   configureFlags = lib.optionals (!lib.systems.equals stdenv.buildPlatform stdenv.hostPlatform) [
