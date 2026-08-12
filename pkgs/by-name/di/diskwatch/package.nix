ac323b9eb266c72d32d220ecfa12435406412aae
Tomás Rivera <tomas.riveral@icloud.com>
2026-08-12 09:05:04 +0200

    diskwatch: remove versionCheckProgramArg

--- pkgs/by-name/di/diskwatch/package.nix
+++ pkgs/by-name/di/diskwatch/package.nix
@@ -24,8 +24,6 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   doInstallCheck = true;
 
-  versionCheckProgramArg = [ "-V" ];
-
   meta = {
     description = "Single-host, read-only disk diagnostics TUI";
     homepage = "https://github.com/matthart1983/diskwatch";
