94d6f987b00447b86068e6530ee9651bfbad9733
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-12 01:51:51 +0000

    kimai: 2.62.0 -> 2.65.0

--- pkgs/by-name/ki/kimai/package.nix
+++ pkgs/by-name/ki/kimai/package.nix
@@ -7,13 +7,13 @@
 
 php.buildComposerProject2 (finalAttrs: {
   pname = "kimai";
-  version = "2.62.0";
+  version = "2.65.0";
 
   src = fetchFromGitHub {
     owner = "kimai";
     repo = "kimai";
     tag = finalAttrs.version;
-    hash = "sha256-IN9AnyN04inRK43YbMas6rUtDhSXCtbEkP6vv+yZREc=";
+    hash = "sha256-lqKHXf5pksyZpKEf3ZIey2ub/3Q9b/Gik7NH+B2ZgAE=";
   };
 
   php = php.buildEnv {
@@ -38,7 +38,7 @@ php.buildComposerProject2 (finalAttrs: {
     '';
   };
 
-  vendorHash = "sha256-MkQlK3DUbLBkkwZkxzGdR98NTHx/o5KD9D194Dg2F38=";
+  vendorHash = "sha256-0nOmdnpGzUszBiAYH0sdkT4UCJc8Sd8eihBQE5Vo4Lo=";
 
   composerNoPlugins = false;
   postInstall = ''
