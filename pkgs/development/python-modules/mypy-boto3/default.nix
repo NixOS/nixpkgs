bad1f84eac5424cf02afb485eeaaa8dadfacf5a5
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-12 13:46:44 +0200

    python3Packages.mypy-boto3-eks: 1.43.38 -> 1.43.69

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -467,8 +467,8 @@ in
       "sha256-11KpPRxGId76g/I4jXwMQ55kwGEQVsasgvMUXsiLbM4=";
 
   mypy-boto3-eks =
-    buildMypyBoto3Package "eks" "1.43.38"
-      "sha256-d8nAhWJTMytBpPwwp0kVjxmeX+l7mFZzd2NfNBNmaIY=";
+    buildMypyBoto3Package "eks" "1.43.69"
+      "sha256-Vn8EzKWaXobr77B3b11dCsH8fu7+ZxLZOC+Eo/IWPmU=";
 
   mypy-boto3-elastic-inference =
     buildMypyBoto3Package "elastic-inference" "1.36.0"
