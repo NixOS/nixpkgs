4b9c5ce3a484bc4a9df80a327a254365e41c525d
Guy Chronister <guychronister@outlook.com>
2026-08-13 00:00:11 +0000

    avahi: use finalAttrs

--- pkgs/by-name/av/avahi/package.nix
+++ pkgs/by-name/av/avahi/package.nix
@@ -25,12 +25,12 @@
   withPython ? false,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "avahi${lib.optionalString withLibdnssdCompat "-compat"}";
   version = "0.8";
 
   src = fetchurl {
-    url = "https://github.com/lathiat/avahi/releases/download/v${version}/avahi-${version}.tar.gz";
+    url = "https://github.com/lathiat/avahi/releases/download/v${finalAttrs.version}/avahi-${finalAttrs.version}.tar.gz";
     sha256 = "1npdixwxxn3s9q1f365x9n9rc5xgfz39hxf23faqvlrklgbhj0q6";
   };
 
@@ -295,4 +295,4 @@ stdenv.mkDerivation rec {
       # "CVE-2024-52615: Avahi Wide-Area DNS Uses Constant Source Port"
     ];
   };
-}
+})
