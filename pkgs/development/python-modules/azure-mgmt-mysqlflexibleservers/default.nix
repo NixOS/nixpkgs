f2f2d568787979805ce8502d037f4d45aab49262
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-13 06:19:22 +0000

    python3Packages.azure-mgmt-mysqlflexibleservers: 1.1.0b3 -> 1.0.0

--- pkgs/development/python-modules/azure-mgmt-mysqlflexibleservers/default.nix
+++ pkgs/development/python-modules/azure-mgmt-mysqlflexibleservers/default.nix
@@ -13,13 +13,13 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-mysqlflexibleservers";
-  version = "1.1.0b3";
+  version = "1.0.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "azure_mgmt_mysqlflexibleservers";
     inherit version;
-    hash = "sha256-15uNv6MKuHxuXxeQYWbpmWQVhmkmzCYjeGkSIamGWAM=";
+    hash = "sha256-0HemVoiKXFl39HmiRKZKxKHTUQAumaft2vakmoIZLlY=";
   };
 
   build-system = [
