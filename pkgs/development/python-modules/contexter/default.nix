94c5d8b10baaa8cda520395e474b9d55942bc039
Harinn <prinn.dev@pm.me>
2026-08-13 22:16:41 +0700

    python3Packages.contexter: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/contexter/default.nix
+++ pkgs/development/python-modules/contexter/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "contexter";
   version = "0.1.4";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "c730890b1a915051414a6350d8ea1cddca7d01d8f756badedb30b9bf305ea0a8";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     license = lib.licenses.mit;
