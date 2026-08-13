9bfadaacc2949eec08631cf756322173ce02d36d
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-13 20:40:04 +0000

    podman: 5.8.4 -> 6.1.0

--- pkgs/by-name/po/podman/package.nix
+++ pkgs/by-name/po/podman/package.nix
@@ -42,13 +42,13 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "podman";
-  version = "5.8.4";
+  version = "6.1.0";
 
   src = fetchFromGitHub {
     owner = "podman-container-tools";
     repo = "podman";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zhEtMZVKiv1L72EMlwgz8sHpmvhejGp98oW63aPj+rQ=";
+    hash = "sha256-wjqB8sQY7Ovz4bY4dBWfLDD7qvu8EA1KubVp6ocWle8=";
   };
 
   patches = [
