1036adb068cc3df1971c86464c4fa1ab93cdce5c
Harinn <prinn.dev@pm.me>
2026-08-13 22:56:59 +0700

    python3Packages.dissononce: modernize

--- pkgs/development/python-modules/dissononce/default.nix
+++ pkgs/development/python-modules/dissononce/default.nix
@@ -8,16 +8,18 @@
   transitions,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "dissononce";
   version = "0.34.3";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "tgalal";
     repo = "dissononce";
-    rev = version;
-    sha256 = "0hn64qfr0d5npmza6rjyxwwp12k2z2y1ma40zpl104ghac6g3mbs";
+    tag = finalAttrs.version;
+    hash = "sha256-etXxDFPwERDo/YCoGrz4YopwOe9eZqN+vbY0kB0mxkI=";
   };
 
   build-system = [ setuptools ];
@@ -32,9 +34,11 @@ buildPythonPackage rec {
     transitions
   ];
 
+  pythonImportsCheck = [ "dissononce" ];
+
   meta = {
     homepage = "https://pypi.org/project/dissononce/";
     license = lib.licenses.mit;
     description = "Python implementation for Noise Protocol Framework";
   };
-}
+})
