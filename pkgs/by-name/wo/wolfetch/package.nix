e537c047b912e2c51eea927fc99f614ad23fa630
TrueWulf <283985092+TrueWulf@users.noreply.github.com>
2026-08-14 01:17:22 +0500

    wolfetch: init at 0.5.4

---
+++ pkgs/by-name/wo/wolfetch/package.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "wolfetch";
+  version = "0.5.4";
+
+  src = fetchFromGitHub {
+    owner = "TrueWulf";
+    repo = "wolfetch";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-U6p2uyhqeKnCaXv3U8Z3riS6pBQGPuPrJ3e8adZr3Gk=";
+  };
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  postInstall = ''
+    ln -s $out/bin/wolfetch $out/bin/wfetch
+  '';
+
+  meta = {
+    description = "Tiny and fast system fetch for Linux and BSD";
+    homepage = "https://github.com/TrueWulf/wolfetch";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.unix;
+    mainProgram = "wolfetch";
+  };
+})
