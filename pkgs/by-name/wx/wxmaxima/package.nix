66a39f3a86d7d6ca587516aa3a775ae1bc64121e
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-13 12:35:36 +0000

    wxmaxima: 26.07.1 -> 26.08.0

--- pkgs/by-name/wx/wxmaxima/package.nix
+++ pkgs/by-name/wx/wxmaxima/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "wxmaxima";
-  version = "26.07.1";
+  version = "26.08.0";
 
   src = fetchFromGitHub {
     owner = "wxMaxima-developers";
     repo = "wxmaxima";
     rev = "Version-${finalAttrs.version}";
-    hash = "sha256-pSo6I2U8yOf083/fNYQzPJtTexnQGt0rRsoMcBl67Ik=";
+    hash = "sha256-bqKptXKYE4ny7EPTMtfoszX1mSm5ADciNdl43UXNP5c=";
   };
 
   buildInputs = [
