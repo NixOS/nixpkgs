43f3c836b7ab6ff5985c0cbe31d45018772ec7fb
Gaetan Lepage <gaetan@glepage.com>
2026-08-13 19:58:03 +0000

    python3Packages.git-dummy: fix build on darwin

--- pkgs/development/python-modules/git-dummy/default.nix
+++ pkgs/development/python-modules/git-dummy/default.nix
@@ -41,12 +41,17 @@ buildPythonPackage (finalAttrs: {
 
   postInstall =
     # https://github.com/NixOS/nixpkgs/issues/308283
-    lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-      installShellCompletion --cmd git-dummy \
-        --bash <($out/bin/git-dummy --show-completion bash) \
-        --fish <($out/bin/git-dummy --show-completion fish) \
-        --zsh <($out/bin/git-dummy --show-completion zsh)
-    '';
+    lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform)
+      # Otherwise, typer ignores the requested shell and instead detects it by walking the
+      # process tree, which yields bash on linux and fails outright on darwin:
+      # https://github.com/fastapi/typer/blob/0.25.1/typer/completion.py#L42-L59
+      ''
+        export _TYPER_COMPLETE_TEST_DISABLE_SHELL_DETECTION=1
+        installShellCompletion --cmd git-dummy \
+          --bash <($out/bin/git-dummy --show-completion bash) \
+          --fish <($out/bin/git-dummy --show-completion fish) \
+          --zsh <($out/bin/git-dummy --show-completion zsh)
+      '';
 
   # No python tests nor --version flag
   doCheck = false;
