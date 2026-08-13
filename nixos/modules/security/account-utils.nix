30cc6365a92342200abf77991b6909921f4912c5
JoTalbot <JoTalbot@users.noreply.github.com>
2026-08-13 11:25:06 +0300

    fix: restrict account-utils pam_unix_ng.so to privileged contexts only

--- nixos/modules/security/account-utils.nix
+++ nixos/modules/security/account-utils.nix
@@ -7,6 +7,7 @@
 let
   cfg = config.security.account-utils;
   format = pkgs.formats.ini { };
+  isPrivilegedContext = lib.isPrivilegedContext or (config.boot.kernelParams ? "privileged" or false);
 in
 {
   options.security.account-utils = {
@@ -23,6 +24,7 @@ in
         List of arguments to pass to the socket activated service executables.
         ::: {.note}
         This is passed to both pwupdd and pwaccessd, which support identical flags.
+        Requires privileged context to operate securely.
         :::
       '';
     };
@@ -39,26 +41,34 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    # use account-utils reimplementation of pam_unix
-    security.pam = {
+    assertions = lib.singleton {
+      assertion = isPrivilegedContext;
+      message = ''
+        account-utils requires privileged context to operate securely.
+        Either run in a privileged container or set kernel parameter "privileged=1".
+      '';
+    };
+
+    # use account-utils reimplementation of pam_unix only in privileged contexts
+    security.pam = lib.mkIf isPrivilegedContext {
       pam_unixModulePath = "${cfg.package}/lib/security/pam_unix_ng.so";
       enableLegacySettings = false;
     };
 
-    systemd = {
+    systemd = lib.mkIf isPrivilegedContext {
       packages = [ cfg.package ];
       sockets.pwaccessd.wantedBy = [ "sockets.target" ];
-      sockets.pwupdd.wantedBy = lib.optional config.users.mutableUsers "sockets.target"; # immutable users do not need password updating
+      sockets.pwupdd.wantedBy = lib.optional config.users.mutableUsers "sockets.target";
       sockets.newidmapd.wantedBy = [ "sockets.target" ];
       services."pwupdd@".environment.PWUPDD_OPTS = lib.escapeShellArgs cfg.extraArgs;
       services."pwaccessd".environment.PWACCESSD_OPTS = lib.escapeShellArgs cfg.extraArgs;
     };
 
-    environment.systemPackages = [ cfg.package ];
+    environment.systemPackages = lib.mkIf isPrivilegedContext [ cfg.package ];
     environment.etc."account-utils/pwaccessd.conf".source =
-      format.generate "pwaccessd.conf" cfg.pwaccessd.settings;
+      lib.mkIf isPrivilegedContext (format.generate "pwaccessd.conf" cfg.pwaccessd.settings);
 
-    security.pam.services = {
+    security.pam.services = lib.mkIf isPrivilegedContext {
       pwupd-passwd = { };
       pwupd-chsh = { };
       pwupd-chfn = { };
@@ -69,10 +79,10 @@ in
       # shadow suid binaries are no longer necessary, but disabling the entire shadow module is too intrusive
       newuidmap.enable = false;
       newgidmap.enable = false;
-      chsh.enable = false;
-      passwd.enable = false;
+      chsh.enable = lib.mkIf isPrivilegedContext false;
+      passwd.enable = lib.mkIf isPrivilegedContext false;
 
-      unix_chkpwd.enable = false; # Not necessary when using pam_unix_ng.so
+      unix_chkpwd.enable = lib.mkIf isPrivilegedContext false; # Not necessary when using pam_unix_ng.so
     };
   };
 }
