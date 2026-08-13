3a00dffcb03a5cc64a75448c4ebe6e65e2749972
Jost Alemann <alemann@posteo.com>
2026-08-13 05:37:42 +0200

    ty: 0.0.70 -> 0.0.71
    Changelog: https://github.com/astral-sh/ty/releases/tag/0.0.71
    Diff: https://github.com/astral-sh/ty/compare/0.0.70...0.0.71

--- pkgs/by-name/ty/ty/package.nix
+++ pkgs/by-name/ty/ty/package.nix
@@ -17,7 +17,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "ty";
-  version = "0.0.70";
+  version = "0.0.71";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
@@ -25,7 +25,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     repo = "ty";
     tag = finalAttrs.version;
     fetchSubmodules = true;
-    hash = "sha256-rlhU/987KaTAXukZDUDY0XyDUQiC6qHGIXS2epg6ziI=";
+    hash = "sha256-3PC85qNI588hGJ4eiI7EEHdBJTqwmMBQeVorOyYyX4A=";
   };
 
   # For Darwin platforms, remove the integration test for file notifications,
@@ -39,7 +39,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   cargoBuildFlags = [ "--package=ty" ];
 
-  cargoHash = "sha256-eM+MdAxx6UqvFSTnUmCk7Nx1MuprrYrv/64Jy90nE0c=";
+  cargoHash = "sha256-1Vsx1OKIvO8GxROiLBYhJuPDKyrvfW/apRnUU/EbEq8=";
 
   nativeBuildInputs = [ installShellFiles ];
   buildInputs = [ rust-jemalloc-sys ];
