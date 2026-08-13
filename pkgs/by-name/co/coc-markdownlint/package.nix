b3cd62423368825abebb86ac9a98f211b4b0e91d
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-13 01:45:17 +0000

    coc-markdownlint: 0-unstable-2026-06-17 -> 0-unstable-2026-08-11

--- pkgs/by-name/co/coc-markdownlint/package.nix
+++ pkgs/by-name/co/coc-markdownlint/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage {
   pname = "coc-markdownlint";
-  version = "0-unstable-2026-06-17";
+  version = "0-unstable-2026-08-11";
 
   src = fetchFromGitHub {
     owner = "fannheyward";
     repo = "coc-markdownlint";
-    rev = "56458cb4ffe81f70e23fafef276dc5eaf8e74061";
-    hash = "sha256-FUWJoT8h/Hz8cOUY71TLDYCsWXPxWdT0NNdrhryOlWA=";
+    rev = "aff1a01b4a04319d4dc86401fd0cee289e93a869";
+    hash = "sha256-ZascCNQEwcBXTR4Gf3q0Y2Upi/T9eXp5LbK9si87hlE=";
   };
 
-  npmDepsHash = "sha256-MuWfnvRWJXEnIa46WBrnhqKHzPe0TAOWuBOMK3XsxcM=";
+  npmDepsHash = "sha256-icdvkKBWa9lVAORvp7RxudFVxoXUXAIU00E7xrXBqNk=";
 
   passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 
