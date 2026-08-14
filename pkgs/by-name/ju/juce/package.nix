239a7bf07517bf5ecba3f38f1da5b9dd1ce9e60d
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-14 00:08:37 +0000

    juce: 8.0.12 -> 9.0.1

--- pkgs/by-name/ju/juce/package.nix
+++ pkgs/by-name/ju/juce/package.nix
@@ -44,13 +44,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "juce";
-  version = "8.0.12";
+  version = "9.0.1";
 
   src = fetchFromGitHub {
     owner = "juce-framework";
     repo = "juce";
     tag = finalAttrs.version;
-    hash = "sha256-mq7lpPHbb1uF3o50/UZY9LiT81ACAk9ptHQ98fhdk1Q=";
+    hash = "sha256-9YbhXKBVER7Ww9pwwd1gwm9R8/975pCNibsCqGviYTk=";
   };
 
   patches = [
