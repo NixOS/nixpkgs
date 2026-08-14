59ab8876b1f4c3e884562c3e335e7f6a27aa59e6
Pascal Dietrich <cameo007@mintux.de>
2026-08-14 20:00:31 +0200

    netpeek: 0.3.1 -> 0.3.2

--- pkgs/by-name/ne/netpeek/package.nix
+++ pkgs/by-name/ne/netpeek/package.nix
@@ -14,7 +14,7 @@
 }:
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "netpeek";
-  version = "0.3.1";
+  version = "0.3.2";
   pyproject = false;
 
   __structuredAttrs = true;
@@ -23,7 +23,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
     owner = "ZingyTomato";
     repo = "NetPeek";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-FsqCR4h5rwmtVJYEH9u7zaFDnfgzRd8KprXUhlGVIKU=";
+    hash = "sha256-ATAhQN1p03zSbwLdSkY9vOcy3WJ3pcDVziOWvxGa0HQ=";
   };
 
   nativeBuildInputs = [
