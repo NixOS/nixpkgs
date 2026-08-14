1f8b84db78455b2f80fc2ac1a9cebaf62af70474
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-14 08:58:32 +0200

    python3Packages.alibabacloud-cs20151215: 7.0.4 -> 7.0.5

--- pkgs/development/python-modules/alibabacloud-cs20151215/default.nix
+++ pkgs/development/python-modules/alibabacloud-cs20151215/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "alibabacloud-cs20151215";
-  version = "7.0.4";
+  version = "7.0.5";
   pyproject = true;
 
   __structuredAttrs = true;
@@ -17,7 +17,7 @@ buildPythonPackage (finalAttrs: {
   src = fetchPypi {
     pname = "alibabacloud_cs20151215";
     inherit (finalAttrs) version;
-    hash = "sha256-9ZCkz2GyDqw9igs83Gu0ufTPDQZ8on8bP/l9/ZOBAjM=";
+    hash = "sha256-GFf+ljri9jO2oAlbnagskTal+nZk8GZB3CS/27az2aQ=";
   };
 
   build-system = [ setuptools ];
