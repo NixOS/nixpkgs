44a3259af1a1cb96f4e92a77ab44c6a7200b1314
Antoine Cotten <hello@acotten.com>
2026-08-14 10:40:24 +0200

    cni-plugin-flannel: update source hash
    Upstream moved the v1.9.1-flannel3 tag on 2026-08-07. It now points at
    flannel-io/cni-plugin@b380f201, one commit past what was tagged the day
    before.

--- pkgs/by-name/cn/cni-plugin-flannel/package.nix
+++ pkgs/by-name/cn/cni-plugin-flannel/package.nix
@@ -12,7 +12,7 @@ buildGoModule rec {
     owner = "flannel-io";
     repo = "cni-plugin";
     rev = "v${version}";
-    sha256 = "sha256-MPR8Nu+EZ/P+xSeEig6EmQVG0qaGt6g6/Fe3jIz1fqU=";
+    sha256 = "sha256-lYn9qDmUn8g3nnD4wQqyzKjd/lPXqoER5nZuY0sVK0s=";
   };
 
   vendorHash = "sha256-JTy9MsTyWvnqtRH0hzezYwgB23mfG7KtI9OmynpcESM=";
