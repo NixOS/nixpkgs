ca2d3ef80e72e84dce8ad8a1f3e29e824d913631
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-14 18:10:21 +0000

    dpp: 10.1.5 -> 10.1.6

--- pkgs/by-name/dp/dpp/package.nix
+++ pkgs/by-name/dp/dpp/package.nix
@@ -11,13 +11,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "dpp";
-  version = "10.1.5";
+  version = "10.1.6";
 
   src = fetchFromGitHub {
     owner = "brainboxdotcc";
     repo = "DPP";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-WMd2c0hfSpmaWwojyztR6UmvGzFayvXBktX6UvdHYZs=";
+    hash = "sha256-hLXRz5z6iluoO34oSrSKiqhaxl2MOOoiBNCBf0QqXlU=";
   };
 
   nativeBuildInputs = [
