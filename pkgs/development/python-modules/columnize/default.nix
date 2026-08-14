9ef5ee81e440148b9f89c6f6cb74496819ead442
Peder Bergebakken Sundt <pbsds@hotmail.com>
2026-08-14 22:30:14 +0200

    Revert "python3Packages.columnize: 0.3.11 -> 3.11"
    This reverts commit 4e45a027fd24b1eeed967221d99d990049f9a450.
    
    see https://github.com/NixOS/nixpkgs/pull/470511

--- pkgs/development/python-modules/columnize/default.nix
+++ pkgs/development/python-modules/columnize/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "columnize";
-  version = "3.11";
+  version = "0.3.11";
   pyproject = true;
 
   # 3.11 is the git tag for the 0.3.11 version
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "rocky";
     repo = "pycolumnize";
-    tag = version;
+    tag = "3.11";
     hash = "sha256-YJEIujoRpLvUM4H4CB1nEJaYStFOSVKIGzchnptlt7M=";
   };
 
