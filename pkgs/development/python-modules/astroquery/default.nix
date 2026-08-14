8e56a5b0690a43c1ca672cc31e78dbc9ddc5560c
abkein <rickbatra0z@gmail.com>
2026-08-14 14:11:28 +0300

    python3Packages.astroquery: fix build

--- pkgs/development/python-modules/astroquery/default.nix
+++ pkgs/development/python-modules/astroquery/default.nix
@@ -18,7 +18,7 @@
   pytestCheckHook,
   pyvo,
   astropy-helpers,
-  setuptools,
+  setuptools_80,
   writableTmpDirAsHomeHook,
 }:
 
@@ -35,8 +35,8 @@ buildPythonPackage rec {
   };
 
   build-system = [
-    astropy-helpers
-    setuptools
+    (astropy-helpers.override { setuptools = setuptools_80; })
+    setuptools_80
   ];
 
   dependencies = [
@@ -50,7 +50,7 @@ buildPythonPackage rec {
 
   # Disable automatic update of the astropy-helper module
   postPatch = ''
-    substituteInPlace setup.cfg --replace "auto_use = True" "auto_use = False"
+    substituteInPlace setup.cfg --replace-fail "auto_use = True" "auto_use = False"
   '';
 
   nativeCheckInputs = [ pytestCheckHook ];
@@ -71,6 +71,10 @@ buildPythonPackage rec {
   preCheck = ''
     cd build/lib
   '';
+
+  # pytest 9 warns about iterator parameters, which this release promotes to
+  # errors via its global filterwarnings setting. This is fixed upstream.
+  pytestFlags = [ "-Wignore::pytest.PytestRemovedIn10Warning" ];
 
   pythonImportsCheck = [ "astroquery" ];
 
