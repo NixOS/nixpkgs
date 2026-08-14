5c2243741d3da3877b28e627be198aa7815e1bf6
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-14 11:47:27 +0000

    python3Packages.drf-spectacular-sidecar: 2026.7.1 -> 2026.8.1

--- pkgs/development/python-modules/drf-spectacular-sidecar/default.nix
+++ pkgs/development/python-modules/drf-spectacular-sidecar/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "drf-spectacular-sidecar";
-  version = "2026.7.1";
+  version = "2026.8.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tfranzel";
     repo = "drf-spectacular-sidecar";
     tag = version;
-    hash = "sha256-RNaYPOw+XPUjGgpWcC3TKikNwv8VMX7dF0HR87HnCvQ=";
+    hash = "sha256-bH/0W+LZeC9zlqpJyXEfRPqhQhOdOktzf/1XOhtMbL4=";
   };
 
   build-system = [ setuptools ];
