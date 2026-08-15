ddad36623f7e2050b3df7ebd1b137ab37a9cbeb1
fjij <30779570+fjij@users.noreply.github.com>
2026-08-14 22:02:37 -0400

    stackblaster: init

---
+++ pkgs/by-name/st/stackblaster/package.nix
@@ -0,0 +1,31 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, git
+}:
+
+buildGoModule {
+  pname = "stackblaster";
+  version = "unstable-2026-08-14";
+
+  src = fetchFromGitHub {
+    owner = "fjij";
+    repo = "stackblaster";
+    rev = "b7df4fcbf703f1ee469b82dd049a934680c6c685";
+    hash = "sha256-9GEFqhvPlDmpfWIS6TzvxUlp1+Za32Dr6kIAJj98J7Q=";
+  };
+
+  vendorHash = "sha256-+m/P4ISOp+l/CDSW8yLbebSeedlSG7uDb4ngYSkApzA=";
+
+  subPackages = [ "cmd/sb" ];
+
+  nativeCheckInputs = [ git ];
+
+  meta = with lib; {
+    description = "TODO";
+    homepage = "https://github.com/fjij/stackblaster";
+    license = licenses.mit;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "stackblaster";
+  };
+}
