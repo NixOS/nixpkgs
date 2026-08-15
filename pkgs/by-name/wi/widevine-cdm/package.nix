73710810300fd4f34e839c727cfbd6060034a04f
fmbearmf <77757734+fmbearmf@users.noreply.github.com>
2026-08-15 16:01:19 -0700

    widevine-cdm: 4.10.3050.0 -> 151.0.7922.137 and update build process
    motivated by https://github.com/NixOS/nixpkgs/issues/550933
    widevine-cdm is now built by extracting Google's official Chrome .deb packages.

--- pkgs/by-name/wi/widevine-cdm/package.nix
+++ pkgs/by-name/wi/widevine-cdm/package.nix
@@ -1,14 +1,84 @@
 {
   lib,
   stdenv,
-  callPackage,
+  fetchurl,
+  autoPatchelfHook,
+  glibc,
 }:
 
 let
-  # TODO: update aarch64 to use crx3
-  targets = lib.genAttrs [
-    "aarch64-linux"
-    "x86_64-linux"
-  ] (name: ./. + "/${name}.nix");
+  platformMeta = {
+    x86_64-linux = {
+      debArch = "amd64";
+      hash = "sha256-5tq/BEz5zQJ5z+hu+kMWgsGL/AbQYznOBVqqh66HFyc=";
+    };
+    aarch64-linux = {
+      debArch = "arm64";
+      hash = "sha256-0Omn4kXbMG80PWYw5NSs14JcUysN91wG0iWMgyFoD6E=";
+    };
+  };
+
+  currentPlatform =
+    platformMeta.${stdenv.hostPlatform.system}
+      or (throw "widevine-cdm isn't supported on ${stdenv.hostPlatform.system}");
 in
-callPackage (targets."${stdenv.hostPlatform.system}" or targets.x86_64-linux) { inherit stdenv; }
+stdenv.mkDerivation (finalAttrs: {
+  pname = "widevine-cdm";
+  version = "151.0.7922.137";
+
+  src = fetchurl {
+    url = "https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${finalAttrs.version}-1_${currentPlatform.debArch}.deb";
+    inherit (currentPlatform) hash;
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    glibc
+    stdenv.cc.cc.lib
+  ];
+
+  sourceRoot = ".";
+
+  unpackPhase = ''
+    runHook preUnpack
+
+    ar x $src
+    tar -xf data.tar.* \
+        --no-same-owner \
+        --no-same-permissions \
+        --wildcards './opt/google/chrome/WidevineCdm*'
+
+    runHook postUnpack
+  '';
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/share/google/chrome"
+    cp -r opt/google/chrome/WidevineCdm "$out/share/google/chrome/"
+
+    find "$out" -type f -name "*.so" -exec chmod +x {} +
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = ./update.py;
+
+  meta = {
+    description = "Widevine Content Decryption Module";
+    homepage = "https://www.widevine.com";
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    license = lib.licenses.unfree;
+    platforms = lib.attrNames platformMeta;
+    maintainers = with lib.maintainers; [
+      jlamur
+      bearfm
+    ];
+  };
+})
