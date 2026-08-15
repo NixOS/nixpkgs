ae380da2fc02924ff4adbca957e67ebbe12f4456
Michael Daniels <mdaniels5757@gmail.com>
2026-08-15 11:38:20 -0400

    python3Packages.shtab: 1.9.2 -> 1.11.0
    Diff: https://github.com/tqdm/shtab/compare/v1.9.2...v1.11.0
    
    Release note for v1.9.3: https://github.com/tqdm/shtab/releases/tag/v1.9.3
    Release note for v1.10.0: https://github.com/tqdm/shtab/releases/tag/v1.10.0
    Release note for v1.11.0: https://github.com/tqdm/shtab/releases/tag/v1.11.0

--- pkgs/development/python-modules/shtab/default.nix
+++ pkgs/development/python-modules/shtab/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "shtab";
-  version = "1.9.2";
+  version = "1.11.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tqdm";
     repo = "shtab";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+9M0IfiD5CJcg4AHqCfq1UON/E63etwzvx7Gc82H0PE=";
+    hash = "sha256-mbki7g0BuMfrSa107nXuvySbxp1AVhGfWFJSVQe6sYE=";
   };
 
   nativeBuildInputs = [
