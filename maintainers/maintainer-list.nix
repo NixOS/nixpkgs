ddd5b41e787fe88f5efeeaa73c025a34a28d027d
Patrick Rauscher <prauscher@prauscher.de>
2026-08-15 10:32:34 +0200

    maintainers: add prauscher

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22936,6 +22936,12 @@
     name = "Pradyuman Vig";
     keys = [ { fingerprint = "240B 57DE 4271 2480 7CE3  EAC8 4F74 D536 1C4C A31E"; } ];
   };
+  prauscher = {
+    email = "prauscher@prauscher.de";
+    github = "prauscher";
+    githubId = 175521;
+    name = "Patrick Rauscher";
+  };
   preisschild = {
     email = "florian@florianstroeger.com";
     github = "Preisschild";
