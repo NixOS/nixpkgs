38d99397932d3e216e74b16bae90b1692a86d63b
Matt McHenry <github@matt.mchenryfamily.org>
2026-08-14 22:11:31 -0400

    pfixtools: remove
    as discussed in #532824, upstream has not made any changes in a
    decade, there is no nixpkgs maintainer with sufficient knowledge of C
    to update this package to use pcre2 (#356387), and postsrsd provides a
    ready alternative that is currently maintained.

--- nixos/modules/rename.nix
+++ nixos/modules/rename.nix
@@ -320,6 +320,9 @@ in
       The Javascript version of Parsoid configured through this module does not work with modern MediaWiki versions,
       and has been deprecated by upstream, so it has been removed. MediaWiki comes with a new PHP-based parser built-in, so there is no need for this module.
     '')
+    (mkRemovedOptionModule [ "services" "pfix-srsd" ] ''
+      The pfixtools project is dormant and does not support pcre2.  `services.postsrsd` is the recommended replacement for Sender Rewriting Scheme support with Postfix.
+    '')
     (mkRemovedOptionModule [ "services" "pingvin-share" ] ''
       The `pingvin-share.backend` package was broken and the project was archived upstream, so it was removed from nixpkgs.
     '')
