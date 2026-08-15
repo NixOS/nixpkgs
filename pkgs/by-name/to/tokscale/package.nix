73df9216e00ca7e43a494b97e87bf4156f42c14e
Peter Tri Ho <mail@petertriho.com>
2026-08-15 20:45:42 +1000

    tokscale: 4.0.4 -> 4.13.0
    Diff: https://github.com/junhoyeo/tokscale/compare/v4.0.4...v4.13.0
    
    Changelog: https://github.com/junhoyeo/tokscale/releases/tag/v4.13.0

--- pkgs/by-name/to/tokscale/package.nix
+++ pkgs/by-name/to/tokscale/package.nix
@@ -11,7 +11,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "tokscale";
-  version = "4.0.4";
+  version = "4.13.0";
 
   __structuredAttrs = true;
 
@@ -19,10 +19,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "junhoyeo";
     repo = "tokscale";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-vFBIq7z0+bmMk2teDORrUVWrv7N3w1CsDrT2s85k6/U=";
+    hash = "sha256-0BQnoIDETgh6S806mHvxqDBpcJJQZbhl46yj6ctUTsk=";
   };
 
-  cargoHash = "sha256-iXHriY+Kyn5pSx3uwouH2rkMBXkJX6zH5/xiFeCMqbQ=";
+  cargoHash = "sha256-kuq1qT4OywO3miSoyMsTUo+o/3jcLjpzQ70lGHpvt+w=";
 
   nativeBuildInputs = [
     pkg-config
@@ -43,6 +43,12 @@ rustPlatform.buildRustPackage (finalAttrs: {
     "--skip=test_pricing_command_json"
     "--skip=test_pricing_command_success"
     "--skip=test_pricing_command_with_provider"
+    "--skip=test_auto_pinning_never_overwrites_a_settings_file_it_could_not_read"
+    "--skip=test_auto_pinning_recovers_from_a_bucket_timezone_that_names_no_zone"
+    "--skip=test_first_run_pins_the_host_timezone_without_changing_its_own_output"
+    "--skip=test_submit_dry_run_preserves_local_date_ahead_of_utc"
+    "--skip=test_submit_excluding_only_generic_gemini_usage_does_not_promise_submission"
+    "--skip=test_unpinned_first_scan_still_buckets_by_the_host_timezone"
   ];
 
   nativeInstallCheckInputs = [ versionCheckHook ];
