08de3829c7e78a079f91b5d9df7ca07c4f775a17
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-15 16:52:30 +0000

    otel-tui: 0.7.3 -> 0.7.4

--- pkgs/by-name/ot/otel-tui/package.nix
+++ pkgs/by-name/ot/otel-tui/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "otel-tui";
-  version = "0.7.3";
+  version = "0.7.4";
 
   src = fetchFromGitHub {
     owner = "ymtdzzz";
     repo = "otel-tui";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-RYGSMdJQ2qj6930PX/UWFrN2orQzRpufHiKDv6lmAw4=";
+    hash = "sha256-iftbofbJxDLaQST8T2Yo2mzNjGEKQVl6tHs/v7JnoFM=";
   };
 
-  vendorHash = "sha256-dItC0l6BawbW2OCjg1KoDo8jHGX7Dbo2gSiVi1lynwI=";
+  vendorHash = "sha256-+TMq/It+93rwYyZkIm0C2ENSs0hbjp8f+yIrECRnnX8=";
 
   env.GOWORK = "off";
 
