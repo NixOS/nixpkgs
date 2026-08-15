f3236fe0e46ad946ff5f1d2225e6b06b7f42075e
Robert Schütz <nix@dotlambda.de>
2026-08-14 19:48:43 -0700

    python3Packages.homeassistant-stubs: 2026.8.1 -> 2026.8.2
    Diff: https://github.com/KapJI/homeassistant-stubs/compare/2026.8.1...2026.8.2
    
    Changelog: https://github.com/KapJI/homeassistant-stubs/releases/tag/2026.8.2

--- pkgs/servers/home-assistant/stubs.nix
+++ pkgs/servers/home-assistant/stubs.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "homeassistant-stubs";
-  version = "2026.8.1";
+  version = "2026.8.2";
   pyproject = true;
 
   disabled = python.version != home-assistant.python3Packages.python.version;
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "KapJI";
     repo = "homeassistant-stubs";
     tag = version;
-    hash = "sha256-woTbzA7I4jl0XX46ZMdQFGhCd2c0H/p48PhoSvSTgCk=";
+    hash = "sha256-cLGvesBrD76vrkG46PnCaadSTLVUZmz0zTBJep15QjM=";
   };
 
   build-system = [
