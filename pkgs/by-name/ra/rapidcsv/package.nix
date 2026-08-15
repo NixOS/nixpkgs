3e7e1ad483bfc8b72924b42b343a891ef138d338
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-15 08:21:08 +0000

    rapidcsv: 8.99 -> 9.07

--- pkgs/by-name/ra/rapidcsv/package.nix
+++ pkgs/by-name/ra/rapidcsv/package.nix
@@ -6,13 +6,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "rapidcsv";
-  version = "8.99";
+  version = "9.07";
 
   src = fetchFromGitHub {
     owner = "d99kris";
     repo = "rapidcsv";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-TJ4IpBt2G+zUf2AEUB9wwz8Zk1UEoqqoemTVJCB+8D0=";
+    hash = "sha256-pU9vQAqftIV54+5aUxedomw+g8tUV1iGeHsLL70OuuI=";
   };
 
   nativeBuildInputs = [
