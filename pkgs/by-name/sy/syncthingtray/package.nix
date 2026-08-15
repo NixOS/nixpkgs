5ed6ab2e7567282f2d9d5befcd51185f4b198476
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-15 04:18:11 +0000

    syncthingtray-minimal: 2.1.2 -> 2.1.4

--- pkgs/by-name/sy/syncthingtray/package.nix
+++ pkgs/by-name/sy/syncthingtray/package.nix
@@ -27,14 +27,14 @@
 }:
 
 stdenv.mkDerivation (finalAttrs: {
-  version = "2.1.2";
+  version = "2.1.4";
   pname = "syncthingtray";
 
   src = fetchFromGitHub {
     owner = "Martchus";
     repo = "syncthingtray";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-VU47ncrgY00LJTrM4GKMDbhtFtBrcTwakhNCbcucoFo=";
+    hash = "sha256-sVRwxmxFE9GchoyBYAhGCFBuk9KOMW4JxBvYbGrprjc=";
   };
 
   buildInputs = [
