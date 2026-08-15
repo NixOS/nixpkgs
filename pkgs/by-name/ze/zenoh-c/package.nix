b9eeaa12f995ac8306e406b2964d8602762d29fb
Markus Kowalewski <markus.kowalewski@gmail.com>
2026-08-15 12:30:41 +0200

    zenoh-c: 1.9.0 -> 1.10.0

--- pkgs/by-name/ze/zenoh-c/package.nix
+++ pkgs/by-name/ze/zenoh-c/package.nix
@@ -10,20 +10,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zenoh-c";
-  version = "1.9.0"; # nixpkgs-update: no auto update
+  version = "1.10.0"; # nixpkgs-update: no auto update
 
   src = fetchFromGitHub {
     owner = "eclipse-zenoh";
     repo = "zenoh-c";
-    # tag = version;
-    # Use 1.9.0 PR merge commit with up-to-date Cargo.lock file
-    rev = "8858e129271f4e05bb34d8ae6df3f3d221ef5299";
-    hash = "sha256-rNvtFFM9tRttuBAIrpaYTadFcUe1El7q5t7PNnMEJXA=";
+    tag = finalAttrs.version;
+    hash = "sha256-p16dbXgPcRcvu+N7OLSVWqFI8JfCWvzLA1iovWqEVSE=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) src pname version;
-    hash = "sha256-7xWu9wgZqDzd60buMnF9B6Y5LRkG5C2JWiG7VwgSCvU=";
+    hash = "sha256-6eDQBXTdA4CwWT3IKFq2RvOfOt3rq4RZrqn0Q6FWRgc=";
   };
 
   outputs = [
