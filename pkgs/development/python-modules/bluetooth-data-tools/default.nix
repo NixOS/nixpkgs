f7d5be9d9756a2f6d5e07c4dc0316321c7efd262
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-15 11:43:58 +0200

    python3Packages.bluetooth-data-tools: 1.29.18 -> 1.29.21
    Diff: https://github.com/Bluetooth-Devices/bluetooth-data-tools/compare/v1.29.18...v1.29.21
    
    Changelog: https://github.com/Bluetooth-Devices/bluetooth-data-tools/blob/v1.29.21/CHANGELOG.md

--- pkgs/development/python-modules/bluetooth-data-tools/default.nix
+++ pkgs/development/python-modules/bluetooth-data-tools/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "bluetooth-data-tools";
-  version = "1.29.18";
+  version = "1.29.21";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Bluetooth-Devices";
     repo = "bluetooth-data-tools";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-hY0b9wJa9qBVDJ+Ve0KhX8TXx770Ep+3sp6+UQrYgJI=";
+    hash = "sha256-rRS/vhj3dfJUHEkZxWZ34ORFmlkLUkOjhSAdTlML29Q=";
   };
 
   # The project can build both an optimized cython version and an unoptimized
