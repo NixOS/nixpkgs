65b21638d6aa6103a0bf072bc85ed903d5f77495
Guy Chronister <guychronister@outlook.com>
2026-08-16 16:14:44 +0000

    altus: use finalAttrs pattern in appimageTools.wrapType2

--- pkgs/by-name/al/altus/package.nix
+++ pkgs/by-name/al/altus/package.nix
@@ -5,28 +5,21 @@
   makeWrapper,
 }:
 
-let
+appimageTools.wrapType2 (finalAttrs: {
   pname = "altus";
   version = "5.8.1";
 
   src = fetchurl {
-    name = "altus-${version}.AppImage";
-    url = "https://github.com/amanharwara/altus/releases/download/${version}/Altus-${version}.AppImage";
+    name = "altus-${finalAttrs.version}.AppImage";
+    url = "https://github.com/amanharwara/altus/releases/download/${finalAttrs.version}/Altus-${finalAttrs.version}.AppImage";
     hash = "sha256-FSyXs9thTQ5T5bvCfg/+QXBZMIOyoijAw0dUsvLRGH8=";
   };
-
-  appimageContents = appimageTools.extract {
-    inherit pname version src;
-  };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
 
   nativeBuildInputs = [ makeWrapper ];
 
   extraInstallCommands = ''
-    install -Dm 644 ${appimageContents}/Altus.desktop -t $out/share/applications
-    install -Dm 644 ${appimageContents}/Altus.png -t $out/share/icons/hicolor/256x256/apps
+    install -Dm 644 ${finalAttrs.contents}/Altus.desktop -t $out/share/applications
+    install -Dm 644 ${finalAttrs.contents}/Altus.png -t $out/share/icons/hicolor/256x256/apps
     substituteInPlace $out/share/applications/Altus.desktop \
       --replace-fail 'Exec=AppRun' 'Exec=altus'
     wrapProgram "$out/bin/altus" \
@@ -36,9 +29,9 @@ appimageTools.wrapType2 {
   meta = {
     description = "Client for WhatsApp Web with themes, notifications and multiple accounts support";
     homepage = "https://github.com/amanharwara/altus";
-    changelog = "https://github.com/amanharwara/altus/releases/tag/v${version}";
+    changelog = "https://github.com/amanharwara/altus/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [ johnrtitor ];
     platforms = [ "x86_64-linux" ];
   };
-}
+})
