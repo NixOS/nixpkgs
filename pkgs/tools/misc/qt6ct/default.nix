636847da0ab6984514eb78a15c109fe35bab20f5
ReStranger <restranger@disroot.org>
2026-08-16 17:54:27 +0300

    qt6ct: add kdeSupport patch

--- pkgs/tools/misc/qt6ct/default.nix
+++ pkgs/tools/misc/qt6ct/default.nix
@@ -8,8 +8,9 @@
   qtwayland,
   stdenv,
   wrapQtAppsHook,
+  kdePackages,
+  kdeSupport ? false,
 }:
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "qt6ct";
   version = "0.11";
@@ -22,17 +23,37 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-aQmqLpM0vogMsYaDS9OeKVI3N53uY4NBC4FF10hK8Uw=";
   };
 
+  # Patch adapted from the AUR qt6ct-kde package:
+  # https://aur.archlinux.org/cgit/aur.git/tree/qt6ct-shenanigans.patch?h=qt6ct-kde
+  patches = lib.optionals kdeSupport [
+    ./qt6ct-shenanigans.patch
+  ];
+
   nativeBuildInputs = [
     cmake
     qttools
     wrapQtAppsHook
-  ];
+  ]
+  ++ (lib.optionals kdeSupport [
+    kdePackages.extra-cmake-modules
+  ]);
 
-  buildInputs = [
-    qtbase
-    qtsvg
-    qtwayland
-  ];
+  buildInputs =
+    with kdePackages;
+    [
+      qtbase
+      qtsvg
+      qtwayland
+    ]
+    ++ (lib.optionals kdeSupport (
+      with kdePackages;
+      [
+        kcolorscheme
+        kconfig
+        kiconthemes
+        qtdeclarative
+      ]
+    ));
 
   cmakeFlags = [
     (lib.cmakeFeature "PLUGINDIR" "${placeholder "out"}/${qtbase.qtPluginPrefix}")
@@ -46,6 +67,7 @@ stdenv.mkDerivation (finalAttrs: {
     maintainers = with lib.maintainers; [
       Flakebi
       Scrumplex
+      ReStranger
     ];
     mainProgram = "qt6ct";
   };
