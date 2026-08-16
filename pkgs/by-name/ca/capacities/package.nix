aaa09b14be56d78ce4cd5337dfbe5a4b4de80a2a
Guy Chronister <guychronister@outlook.com>
2026-08-16 21:02:59 +0000

    capacities: switch to finalAttrs pattern in appimageTools

--- pkgs/by-name/ca/capacities/package.nix
+++ pkgs/by-name/ca/capacities/package.nix
@@ -5,7 +5,8 @@
   imagemagick,
   lib,
 }:
-let
+
+appimageTools.wrapType2 (finalAttrs: {
   pname = "capacities";
   version = "1.65.13";
 
@@ -14,44 +15,29 @@ let
     hash = "sha256-ATiX1h9hXmKMFtY6OEyZEoJ/SxJGgbj5/QZwFF1sfFQ=";
   };
 
-  appimageContents = appimageTools.extract {
-    inherit
-      pname
-      src
-      version
-      ;
-  };
-in
-appimageTools.wrapType2 {
-  inherit
-    pname
-    src
-    version
-    ;
-
   extraInstallCommands = ''
     source "${makeWrapper}/nix-support/setup-hook"
     wrapProgram $out/bin/capacities \
       --add-flags "--ozone-platform-hint=auto"
 
     # Check for required desktop file
-    if [ ! -f ${appimageContents}/capacities.desktop ]; then
-      echo "Error: Missing .desktop file in ${appimageContents}"
+    if [ ! -f ${finalAttrs.contents}/capacities.desktop ]; then
+      echo "Error: Missing .desktop file in ${finalAttrs.contents}"
       exit 1
     else
       # Install and modify the desktop file
-      install -m 444 -D ${appimageContents}/capacities.desktop $out/share/applications/capacities.desktop
+      install -m 444 -D ${finalAttrs.contents}/capacities.desktop $out/share/applications/capacities.desktop
       substituteInPlace $out/share/applications/capacities.desktop \
         --replace-fail "Exec=AppRun" "Exec=capacities"
     fi
 
     # Check for required icon file
-    if [ ! -f ${appimageContents}/capacities.png ]; then
-      echo "Error: Missing icon file in ${appimageContents}"
+    if [ ! -f ${finalAttrs.contents}/capacities.png ]; then
+      echo "Error: Missing icon file in ${finalAttrs.contents}"
       exit 1
     else
       # Resize and install the icon
-      ${lib.getExe imagemagick} ${appimageContents}/capacities.png -resize 512x512 capacities_512.png
+      ${lib.getExe imagemagick} ${finalAttrs.contents}/capacities.png -resize 512x512 capacities_512.png
       install -m 444 -D capacities_512.png $out/share/icons/hicolor/512x512/apps/capacities.png
     fi
   '';
@@ -65,4 +51,4 @@ appimageTools.wrapType2 {
     mainProgram = "capacities";
     maintainers = [ lib.maintainers.keysmashes ];
   };
-}
+})
