16ec8dbde04a4e429d43e915bbad0f4e6221b4f0
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-16 02:13:37 +0000

    clboss: 0.16.1 -> 0.16.2

--- pkgs/by-name/cl/clboss/package.nix
+++ pkgs/by-name/cl/clboss/package.nix
@@ -13,12 +13,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "clboss";
-  version = "0.16.1";
+  version = "0.16.2";
 
   # The release tarball includes the pre-generated file `commit_hash.h` that is required for building
   src = fetchzip {
     url = "https://github.com/ZmnSCPxj/clboss/releases/download/v${finalAttrs.version}/clboss-v${finalAttrs.version}.tar.gz";
-    hash = "sha256-32v57d0ntNkPH6vMPd+mcY/z7gQi0shqXD3Fw10dHb4=";
+    hash = "sha256-I83OH+31oqx2FSjzEIsz7uOko3/MbUqjrZ+xZTXxtto=";
   };
 
   nativeBuildInputs = [
