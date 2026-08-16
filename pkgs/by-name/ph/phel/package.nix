1bdf29ef61f5b1252f9d9ba68e9156aeb3b2d27c
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-16 22:21:36 +0000

    phel: 0.45.1 -> 0.50.0

--- pkgs/by-name/ph/phel/package.nix
+++ pkgs/by-name/ph/phel/package.nix
@@ -7,16 +7,16 @@
 
 php.buildComposerProject2 (finalAttrs: {
   pname = "phel";
-  version = "0.45.1";
+  version = "0.50.0";
 
   src = fetchFromGitHub {
     owner = "phel-lang";
     repo = "phel-lang";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+4sseXsHXnOYC4I6ntDdloPYxhrusVT5D7S6h/NWdCs=";
+    hash = "sha256-bR1UwwtJZj4ja5PJKp4M4p1PDVBx5cDqASIhvuv3xJI=";
   };
 
-  vendorHash = "sha256-GYBriug9CmIsZkyG1xEmqyu1K3NDiskDnxeHWIZzM7o=";
+  vendorHash = "sha256-b2L4kr70QLTp9g01ChnutYT4IPZOvfGithhg1OAnN0k=";
 
   doInstallCheck = true;
   nativeInstallCheckInputs = [ versionCheckHook ];
