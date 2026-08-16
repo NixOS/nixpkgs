31004233aab343fe01be0a23e2a8095aa3a1e0c6
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-16 06:15:53 +0000

    llmfit: 1.1.8 -> 1.1.9

--- pkgs/by-name/ll/llmfit/package.nix
+++ pkgs/by-name/ll/llmfit/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "llmfit";
-  version = "1.1.8";
+  version = "1.1.9";
 
   src = fetchFromGitHub {
     owner = "AlexsJones";
     repo = "llmfit";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-6mARQqRsPtsN0WAp4oKqG1jl062BqDJ1D9AZBLomel8=";
+    hash = "sha256-DCJ6TtPza/pA8cbYiVBiK2iyOB2gwSIZsJuL2GeB810=";
   };
 
-  cargoHash = "sha256-iUh9VBtDfuil96av1I5XlO/mZ/Q9HccBJitBfJ+i69A=";
+  cargoHash = "sha256-sKfG6kiqkCZ5vlG71cjR7aMNBGe7RoZCiQogYDywMC4=";
 
   passthru.updateScript = nix-update-script { };
 
