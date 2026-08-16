26f48f73c77bc39225d49cedd82ceabd7c9c3b73
SchweGELBin <schwegelbin@gmail.com>
2026-08-16 23:34:34 +0200

    meowlnir: 26.06 -> 26.08

--- pkgs/by-name/me/meowlnir/package.nix
+++ pkgs/by-name/me/meowlnir/package.nix
@@ -9,19 +9,19 @@
 
 buildGoModule rec {
   pname = "meowlnir";
-  version = "26.06";
-  tag = "v0.2606.0";
+  version = "26.08";
+  tag = "v0.2608.0";
 
   src = fetchFromGitHub {
     owner = "maunium";
     repo = "meowlnir";
     inherit tag;
-    hash = "sha256-mhm/CznhaQCgx0ZQ/GArmWrhDS0sPkIkJrP3cAOIFME=";
+    hash = "sha256-q9/pdKmJOav41P8Rm4ZzNgeEPJqui22HUbH4UBoa3tY=";
   };
 
   buildInputs = [ olm ];
 
-  vendorHash = "sha256-0PgI0m2EsfiZjpOQ9lTfVavHNzKWKFdlmkIz2gqUXM0=";
+  vendorHash = "sha256-HXXAQaBaWUjnBnoiyNZB51BnPuQg9BhlDU5x2CyCW1Y=";
 
   doCheck = true;
   doInstallCheck = true;
