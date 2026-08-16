b31f4780184f666ceb49f62f5ee36428e7b428c2
pinage404 <pinage404@gmail.com>
2026-08-16 18:44:36 +0200

    mdbook-rss-feed: tell the difference with the full package

--- pkgs/by-name/md/mdbook-rss-feed/package.nix
+++ pkgs/by-name/md/mdbook-rss-feed/package.nix
@@ -7,6 +7,11 @@
   withJsonFeed ? false,
 }:
 
+let
+  buildFeatures = lib.optional withAtom "atom" ++ lib.optional withJsonFeed "json-feed";
+  hasOptionalFeatures = buildFeatures != [ ];
+  withOptionalFeatures = lib.optionalString hasOptionalFeatures " with ${lib.strings.concatStringsSep " and " buildFeatures} features";
+in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "mdbook-rss-feed";
   version = "1.8.1";
@@ -21,12 +26,13 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   cargoHash = "sha256-UkoNcwEq2Ga3PEF04Xly++VdBekdCBKSHggIGXjTMXU=";
 
-  buildFeatures = lib.optional withAtom [ "atom" ] ++ lib.optional withJsonFeed [ "json-feed" ];
+  inherit buildFeatures;
 
   passthru.updateScript = nix-update-script { };
 
   meta = {
-    description = "mdBook preprocessor that generates a full-content RSS 2.0 feed";
+    description =
+      "mdBook preprocessor that generates a full-content RSS 2.0 feed" + withOptionalFeatures;
     homepage = "https://github.com/saylesss88/mdbook-rss-feed";
     changelog = "https://github.com/saylesss88/mdbook-rss-feed/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.asl20;
