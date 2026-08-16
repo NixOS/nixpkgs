af9e41041b6d0523ef77822c9339e1d209dd17ce
rdk31 <git@rdk31.com>
2026-08-16 16:46:02 +0200

    python3Packages.skan: init at 0.13.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18685,6 +18685,8 @@ self: super: with self; {
 
   sjcl = callPackage ../development/python-modules/sjcl { };
 
+  skan = callPackage ../development/python-modules/skan { };
+
   skein = callPackage ../development/python-modules/skein { };
 
   skia-pathops = callPackage ../development/python-modules/skia-pathops { };
