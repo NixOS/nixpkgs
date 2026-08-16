27d3b82417ee1d0aaec8659b1bbe207f16636aca
Guy Chronister <guychronister@outlook.com>
2026-08-16 21:02:58 +0000

    beyond-all-reason: switch to finalAttrs pattern in appimageTools

--- pkgs/by-name/be/beyond-all-reason/package.nix
+++ pkgs/by-name/be/beyond-all-reason/package.nix
@@ -4,24 +4,21 @@
   appimageTools,
   openal,
 }:
-let
+
+appimageTools.wrapType2 (finalAttrs: {
   version = "1.2988.0";
   pname = "beyond-all-reason";
 
   src = fetchurl {
-    url = "https://github.com/beyond-all-reason/BYAR-Chobby/releases/download/v${version}/Beyond-All-Reason-${version}.AppImage";
+    url = "https://github.com/beyond-all-reason/BYAR-Chobby/releases/download/v${finalAttrs.version}/Beyond-All-Reason-${finalAttrs.version}.AppImage";
     hash = "sha256-ZJW5BdxxqyrM2TJTO0SBp4BXt3ILyi77EZx73X8hqJE=";
   };
-  appimageContents = appimageTools.extract { inherit pname version src; };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
 
   extraPkgs = pkgs: [ openal ];
 
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/beyond-all-reason.desktop $out/share/applications/beyond-all-reason.desktop
-    install -m 444 -D ${appimageContents}/beyond-all-reason.png \
+    install -m 444 -D ${finalAttrs.contents}/beyond-all-reason.desktop $out/share/applications/beyond-all-reason.desktop
+    install -m 444 -D ${finalAttrs.contents}/beyond-all-reason.png \
       $out/share/icons/hicolor/256x256/apps/beyond-all-reason.png
     substituteInPlace $out/share/applications/beyond-all-reason.desktop \
       --replace-fail 'Exec=AppRun' 'Exec=beyond-all-reason'
@@ -30,7 +27,7 @@ appimageTools.wrapType2 {
   meta = {
     homepage = "https://www.beyondallreason.info/";
     downloadPage = "https://www.beyondallreason.info/download";
-    changelog = "https://github.com/beyond-all-reason/BYAR-Chobby/releases/tag/v${version}";
+    changelog = "https://github.com/beyond-all-reason/BYAR-Chobby/releases/tag/v${finalAttrs.version}";
     description = "Free Real Time Strategy Game with a grand scale and full physical simulation in a sci-fi setting";
     license = lib.licenses.gpl2Plus;
     platforms = [ "x86_64-linux" ];
@@ -38,4 +35,4 @@ appimageTools.wrapType2 {
       kiyotoko
     ];
   };
-}
+})
