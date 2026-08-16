97e8e441259399790479d4d42acf2efe0f4870a6
Guy Chronister <guychronister@outlook.com>
2026-08-16 21:02:58 +0000

    buckets: switch to finalAttrs pattern in appimageTools

--- pkgs/by-name/bu/buckets/package.nix
+++ pkgs/by-name/bu/buckets/package.nix
@@ -26,25 +26,26 @@ let
     }
     .${system};
 
+in
+appimageTools.wrapType2 (finalAttrs: {
   pname = "buckets";
   version = "0.80.0";
+
   src = fetchurl {
-    url = "https://github.com/buckets/application/releases/download/v${version}/Buckets-linux-latest-${platform}-${version}.AppImage";
+    url = "https://github.com/buckets/application/releases/download/v${finalAttrs.version}/Buckets-linux-latest-${platform}-${finalAttrs.version}.AppImage";
     inherit hash;
   };
-  appimageContents = appimageTools.extract { inherit pname version src; };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
+
   extraPkgs = pkgs: (appimageTools.defaultFhsEnvArgs.multiPkgs pkgs) ++ [ pkgs.libsodium ];
+
   extraInstallCommands = ''
     source "${makeWrapper}/nix-support/setup-hook"
     wrapProgram $out/bin/buckets
-    install -m 444 -D ${appimageContents}/buckets.desktop -t $out/share/applications
+    install -m 444 -D ${finalAttrs.contents}/buckets.desktop -t $out/share/applications
     substituteInPlace $out/share/applications/buckets.desktop \
         --replace-fail 'Exec=AppRun' 'Exec=buckets'
     for size in 16 32 48 64 128 256 512; do
-        install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/''${size}x''${size}/apps/buckets.png \
+        install -m 444 -D ${finalAttrs.contents}/usr/share/icons/hicolor/''${size}x''${size}/apps/buckets.png \
           $out/share/icons/hicolor/''${size}x''${size}/apps/buckets.png
     done
   '';
@@ -60,4 +61,4 @@ appimageTools.wrapType2 {
     ];
     mainProgram = "buckets";
   };
-}
+})
