c0d0603297974b8cda632953321a3791decc7fe3
ArisoN <arison@duck.com>
2026-08-17 02:14:00 +0300

    pipewire-control-center: 0.5.0 -> 0.5.1
    Diff: https://github.com/knightinfected/PipeWireController/compare/v0.5.0...v0.5.1
    
    Changelog: https://github.com/knightinfected/PipeWireController/blob/refs/tags/v0.5.1/CHANGELOG.md

--- pkgs/by-name/pi/pipewire-control-center/package.nix
+++ pkgs/by-name/pi/pipewire-control-center/package.nix
@@ -13,7 +13,7 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "pipewire-control-center";
-  version = "0.5.0";
+  version = "0.5.1";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -21,7 +21,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
     owner = "knightinfected";
     repo = "PipeWireController";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-18Tbwv4uv75U+l7TQY+6spppB5OkSfHmDn2pSy5EUGM=";
+    hash = "sha256-9XODnl2FDQAVZ3mtiP24wgzK5K2qYO9eW1hFr3y5Ra0=";
   };
 
   build-system = [
