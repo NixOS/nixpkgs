540b68829def61589f7807020d2466897ac37296
Guy Chronister <guychronister@outlook.com>
2026-08-16 21:02:58 +0000

    artix-games-launcher: switch to finalAttrs pattern in appimageTools

--- pkgs/by-name/ar/artix-games-launcher/package.nix
+++ pkgs/by-name/ar/artix-games-launcher/package.nix
@@ -1,27 +1,22 @@
 {
   appimageTools,
-  stdenv,
   fetchurl,
   lib,
 }:
-let
+
+appimageTools.wrapType2 (finalAttrs: {
   pname = "artix-games-launcher";
   version = "2.20";
+
   src = fetchurl {
     url = "https://web.archive.org/web/20250924101414/https://launch.artix.com/latest/Artix_Games_Launcher-x86_64.AppImage";
     hash = "sha256-8eVXOm5g92wErWa6lbTXrCL04MWYlObjonHJk+oUI3E=";
   };
-  appimageContents = appimageTools.extract {
-    inherit pname version src;
-  };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
 
   extraInstallCommands = ''
     mkdir -p $out/share/applications
-    install -m 444 -D ${appimageContents}/ArtixGamesLauncher.desktop $out/share/applications/ArtixGamesLauncher.desktop
-    install -m 444 -D ${appimageContents}/ArtixLogo.png $out/share/icons/ArtixLogo.png
+    install -m 444 -D ${finalAttrs.contents}/ArtixGamesLauncher.desktop $out/share/applications/ArtixGamesLauncher.desktop
+    install -m 444 -D ${finalAttrs.contents}/ArtixLogo.png $out/share/icons/ArtixLogo.png
     substituteInPlace $out/share/applications/ArtixGamesLauncher.desktop --replace-fail 'Exec=ArtixGameLauncher %u' 'Exec=artix-games-launcher %u'
   '';
 
@@ -33,4 +28,4 @@ appimageTools.wrapType2 {
     maintainers = with lib.maintainers; [ jtliang24 ];
     platforms = [ "x86_64-linux" ];
   };
-}
+})
