8af152ca3d1fa4c7c05aa8b319c6f68b11e8ecc6
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-16 10:06:27 +0200

    ffuf: 2.1.0 -> 2.2.1
    Changelog: https://github.com/ffuf/ffuf/releases/tag/v2.2.1

--- pkgs/by-name/ff/ffuf/package.nix
+++ pkgs/by-name/ff/ffuf/package.nix
@@ -2,47 +2,45 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
-  fetchpatch,
+  versionCheckHook,
 }:
 
 buildGoModule (finalAttrs: {
   pname = "ffuf";
-  version = "2.1.0";
+  version = "2.2.1";
 
   src = fetchFromGitHub {
     owner = "ffuf";
     repo = "ffuf";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+wcNqQHtB8yCLiJXMBxolCWsYZbBAsBGS1hs7j1lzUU=";
+    hash = "sha256-xN7FxxxIpkaGlfBgs0RwEPlzo/HLMfioC6MAMVP2su8=";
   };
 
   vendorHash = "sha256-SrC6Q7RKf+gwjJbxSZkWARw+kRtkwVv1UJshc/TkNdc=";
 
-  patches = [
-    # Fix CSV test, https://github.com/ffuf/ffuf/pull/731
-    (fetchpatch {
-      name = "fix-csv-test.patch";
-      url = "https://github.com/ffuf/ffuf/commit/7f2aae005ad73988a1fa13c1c33dab71f4ae5bbd.patch";
-      hash = "sha256-/v9shGICmsbFfEJe4qBkBHB9PVbBlrjY3uFmODxHu9M=";
-    })
+  ldflags = [
+    "-s"
+    "-X github.com/ffuf/ffuf/v${(lib.versions.major finalAttrs.version)}/pkg/ffuf.VERSION=${finalAttrs.version}"
+    "-X github.com/ffuf/ffuf/v${(lib.versions.major finalAttrs.version)}/pkg/ffuf.VERSION_APPENDIX="
   ];
 
-  ldflags = [
-    "-w"
-    "-s"
-  ];
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  doInstallCheck = true;
+
+  versionCheckProgramArg = "-V";
 
   meta = {
     description = "Tool for web fuzzing";
-    mainProgram = "ffuf";
     longDescription = ''
-      FFUF, or “Fuzz Faster you Fool” is an open source web fuzzing tool,
+      FFUF, or "Fuzz Faster you Fool" is an open source web fuzzing tool,
       intended for discovering elements and content within web applications
       or web servers.
     '';
     homepage = "https://github.com/ffuf/ffuf";
-    changelog = "https://github.com/ffuf/ffuf/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/ffuf/ffuf/releases/tag/v${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
+    mainProgram = "ffuf";
   };
 })
