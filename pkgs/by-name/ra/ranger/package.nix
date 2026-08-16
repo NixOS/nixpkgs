07a916a4af913ac86a957f29d18f3c4fbd8d69f5
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-16 06:38:25 +0000

    ranger: 1.9.4-unstable-2026-08-06 -> 1.9.4-unstable-2026-08-15

--- pkgs/by-name/ra/ranger/package.nix
+++ pkgs/by-name/ra/ranger/package.nix
@@ -19,14 +19,14 @@
 
 python3Packages.buildPythonApplication {
   pname = "ranger";
-  version = "1.9.4-unstable-2026-08-06";
+  version = "1.9.4-unstable-2026-08-15";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ranger";
     repo = "ranger";
-    rev = "d45f14c04dd5cee64cdd6d1c560a1e3e1e9efd05";
-    hash = "sha256-Y+kEHp5eEi+ydf87WIwK6xzzohCNrmM5Q5Bv9/RmjdQ=";
+    rev = "3187384e4078d91848bd22ea0dc8eb5fe2866f59";
+    hash = "sha256-v5f7bSpfc3Up7sdnd+ILVG+crB4Aiw8uUfdR/QdZXVw=";
   };
 
   build-system = with python3Packages; [
