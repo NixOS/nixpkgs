fe3a566a4ca6f7a4fe6aee73e9abb5a92f92ed2f
OPNA2608 <opna2608@protonmail.com>
2026-08-16 16:25:11 +0200

    dosbox-x: Fix compat with FFmpeg 9

--- pkgs/by-name/do/dosbox-x/package.nix
+++ pkgs/by-name/do/dosbox-x/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   gitUpdater,
   alsa-lib,
   autoreconfHook,
@@ -37,6 +38,16 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "dosbox-x-v${finalAttrs.version}";
     hash = "sha256-HO5JJQHeEJYXU7TtNCXZALgqkNbIMGI+TjDwbvs5L8w=";
   };
+
+  patches = [
+    # https://github.com/joncampbell123/dosbox-x/issues/6446
+    # Remove when version > 2026.08.02
+    (fetchpatch {
+      name = "0001-dosbox-x-Fix-FFmpeg-9-compat.patch";
+      url = "https://github.com/joncampbell123/dosbox-x/commit/18062c0ffb33db35fa52ce937ce25223140c0293.patch";
+      hash = "sha256-S6DMmofFBloZFut0M3ZVAV/awG8Af9xjO2AcfVusOdg=";
+    })
+  ];
 
   # sips is unavailable in sandbox, replacing with imagemagick breaks build due to wrong Foundation propagation(?) so don't generate resolution variants
   # iconutil is unavailable, replace with png2icns from libicns
