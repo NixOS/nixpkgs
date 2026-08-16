2d2d9880595f83ace0b0179551aa0afbecca6f0b
Guy Chronister <guychronister@outlook.com>
2026-08-16 21:02:58 +0000

    bloomrpc: switch to finalAttrs pattern in appimageTools

--- pkgs/by-name/bl/bloomrpc/package.nix
+++ pkgs/by-name/bl/bloomrpc/package.nix
@@ -4,33 +4,26 @@
   appimageTools,
 }:
 
-let
+appimageTools.wrapType2 (finalAttrs: {
   pname = "bloomrpc";
   version = "1.5.3";
 
   src = fetchurl {
-    url = "https://github.com/uw-labs/${pname}/releases/download/${version}/BloomRPC-${version}.AppImage";
-    name = "${pname}-${version}.AppImage";
+    url = "https://github.com/uw-labs/bloomrpc/releases/download/${finalAttrs.version}/BloomRPC-${finalAttrs.version}.AppImage";
+    name = "bloomrpc-${finalAttrs.version}.AppImage";
     hash = "sha512-PebdYDpcplPN5y3mRu1mG6CXenYfYvBXNLgIGEr7ZgKnR5pIaOfJNORSNYSdagdGDb/B1sxuKfX4+4f2cqgb6Q==";
   };
-
-  appimageContents = appimageTools.extract {
-    inherit pname src version;
-  };
-in
-appimageTools.wrapType2 {
-  inherit pname src version;
 
   profile = ''
     export LC_ALL=C.UTF-8
   '';
 
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
-    install -m 444 -D ${appimageContents}/${pname}.png \
-      $out/share/icons/hicolor/512x512/apps/${pname}.png
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' 'Exec=${pname}'
+    install -m 444 -D ${finalAttrs.contents}/bloomrpc.desktop $out/share/applications/bloomrpc.desktop
+    install -m 444 -D ${finalAttrs.contents}/bloomrpc.png \
+      $out/share/icons/hicolor/512x512/apps/bloomrpc.png
+    substituteInPlace $out/share/applications/bloomrpc.desktop \
+      --replace-fail 'Exec=AppRun' 'Exec=bloomrpc'
   '';
 
   meta = {
@@ -44,4 +37,4 @@ appimageTools.wrapType2 {
     maintainers = with lib.maintainers; [ zoedsoupe ];
     platforms = [ "x86_64-linux" ];
   };
-}
+})
