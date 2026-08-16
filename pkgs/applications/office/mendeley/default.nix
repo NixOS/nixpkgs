0101b1c4f5abe09d36ec9a579508e776f511cd12
Guy Chronister <guychronister@outlook.com>
2026-08-16 15:24:46 +0000

    mendeley: simplify AppImage wrapping, remove imagemagick, fix desktop/icon install
    - Switch to `wrapType2 (finalAttrs: { ... })` style for cleaner access to
      `finalAttrs.contents` and `finalAttrs.version`.
    - Drop `imagemagick` dependency by installing the bundled 512×512 icon directly.
    - Remove manual renaming of the AppImage binary; rely on wrapType2’s default
      `mendeley` executable.
    - Install the upstream `.desktop` file from extracted contents and patch Exec to
      point to the wrapped binary.
    - Update `mainProgram` to `mendeley`.
    - Add strictDeps and structuredAttrs

--- pkgs/applications/office/mendeley/default.nix
+++ pkgs/applications/office/mendeley/default.nix
@@ -2,35 +2,27 @@
   lib,
   fetchurl,
   appimageTools,
-  imagemagick,
 }:
 
-let
+appimageTools.wrapType2 (finalAttrs: {
   pname = "mendeley";
   version = "2.145.0";
 
-  executableName = "${pname}-reference-manager";
-
   src = fetchurl {
-    url = "https://static.mendeley.com/bin/desktop/mendeley-reference-manager-${version}-x86_64.AppImage";
+    url = "https://static.mendeley.com/bin/desktop/mendeley-reference-manager-${finalAttrs.version}-x86_64.AppImage";
     hash = "sha256-yuoNGAV6JuPfm5GagzD4R2ojBRpKo9aZ8K92jC63MQE=";
   };
 
-  appimageContents = appimageTools.extract {
-    inherit pname version src;
-  };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
+  strictDeps = true;
+  __structuredAttrs = true;
 
   extraInstallCommands = ''
-    mv $out/bin/$pname $out/bin/${executableName}
-    install -m 444 -D ${appimageContents}/${executableName}.desktop $out/share/applications/${executableName}.desktop
-    ${imagemagick}/bin/convert ${appimageContents}/${executableName}.png -resize 512x512 ${pname}_512.png
-    install -m 444 -D ${pname}_512.png $out/share/icons/hicolor/512x512/apps/${executableName}.png
+    install -m 444 -D ${finalAttrs.contents}/mendeley-reference-manager.desktop $out/share/applications/mendeley-reference-manager.desktop
 
-    substituteInPlace $out/share/applications/${executableName}.desktop \
-      --replace 'Exec=AppRun' 'Exec=${executableName}'
+    install -m 444 -D ${finalAttrs.contents}/mendeley-reference-manager.png $out/share/icons/hicolor/512x512/apps/mendeley-reference-manager.png
+
+    substituteInPlace $out/share/applications/mendeley-reference-manager.desktop \
+      --replace-fail 'Exec=AppRun' 'Exec=mendeley'
   '';
 
   meta = {
@@ -40,7 +32,6 @@ appimageTools.wrapType2 {
     license = lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
     maintainers = [ ];
-    mainProgram = "mendeley-reference-manager";
+    mainProgram = "mendeley";
   };
-
-}
+})
