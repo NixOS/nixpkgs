ffb4da48a03b9e5098d2838458725b8456219145
qubitnano <146656568+qubitnano@users.noreply.github.com>
2026-08-15 23:38:26 -0400

    starfox64recomp: switch to gitlab

--- pkgs/by-name/st/starfox64recomp/package.nix
+++ pkgs/by-name/st/starfox64recomp/package.nix
@@ -4,7 +4,8 @@
   requireFile,
   fetchurl,
   fetchFromGitHub,
-  llvmPackages_19,
+  fetchFromGitLab,
+  llvmPackages_21,
   cmake,
   copyDesktopItems,
   installShellFiles,
@@ -42,31 +43,84 @@ let
 
   comptool = fetchurl {
     name = "comptool.py";
-    url = "https://raw.githubusercontent.com/sonicdcer/sf64/83eeee7d96fbe5a6b6fa013e13ed7eda8213b3e3/tools/comptool.py";
+    url = "https://gitlab.com/sonicdcer/sf64/-/raw/3c5d492d875c8a74f9657c86fb9c2480f4c4919f/tools/comptool.py";
     hash = "sha256-md56iEj6DKODKm3U0XrAygiaRlUgvFCSWfWyyh4lmzw=";
+  };
+
+  submodules = {
+    "lib/RmlUi" = fetchFromGitHub {
+      owner = "mikke89";
+      repo = "RmlUi";
+      rev = "7a06f27db04fe5d13a5dacc19b2b4544673a4eca";
+      hash = "sha256-aBu97ZugJIfJTYFpgDYC/OU+pX/fe9ne4PsTG01mWIM=";
+    };
+    "lib/freetype-windows-binaries" = fetchFromGitHub {
+      owner = "ubawurinna";
+      repo = "freetype-windows-binaries";
+      rev = "d6fb49d11a9d0011bf4ecfe7e570beaaa189838a";
+      hash = "sha256-rk+D+bv84yCS9fBv3ZpMqgUwsAzz9wa/TiLoWyhI3f0=";
+    };
+    "lib/lunasvg" = fetchFromGitHub {
+      owner = "sammycage";
+      repo = "lunasvg";
+      rev = "4166d0cccfc059b39d5ecfc372524375e59448f9";
+      hash = "sha256-U/ohYe5j/c7bGvEFkEHZPggdzt6vu9ThnzVgECG8AWk=";
+    };
+    "lib/sse2neon" = fetchFromGitHub {
+      owner = "DLTcollab";
+      repo = "sse2neon";
+      rev = "706d3b58025364c2371cafcf9b16e32ff7e630ed";
+      hash = "sha256-jeaGP6j/ML6W+ls1ZKUQHWy4gXqkIrV3V918+YIDoXY=";
+    };
+    "lib/N64ModernRuntime" = fetchFromGitHub {
+      owner = "N64Recomp";
+      repo = "N64ModernRuntime";
+      rev = "9ae9dbbe41cafcd6f425135c726aebe80777d682";
+      hash = "sha256-SpDHUBcnTR0gg+qmffX3fdUn8KvN4pEwoZXdfvuuZlI=";
+      fetchSubmodules = true;
+    };
+    "lib/rt64" = fetchFromGitHub {
+      owner = "rt64";
+      repo = "rt64";
+      rev = "aa047b8158034552466175b8e8554988caa18976";
+      hash = "sha256-Jc47Hr4R3GLCPXzPR7Z7T+MMi0aaZcF3kK1FmjA6cl0=";
+      fetchSubmodules = true;
+    };
+    "lib/sf64decomp" = fetchFromGitLab {
+      owner = "sonicdcer";
+      repo = "sf64";
+      rev = "b870a09cf33e882463dfecedaf917a9505b79cec";
+      hash = "sha256-VJjGPNgPWP6McMxAM9P6Nt0H9CY5mQheD7lsq7e9igE=";
+    };
+    "Starfox64RecompSyms" = fetchFromGitLab {
+      owner = "sonicdcer";
+      repo = "Starfox64RecompSyms";
+      rev = "156aa2e840e79be04e752774cbc8d09d6f44d7a8";
+      hash = "sha256-NK1UOjz5DED9TYMWjxxz8MrFY9EHEvTchfXzwv0xVpk=";
+    };
   };
 
 in
 
-llvmPackages_19.stdenv.mkDerivation (finalAttrs: {
+llvmPackages_21.stdenv.mkDerivation (finalAttrs: {
   pname = "starfox64recomp";
   version = "1.0.3";
 
-  src = fetchFromGitHub {
+  src = fetchFromGitLab {
     owner = "sonicdcer";
     repo = "Starfox64Recomp";
-    tag = finalAttrs.version;
-    hash = "sha256-JhrVF7YjDrZcmL+7S8g/g5SQ3WcxnM9LJryT0Ss3uMw=";
-    fetchSubmodules = true;
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-fOc64p5WZ98rxW/KWk1dYs3aqRxqBkFP66m7z07P3oQ=";
   };
 
   strictDeps = true;
+  __structuredAttrs = true;
 
   nativeBuildInputs = [
     cmake
     copyDesktopItems
     installShellFiles
-    llvmPackages_19.lld
+    llvmPackages_21.lld
     makeWrapper
     ninja
     pkg-config
@@ -90,6 +144,13 @@ llvmPackages_19.stdenv.mkDerivation (finalAttrs: {
       categories = [ "Game" ];
     })
   ];
+
+  postUnpack = lib.concatLines (
+    lib.mapAttrsToList (path: drv: ''
+      mkdir -p "$sourceRoot/${path}"
+      cp -r ${drv}/. "$sourceRoot/${path}/"
+    '') submodules
+  );
 
   preConfigure = ''
     ln -s ${baseRom} ./starfox64.us.rev1.z64
