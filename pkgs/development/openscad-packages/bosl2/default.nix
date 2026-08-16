76d94e7766115862d38c89184f34f2761d805203
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-16 08:07:51 +0000

    openscadPackages.bosl2: 2.0.749 -> 2.0.750

--- pkgs/development/openscad-packages/bosl2/default.nix
+++ pkgs/development/openscad-packages/bosl2/default.nix
@@ -9,7 +9,7 @@
 
 buildOpenSCADPackage (finalAttrs: {
   pname = "bosl2";
-  version = "2.0.749";
+  version = "2.0.750";
   libName = "BOSL2";
 
   installTargets = [ "*.scad" ];
@@ -18,7 +18,7 @@ buildOpenSCADPackage (finalAttrs: {
     owner = "BelfrySCAD";
     repo = "BOSL2";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-YUJGllNgkw+w1fzsa8rzxyjKq2ZawrcuoRpqY7rTAxc=";
+    hash = "sha256-ErSJxw1hLlnyUQCXBChwBrN9kS6+l4KXYvu9bBO1m6w=";
   };
 
   passthru = {
