480683413e6ad18fd6abbd6eceb471cf40c6ba0d
Gaetan Lepage <gaetan@glepage.com>
2026-08-16 19:19:13 +0000

    python3Packages.nipreps-versions: 1.0.4 -> 1.2.0
    Diff: https://github.com/nipreps/version-schemes/compare/1.0.4...1.2.0

--- pkgs/development/python-modules/nipreps-versions/default.nix
+++ pkgs/development/python-modules/nipreps-versions/default.nix
@@ -2,39 +2,59 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  flit-scm,
+
+  # build-system
+  hatch-vcs,
+  hatchling,
+
+  # dependencies
   packaging,
   setuptools-scm,
+  vcs-versioning,
+
+  # tests
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "nipreps-versions";
-  version = "1.0.4";
+  version = "1.2.0";
   pyproject = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "nipreps";
     repo = "version-schemes";
-    tag = version;
-    hash = "sha256-B2wtLurzgk59kTooH51a2dewK7aEyA0dAm64Wp+tqhM=";
+    tag = finalAttrs.version;
+    hash = "sha256-SPJ4L68dFcSv1+Ytp3b3n+lr35Krq71fIlaReFux1nk=";
   };
 
-  nativeBuildInputs = [
-    flit-scm
-    setuptools-scm
+  build-system = [
+    hatch-vcs
+    hatchling
   ];
 
-  propagatedBuildInputs = [ packaging ];
+  dependencies = [
+    packaging
+    setuptools-scm
+    vcs-versioning
+  ];
 
   nativeCheckInputs = [ pytestCheckHook ];
+
   pythonImportsCheck = [ "nipreps_versions" ];
+
+  disabledTests = [
+    # Rely on the current date, which is pinned to 1980 in the sandbox (`SOURCE_DATE_EPOCH`)
+    "invalid_branch_version"
+    "test_next_calver"
+  ];
 
   meta = {
     description = "Setuptools_scm plugin for nipreps version schemes";
     homepage = "https://github.com/nipreps/version-schemes";
-    changelog = "https://github.com/nipreps/version-schemes/blob/${src.rev}/CHANGES.md";
+    changelog = "https://github.com/nipreps/version-schemes/blob/${finalAttrs.src.tag}/CHANGES.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ bcdarwin ];
   };
-}
+})
