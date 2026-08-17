619a5b78ee41c023a23582d64a3df1fb6a2b4934
Gaetan Lepage <gaetan@glepage.com>
2026-08-17 11:20:34 +0000

    python3Packages.python-lsp-ruff: 2.3.2 -> 2.3.3
    Diff: https://github.com/python-lsp/python-lsp-ruff/compare/v2.3.2...v2.3.3
    
    Changelog: https://github.com/python-lsp/python-lsp-ruff/releases/tag/v2.3.3

--- pkgs/development/python-modules/python-lsp-ruff/default.nix
+++ pkgs/development/python-modules/python-lsp-ruff/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "python-lsp-ruff";
-  version = "2.3.2";
+  version = "2.3.3";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -24,7 +24,7 @@ buildPythonPackage (finalAttrs: {
     owner = "python-lsp";
     repo = "python-lsp-ruff";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dTQw1j7nkwc2Usy1TzziBgMl5UmLJU/6JWt7P3qAzL8=";
+    hash = "sha256-KRA/yDZaDtxNV8X9nOGfE+H1EyNYtRwNzsRmIqJASmU=";
   };
 
   postPatch =
