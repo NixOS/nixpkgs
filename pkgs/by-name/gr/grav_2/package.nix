98727f8a61266f9761012297f510a9950cdac211
Robert Helgesson <robert@rycee.net>
2026-08-17 15:40:50 +0200

    grav_2: init at 2.0.24

---
+++ pkgs/by-name/gr/grav_2/package.nix
@@ -0,0 +1,42 @@
+{
+  stdenvNoCC,
+  lib,
+  fetchzip,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "grav";
+  version = "2.0.24";
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  src =
+    let
+      v = finalAttrs.version;
+    in
+    fetchzip {
+      url = "https://github.com/getgrav/grav/releases/download/${v}/grav-admin-v${v}.zip";
+      hash = "sha256-Plow++quy9/77wVr/Thz6QcFAn+rIJercbUdhKDe0Qs=";
+    };
+
+  patches = [
+    # Disables functionality that attempts to edit files in Nix store. Also adds
+    # a block of the self-upgrade command.
+    ./01-nix.patch
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/
+    cp -R . $out/
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Fast, simple, and flexible, file-based web platform";
+    homepage = "https://getgrav.com";
+    maintainers = with lib.maintainers; [ rycee ];
+    license = lib.licenses.mit;
+  };
+})
