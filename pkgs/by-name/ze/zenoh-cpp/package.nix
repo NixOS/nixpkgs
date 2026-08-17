7f24fa6f528c4842f9d462a66dea211f2f565f08
Markus Kowalewski <markus.kowalewski@gmail.com>
2026-08-17 13:40:48 +0200

    zenoh-cpp: 1.9.0 -> 1.10.0

--- pkgs/by-name/ze/zenoh-cpp/package.nix
+++ pkgs/by-name/ze/zenoh-cpp/package.nix
@@ -2,30 +2,20 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch2,
   cmake,
   zenoh-c,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zenoh-cpp";
-  version = "1.9.0"; # nixpkgs-update: no auto update
+  version = "1.10.0"; # nixpkgs-update: no auto update
 
   src = fetchFromGitHub {
     owner = "eclipse-zenoh";
     repo = "zenoh-cpp";
     tag = finalAttrs.version;
-    hash = "sha256-MwQKTxrQqfoASCRk+vBeS9EHvmh6sqrpqygQVrdGkWw=";
+    hash = "sha256-EX3TSm0gAaRS2mj8o90zKsFvSqv2bgjrCnW4b1cC4JM=";
   };
-
-  patches = [
-    # ref. https://github.com/eclipse-zenoh/zenoh-cpp/pull/790 merged upstream
-    (fetchpatch2 {
-      name = "fix-cmake-exports-and-pc.patch";
-      url = "https://github.com/eclipse-zenoh/zenoh-cpp/commit/a55543277ce93fa3d0871b5b30fb18c04a283db2.patch?full_index=true";
-      hash = "sha256-oaCeLTrQ7veWzpTEKGo4pDmNLKmnBIjBkuX71vRtjoo=";
-    })
-  ];
 
   cmakeFlags = [
     (lib.cmakeBool "ZENOHCXX_ZENOHC" true)
