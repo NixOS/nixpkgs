27ad3a5c65a3e24759e95fdda2b01bcb807147a7
qubitnano <146656568+qubitnano@users.noreply.github.com>
2026-08-16 20:44:05 -0400

    mongodb-7_0: 7.0.39 -> 7.0.40
    https://www.mongodb.com/docs/manual/release-notes/7.0-changelog/#7040-changelog
    
    Fixes: CVE-2026-18688
    Fixes: CVE-2026-18690
    Fixes: CVE-2026-18691
    Fixes: CVE-2026-18693
    Fixes: CVE-2026-18694
    Fixes: CVE-2026-18695
    Fixes: CVE-2026-18696
    Fixes: CVE-2026-18697
    Fixes: CVE-2026-18698
    Fixes: CVE-2026-18699
    Fixes: CVE-2026-18700
    Fixes: CVE-2026-18701
    Fixes: CVE-2026-18702
    Fixes: CVE-2026-18705
    Fixes: CVE-2026-18708
    Fixes: CVE-2026-18709
    Fixes: CVE-2026-18712
    (cherry picked from commit d6e761a9557061da749b62192da8f077511857f6)

--- pkgs/servers/nosql/mongodb/7.0.nix
+++ pkgs/servers/nosql/mongodb/7.0.nix
@@ -21,8 +21,8 @@ let
 in
 buildMongoDB {
   inherit avxSupport;
-  version = "7.0.39";
-  hash = "sha256-VB20nhb0mMf9xVs/R4i+4wWoqHoDNWNouDw3FRehBP8=";
+  version = "7.0.40";
+  hash = "sha256-0KDqI/tMxklGkOvXcehmufiWvmNrf8Jj2llCH91gNlw=";
   patches = [
     # ModuleNotFoundError: No module named 'mongo_tooling_metrics':
     # NameError: name 'SConsToolingMetrics' is not defined:
