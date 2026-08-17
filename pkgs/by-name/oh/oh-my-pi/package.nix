37a1c17ad3846d90a985f25092888d1c22033513
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-08-17 19:21:30 +0200

    oh-my-pi: address code review
    Co-authored-by: Dominique Hummel <naxdy@naxdy.org>

--- pkgs/by-name/oh/oh-my-pi/package.nix
+++ pkgs/by-name/oh/oh-my-pi/package.nix
@@ -53,14 +53,12 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-LzBuO6T13J7oG4mvsOG2faBIkbN7b636iaLrUuLDDvA=";
   };
 
-  # Upstream requires Bun >= 1.3.14, but Nixpkgs currently provides Bun 1.3.13.
-  # Bypass the strict semver check to allow running under pkgs.bun until Nixpkgs updates Bun.
   postPatch = ''
     substituteInPlace packages/coding-agent/src/cli.ts \
       --replace-fail 'if (Bun.semver.order(Bun.version, MIN_BUN_VERSION) < 0)' 'if (false)'
   '';
 
-  # required until https://github.com/NixOS/nixpkgs/issues/255890 & https://github.com/NixOS/nixpkgs/issues/#335534 are fixed
+  # required until https://github.com/NixOS/nixpkgs/issues/255890 & https://github.com/NixOS/nixpkgs/issues/335534 are fixed
   passthru.node_modules = stdenv.mkDerivation {
     pname = "oh-my-pi-node_modules";
     inherit (finalAttrs) version src;
@@ -100,6 +98,7 @@ stdenv.mkDerivation (finalAttrs: {
       runHook postInstall
     '';
 
+    # Prevents breaking symlinks in node_modules
     dontFixup = true;
 
     outputHash = "sha256-MQzVxwhWIxvEd4X/KcB/oYILrSvnYB4wjjetgJxspSA=";
@@ -133,7 +132,6 @@ stdenv.mkDerivation (finalAttrs: {
   env = {
     RUSTC_BOOTSTRAP = "1";
   }
-  // lib.optionalAttrs stdenv.hostPlatform.isx86_64 { RUSTFLAGS = "-C target-cpu=x86-64-v2"; }
   // lib.optionalAttrs stdenv.hostPlatform.isDarwin { BUN_NO_CODESIGN_MACHO_BINARY = "1"; };
 
   configurePhase = ''
