2a27d4027eb2d926cb6d9ef6096b0123b48f9829
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-17 19:52:42 +0000

    palemoon-bin: 34.3.2 -> 34.3.2.1

--- pkgs/by-name/pa/palemoon-bin/package.nix
+++ pkgs/by-name/pa/palemoon-bin/package.nix
@@ -22,7 +22,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "palemoon-bin";
-  version = "34.3.2";
+  version = "34.3.2.1";
 
   src = finalAttrs.passthru.sources."gtk${if withGTK3 then "3" else "2"}";
 
@@ -171,11 +171,11 @@ stdenv.mkDerivation (finalAttrs: {
       {
         gtk3 = fetchzip {
           urls = urlRegionVariants "gtk3";
-          hash = "sha256-zTkIluGSnbpPJGkVGiXKYBGBrOVbbEnarmlW4zKC+cQ=";
+          hash = "sha256-IjD3En6qNMeUWe4sX5eb2Y76pMlWDv/NOuQr3uKluiE=";
         };
         gtk2 = fetchzip {
           urls = urlRegionVariants "gtk2";
-          hash = "sha256-xZDD9cMsgy8NZQnbCzQyCxOnU5S7BPW2NBDfyUmh3EQ=";
+          hash = "sha256-o0vsOw1yxWURckqoq2rb9Acl6IyYenunEuy7Trb8VmM=";
         };
       };
 
