1e31aed3b858024425604f618791cb8b4aba61d9
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-17 04:53:02 +0000

    sidplayfp: 3.1.0 -> 3.2.0

--- pkgs/by-name/si/sidplayfp/package.nix
+++ pkgs/by-name/si/sidplayfp/package.nix
@@ -37,13 +37,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "sidplayfp";
-  version = "3.1.0";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "libsidplayfp";
     repo = "sidplayfp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-wkZ/iJzz1QikNEaI00PFHaeewOrP+lYHF/iaws1aSro=";
+    hash = "sha256-+pl8tsKlmfZkFRvVNza57xUWYnZDMQ+c9Z9pPytzpFo=";
   };
 
   postPatch = ''
