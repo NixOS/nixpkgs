057a674efbb3b556be6b8b1fee593232684696ae
eljamm <fedi.jamoussi@protonmail.ch>
2026-08-17 12:27:52 +0200

    badkeys: fix tests

--- pkgs/by-name/ba/badkeys/package.nix
+++ pkgs/by-name/ba/badkeys/package.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   python3Packages,
   testers,
+  fetchpatch2,
 }:
 
 python3Packages.buildPythonApplication (finalAttrs: {
@@ -17,6 +18,20 @@ python3Packages.buildPythonApplication (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-i6wKe3djZL2tHjq3UrC+edU6Gz8EYSXSUry61BaI0v4=";
   };
+
+  patches = [
+    # TODO: remove on version > 0.0.19
+    (fetchpatch2 {
+      name = "Adapt-tests-for-rsainvalid-check-for-latest-Python-c.patch";
+      url = "https://github.com/badkeys/badkeys/commit/0bdcc68378bf389aebcc714b8c24de1c8a88a4e9.patch?full_index=1";
+      hash = "sha256-K0AsRNxfDQ/qC7abt0rr8tKNP9cfvWJ75DscdyoxF08=";
+    })
+    (fetchpatch2 {
+      name = "Add-test-for-Diffie-Hellman-DH-key.patch";
+      url = "https://github.com/badkeys/badkeys/commit/4e16c47dc79dd98865f4e3ec4b981306630a57a3.patch?full_index=1";
+      hash = "sha256-6F4lfUOmSkCF4XuiPwnJGD2RrxT5/jsillAyW9vzXMw=";
+    })
+  ];
 
   build-system = with python3Packages; [
     setuptools
