d39ffc7ab187d034e02aa0fc63947aa9a8266801
Conor <conorboyle07@protonmail.com>
2026-08-17 22:42:25 +0100

    mkdocs-simple-blog: modernise

--- pkgs/development/python-modules/mkdocs-simple-blog/default.nix
+++ pkgs/development/python-modules/mkdocs-simple-blog/default.nix
@@ -8,23 +8,24 @@
   pyprojectVersionPatchHook,
   pytest-cov-stub,
 }:
-buildPythonPackage rec {
+
+buildPythonPackage (finalAttrs: {
   pname = "mkdocs-simple-blog";
   version = "0.4.1";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "FernandoCelmer";
     repo = "mkdocs-simple-blog";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-lp0+mJYyP7Qz/gJCI7+tKh9fZArWs2u1ZusnVUax7A4=";
   };
 
   build-system = [ poetry-core ];
 
-  dependencies = [
-    mkdocs
-  ];
+  dependencies = [ mkdocs ];
 
   nativeBuildInputs = [ pyprojectVersionPatchHook ];
 
@@ -38,8 +39,8 @@ buildPythonPackage rec {
   meta = {
     description = "Simple blog generator plugin for MkDocs";
     homepage = "https://fernandocelmer.github.io/mkdocs-simple-blog/";
-    changelog = "https://github.com/FernandoCelmer/mkdocs-simple-blog/releases/tag/${src.tag}";
+    changelog = "https://github.com/FernandoCelmer/mkdocs-simple-blog/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ guelakais ];
   };
-}
+})
