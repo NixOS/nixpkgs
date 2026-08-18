bba7c2f268aae75cfeb86c75c539a308ed4ac368
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 06:16:10 +0000

    grafanaPlugins.grafana-metricsdrilldown-app: 2.4.0 -> 2.5.0

--- pkgs/servers/monitoring/grafana/plugins/grafana-metricsdrilldown-app/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-metricsdrilldown-app/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "grafana-metricsdrilldown-app";
-  version = "2.4.0";
-  zipHash = "sha256-OeDvm8tJSu/Tv80PKqqmc7932omgKXO50wTy04QPyUQ=";
+  version = "2.5.0";
+  zipHash = "sha256-AVWcT2SORlVYmHjTNX50S+3CKbtsuM1fRU290qfGYcs=";
   meta = {
     description = "Queryless experience for browsing Prometheus-compatible metrics. Quickly find related metrics without writing PromQL queries";
     license = lib.licenses.agpl3Only;
