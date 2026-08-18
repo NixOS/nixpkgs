7ee02e603370387e6fa968bd72ba495573ae2750
whoomee <git@miglix.eu>
2026-08-18 18:42:17 +0200

    flann: 1.9.1 -> 1.9.2

--- pkgs/by-name/fl/flann/package.nix
+++ pkgs/by-name/fl/flann/package.nix
@@ -1,73 +1,91 @@
 {
   lib,
+  testers,
   cmake,
   fetchFromGitHub,
   fetchpatch,
+  fetchurl,
   lz4,
   pkg-config,
   python3,
   stdenv,
   unzip,
+  llvmPackages,
+  gtest,
+  hdf5,
+
+  clangStdenv,
+
   enablePython ? false,
+
+  flann,
 }:
 
+let
+  hashes = {
+    cloud = "sha256-PaUn4yZ4fuidu+iCOFAFj1172YU9GjP94x/2cYuMJEU=";
+    sift10K = "sha256-nZK3YhIPDuIeHmH5bQ6IQRXwBT4du37vWaW3oBcXfxE=";
+    sift10K_byte = "sha256-X2JckKNyxAa1aLvk4lVZuSc8P2Loox9zfaNQEzWVhN4=";
+    sift100K = "sha256-qs9YIaZdv1JDNatz/YMYF6YTKcUUvkBdALlbev/zpbY=";
+    sift100K_byte = "sha256-CXKpppiR3V+7ZcV+Ef40fxP8sm5ciiGizr3BG4GDBII=";
+    brief100K = "sha256-bF81Kj86kH5wwducEEhMPoesHu2A3kshTCGjrgCqjUU=";
+  };
+  mkTestdata =
+    name:
+    fetchurl {
+      name = "${name}.h5";
+      url = "https://www.cs.ubc.ca/research/flann/uploads/FLANN/datasets/${name}.h5";
+      hash = hashes.${name};
+    };
+
+  testdata = map mkTestdata (lib.attrNames hashes);
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "flann";
-  version = "1.9.1";
+  version = "1.9.2";
 
   src = fetchFromGitHub {
     owner = "flann-lib";
     repo = "flann";
-    rev = finalAttrs.version;
-    sha256 = "13lg9nazj5s9a41j61vbijy04v6839i67lqd925xmxsbybf36gjc";
+    tag = finalAttrs.version;
+    hash = "sha256-5GCz28CbnPDQhEz6axFiQZMmOasd2Rph4a/bMQ53T2Q=";
   };
 
   patches = [
-    # Patch HDF5_INCLUDE_DIR -> HDF_INCLUDE_DIRS.
+    # Add "Requires:" to generated pkg-config file, see https://github.com/flann-lib/flann/pull/481
+    ./pkg-config-requires.patch
+
+    # Patch HDF5_INCLUDE_DIR -> HDF5_INCLUDE_DIRS.
     (fetchpatch {
       url = "https://salsa.debian.org/science-team/flann/-/raw/debian/1.9.1+dfsg-9/debian/patches/0001-Updated-fix-cmake-hdf5.patch";
       sha256 = "yM1ONU4mu6lctttM5YcSTg8F344TNUJXwjxXLqzr5Pk=";
     })
-    # Patch no-source library workaround that breaks on CMake > 3.11.
-    (fetchpatch {
-      url = "https://salsa.debian.org/science-team/flann/-/raw/debian/1.9.1+dfsg-9/debian/patches/0001-src-cpp-fix-cmake-3.11-build.patch";
-      sha256 = "REsBnbe6vlrZ+iCcw43kR5wy2o6q10RM73xjW5kBsr4=";
-    })
-  ]
-  ++ lib.optionals (!stdenv.cc.isClang) [
-    # Avoid the bundled version of LZ4 and instead use the system one.
-    (fetchpatch {
-      url = "https://salsa.debian.org/science-team/flann/-/raw/debian/1.9.1+dfsg-9/debian/patches/0003-Use-system-version-of-liblz4.patch";
-      sha256 = "xi+GyFn9PEjLgbJeAIEmsbp7ut9G9KIBkVulyT3nfsg=";
-    })
+
     # Fix LZ4 string separator issue, see: https://github.com/flann-lib/flann/pull/480
-    (fetchpatch {
-      url = "https://github.com/flann-lib/flann/commit/25eb56ec78472bd419a121c6905095a793cf8992.patch";
-      sha256 = "qt8h576Gn8uR7+T9u9bEBIRz6e6AoTKpa1JfdZVvW9s=";
-    })
-  ]
-  ++ lib.optionals stdenv.cc.isClang [
-    # Fix build with Clang 16.
-    (fetchpatch {
-      url = "https://github.com/flann-lib/flann/commit/be80cefa69b314a3d9e1ab971715e84145863ebb.patch";
-      hash = "sha256-4SUKzQCm0Sx8N43Z6ShuMbgbbe7q8b2Ibk3WgkB0qa4=";
-    })
+    ./pkg-config-lz4-expand-list.patch
   ];
 
   # The LZ4 patch updates cmake_minimum_required to 3.12, but only for non-clang builds.
   # For clang builds (like Darwin), we need to manually update it.
   # ref. https://github.com/flann-lib/flann/pull/526 not merged yet
-  postPatch = lib.optionalString stdenv.cc.isClang ''
-    substituteInPlace CMakeLists.txt \
-      --replace-fail "cmake_minimum_required(VERSION 2.6)" "cmake_minimum_required(VERSION 3.5)"
+  postPatch = lib.optionalString finalAttrs.finalPackage.doCheck ''
+    substituteInPlace test/CMakeLists.txt \
+      --replace-fail "add_dependencies(test flann_ruby_spec)" ""
+
+    ${lib.concatMapStringsSep "\n" (d: "cp -v ${toString d} test/${d.name}") testdata}
+    chmod +w test/*.h5
   '';
 
-  cmakeFlags = [
-    "-DBUILD_EXAMPLES:BOOL=OFF"
-    "-DBUILD_TESTS:BOOL=OFF"
-    "-DBUILD_MATLAB_BINDINGS:BOOL=OFF"
-    "-DBUILD_PYTHON_BINDINGS:BOOL=${if enablePython then "ON" else "OFF"}"
-  ];
+  cmakeFlags = lib.mapAttrsToList lib.cmakeBool {
+    BUILD_EXAMPLES = false;
+    BUILD_DOC = false;
+    BUILD_TESTS = finalAttrs.finalPackage.doCheck;
+    BUILD_MATLAB_BINDINGS = false;
+    BUILD_PYTHON_BINDINGS = enablePython;
+    BUILD_C_BINDINGS = true;
+    USE_OPENMP = true;
+    USE_MPI = false;
+  };
 
   nativeBuildInputs = [
     cmake
@@ -75,14 +93,43 @@ stdenv.mkDerivation (finalAttrs: {
     unzip
   ];
 
-  # lz4 unbundling broken for llvm, use internal version
-  propagatedBuildInputs = lib.optional (!stdenv.cc.isClang) lz4;
+  propagatedBuildInputs = [ lz4 ];
 
-  buildInputs = lib.optionals enablePython [ python3 ];
+  buildInputs =
+    lib.optionals enablePython [ python3 ] ++ lib.optional stdenv.cc.isClang llvmPackages.openmp;
+
+  checkInputs = [
+    gtest
+    hdf5
+  ];
+
+  doCheck = true;
+
+  # 'tests' target builds test, 'test' target runs them
+  postBuild = lib.optionalString finalAttrs.finalPackage.doCheck ''
+    make tests
+  '';
+
+  checkPhase = ''
+    runHook preCheck
+    make test
+    runHook postCheck
+  '';
+
+  passthru.tests = {
+    flann-clang = flann.override {
+      stdenv = clangStdenv;
+    };
+    flann-python = flann.override {
+      enablePython = true;
+    };
+    pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+  };
 
   meta = {
     homepage = "https://github.com/flann-lib/flann";
     license = lib.licenses.bsd3;
+    pkgConfigModules = [ "flann" ];
     description = "Fast approximate nearest neighbor searches in high dimensional spaces";
     maintainers = with lib.maintainers; [ tmarkus ];
     platforms = with lib.platforms; linux ++ darwin;
