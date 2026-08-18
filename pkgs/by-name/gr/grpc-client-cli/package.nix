93fd8c9cc472d07f20caac518437ee6c36da859e
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 18:33:28 +0000

    grpc-client-cli: 1.24.7 -> 1.24.8

--- pkgs/by-name/gr/grpc-client-cli/package.nix
+++ pkgs/by-name/gr/grpc-client-cli/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "grpc-client-cli";
-  version = "1.24.7";
+  version = "1.24.8";
 
   src = fetchFromGitHub {
     owner = "vadimi";
     repo = "grpc-client-cli";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-F7huQ5iVdhwfMvG5GEyg3s2S1I9SiFuBznJpYnxoMsY=";
+    sha256 = "sha256-7tkVEgxYd41uk2zUOVu/pjAOlrZsmRsZb/qCp3w3M4A=";
   };
 
-  vendorHash = "sha256-LjLV3WcmbUU3x1JPC4qennwH3ZQxD96CyhjeFo6H2XY=";
+  vendorHash = "sha256-bZI96h+hHSWBrexM41tPpcc1cPFbY2/s0+ZiO38sycw=";
 
   meta = {
     description = "Generic gRPC command line client";
