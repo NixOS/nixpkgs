39c38a1c005faf4b5d7040aacc5b2feace3dec07
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 23:04:05 +0000

    uutils-hostname: 0-unstable-2026-05-12 -> 0-unstable-2026-08-13

--- pkgs/by-name/uu/uutils-hostname/package.nix
+++ pkgs/by-name/uu/uutils-hostname/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "uutils-hostname";
-  version = "0-unstable-2026-05-12";
+  version = "0-unstable-2026-08-13";
 
   src = fetchFromGitHub {
     owner = "uutils";
     repo = "hostname";
-    rev = "e249b7e976b31116b90531672e7b3e414d599345";
-    hash = "sha256-SU/Tm8Q/+AKvf+O+tERGBlVMgifwByg6dQpZCcENYXo=";
+    rev = "b2225a12d117b08ab675c3c6c5691843623b7c96";
+    hash = "sha256-11rZ2ouja170U4XqnC35bIZQlCKiZx3mVs/z3scF6Fc=";
   };
 
-  cargoHash = "sha256-WlXYGvzbdcGb7qWB3L00oJpMZROmKq6zDrtSj6OwtYA=";
+  cargoHash = "sha256-2KrcR744d3UOc9uDOzjvn7ynm9h14mVtXvRmqjmRQh0=";
 
   cargoBuildFlags = [ "--package uu_hostname" ];
 
