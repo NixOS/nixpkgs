05519687ab2463f248540097055275a4b33cbf0a
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 09:13:41 +0000

    discordo: 0-unstable-2026-08-08 -> 0-unstable-2026-08-18

--- pkgs/by-name/di/discordo/package.nix
+++ pkgs/by-name/di/discordo/package.nix
@@ -11,16 +11,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "discordo";
-  version = "0-unstable-2026-08-08";
+  version = "0-unstable-2026-08-18";
 
   src = fetchFromGitHub {
     owner = "ayn2op";
     repo = "discordo";
-    rev = "be5aebdd14f09d0ddf4260557ff4fd38d0cb6bdb";
-    hash = "sha256-OEfR63EJTQ2nwyG2KDtRwFX4+zNKuD33ro6pEmi844c=";
+    rev = "26dc8cf7481db8fbf3da0e62fceea4aa36aaf1bf";
+    hash = "sha256-XIInSkAgSuKSpDsCyDwQD0KDtOnDZEWbbKVf10s+x7Q=";
   };
 
-  vendorHash = "sha256-B2L26bSAwOTuJk3cerhfYecEr/1AZNwqnoifJavFj24=";
+  vendorHash = "sha256-z9Hv3PqaR2XtDtsCqNR/zlZ5P2Olss83JeeC1UrTPBE=";
 
   env.CGO_ENABLED = 1;
 
