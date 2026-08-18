6c020e42a43258063260a689b33393bd961ba0b6
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 02:27:56 +0000

    process-compose: 1.120.0 -> 1.122.0

--- pkgs/by-name/pr/process-compose/package.nix
+++ pkgs/by-name/pr/process-compose/package.nix
@@ -10,13 +10,13 @@ let
 in
 buildGoModule (finalAttrs: {
   pname = "process-compose";
-  version = "1.120.0";
+  version = "1.122.0";
 
   src = fetchFromGitHub {
     owner = "F1bonacc1";
     repo = "process-compose";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-J1Sk6TfTVFgw+FU48CxYBxT5NVbR/pACpsnq2TRuf38=";
+    hash = "sha256-K1j4hJE23Wxf7P/ipMkMl/3uOtdE8Xj7LfyjvwC2rMI=";
 
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
@@ -46,7 +46,7 @@ buildGoModule (finalAttrs: {
     installShellFiles
   ];
 
-  vendorHash = "sha256-50N4IsSnUh3qmqqw+WACx2j0fMhLZKZdYK+7HAszxAY=";
+  vendorHash = "sha256-Wl3vMR122wn5ivQVbdS79nATQBlEHu44h0OZLGeEasU=";
 
   doCheck = false;
 
