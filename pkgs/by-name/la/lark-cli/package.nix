5efe3e038b8e00904bb5d31a79b90d42e23190fd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-08-18 17:36:45 +0200

    Revert "lark-cli: 1.0.58 -> 1.0.83"

--- pkgs/by-name/la/lark-cli/package.nix
+++ pkgs/by-name/la/lark-cli/package.nix
@@ -10,7 +10,7 @@
 
 buildGoModule (finalAttrs: {
   pname = "lark-cli";
-  version = "1.0.83";
+  version = "1.0.58";
 
   __structuredAttrs = true;
 
@@ -18,10 +18,10 @@ buildGoModule (finalAttrs: {
     owner = "larksuite";
     repo = "cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-TFZiYaZvSfvnczmaFaVPNYEAPW5eueUanPqlkrhubGs=";
+    hash = "sha256-MqaxcmzX/79vM2EI8wD4ZAFsUfqWvPAovlpmuDP1IWU=";
   };
 
-  vendorHash = "sha256-VoLp1fCDMi/90swzURF7An1WzFB2ywYyXObYwrN5B0o=";
+  vendorHash = "sha256-M0/Y62Y+M/P1B/YIDjX5bEyB/GKihCWQakTWVd7zvBg=";
 
   subPackages = [ "." ];
 
