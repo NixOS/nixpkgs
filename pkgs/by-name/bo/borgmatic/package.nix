f3322c1d4dee5c0d60e01f6196ab0f7cc40970c1
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 22:07:46 +0000

    borgmatic: 2.1.6 -> 2.1.7

--- pkgs/by-name/bo/borgmatic/package.nix
+++ pkgs/by-name/bo/borgmatic/package.nix
@@ -15,7 +15,7 @@
 }:
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "borgmatic";
-  version = "2.1.6";
+  version = "2.1.7";
   pyproject = true;
 
   strictDeps = true;
@@ -23,7 +23,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-Mgx8PnGfTa5j6+53RVntPHa5EDJAY2NQC3fvmyRj24Y=";
+    hash = "sha256-qsB8564wCWBemsxn7vIBmntdabzK1j7agP8ZK2nzdc8=";
   };
 
   passthru.updateScript = nix-update-script { };
