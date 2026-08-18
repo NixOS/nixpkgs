fdab920a4c113474606b17f4cc42673979b4c34a
Letgamer <alexstephan005@protonmail.com>
2026-08-18 11:06:00 +0200

    spotify: improved library handling

--- pkgs/by-name/sp/spotify/linux.nix
+++ pkgs/by-name/sp/spotify/linux.nix
@@ -112,6 +112,8 @@ let
     libxscrnsaver
     libxshmfence
     libxtst
+    nspr
+    openssl
     zlib
   ];
 in
@@ -160,14 +162,6 @@ stdenv.mkDerivation (finalAttrs: {
     runHook preUnpack
     unsquashfs "$src" '/usr/share/spotify' '/usr/bin/spotify' '/meta/snap.yaml'
     cd squashfs-root
-    if ! grep -q 'grade: stable' meta/snap.yaml; then
-      # Unfortunately this check is not reliable: At the moment (2018-07-26) the
-      # latest version in the "edge" channel is also marked as stable.
-      echo "The snap package is marked as unstable:"
-      grep 'grade: ' meta/snap.yaml
-      echo "You probably chose the wrong revision."
-      exit 1
-    fi
     if ! grep -q '${finalAttrs.version}' meta/snap.yaml; then
       echo "Package version differs from version found in snap metadata:"
       grep 'version: ' meta/snap.yaml
@@ -181,41 +175,22 @@ stdenv.mkDerivation (finalAttrs: {
   # Prevent double wrapping
   dontWrapGApps = true;
 
-  env = rec {
-    libdir = "${placeholder "out"}/lib/spotify";
-    librarypath = "${lib.makeLibraryPath deps}:${libdir}";
-  };
-
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $libdir
+    mkdir $out
     mv ./usr/* $out/
-
-    # Work around Spotify referring to a specific minor version of
-    # OpenSSL.
-
-    ln -s ${lib.getLib openssl}/lib/libssl.so $libdir/libssl.so.1.0.0
-    ln -s ${lib.getLib openssl}/lib/libcrypto.so $libdir/libcrypto.so.1.0.0
-    ln -s ${nspr.out}/lib/libnspr4.so $libdir/libnspr4.so
-    ln -s ${nspr.out}/lib/libplc4.so $libdir/libplc4.so
-
-    ln -s ${ffmpeg_7-headless.lib}/lib/libavcodec.so* $libdir
-    ln -s ${ffmpeg_7-headless.lib}/lib/libavformat.so* $libdir
-
-    rpath="$out/share/spotify:$libdir"
 
     chmod +w "$out/share/spotify/spotify"
     patchelf \
       --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath $rpath $out/share/spotify/spotify
+      $out/share/spotify/spotify
 
     # fix Icon line in the desktop file (#48062)
     sed -i "s:^Icon=.*:Icon=spotify-client:" "$out/share/spotify/spotify.desktop"
 
     # Desktop file
-    mkdir -p "$out/share/applications/"
-    cp "$out/share/spotify/spotify.desktop" "$out/share/applications/"
+    install -Dm644 "$out/share/spotify/spotify.desktop" "$out/share/applications/spotify.desktop"
 
     # Icons
     for i in 16 22 24 32 48 64 128 256 512; do
@@ -238,7 +213,7 @@ stdenv.mkDerivation (finalAttrs: {
           --add-flags "--force-device-scale-factor=${toString deviceScaleFactor}" \
         ''
       } \
-      --prefix LD_LIBRARY_PATH : "$librarypath" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath deps}" \
       --prefix PATH : "${zenity}/bin" \
       --run 'if [[ "''${NIXOS_OZONE_WL:-default}" == "1" ]]; then unset DISPLAY; fi'
 
