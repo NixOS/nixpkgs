166c4080991ffad93a9c3ce9d9d86a0e3f3f8efd
phanirithvij <phanirithvij2000@gmail.com>
2026-08-18 12:21:09 +0530

    python3Packages.{cryptodatahub,cryptolyzer,cryptoparser}: 1.2.1 -> 1.5.0
    Changelog:
    - https://gitlab.com/coroner/cryptodatahub/-/blob/v1.5.0/CHANGELOG.rst
    - https://gitlab.com/coroner/cryptolyzer/-/blob/v1.5.0/CHANGELOG.rst
    - https://gitlab.com/coroner/cryptoparser/-/blob/v1.5.0/CHANGELOG.rst
    
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>
    (cherry picked from commit 005d36e6208384e8028a47b10a2d2b9e9d8b3fb5)

--- pkgs/development/python-modules/cryptoparser/default.nix
+++ pkgs/development/python-modules/cryptoparser/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "cryptoparser";
-  version = "1.2.1";
+  version = "1.5.0";
   pyproject = true;
 
   src = fetchFromGitLab {
     owner = "coroner";
     repo = "cryptoparser";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-thhpXfLH5yB3pMUKFrMUJ8+8IGchF813ApKUrN+UuZA=";
+    hash = "sha256-HlsjenwXFDOA1uK+sn1oDHWxbzxIDriWS6pcZycEsis=";
   };
 
   patches = [
