adcf77f72ed93e03c306f3ec439f4bee8f073d59
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 17:46:28 +0000

    vscode-extensions.ms-vscode.cpptools: 1.32.2 -> 1.33.8

--- pkgs/applications/editors/vscode/extensions/ms-vscode.cpptools/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-vscode.cpptools/default.nix
@@ -42,15 +42,15 @@ let
   isDarwin = stdenv.hostPlatform.isDarwin;
   supported = {
     x86_64-linux = {
-      hash = "sha256-KTGDbX/T1BhoYxEjqPmPTAkyNjGDEV1Ao4nMIhHpGys=";
+      hash = "sha256-Ok5jk6XUqrSh6vJJyIydA6aYEw3GhsRHqjjEsmzr7/0=";
       arch = "linux-x64";
     };
     aarch64-linux = {
-      hash = "sha256-4gl6sVgWAkV2XJFOipVbBUDGG1ncm41TjSoFnRW78m0=";
+      hash = "sha256-/9TFwQP7g1PRUY93tohOqnTvLC6NS9xulBYxwqrIgfY=";
       arch = "linux-arm64";
     };
     aarch64-darwin = {
-      hash = "sha256-EMkq7Y0Eo629GzW6pDL1UYijyvLU6sp7YxPaKm0va6s=";
+      hash = "sha256-fsHmPWUnDDr4fcRMBPepXnxIhqXDt9ZsBjtUL1VD71E=";
       arch = "darwin-arm64";
     };
   };
@@ -63,7 +63,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = base // {
     name = "cpptools";
     publisher = "ms-vscode";
-    version = "1.32.2";
+    version = "1.33.8";
   };
 
   nativeBuildInputs = [
