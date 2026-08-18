9ce4e476ccc244d0192956f3e41e7f1b5b7634e1
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-18 17:08:09 +0200

    python3Packages.webargs: add pkg-resources-backport

--- pkgs/development/python-modules/webargs/default.nix
+++ pkgs/development/python-modules/webargs/default.nix
@@ -1,20 +1,22 @@
 {
-  buildPythonPackage,
-  fetchPypi,
   lib,
+  aiohttp,
+  bottle,
+  buildPythonPackage,
+  django,
+  falcon,
+  fetchPypi,
+  flask,
   flit-core,
   marshmallow,
-  pytestCheckHook,
-  pytest-aiohttp,
-  webtest,
-  webtest-aiohttp,
-  flask,
-  django,
-  bottle,
-  tornado,
+  packaging,
+  pkg-resources-backport,
   pyramid,
-  falcon,
-  aiohttp,
+  pytest-aiohttp,
+  pytestCheckHook,
+  tornado,
+  webtest-aiohttp,
+  webtest,
 }:
 
 buildPythonPackage rec {
@@ -29,23 +31,32 @@ buildPythonPackage rec {
 
   build-system = [ flit-core ];
 
-  dependencies = [ marshmallow ];
+  dependencies = [
+    marshmallow
+    packaging
+    pkg-resources-backport
+  ];
 
   nativeCheckInputs = [
-    pytestCheckHook
+    aiohttp
+    bottle
+    django
+    falcon
+    flask
+    pyramid
     pytest-aiohttp
+    pytestCheckHook
+    tornado
     webtest
     webtest-aiohttp
-    flask
-    django
-    bottle
-    tornado
-    pyramid
-    falcon
-    aiohttp
   ];
 
   pythonImportsCheck = [ "webargs" ];
+
+  disabledTests = [
+    # Tests is outdated
+    "test_it_should_handle_type_error_on_load_json"
+  ];
 
   meta = {
     description = "Declarative parsing and validation of HTTP request objects, with built-in support for popular web frameworks";
