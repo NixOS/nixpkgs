b85736a0a1ef847c4aff056828425a2972b985a1
Alex Galvin <alex.v.galvin@gmail.com>
2026-08-18 02:03:29 -0400

    bcachefs-tools: 1.39.1 -> 1.39.2
    (cherry picked from commit f66aa0de15ccb5063d29038d6624e7db888e0622)

--- pkgs/by-name/bc/bcachefs-tools/package.nix
+++ pkgs/by-name/bc/bcachefs-tools/package.nix
@@ -31,18 +31,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bcachefs-tools";
-  version = "1.39.1";
+  version = "1.39.2";
 
   src = fetchFromGitHub {
     owner = "koverstreet";
     repo = "bcachefs-tools";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-KJBzVbK5DL+ZK27Oyyn8vCWRQUHrIAelrex1oX+fWq4=";
+    hash = "sha256-1ilzebplgFVaqiSzpTeGRgnSqYfo9W8G7xj/6KlX990=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) src;
-    hash = "sha256-Yb2DaFLuhAkwYED+s9SRKsjxluWoES4RSvKPKfd/kyE=";
+    hash = "sha256-g3r99wa4S0ZVUvTSYo4PTzOswjsOn83hkocoziHcn2A=";
   };
 
   postPatch = ''
