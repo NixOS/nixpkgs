70d27ada0077c8c4835740efa91cc5db9c894d39
ash <x-hv2j5m@keysmash.solutions>
2026-08-18 21:50:53 +0100

    capacities: 1.65.13 -> 1.68.2

--- pkgs/by-name/ca/capacities/package.nix
+++ pkgs/by-name/ca/capacities/package.nix
@@ -33,23 +33,23 @@ appimageTools.wrapType2 {
       --add-flags "--ozone-platform-hint=auto"
 
     # Check for required desktop file
-    if [ ! -f ${appimageContents}/capacities.desktop ]; then
+    if [ ! -f ${appimageContents}/io.capacities.app.desktop ]; then
       echo "Error: Missing .desktop file in ${appimageContents}"
       exit 1
     else
       # Install and modify the desktop file
-      install -m 444 -D ${appimageContents}/capacities.desktop $out/share/applications/capacities.desktop
-      substituteInPlace $out/share/applications/capacities.desktop \
+      install -m 444 -D ${appimageContents}/io.capacities.app.desktop $out/share/applications/io.capacities.app.desktop
+      substituteInPlace $out/share/applications/io.capacities.app.desktop \
         --replace-fail "Exec=AppRun" "Exec=capacities"
     fi
 
     # Check for required icon file
-    if [ ! -f ${appimageContents}/capacities.png ]; then
+    if [ ! -f ${appimageContents}/capacities.svg ]; then
       echo "Error: Missing icon file in ${appimageContents}"
       exit 1
     else
       # Resize and install the icon
-      ${lib.getExe imagemagick} ${appimageContents}/capacities.png -resize 512x512 capacities_512.png
+      ${lib.getExe imagemagick} ${appimageContents}/capacities.svg -resize 512x512 capacities_512.png
       install -m 444 -D capacities_512.png $out/share/icons/hicolor/512x512/apps/capacities.png
     fi
   '';
