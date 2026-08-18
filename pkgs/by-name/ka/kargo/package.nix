49169b0ccab00414508ea3a21dc754ea382d5d76
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-18 10:56:13 +0000

    kargo: 1.11.0 -> 1.11.2

--- pkgs/by-name/ka/kargo/package.nix
+++ pkgs/by-name/ka/kargo/package.nix
@@ -11,16 +11,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "kargo";
-  version = "1.11.0";
+  version = "1.11.2";
 
   src = fetchFromGitHub {
     owner = "akuity";
     repo = "kargo";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-yeHLLgRfKb6DYCxKRh0tNupT7ifTsdCt+qRCde+U91s=";
+    hash = "sha256-9t1cuQOxBUpR8fx+S0OdoOaz3Psta5hQPipDSRF1VWY=";
   };
 
-  vendorHash = "sha256-2kInoWXC01BbvISg6F02KC2xUWUcc4SGlm41imW45DU=";
+  vendorHash = "sha256-b7OWQ7M+1FRWruyFTyKKgHTVOMzgu+qQ+CRpQ5w/2z0=";
 
   subPackages = [ "cmd/cli" ];
 
