7f11ab374e435b6d3301a3ff86da2b2439f82171
aquanjsw <zhdlcc@gmail.com>
2026-08-18 15:11:13 +0800

    python3Packages.litellm: 1.89.0 -> 1.97.0
    Changelog: https://github.com/BerriAI/litellm/releases/tag/v1.97.0

--- pkgs/development/python-modules/litellm/default.nix
+++ pkgs/development/python-modules/litellm/default.nix
@@ -24,9 +24,11 @@
   gunicorn,
   httpx,
   importlib-metadata,
+  inquirerpy,
   jinja2,
   jsonschema,
   langfuse,
+  maturin,
   mcp,
   openai,
   opentelemetry-api,
@@ -37,6 +39,7 @@
   prisma,
   prometheus-client,
   pydantic,
+  pydantic-settings,
   pyjwt,
   pynacl,
   pypdf,
@@ -47,11 +50,11 @@
   restrictedpython,
   rich,
   rq,
+  rustPlatform,
   sentry-sdk,
   soundfile,
   tiktoken,
   tokenizers,
-  uv-build,
   uvicorn,
   uvloop,
   websockets,
@@ -61,22 +64,37 @@
 
 buildPythonPackage rec {
   pname = "litellm";
-  version = "1.89.0";
+  version = "1.97.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "BerriAI";
     repo = "litellm";
     tag = "v${version}";
-    hash = "sha256-tPw4cDqCQgyC8EoB5EPfui2gT+frjlSMOv95ntUXTWk=";
+    hash = "sha256-9nVVXRvtfxntAgSetCy66jfDpunR12DEIoQKAjSZn/4=";
+  };
+
+  nativeBuildInputs = with rustPlatform; [
+    cargoSetupHook
+    maturinBuildHook
+  ];
+
+  cargoRoot = "litellm-rust";
+
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit
+      pname
+      version
+      src
+      cargoRoot
+      ;
+    hash = "sha256-iwgIclG8BGeHDNtm686w2Rxe+9ddvBrz1sMfOBeuKK0=";
   };
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace-fail "uv_build==0.11.8" "uv_build"
+      --replace-fail "maturin==1.9.4" "maturin==${maturin.version}"
   '';
-
-  build-system = [ uv-build ];
 
   dependencies = [
     aiohttp
@@ -88,6 +106,7 @@ buildPythonPackage rec {
     jsonschema
     openai
     pydantic
+    pydantic-settings
     python-dotenv
     tiktoken
     tokenizers
@@ -104,6 +123,7 @@ buildPythonPackage rec {
       fastapi
       fastapi-sso
       gunicorn
+      inquirerpy
       # FIXME package litellm-enterprise
       # FIXME package litellm-proxy-extras
       mcp
