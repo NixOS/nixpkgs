5fc7f287269de68124402f9f910a29beab063b99
LucasOe <github@lucasoe.com>
2026-08-19 00:06:59 +0200

    maintainers: add lucasoe

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16782,6 +16782,12 @@
     github = "LucasFA";
     matrix = "@lucasfa:matrix.org";
   };
+  lucasoe = {
+    email = "github@lucasoe.com";
+    github = "LucasOe";
+    githubId = 16548335;
+    name = "Lucas Oelker";
+  };
   lucastso10 = {
     email = "lucastso10@gmail.com";
     github = "lucastso10";
