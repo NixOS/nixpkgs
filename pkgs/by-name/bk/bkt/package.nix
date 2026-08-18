2126027357b47c6d8ed667103e48752ce12180dd
KangaZero <samuelyongw@gmail.com>
2026-08-18 21:22:44 +0900

    bkt: 0.8.0 -> 0.8.2
    Added `versionCheckHook`, `nix-update-script` and `changelog`
    Replaced `sha-256` attr with `hash`
    Added `checkFlags` to skip test `cache_dirs_multi_user` that requires
    the use of `sudo` which nix sandbox does not have

--- pkgs/by-name/bk/bkt/package.nix
+++ pkgs/by-name/bk/bkt/package.nix
@@ -2,24 +2,37 @@
   rustPlatform,
   fetchFromGitHub,
   lib,
+  versionCheckHook,
+  nix-update-script,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
 
   pname = "bkt";
-  version = "0.8.0";
+  version = "0.8.2";
 
   src = fetchFromGitHub {
     owner = "dimo414";
     repo = "bkt";
     tag = finalAttrs.version;
-    sha256 = "sha256-XQK7oZfutqCvFoGzMH5G5zoGvqB8YaXSdrwjS/SVTNU=";
+    hash = "sha256-qb7uRvCAXCayDIg8yQfF/Yxe0pNvR3giCQYmMIur2rM=";
   };
 
-  cargoHash = "sha256-4CY2A6mPTfGhqUh+nNg6eaTIVwA9ZtgH5jHQDGHnK4c=";
+  cargoHash = "sha256-locf3k0jIT9RNQS9yCUtOpj4oKo5pOBU3CEYAJDbaPU=";
+
+  checkFlags = [
+    # Requires the use of "sudo" cmd which nix sandbox does not have
+    "--skip=cli::cache_dirs_multi_user"
+  ];
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Subprocess caching utility";
     homepage = "https://github.com/dimo414/bkt";
+    changelog = "https://github.com/dimo414/bkt/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.mangoiv ];
     mainProgram = "bkt";
