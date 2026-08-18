91336d53d89287a63e3604b26545187e638f4c8b
Gliczy <129636582+Gliczy@users.noreply.github.com>
2026-08-18 14:54:37 +0200

    komikku: 50.11.0 -> 51.0.0

--- pkgs/by-name/ko/komikku/package.nix
+++ pkgs/by-name/ko/komikku/package.nix
@@ -8,6 +8,7 @@
   gobject-introspection,
   blueprint-compiler,
   gtk4,
+  gtksourceview5,
   libadwaita,
   libglycin,
   webkitgtk_6_0,
@@ -24,14 +25,14 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "komikku";
-  version = "50.11.0";
+  version = "51.0.0";
   pyproject = false;
 
   src = fetchFromCodeberg {
     owner = "valos";
     repo = "Komikku";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-WO0wqDZP6rIe7B2oboJctQ302Ot2ooecqCSWOERdURg=";
+    hash = "sha256-5YEvYdkktoIEWoyTLop4tNVxG69/qPneOO0i0nW/Tfg=";
   };
 
   nativeBuildInputs = [
@@ -50,6 +51,7 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
     glib
     glib-networking
     gtk4
+    gtksourceview5
     libadwaita
     libglycin
     webkitgtk_6_0
@@ -72,6 +74,7 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
     pygobject3
     pyjwt
     pypdf
+    pytesseract
     python-magic
     rarfile
     requests
