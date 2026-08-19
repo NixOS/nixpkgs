7fb09c1338f36062d8ef0633475d090c2a57841f
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-19 13:39:24 +0100

    minesweeper-tui: init at 0.1.0

---
+++ pkgs/by-name/mi/minesweeper-tui/package.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  ncurses,
+  pkg-config,
+}:
+
+stdenv.mkDerivation {
+  pname = "minesweeper-tui";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "ZhaolunYin";
+    repo = "minesweeper-tui";
+    rev = "a6075d3f54ab0e81c94b71e9b7f92b1030603668";
+    hash = "sha256-1cY1d3q6+/llsrLRN9UGodvNxXwbUDKuD2a5fxSCy20=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    ncurses
+  ];
+
+  installPhase = ''
+    install -Dm755 minesweeper $out/bin/minesweeper
+  '';
+
+  meta = {
+    description = "A TUI Minesweeper game written in C";
+    homepage = "https://github.com/ZhaolunYin/minesweeper-tui";
+    license = lib.licenses.mit;
+    mainProgram = "minesweeper";
+    platforms = lib.platforms.linux;
+  };
+}
