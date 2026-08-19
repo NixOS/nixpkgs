015efd8390adfe73183f79ae76e8b161b8f8b00b
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-19 22:54:22 +0000

    python3Packages.robot-descriptions: 3.0.0 -> 3.1.0

--- pkgs/development/python-modules/robot-descriptions/default.nix
+++ pkgs/development/python-modules/robot-descriptions/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "robot-descriptions";
-  version = "3.0.0";
+  version = "3.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "robot-descriptions";
     repo = "robot_descriptions.py";
     tag = "v${version}";
-    hash = "sha256-THLGQWZVeXwbanBIyYw8ymM+c/x5rXJxD7YWgn8GCWw=";
+    hash = "sha256-tbyMcoZXnzWGZ9tO/2hBXNimJQa1UTxPLpyrI1yalWI=";
   };
 
   build-system = [
