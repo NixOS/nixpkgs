775afc1dbc041bd3a4de321fa7988e7fb91dc207
phanirithvij <phanirithvij2000@gmail.com>
2026-08-19 20:59:11 +0530

    git-pages.services.default: init
    Co-authored-by: Tom Oostveen <tomoostveen.06@gmail.com>
    Co-authored-by: Bart Oostveen <bart@bartoostveen.nl>
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -697,6 +697,7 @@ in
   geth = runTest ./geth.nix;
   ghostunnel = runTest ./ghostunnel.nix;
   ghostunnel-modular = runTest ./ghostunnel-modular.nix;
+  git-pages-modular = runTest ./git-pages.nix;
   gitdaemon = runTest ./gitdaemon.nix;
   gitea = import ./gitea.nix {
     inherit pkgs runTest;
