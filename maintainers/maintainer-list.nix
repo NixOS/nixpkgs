c282f7553144589391c178b55a8f2b0087e98bca
Ruslan Shevchuk <ShevRuslan1>
2026-08-19 15:52:46 -0400

    maintainers: add ShevRuslan1

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3,12 +3,12 @@
    ```nix
    handle = {
      # Required
-     name = "Your name";
-     github = "GithubUsername";
-     githubId = your-github-id;
+     name = "Ruslan Shevchuk";
+     github = "ShevRuslan1";
+     githubId = 250438229;
 
      # Optional
-     email = "address@example.org";
+     email = "rshevchuk2007@gmail.com";
      matrix = "@user:example.org";
      keys = [ { fingerprint = "AAAA BBBB CCCC DDDD EEEE  FFFF 0000 1111 2222 3333"; } ];
    };
