f091fb0ae73d2b436f9863c49b17653b78315149
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-19 19:11:00 +0200

    python3Packages.cyclopts: 4.22.5 -> 4.23.0
    Diff: https://github.com/BrianPugh/cyclopts/compare/v4.22.5...v4.23.0
    
    Changelog: https://github.com/BrianPugh/cyclopts/releases/tag/v4.23.0

--- pkgs/development/python-modules/cyclopts/default.nix
+++ pkgs/development/python-modules/cyclopts/default.nix
@@ -28,14 +28,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "cyclopts";
-  version = "4.22.5";
+  version = "4.23.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "BrianPugh";
     repo = "cyclopts";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Jjhlo8QWettZMvgHzY/QP6Q74IBSqdCqZWK6lYlYUng=";
+    hash = "sha256-XNrskQvz/W0kqzKCq4pHaIdMaYjm0JwAWeXUKh6ib84=";
   };
 
   pythonRelaxDeps = [ "rich-rst" ];
