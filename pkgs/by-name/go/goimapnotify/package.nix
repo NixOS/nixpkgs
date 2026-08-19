a95a87879eb28ab7a85dd6e3e996ecc214ae2537
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-19 00:45:02 +0000

    goimapnotify: 2.5.7 -> 2.5.8

--- pkgs/by-name/go/goimapnotify/package.nix
+++ pkgs/by-name/go/goimapnotify/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "goimapnotify";
-  version = "2.5.7";
+  version = "2.5.8";
 
   src = fetchFromGitLab {
     owner = "shackra";
     repo = "goimapnotify";
     tag = finalAttrs.version;
-    hash = "sha256-c84vjn9oYb6EdKG3gLrW6kF+IESgdVyEAKE4JulEQ8k=";
+    hash = "sha256-3UXpZOTVSDrxkRCFkeUoJApYoy4g2tg19BxIPZ5KT0Y=";
   };
 
-  vendorHash = "sha256-3yavkH0b4ZLLt1a7MhdeHSNVAAOYKiKC+D9zfEv9bSA=";
+  vendorHash = null;
 
   postPatch = ''
     for f in internal/util/command.go internal/util/command_test.go; do
