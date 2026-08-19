8200e362ff5d9894e06c230bd51d8fd11b694c9d
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-19 07:42:58 +0000

    keycloak: 26.7.1 -> 26.7.2

--- pkgs/by-name/ke/keycloak/package.nix
+++ pkgs/by-name/ke/keycloak/package.nix
@@ -24,11 +24,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "keycloak";
-  version = "26.7.1";
+  version = "26.7.2";
 
   src = fetchzip {
     url = "https://github.com/keycloak/keycloak/releases/download/${finalAttrs.version}/keycloak-${finalAttrs.version}.zip";
-    hash = "sha256-YkjFJcD2G132mUm4HmyHriAK+b+3UCUeLilEMeKBZho=";
+    hash = "sha256-D4Hj4OHX8veFjIDbvbQN0E7C2oHVpbD2U4TV1Z8fZ8Y=";
   };
 
   nativeBuildInputs = [
