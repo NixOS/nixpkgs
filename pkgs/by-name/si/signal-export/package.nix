590d975bb1499b09c2d7c952dbb00588b20d21e6
isolatedwebappdev <231138628+isolatedwebappdev@users.noreply.github.com>
2026-08-18 19:01:35 -0500

    signal-export: 3.8.3 -> 3.9.1

--- pkgs/by-name/si/signal-export/package.nix
+++ pkgs/by-name/si/signal-export/package.nix
@@ -7,13 +7,13 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "signal-export";
-  version = "3.8.3";
+  version = "3.9.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) version;
     pname = "signal_export";
-    hash = "sha256-V6yo1nimjQJgbf17A/RSe/vykfCxcFFL0xZaQY3k0Tk=";
+    hash = "sha256-wzaVP1wnInpH6N07EFxspoR1kVowChniUHKDH75ZnLc=";
   };
 
   build-system = with python3.pkgs; [
