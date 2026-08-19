3d9d81ca464ec27d3af2766ef259da8677192090
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-19 14:38:55 +0000

    vscode-extensions.google.colab: 0.8.1 -> 0.9.0

--- pkgs/applications/editors/vscode/extensions/google.colab/default.nix
+++ pkgs/applications/editors/vscode/extensions/google.colab/default.nix
@@ -7,8 +7,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     publisher = "google";
     name = "colab";
-    version = "0.8.1";
-    hash = "sha256-QKGE6heSrSCm7X0YSOQy6SQeD50AZPS28UYWSWYdJxs=";
+    version = "0.9.0";
+    hash = "sha256-e2w9aZD6GedPXPcTc8j5itYHuL2A4zG+gwhFH+43iDw=";
   };
 
   meta = {
