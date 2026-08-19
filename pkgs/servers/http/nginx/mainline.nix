4be16741e954f52f3809343fb5b2f3bb37449d1b
Leona Maroni <dev@leona.is>
2026-08-19 17:40:00 +0200

    nginxMainline: 1.31.3 -> 1.31.4
    https://nginx.org/en/CHANGES
    https://github.com/nginx/nginx/releases/tag/release-1.31.4

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix args {
-  version = "1.31.3";
-  hash = "sha256-p2V8UIEcLZLZiVOV6Lhz72A5gULE2yHrZHgRw49t1SU=";
+  version = "1.31.4";
+  hash = "sha256-5vILZEoXpkPwWa5kZ6GXH+KBFYfQJeBxBodTofHjs8M=";
 }
