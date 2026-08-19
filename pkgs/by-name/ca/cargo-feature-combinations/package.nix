2a1e7670015c436426ad6c8683aa975d1528f011
KangaZero <samuelyongw@gmail.com>
2026-08-19 11:57:29 +0900

    cargo-feature-combinations: 0.4.3 -> 0.5.0

--- pkgs/by-name/ca/cargo-feature-combinations/package.nix
+++ pkgs/by-name/ca/cargo-feature-combinations/package.nix
@@ -9,16 +9,16 @@ rustPlatform.buildRustPackage (finalAttrs: {
   __structuredAttrs = true;
 
   pname = "cargo-feature-combinations";
-  version = "0.4.3";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "romnn";
     repo = "cargo-feature-combinations";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-UvKEXWBT2W4wsjlKUTeyF2A8NIqLnfpjQRKJpCqszp8=";
+    hash = "sha256-0rAo6zH/SRe2cV2H2koYJ0Pe0Y8pqVFIhCl3CzS8wnA=";
   };
 
-  cargoHash = "sha256-wDd7910GjQwSPk44HwF9LhqNlgUwAEC9D2J0essIT1o=";
+  cargoHash = "sha256-Ihy1qRrZPmBIi9yDHS+wNxx2CATxtaIbxSuRz94RGUE=";
 
   passthru.updateScript = nix-update-script { };
 
