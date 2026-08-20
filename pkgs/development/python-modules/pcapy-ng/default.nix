5487f7fdc5c07498f44ea259b974a3bda359e398
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-20 15:02:49 +0200

    python3Pakcages.pcapy-ng: modernize

--- pkgs/development/python-modules/pcapy-ng/default.nix
+++ pkgs/development/python-modules/pcapy-ng/default.nix
@@ -6,19 +6,22 @@
   libpcap,
   pkgconfig,
   pytestCheckHook,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pcapy-ng";
   version = "2.1.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "stamparm";
     repo = "pcapy-ng";
-    rev = version;
+    tag = finalAttrs.version;
     hash = "sha256-7Bm+cEK2cAvsRO1v9m3iwdt0kx0bz0YKSpCd4p3JsYk=";
   };
+
+  build-system = [ setuptools ];
 
   nativeBuildInputs = [
     cython
@@ -42,7 +45,8 @@ buildPythonPackage rec {
   meta = {
     description = "Module to interface with the libpcap packet capture library";
     homepage = "https://github.com/stamparm/pcapy-ng/";
+    changelog = "https://github.com/stamparm/pcapy-ng/releases/tag/finalAttrs.src.tag";
     license = lib.licenses.bsd2;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
