fd6547ea46f58ca7a379e194ae805ac81424a0ef
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-08-20 11:18:25 -0400

    par2cmdline: modernize
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/by-name/pa/par2cmdline/package.nix
+++ pkgs/by-name/pa/par2cmdline/package.nix
@@ -3,20 +3,27 @@
   stdenv,
   fetchFromGitHub,
   autoreconfHook,
+  versionCheckHook,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "par2cmdline";
   version = "1.3.0";
 
+  strictDeps = true;
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "Parchive";
     repo = "par2cmdline";
-    rev = "v${finalAttrs.version}";
-    sha256 = "sha256-TEWfkjyjqG5cRsVkckIoIo/+/LwhwH1GVivX6Dpvpxw=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-TEWfkjyjqG5cRsVkckIoIo/+/LwhwH1GVivX6Dpvpxw=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
 
   meta = {
     homepage = "https://github.com/Parchive/par2cmdline";
@@ -26,6 +33,7 @@ stdenv.mkDerivation (finalAttrs: {
       damage in data files and repair them if necessary. It can be used with
       any kind of file.
     '';
+    mainProgram = "par2";
     license = lib.licenses.gpl2Plus;
     maintainers = with lib.maintainers; [ tallesCoelho ];
     platforms = lib.platforms.all;
