836949408f2c19cb7c822a7fb0d4792007ee0b44
Philippe Loctaux <p@philippeloctaux.com>
2026-08-20 22:12:15 +0200

    opencloud-production: init at 7.2.3
    refactor opencloud package into generic packages, like it was done for forgejo

---
+++ pkgs/by-name/op/opencloud/production.nix
@@ -0,0 +1,9 @@
+import ./generic.nix {
+  version = "7.2.3";
+  production = true;
+  hash = "sha256-5wmXBLO6wmlBRu0FWDGMzFipRSARyK2LED1BR0Vwnt0=";
+  webVersion = "7.1.4";
+  webHash = "sha256-Wg4VGweWBiTRGDRIbe4spFQB1MEC25zXZBAIa4+YttY=";
+  webPnpmDepsHash = "sha256-YEdZ5B11I6U140qam7e1TMOacRqUeINhr/TI13ddAa0=";
+  idpWebPnpmDepsHash = "sha256-pQ01vBvC29B5oxDWtt7anI5QtFbvQFFBVamQtA2WTNo=";
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7385,6 +7385,8 @@ with pkgs;
 
   nsdiff = perlPackages.nsdiff;
 
+  opencloud-production = callPackage ../by-name/op/opencloud/production.nix { };
+
   openresty = callPackage ../servers/http/openresty {
     zlib-ng = zlib;
     withPerl = false;
