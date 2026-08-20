854009ea6a1d6e752828e46eab07a743e0ea07bd
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-08-20 08:34:21 +0200

    ocaml: default to version 5.5

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -2422,7 +2422,7 @@ rec {
 
   ocamlPackages_latest = ocamlPackages_5_5;
 
-  ocamlPackages = ocamlPackages_5_4;
+  ocamlPackages = ocamlPackages_5_5;
 
   # We still have packages that rely on unsafe-string, which is deprecated in OCaml 4.06.0.
   # Below are aliases for porting them to the latest versions of the OCaml 4 series.
