2af4fdd32d309d971173bd0190c6f627b70fa45c
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-20 08:24:31 +0000

    python3Packages.pycrdt: 0.14.2 -> 0.14.3

--- pkgs/development/python-modules/pycrdt/default.nix
+++ pkgs/development/python-modules/pycrdt/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pycrdt";
-  version = "0.14.2";
+  version = "0.14.3";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -28,7 +28,7 @@ buildPythonPackage (finalAttrs: {
     owner = "y-crdt";
     repo = "pycrdt";
     tag = finalAttrs.version;
-    hash = "sha256-s61r5b0wfEzvWUTEgwSqwURGbAkW1HENksmutRDBvkY=";
+    hash = "sha256-eOd1arKNUtaWhPR/WoHH7B3j1dpszR5859pKBBDycjQ=";
   };
 
   postPatch = ''
