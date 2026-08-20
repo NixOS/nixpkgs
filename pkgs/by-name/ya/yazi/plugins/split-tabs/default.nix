c5c0e44895f65479dcf62d9a81bb3d91b625af6e
Austin Horstman <khaneliman12@gmail.com>
2026-08-20 03:03:01 -0500

    yaziPlugins.split-tabs: 0-unstable-2026-08-17 → 0-unstable-2026-08-18
    Compare: https://github.com/terrakok/split-tabs.yazi/compare/5adeacdbbbdf0907fdad3ea19adc3f4ece584245...d0531f49503003d669b98ebd468f5dddb90391ba

--- pkgs/by-name/ya/yazi/plugins/split-tabs/default.nix
+++ pkgs/by-name/ya/yazi/plugins/split-tabs/default.nix
@@ -6,13 +6,13 @@
 
 mkYaziPlugin {
   pname = "split-tabs.yazi";
-  version = "0-unstable-2026-08-17";
+  version = "0-unstable-2026-08-18";
 
   src = fetchFromGitHub {
     owner = "terrakok";
     repo = "split-tabs.yazi";
-    rev = "5adeacdbbbdf0907fdad3ea19adc3f4ece584245";
-    hash = "sha256-W+0wyc699FxBkrpvF68VjoFCZT8p70MJBGwYkaNq81U=";
+    rev = "d0531f49503003d669b98ebd468f5dddb90391ba";
+    hash = "sha256-25PnsplMhF9satXHA1Ow0bZSksoLwH2zpuqrEZ2aFjQ=";
   };
 
   meta = {
