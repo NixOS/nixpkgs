c78059df05a471369de3ffc2d1d9d7680292cac8
Austin Horstman <khaneliman12@gmail.com>
2026-08-20 03:03:16 -0500

    yaziPlugins.vcs-files: 0-unstable-2026-08-17 → 0-unstable-2026-08-18
    Compare: https://github.com/yazi-rs/plugins/compare/3d25b6705fb1fb7967dfe393cf1b4a2926ebc40b...6af9bf23be808db4a86e89d2fc32d488dcafdd34

--- pkgs/by-name/ya/yazi/plugins/vcs-files/default.nix
+++ pkgs/by-name/ya/yazi/plugins/vcs-files/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "vcs-files.yazi";
-  version = "0-unstable-2026-08-17";
+  version = "0-unstable-2026-08-18";
 
   src = fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "3d25b6705fb1fb7967dfe393cf1b4a2926ebc40b";
-    hash = "sha256-vEm2AO1tEHnsX93LlxBytjFFNnwpnoZd86WiVQf67BU=";
+    rev = "6af9bf23be808db4a86e89d2fc32d488dcafdd34";
+    hash = "sha256-LzIM/X2+/KLo/TO7XhFEBtgkzP7g5O98haOUb1wUCdU=";
   };
 
   meta = {
