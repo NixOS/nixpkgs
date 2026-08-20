c9e01183b06d56d4c492b5084b32065990f076c9
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-08-20 11:16:54 -0400

    copilot-language-server: modernize
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/by-name/co/copilot-language-server/package.nix
+++ pkgs/by-name/co/copilot-language-server/package.nix
@@ -15,6 +15,9 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "copilot-language-server";
   version = "1.533.0";
 
+  strictDeps = true;
+  __structuredAttrs = true;
+
   src = fetchzip {
     url = "https://github.com/github/copilot-language-server-release/releases/download/${finalAttrs.version}/copilot-language-server-js-${finalAttrs.version}.zip";
     hash = "sha256-E0KA39Gc7ISeNxeUz/vjPxPD/3ugLZ1Nxa4OP1bqKpw=";
