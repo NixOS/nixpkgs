71d3df2e8dd44d9fa80a6cfba922140de43321ff
Marie Ramlow <me@nycode.dev>
2026-08-20 12:07:40 +0000

    forgejo-lts: 15.0.6 -> 15.0.7
    Changelog: https://codeberg.org/forgejo/forgejo/milestone/137461
    Diff: https://codeberg.org/forgejo/forgejo/compare/v15.0.6..v15.0.7

--- pkgs/by-name/fo/forgejo/lts.nix
+++ pkgs/by-name/fo/forgejo/lts.nix
@@ -1,8 +1,8 @@
 import ./generic.nix {
-  version = "15.0.6";
-  hash = "sha256-kWmBs/qAiBlmVcSwBM+rXapDbC8IpJtKfQRVPLH4geI=";
-  npmDepsHash = "sha256-wPta+potJJeOac7TyMk3BZg6su6mgHCEgesrsr7SCR4=";
-  vendorHash = "sha256-Kx+mP3GKfEOlsy5bkF7QYDebkrV+FHr37aQC4th/XJM=";
+  version = "15.0.7";
+  hash = "sha256-RzrOnnxHUKWfiQnF2TSDkMa2AZfqBhWVAjw26/B3ZgM=";
+  npmDepsHash = "sha256-VFkOYYznOXXis8mE4njTcLbh36AnW4wQgMLcvyCTSKY=";
+  vendorHash = "sha256-7KqezBwhcbOhX426HTOcOOIzscuqWJLmkgpMYuBRVr8=";
   lts = true;
   nixUpdateExtraArgs = [
     "--override-filename"
