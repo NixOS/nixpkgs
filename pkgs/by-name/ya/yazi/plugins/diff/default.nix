769c73bcc5b486cd457e33078a6797884e1beb3d
Austin Horstman <khaneliman12@gmail.com>
2026-08-20 03:01:36 -0500

    yaziPlugins.diff: 0-unstable-2026-07-22 → 0-unstable-2026-08-18
    Compare: https://github.com/yazi-rs/plugins/compare/bbac5e75b22a2893ef7cdd2bd6814b15f2abb91e...6af9bf23be808db4a86e89d2fc32d488dcafdd34

--- pkgs/by-name/ya/yazi/plugins/diff/default.nix
+++ pkgs/by-name/ya/yazi/plugins/diff/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "diff.yazi";
-  version = "0-unstable-2026-07-22";
+  version = "0-unstable-2026-08-18";
 
   src = fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "bbac5e75b22a2893ef7cdd2bd6814b15f2abb91e";
-    hash = "sha256-lio4pvrqK575q7M+GtRr/5EdA4h2J/7gIvXK8c5rq1U=";
+    rev = "6af9bf23be808db4a86e89d2fc32d488dcafdd34";
+    hash = "sha256-LzIM/X2+/KLo/TO7XhFEBtgkzP7g5O98haOUb1wUCdU=";
   };
 
   meta = {
