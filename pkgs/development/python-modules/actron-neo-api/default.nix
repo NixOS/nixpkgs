182953ef4402afb5885de235bca9995e8a92ea08
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-20 18:34:08 +0000

    python3Packages.actron-neo-api: 0.5.12 -> 0.5.14

--- pkgs/development/python-modules/actron-neo-api/default.nix
+++ pkgs/development/python-modules/actron-neo-api/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "actron-neo-api";
-  version = "0.5.12";
+  version = "0.5.14";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "kclif9";
     repo = "actronneoapi";
     tag = "v${version}";
-    hash = "sha256-j7qjVkpZFkWLVQd+/ndnjPOi8/xo357ez6yte78ny5U=";
+    hash = "sha256-5ijykxSZmCUInMezP/8GCuOXIxwOb7Kc9Zqr9/+ZPvU=";
   };
 
   build-system = [
