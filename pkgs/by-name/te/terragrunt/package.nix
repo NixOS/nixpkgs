8343b8727fa2a3a916ca891bfdfb889746862093
06kellyjac <dev@j-k.io>
2026-08-21 17:04:32 +0100

    terragrunt: enable tests

--- pkgs/by-name/te/terragrunt/package.nix
+++ pkgs/by-name/te/terragrunt/package.nix
@@ -1,9 +1,15 @@
 {
   lib,
+
   buildGoModule,
   fetchFromGitHub,
+
+  gitMinimal,
+  gitSetupHook,
+  opentofu,
+  writableTmpDirAsHomeHook,
+
   versionCheckHook,
-  mockgen,
 }:
 buildGoModule (finalAttrs: {
   pname = "terragrunt";
@@ -19,27 +25,71 @@ buildGoModule (finalAttrs: {
     hash = "sha256-JovTD88P/9IUX1y1AG/NhkIRRPCa0eAwJSx5qfg+4Ck=";
   };
 
-  nativeBuildInputs = [
-    mockgen
-  ];
-
   proxyVendor = true;
-
-  preBuild = ''
-    make generate-mocks
-  '';
 
   vendorHash = "sha256-eqoT9On/nGwJIbWug4RQVmibbsqbTRa5MzOoFXgGmxc=";
 
-  excludedPackages = [ "test/flake" ];
-
-  doCheck = false;
+  excludedPackages = [
+    # utility for identifying flakey tests
+    "test/flake"
+  ];
 
   ldflags = [
     "-s"
     "-w"
     "-X github.com/gruntwork-io/terragrunt/internal/version.Version=v${finalAttrs.version}"
   ];
+
+  nativeCheckInputs = [
+    # required for git to resolve $HOME/.gitconfig
+    # terragrunt's CAS feature also wants to write inside $HOME
+    writableTmpDirAsHomeHook
+    gitSetupHook
+    # needs to run git mid test
+    gitMinimal
+    opentofu
+  ];
+
+  preCheck = ''
+    # make executable so it's a valid target for `patchShebangs`
+    chmod +x test/fixtures/feature-flags/run-all-isolated-defaults/fake-tf.sh
+
+    # patch scripts used in tests
+    patchShebangs \
+      internal/os/exec/testdata \
+      internal/tf/testdata \
+      internal/shell/testdata \
+      test/fixtures/
+
+    # prep directory so git commands during testing will work
+    git config --global init.defaultBranch master
+    git init
+    # dir needs to be part of history
+    git add test/fixtures/config-terraform-functions
+    git commit --quiet --message "initial commit"
+  '';
+
+  checkFlags =
+    let
+      skippedTests = [
+        # calls out to the internet
+        "TestToSourceUrl"
+        # requires cloning it's own terragrunt repo via github.com
+        "TestCatalogWithLocalDefaultTemplate"
+        # requires cloning https://github.com/gruntwork-io/terraform-fake-modules.git/
+        "TestScaffoldGitModule"
+        "TestScaffoldGitRepo"
+        # requires cloning https://github.com/Azure/terraform-azurerm-avm-res-compute-virtualmachine.git
+        "TestScaffold3rdPartyModule"
+        # requires terraform
+        "TestDependencyOutputSkipDependencyOutputsFlag"
+        "TestCatalogGitRepoUpdate"
+        "TestRunAllHonorsTerraformBinaryWithBothOnPath"
+        # requires remote terraform/tofu providers
+        "TestTerragruntFullLockfile"
+      ];
+    in
+    [ "-skip=^${builtins.concatStringsSep "$|^" skippedTests}$" ];
 
   nativeInstallCheckInputs = [
     versionCheckHook
