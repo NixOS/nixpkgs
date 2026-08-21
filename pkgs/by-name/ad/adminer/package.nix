3fb783b8a45eef4af0c26113add86e9dbe6294b7
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-21 01:01:50 +0000

    adminer: 5.5.1 -> 6.0.1

--- pkgs/by-name/ad/adminer/package.nix
+++ pkgs/by-name/ad/adminer/package.nix
@@ -9,12 +9,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "adminer";
-  version = "5.5.1";
+  version = "6.0.1";
 
   # not using fetchFromGitHub as the git repo relies on submodules that are included in the tar file
   src = fetchurl {
     url = "https://github.com/vrana/adminer/releases/download/v${finalAttrs.version}/adminer-${finalAttrs.version}.zip";
-    hash = "sha256-vgPo43c56eD424z3SyR8i7UkLzt2/PaUaT/7778yl7M=";
+    hash = "sha256-BAHPwr/eVfETJDwBQUMV5g1wHmVkxmtf9BziEiOCVhE=";
   };
 
   nativeBuildInputs = [
