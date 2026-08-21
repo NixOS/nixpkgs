19e416de155a783fc281c941afd805b322271149
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-22 01:46:41 +0200

    python3Packages.types-pyyaml: 6.0.12.20250915 -> 6.0.12.20260815

--- pkgs/development/python-modules/types-pyyaml/default.nix
+++ pkgs/development/python-modules/types-pyyaml/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "types-pyyaml";
-  version = "6.0.12.20250915";
+  version = "6.0.12.20260815";
   pyproject = true;
 
   src = fetchPypi {
     pname = "types_pyyaml";
     inherit (finalAttrs) version;
-    hash = "sha256-D4tUpSjDA/Dm9xZWh90z+vqByAf8rCP2MrY6piTO0dM=";
+    hash = "sha256-KHZBEMnPNYRucz2jLY1zTfdHPF3envZ8O3My7A6BmFg=";
   };
 
   build-system = [ setuptools ];
