3295138139f44c601596e55372d31ac7396abdb7
zinzilulo <214774502+zinzilulo@users.noreply.github.com>
2026-08-21 20:20:37 +0800

    gnat{13, 14, 15, 16}: fix Ada trampolines on Darwin
    Use heap trampolines for Ada on all Darwin targets.

--- pkgs/development/compilers/gcc/patches/default.nix
+++ pkgs/development/compilers/gcc/patches/default.nix
@@ -176,6 +176,8 @@ optionals noSysDirs (
   is13 && langAda && stdenv.targetPlatform.isDarwin
 ) ./13/gnat13-aarch64-darwin-trampoline.patch
 
+++ optional (langAda && stdenv.targetPlatform.isDarwin) ./gnat-darwin-trampoline.patch
+
 ++ optional (targetPlatform.isWindows || targetPlatform.isCygwin) (fetchpatch {
   name = "libstdc-fix-compilation-in-freestanding-win32.patch";
   url = "https://inbox.sourceware.org/gcc-patches/20250922182808.2599390-2-corngood@gmail.com/raw";
