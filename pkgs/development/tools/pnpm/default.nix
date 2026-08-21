a2adf2fed935cad4430282a58ccc2c9e2f5a8f72
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-21 18:56:54 +0000

    pnpm: 11.21.0 -> 11.22.0
    (cherry picked from commit 99297a4daddd0f6ea8da2452e16a57c2ce45afce)

--- pkgs/development/tools/pnpm/default.nix
+++ pkgs/development/tools/pnpm/default.nix
@@ -53,8 +53,8 @@ let
       hash = "sha256-WOFDJYhx31FYm2UcBiBdq+xIdmpdu6PCWZm2m1C+WY4=";
     };
     "11" = {
-      version = "11.21.0";
-      hash = "sha256-hyN9N+rbedxiagV26zpS0j1wQiwyOuXgD8BckfQyN4A=";
+      version = "11.22.0";
+      hash = "sha256-V6l+byOj+v/AMVOk74x3CgVSYSuGQK6+Ob/dV1TQ69w=";
     };
   };
 
