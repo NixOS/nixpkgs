9b2f9eaca2181e992d26ffdc41f7bed141488a7c
Zane van Iperen <zane@zanevaniperen.com>
2026-08-21 21:18:13 +1000

    python3Packages.expression: init at 5.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5729,6 +5729,8 @@ self: super: with self; {
 
   explorerscript = callPackage ../development/python-modules/explorerscript { };
 
+  expression = callPackage ../development/python-modules/expression { };
+
   exrex = callPackage ../development/python-modules/exrex { };
 
   extension-helpers = callPackage ../development/python-modules/extension-helpers { };
