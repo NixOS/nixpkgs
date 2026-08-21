257ceeab24274d496b88b11d2cd27b29c9232adf
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-08-21 04:15:01 +0200

    python3Packages.s3tokenizer: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18388,6 +18388,8 @@ self: super: with self; {
 
   s3fs = callPackage ../development/python-modules/s3fs { };
 
+  s3tokenizer = callPackage ../development/python-modules/s3tokenizer { };
+
   s3transfer = callPackage ../development/python-modules/s3transfer { };
 
   sabctools = callPackage ../development/python-modules/sabctools { };
