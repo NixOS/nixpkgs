40d6e20c1560ec17f10064ce0f45c02c6e161c98
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-21 03:07:38 +0000

    wiki-go: 1.8.12 -> 1.8.13

--- pkgs/by-name/wi/wiki-go/package.nix
+++ pkgs/by-name/wi/wiki-go/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "wiki-go";
-  version = "1.8.12";
+  version = "1.8.13";
 
   src = fetchFromGitHub {
     owner = "leomoon-studios";
     repo = "wiki-go";
     tag = "v${version}";
-    hash = "sha256-j4a3jUmT9YTrdCCGBMmnzXbPD+81/CzXfhrQA2uN+Zk=";
+    hash = "sha256-o+sAaA48242hadFvBvE5nGxrUKdi2uQjwpGM5B/91pA=";
   };
 
   vendorHash = null;
