da49bec53ed760b6035f8a5b01c74bb2953bc62d
phaer <phaer@phaer.org>
2026-08-22 09:21:48 +0200

    signal-export: 3.9.0 -> 3.9.2

--- pkgs/by-name/si/signal-export/package.nix
+++ pkgs/by-name/si/signal-export/package.nix
@@ -7,13 +7,13 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "signal-export";
-  version = "3.9.0";
+  version = "3.9.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) version;
     pname = "signal_export";
-    hash = "sha256-iJfbeY1xVWsg95TpZqauTyy9uywWp6jZAdMlZaPDDmQ=";
+    hash = "sha256-ZAQ/qGpNzlpvFMIxKCDhtm5sez0tATY+l+jvXWaNbIc=";
   };
 
   build-system = with python3.pkgs; [
