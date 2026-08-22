19d047b12df25536d7b3d5b4324103c9b6b6097e
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-08-22 23:46:11 +0200

    python3Packages.skia-pathops: clean up patches

--- pkgs/development/python-modules/skia-pathops/default.nix
+++ pkgs/development/python-modules/skia-pathops/default.nix
@@ -37,16 +37,8 @@ buildPythonPackage rec {
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace "build_cmd = [sys.executable, build_skia_py, build_dir]" \
+      --replace-fail "build_cmd = [sys.executable, build_skia_py, build_dir]" \
         'build_cmd = [sys.executable, build_skia_py, "--no-fetch-gn", "--no-virtualenv", "--gn-path", "${gn}/bin/gn", build_dir]'
-  ''
-  + lib.optionalString (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isAarch64) ''
-    substituteInPlace src/cpp/skia-builder/skia/gn/skia/BUILD.gn \
-      --replace "-march=armv7-a" "-march=armv8-a" \
-      --replace "-mfpu=neon" "" \
-      --replace "-mthumb" ""
-    substituteInPlace src/cpp/skia-builder/skia/src/core/SkOpts.cpp \
-      --replace "defined(SK_CPU_ARM64)" "0"
   '';
 
   build-system = [
