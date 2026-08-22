d758d5059bf13844600bd8b8690c28d1bb150425
Aaron Andersen <aaron@fosslib.net>
2026-08-21 20:06:14 -0400

    qtserialport: make udev dependency optional

--- pkgs/development/libraries/qt-5/modules/qtserialport.nix
+++ pkgs/development/libraries/qt-5/modules/qtserialport.nix
@@ -4,10 +4,11 @@
   lib,
   qtbase,
   udev,
+  udevSupport ? stdenv.hostPlatform.isLinux,
 }:
 
 qtModule {
   pname = "qtserialport";
   propagatedBuildInputs = [ qtbase ];
-  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isLinux "-DNIXPKGS_LIBUDEV=\"${udev}/lib/libudev\"";
+  env.NIX_CFLAGS_COMPILE = lib.optionalString udevSupport "-DNIXPKGS_LIBUDEV=\"${udev}/lib/libudev\"";
 }
