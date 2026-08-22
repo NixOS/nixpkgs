bc41676e62d971c08a2aa2322709025a58db5df4
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-22 20:46:54 +0000

    fluent-bit: 5.1.0 -> 5.1.1

--- pkgs/by-name/fl/fluent-bit/package.nix
+++ pkgs/by-name/fl/fluent-bit/package.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fluent-bit";
-  version = "5.1.0";
+  version = "5.1.1";
 
   src = fetchFromGitHub {
     owner = "fluent";
     repo = "fluent-bit";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VXLHuyOxKWiMaYSRObu/I8kFbmUx8pGa4oKMGaEItbg=";
+    hash = "sha256-26E9UNYD8Zit+Ml9NfaxyX3Wv45tE/EFOdHd7aPspAg=";
   };
 
   # The source build documentation covers some dependencies and CMake options.
