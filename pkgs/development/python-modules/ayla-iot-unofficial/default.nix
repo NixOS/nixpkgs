bc0a461047df19846255b601a41a2085e277eb5b
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-22 14:38:48 +0200

    python3Packages.ayla-iot-unofficial: migrate to finalAttrs

--- pkgs/development/python-modules/ayla-iot-unofficial/default.nix
+++ pkgs/development/python-modules/ayla-iot-unofficial/default.nix
@@ -10,7 +10,7 @@
   ujson,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "ayla-iot-unofficial";
   version = "1.5.1";
   pyproject = true;
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "rewardone";
     repo = "ayla-iot-unofficial";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-N13xC2riZ31J1DzwP9vFXvz69w1DVbwEtbFubX5dvwQ=";
   };
 
@@ -43,10 +43,10 @@ buildPythonPackage rec {
   doCheck = false;
 
   meta = {
-    changelog = "https://github.com/rewardone/ayla-iot-unofficial/releases/tag/${src.tag}";
+    changelog = "https://github.com/rewardone/ayla-iot-unofficial/releases/tag/${finalAttrs.src.tag}";
     description = "Unofficial python library for interacting with the Ayla IoT API";
     homepage = "https://github.com/rewardone/ayla-iot-unofficial";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ dotlambda ];
   };
-}
+})
