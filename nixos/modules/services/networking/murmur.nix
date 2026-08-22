9f5ec04a46c511f8420aefccd121acc02c6b06e9
Felix Singer <felixsinger@posteo.net>
2026-08-22 02:00:13 +0200

    nixos/murmur: PrivatePid
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- nixos/modules/services/networking/murmur.nix
+++ nixos/modules/services/networking/murmur.nix
@@ -380,6 +380,7 @@ in
         NoNewPrivileges = true;
         PrivateDevices = true;
         PrivateMounts = true;
+        PrivatePid = true;
         PrivateTmp = true;
         PrivateUsers = true;
         ProcSubset = "pid";
