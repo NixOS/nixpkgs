6a1df0a9a09a810e1f057d5049f5b20099c7d0ad
Gaetan Lepage <gaetan@glepage.com>
2026-08-22 14:46:34 +0000

    protobuf_36: init at 36.0
    Changelog:
    https://github.com/protocolbuffers/protobuf/releases/tag/v36.0
    
    Diff: https://github.com/protocolbuffers/protobuf/compare/v35.1...v36.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6430,6 +6430,7 @@ with pkgs;
 
   inherit
     ({
+      protobuf_36 = callPackage ../development/libraries/protobuf/36.nix { };
       protobuf_35 = callPackage ../development/libraries/protobuf/35.nix { };
       protobuf_34 = callPackage ../development/libraries/protobuf/34.nix { };
       protobuf_33 = callPackage ../development/libraries/protobuf/33.nix { };
@@ -6449,6 +6450,7 @@ with pkgs;
         abseil-cpp = abseil-cpp_202103;
       };
     })
+    protobuf_36
     protobuf_35
     protobuf_34
     protobuf_33
