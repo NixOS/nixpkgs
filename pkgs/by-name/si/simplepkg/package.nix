010223a874941a512e3b344ad7cd274a16811c4f
void01n <aetherlowk@proton.me>
2026-08-21 20:57:17 -0400

    simplepkg: install a real bin/pkg wrapper for bash/zsh support

--- pkgs/by-name/si/simplepkg/package.nix
+++ pkgs/by-name/si/simplepkg/package.nix
@@ -5,38 +5,42 @@
   nix,
   git,
   sqlite,
+  fish,
+  makeWrapper,
 }:
-
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "simplepkg";
   version = "0.1.0";
-
   src = fetchFromGitHub {
     owner = "void01n";
     repo = "simple-pkg";
     tag = "v${finalAttrs.version}";
     hash = "sha256-eYQ7szXFhI8azEz86OL3OjkLbdHtZgADRuZ1A+Gu6/E=";
   };
-
   dontBuild = true;
   dontConfigure = true;
-
+  nativeBuildInputs = [ makeWrapper ];
   installPhase = ''
     runHook preInstall
-
     mkdir -p $out/share/fish/vendor_functions.d
     cp pkg.fish $out/share/fish/vendor_functions.d/
     cp pkg-import.fish $out/share/fish/vendor_functions.d/
 
+    mkdir -p $out/bin
+    cat > $out/bin/pkg <<'EOF'
+#!/usr/bin/env bash
+exec fish -c 'pkg $argv' -- "$@"
+EOF
+    chmod +x $out/bin/pkg
+    wrapProgram $out/bin/pkg --prefix PATH : ${lib.makeBinPath [ fish ]}
+
     runHook postInstall
   '';
-
   propagatedBuildInputs = [
     nix
     git
     sqlite
   ];
-
   meta = {
     description = "Declarative-feeling pkg manager for NixOS packages.nix";
     homepage = "https://github.com/void01n/simple-pkg";
