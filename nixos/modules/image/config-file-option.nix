13ee994642eae808c879f44a428f86986f1aff90
phaer <phaer@phaer.org>
2026-08-22 13:19:24 +0200

    nixos/config-file-option: allow null, add defaultText

--- nixos/modules/image/config-file-option.nix
+++ nixos/modules/image/config-file-option.nix
@@ -2,11 +2,15 @@
 
 {
   options.virtualisation.configFile = lib.mkOption {
-    type = lib.types.path;
+    type = lib.types.nullOr lib.types.path;
+    default = null;
+    defaultText = lib.literalMD "a default configuration file supplied by the respective image builder";
     description = ''
       A path to a NixOS configuration file which will be placed at
       `/etc/nixos/configuration.nix` in generated virtual machine images and
       used when switching to a new configuration.
+      Image builders set a builder-specific default; if set to `null` and no
+      builder default applies, no configuration file is installed.
     '';
   };
 }
