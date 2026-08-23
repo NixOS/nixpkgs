873957f0d4260c1ab81d1a3f2774e28926ce56c7
coolcuber <rpgeorg@clemson.edu>
2026-08-22 21:53:46 -0400

    gsmlib: fix version, license; modernize

--- pkgs/by-name/gs/gsmlib/package.nix
+++ pkgs/by-name/gs/gsmlib/package.nix
@@ -6,13 +6,16 @@
 }:
 stdenv.mkDerivation {
   pname = "gsmlib";
-  version = "unstable-2017-10-06";
+  version = "0-unstable-2017-10-06";
+
+  strictDeps = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "x-logLT";
     repo = "gsmlib";
     rev = "4f794b14450132f81673f7d3570c5a859aecf7ae";
-    sha256 = "16v8aj914ac1ipf14a867ljib3gy7fhzd9ypxnsg9l0zi8mm3ml5";
+    hash = "sha256-hdZRK4of0PS07den9qE7/o0VJT0GKRLcjYEpEpJUaJs=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
@@ -25,7 +28,7 @@ stdenv.mkDerivation {
   meta = {
     description = "Library to access GSM mobile phones through GSM modems";
     homepage = "https://github.com/x-logLT/gsmlib";
-    license = lib.licenses.lgpl2;
+    license = lib.licenses.lgpl21Plus;
     platforms = lib.platforms.linux;
     maintainers = [ lib.maintainers.misuzu ];
   };
