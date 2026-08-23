5dce49ccba69dde1f1732c88a4cc6f06e4facb53
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-23 22:34:49 +0000

    postfix-tlspol: 1.12.2 -> 1.13.0

--- pkgs/by-name/po/postfix-tlspol/package.nix
+++ pkgs/by-name/po/postfix-tlspol/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "postfix-tlspol";
-  version = "1.12.2";
+  version = "1.13.0";
 
   src = fetchFromGitHub {
     owner = "Zuplu";
     repo = "postfix-tlspol";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-1NIk3KXsUgf+CsbPRErNCJEP4RVuOUKnDq0akUfNKuE=";
+    hash = "sha256-wSE2Z5iZeE8YcDqeS5efUz7HKyuJkqtisj/YVfk/AXo=";
   };
 
   vendorHash = null;
