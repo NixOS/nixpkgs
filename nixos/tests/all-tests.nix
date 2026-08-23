2bc9aa576404d0aa472dc8630a8be387414462d4
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-08-23 01:10:24 -0400

    nixosTests.kafka: migrate to runTest
    0 rebuilds!
    
    Part of https://github.com/NixOS/nixpkgs/issues/386873
    
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -886,7 +886,9 @@ in
     ];
     rancherDistro = "k3s";
   };
-  kafka = handleTest ./kafka { };
+  kafka = import ./kafka {
+    inherit runTest pkgs;
+  };
   kaidan = runTest ./kaidan;
   kanboard = runTest ./web-apps/kanboard.nix;
   kanidm = runTest ./kanidm.nix;
