5e74eaa0d2c3c4ab4368b5c046ab25e4ebeb0e91
pagedmov <kylerclay@proton.me>
2026-08-23 04:28:46 -0400

    maintainers: update pagedMov github username/id

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21948,8 +21948,8 @@
   };
   pagedMov = {
     email = "kylerclay@proton.me";
-    github = "your-github-username";
-    githubId = 19557376;
+    github = "km-clay";
+    githubId = 21270915;
     name = "Kyler Clay";
     keys = [ { fingerprint = "784B 3623 94E7 8F11 0B9D AE0F 56FD CFA6 2A93 B51E"; } ];
   };
