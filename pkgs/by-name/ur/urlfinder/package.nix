022ee2ed20b8682e8d187d27a5d7c3846718b122
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-23 14:23:49 +0200

    urlfinder: add nix-update-script

--- pkgs/by-name/ur/urlfinder/package.nix
+++ pkgs/by-name/ur/urlfinder/package.nix
@@ -2,6 +2,7 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  nix-update-script,
   versionCheckHook,
   writableTmpDirAsHomeHook,
 }:
@@ -31,6 +32,8 @@ buildGoModule (finalAttrs: {
   doInstallCheck = true;
 
   versionCheckKeepEnvironment = [ "HOME" ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Tool for passively gathering URLs without active scanning";
