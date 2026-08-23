e8e7a8fdd1436401dc33a911425cee2ac2895a00
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-23 20:19:13 +0000

    kde-rounded-corners: 0.9.0 -> 0.10.0

--- pkgs/by-name/kd/kde-rounded-corners/package.nix
+++ pkgs/by-name/kd/kde-rounded-corners/package.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "kde-rounded-corners";
-  version = "0.9.0";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "matinlotfali";
     repo = "KDE-Rounded-Corners";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-JZlrjVjA2OGZhWpkSYvwYpPx4tmqlMvi4cP4zM4hxFc=";
+    hash = "sha256-YAO1cnOYD+f8BU71r567xwug7VBkelFzh4vpUTV5pNg=";
   };
 
   nativeBuildInputs = [
