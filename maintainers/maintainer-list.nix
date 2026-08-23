8a2bf2332bae7861e0f22cf17058a8c8d221d71f
Linus Emmerich <emmerichlinus@gmail.com>
2026-08-23 13:39:16 +0200

    maintainers: add linusemr618

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16439,6 +16439,12 @@
     githubId = 725613;
     name = "Linus Arver";
   };
+  linusemr618 = {
+    email = "emmerichlinus@gmail.com";
+    github = "linusemr618";
+    githubId = 96724651;
+    name = "Linus Emmerich";
+  };
   linuxissuper = {
     email = "m+nix@linuxistcool.de";
     matrix = "@m:linuxistcool.de";
