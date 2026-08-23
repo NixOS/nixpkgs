5f9fe8f62b2ac2709caba49cb1737aeb9c0fb968
skirlez <skirlez3@gmail.com>
2026-08-23 19:59:27 +0300

    undertalemodcli: 0.9.1.2 -> 0.9.2.0

--- pkgs/by-name/un/undertalemodcli/package.nix
+++ pkgs/by-name/un/undertalemodcli/package.nix
@@ -3,16 +3,17 @@
   fetchFromGitHub,
   dotnetCorePackages,
   buildDotnetModule,
+  nix-update-script,
   versionCheckHook,
 }:
 buildDotnetModule rec {
   pname = "undertalemodcli";
-  version = "0.9.1.2";
+  version = "0.9.2.0";
   src = fetchFromGitHub {
     owner = "UnderminersTeam";
     repo = "UndertaleModTool";
     tag = version;
-    sha256 = "sha256-I1UxVJ3AKRDPmYs0MoYXGukcj7krycVKb4jEZ4Y0pjI=";
+    sha256 = "sha256-bNSIU5SdyvFqDt1a63NUIuBWRxzk4bNYALSrCku6Cvg=";
     fetchSubmodules = true;
   };
   projectFile = "UndertaleModCli";
@@ -35,6 +36,8 @@ buildDotnetModule rec {
   dotnet-sdk = dotnetCorePackages.sdk_10_0;
   dotnet-runtime = dotnetCorePackages.runtime_10_0;
   executables = [ "UndertaleModCli" ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     homepage = "https://underminersteam.github.io/";
