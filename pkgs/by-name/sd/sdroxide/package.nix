1b33c80c8271d1fdac88a8faff5fe079d8d7fb74
nicoo <nicoo@debian.org>
2026-08-23 21:12:49 +0000

    sdroxide: init at 1.4.3 [WiP]

---
+++ pkgs/by-name/sd/sdroxide/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  pkgconf,
+  alsa-lib,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "sdroxide";
+  version = "1.4.3";
+
+  src = fetchFromGitHub {
+    owner = "dividebysandwich";
+    repo = "sdroxide";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-9agV8Y6Vk7CFacm8o6sPJXW23OP+5Ze7kK38QHLmOiU=";
+  };
+
+  buildInputs = [ alsa-lib ];
+  nativeBuildInputs = [ pkgconf ];
+
+  cargoHash = "sha256-u662PZWoIWdMEfUXiViKu+vs2fagCSZ12P2RYXSx2K4=";
+
+  meta = {
+    description = "A native SDR client for many radios, written in Rust, with native and web remote UI";
+    homepage = "https://github.com/dividebysandwich/sdroxide";
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [
+      nicoo
+    ];
+  };
+})
