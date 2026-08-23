7b85c3d3e31f636e196b9b5813f931332d5a4cc1
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-23 11:40:58 +0200

    nuclei: add nix-update-script
    (cherry picked from commit 7d044e385e234ba3bccecd250bd8f883df1e4b6c)

--- pkgs/by-name/nu/nuclei/package.nix
+++ pkgs/by-name/nu/nuclei/package.nix
@@ -2,6 +2,7 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  nix-update-script,
   versionCheckHook,
 }:
 
@@ -34,6 +35,8 @@ buildGoModule (finalAttrs: {
   doInstallCheck = true;
 
   versionCheckProgramArg = "-version";
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Tool for configurable targeted scanning";
