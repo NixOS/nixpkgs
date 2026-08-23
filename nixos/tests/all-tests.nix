6d9b09d6800df3d2f29de42b73052062d6b3e3a5
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-08-23 00:49:56 -0400

    nixosTests.grafana: migrate to runTest
    0 rebuilds!
    
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -730,7 +730,9 @@ in
   gotify-server = runTest ./gotify-server.nix;
   gotosocial = runTest ./web-apps/gotosocial.nix;
   goupile = runTest ./web-apps/goupile;
-  grafana = handleTest ./grafana { };
+  grafana = import ./grafana {
+    inherit runTest;
+  };
   grafana-to-ntfy = runTest ./grafana-to-ntfy.nix;
   graphite = runTest ./graphite.nix;
   grav = runTest ./web-apps/grav.nix;
