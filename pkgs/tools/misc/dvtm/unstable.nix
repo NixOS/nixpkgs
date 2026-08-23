e393788e07401e8a7682efdfba764809f2a1ac36
Guy Chronister <guychronister@outlook.com>
2026-08-23 15:01:50 +0000

    dvtm-unstable: inline unstable derivation and modernize packaging
    - Replace callPackage wrapper with direct stdenv.mkDerivation
    - Add __structuredAttrs and strictDeps
    - Add customConfig option and inject config.h via postPatch
    - Update patches and source to use `hash` instead of deprecated `sha256`
    - Add Darwin-specific CFLAGS for _DARWIN_C_SOURCE
    - Ensure terminfo directory exists before running tic
    - Add ncurses to nativeBuildInputs and buildInputs
    - Define outputs = [ "out" "man" ]
    - Refresh meta attributes and platforms

--- pkgs/tools/misc/dvtm/unstable.nix
+++ pkgs/tools/misc/dvtm/unstable.nix
@@ -1,13 +1,16 @@
 {
-  callPackage,
+  lib,
+  stdenv,
   fetchpatch,
   fetchzip,
+  ncurses,
+  customConfig ? null,
 }:
 
 let
   rev = "7bcf43f8dbd5c4a67ec573a1248114caa75fa3c2";
 in
-callPackage ./dvtm.nix {
+stdenv.mkDerivation (finalAttrs: {
   pname = "dvtm-unstable";
   version = "0.15-unstable-2021-03-09";
 
@@ -19,13 +22,51 @@ callPackage ./dvtm.nix {
     hash = "sha256-UtkNsW0mvLfbPSAIIZ1yvX9xzIDtiBeXCjhN2R8JhDc=";
   };
 
+  strictDeps = true;
+  __structuredAttrs = true;
+
   patches = [
     # https://github.com/martanne/dvtm/pull/69
     # Use self-pipe instead of signal blocking fixes issues on darwin.
     (fetchpatch {
       name = "use-self-pipe-fix-darwin";
       url = "https://github.com/martanne/dvtm/commit/1f1ed664d64603f3f1ce1388571227dc723901b2.patch";
-      sha256 = "14j3kks7b1v6qq12442v1da3h7khp02rp0vi0qrz0rfgkg1zilpb";
+      hash = "sha256-69L4w5vPZfAzBnGDmwW4cB44VAtbECICxmaHdfScQ5I=";
     })
   ];
-}
+
+  env = lib.optionalAttrs stdenv.hostPlatform.isDarwin {
+    CFLAGS = "-D_DARWIN_C_SOURCE";
+  };
+
+  postPatch = lib.optionalString (customConfig != null) ''
+    cp ${builtins.toFile "config.h" customConfig} ./config.h
+  '';
+
+  nativeBuildInputs = [ ncurses ];
+  buildInputs = [ ncurses ];
+
+  # 'tic' does not reliably create the target directory, so we make it first
+  preInstall = ''
+    mkdir -p $out/share/terminfo
+  '';
+
+  makeFlags = [
+    "PREFIX=$(out)"
+    "TERMINFO=$(out)/share/terminfo"
+  ];
+
+  outputs = [
+    "out"
+    "man"
+  ];
+
+  meta = {
+    description = "Dynamic virtual terminal manager";
+    homepage = "https://www.brain-dump.org/projects/dvtm";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "dvtm";
+    platforms = lib.platforms.unix;
+  };
+})
