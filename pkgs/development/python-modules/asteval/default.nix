12d476015a3cd50dd0cc76a37e3c6f9b95786fe8
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-23 08:57:27 +0200

    python3Packages.asteval: migrate to finalAttrs

--- pkgs/development/python-modules/asteval/default.nix
+++ pkgs/development/python-modules/asteval/default.nix
@@ -7,7 +7,7 @@
   setuptools-scm,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "asteval";
   version = "1.0.10";
   pyproject = true;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "lmfit";
     repo = "asteval";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-Z30H1bSud/VbYnHqRYzyJnDufFM4uBhPcMtVmZ1Sl94=";
   };
 
@@ -36,8 +36,8 @@ buildPythonPackage rec {
   meta = {
     description = "AST evaluator of Python expression using ast module";
     homepage = "https://github.com/lmfit/asteval";
-    changelog = "https://github.com/lmfit/asteval/releases/tag/${src.tag}";
+    changelog = "https://github.com/lmfit/asteval/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
