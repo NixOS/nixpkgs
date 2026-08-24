a9f07cd22b8a1c182a10201a466233ad515cfae4
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:33 +0200

    python3Packages.lingua: 4.15.0 -> 4.16.2
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/lingua/default.nix
+++ pkgs/development/python-modules/lingua/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "lingua";
-  version = "4.15.0";
+  version = "4.16.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-DhqUZ0HbKIpANhrQT/OP4EvwgZg0uKu4TEtTX+2bpO8=";
+    hash = "sha256-seXLu+zUCv057z1Py0Dk4/yblrwOBD5wiESm0ibuVL0=";
   };
 
   postPatch = ''
