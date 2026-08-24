b1955960452b94bb5158390ad5d7fa6638aafd41
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 13:54:59 +0200

    python3Packages.dunamai: 1.25.0 -> 1.26.2
    Diff: https://github.com/mtkennerly/dunamai/compare/v1.25.0...v1.26.2
    
    Changelog: https://github.com/mtkennerly/dunamai/blob/v1.26.2/CHANGELOG.md

--- pkgs/development/python-modules/dunamai/default.nix
+++ pkgs/development/python-modules/dunamai/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "dunamai";
-  version = "1.25.0";
+  version = "1.26.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mtkennerly";
     repo = "dunamai";
     tag = "v${version}";
-    hash = "sha256-kPOEhJwsSzGea7fS5y5tbAvzZZ+OxIyjpYpS6i++rHE=";
+    hash = "sha256-pP0KeQ1tThvBomQIlrWNCVzB8ghR9rfaLm5NeBKwiuE=";
   };
 
   build-system = [ poetry-core ];
