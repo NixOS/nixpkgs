b2f5b130bf16da2fdcc991fcdee257c4dfc6c5c4
Austin Horstman <khaneliman12@gmail.com>
2026-08-24 03:03:28 -0500

    yaziPlugins.sudo: 0-unstable-2026-05-07 → 0-unstable-2026-08-23
    Compare: https://github.com/TD-Sky/sudo.yazi/compare/afd61cedbcd13c549c552766755645069561d28c...be3670f3eae8c565e541d65e5f8f292f2e4f2398

--- pkgs/by-name/ya/yazi/plugins/sudo/default.nix
+++ pkgs/by-name/ya/yazi/plugins/sudo/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "sudo.yazi";
-  version = "0-unstable-2026-05-07";
+  version = "0-unstable-2026-08-23";
 
   src = fetchFromGitHub {
     owner = "TD-Sky";
     repo = "sudo.yazi";
-    rev = "afd61cedbcd13c549c552766755645069561d28c";
-    hash = "sha256-5wa4fdYo7wOXOzdrigWMtADZIL/7alG8Jw7r8iWz9yA=";
+    rev = "be3670f3eae8c565e541d65e5f8f292f2e4f2398";
+    hash = "sha256-GNhoRnEKfA6otuXal87JWLW1bUW4Ago94BbQQ1ae3zA=";
   };
 
   meta = {
