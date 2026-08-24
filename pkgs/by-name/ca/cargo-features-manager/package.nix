dcc84d1f026b6fc2ffa8925832821aab7eda0f3c
lukas-sgx <lukas.soigneux@epitech.eu>
2026-08-24 21:25:21 +0000

    cargo-features-manager: 0.11.0 -> 0.11.1

--- pkgs/by-name/ca/cargo-features-manager/package.nix
+++ pkgs/by-name/ca/cargo-features-manager/package.nix
@@ -5,13 +5,13 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cargo-features-manager";
-  version = "0.11.0";
+  version = "0.11.1";
 
   src = fetchFromGitHub {
     owner = "ToBinio";
     repo = "cargo-features-manager";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-ay6nhRmGRBURisfr7qnnWCKn8JCnFh9x0TJ7vK2p4PU=";
+    hash = "sha256-iPgTMeRyzw7NjNPRHfQp3LWbkUAU1ubiEWCGLbGckks=";
   };
 
   cargoHash = "sha256-QB+0ezc7IQPzSym/Lfqjnh24tHKKwLqIpnGPNFQxI5M=";
