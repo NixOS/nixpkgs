e769f224055acc09f5ac2d732e339352e185add0
BatteredBunny <ayes2022@protonmail.com>
2026-08-24 18:43:39 +0300

    plezy: 2.16.0 -> 2.17.0

--- pkgs/by-name/pl/plezy/package.nix
+++ pkgs/by-name/pl/plezy/package.nix
@@ -27,13 +27,13 @@
 
 let
   pname = "plezy";
-  version = "2.16.0";
+  version = "2.17.0";
 
   src = fetchFromGitHub {
     owner = "edde746";
     repo = "plezy";
     tag = version;
-    hash = "sha256-i4QJ8o8zOivUKarXgEmKJeX5XyYTyaqxjevD6nwMjII=";
+    hash = "sha256-lY8uwz+OyrUFuFxRQ+2GuTLwJYigLy/JZYv4R5tRszM=";
   };
 
   simdutf = fetchurl {
@@ -146,7 +146,7 @@ let
 
     src = fetchurl {
       url = "https://github.com/edde746/plezy/releases/download/${version}/plezy-macos.dmg";
-      hash = "sha256-h1aBFYEP1ie6v9BZZ/vH0b+fQL047tMZz3tASwnkcWI=";
+      hash = "sha256-MYuawP8NI5s+261XpEwAjnqvDFYjvCxZVAf84ph7peQ=";
     };
 
     nativeBuildInputs = [
