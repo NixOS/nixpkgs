2acab174f177e71031462170c79a8a52cd736cca
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:30 +0200

    python3Packages.jupyterlab-widgets: 3.0.16 -> 3.0.17
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/jupyterlab-widgets/default.nix
+++ pkgs/development/python-modules/jupyterlab-widgets/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "jupyterlab-widgets";
-  version = "3.0.16";
+  version = "3.0.17";
   pyproject = true;
 
   src = fetchPypi {
     pname = "jupyterlab_widgets";
     inherit version;
-    hash = "sha256-Qj2gUHHVXPJ6nmAiFtNaOmWj5BzfnF07ZDuBTOOMGeA=";
+    hash = "sha256-bmH+IcqKZgORgKXMUqQz4HJ50v7nnIvpY+ANVRk/F6g=";
   };
 
   # jupyterlab is required to build from source but we use the pre-build package
