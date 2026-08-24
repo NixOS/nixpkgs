2b685758625c7b705a94f60b49c7b5187838a3a4
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 16:03:55 +0200

    python3Packages.sphinxcontrib-bibtex: 2.6.5 -> 2.7.0
    Changelog: https://github.com/mcmtroffaes/sphinxcontrib-bibtex/blob/2.7.0/CHANGELOG.rst

--- pkgs/development/python-modules/sphinxcontrib-bibtex/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-bibtex/default.nix
@@ -2,7 +2,6 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch2,
   setuptools,
   docutils,
   oset,
@@ -13,34 +12,27 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "sphinxcontrib-bibtex";
-  version = "2.6.5";
+  version = "2.7.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mcmtroffaes";
     repo = "sphinxcontrib-bibtex";
-    tag = version;
-    hash = "sha256-sT23DkIfJcb3cFBFdL31RRzlDoJRcCUYIdpUVuYjGuo=";
+    tag = finalAttrs.version;
+    hash = "sha256-8VmLTr9l+JTUb9oVXGI/j5Yz7Fe+ybEA8t2VUeRGBqI=";
   };
 
-  patches = [
-    (fetchpatch2 {
-      name = "fix-tests-docutils-0.22.diff";
-      url = "https://github.com/mcmtroffaes/sphinxcontrib-bibtex/commit/20781600dad48fdfee91353c821597690bfe5f54.diff?full_index=1";
-      hash = "sha256-Ia/ng3yUfhLueEB/n+CW51w/UWfzRhrv/5//Mq2OJ0M=";
-    })
-  ];
-
   build-system = [ setuptools ];
+
+  buildInputs = [ sphinx ];
 
   dependencies = [
     docutils
     oset
     pybtex
     pybtex-docutils
-    sphinx
   ];
 
   nativeCheckInputs = [
@@ -55,7 +47,8 @@ buildPythonPackage rec {
   meta = {
     description = "Sphinx extension for BibTeX style citations";
     homepage = "https://github.com/mcmtroffaes/sphinxcontrib-bibtex";
+    changelog = "https://github.com/mcmtroffaes/sphinxcontrib-bibtex/blob/${finalAttrs.src.tag}/CHANGELOG.rst";
     license = lib.licenses.bsd2;
     maintainers = [ ];
   };
-}
+})
