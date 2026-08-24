9020c570ba45eecd4d1cabefcaf8ecbc5d618af8
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:05 +0200

    python3Packages.ansible: 14.2.0 -> 14.3.1
    https://github.com/ansible-community/ansible-build-data/blob/14.3.1/14/CHANGELOG-v14.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/ansible/default.nix
+++ pkgs/development/python-modules/ansible/default.nix
@@ -24,7 +24,7 @@
 
 let
   pname = "ansible";
-  version = "14.2.0";
+  version = "14.3.1";
 in
 buildPythonPackage {
   inherit pname version;
@@ -32,7 +32,7 @@ buildPythonPackage {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-HI8YOZAn0msBdScMtjutmfm+Fu/nlgAkLJFOqkmUECU=";
+    hash = "sha256-mNStKzVf6sjcBNmeg3fWy4AbHVi7+t1tgFNORgf7XE0=";
   };
 
   # we make ansible-core depend on ansible, not the other way around,
