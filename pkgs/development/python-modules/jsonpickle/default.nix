33384c706e372730c1ae50f43ab8e815c3cde052
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:29 +0200

    python3Packages.jsonpickle: 4.1.1 -> 4.1.2
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/jsonpickle/default.nix
+++ pkgs/development/python-modules/jsonpickle/default.nix
@@ -15,12 +15,12 @@
 
 buildPythonPackage rec {
   pname = "jsonpickle";
-  version = "4.1.1";
+  version = "4.1.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-+G4Y8T4rlsHB7t4Le5AJW7th2Z/twUgTxE3C82HbuuE=";
+    hash = "sha256-iv7RiqGJ/YHi6DO0JrtK9IVZSSHwsdNsIAH8Vjei8hA=";
   };
 
   build-system = [
