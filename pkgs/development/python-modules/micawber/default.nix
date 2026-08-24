9fe50455d22eb6192c8476f87b04d785d0675a14
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:35 +0200

    python3Packages.micawber: 0.6.2 -> 0.7.0
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/micawber/default.nix
+++ pkgs/development/python-modules/micawber/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "micawber";
-  version = "0.6.2";
+  version = "0.7.0";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-zATBml4Os2FbjNldWu/a1c3k9SphwS1FBxcn3+Ext2A=";
+    hash = "sha256-q5xZx1ac/Eh902fcTtoT45S6UhbhnQLxhxrFbQoaGUQ=";
   };
 
   propagatedBuildInputs = [ beautifulsoup4 ];
