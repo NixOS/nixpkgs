dfa50749dc4aba7092ce9dcd690c17d203009b20
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-08-24 06:29:16 +0200

    python3Packages.comfyui-workflow-templates: 0.11.41 -> 0.11.44

--- pkgs/development/python-modules/comfyui-workflow-templates/default.nix
+++ pkgs/development/python-modules/comfyui-workflow-templates/default.nix
@@ -15,13 +15,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "comfyui-workflow-templates";
-  version = "0.11.41";
+  version = "0.11.44";
   pyproject = true;
 
   src = fetchPypi {
     pname = "comfyui_workflow_templates";
     inherit (finalAttrs) version;
-    hash = "sha256-vzTTCvGVM9ckvF9mSY5umpZ784CfScEcLiNrzJAq/N0=";
+    hash = "sha256-6C+qy6Wx4Wrlb0macBY4o/fgJPZ3WNs4RcA0OTm0ucQ=";
   };
 
   build-system = [ setuptools ];
