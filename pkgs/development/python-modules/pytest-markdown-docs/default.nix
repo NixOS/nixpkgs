b1e31763086839e1f6f8fe06abb8cbc5ede7f822
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 21:08:07 +0200

    python3Packages.pytest-markdown-docs: 0.9.1 -> 0.9.2
    Changelog: https://github.com/modal-labs/pytest-markdown-docs/releases/tag/v0.9.2

--- pkgs/development/python-modules/pytest-markdown-docs/default.nix
+++ pkgs/development/python-modules/pytest-markdown-docs/default.nix
@@ -4,7 +4,7 @@
   fetchFromGitHub,
 
   # build-system
-  hatchling,
+  uv-build,
 
   # dependencies
   markdown-it-py,
@@ -12,22 +12,28 @@
 
   # tests
   mdit-py-plugins,
+  pytest-asyncio,
   pytestCheckHook,
 }:
 
 buildPythonPackage (finalAttrs: {
   pname = "pytest-markdown-docs";
-  version = "0.9.1";
+  version = "0.9.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "modal-com";
     repo = "pytest-markdown-docs";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-7fGuKTHeaMEbsHD9Zje0ODP2FRWSi0WrCZsPwRYP6rg=";
+    hash = "sha256-V1volx1BckCmYwnM9Q43gRECYdccwaPWX7zUsk/+BoI=";
   };
 
-  build-system = [ hatchling ];
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build>=0.10.9,<0.11.0" "uv_build"
+  '';
+
+  build-system = [ uv-build ];
 
   pythonRelaxDeps = [
     "markdown-it-py"
@@ -41,12 +47,14 @@ buildPythonPackage (finalAttrs: {
 
   nativeCheckInputs = [
     mdit-py-plugins
+    pytest-asyncio
     pytestCheckHook
   ];
 
   meta = {
     description = "Run pytest on markdown code fence blocks";
     homepage = "https://github.com/modal-com/pytest-markdown-docs";
+    changelog = "https://github.com/modal-labs/pytest-markdown-docs/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ GaetanLepage ];
   };
