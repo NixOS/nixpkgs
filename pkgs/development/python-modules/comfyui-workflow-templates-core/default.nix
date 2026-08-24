69e30b8505fa5305010ad2918dc296400a0c096e
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:15 +0200

    python3Packages.comfyui-workflow-templates-core: 0.3.312 -> 0.3.320
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/comfyui-workflow-templates-core/default.nix
+++ pkgs/development/python-modules/comfyui-workflow-templates-core/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "comfyui-workflow-templates-core";
-  version = "0.3.312";
+  version = "0.3.320";
   pyproject = true;
 
   src = fetchPypi {
     pname = "comfyui_workflow_templates_core";
     inherit (finalAttrs) version;
-    hash = "sha256-OWhVWGEqf3cZRH1njGLpk6LcAme32JtZZ5mBO875/Zo=";
+    hash = "sha256-aoX4fHyr+HuIq0OL+IYu1KA3rYD4hltLFBxAPID+xsg=";
   };
 
   build-system = [ setuptools ];
