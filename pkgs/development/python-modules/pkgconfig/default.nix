463b92737c394d8d9b81b33ac82cb0d2529760cc
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 23:18:16 +0200

    python3Packages.pkgconfig: 1.5.5 -> 1.6.0
    Changelog: https://github.com/matze/pkgconfig/releases/tag/v1.6.0

--- pkgs/development/python-modules/pkgconfig/default.nix
+++ pkgs/development/python-modules/pkgconfig/default.nix
@@ -6,11 +6,12 @@
   poetry-core,
   pkg-config,
   pytestCheckHook,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pkgconfig";
-  version = "1.5.5";
+  version = "1.6.0";
   pyproject = true;
 
   inherit (pkg-config)
@@ -24,33 +25,33 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "matze";
     repo = "pkgconfig";
-    rev = "v${version}";
-    hash = "sha256-uuLUGRNLCR3NS9g6OPCI+qG7tPWsLhI3OE5WmSI3vm8=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Glla/tg83Vd2VWvUnCPrcncS73vMhqaGpo+AviR8jnY=";
   };
 
   postPatch = ''
-    substituteInPlace pkgconfig/pkgconfig.py \
+    substituteInPlace src/pkgconfig/pkgconfig.py \
       --replace "pkg_config_exe = os.environ.get('PKG_CONFIG', None) or 'pkg-config'" "pkg_config_exe = '${pkg-config}/bin/${pkg-config.targetPrefix}pkg-config'"
 
     # those pc files are missing and pkg-config validates that they exist
-    substituteInPlace data/fake-openssl.pc \
+    substituteInPlace tests/data/fake-openssl.pc \
       --replace "Requires: libssl libcrypto" ""
   '';
 
-  nativeBuildInputs = [ poetry-core ];
+  build-system = [ poetry-core ];
 
-  # ModuleNotFoundError: No module named 'distutils'
-  # https://github.com/matze/pkgconfig/issues/64
-  doCheck = pythonOlder "3.12";
-
-  nativeCheckInputs = [ pytestCheckHook ];
+  nativeCheckInputs = [
+    pytestCheckHook
+    setuptools
+  ];
 
   pythonImportsCheck = [ "pkgconfig" ];
 
   meta = {
     description = "Interface Python with pkg-config";
     homepage = "https://github.com/matze/pkgconfig";
+    changelog = "https://github.com/matze/pkgconfig/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ nickcao ];
   };
-}
+})
