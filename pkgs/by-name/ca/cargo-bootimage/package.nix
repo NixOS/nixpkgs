6610a7b6d565732a690624ffb20f203b6124ed39
lukas-sgx <lukas.soigneux@epitech.eu>
2026-08-24 21:13:53 +0000

    cargo-bootimage: 0.10.3 -> 0.10.5

--- pkgs/by-name/ca/cargo-bootimage/package.nix
+++ pkgs/by-name/ca/cargo-bootimage/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "bootimage";
-  version = "0.10.3";
+  version = "0.10.5";
 
   src = fetchFromGitHub {
     owner = "rust-osdev";
     repo = "bootimage";
     rev = "v${finalAttrs.version}";
-    sha256 = "12p18mk3l473is3ydv3zmn6s7ck8wgjwavllimcpja3yjilxm3zg";
+    sha256 = "sha256-Q/9T+6JZPfPV3DvMqNkFHow6gBpbk3HQcbfuTaAxwHo=";
   };
 
-  cargoHash = "sha256-CkFJHW7yrIJi/KMGJgyhnLTMkrxnDwO3X4M1aml9cuM=";
+  cargoHash = "sha256-UylacTDWZVkoyAA7fDsX1v+8TMnynIIfE3v+vxDXxyY=";
 
   meta = {
     description = "Creates a bootable disk image from a Rust OS kernel";
