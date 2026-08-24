ec5de6e3d8edd717a0e4e447987b91bde66f5592
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:25:05 +0200

    python3Packages.webencodings: 0.5.1 -> 0.6.1
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/webencodings/default.nix
+++ pkgs/development/python-modules/webencodings/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "webencodings";
-  version = "0.5.1";
+  version = "0.6.1";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b36a1c245f2d304965eb4e0a82848379241dc04b865afcc4aab16748587e1923";
+    sha256 = "sha256-Vl+a0DHHAtrkBOJ6CZ4+CRhqOrG5Ug8G0hVQK2Uf2RA=";
   };
 
   nativeCheckInputs = [ pytest ];
