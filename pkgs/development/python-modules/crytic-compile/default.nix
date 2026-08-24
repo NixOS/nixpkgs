243023c59f2fb8759b6674e154fb6e9752e76378
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 21:34:52 +0200

    python3Packages.crytic-compile: 0.3.11 -> 0.4.2
    Changelog: https://github.com/crytic/crytic-compile/releases/tag/0.4.2

--- pkgs/development/python-modules/crytic-compile/default.nix
+++ pkgs/development/python-modules/crytic-compile/default.nix
@@ -4,27 +4,33 @@
   cbor2,
   fetchFromGitHub,
   pycryptodome,
-  setuptools,
+  uv-build,
   solc-select,
   toml,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "crytic-compile";
-  version = "0.3.11";
-  format = "setuptools";
+  version = "0.4.2";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "crytic";
     repo = "crytic-compile";
-    tag = version;
-    hash = "sha256-NVAIVUfh1bizg/HG1z7Ze6o5w6wto744Ogq0LPg0gXg=";
+    tag = finalAttrs.version;
+    hash = "sha256-0zpalWsyFzsgSrmTi9WyHfRcRympv2WoJNEtzpWXmGk=";
   };
 
-  propagatedBuildInputs = [
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build>=0.6,<0.10" "uv_build"
+  '';
+
+  build-system = [ uv-build ];
+
+  dependencies = [
     cbor2
     pycryptodome
-    setuptools
     solc-select
     toml
   ];
@@ -35,17 +41,18 @@ buildPythonPackage rec {
   # required for import check to work
   # PermissionError: [Errno 13] Permission denied: '/homeless-shelter'
   env.HOME = "/tmp";
+
   pythonImportsCheck = [ "crytic_compile" ];
 
   meta = {
     description = "Abstraction layer for smart contract build systems";
-    mainProgram = "crytic-compile";
     homepage = "https://github.com/crytic/crytic-compile";
-    changelog = "https://github.com/crytic/crytic-compile/releases/tag/${src.tag}";
+    changelog = "https://github.com/crytic/crytic-compile/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.agpl3Plus;
     maintainers = with lib.maintainers; [
       arturcygan
       hellwolf
     ];
+    mainProgram = "crytic-compile";
   };
-}
+})
