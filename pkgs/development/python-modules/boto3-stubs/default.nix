03570f0fe0c57bc6c4153cc5670e9b971839be13
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:11 +0200

    python3Packages.boto3-stubs: 1.43.72 -> 1.43.79
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/boto3-stubs/default.nix
+++ pkgs/development/python-modules/boto3-stubs/default.nix
@@ -358,13 +358,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "boto3-stubs";
-  version = "1.43.72";
+  version = "1.43.79";
   pyproject = true;
 
   src = fetchPypi {
     pname = "boto3_stubs";
     inherit (finalAttrs) version;
-    hash = "sha256-yuwRBavw54+bLbXdPDZM4QwoPVoSjGlmXbQoxwXQjz8=";
+    hash = "sha256-bfdQ6vu9WakrZ4v7ci0FLJ2b6FtACw5KI2e/LgJZ4k4=";
   };
 
   build-system = [ setuptools ];
