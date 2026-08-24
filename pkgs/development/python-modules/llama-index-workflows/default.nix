8950825857fbea489367a10c9c0d3fe8741be05f
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:34 +0200

    python3Packages.llama-index-workflows: 2.23.0 -> 2.23.3
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/llama-index-workflows/default.nix
+++ pkgs/development/python-modules/llama-index-workflows/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "llama-index-workflows";
-  version = "2.23.0";
+  version = "2.23.3";
   pyproject = true;
 
   src = fetchPypi {
     pname = "llama_index_workflows";
     inherit (finalAttrs) version;
-    hash = "sha256-lgVt/q1YvTPIadIZyO9WxKv5dsBCJtf28RPqONsZ5W0=";
+    hash = "sha256-+6FA6k1Kp+fKWPPdwlJrkr+zG/r0WrK0NFQBrlJwYbQ=";
   };
 
   postPatch = ''
