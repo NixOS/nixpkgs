7ea33c5a6ef67f3c804442591eeba2626ab1b2e3
Th1nkK1D <witheep@gmail.com>
2026-08-25 00:55:23 +0700

    maintainers: add th1nkk1d

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -28503,6 +28503,12 @@
     githubId = 41830259;
     name = "Thomas Marchand";
   };
+  th1nkk1d = {
+    email = "witheep@gmail.com";
+    github = "Th1nkK1D";
+    githubId = 8860448;
+    name = "Withee Poositasai";
+  };
   thall = {
     email = "niclas.thall@gmail.com";
     github = "thall";
