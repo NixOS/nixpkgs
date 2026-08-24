a4821e432760ad4b62c66630984854aa61cd6d0c
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:12 +0200

    python3Packages.cantools: 41.3.1 -> 41.4.3
    https://github.com/cantools/cantools/releases/tag/41.4.3
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/cantools/default.nix
+++ pkgs/development/python-modules/cantools/default.nix
@@ -18,12 +18,12 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "cantools";
-  version = "41.3.1";
+  version = "41.4.3";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-Y5ZbAorAKrG0yGeqIH7Zn5D1WziuEHq+KH19ZtVDXZ8=";
+    hash = "sha256-pGYo2pWo13KyNcaEhwyzLVNLl9LQ68erB5+E1N9aKeU=";
   };
 
   build-system = [
