186cec7145eed2fd073937c0af7df38cce04eb88
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 15:39:40 +0200

    python3Packages.sphinxcontrib-programoutput: 0.18 -> 0.20
    Changelog: https://github.com/NextThought/sphinxcontrib-programoutput/releases/tag/v0.20

--- pkgs/development/python-modules/sphinxcontrib-programoutput/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-programoutput/default.nix
@@ -1,27 +1,30 @@
 {
   lib,
   buildPythonPackage,
+  docutils,
   fetchFromGitHub,
   setuptools,
   sphinx,
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "sphinxcontrib-programoutput";
-  version = "0.18";
+  version = "0.20";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "NextThought";
     repo = "sphinxcontrib-programoutput";
-    tag = version;
-    hash = "sha256-WI4R96G4cYYTxTwW4dKAayUNQyhVSrjhdWJyy8nZBUk=";
+    tag = finalAttrs.version;
+    hash = "sha256-1nWEVE4Ie6QRtm8IvSIrxYCjNtyp831teBskv63YETI=";
   };
 
   build-system = [ setuptools ];
 
   buildInputs = [ sphinx ];
+
+  dependencies = [ docutils ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
@@ -29,10 +32,16 @@ buildPythonPackage rec {
 
   pythonNamespaces = [ "sphinxcontrib" ];
 
+  disabledTests = [
+    # erbsland-sphinx-ansi is not available in nixpkgs
+    "test_use_ansi_enabled_extension"
+  ];
+
   meta = {
     description = "Sphinx extension to include program output";
     homepage = "https://github.com/NextThought/sphinxcontrib-programoutput";
+    changelog = "https://github.com/NextThought/sphinxcontrib-programoutput/releases/tag/v${finalAttrs.src.tag}";
     license = lib.licenses.bsd2;
     maintainers = [ ];
   };
-}
+})
