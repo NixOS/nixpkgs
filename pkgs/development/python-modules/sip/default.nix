49d5798f4008f6d173514caed2fc8091f863bd08
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:24:56 +0200

    python3Packages.sip: 6.15.1 -> 6.16.1
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/sip/default.nix
+++ pkgs/development/python-modules/sip/default.nix
@@ -14,12 +14,12 @@
 
 buildPythonPackage rec {
   pname = "sip";
-  version = "6.15.1";
+  version = "6.16.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-3C5YwXmKdOGzHCjoNzOYIv6PpVKIrjDomG6ygQDrylo=";
+    hash = "sha256-CnOcnNKSneTgiERW2Mrzz7IsEFNHV8d5e9jca9ntabw=";
   };
 
   build-system = [
