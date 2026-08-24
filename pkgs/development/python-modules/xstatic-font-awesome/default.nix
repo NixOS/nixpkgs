821eced83dbb0f697ff076c330b8f7c6a1a83713
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:25:06 +0200

    python3Packages.xstatic-font-awesome: 6.2.1.1 -> 6.2.1.2
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/xstatic-font-awesome/default.nix
+++ pkgs/development/python-modules/xstatic-font-awesome/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "xstatic-font-awesome";
-  version = "6.2.1.1";
+  version = "6.2.1.2";
   format = "setuptools";
 
   src = fetchPypi {
     pname = "XStatic-Font-Awesome";
     inherit version;
-    hash = "sha256-8HWHEJYShjjy4VOQINgid1TD2IXdaOfubemgEjUHaCg=";
+    hash = "sha256-nzyy8Dj619NSciN10/Ja80banuCT7Z3CyMRr2RGrGXE=";
   };
 
   # xstatic uses pkg_resources.declare_namespace, removed in setuptools 83.
