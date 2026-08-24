b3741499c485621fe86c905b5ff517b5dd6e5a71
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 12:01:47 +0200

    python3Packages.py-cpuinfo2: init at 10.0.1
    Module for getting CPU info with pure Python
    
    https://github.com/akx/py-cpuinfo2

---
+++ pkgs/development/python-modules/py-cpuinfo2/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  nix-update-script,
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "py-cpuinfo2";
+  version = "10.1.0";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "akx";
+    repo = "py-cpuinfo2";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-pNesRzR2LynC/if95SZH+47sW6WOJ9soXCyX8TQrpso=";
+  };
+
+  build-system = [ hatchling ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "cpuinfo" ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Module for getting CPU info with pure Python";
+    homepage = "https://github.com/akx/py-cpuinfo2";
+    changelog = "https://github.com/akx/py-cpuinfo2/blob/${finalAttrs.src.rev}/ChangeLog";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ fab ];
+  };
+})
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14181,6 +14181,8 @@ self: super: with self; {
 
   py-cpuinfo = callPackage ../development/python-modules/py-cpuinfo { };
 
+  py-cpuinfo2 = callPackage ../development/python-modules/py-cpuinfo2 { };
+
   py-dactyl = callPackage ../development/python-modules/py-dactyl { };
 
   py-datastruct = callPackage ../development/python-modules/py-datastruct { };
