607b78b8d6bb56599b8d2876efe4efac077dfa9b
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-24 21:14:23 +0200

    python3Packages.python-engineio: 4.13.3 -> 4.13.5
    Diff: https://github.com/miguelgrinberg/python-engineio/compare/v4.13.3...v4.13.5
    
    Changelog: https://github.com/miguelgrinberg/python-engineio/blob/v4.13.5/CHANGES.md

--- pkgs/development/python-modules/python-engineio/default.nix
+++ pkgs/development/python-modules/python-engineio/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "python-engineio";
-  version = "4.13.3";
+  version = "4.13.5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "miguelgrinberg";
     repo = "python-engineio";
     tag = "v${version}";
-    hash = "sha256-3KWhQE4STzwEtFzuhiTQZcc9a3lWoQlW74oHtbR4N6M=";
+    hash = "sha256-RK6biy3X5xEgltjV/EhpBcbUBeVcayCdYTEfy5cJPFw=";
   };
 
   build-system = [ setuptools ];
