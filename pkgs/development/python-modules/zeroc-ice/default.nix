0b8d1e228b6f7652b428cfbe663b50b2f1d61561
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 01:25:06 +0200

    python3Packages.zeroc-ice: 3.8.0.post1 -> 3.8.2
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/zeroc-ice/default.nix
+++ pkgs/development/python-modules/zeroc-ice/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "zeroc-ice";
-  version = "3.8.0.post1";
+  version = "3.8.2";
   pyproject = true;
 
   # Upstream PR: https://github.com/zeroc-ice/ice/pull/2910
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "zeroc_ice";
     inherit version;
-    hash = "sha256-SwR/4BSH/8+cYWVKA7aE3bnFOL52fWzynFehZr5VC9c=";
+    hash = "sha256-06PQiAWdbztd6WbrLFUO7DNDKMXwxGuaPZ3DBX6XXcM=";
   };
 
   build-system = [ setuptools ];
