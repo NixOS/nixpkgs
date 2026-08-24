f57561f628e18fa7efd495493a0de9726702f782
nicoo <nicoo@debian.org>
2026-08-24 19:54:37 +0000

    sdroxide: work around bug in fetchCargoVendor

--- pkgs/by-name/sd/sdroxide/package.nix
+++ pkgs/by-name/sd/sdroxide/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   fetchFromGitHub,
+  fetchurl,
   rustPlatform,
   pkgconf,
   alsa-lib,
@@ -15,6 +16,18 @@ rustPlatform.buildRustPackage (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-obVqYyrZRzKpt2/LU0T6XPg8wa6GdegLy3NykAHCHl0=";
   };
+
+  # HACK: the deep_filter crate include_bytes! from its workspace
+  #  (but outside its directory) which fetchCargoVendor cannot handle
+  prePatch = let
+    model = fetchurl {
+      url = "https://github.com/Rikorose/DeepFilterNet/raw/978576aa8400552a4ce9730838c635aa30db5e61/models/DeepFilterNet3_onnx.tar.gz";
+      hash = "sha256-yU2R9wkRAByUbg+rtKqa3DcEX0WgO1YAjLDIJEy2NhY=";
+    };
+  in ''
+    mkdir ../sdroxide-1.5.0-vendor/source-git-0/models
+    ln -s ${model} ../sdroxide-1.5.0-vendor/source-git-0/models/DeepFilterNet3_onnx.tar.gz
+  '';
 
   cargoHash = "sha256-RChkuoXZ/Ex45P+D+9t2ZQ2JPM3O06DpkFyrSCOe/9s=";
 
