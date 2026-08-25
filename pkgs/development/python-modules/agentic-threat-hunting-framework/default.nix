fb55523cc90a3abdb429e53934fd27c4d2007260
Michael Daniels <mdaniels5757@gmail.com>
2026-08-25 18:12:30 -0400

    fixup! python3Packages.agentic-threat-hunting-framework: 0.18.0 -> 0.19.0

--- pkgs/development/python-modules/agentic-threat-hunting-framework/default.nix
+++ pkgs/development/python-modules/agentic-threat-hunting-framework/default.nix
@@ -22,7 +22,7 @@ buildPythonPackage (finalAttrs: {
   src = fetchFromGitHub {
     owner = "Nebulock-Inc";
     repo = "agentic-threat-hunting-framework";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-IUOn3q6SA2P77/Bt7Hr21NglgjWhyNBOA7c2lCzUTN0=";
   };
 
