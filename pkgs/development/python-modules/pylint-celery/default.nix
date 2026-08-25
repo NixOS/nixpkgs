9626bfd0b38b188d574ab5a4f24ce0616092ac9e
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 22:01:49 +0200

    python3Packages.pylint-celery: modernize

--- pkgs/development/python-modules/pylint-celery/default.nix
+++ pkgs/development/python-modules/pylint-celery/default.nix
@@ -1,35 +1,39 @@
 {
+  lib,
   buildPythonPackage,
   fetchFromGitHub,
-  isPy3k,
-  lib,
-
-  # pythonPackages
+  pylint,
   pylint-plugin-utils,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pylint-celery";
   version = "0.3";
-  format = "setuptools";
-  disabled = !isPy3k;
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "PyCQA";
     repo = "pylint-celery";
-    rev = version;
-    sha256 = "05fhwraq12c2724pn4py1bjzy5rmsrb1x68zck73nlp5icba6yap";
+    tag = finalAttrs.version;
+    hash = "sha256-V3mjFovlUjvOZB+ZHlbWNRf/5Qr+EnuJOIKJgFXm0BU=";
   };
 
-  propagatedBuildInputs = [ pylint-plugin-utils ];
+  build-system = [ setuptools ];
+
+  buildInputs = [ pylint ];
+
+  dependencies = [ pylint-plugin-utils ];
 
   # Testing requires a very old version of pylint, incompatible with other dependencies
   doCheck = false;
 
+  pythonImportsCheck = [ "pylint_celery" ];
+
   meta = {
     description = "Pylint plugin to analyze Celery applications";
     homepage = "https://github.com/PyCQA/pylint-celery";
-    license = lib.licenses.gpl2;
+    license = lib.licenses.gpl2Only;
     maintainers = with lib.maintainers; [ kamadorueda ];
   };
-}
+})
