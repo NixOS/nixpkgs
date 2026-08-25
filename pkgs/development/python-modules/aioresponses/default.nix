944f2aedeffc55c5ccee8dc97816525e5fb35cbd
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 16:53:53 +0200

    fixup! python3Packages.aioresponses: 0.7.8 -> 0.7.9

--- pkgs/development/python-modules/aioresponses/default.nix
+++ pkgs/development/python-modules/aioresponses/default.nix
@@ -12,16 +12,17 @@
 
   # tests
   ddt,
+  pytest-asyncio,
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aioresponses";
   version = "0.7.9";
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-Hc+iiTj8AG8Eapg4OnwHrBgL56SSwe1Vf1zXsIBTV9M=";
   };
 
@@ -31,25 +32,20 @@ buildPythonPackage rec {
     ./aiohttp-3.14-compat.patch
   ];
 
-  postPatch = ''
-    # https://github.com/pnuckowski/aioresponses/pull/278
-    substituteInPlace aioresponses/core.py \
-      --replace-fail asyncio.iscoroutinefunction inspect.iscoroutinefunction
-  '';
-
-  nativeBuildInputs = [
+  build-system = [
     pbr
     setuptools
   ];
 
-  propagatedBuildInputs = [ aiohttp ];
-
-  pythonImportsCheck = [ "aioresponses" ];
+  dependencies = [ aiohttp ];
 
   nativeCheckInputs = [
     ddt
+    pytest-asyncio
     pytestCheckHook
   ];
+
+  pythonImportsCheck = [ "aioresponses" ];
 
   disabledTests = [
     # Skip tests which make requests to httpbin.org
@@ -61,6 +57,8 @@ buildPythonPackage rec {
   meta = {
     description = "Helper to mock/fake web requests in python aiohttp package";
     homepage = "https://github.com/pnuckowski/aioresponses";
+    changelog = "https://github.com/pnuckowski/aioresponses/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit;
+    maintainers = [ ];
   };
-}
+})
