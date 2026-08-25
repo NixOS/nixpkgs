78606619c9b173010225a870facefba53e0ef05a
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 15:46:37 +0200

    fixup! python3Packages.prov: 2.1.1 -> 2.5.3

--- pkgs/development/python-modules/prov/default.nix
+++ pkgs/development/python-modules/prov/default.nix
@@ -2,41 +2,64 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  hypothesis,
   lxml,
+  matplotlib,
   networkx,
+  pydot,
+  pytestCheckHook,
   python-dateutil,
   rdflib,
-  pydot,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "prov";
   version = "2.5.3";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-tNI2zwHDszvf331fZ68k0xxHFvJZ4PtzHo36zz1URp4=";
   };
 
-  propagatedBuildInputs = [
-    lxml
-    networkx
-    python-dateutil
-    rdflib
-  ];
+  build-system = [ setuptools ];
 
-  nativeCheckInputs = [ pydot ];
+  dependencies = [ python-dateutil ];
 
-  # Multiple tests are out-dated and failing
-  doCheck = false;
+  optional-dependencies = {
+    dot = [
+      networkx
+      pydot
+    ];
+    graph = [ networkx ];
+    rdf = [ rdflib ];
+    plot = [
+      matplotlib
+      networkx
+      pydot
+    ];
+    xml = [ lxml ];
+  };
+
+  nativeCheckInputs = [
+    hypothesis
+    pytestCheckHook
+  ]
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
 
   pythonImportsCheck = [ "prov" ];
+
+  preCheck = ''
+    export PATH="$PATH:$out/bin";
+  '';
 
   meta = {
     description = "Python library for W3C Provenance Data Model (PROV)";
     homepage = "https://github.com/trungdong/prov";
+    changelog = "https://github.com/trungdong/prov/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ashgillman ];
+    mainProgram = "prov-convert";
   };
-}
+})
