1c0485361e60d5c94ff164404acee59756cae35d
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 13:09:44 +0200

    python3Packages.jupyter-client: migrate to finalAttrs

--- pkgs/development/python-modules/jupyter-client/default.nix
+++ pkgs/development/python-modules/jupyter-client/default.nix
@@ -11,14 +11,14 @@
   typing-extensions,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "jupyter-client";
   version = "8.9.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "jupyter_client";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-pY9zDdnnKLoWuh1i68z3/+Hrvbzk6Vz66UG3Mhrh9Po=";
   };
 
@@ -41,8 +41,8 @@ buildPythonPackage rec {
   meta = {
     description = "Jupyter protocol implementation and client libraries";
     homepage = "https://github.com/jupyter/jupyter_client";
-    changelog = "https://github.com/jupyter/jupyter_client/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/jupyter/jupyter_client/blob/v${finalAttrs.version}/CHANGELOG.md";
     license = lib.licenses.bsd3;
     teams = [ lib.teams.jupyter ];
   };
-}
+})
