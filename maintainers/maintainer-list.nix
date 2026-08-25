468cb2ca9f0217c120adea2382b35608d0f4f45f
fin444 <fin444@users.noreply.github.com>
2026-08-25 19:37:09 -0400

    maintainers: add fin444

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9329,6 +9329,12 @@
     githubId = 41450706;
     name = "fin-w";
   };
+  fin444 = {
+    email = "fin444_dev@proton.me";
+    github = "fin444";
+    githubId = 24360839;
+    name = "fin444";
+  };
   findus = {
     email = "findus-nix@trsk.cc";
     github = "findus";
