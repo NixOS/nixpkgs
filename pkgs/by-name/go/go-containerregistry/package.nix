d8e6fb3764ed3e916357989b6c2548f4996870a1
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-25 20:28:31 +0000

    go-containerregistry: 0.21.9 -> 0.22.0

--- pkgs/by-name/go/go-containerregistry/package.nix
+++ pkgs/by-name/go/go-containerregistry/package.nix
@@ -15,13 +15,13 @@ in
 
 buildGoModule (finalAttrs: {
   pname = "go-containerregistry";
-  version = "0.21.9";
+  version = "0.22.0";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "go-containerregistry";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-D4N8ONuhoajYwb0SOtvwbrxI/st86KI4YXd9X1moKKQ=";
+    sha256 = "sha256-RA+LfxVI5vVspCQTlLSgFxxfp9JnGCFBQlY6A9DSkH4=";
   };
   vendorHash = null;
 
