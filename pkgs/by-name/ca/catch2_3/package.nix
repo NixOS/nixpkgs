525bfa71887eaa84971488b19bbecdfb6522fb2e
lukas-sgx <lukas.soigneux@epitech.eu>
2026-08-25 16:33:49 +0000

    catch2_3: 3.15.3 -> 3.16.0

--- pkgs/by-name/ca/catch2_3/package.nix
+++ pkgs/by-name/ca/catch2_3/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "catch2";
-  version = "3.15.3";
+  version = "3.16.0";
 
   src = fetchFromGitHub {
     owner = "catchorg";
     repo = "Catch2";
     tag = "v${version}";
-    hash = "sha256-ZuH3tUWNklq0bKp0Yu9w3L5FNsQwUxe6lyGBv4M6U1E=";
+    hash = "sha256-vvPZTQzLNGIcdDbuo0w6sQvxzLdKFCl2LNwcHu0d5I8=";
   };
 
   patches = lib.optionals stdenv.cc.isClang [
