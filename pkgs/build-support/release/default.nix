ba6224fc6f6f4880f620bca5ad1143b13ef7823e
Vladimír Čunát <v@cunat.cz>
2026-08-25 18:52:33 +0200

    Revert "top-level/release.nix: use named constituents in aggregates"

--- pkgs/build-support/release/default.nix
+++ pkgs/build-support/release/default.nix
@@ -124,16 +124,12 @@ rec {
       name,
       constituents,
       meta ? { },
-      # Interpret string constituents as fnmatch patterns matched
-      # against all job names of the jobset.
-      globConstituents ? false,
     }:
     pkgs.runCommand name
       {
         inherit constituents meta;
         preferLocalBuild = true;
         _hydraAggregate = true;
-        _hydraGlobConstituents = globConstituents;
       }
       ''
         mkdir -p $out/nix-support
