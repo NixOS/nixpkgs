8234854b81d28c82948e1976faf9e70c5c88eef3
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2026-08-25 17:48:40 -0400

    nixos/fittrackee: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1699,6 +1699,7 @@
   ./services/web-apps/filebrowser.nix
   ./services/web-apps/firefly-iii-data-importer.nix
   ./services/web-apps/firefly-iii.nix
+  ./services/web-apps/fittrackee.nix
   ./services/web-apps/flarum.nix
   ./services/web-apps/fluidd.nix
   ./services/web-apps/freescout.nix
