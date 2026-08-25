3647038b497545fff4af644b631558bce4a130b3
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 11:00:12 +0200

    fixup! python3Packages.lingua: 4.15.0 -> 4.16.2

--- pkgs/development/python-modules/lingua/default.nix
+++ pkgs/development/python-modules/lingua/default.nix
@@ -1,32 +1,47 @@
 {
   lib,
-  fetchPypi,
   buildPythonPackage,
-  flit-core,
-  polib,
+  chameleon,
   click,
+  fetchFromGitHub,
+  polib,
+  pyprojectVersionPatchHook,
+  pytestCheckHook,
+  uv-build,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "lingua";
   version = "4.16.2";
   pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-seXLu+zUCv057z1Py0Dk4/yblrwOBD5wiESm0ibuVL0=";
+  src = fetchFromGitHub {
+    owner = "wichert";
+    repo = "lingua";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-C/F676BoiZDNy8BhP0I2Z2Bh5gdMztyqo4zOL88Jw9Q=";
   };
 
   postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build>=0.10.5,<0.11.0" "uv_build"
+
     substituteInPlace src/lingua/extract.py \
       --replace-fail SafeConfigParser ConfigParser
   '';
 
-  nativeBuildInputs = [ flit-core ];
+  build-system = [ uv-build ];
 
-  propagatedBuildInputs = [
+  nativeBuildInputs = [ pyprojectVersionPatchHook ];
+
+  dependencies = [
     click
     polib
+  ];
+
+  nativeCheckInputs = [
+    chameleon
+    pytestCheckHook
   ];
 
   pythonImportsCheck = [ "lingua" ];
@@ -34,7 +49,8 @@ buildPythonPackage rec {
   meta = {
     description = "Translation toolset";
     homepage = "https://github.com/wichert/lingua";
+    changelog = "https://github.com/wichert/lingua/releases/tag/v${finalAttrs.src.tag}";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ np ];
   };
-}
+})
