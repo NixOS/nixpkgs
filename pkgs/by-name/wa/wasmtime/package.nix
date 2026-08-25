e8317f8e7955ea5c7e71a35a2a2b9bcf45225097
winston <hey@winston.sh>
2026-08-25 17:22:38 +0200

    wasmtime: 48.0.0 -> 48.0.1

--- pkgs/by-name/wa/wasmtime/package.nix
+++ pkgs/by-name/wa/wasmtime/package.nix
@@ -22,9 +22,9 @@ let
       cargoHash = "sha256-2Nwcw3Z4/cbkRcG2hJ2/1PcXZf3vYB9NGUlr7pxdqzU=";
     };
     main = {
-      version = "48.0.0";
-      hash = "sha256-WGnDC7iTEK2eLD1rPIy2jgbrsamWT7fElRwoy+EZ0dg=";
-      cargoHash = "sha256-cYv+Ml8xgukxwnHVge6swtC0Ul5Vt2+diUini4C61eo=";
+      version = "48.0.1";
+      hash = "sha256-nvDiKIyo0/BmwjPTfNGIX1kSVVJQbmjjT4qed0dr8cc=";
+      cargoHash = "sha256-NVea5PiuFTr1NrIcyEJSq0gFf2XkDZj/VKubHwO34U4=";
     };
   };
   source = sources.${variant};
