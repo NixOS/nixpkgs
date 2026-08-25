48e0c1dff93d9b1917484690c0e8e9ec1e416a2c
Sh0rtRound <97189175+Sh0rtRound-wq@users.noreply.github.com>
2026-08-25 18:48:43 +0000

    nix-easy-search: init at 1.0.0

---
+++ pkgs/by-name/ni/nix-easy-search/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule rec {
+  pname = "nix-easy-search";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "Sh0rtRound-wq";
+    repo = "NixEasySearch";
+    rev = "v${version}";
+    hash = "sha256-LvWXZlTUVVJpcjKdVDojyuw246RgOm9dDh9LgWAT49I=";
+  };
+
+  vendorHash = null;
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  postInstall = ''
+    mv $out/bin/nix-easy-search $out/bin/nes
+  '';
+
+  meta = {
+    description = "Fast NixOS package search CLI with clean output";
+    homepage = "https://github.com/Sh0rtRound-wq/NixEasySearch";
+    license = lib.licenses.mit;
+    mainProgram = "nes";
+    maintainers = [ ];
+  };
+}
