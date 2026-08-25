84e5bb992b119ee23b390c4599bb4cb23b25e551
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 09:16:40 +0200

    fixup! python3Packages.imagesize: 2.0.0 -> 2.0.1

--- pkgs/development/python-modules/imagesize/default.nix
+++ pkgs/development/python-modules/imagesize/default.nix
@@ -2,21 +2,28 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "imagesize";
   version = "2.0.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-srpqTepIen681TJI00dqykSdMNsSot3l4MXKliT9d+U=";
   };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "imagesize" ];
 
   meta = {
     description = "Getting image size from png/jpeg/jpeg2000/gif file";
     homepage = "https://github.com/shibukawa/imagesize_py";
+    changelog = "https://github.com/shibukawa/imagesize_py/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit;
+    maintainers = [ ];
   };
-}
+})
