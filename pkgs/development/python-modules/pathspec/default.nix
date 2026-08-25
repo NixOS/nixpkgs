cf6a587939cf1e7fc21b7a43c16796a49759b373
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-25 09:45:18 +0200

    python3Packages.pathspec: modernize

--- pkgs/development/python-modules/pathspec/default.nix
+++ pkgs/development/python-modules/pathspec/default.nix
@@ -12,21 +12,21 @@
   yamllint,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pathspec";
   version = "1.1.1";
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-F9tezVJBBKEg4XOBTJA2epapjQfEWy4QwvORn/+Rv1o=";
   };
 
-  nativeBuildInputs = [ flit-core ];
+  build-system = [ flit-core ];
 
   pythonImportsCheck = [ "pathspec" ];
 
-  checkInputs = [ unittestCheckHook ];
+  nativeCheckInputs = [ unittestCheckHook ];
 
   passthru.tests = {
     inherit
@@ -40,8 +40,8 @@ buildPythonPackage rec {
   meta = {
     description = "Utility library for gitignore-style pattern matching of file paths";
     homepage = "https://github.com/cpburnz/python-path-specification";
-    changelog = "https://github.com/cpburnz/python-pathspec/blob/v${version}/CHANGES.rst";
+    changelog = "https://github.com/cpburnz/python-pathspec/blob/v${finalAttrs.version}/CHANGES.rst";
     license = lib.licenses.mpl20;
     maintainers = [ ];
   };
-}
+})
