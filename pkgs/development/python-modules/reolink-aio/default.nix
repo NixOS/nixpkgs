d0c571f515b4b9e2dff45f083fbd4afb0a959c09
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-26 16:05:31 +0200

    python3Packages.reolink-aio: 0.21.9 -> 0.21.13
    Diff: https://github.com/starkillerOG/reolink_aio/compare/0.21.9...0.21.13
    
    Changelog: https://github.com/starkillerOG/reolink_aio/releases/tag/0.21.13

--- pkgs/development/python-modules/reolink-aio/default.nix
+++ pkgs/development/python-modules/reolink-aio/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "reolink-aio";
-  version = "0.21.9";
+  version = "0.21.13";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "starkillerOG";
     repo = "reolink_aio";
     tag = finalAttrs.version;
-    hash = "sha256-t1mmmHhh5J9i28ISVbZp8PbRQTXv8YUcAVl6ZtSCF1o=";
+    hash = "sha256-YnzP4ArMoyMaaAI90Hz5wEKHcTLY3ER/Ar4u4MJ2gFc=";
   };
 
   build-system = [ setuptools ];
