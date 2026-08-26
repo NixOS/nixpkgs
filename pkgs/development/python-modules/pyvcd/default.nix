10a1d7bbc7e8bfe99493c60c1624bf85bef16406
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-27 00:50:14 +0200

    fixup! python3Packages.pyvcd: 0.4.1 -> 0.5.0

--- pkgs/development/python-modules/pyvcd/default.nix
+++ pkgs/development/python-modules/pyvcd/default.nix
@@ -2,34 +2,31 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  setuptools-scm,
-  six,
   pytestCheckHook,
+  uv-build,
 }:
 
 buildPythonPackage rec {
-  version = "0.5.0";
-  format = "setuptools";
   pname = "pyvcd";
+  version = "0.5.0";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-luPHuUCeY/WOtLXCKwF/nzQS3b38c0zsLh361cKag98=";
   };
 
-  buildInputs = [ setuptools-scm ];
-
-  propagatedBuildInputs = [ six ];
+  build-system = [ uv-build ];
 
   nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "vcd" ];
 
   meta = {
     description = "Python package for writing Value Change Dump (VCD) files";
     homepage = "https://github.com/SanDisk-Open-Source/pyvcd";
     changelog = "https://github.com/SanDisk-Open-Source/pyvcd/blob/${version}/CHANGELOG.rst";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [
-      sb0
-    ];
+    maintainers = with lib.maintainers; [ sb0 ];
   };
 }
