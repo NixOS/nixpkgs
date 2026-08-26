33d84624d07c94c60342488357390c6ae5f33c18
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-27 00:15:41 +0200

    fixup! python3Packages.datamodeldict: 0.9.9 -> 0.9.10

--- pkgs/development/python-modules/datamodeldict/default.nix
+++ pkgs/development/python-modules/datamodeldict/default.nix
@@ -1,31 +1,41 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
+  pytestCheckHook,
+  uv-build,
   xmltodict,
 }:
 
 buildPythonPackage rec {
   pname = "datamodeldict";
   version = "0.9.10";
-  format = "setuptools";
+  pyproject = true;
 
-  src = fetchPypi {
-    pname = "DataModelDict";
-    inherit version;
-    hash = "sha256-1kBPRoLaunpPsHq2cKjvJ3r9j9SSY0XJHVmGcZ29BOc=";
+  src = fetchFromGitHub {
+    owner = "usnistgov";
+    repo = "DataModelDict";
+    tag = "v${version}";
+    hash = "sha256-4tyf3zlzxbtHkvADP+Kmw3/XMugAGi4FNO0qM16m8DU=";
   };
 
-  propagatedBuildInputs = [ xmltodict ];
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build>=0.11.26,<0.12" "uv_build"
+  '';
 
-  # no tests
-  doCheck = false;
+  build-system = [ uv-build ];
+
+  dependencies = [ xmltodict ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "DataModelDict" ];
 
   meta = {
     description = "Class allowing for data models equivalently represented as Python dictionaries, JSON, and XML";
     homepage = "https://github.com/usnistgov/DataModelDict/";
+    changelog = "https://github.com/usnistgov/DataModelDict/releases/tag/v${version}";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
