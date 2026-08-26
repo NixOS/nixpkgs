3ae9c6bfdcb5c8d43df69c38c4706eb52d3598f4
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-26 20:08:46 +0200

    python3Packages.json-repair: 0.55.2 -> 0.63.4
    Changelog: https://github.com/mangiucugna/json_repair/releases/tag/v0.63.4

--- pkgs/development/python-modules/json-repair/default.nix
+++ pkgs/development/python-modules/json-repair/default.nix
@@ -2,25 +2,37 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  setuptools,
+  jsonschema,
+  pydantic,
   pytestCheckHook,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "json-repair";
-  version = "0.55.2";
+  version = "0.63.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mangiucugna";
     repo = "json_repair";
-    tag = "v${version}";
-    hash = "sha256-CzoGu6JNOaqdLZK4DyDUv+TMIA+k9AlZZy1fKnpMbkE=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-VRRtL8X9egdqdz6Celw1CGGTuWd9YMVbq/uHwHtmv64=";
   };
 
   build-system = [ setuptools ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  optional-dependencies = {
+    schema = [
+      jsonschema
+      pydantic
+    ];
+  };
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ]
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
 
   disabledTestPaths = [
     # Disable benchmark tests
@@ -32,9 +44,9 @@ buildPythonPackage rec {
   meta = {
     description = "Module to repair invalid JSON, commonly used to parse the output of LLMs";
     homepage = "https://github.com/mangiucugna/json_repair/";
-    changelog = "https://github.com/mangiucugna/json_repair/releases/tag/${src.tag}";
+    changelog = "https://github.com/mangiucugna/json_repair/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ greg ];
     mainProgram = "json_repair";
   };
-}
+})
