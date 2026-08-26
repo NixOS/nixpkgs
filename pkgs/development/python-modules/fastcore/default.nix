28102d6050f15adbca60c5b98f965734eb2325c0
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-26 16:44:52 +0200

    python3Packages.fastcore: 1.14.5 -> 2.2.16
    Diff: https://github.com/fastai/fastcore/compare/1.14.5...2.2.16
    
    Changelog: https://github.com/fastai/fastcore/blob/2.2.16/CHANGELOG.md

--- pkgs/development/python-modules/fastcore/default.nix
+++ pkgs/development/python-modules/fastcore/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "fastcore";
-  version = "1.14.5";
+  version = "2.2.16";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "fastai";
     repo = "fastcore";
     tag = finalAttrs.version;
-    hash = "sha256-lWCUVgu/4Udv3r5BPbBoyn+VyMJYWLWvlcPP89pcC+w=";
+    hash = "sha256-MWQl2O5NqxVzWcCU9dewUdh6iT8zFSMDZey/YogJBik=";
   };
 
   build-system = [ setuptools ];
