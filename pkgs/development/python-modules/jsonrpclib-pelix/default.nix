9e5cc8229d03bb520bd6850f570d7a790a3e2d54
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-26 23:18:19 +0200

    fixup! python3Packages.jsonrpclib-pelix: 1.1.0 -> 1.2.0

--- pkgs/development/python-modules/jsonrpclib-pelix/default.nix
+++ pkgs/development/python-modules/jsonrpclib-pelix/default.nix
@@ -1,15 +1,14 @@
 {
-  buildPythonPackage,
-  hatchling,
-  fetchPypi,
   lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "jsonrpclib-pelix";
   version = "1.2.0";
   pyproject = true;
-  build-system = [ hatchling ];
 
   src = fetchPypi {
     pname = "jsonrpclib_pelix";
@@ -17,11 +16,16 @@ buildPythonPackage rec {
     hash = "sha256-NTtmcHwPxCY+3I/Wu9Rxt67egjqD5NvxYUsMg67iDxg=";
   };
 
+  build-system = [ setuptools ];
+
   doCheck = false; # test_suite="tests" in setup.py but no tests in pypi.
+
+  pythonImportsCheck = [ "jsonrpclib" ];
 
   meta = {
     description = "JSON RPC client library - Pelix compatible fork";
     homepage = "https://pypi.org/project/jsonrpclib-pelix/";
+    changelog = "https://github.com/tcalmant/jsonrpclib/releases/tag/v${version}";
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
