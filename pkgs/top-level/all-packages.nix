2e6362e7030beb1da64fd619b1cae41c71b377a6
teutat3s <10206665+teutat3s@users.noreply.github.com>
2026-08-26 20:59:23 +0200

    electron-source.electron_44: init at 44.0.0
    - Changelog: https://github.com/electron/electron/releases/tag/v44.0.0
    
    - Diff: https://github.com/electron/electron/compare/refs/tags/v44.0.0...v43.4.1
    
    Co-Authored-By: Yureka <yuka@yuka.dev>
    (cherry picked from commit 69bdb34679702f77db1e5876f5b14083a6c348bb)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5168,6 +5168,10 @@ with pkgs;
           src = electron-source.electron_43;
           bin = electron_43-bin;
         };
+        electron_44 = getElectronPkg {
+          src = electron-source.electron_44;
+          bin = electron_44-bin;
+        };
       }
     )
     electron_38
@@ -5176,6 +5180,7 @@ with pkgs;
     electron_41
     electron_42
     electron_43
+    electron_44
     ;
   electron = electron_43;
   electron-bin = electron_43-bin;
