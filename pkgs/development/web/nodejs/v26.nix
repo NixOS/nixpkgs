227b7ffc599fa44b587561f7e0691b56d2d43254
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-08-27 00:11:55 +0200

    nodejs_26: 26.8.0 -> 26.8.1

--- pkgs/development/web/nodejs/v26.nix
+++ pkgs/development/web/nodejs/v26.nix
@@ -23,8 +23,8 @@ let
       [ ];
 in
 buildNodejs {
-  version = "26.8.0";
-  sha256 = "2287613caa71f9ab8dff4cbec7da32f0fae3e8807a6876a77764acc16d4cca4c";
+  version = "26.8.1";
+  sha256 = "d1698832a1a10f050cdda044a3e3d6a748246811e2e7bc89ba9a8bd693dc45f2";
   patches =
     (lib.optional (!(stdenv.hostPlatform.emulatorAvailable buildPackages)) (fetchpatch2 {
       url = "https://raw.githubusercontent.com/buildroot/buildroot/2f0c31bffdb59fb224387e35134a6d5e09a81d57/package/nodejs/nodejs-src/0003-include-obj-name-in-shared-intermediate.patch";
