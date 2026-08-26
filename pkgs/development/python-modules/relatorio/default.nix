fc4addd5e212ed313e97995a74b2750b6f5a0a16
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-27 01:17:12 +0200

    fixup! python3Packages.relatorio: 0.11.1 -> 0.12.1

--- pkgs/development/python-modules/relatorio/default.nix
+++ pkgs/development/python-modules/relatorio/default.nix
@@ -7,20 +7,26 @@
   pyyaml,
   python-magic,
   pytestCheckHook,
+  hatch-tryton,
+  hatchling,
 }:
 
 buildPythonPackage rec {
   pname = "relatorio";
   version = "0.12.1";
-
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-Jh5HxN8Ey2PbNL1TQ3cHLHhB1vlfndmGwTlD2klR/U4=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [
+    hatch-tryton
+    hatchling
+  ];
+
+  dependencies = [
     genshi
     lxml
   ];
@@ -38,11 +44,11 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "relatorio" ];
 
   meta = {
+    description = "Templating library able to output odt and pdf files";
     homepage = "https://relatorio.tryton.org/";
     changelog = "https://hg.tryton.org/relatorio/file/${version}/CHANGELOG";
-    description = "Templating library able to output odt and pdf files";
-    mainProgram = "relatorio-render";
     maintainers = with lib.maintainers; [ johbo ];
     license = lib.licenses.gpl2Plus;
+    mainProgram = "relatorio-render";
   };
 }
