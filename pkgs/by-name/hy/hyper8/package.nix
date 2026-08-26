6a0b31894d658d06771a1dc2fc2dbd3b844a383e
Darren Rambaud <225436867+debtquity@users.noreply.github.com>
2026-08-26 17:56:25 -0500

    hyper8: fix build failure on `*-linux` platforms
    * explicitly define openssl libraries rather than depend on system
    * building/compiling this package using `rustPlatform` requires wiring
      up these dependencies -- pkg-config, glib, gtk3, libsoup_3,
      webkitgtk_4_1 -- during build phase on linux. However, it's not
      required on darwin systems. In fact the entire derivation will compile
      on darwin without `depsBuildBuild` defined.
    
    Resolves: #556831

--- pkgs/by-name/hy/hyper8/package.nix
+++ pkgs/by-name/hy/hyper8/package.nix
@@ -2,6 +2,14 @@
   lib,
   rustPlatform,
   fetchFromCodeberg,
+  versionCheckHook,
+  stdenv,
+  openssl,
+  pkg-config,
+  glib,
+  gtk3,
+  libsoup_3,
+  webkitgtk_4_1,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
@@ -15,9 +23,31 @@ rustPlatform.buildRustPackage (finalAttrs: {
     hash = "sha256-pvtQPL/hPgoKDLYWC/IL04db7Q/FUlgiExthu4xBQEw=";
   };
 
+  cargoHash = "sha256-AQAWGmzixDFfL7wqJJXCvNSYojVtYHRP0zqdj0C8JRE=";
+
   __structuredAttrs = true;
 
-  cargoHash = "sha256-AQAWGmzixDFfL7wqJJXCvNSYojVtYHRP0zqdj0C8JRE=";
+  env = {
+    OPENSSL_DIR = lib.getDev openssl;
+    OPENSSL_LIB_DIR = "${lib.getLib openssl}/lib";
+  };
+
+  depsBuildBuild = [
+    pkg-config
+    glib
+    gtk3
+    libsoup_3
+  ]
+  ++ lib.optionals (stdenv.hostPlatform.isLinux) [
+    webkitgtk_4_1 # broken on darwin systems
+  ];
+
+  buildInputs = [
+    openssl
+  ];
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
 
   meta = {
     homepage = "https://hyper8.org";
