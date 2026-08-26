76952c433e226a29f0d984d8cb220de6914f8bf7
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-26 20:26:44 +0200

    python3Packages.korean-lunar-calendar: fix build

--- pkgs/development/python-modules/korean-lunar-calendar/default.nix
+++ pkgs/development/python-modules/korean-lunar-calendar/default.nix
@@ -2,12 +2,13 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "korean-lunar-calendar";
   version = "0.4.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit version;
@@ -15,12 +16,15 @@ buildPythonPackage rec {
     hash = "sha256-vlbye8BZT9u997vgD1BKn5KaMeMRvX2buTVhtkWvrec=";
   };
 
+  build-system = [ setuptools ];
+
   # no real tests
   pythonImportsCheck = [ "korean_lunar_calendar" ];
 
   meta = {
     description = "Library to convert Korean lunar-calendar to Gregorian calendar";
     homepage = "https://github.com/usingsky/korean_lunar_calendar_py";
+    changelog = "https://github.com/usingsky/korean_lunar_calendar_py/releases/tag/v${version}";
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.ris ];
   };
