8a7e8fa5d700426e4f98c169f06ac0399fd12cac
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-27 22:37:03 +0200

    fixup! python3Packages.unearth: 0.18.2 -> 0.18.3

--- pkgs/development/python-modules/unearth/default.nix
+++ pkgs/development/python-modules/unearth/default.nix
@@ -2,7 +2,6 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  fetchpatch,
   packaging,
   pdm-backend,
   httpx,
@@ -23,22 +22,6 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-FAZ88RQckG94fW2dBwy8/dRD/RBYrsqmUM6VGaoQ29w=";
   };
-
-  patches = [
-    # https://github.com/frostming/unearth/pull/176
-    (fetchpatch {
-      name = "fix-packaging-26.0-changes.patch";
-      url = "https://github.com/frostming/unearth/commit/69ece0800edeefb1daf035bb0ee348e17a4393fd.patch";
-      hash = "sha256-t/Ubv9qC1Fvh4JsnfVgOZO/O7ZpCGHugBUt9qAjnH8c=";
-      excludes = [ "pdm.lock" ];
-    })
-    # Remove when updating to the first release containing this fix.
-    (fetchpatch {
-      name = "CVE-2026-73030.patch";
-      url = "https://github.com/frostming/unearth/commit/6c78164e7bfa28b8b3d6f247b87e560692e3c8ba.patch";
-      hash = "sha256-OEf4YnpNhZcIWaFMSXQP0SA7kRV9FqKIpnkLbUrQj+4=";
-    })
-  ];
 
   build-system = [ pdm-backend ];
 
@@ -62,10 +45,10 @@ buildPythonPackage rec {
 
   meta = {
     description = "Utility to fetch and download Python packages";
-    mainProgram = "unearth";
     homepage = "https://github.com/frostming/unearth";
     changelog = "https://github.com/frostming/unearth/releases/tag/${version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ betaboon ];
+    mainProgram = "unearth";
   };
 }
