09f093721b932e04cfdd02d2edcc3607050df8f0
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-08-27 04:12:43 +0000

    python3Packages.numpy-typing-compat: 20251206.2.4 -> 20260602.2.5

--- pkgs/development/python-modules/numpy-typing-compat/default.nix
+++ pkgs/development/python-modules/numpy-typing-compat/default.nix
@@ -7,18 +7,14 @@
 }:
 buildPythonPackage (finalAttrs: {
   pname = "numpy-typing-compat";
-  version = "20251206.2.4";
+  version = "20260602.2.5";
   pyproject = true;
 
   src = fetchPypi {
     pname = "numpy_typing_compat";
     version = finalAttrs.version;
-    hash = "sha256-WYgtI6r/BUolNtqAVkASzc4zSHZXvk15xZJbuHBfyrw=";
+    hash = "sha256-GIWmeOmiRWSDntXRcRwAMXNft95/C17YjVUOXUWo1Pk=";
   };
-
-  postPatch = ''
-    substituteInPlace pyproject.toml --replace-fail "uv_build>=0.9,<0.10" "uv_build>=0.9,<=0.10"
-  '';
 
   build-system = [
     uv-build
