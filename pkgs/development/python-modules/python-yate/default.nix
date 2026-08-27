99389e601eb25006c1f195df07cf38b67307478f
Timo Gottszky <timogottszky+git@gmail.com>
2026-08-27 15:43:51 +0200

    python3Packages.python-yate: enable __structuredAttrs

--- pkgs/development/python-modules/python-yate/default.nix
+++ pkgs/development/python-modules/python-yate/default.nix
@@ -12,6 +12,8 @@ buildPythonPackage (finalAttrs: {
   version = "0.5.0";
   format = "setuptools";
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "eventphone";
     repo = "python-yate";
