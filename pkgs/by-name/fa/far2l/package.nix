996cc1a22f8e690a6e5b5dc3687d2f1a815bb479
Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>
2026-08-27 17:22:18 +0300

    far2l: switch to finalAttrs
    Signed-off-by: Sergey Makarov <12733495+SeTSeR@users.noreply.github.com>

--- pkgs/by-name/fa/far2l/package.nix
+++ pkgs/by-name/fa/far2l/package.nix
@@ -44,7 +44,7 @@
   python3Packages,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "far2l";
   version = "2.9.0-unstable-27-08-2026";
 
@@ -124,7 +124,7 @@ stdenv.mkDerivation rec {
 
   postInstall = ''
     wrapProgram $out/bin/far2l \
-      --prefix PATH : ${lib.makeBinPath runtimeDeps} \
+      --prefix PATH : ${lib.makeBinPath finalAttrs.runtimeDeps} \
       --suffix PATH : ${lib.makeBinPath [ xdg-utils ]}
     # Link 7z plugin
     echo "Linking 7z libraries..."
@@ -143,4 +143,4 @@ stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ smakarov ];
     platforms = lib.platforms.unix;
   };
-}
+})
