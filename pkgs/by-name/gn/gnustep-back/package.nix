2939bcd0413e0d84e38b284d34df353884680352
Andrew Benbow <andrew.benbow@posteo.com>
2026-08-27 12:47:57 +1200

    gnustep-back: allow patches for gershwin

--- pkgs/by-name/gn/gnustep-back/package.nix
+++ pkgs/by-name/gn/gnustep-back/package.nix
@@ -3,12 +3,14 @@
   clangStdenv,
   fetchzip,
   cairo,
+  fetchpatch,
   fontconfig,
   freetype,
   gnustep-gui,
   libxft,
   libxmu,
   pkg-config,
+  withGershwinPatches ? false,
   wrapGNUstepAppsHook,
 }:
 
@@ -32,6 +34,19 @@ clangStdenv.mkDerivation (finalAttrs: {
     freetype
     libxft
     libxmu
+  ];
+
+  patches = lib.optionals withGershwinPatches [
+    (fetchpatch {
+      name = "inter-bold.patch";
+      url = "https://raw.githubusercontent.com/gershwin-desktop/gershwin-developer/54f56923a7c47fc721c51e6195369a98f5e55661/Library/Patches/libs-back/inter-bold.patch";
+      sha256 = "sha256-jau7Npjy/aGfBK3SC0ub/ZJS2oz3rUIeUPZZaks5nho=";
+    })
+    (fetchpatch {
+      name = "net-wm-pid.patch";
+      url = "https://raw.githubusercontent.com/gershwin-desktop/gershwin-developer/5983da933e19dc6c84498501314cde0310961c73/Library/Patches/libs-back/net-wm-pid.patch";
+      sha256 = "sha256-hB6NobYKZIlhR728TDObU8CN/6Slzf7DawPHicZfE4o=";
+    })
   ];
 
   propagatedBuildInputs = [ gnustep-gui ];
