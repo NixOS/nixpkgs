659a103a21a775011a5410b9e0c33044c6aafcc3
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-08-28 00:13:48 +0200

    python3Packages.comfyui-workflow-templates-media-assets-01: 0.1.30 -> 0.1.35

--- pkgs/development/python-modules/comfyui-workflow-templates-media-assets-01/default.nix
+++ pkgs/development/python-modules/comfyui-workflow-templates-media-assets-01/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "comfyui-workflow-templates-media-assets-01";
-  version = "0.1.30";
+  version = "0.1.35";
   pyproject = true;
 
   src = fetchPypi {
     pname = "comfyui_workflow_templates_media_assets_01";
     inherit (finalAttrs) version;
-    hash = "sha256-5N6pN46RdJkKMVqCC/I6Zu0o7YS90V9y8pu10IryoMY=";
+    hash = "sha256-xrzUGrvjhWyUhYxs4H2QNIf5PsbEBXTsu4OU3eEkk6M=";
   };
 
   build-system = [ setuptools ];
