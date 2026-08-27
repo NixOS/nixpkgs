c310d5eb5374c8f4c85490973578e74338d65b25
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-27 12:53:48 +0000

    openttd-jgrpp: 0.73.1 -> 0.73.2

--- pkgs/by-name/op/openttd-jgrpp/package.nix
+++ pkgs/by-name/op/openttd-jgrpp/package.nix
@@ -7,13 +7,13 @@
 
 openttd.overrideAttrs (oldAttrs: rec {
   pname = "openttd-jgrpp";
-  version = "0.73.1";
+  version = "0.73.2";
 
   src = fetchFromGitHub {
     owner = "JGRennison";
     repo = "OpenTTD-patches";
     rev = "jgrpp-${version}";
-    hash = "sha256-L99u4J/yiaQB2TTP63gIX6N4OrVOuM4/1bpLkCzVvXU=";
+    hash = "sha256-FfR/H+EieZgVaGTJOQZrQpUMbJa/eXE37FWEYFD2sM0=";
   };
   patches = [ ];
 
