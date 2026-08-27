5f82789a4cd8f612f766ac8d40ee4d42f91e754d
Timo Gottszky <timogottszky+git@gmail.com>
2026-08-27 16:34:34 +0200

    python3Packages.mutesync: use finalAttrs

--- pkgs/development/python-modules/mutesync/default.nix
+++ pkgs/development/python-modules/mutesync/default.nix
@@ -6,13 +6,14 @@
   fetchPypi,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "mutesync";
   version = "0.0.2";
   format = "setuptools";
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "mutesync";
+    inherit (finalAttrs) version;
     sha256 = "1lz3q3q9lw8qxxb8jyrak77v6hkxwi39akyx96j8hd5jjaq2k5qc";
   };
 
@@ -32,4 +33,4 @@ buildPythonPackage rec {
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
