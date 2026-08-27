05e3304155398c7834f14e8de927de9b4fb7b0ce
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-08-28 00:10:35 +0200

    comfyui: 0.33.3 -> 0.34.1

--- pkgs/by-name/co/comfyui/package.nix
+++ pkgs/by-name/co/comfyui/package.nix
@@ -74,7 +74,7 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "comfyui";
-  version = "0.33.3";
+  version = "0.34.1";
 
   strictDeps = true;
   __structuredAttrs = true;
@@ -83,7 +83,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "Comfy-Org";
     repo = "ComfyUI";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-c3a5xBKusx3Xk26U421JrK9tqb27XTq7I9HBiNHP1/0=";
+    hash = "sha256-ep0ghTptdzn82a5Mwwgzu4Ka7v7PrNBb3pAWqrykdcM=";
   };
 
   nativeBuildInputs = [ makeBinaryWrapper ];
