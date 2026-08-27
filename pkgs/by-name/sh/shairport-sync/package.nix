d2b7742d50cb9d4b9f970ad5e144e07183760cbc
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-27 12:15:50 +0000

    shairport-sync: 5.2.2 -> 5.2.3

--- pkgs/by-name/sh/shairport-sync/package.nix
+++ pkgs/by-name/sh/shairport-sync/package.nix
@@ -56,13 +56,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "shairport-sync";
-  version = "5.2.2";
+  version = "5.2.3";
 
   src = fetchFromGitHub {
     repo = "shairport-sync";
     owner = "mikebrady";
     tag = finalAttrs.version;
-    hash = "sha256-hwm1B4Q85hF61hzv579L+VTEfE4gvEhsGvh9Xq7XCNE=";
+    hash = "sha256-P2QrWLDJ9kPAw2o79gcQpAm17h2wFWAYpWwEi1V32Kk=";
   };
 
   nativeBuildInputs = [
