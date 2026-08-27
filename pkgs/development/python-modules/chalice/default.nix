acee2e4eb15eaaf70eab8009c8876ef248033b20
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-27 15:51:39 +0200

    python3Packages.chalice: disable failing tests

--- pkgs/development/python-modules/chalice/default.nix
+++ pkgs/development/python-modules/chalice/default.nix
@@ -34,6 +34,8 @@ buildPythonPackage (finalAttrs: {
     hash = "sha256-c5xzgrxRFRlvgMnf/L8rhG7rYJLtuMvDZHYsPaHkdRs=";
   };
 
+  pythonRelaxDeps = [ "pip" ];
+
   build-system = [ setuptools ];
 
   dependencies = [
@@ -46,8 +48,6 @@ buildPythonPackage (finalAttrs: {
     # setuptools
     six
   ];
-
-  pythonRelaxDeps = [ "pip" ];
 
   nativeCheckInputs = [
     hypothesis
@@ -82,16 +82,34 @@ buildPythonPackage (finalAttrs: {
     "test_both_tar_bz2"
     # AssertionError
     "test_no_error_message_printed_on_empty_reqs_file"
+    # botocore.exceptions.ParamValidationError:
+    "test_can_create_kinesis_event_source"
+    "test_can_create_kinesis_event_source_batching_window"
+    "test_can_create_sqs_event_source"
+    "test_can_retry_create_sqs_event_source"
+    "test_can_delete_sqs_event_source"
+    "test_can_retry_delete_event_source"
+    "test_only_retry_settling_errors "
+    "test_can_retry_update_event_source"
+    "test_can_retry_update_event_source_batching_window "
+    "test_verify_event_source_current[queue-name-sqs-True]"
+    "test_verify_event_source_current[queue-name-not-sqs-False]"
+    "test_verify_event_source_current[not-queue-name-sqs-False]"
+    "test_verify_event_source_current[not-queue-name-not-sqs-False]"
+    "test_verify_event_source_arn_current"
+    "test_event_source_uuid_does_not_exist"
+    "test_event_source_does_not_exist"
+    "test_can_update_lambda_event_source"
   ];
 
   pythonImportsCheck = [ "chalice" ];
 
   meta = {
     description = "Python Serverless Microframework for AWS";
-    mainProgram = "chalice";
     homepage = "https://github.com/aws/chalice";
     changelog = "https://github.com/aws/chalice/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = [ ];
+    mainProgram = "chalice";
   };
 })
