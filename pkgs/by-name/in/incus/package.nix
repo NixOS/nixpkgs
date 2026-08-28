46dd8d8b99fa6f083c4c9996a904d6da7a0f2436
Adam C. Stephens <adam@valkor.net>
2026-08-27 22:47:01 -0400

    incus: loosen go requirement to 1.26.6

--- pkgs/by-name/in/incus/package.nix
+++ pkgs/by-name/in/incus/package.nix
@@ -2,7 +2,9 @@ import ./generic.nix {
   hash = "sha256-9q4YrumeCb8u0O6e0Ftisb33s2dz/DOdUO3JD05W8K0=";
   version = "7.4.0";
   vendorHash = "sha256-zplmn+JH/zEaQgo2xa5wzc6rCIb5tLIVlM1vYJpw9zQ=";
-  patches = fetchpatch2: [ ];
+  patches = fetchpatch2: [
+    ./go_1_26_6.patch
+  ];
   nixUpdateExtraArgs = [
     "--override-filename=pkgs/by-name/in/incus/package.nix"
   ];
