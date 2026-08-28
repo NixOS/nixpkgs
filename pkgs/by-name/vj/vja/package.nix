f741f743a9462f8403573ea31c5f14a64da4b139
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-28 08:53:29 +0000

    vja: 5.4.2 -> 6.0.2

--- pkgs/by-name/vj/vja/package.nix
+++ pkgs/by-name/vj/vja/package.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "vja";
-  version = "5.4.2";
+  version = "6.0.2";
   pyproject = true;
 
   src = fetchFromGitLab {
     owner = "ce72";
     repo = "vja";
     tag = finalAttrs.version;
-    hash = "sha256-7Q6NRsGzh5eWIkYpf84TYr065x+anHIOhDegrKKowF8=";
+    hash = "sha256-XtZQO1yo8iULM6Kz120BrwBm4u7YDcmNxY2c05ZeSUQ=";
   };
 
   build-system = [
