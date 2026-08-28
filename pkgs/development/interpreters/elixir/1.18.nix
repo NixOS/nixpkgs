6f8d036c58b3d45b8c88f0091b4e316c6ed713ee
Adam C. Stephens <adam@valkor.net>
2026-08-28 10:23:06 -0400

    beamPackages.elixir_1_18: 1.18.4 -> 1.18.5
    Changelog: https://github.com/elixir-lang/elixir/releases/tag/v1.18.5

--- pkgs/development/interpreters/elixir/1.18.nix
+++ pkgs/development/interpreters/elixir/1.18.nix
@@ -1,6 +1,6 @@
 import ./generic-builder.nix {
-  version = "1.18.4";
-  hash = "sha256-PwogI+HfRXy5M7Xn/KyDjm5vUquTBoGxliSV0A2AwSA=";
+  version = "1.18.5";
+  hash = "sha256-C7RXBjZZbdSgz4jdoOCKv8xfM95ChrYjXIIS/ahX+3Y=";
   # https://hexdocs.pm/elixir/1.18.0/compatibility-and-deprecations.html#between-elixir-and-erlang-otp
   minimumOTPVersion = "25";
 }
