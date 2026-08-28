16f22d7599350f442b6e1729b3b71b3650597485
Adam C. Stephens <adam@valkor.net>
2026-08-28 13:46:34 -0400

    beamPackages.mixRelease: extract mixNixDepsSetupHook

--- pkgs/development/beam-modules/mix-release.nix
+++ pkgs/development/beam-modules/mix-release.nix
@@ -21,6 +21,7 @@
   mixCompileHook,
   mixEscriptSetupHook,
   mixFodDepsSetupHook,
+  mixNixDepsSetupHook,
   mixReleaseSetupHook,
 }@inputs:
 
@@ -30,7 +31,6 @@ lib.extendMkDerivation {
   excludeDrvArgNames = [
     "compileFlags"
     "erlangCompilerOptions"
-    "mixNixDeps"
   ];
 
   extendDrvArgs =
@@ -125,8 +125,8 @@ lib.extendMkDerivation {
           ]
         ++ lib.optionals (escriptBinName != null) [ mixEscriptSetupHook ]
         ++ lib.optionals (escriptBinName == null) [ mixReleaseSetupHook ]
-        ++ (builtins.attrValues mixNixDeps)
-        ++ lib.optionals (mixFodDeps != null) [ mixFodDepsSetupHook ];
+        ++ lib.optionals (mixFodDeps != null) [ mixFodDepsSetupHook ]
+        ++ lib.optionals (mixNixDeps != { }) ([ mixNixDepsSetupHook ] ++ (builtins.attrValues mixNixDeps));
 
       buildInputs = [
         bashNonInteractive
@@ -175,20 +175,6 @@ lib.extendMkDerivation {
           #
           # Phoenix projects for example will need compile.phoenix.
           mix deps.compile --no-deps-check --skip-umbrella-children
-
-          # Symlink dependency sources. This is needed for projects that require
-          # access to the source of their dependencies. For example, Phoenix
-          # projects need javascript assets to build asset bundles.
-          ${lib.optionalString (mixNixDeps != { }) ''
-            mkdir -p deps
-
-            ${lib.concatMapAttrsStringSep "\n" (name: dep: ''
-              dep_path="deps/${name}"
-              if [ -d "${dep}/src" ]; then
-                ln -sv ${dep}/src $dep_path
-              fi
-            '') mixNixDeps}
-          ''}
 
           runHook postConfigure
         '';
