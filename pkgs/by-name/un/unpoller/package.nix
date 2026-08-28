35e708c797cc9cee2c046fb9435467fd80fe7edf
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-28 10:03:50 +0000

    unpoller: 3.5.0 -> 5.1.0

--- pkgs/by-name/un/unpoller/package.nix
+++ pkgs/by-name/un/unpoller/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "unpoller";
-  version = "3.5.0";
+  version = "5.1.0";
 
   src = fetchFromGitHub {
     owner = "unpoller";
     repo = "unpoller";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-VLGURSaFCtGYHEq9wZt7dOCxOS/EqHizw9dmutYWIuQ=";
+    hash = "sha256-P5rPbFuEbOMIGDpvIXLfPN64pYY3ggSsoHEhZVJSbGE=";
   };
 
-  vendorHash = "sha256-Im99hlpLDDIbxVusNgaNDKwN/axyPrd0nHj486qR94k=";
+  vendorHash = "sha256-vtCSNBACKB/JgOuWBPZnvWg5mLfpYWyYPGdn/I6QcI0=";
 
   ldflags = [
     "-w"
