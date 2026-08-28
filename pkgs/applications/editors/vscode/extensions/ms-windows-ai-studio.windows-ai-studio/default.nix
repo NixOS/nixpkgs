60db5c30f2a61de35876284b3d92e6afad8d0e86
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-28 14:45:21 +0000

    vscode-extensions.ms-windows-ai-studio.windows-ai-studio: 1.6.8 -> 1.6.10

--- pkgs/applications/editors/vscode/extensions/ms-windows-ai-studio.windows-ai-studio/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-windows-ai-studio.windows-ai-studio/default.nix
@@ -7,8 +7,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "windows-ai-studio";
     publisher = "ms-windows-ai-studio";
-    version = "1.6.8";
-    hash = "sha256-UrflXIUTgoMc8D1oyxdmTw6LQz2lLFarEAB/PW1wM5w=";
+    version = "1.6.10";
+    hash = "sha256-ztzf1mrQT6/C89R5DkSNlxSXwyWgRi8uzawXAV7ZhSo=";
   };
 
   meta = {
