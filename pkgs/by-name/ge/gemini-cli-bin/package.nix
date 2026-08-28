fba56a9a4860de70dc0147a9f9c57d29e473f6d9
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-28 04:08:01 +0000

    gemini-cli-bin: 0.42.0 -> 0.57.0

--- pkgs/by-name/ge/gemini-cli-bin/package.nix
+++ pkgs/by-name/ge/gemini-cli-bin/package.nix
@@ -11,11 +11,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "gemini-cli-bin";
-  version = "0.42.0";
+  version = "0.57.0";
 
   src = fetchzip {
     url = "https://github.com/google-gemini/gemini-cli/releases/download/v${finalAttrs.version}/gemini-cli-bundle.zip";
-    hash = "sha256-Qkb39ehFabpRGxqpl3wCzoK3A2z5TMnKswngLz6kP/s=";
+    hash = "sha256-gKctdWt2djpTa5OttwrJBBfh3N0dvM72gymV9Dv3U8Q=";
     stripRoot = false;
   };
 
