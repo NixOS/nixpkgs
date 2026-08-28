45db34b3082b519ed62a978a8cc26cca38ed283a
Andreas Rammhold <andreas@rammhold.de>
2026-08-28 13:14:43 +0200

    stdenv/check-meta: set meta.broken also based on the problem code
    This pulls in the brokenness information from the meta.problems
    fields. This decouples the both implementation from each
    other (slightly). Whatever the metric for brokenness in meta.problems
    is now carries over to old meta.broken attribute.
    
    Right now there is no difference in the value of meta.broken since the
    both implementations are almost identical. As future work we could now
    drop the logic from `check-meta.nix` and rely solely on the problems
    code to do the brokenness detection without abandoning `meta.broken`
    as value to query.

--- pkgs/stdenv/generic/check-meta.nix
+++ pkgs/stdenv/generic/check-meta.nix
@@ -18,6 +18,7 @@ let
     isFunction
     isString
     length
+    mapAttrsToList
     mutuallyExclusive
     optional
     optionalString
@@ -25,6 +26,7 @@ let
     unsafeGetAttrPos
     warn
     all
+    groupBy
     ;
 
   inherit (lib.lists)
@@ -468,6 +470,17 @@ let
       hasOutput = out: elem out outputs;
       maintainersPosition = unsafeGetAttrPos "maintainers" (attrs.meta or { });
       teamsPosition = unsafeGetAttrPos "teams" (attrs.meta or { });
+
+      problems = completeMetaProblems' attrs;
+      problemsGroupedByKind = groupBy (p: p.name) (
+        mapAttrsToList (name: problem: {
+          inherit name;
+          inherit problem;
+        }) problems
+      );
+
+      problemsByKind = kind: problemsGroupedByKind.${kind} or [ ];
+      hasProblemKind = kind: (problemsByKind kind) != [ ];
     in
     {
       # `name` derivation attribute includes cross-compilation cruft,
@@ -594,11 +607,11 @@ let
 
       # Expose the result of the checks for everyone to see.
       unfree = hasUnfreeLicense attrs;
-      broken = isMarkedBroken attrs;
+      broken = isMarkedBroken attrs || hasProblemKind "broken";
       unsupported = hasUnsupportedPlatform' attrs;
       insecure = isMarkedInsecure attrs;
 
-      problems = completeMetaProblems' attrs;
+      inherit problems;
 
       available =
         validity.valid != "no"
