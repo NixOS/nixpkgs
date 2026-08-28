175ce6c9ae5f9999edbf3f586c32cb4eeee61b74
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-28 21:19:32 +0200

    python3Packages.pybravia: migrate to finalAttrs

--- pkgs/development/python-modules/pybravia/default.nix
+++ pkgs/development/python-modules/pybravia/default.nix
@@ -7,7 +7,7 @@
   pyprojectVersionPatchHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pybravia";
   version = "0.5.3";
   pyproject = true;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "Drafteed";
     repo = "pybravia";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-dHc1jmwmLRXpxxIKPMyscDtyWB/UU8xyL7Uv4ioi2TY=";
   };
 
@@ -33,8 +33,8 @@ buildPythonPackage rec {
   meta = {
     description = "Library for remote control of Sony Bravia TVs 2013 and newer";
     homepage = "https://github.com/Drafteed/pybravia";
-    changelog = "https://github.com/Drafteed/pybravia/releases/tag/${src.tag}";
+    changelog = "https://github.com/Drafteed/pybravia/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
