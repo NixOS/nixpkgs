e26c4bb98574a9d49cb9ab4372d895711b194dfa
rokuroo171 <mrakkakhairilazwar@gmail.com>
2026-08-28 15:18:12 +0700

    maintainers: add rokuroo171

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -24814,6 +24814,12 @@
     githubId = 129837916;
     name = "Rohith S";
   };
+  rokuroo171 = {
+    email = "mrakkakhairilazwar@gmail.com";
+    github = "rokuroo171";
+    githubId = 238833795;
+    name = "rokuroo";
+  };
   rolfschr = {
     email = "rolf.schr@posteo.de";
     github = "rolfschr";
