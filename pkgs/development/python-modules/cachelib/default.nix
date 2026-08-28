a1c4313910dda40d2a135d4957a6623b11c2fe1f
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-28 11:05:59 +0200

    python3Packages.cachelib: 0.13.0 -> 0.17.0
    Changelog: https://github.com/pallets-eco/cachelib/releases/tag/0.17.0

--- pkgs/development/python-modules/cachelib/default.nix
+++ pkgs/development/python-modules/cachelib/default.nix
@@ -1,40 +1,64 @@
 {
   lib,
+  boto3,
   buildPythonPackage,
   fetchFromGitHub,
+  flit-core,
+  pylibmc,
+  pymongo,
   pytest-xprocess,
   pytestCheckHook,
+  redis,
+  uwsgi,
+  valkey,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "cachelib";
-  version = "0.13.0";
-  format = "setuptools";
+  version = "0.17.0";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pallets";
     repo = "cachelib";
-    tag = version;
-    hash = "sha256-8jg+zfdIATvu/GSFvqHl4cNMu+s2IFWC22vPZ7Q3WYI=";
+    tag = finalAttrs.version;
+    hash = "sha256-egAg0X4xpbvtiAXU6Cu/IIs8/GJAy5i73gLIkdnvOmc=";
+  };
+
+  build-system = [ flit-core ];
+
+  optional-dependencies = {
+    dynamodb = [ boto3 ];
+    memcached = [ pylibmc ];
+    mongodb = [ pymongo ];
+    redis = [ redis ];
+    uwsgi = [ uwsgi ];
+    valkey = [ valkey ];
   };
 
   nativeCheckInputs = [
     pytest-xprocess
     pytestCheckHook
-  ];
+  ]
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
 
   disabledTestPaths = [
-    # requires set up local server
+    # Tests require to set up local server
     "tests/test_dynamodb_cache.py"
+    "tests/test_interface_uniformity.py"
+    "tests/test_memcached_cache.py"
     "tests/test_mongodb_cache.py"
+    "tests/test_redis_cache.py"
+    "tests/test_valkey_cache.py"
   ];
 
   pythonImportsCheck = [ "cachelib" ];
 
   meta = {
-    homepage = "https://github.com/pallets/cachelib";
     description = "Collection of cache libraries in the same API interface";
+    homepage = "https://github.com/pallets/cachelib";
+    changelog = "https://github.com/pallets-eco/cachelib/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.bsd3;
     maintainers = [ ];
   };
-}
+})
