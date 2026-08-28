7b7b2cd2a43b86c6a3ca55b789171154c835859f
Fabian Affolter <mail@fabian-affolter.ch>
2026-08-28 11:30:52 +0200

    fixup! python3Packages.patch-ng: 1.19.0 -> 1.19.1

--- pkgs/development/python-modules/patch-ng/default.nix
+++ pkgs/development/python-modules/patch-ng/default.nix
@@ -1,23 +1,31 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
+  setuptools,
 }:
 
-buildPythonPackage rec {
-  version = "1.19.1"; # note: `conan` package may require a hardcoded one
-  format = "setuptools";
+buildPythonPackage (finalAttrs: {
   pname = "patch-ng";
+  version = "1.19.1"; # note: `conan` package may require a hardcoded one
+  pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-A2o8wAE07FPzfpIzOVjudeEX8uYqXsK4XHEi5egVwp4=";
+  src = fetchFromGitHub {
+    owner = "conan-io";
+    repo = "python-patch-ng";
+    tag = finalAttrs.version;
+    hash = "sha256-X/OujL/zEZ5KkijFY6vgfUs/L1pslIfAl6TcAaTjA6U=";
   };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "patch_ng" ];
 
   meta = {
     description = "Library to parse and apply unified diffs";
-    homepage = "https://github.com/conan-io/python-patch";
+    homepage = "https://github.com/conan-io/python-patch-ng";
+    changelog = "https://github.com/conan-io/python-patch-ng/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
