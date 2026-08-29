d56b8c23bf6b74df545b6b49395dc498b1408bf6
Sigmanificient <edhyjox@gmail.com>
2026-08-29 23:59:59 +0200

    python3Packages.pylatexenc: modernize

--- pkgs/development/python-modules/pylatexenc/default.nix
+++ pkgs/development/python-modules/pylatexenc/default.nix
@@ -6,21 +6,24 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pylatexenc";
   version = "2.11";
+
   pyproject = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "phfaist";
     repo = "pylatexenc";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-Zv3Sjx0YpSCnoHHf3CSiGaFMS7FNgUUwvIxNX/k9tOg=";
   };
 
   build-system = [ setuptools ];
 
   pythonImportsCheck = [ "pylatexenc" ];
+
   nativeCheckInputs = [ pytestCheckHook ];
 
   meta = {
@@ -31,4 +34,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
