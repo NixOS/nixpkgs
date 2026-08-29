1a90d3cd04285497d8b8a88ffb5580eb9f25f988
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-29 23:58:15 +0000

    python3Packages.django-tree-queries: 0.24 -> 0.25.1

--- pkgs/development/python-modules/django-tree-queries/default.nix
+++ pkgs/development/python-modules/django-tree-queries/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "django-tree-queries";
-  version = "0.24";
+  version = "0.25.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "feincms";
     repo = "django-tree-queries";
     tag = version;
-    hash = "sha256-VPJU/0tnnLSayJhuOO0YOtqegULF6CB6ve8/1ytFydA=";
+    hash = "sha256-Ev6vEjQuPuUhWINRJelaAAvMUAsIB+5ilhSCaTikKW0=";
   };
 
   build-system = [
