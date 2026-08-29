9d9c6871941b89cf1c6d8670f6e4c6d97217482e
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-29 18:51:07 +0000

    voxtype: 0.7.5 -> 1.0.0

--- pkgs/by-name/vo/voxtype/package.nix
+++ pkgs/by-name/vo/voxtype/package.nix
@@ -52,16 +52,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "voxtype";
-  version = "0.7.5";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "peteonrails";
     repo = "voxtype";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zsOG1mBTXN4gdsTb1pUPKXATfhV5ZjgEsIUk07asaGo=";
+    hash = "sha256-kd6QThm6+i64ez9KMZFhARV5BxB0aqfiyIfuklwgMKo=";
   };
 
-  cargoHash = "sha256-YK5xZWPo7KAeWZeuMxNxHA3k6RR/MT2MIfEPcgMND00=";
+  cargoHash = "sha256-SSWrw26rgMLn3gZ/8xZfMdYUKOq2qrpD1+/txkm2jE0=";
 
   buildFeatures =
     [ ]
