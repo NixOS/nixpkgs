da6428a8e90a855b0cb0754eedf793f541d579b4
Michael Daniels <mdaniels5757@gmail.com>
2026-08-29 13:06:21 -0400

    amend! python3Packages.xstatic-font-awesome: 6.2.1.1 -> 6.2.1.2
    python3Packages.xstatic-font-awesome: 6.2.1.1 -> 6.2.1.2
    
    This commit was automatically generated using update-python-libraries.
    
    Co-authored-by: Michael Daniels <mdaniels5757@gmail.com>

--- pkgs/development/python-modules/xstatic-font-awesome/default.nix
+++ pkgs/development/python-modules/xstatic-font-awesome/default.nix
@@ -1,29 +1,30 @@
 {
   buildPythonPackage,
   lib,
-  fetchPypi,
-  setuptools_80,
+  fetchFromGitHub,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "xstatic-font-awesome";
   version = "6.2.1.2";
-  format = "setuptools";
+  pyproject = true;
 
-  src = fetchPypi {
-    pname = "XStatic-Font-Awesome";
-    inherit version;
-    hash = "sha256-nzyy8Dj619NSciN10/Ja80banuCT7Z3CyMRr2RGrGXE=";
+  src = fetchFromGitHub {
+    owner = "xstatic-py";
+    repo = "xstatic-font-awesome";
+    tag = version;
+    hash = "sha256-O70Io+9PCFDn+K9GqR/AeLSGzdRtu7cqn69dr4/QBSc=";
   };
 
   # xstatic uses pkg_resources.declare_namespace, removed in setuptools 83.
-  build-system = [ setuptools_80 ];
+  build-system = [ setuptools ];
 
   # no tests implemented
   doCheck = false;
 
   meta = {
-    homepage = "https://github.com/python-xstatic/font-awesome";
+    homepage = "https://github.com/xstatic-py/xstatic-font-awesome";
     description = "Font Awesome packaged for python";
     license = lib.licenses.ofl;
     maintainers = with lib.maintainers; [ aither64 ];
