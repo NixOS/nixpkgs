efd5542fbfdf1e65ef132ec56aa0793d8c6098eb
Robert Schütz <nix@dotlambda.de>
2026-08-29 22:22:13 -0700

    poetryPlugins.poeblix: modernize

--- pkgs/by-name/po/poetry/plugins/poeblix.nix
+++ pkgs/by-name/po/poetry/plugins/poeblix.nix
@@ -2,10 +2,11 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  poetry,
   poetry-core,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "poeblix";
   version = "0.10.0";
   pyproject = true;
@@ -13,26 +14,30 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "spoorn";
     repo = "poeblix";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-TKadEOk9kM3ZYsQmE2ftzjHNGNKI17p0biMr+Nskigs=";
   };
 
-  postPatch = ''
-    sed -i '/poetry =/d' pyproject.toml
-  '';
-
-  nativeBuildInputs = [
+  build-system = [
     poetry-core
+  ];
+
+  pythonRelaxDeps = [
+    "poetry"
+  ];
+
+  buildInputs = [
+    poetry
   ];
 
   doCheck = false;
   pythonImportsCheck = [ "poeblix" ];
 
   meta = {
-    changelog = "https://github.com/spoorn/poeblix/releases/tag/${lib.removePrefix "refs/tags/" src.rev}";
+    changelog = "https://github.com/spoorn/poeblix/releases/tag/${finalAttrs.src.tag}";
     description = "Poetry Plugin that adds various features that extend the poetry command such as building wheel files with locked dependencies, and validations of wheel/docker containers";
     license = lib.licenses.mit;
     homepage = "https://github.com/spoorn/poeblix";
     maintainers = with lib.maintainers; [ hennk ];
   };
-}
+})
