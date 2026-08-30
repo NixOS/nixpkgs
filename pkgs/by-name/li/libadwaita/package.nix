0e266b87b932d92272d1417af63e7dec218cb3f6
Marco "Capypara" Köpcke <hello@capypara.de>
2026-08-30 21:07:22 +0200

    libadwaita: 1.9.3 -> 1.10.beta.1
    Diff: https://gitlab.gnome.org/GNOME/libadwaita/-/compare/1.9.3...1.10.beta.1
    
    Changelog: https://gitlab.gnome.org/GNOME/libadwaita/-/blob/1.10.beta.1/NEWS

--- pkgs/by-name/li/libadwaita/package.nix
+++ pkgs/by-name/li/libadwaita/package.nix
@@ -23,7 +23,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libadwaita";
-  version = "1.9.3";
+  version = "1.10.beta.1";
 
   outputs = [
     "out"
@@ -37,7 +37,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "GNOME";
     repo = "libadwaita";
     tag = finalAttrs.version;
-    hash = "sha256-1V3L10YgRnOoJud/lybfSj2AYOY0kRAJdfamJg+S1fo=";
+    hash = "sha256-0ikPTLfM5DpaZDuKUnCBe/BB5M7n2Ef6emSlCHtWHKg=";
   };
 
   depsBuildBuild = [
