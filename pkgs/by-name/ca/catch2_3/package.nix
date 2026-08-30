cb288385c43f8f9ab93097e73a1c148fbb09b64a
Robert Schütz <nix@dotlambda.de>
2026-08-29 21:49:18 -0700

    catch2_3: 3.15.3 -> 3.16.0
    Diff: https://github.com/catchorg/Catch2/compare/v3.15.3...v3.16.0
    
    Changelog: https://github.com/catchorg/Catch2/blob/v3.16.0/docs/release-notes.md

--- pkgs/by-name/ca/catch2_3/package.nix
+++ pkgs/by-name/ca/catch2_3/package.nix
@@ -9,20 +9,14 @@
 
 stdenv.mkDerivation rec {
   pname = "catch2";
-  version = "3.15.3";
+  version = "3.16.0";
 
   src = fetchFromGitHub {
     owner = "catchorg";
     repo = "Catch2";
     tag = "v${version}";
-    hash = "sha256-ZuH3tUWNklq0bKp0Yu9w3L5FNsQwUxe6lyGBv4M6U1E=";
+    hash = "sha256-vvPZTQzLNGIcdDbuo0w6sQvxzLdKFCl2LNwcHu0d5I8=";
   };
-
-  patches = lib.optionals stdenv.cc.isClang [
-    # This test fails to compile with Clang 20
-    # See: https://github.com/catchorg/Catch2/issues/2991
-    ./clang-20-disable-broken-test.patch
-  ];
 
   postPatch = ''
     substituteInPlace CMake/*.pc.in \
