347df8550cf6e80ec9d9fc56fe1310649527aeda
Marco "Capypara" Köpcke <hello@capypara.de>
2026-08-30 21:06:55 +0200

    gnome-calculator: 50.0 -> 51.beta

--- pkgs/by-name/gn/gnome-calculator/package.nix
+++ pkgs/by-name/gn/gnome-calculator/package.nix
@@ -29,11 +29,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-calculator";
-  version = "50.0";
+  version = "51.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-calculator/${lib.versions.major finalAttrs.version}/gnome-calculator-${finalAttrs.version}.tar.xz";
-    hash = "sha256-gFPWiRVl6IKHS2XB21HFvzEABet4i4usNUY5B0M1CpA=";
+    hash = "sha256-1E4wjJiPiEcHO4I1hngVfQNvHv3Uyz9uhPPlCOeEWx4=";
   };
 
   patches = [
