37535c177383e775bb83ca53c8678d2fc1f6bf85
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-30 10:36:24 +0000

    stripe-cli: 1.43.2 -> 1.50.6

--- pkgs/by-name/st/stripe-cli/package.nix
+++ pkgs/by-name/st/stripe-cli/package.nix
@@ -12,7 +12,7 @@
 
 buildGoModule (finalAttrs: {
   pname = "stripe-cli";
-  version = "1.43.2";
+  version = "1.50.6";
 
   # required for tests
   __darwinAllowLocalNetworking = true;
@@ -21,9 +21,9 @@ buildGoModule (finalAttrs: {
     owner = "stripe";
     repo = "stripe-cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-FopjU7m6RbEJTekIFG2XppZ52U/TrFpipVUDWwngPJI=";
+    hash = "sha256-CcMXJcggQ03L1NEIMNO0wvrPXtCh8O4+iZey0AkzEKg=";
   };
-  vendorHash = "sha256-6z6jfRMmEll1703xUJYSc4WU7CN7tMMyidNtay6vo2M=";
+  vendorHash = "sha256-ab3um1ewUzTUGUlIsm8ed8xtDKulmXiRN+HJK2wP2h8=";
 
   nativeBuildInputs = [ installShellFiles ];
 
