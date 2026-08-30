b9cae8279852a1c5f6ecc5856c5eac22b819380c
R. Ryantm <ryantm-bot@ryantm.com>
2026-08-30 02:36:35 +0000

    python3Packages.langgraph: 1.2.10 -> 1.2.11

--- pkgs/development/python-modules/langgraph/default.nix
+++ pkgs/development/python-modules/langgraph/default.nix
@@ -41,7 +41,7 @@
 }:
 buildPythonPackage (finalAttrs: {
   pname = "langgraph";
-  version = "1.2.10";
+  version = "1.2.11";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -49,7 +49,7 @@ buildPythonPackage (finalAttrs: {
     owner = "langchain-ai";
     repo = "langgraph";
     tag = finalAttrs.version;
-    hash = "sha256-2P/JsZYQyXIselQaJVuyWTdkRQN7K1TW7w28D7BecRk=";
+    hash = "sha256-5CUQh9KNYQ0HBIuREOP0MxZgn5Z1MxMuqB9Be4e6AJo=";
   };
 
   postgresqlTestSetupPost = ''
