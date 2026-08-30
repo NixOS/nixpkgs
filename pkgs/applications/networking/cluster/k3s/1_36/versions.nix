69d6bd80b9e1bb390b590401b80bbba4a086b0c6
rorosen <robert.rose@mailbox.org>
2026-08-30 21:31:16 +0200

    k3s_1_36: 1.36.3+k3s1 -> 1.36.4+k3s1
    Attention:
    
    This release upgrades Traefik chart to v40.x which includes
    a breaking change for the ingress-nginx migration: the
    provider name changes from kubernetesIngressNginx to
    kubernetesIngressNGINX. Check
    https://github.com/traefik/traefik-helm-chart/releases/tag/v40.0.0
    for more details
    
    https://github.com/k3s-io/k3s/releases/tag/v1.36.4%2Bk3s1

--- pkgs/applications/networking/cluster/k3s/1_36/versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_36/versions.nix
@@ -1,16 +1,16 @@
 {
-  k3sVersion = "1.36.3+k3s1";
-  k3sCommit = "5aed4d7beddeb3e67120da477c876ac9efd70318";
-  k3sRepoSha256 = "0y5lc93f5pdvgm3mmk87mh50z7iway4cz99nskfh1600zfhia2s6";
-  k3sVendorHash = "sha256-pxYh1AJVZL8Xx23Nairoi5bSNNSVs+EqJbXF7ISN3wg=";
+  k3sVersion = "1.36.4+k3s1";
+  k3sCommit = "4dedb15be78017a8ddd5b9e81acd44f3481078ed";
+  k3sRepoSha256 = "07ynarjmjqybfyq30kaa08nx9rw5h83nifir050mplqjwb10mrk5";
+  k3sVendorHash = "sha256-naO/9O5aLGNf7FwQvsvSb/eG0eCBFc/Um3yfH2fP+8Y=";
   chartVersions = import ./chart-versions.nix;
   imagesVersions = builtins.fromJSON (builtins.readFile ./images-versions.json);
   k3sRootVersion = "0.15.2";
   k3sRootSha256 = "0yxq2jqqb7flm4rs9dl7fqxba3mmwkmjbc8rx7pgai4qa1lzyigy";
   k3sCNIVersion = "1.9.1-k3s1";
   k3sCNISha256 = "1ggaz0p1c2k94car9d89a05smz3zx32sxn197b1l5kmjcnzdwadh";
-  containerdVersion = "2.3.2-k3s2";
-  containerdSha256 = "1jzhkh0zg1s2922fkr4r5v1680apafkjqba8ic6br8nc8bk7j4xq";
+  containerdVersion = "2.3.4-k3s1.36";
+  containerdSha256 = "0mlazx89616j90s73953spi4ahcas8w9kb51a7qf02pa9isyz5vi";
   containerdPackage = "github.com/k3s-io/containerd/v2";
   criCtlVersion = "1.36.0-k3s1";
   flannelVersion = "v0.28.4";
