ca4f0e4d8b1fd27588e0d287d276c265bc726e89
whoomee <git@miglix.eu>
2026-08-31 16:36:59 +0200

    treewide: use nix-instantiate --eval --raw in update scripts
    Various update scripts used hacks like jq -e -r or tr -d '"' to strip
    quotes from strings, instead of simply adding the --raw flag to nix-instantiate.

--- pkgs/by-name/ej/ejabberd/update.sh
+++ pkgs/by-name/ej/ejabberd/update.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail
 
 version=$(curl -s ${GITHUB_TOKEN:+-u ":$GITHUB_TOKEN"} \
     https://api.github.com/repos/processone/ejabberd/releases/latest | jq -e -r .tag_name)
-old_version=$(nix-instantiate --eval -A ejabberd.version | jq -e -r)
+old_version=$(nix-instantiate --eval --raw -A ejabberd.version)
 
 if [[ $version == "$old_version" ]]; then
     echo "New version same as old version, nothing to do." >&2
--- pkgs/by-name/me/melonloader-installer/update.sh
+++ pkgs/by-name/me/melonloader-installer/update.sh
@@ -8,7 +8,7 @@ set -eu -o pipefail
 
 version=$(curl -s ${GITHUB_TOKEN:+-u ":$GITHUB_TOKEN"} \
     https://api.github.com/repos/LavaGang/MelonLoader.Installer/releases/latest | jq -e -r .tag_name)
-old_version=$(nix-instantiate --eval -A melonloader-installer.version | jq -e -r)
+old_version=$(nix-instantiate --eval --raw -A melonloader-installer.version)
 
 if [[ $version == "$old_version" ]]; then
     echo "New version same as old version, nothing to do." >&2
