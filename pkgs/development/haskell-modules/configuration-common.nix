cd641314f125a15165bd3c1cba2c49d991ade4de
coolcuber <rpgeorg@clemson.edu>
2026-08-31 17:11:17 -0400

    haskellPackages.hoogle: fix version

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2341,7 +2341,7 @@ with haskellLib;
 
   # Latest release depends on crypton-connection ==0.3.2 https://github.com/ndmitchell/hoogle/issues/435
   hoogle = overrideSrc {
-    version = "unstable-2024-07-29";
+    version = "5.0.18.4-unstable-2024-07-28";
     src = pkgs.fetchFromGitHub {
       owner = "ndmitchell";
       repo = "hoogle";
