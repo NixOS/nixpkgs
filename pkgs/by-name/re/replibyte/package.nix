a906f9e6860c798e690722f08182e1c071dcc278
Darren Rambaud <225436867+debtquity@users.noreply.github.com>
2026-08-31 02:49:18 -0500

    replibyte: fix `aarch64-darwin` build
    * both `--defsym` and `__rust_probestack=0` are unknown/invalid options
      during link on darwin systems using clang
    
    Resolves: #558280

--- pkgs/by-name/re/replibyte/package.nix
+++ pkgs/by-name/re/replibyte/package.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  stdenv,
   rustPlatform,
   fetchFromGitHub,
   pkg-config,
@@ -29,7 +30,17 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   # Fix undefined reference to `__rust_probestack` from wasmer_vm.
   # Define it as a no-op since it's only needed for stack overflow detection.
-  env.RUSTFLAGS = "-C link-arg=-Wl,--defsym,__rust_probestack=0";
+  env.RUSTFLAGS =
+    let
+      linkArgs = [
+        "-Wl"
+      ]
+      ++ lib.optionals stdenv.hostPlatform.isLinux [
+        "--defsym"
+        "__rust_probestack=0"
+      ];
+    in
+    "-C link-arg=${builtins.concatStringsSep "," linkArgs}";
 
   cargoBuildFlags = [ "--all-features" ];
 
