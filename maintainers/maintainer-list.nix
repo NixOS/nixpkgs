c1ef93692f0743c6a621953963e1e18038d64c05
Marius Raducan <marius-catalin.raducan@tuta.io>
2026-09-01 22:32:38 +0300

    Update maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23674,12 +23674,6 @@
     github = "rachalaraj";
     githubId = 124191100;
   };
-  raducu427 = {
-    name = "Marius Raducan";
-    email = "marius-catalin.raducan@tuta.io";
-    github = "raducu427";
-    githubId = 32168993;
-  };
   RadxaYuntian = {
     # This is the work account for @MakiseKurisu
     name = "ZHANG Yuntian";
