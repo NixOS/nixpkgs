c2c8b9942c2afe7e21aaa49cfc4244867e287c2e
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-01 19:11:21 +0000

    python3Packages.zigpy-xbee: 0.21.1 -> 0.22.0

--- pkgs/development/python-modules/zigpy-xbee/default.nix
+++ pkgs/development/python-modules/zigpy-xbee/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "zigpy-xbee";
-  version = "0.21.1";
+  version = "0.22.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "zigpy";
     repo = "zigpy-xbee";
     tag = version;
-    hash = "sha256-ALwhl9WUDkv0POufF/G/rZrn+ITbMdh6y86lShy6ZTg=";
+    hash = "sha256-ni+YY8MGPik8/qxAKlY1loAbRBFcdOch1qT4xvN1Kyc=";
   };
 
   postPatch = ''
