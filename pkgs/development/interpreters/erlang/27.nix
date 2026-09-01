a3ca8fe84ec6feaee2528b36d70175f58c754888
Adam C. Stephens <adam@valkor.net>
2026-09-01 12:07:23 -0400

    beam27Packages.erlang: 27.3.4.16 -> 27.3.4.17
    Changelog: https://github.com/erlang/otp/releases/tag/OTP-27.3.4.17

--- pkgs/development/interpreters/erlang/27.nix
+++ pkgs/development/interpreters/erlang/27.nix
@@ -1,6 +1,6 @@
 genericBuilder:
 
 genericBuilder {
-  version = "27.3.4.16";
-  hash = "sha256-OEz/X3HzcgmX9gyg8RrcMKEVsu7X8PW60G3SfLl0O3Y=";
+  version = "27.3.4.17";
+  hash = "sha256-os1xGN+3boKGehVKtuM0Buetb56IGhn8SRuoHlJg5bA=";
 }
