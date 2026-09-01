34b9e4415182a7c4f0475df48f340080896c1737
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-01 13:12:18 +0000

    python3Packages.dbt-protos: 1.0.455 -> 1.0.569

--- pkgs/development/python-modules/dbt-protos/default.nix
+++ pkgs/development/python-modules/dbt-protos/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "dbt-protos";
-  version = "1.0.455";
+  version = "1.0.569";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dbt-labs";
     repo = "proto-python-public";
     tag = "v${version}";
-    hash = "sha256-o0H5sGXVxiZgc9Vdwgd5IUlzqHRqSuYbkwI/R9M8uY8=";
+    hash = "sha256-zaQDYRQ4n+eulYUnXGxcF18UXr7w9O0KnP+GCvUfA3M=";
   };
 
   build-system = [
