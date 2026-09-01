055665af0a628fb3bb9d7844d252cb766a82df23
Gaetan Lepage <gaetan@glepage.com>
2026-09-01 10:52:55 +0000

    rerun: 0.36.3 -> 0.37.0
    Diff: https://github.com/rerun-io/rerun/compare/0.36.3...0.37.0
    
    Changelog: https://github.com/rerun-io/rerun/blob/0.37.0/CHANGELOG.md

--- pkgs/development/python-modules/rerun-notebook/default.nix
+++ pkgs/development/python-modules/rerun-notebook/default.nix
@@ -22,7 +22,7 @@ buildPythonPackage (finalAttrs: {
     inherit (finalAttrs) version;
     format = "wheel";
     python = "py2.py3";
-    hash = "sha256-QYOfTf2PCEjLA0YI6G/HDtA3UlUkhV5t7fCdw9QU584=";
+    hash = "sha256-O5e4oxNZo6fI4x+0+4vl3saLATObRDL9Zx/+JKXJGCQ=";
   };
 
   pythonRelaxDeps = [
