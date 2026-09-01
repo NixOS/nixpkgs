75dc4983e660272c8ca77554a5c25cc60d70b34b
whispers <whispersofthedawn@duck.com>
2026-09-01 10:44:03 -0400

    mullvad-browser: 15.0.20 -> 15.0.21
    changelog: https://gitlab.torproject.org/tpo/applications/tor-browser-build/-/raw/mb-15.0.21-build1/projects/browser/Bundle-Data/Docs-MB/ChangeLog.txt
    firefox esr: https://www.firefox.com/en-US/firefox/140.15.0/releasenotes/
    firefox security advisories: https://www.mozilla.org/en-US/security/advisories/mfsa2026-84/
    
    (cherry picked from commit 8c26bb07918f46d68780b77b8df31db0e4cbe5bd)

--- pkgs/by-name/mu/mullvad-browser/package.nix
+++ pkgs/by-name/mu/mullvad-browser/package.nix
@@ -97,7 +97,7 @@ let
     ++ lib.optionals mediaSupport [ ffmpeg_7 ]
   );
 
-  version = "15.0.20";
+  version = "15.0.21";
 
   sources = {
     x86_64-linux = fetchurl {
@@ -109,7 +109,7 @@ let
         "https://tor.eff.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
         "https://tor.calyxinstitute.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
       ];
-      hash = "sha256-EyVih1kklIg7b3fdh6tqtEE3Xh/UPpPMZgfdxtY6c5s=";
+      hash = "sha256-LHv/hY/abmy7/8nsNkKiwJh+SvRLzEO97fNNvPuYXQo=";
     };
   };
 
