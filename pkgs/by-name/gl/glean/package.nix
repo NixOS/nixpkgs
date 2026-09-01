e2e1d0a1c3df21d0a90c71395b42b55c0e7813b1
rokuroo171 <mrakkakhairilazwar@gmail.com>
2026-09-01 18:34:36 +0700

    glean: init at 1.5.0
    Adds glean note-taking app (v1.5.0) under pkgs/by-name/gl/glean.
    Built from source using buildGoModule with frontend npm build.

---
+++ pkgs/by-name/gl/glean/package.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  npmHooks,
+  nodejs,
+  fetchNpmDeps,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "glean";
+  version = "1.5.0";
+
+  src = fetchFromGitHub {
+    owner = "rokuroo171";
+    repo = "glean";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-frdwT4wodBHOAChOxXhzLwDNOON03YtWBy19wWtZZTQ=";
+  };
+
+  __structuredAttrs = true;
+
+  vendorHash = "sha256-0wokAwAd41GL/KK339fnmjQa8/mEeySg8J2Pcn+lmP8=";
+
+  npmDeps = fetchNpmDeps {
+    name = "glean-frontend-deps";
+    src = "${finalAttrs.src}/frontend";
+    hash = "sha256-MOEW3oPuZuQq/xR6fnNs0/yKtbFsH6d56aY8OGJ0ZuE=";
+  };
+
+  npmRoot = "frontend";
+
+  nativeBuildInputs = [
+    npmHooks.npmConfigHook
+    nodejs
+  ];
+
+  preBuild = ''
+    cd frontend
+    npm run build
+    cd ..
+  '';
+
+  meta = {
+    description = "Open-source note-taking app with lots of customization";
+    homepage = "https://github.com/rokuroo171/glean";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "glean";
+    maintainers = with lib.maintainers; [ rokuroo171 ];
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+})
