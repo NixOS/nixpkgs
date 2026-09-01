7115374852f465f98bf00944f0be020a6c753130
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-01 22:19:22 +0000

    pfcpsim: 1.5.0 -> 1.5.1

--- pkgs/by-name/pf/pfcpsim/package.nix
+++ pkgs/by-name/pf/pfcpsim/package.nix
@@ -5,17 +5,17 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "pfcpsim";
-  version = "1.5.0";
+  version = "1.5.1";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "omec-project";
     repo = "pfcpsim";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-RhroRtnHweZxQCHUFNlCCekUUrLvtu5YpGB8bKAT5ic=";
+    hash = "sha256-7SS2qQ27W8LgSVr+8dnBDqfBsYriAhLWNvpNhse2OOA=";
   };
 
-  vendorHash = "sha256-Rb2MJbWqFbkTFLZs/oLPNFcmKDkXQt4IdPwnsdc4QXU=";
+  vendorHash = "sha256-gvO5Nwo7DASuNRnWvo682UFl2Cj50PFZ1fkWQVqX9Go=";
 
   # Fuzzing cannot be performed without user plane function (upf)
   checkFlags = [ "-skip=^Fuzz$" ];
