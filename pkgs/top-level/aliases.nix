e67e08d2343f6f957507505695ffe5636c5647f9
Ben Siraphob <bensiraphob@gmail.com>
2026-09-02 06:25:22 -0700

    jpegrescan: drop
    Upstream archived it: https://github.com/kud/jpegrescan

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1204,6 +1204,7 @@ mapAliases {
   jitsi = throw "'jitsi' has been removed as it relies on gtk2 libraries"; # Added 2026-07-24
   jmtpfs = throw "'jmtpfs' has been removed, as it is unmaintained"; # Added 2026-06-06
   joplin = joplin-cli; # Added 2025-11-03
+  jpegrescan = throw "'jpegrescan' has been removed because it was archived upstream. Consider using 'mozjpeg' instead."; # Added 2026-09-02
   jscoverage = throw "jscoverage has been removed, as it was broken"; # Added 2025-08-25
   jsduck = throw "jsduck has been removed, as it was broken and unmaintained upstream."; # Added 2025-12-02
   jwm-settings-manager = throw "'jwm-settings-manager' has been removed, as it is unmaintained upstream"; # Added 2026-08-01
