4e340b5771d395f00a46c2ffaf032a2174e83af1
Nick Cao <nickcao@nichi.co>
2026-09-02 16:40:42 -0400

    _64gram: 1.2.7 -> 1.2.8
    Diff: https://github.com/TDesktop-x64/tdesktop/compare/v1.2.7...v1.2.8
    
    Changelog: https://github.com/TDesktop-x64/tdesktop/releases/tag/v1.2.8

--- pkgs/by-name/_6/_64gram/package.nix
+++ pkgs/by-name/_6/_64gram/package.nix
@@ -10,13 +10,13 @@ telegram-desktop.override {
   inherit withWebkit;
   unwrapped = telegram-desktop.unwrapped.overrideAttrs (old: rec {
     pname = "64gram-unwrapped";
-    version = "1.2.7";
+    version = "1.2.8";
 
     src = fetchFromGitHub {
       owner = "TDesktop-x64";
       repo = "tdesktop";
       tag = "v${version}";
-      hash = "sha256-5Ovx/71wf0SV4RUwEmIvf1Q686AZ5WPxn9WlgklAMf4=";
+      hash = "sha256-B/Gr8QxpIk81/ZFdcLB5Rjo9044p3lflv3jJpmmBsbA=";
       fetchSubmodules = true;
     };
 
