a2ded6ccbda4858e390d419513b93e77bc2070ff
whispers <whispersofthedawn@duck.com>
2026-09-02 00:13:39 -0400

    clblas: drop
    clblas' last commit upstream was march 2017, and it currently requires
    us to pin an old compiler version (llvm 19) on darwin. it does not seem
    particularly used (unmaintained in nixpkgs, only dependent is the old
    and now-removed clmagma), so let's simply remove it.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -560,6 +560,7 @@ mapAliases {
   classads = throw "'classads' has been removed, as it is unmaintained both upstream and in nixpkgs"; # Added 2026-06-27
   claude-code-acp = warnAlias "'claude-code-acp' has been renamed to 'claude-agent-acp'" claude-agent-acp; # Added 2026-03-31
   claude-code-bin = warnAlias "'claude-code-bin' has been merged into 'claude-code'" claude-code; # Added 2026-04-18
+  clblas = throw "'clblas' has been removed as it was unmaintained upstream"; # Added 2026-09-02
   clearlyU = clearly-u; # Added 2026-02-08
   cli-visualizer = throw "'cli-visualizer' has been removed as the upstream repository is gone"; # Added 2025-06-05
   clima = throw "'clima' has been removed, as it has been unmaintained upstream since December 2024, use glow instead"; # Added 2026-01-01
