fb7dff81d956c7f83fd39d42cfebcf9e5f2ae174
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-02 01:10:59 +0000

    butler: 15.30.0 -> 15.31.0

--- pkgs/by-name/bu/butler/package.nix
+++ pkgs/by-name/bu/butler/package.nix
@@ -7,20 +7,20 @@
 
 buildGoModule (finalAttrs: {
   pname = "butler";
-  version = "15.30.0";
+  version = "15.31.0";
 
   src = fetchFromGitHub {
     owner = "itchio";
     repo = "butler";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zq6ZmPNhRrp2MiDr6VElPMJGXEEkdK9ZjI0XUM0nED8=";
+    hash = "sha256-gH0aSS5VvKdBr9zD7CGHo3Qhs/MFkJWVMaG9m9yDPXw=";
   };
 
   buildInputs = [ brotli ];
 
   doCheck = false; # disabled because the tests don't work in a non-FHS compliant environment.
 
-  vendorHash = "sha256-kNc3twHNWgxJEVDte4GO6da8KrUmClaFZuip1bb1fMM=";
+  vendorHash = "sha256-4Uleff7XO9hs72/G+s8Ak1yaDLNpdHZa2GKx1aCtLZI=";
 
   meta = {
     description = "Command-line itch.io helper";
