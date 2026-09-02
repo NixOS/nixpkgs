b0314d5e9358f8a37770dd15bcf54631c9a7408d
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-09-02 15:42:20 +0000

    fishPlugins.sponge: modernize

--- pkgs/shells/fish/plugins/sponge.nix
+++ pkgs/shells/fish/plugins/sponge.nix
@@ -3,15 +3,14 @@
   buildFishPlugin,
   fetchFromGitHub,
 }:
-
-buildFishPlugin rec {
+buildFishPlugin (finalAttrs: {
   pname = "sponge";
   version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "meaningful-ooo";
     repo = "sponge";
-    rev = version;
+    tag = finalAttrs.version;
     sha256 = "sha256-MdcZUDRtNJdiyo2l9o5ma7nAX84xEJbGFhAVhK+Zm1w=";
   };
 
@@ -21,4 +20,4 @@ buildFishPlugin rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ quantenzitrone ];
   };
-}
+})
