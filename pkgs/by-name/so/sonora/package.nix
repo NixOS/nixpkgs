cbd21d49c7a6de7955a17d2114d7fb7d447e0d24
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-09-02 17:30:04 +0200

    sonora: init at 0.33.0

---
+++ pkgs/by-name/so/sonora/package.nix
@@ -0,0 +1,125 @@
+{
+  lib,
+  stdenv,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  cmake,
+  makeWrapper,
+  alsa-lib,
+  alsa-plugins,
+  pipewire,
+  symlinkJoin,
+  dbus,
+  fontconfig,
+  freetype,
+  sqlite,
+  vulkan-loader,
+  wayland,
+  libxkbcommon,
+  libxcb,
+  libx11,
+  libxcursor,
+  libxi,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "sonora";
+  version = "0.33.0";
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "sonorahq";
+    repo = "sonora";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-EoOn+PJ3xltxmCMH7yEASM/Ai27BGKAAgM0hPGAsEDQ=";
+  };
+
+  cargoHash = "sha256-1wOdbUwQ0cQ/imBbKxs+cu6DQZ/3Ba2Y3jfT4OzuC88=";
+
+  postPatch = ''
+    rm .cargo/config.toml
+  '';
+
+  nativeBuildInputs = [
+    pkg-config
+    cmake
+    makeWrapper
+  ];
+
+  buildInputs = [
+    alsa-lib
+    dbus
+    fontconfig
+    freetype
+    sqlite
+    vulkan-loader
+    wayland
+    libxkbcommon
+    libxcb
+    libx11
+    libxcursor
+    libxi
+  ];
+
+  env.LIBSQLITE3_SYS_USE_PKG_CONFIG = "1";
+
+  cargoBuildFlags = [ "--package=sonora" ];
+
+  doCheck = false;
+
+  postInstall = ''
+    install -Dm644 assets/linux/sonora.desktop $out/share/applications/sonora.desktop
+    install -Dm644 assets/linux/sonora.svg $out/share/icons/hicolor/scalable/apps/sonora.svg
+    for icon in assets/linux/icons/hicolor/*/apps/sonora.png; do
+      size="$(basename "$(dirname "$(dirname "$icon")")")"
+      install -Dm644 "$icon" "$out/share/icons/hicolor/$size/apps/sonora.png"
+    done
+  '';
+
+  postFixup = lib.optionalString stdenv.hostPlatform.isLinux ''
+    patchelf --add-rpath ${
+      lib.makeLibraryPath [
+        vulkan-loader
+        wayland
+        libxkbcommon
+      ]
+    } $out/bin/sonora
+    wrapProgram $out/bin/sonora \
+      --set ALSA_PLUGIN_DIR ${finalAttrs.passthru.alsaPluginDirectory}
+  '';
+
+  passthru = {
+    updateScript = nix-update-script { };
+    alsaPluginDirectory = symlinkJoin {
+      name = "sonora-alsa-plugins";
+      paths = [
+        "${pipewire}/lib/alsa-lib"
+        "${alsa-plugins}/lib/alsa-lib"
+      ];
+    };
+  };
+
+  meta = {
+    description = "Native music streaming client for Spotify, YouTube Music, and local files";
+    homepage = "https://github.com/sonorahq/sonora";
+    changelog = "https://github.com/sonorahq/sonora/releases/tag/v${finalAttrs.version}";
+    license = with lib.licenses; [
+      gpl3Plus
+      ofl
+      isc
+      mit
+      asl20
+      cc-by-40
+      cc0
+    ];
+    maintainers = with lib.maintainers; [
+      Ra77a3l3-jar
+      nolight132
+    ];
+    mainProgram = "sonora";
+    platforms = lib.platforms.linux;
+  };
+})
