4b7a54190738facee3c94a740d9066857b23fa40
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-02 12:40:04 +0000

    drumkv1: 1.4.2 -> 1.4.3

--- pkgs/by-name/dr/drumkv1/package.nix
+++ pkgs/by-name/dr/drumkv1/package.nix
@@ -15,11 +15,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "drumkv1";
-  version = "1.4.2";
+  version = "1.4.3";
 
   src = fetchurl {
     url = "mirror://sourceforge/drumkv1/drumkv1-${finalAttrs.version}.tar.gz";
-    hash = "sha256-jTOTOziCrycFyMe6wIfUnw7d6p+gNZfO7Q9BcZOyOME=";
+    hash = "sha256-tMzNLWclKkYStICOn4ztb2vcYnA2WniSPSjZNk9TmqQ=";
   };
 
   buildInputs = [
