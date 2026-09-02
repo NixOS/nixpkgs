a2053c017bf9d45c6ffed67599c0c492e504f7d5
Ben Siraphob <bensiraphob@gmail.com>
2026-09-02 08:19:59 -0700

    rainloop-{community,standard}: drop
    Upstream archived it: https://github.com/RainLoop/rainloop-webmail

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2160,6 +2160,8 @@ mapAliases {
   radicale3 = throw "'radicale3' has been renamed to/replaced by 'radicale'"; # Converted to throw 2025-10-27
   ragnarwm = throw "'ragnarwm' has been removed as it was broken"; # Added 2026-04-21
   railway-travel = throw "'railway-travel' has been renamed to/replaced by 'diebahn'"; # Converted to throw 2025-10-27
+  rainloop-community = throw "'rainloop-community' has been removed because RainLoop is archived upstream; consider the maintained SnappyMail successor."; # Added 2026-09-02
+  rainloop-standard = throw "'rainloop-standard' has been removed because RainLoop is archived upstream; consider the maintained SnappyMail successor."; # Added 2026-09-02
   rambox-pro = throw "'rambox-pro' has been renamed to/replaced by 'rambox'"; # Converted to throw 2025-10-27
   rapidjson-unstable = throw "'rapidjson-unstable' has been renamed to/replaced by 'rapidjson'"; # Converted to throw 2025-10-27
   rar2fs = throw "'rar2fs' has been removed as it is unmaintained, and depends on the unmaintained fuse2 library"; # Added 2026-05-19
