b9c62874ced3b008d577fcde62d5ce4a6d5d7721
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-02 09:04:50 +0200

    python3Packages.mypy-boto3-kinesis: 1.43.84 -> 1.43.86

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -726,8 +726,8 @@ in
       "sha256-f36IwT8zw4RvLqbZgGas6euLVdKR5gJJl7eLBF8PjaE=";
 
   mypy-boto3-kinesis =
-    buildMypyBoto3Package "kinesis" "1.43.84"
-      "sha256-nKymAOSmsdkjHHSonZsGuBu0BJKJkwACqi5OwvNT988=";
+    buildMypyBoto3Package "kinesis" "1.43.86"
+      "sha256-ad8diP7891WECwzfbFMStIqcBNeRChyQ/euFIQX7asc=";
 
   mypy-boto3-kinesis-video-archived-media =
     buildMypyBoto3Package "kinesis-video-archived-media" "1.43.0"
