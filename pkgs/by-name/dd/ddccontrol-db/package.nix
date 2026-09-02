a5f09f763fd08e231fdec6fe419cfdc6d3c275b6
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-02 15:23:18 +0000

    ddccontrol-db: 20260813 -> 20260902

--- pkgs/by-name/dd/ddccontrol-db/package.nix
+++ pkgs/by-name/dd/ddccontrol-db/package.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ddccontrol-db";
-  version = "20260813";
+  version = "20260902";
 
   src = fetchFromGitHub {
     owner = "ddccontrol";
     repo = "ddccontrol-db";
     tag = finalAttrs.version;
-    sha256 = "sha256-8FSPHOK5/2nSpb8qSOXY/SdtkWfpzNKMq9pm8wL7UCg=";
+    sha256 = "sha256-Nu0SC9Qh4+wUzVCdo7m2z4pZH50FvCXehF3IiCoHXKM=";
   };
 
   nativeBuildInputs = [
