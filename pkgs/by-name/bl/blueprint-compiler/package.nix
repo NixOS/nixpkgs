2377fd3d60a9ea582de02091816d9a51964d5c53
Tom Hunze <dev@thunze.de>
2026-09-02 22:16:01 +0200

    blueprint-compiler: buildPythonApplication WIP

--- pkgs/by-name/bl/blueprint-compiler/package.nix
+++ pkgs/by-name/bl/blueprint-compiler/package.nix
@@ -6,17 +6,19 @@
   libadwaita,
   meson,
   ninja,
-  python3,
   runCommand,
   stdenv,
   testers,
   wrapGAppsNoGuiHook,
   xvfb-run,
   gnome,
+  python3Packages
 }:
-stdenv.mkDerivation (finalAttrs: {
+
+python3Packages.buildPythonApplication (finalAttrs: {
   pname = "blueprint-compiler";
   version = "0.22.2";
+  pyproject = false;
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
@@ -34,17 +36,11 @@ stdenv.mkDerivation (finalAttrs: {
     gobject-introspection
     meson
     ninja
-    python3
     wrapGAppsNoGuiHook
   ];
 
   buildInputs = [
     libadwaita
-    (python3.withPackages (
-      ps: with ps; [
-        pygobject3
-      ]
-    ))
   ];
 
   propagatedBuildInputs = [
@@ -52,22 +48,31 @@ stdenv.mkDerivation (finalAttrs: {
     gobject-introspection
   ];
 
+  dependencies = [ python3Packages.pygobject3 ];
+
   nativeCheckInputs = [
     dbus
     xvfb-run
   ];
 
   # requires xvfb-run
-  doCheck = !stdenv.hostPlatform.isDarwin && false; # tests time out
+  doInstallCheck = !stdenv.hostPlatform.isDarwin && false; # tests time out
 
-  checkPhase = ''
-    runHook preCheck
+  installCheckPhase = ''
+    runHook preInstallCheck
 
     xvfb-run dbus-run-session \
       --config-file=${dbus}/share/dbus-1/session.conf \
       meson test --no-rebuild --print-errorlogs
 
-    runHook postCheck
+    runHook postInstallCheck
+  '';
+
+  # Prevent double wrapping, let the Python wrapper use the args in preFixup.
+  dontWrapGApps = true;
+
+  preFixup = ''
+    makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
   '';
 
   passthru = {
