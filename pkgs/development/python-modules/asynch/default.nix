4e9afd8fa7f8b9bc7128630d76623bd4e103d48c
João Santos Reis <joao.reis@tweag.io>
2026-09-03 09:37:07 +0100

    python3Packages.asynch: init at 0.4.0
    Co-authored-by: José Luis Lafuente <jl@lafuente.dev>
    Co-authored-by: João Santos Reis <joao.reis@tweag.io>

---
+++ pkgs/development/python-modules/asynch/default.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pythonOlder,
+  poetry-core,
+  setuptools,
+  cython,
+  ciso8601,
+  tzdata,
+  lz4,
+  tzlocal,
+  zstd,
+  clickhouse-cityhash,
+  stdenv,
+  nix-update-script,
+  nixosTests,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "asynch";
+  version = "0.4.0";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "long2ice";
+    repo = "asynch";
+    tag = "v${finalAttrs.version}";
+    sha256 = "sha256-iLhhk7EiNHMVzxlw7HWjS00GwZ8cRXPz3jih1edWde4=";
+  };
+
+  disabled = pythonOlder "3.11";
+
+  build-system = [
+    poetry-core
+    setuptools
+    cython
+  ];
+
+  dependencies = [
+    ciso8601
+    tzlocal
+  ]
+  ++ lib.optionals (stdenv.hostPlatform.isWindows) [
+    tzdata
+  ];
+
+  optional-dependencies = {
+    compression = [
+      clickhouse-cityhash
+      lz4
+    ]
+    ++ lib.optionals (pythonOlder "3.14") [
+      zstd
+    ];
+  };
+
+  pythonImportsCheck = [
+    "asynch"
+  ];
+
+  meta = {
+    description = "Asyncio driver for ClickHouse with native TCP support";
+    homepage = "https://github.com/long2ice/asynch";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
+      jlesquembre
+      joaosreis
+    ];
+  };
+})
