621b3f69d695b4e0d86e4e9e7c5e8ee10174a238
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-03 09:41:23 +0000

    python3Packages.teslemetry-stream: 0.10.0 -> 0.13.0

--- pkgs/development/python-modules/teslemetry-stream/default.nix
+++ pkgs/development/python-modules/teslemetry-stream/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "teslemetry-stream";
-  version = "0.10.0";
+  version = "0.13.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Teslemetry";
     repo = "python-teslemetry-stream";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ko9H0qhstD9VnR5707E9BUdJG2/KGGLsUxQpQ8F28y4=";
+    hash = "sha256-qhyD+CvpnmZgR1Zj5/W+yHYsQPwu5dbUAeivX9fZ5cY=";
   };
 
   build-system = [ setuptools ];
