b3fce0d692d16153388976ad9f1b8bad3c380246
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-03 01:50:40 +0000

    python3Packages.python-xbox: 0.2.0 -> 0.2.1

--- pkgs/development/python-modules/python-xbox/default.nix
+++ pkgs/development/python-modules/python-xbox/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "python-xbox";
-  version = "0.2.0";
+  version = "0.2.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tr4nt0r";
     repo = "python-xbox";
     tag = "v${version}";
-    hash = "sha256-5sYN7w/cemZPMt8awsoaUPo845oXiiUsIfl8GG85Umw=";
+    hash = "sha256-zI0eFbIw1BL1MsBle1hMs8WuBaN3FHJMgPSlZ2aOjBo=";
   };
 
   build-system = [
