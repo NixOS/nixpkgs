26c885438c25912426e0484c7117af7b73aa0111
Ryan Omasta <git@ryand.ca>
2026-09-02 19:41:32 -0600

    python3Packages.kicad-python: 0.5.0 -> 0.8.0
    Diff: https://gitlab.com/kicad/code/kicad-python/-/compare/0.5.0...0.8.0

--- pkgs/development/python-modules/kicad-python/default.nix
+++ pkgs/development/python-modules/kicad-python/default.nix
@@ -2,29 +2,50 @@
   lib,
   buildPythonPackage,
   fetchFromGitLab,
+  replaceVars,
   poetry-core,
   protoletariat,
   mypy-protobuf_3_6,
   pkgs,
   protobuf,
   pynng,
+  jsonschema,
   pytestCheckHook,
-  gitMinimal,
   pythonOlder,
   typing-extensions,
+
+  kicad,
 }:
 buildPythonPackage (finalAttrs: {
   pname = "kicad-python";
-  version = "0.5.0";
+  version = "0.8.0";
   pyproject = true;
 
   src = fetchFromGitLab {
     owner = "kicad/code";
     repo = "kicad-python";
     tag = finalAttrs.version;
-    hash = "sha256-FIWTYBUauq4yUdnijjPgxaXynh/U03ppnLU8YVkKYHw=";
-    fetchSubmodules = true;
+    hash = "sha256-Zt32e1Ib996cvE1lFEUEKcUb3rfPOfcOJu1Ikrhp7h4=";
   };
+
+  patches = [
+    (replaceVars ./use-packaged-kicad.patch {
+      kicadVersion = kicad.version;
+    })
+  ];
+
+  postPatch = ''
+    rm -rf kicad
+    ln -sfn ${kicad.src} kicad
+
+    # fixes: FileExistsError: File already exists .../kipy/__init__.py
+    substituteInPlace pyproject.toml \
+      --replace-fail 'script =' "#"
+  '';
+
+  preBuild = ''
+    python build.py
+  '';
 
   build-system = [
     poetry-core
@@ -34,26 +55,16 @@ buildPythonPackage (finalAttrs: {
   dependencies = [
     protobuf
     pynng
+    jsonschema
   ]
   ++ (lib.optional (pythonOlder "3.13") typing-extensions);
 
   nativeBuildInputs = [
     pkgs.protobuf
     mypy-protobuf_3_6
-    gitMinimal
   ];
 
   pythonRelaxDeps = [ "protobuf" ];
-
-  # fixes: FileExistsError: File already exists .../kipy/__init__.py
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace-fail 'script =' "#"
-  '';
-
-  preBuild = ''
-    python build.py
-  '';
 
   pythonImportsCheck = [ "kipy" ];
 
@@ -64,6 +75,9 @@ buildPythonPackage (finalAttrs: {
     homepage = "https://kicad.org/";
     downloadPage = "https://gitlab.com/kicad/code/kicad-python";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ sigmanificient ];
+    maintainers = with lib.maintainers; [
+      sigmanificient
+      ryand56
+    ];
   };
 })
