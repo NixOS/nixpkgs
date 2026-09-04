1f388108f7b4e47e9340dd0e9a42e51e3730886e
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-04 17:10:42 +0200

    [wip] tinysparql: fix build

--- pkgs/by-name/ti/tinysparql/package.nix
+++ pkgs/by-name/ti/tinysparql/package.nix
@@ -50,18 +50,6 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-Q6NgPH7vQqESPX8jfBIhpJ6Xioza/H+UP6vrUIREsJE=";
   };
 
-  patches = [
-    # sqlite changed the precision of float <-> text conversions, causing
-    # failures in the test suite. patch here until this appears in a release.
-    # https://gitlab.gnome.org/GNOME/tinysparql/-/work_items/496
-    # https://gitlab.gnome.org/GNOME/tinysparql/-/merge_requests/811
-    (fetchpatch {
-      name = "tinysparql-sqlite-double-value-precision.patch";
-      url = "https://gitlab.gnome.org/GNOME/tinysparql/-/commit/47d5bf9313d0ccb1feb7169eed9047d0e1597a39.patch";
-      hash = "sha256-k6eELZCEEtD8s7GiMckjTlf6QcAiUNY1Mraw7GROsm4=";
-    })
-  ];
-
   strictDeps = true;
 
   depsBuildBuild = [
