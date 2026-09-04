1fba7a4414419e9cd874e41b10cd5c6447efbac8
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-09-04 18:46:50 -0400

    llvmPackages.lldb: add Python runtime dependency

--- pkgs/development/compilers/llvm/common/lldb/default.nix
+++ pkgs/development/compilers/llvm/common/lldb/default.nix
@@ -106,6 +106,7 @@ stdenv.mkDerivation (
       libedit
       libxml2
       libllvm
+      python3
       # Starting with LLVM 16, the resource dir patch is no longer enough to get
       # libclang into the rpath of the lldb executables. By putting it into
       # buildInputs cc-wrapper will set up rpath correctly for us.
