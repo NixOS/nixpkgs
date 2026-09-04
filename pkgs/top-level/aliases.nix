0a3c210c39d5f9a53ac4e14c0d5d0c59b28f5dc8
Tom Hunze <dev@thunze.de>
2026-09-04 22:26:54 +0200

    gtksourceviewmm4: drop
    This version of gtksourceviewmm is unused and has been unmaintained
    upstream since December 2018 [1].
    
    [1] https://gitlab.gnome.org/GNOME/gtksourceviewmm/-/tree/gtksourceviewmm-4

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1102,6 +1102,7 @@ mapAliases {
   gtklp = throw "'gtklp' has been removed, as it depended on GTK 2. Consider using 'system-config-printer' instead."; # Added 2026-05-22
   gtkmm2 = throw "'gtkmm2' has been removed as it depended on the deprecated gtk2. Consider using 'gtkmm3' or 'gtkmm4' instead."; # Added 2026-08-09
   gtkradiant = throw "'gtkradiant' has been removed, as it relies on gtk2"; # Added 2026-06-18
+  gtksourceviewmm4 = throw "'gtksourceviewmm4' has been removed as this version of gtksourceviewmm was unused in nixpkgs and unmaintained upstream"; # Added 2026-09-04
   gtkspell2 = throw "'gtkspell2' has been removed as it depended on the deprecated GTK2 engine. Consider using 'gtkspell3' instead"; # Added 2026-07-23
   gtuber = throw "'gtuber' has been removed due to being discontinued by upstream."; # Added 2025-12-12
   gui-for-clash = throw "'gui-for-clash' has been removed, as it is unmaintained"; # Added 2026-05-28
