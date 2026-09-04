a0b99c4e96a032a76eebc7832752f6ebc35020db
Ladas552 <me@ladas552.me>
2026-09-04 18:29:04 +0500

    norgolith-plugin-sdk: 1.1.1 -> 1.2.0

--- pkgs/by-name/no/norgolith-plugin-sdk/package.nix
+++ pkgs/by-name/no/norgolith-plugin-sdk/package.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "norgolith-plugin-sdk";
-  version = "1.1.1";
+  version = "1.2.0";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "norgolith";
     repo = "core";
     tag = "norgolith-plugin-sdk-v${finalAttrs.version}";
-    hash = "sha256-jL8Ajv3K70t1eXttqtrm9WoN9QRW6aucK380iJn3ACw=";
+    hash = "sha256-XCcycFHAi3NAVGg7toCLMkVylV0kTAUb5CkLmvplR1w=";
   };
 
-  cargoHash = "sha256-KhsNCVZAFDWVlxTzkpTdDT6RqE9j+nejTj0botqywvM=";
+  cargoHash = "sha256-zNch2yFswY9L9cYckYB7P5lV82XYbFY7Zct9Bw6hjd8=";
 
   cargoRoot = "sdk";
   buildAndTestSubdir = "sdk";
@@ -24,7 +24,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   meta = {
     description = "SDK for building Norgolith plugins";
     homepage = "https://norgolith.dev";
-    changelog = "https://github.com/norgolith/core/releases/tag/norgolith-plugin-sdk-v${finalAttrs.version}";
+    changelog = "https://github.com/norgolith/core/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.gpl2Only;
     maintainers = [ lib.maintainers.Ladas552 ];
     mainProgram = "norgolith-plugin-sdk";
