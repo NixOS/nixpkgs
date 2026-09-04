c3b4f645b5a23f988e6d49a61a2b54836a46c2b2
Yifei Sun <ysun@hey.com>
2026-09-04 16:54:26 +0200

    ocamlPackages.utcp: init at 0.0.7

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -2248,6 +2248,8 @@ let
 
         uring = callPackage ../development/ocaml-modules/uring { };
 
+        utcp = callPackage ../development/ocaml-modules/utcp { };
+
         utop = callPackage ../development/tools/ocaml/utop { };
 
         uucd = callPackage ../development/ocaml-modules/uucd { };
