db01a9adbeda7fa4991c1d76f1b190f24a315874
Maximilian Bosch <maximilian@mbosch.me>
2026-09-04 13:31:02 +0200

    orcania: fix build w/ glibc-2.44
    ChangeLog: https://hydra.nixos.org/build/344174427

--- pkgs/by-name/or/orcania/package.nix
+++ pkgs/by-name/or/orcania/package.nix
@@ -5,6 +5,7 @@
   cmake,
   check,
   subunit,
+  fetchpatch,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "orcania";
@@ -16,6 +17,18 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "v${finalAttrs.version}";
     sha256 = "sha256-Cz3IE5UrfoWjMxQ/+iR1bLsYxf5DVN+7aJqLBcPjduA=";
   };
+
+  patches = [
+    # fix build w/ glibc-2.44
+    (fetchpatch {
+      url = "https://github.com/babelouest/orcania/commit/ee2f45b5da8b7fb2c747419c17880ccdca14521d.patch";
+      hash = "sha256-BNGL2Q5STrrAO8/OKMS4S5GqlikGnvg4hgBwKTMulgU=";
+    })
+    (fetchpatch {
+      url = "https://github.com/babelouest/orcania/commit/f261393b4dd1b4f50aca389916407e0dfa5f2e55.patch";
+      hash = "sha256-KyRedPj5gBFPZmefmjLL49OY8eJNmMyd5jsFsQByTUE=";
+    })
+  ];
 
   nativeBuildInputs = [ cmake ];
 
