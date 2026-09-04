fa1394d9fe9f062a594062e7f98f04e9ab650239
An Long <aisk1988@gmail.com>
2026-09-04 21:08:31 +0900

    maintainers: add aisk

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -967,6 +967,11 @@
     githubId = 44871469;
     name = "Etienne Wodey";
   };
+  aisk = {
+    github = "aisk";
+    githubId = 699636;
+    name = "An Long";
+  };
   aither64 = {
     email = "aither@havefun.cz";
     github = "aither64";
