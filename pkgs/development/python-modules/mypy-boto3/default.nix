28e9238863be40bdf15e88a31ef6e3778905d267
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-05 02:12:25 +0200

    python3Packages.mypy-boto3-mediatailor: 1.43.67 -> 1.43.89

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -886,8 +886,8 @@ in
       "sha256-8K0Xm6PMo+daS6xt4kBqaVvO2/LruFV7PVvugI7sNDU=";
 
   mypy-boto3-mediatailor =
-    buildMypyBoto3Package "mediatailor" "1.43.67"
-      "sha256-KByjjBZAkDzURyVPfYAgESJXKrHQFwqIKJHfuGbV92c=";
+    buildMypyBoto3Package "mediatailor" "1.43.89"
+      "sha256-K3D0CyrJxS4P6avsEq33xhlpp6Tl/mjEiVl46r6aKBc=";
 
   mypy-boto3-medical-imaging =
     buildMypyBoto3Package "medical-imaging" "1.43.4"
