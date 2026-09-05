c803c2bef42a589b4418b207cc55f0e837b4f18e
whispers <whispersofthedawn@duck.com>
2026-09-04 23:30:22 -0400

    libjpeg_turbo: 3.1.4.1 -> 3.2.0
    changelog: https://github.com/libjpeg-turbo/libjpeg-turbo/releases/tag/3.2.0
    diff: https://github.com/libjpeg-turbo/libjpeg-turbo/compare/3.1.4.1...3.2.0

--- pkgs/by-name/li/libjpeg_turbo/package.nix
+++ pkgs/by-name/li/libjpeg_turbo/package.nix
@@ -5,6 +5,7 @@
   cmake,
   nasm,
   openjdk,
+  jna,
   enableJava ? false, # whether to build the java wrapper
   enableJpeg7 ? false, # whether to build libjpeg with v7 compatibility
   enableJpeg8 ? false, # whether to build libjpeg with v8 compatibility
@@ -32,13 +33,13 @@ assert !(enableJpeg7 && enableJpeg8); # pick only one or none, not both
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libjpeg-turbo";
-  version = "3.1.4.1";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "libjpeg-turbo";
     repo = "libjpeg-turbo";
     tag = finalAttrs.version;
-    hash = "sha256-jBajigX4/j4jG11prTPeGkTVRrRzheFL/LxgnPufzb4=";
+    hash = "sha256-SPxWCDt9hFQ8uRaaKLkpWp9oPhfcRkDBm5MarTgdmV4=";
   };
 
   patches =
@@ -66,9 +67,11 @@ stdenv.mkDerivation (finalAttrs: {
   cmakeFlags = [
     (lib.cmakeBool "ENABLE_STATIC" enableStatic)
     (lib.cmakeBool "ENABLE_SHARED" enableShared)
-    (lib.cmakeBool "WITH_JAVA" enableJava)
     (lib.cmakeBool "WITH_JPEG7" enableJpeg7)
     (lib.cmakeBool "WITH_JPEG8" enableJpeg8)
+  ]
+  ++ lib.optionals enableJava [
+    (lib.cmakeFeature "WITH_JNA" "${jna}/share/java/jna.jar")
   ]
   ++ lib.optionals stdenv.hostPlatform.isRiscV [
     # https://github.com/libjpeg-turbo/libjpeg-turbo/issues/428
