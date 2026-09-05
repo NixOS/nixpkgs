4ed196cb4f85fa996d9c43b00be097db8d41b603
Jamie Magee <jamie.magee@gmail.com>
2026-09-04 19:11:48 -0700

    python3Packages.iseo-argo-ble: init at 0.9.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8538,6 +8538,8 @@ self: super: with self; {
 
   isbnlib = callPackage ../development/python-modules/isbnlib { };
 
+  iseo-argo-ble = callPackage ../development/python-modules/iseo-argo-ble { };
+
   islpy = callPackage ../development/python-modules/islpy { isl = pkgs.isl_0_27; };
 
   ismartgate = callPackage ../development/python-modules/ismartgate { };
