6891bc048f55fe55c9c2fba056a838021b444969
Hythera <87016780+Hythera@users.noreply.github.com>
2026-09-06 00:20:53 +0200

    cel-go: 0.31.0 -> 0.32.0
    changelog: https://github.com/cel-expr/cel-go/releases/tag/v0.32.0
    
    diff: https://github.com/cel-expr/cel-go/compare/v0.31.0...v0.32.0

--- pkgs/by-name/ce/cel-go/package.nix
+++ pkgs/by-name/ce/cel-go/package.nix
@@ -3,41 +3,21 @@
   buildGoModule,
   fetchFromGitHub,
 }:
-let
-  cel-spec = buildGoModule (finalAttrs: {
-    pname = "cel-spec";
-    version = "0.25.2";
 
-    src = fetchFromGitHub {
-      owner = "cel-expr";
-      repo = "cel-spec";
-      tag = "v${finalAttrs.version}";
-      hash = "sha256-aNyBGUlpTqILCiQHo7BxaZShI6q9xgtRegywd+jQSlo=";
-    };
-
-    vendorHash = "sha256-7Ngemih4jRO6VHSH2QxU/p1Q/E/ukUZ5wuUbZzRj6kA=";
-
-    installPhase = ''
-      runHook preInstall
-      cp -r . $out
-      runHook postInstall
-    '';
-  });
-in
 buildGoModule (finalAttrs: {
   pname = "cel-go";
-  version = "0.31.0";
+  version = "0.32.0";
 
   src = fetchFromGitHub {
     owner = "cel-expr";
     repo = "cel-go";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-d2qteEY7aRKvzD+uF7uNibUv5dECaL61NogbZfd3cAQ=";
+    hash = "sha256-sFS6Kei7KpNFdjR5pyicTZdIDYZi5Juxab4YzgB25sM=";
   };
 
   modRoot = "repl";
 
-  vendorHash = "sha256-SetkDfzfR/zqkNirGeGlczvV5h/CM9GiAPP2pKq6QDU=";
+  vendorHash = "sha256-kJhpVH+Ak2/yICshXreVgPs5W/Cq63l+FwUMW6G1l8k=";
 
   subPackages = [
     "main"
@@ -48,24 +28,9 @@ buildGoModule (finalAttrs: {
     "-w"
   ];
 
-  postPatch = ''
-    substituteInPlace repl/go.mod \
-      --replace-fail "../../cel-spec" "./cel-spec"
-  '';
-
-  preBuild = ''
-    mkdir cel-spec
-    cp -r ${cel-spec}/* cel-spec
-  '';
-
   postInstall = ''
     mv $out/bin/{main,cel-go}
   '';
-
-  passthru = {
-    inherit cel-spec;
-    updateScript = ./update.sh;
-  };
 
   meta = {
     changelog = "https://github.com/cel-expr/cel-go/releases/tag/${finalAttrs.src.tag}";
