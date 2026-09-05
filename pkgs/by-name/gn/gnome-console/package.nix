46c9a6345599c034bd5c2c4d279f8a2e41f42bb4
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-05 19:36:14 +0200

    gnome-console: 50.0 -> 51.rc

--- pkgs/by-name/gn/gnome-console/package.nix
+++ pkgs/by-name/gn/gnome-console/package.nix
@@ -21,11 +21,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-console";
-  version = "50.0";
+  version = "51.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-console/${lib.versions.major finalAttrs.version}/gnome-console-${finalAttrs.version}.tar.xz";
-    hash = "sha256-5JUCB/BUfmpsDxjuv89uGhBGHqsPL64KrlErEETHrG4=";
+    hash = "sha256-j3Lg3hfHA0TKhDuJYYpSWf6ThJqeg3FUv41ahGgzPIY=";
   };
 
   nativeBuildInputs = [
