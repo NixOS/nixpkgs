6bb778e44c5811edaf34e2cf177dfd6f949b65a9
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-06 14:28:38 +0000

    ocamlPackages.dockerfile: 8.4.1 -> 8.4.3

--- pkgs/development/ocaml-modules/dockerfile/default.nix
+++ pkgs/development/ocaml-modules/dockerfile/default.nix
@@ -10,13 +10,13 @@
 
 buildDunePackage (finalAttrs: {
   pname = "dockerfile";
-  version = "8.4.1";
+  version = "8.4.3";
 
   src = fetchFromGitHub {
     owner = "ocurrent";
     repo = "ocaml-dockerfile";
     tag = finalAttrs.version;
-    hash = "sha256-xI+kjIwaGd7K9zLyoBgN2AFmQ/rL0/C/hPwrEgHkCgs=";
+    hash = "sha256-uQlsSTetRq9llK/dYOlIvK7nnSmDwo7zDzIR7JdMw8I=";
   };
 
   propagatedBuildInputs = [
