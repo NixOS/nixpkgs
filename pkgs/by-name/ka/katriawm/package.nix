3fae8b9a197dc02732c461247fe8dfb15b482fbf
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-06 15:08:21 +0000

    katriawm: 26.03 -> 26.08

--- pkgs/by-name/ka/katriawm/package.nix
+++ pkgs/by-name/ka/katriawm/package.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "katriawm";
-  version = "26.03";
+  version = "26.08";
 
   src = fetchzip {
     url = "https://www.uninformativ.de/git/katriawm/archives/katriawm-v${finalAttrs.version}.tar.gz";
-    hash = "sha256-vnnc5SkNzCLZTBxKcaHDo9F5f++7dtESD5hOB0zrxjo=";
+    hash = "sha256-q7HwQd/19eV8g2FAJypWWgj7tKphqIL1VusjXtBSnpY=";
   };
 
   nativeBuildInputs = [ pkg-config ];
