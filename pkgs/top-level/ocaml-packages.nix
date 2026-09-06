7db72799a5a1f790aad969cd64b046edf764ba14
Yifei Sun <ysun@hey.com>
2026-09-06 21:46:35 +0200

    ocamlPackages.mirage-solo5: init at 0.10.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1377,6 +1377,8 @@ let
 
         mirage-sleep = callPackage ../development/ocaml-modules/mirage-sleep { };
 
+        mirage-solo5 = callPackage ../development/ocaml-modules/mirage-solo5 { };
+
         mirage-time = callPackage ../development/ocaml-modules/mirage-time { };
 
         mirage-time-unix = callPackage ../development/ocaml-modules/mirage-time/unix.nix { };
