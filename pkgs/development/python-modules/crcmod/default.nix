5c2f79b8030f31dc1040bcf8a8258ae73285c263
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:16 +0700

    python3Packages.crcmod: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/crcmod/default.nix
+++ pkgs/development/python-modules/crcmod/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "crcmod";
   version = "1.7";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "07k0hgr42vw2j92cln3klxka81f33knd7459cn3d8aszvfh52w6w";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     description = "Python module for generating objects that compute the Cyclic Redundancy Check (CRC)";
