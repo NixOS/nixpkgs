8b78d47c1791eafd177fe2884275a312f4ada6f1
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:17 +0700

    python3Packages.cson: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/cson/default.nix
+++ pkgs/development/python-modules/cson/default.nix
@@ -2,20 +2,23 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   speg,
 }:
 
 buildPythonPackage rec {
   pname = "cson";
   version = "0.8";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-7owBZvzR9ReJiHGX4+g1Sse++jlvwpcGvOta8l7cngE=";
   };
 
-  propagatedBuildInputs = [ speg ];
+  build-system = [ setuptools ];
+
+  dependencies = [ speg ];
 
   pythonImportsCheck = [ "cson" ];
 
