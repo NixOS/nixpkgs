e798e35e8be46f4055e441e2362af4d613be2aa2
sorooris <10185314+sorooris@users.noreply.github.com>
2026-09-07 21:05:14 +0200

    civicrm: split core from standalone

--- pkgs/by-name/ci/civicrm-core/package.nix
+++ pkgs/by-name/ci/civicrm-core/package.nix
@@ -30,9 +30,9 @@ php.buildComposerProject2 (finalAttrs: {
   meta = {
     homepage = "https://civicrm.org/";
     changelog = "https://download.civicrm.org/release/${finalAttrs.version}";
-    description = "Standalone version of CiviCRM, a CRM software for non-profit organizations";
+    description = "Core version of CiviCRM, a CRM software for non-profit organizations";
     license = lib.licenses.agpl3Plus;
     maintainers = [ lib.maintainers.sorooris ];
-    mainProgram = "civicrm";
+    mainProgram = "civicrm-core";
   };
 })
