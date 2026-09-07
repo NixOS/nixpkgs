3fb54bd04d84964474d94a4b496188b3dc26a85b
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 17:53:17 +0000

    gnome-calculator: 50.0 -> 51.rc
    https://gitlab.gnome.org/GNOME/gnome-calculator/-/compare/50.0...51.rc

--- pkgs/by-name/gn/gnome-calculator/package.nix
+++ pkgs/by-name/gn/gnome-calculator/package.nix
@@ -29,11 +29,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-calculator";
-  version = "50.0";
+  version = "51.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-calculator/${lib.versions.major finalAttrs.version}/gnome-calculator-${finalAttrs.version}.tar.xz";
-    hash = "sha256-gFPWiRVl6IKHS2XB21HFvzEABet4i4usNUY5B0M1CpA=";
+    hash = "sha256-24CgNXkGQubxslylG8ZMyi7rGqXHZg/UwTJaorvKHYE=";
   };
 
   patches = [
