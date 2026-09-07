e96c3d47cb9661af23e0cf368824cc4756259a69
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 17:55:58 +0000

    gnome-session: 50.1 -> 51.rc
    https://gitlab.gnome.org/GNOME/gnome-session/-/compare/50.1...51.rc

--- pkgs/by-name/gn/gnome-session/package.nix
+++ pkgs/by-name/gn/gnome-session/package.nix
@@ -27,7 +27,7 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-session";
   # Also bump ./ctl.nix when bumping major version.
-  version = "50.1";
+  version = "51.rc";
 
   outputs = [
     "out"
@@ -36,7 +36,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-session/${lib.versions.major finalAttrs.version}/gnome-session-${finalAttrs.version}.tar.xz";
-    hash = "sha256-Yom2r6RNPkyZnOV2H/iywQujCfVflCXysT+YIIyB9vs=";
+    hash = "sha256-DO4CZpjmr3m0HG7TKp/cy/L2fpKxV7F6BG7FpkPxEuU=";
   };
 
   patches = [
