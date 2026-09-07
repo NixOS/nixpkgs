ff6870f9eab6933ebde210e8a9a290caa4584be0
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 18:03:27 +0000

    nautilus: 50.2.2 -> 51.rc
    https://gitlab.gnome.org/GNOME/nautilus/-/compare/50.2.2...51.rc

--- pkgs/by-name/na/nautilus/package.nix
+++ pkgs/by-name/na/nautilus/package.nix
@@ -45,7 +45,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "nautilus";
-  version = "50.2.2";
+  version = "51.rc";
 
   outputs = [
     "out"
@@ -55,7 +55,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/nautilus/${lib.versions.major finalAttrs.version}/nautilus-${finalAttrs.version}.tar.xz";
-    hash = "sha256-4eKF7930LtMN2lsp9/jSQtq0vBQJqQVIY7NnutSzTVo=";
+    hash = "sha256-Fo4WhvApzKjEGEOKFdrmJFy2QFjh70h0EraHdOdKyC8=";
   };
 
   patches = [
