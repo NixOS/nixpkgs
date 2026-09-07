158c77c85779c8fa448300bbcd5fd5cf3f5b9f84
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-09-07 21:01:08 +1000

    lxgw-neoxihei: modernize

--- pkgs/by-name/lx/lxgw-neoxihei/package.nix
+++ pkgs/by-name/lx/lxgw-neoxihei/package.nix
@@ -3,13 +3,12 @@
   fetchurl,
   stdenvNoCC,
 }:
-
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "lxgw-neoxihei";
   version = "1.305";
 
   src = fetchurl {
-    url = "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v${version}/LXGWNeoXiHei.ttf";
+    url = "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v${finalAttrs.version}/LXGWNeoXiHei.ttf";
     hash = "sha256-iTz77GBHaPA3hatPNpUI3mjCdCczWaXK3uF+KFKXXVw=";
   };
 
@@ -23,6 +22,8 @@ stdenvNoCC.mkDerivation rec {
     runHook postInstall
   '';
 
+  __structuredAttrs = true;
+
   meta = {
     description = "Simplified Chinese sans-serif font derived from IPAex Gothic";
     homepage = "https://github.com/lxgw/LxgwNeoXiHei";
@@ -30,4 +31,4 @@ stdenvNoCC.mkDerivation rec {
     platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ zendo ];
   };
-}
+})
