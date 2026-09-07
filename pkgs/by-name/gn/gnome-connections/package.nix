a742bc1b8bd81962cd2eb3bccd6d1a8d84494b36
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 17:54:09 +0000

    gnome-connections: 50.0 -> 51.rc.1
    https://gitlab.gnome.org/GNOME/gnome-connections/-/compare/50.0...51.rc.1

--- pkgs/by-name/gn/gnome-connections/package.nix
+++ pkgs/by-name/gn/gnome-connections/package.nix
@@ -24,11 +24,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-connections";
-  version = "50.0";
+  version = "51.rc.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-connections/${lib.versions.major finalAttrs.version}/gnome-connections-${finalAttrs.version}.tar.xz";
-    hash = "sha256-Vnv2NcbTA66Ex083yE35+4/Pal6d/0UuFBTGcXRNldA=";
+    hash = "sha256-JEdMFwgMwe+X7fdFtvI893CW8mjOL2zzx4L/A/20POE=";
   };
 
   nativeBuildInputs = [
