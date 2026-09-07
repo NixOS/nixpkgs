5bef790a8788c9bb2a931f7e61f6134537e60e53
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 18:04:43 +0000

    python3.pkgs.pygobject3: 3.56.3 -> 3.58.0
    https://gitlab.gnome.org/GNOME/pygobject/-/compare/3.56.3...3.58.0

--- pkgs/development/python-modules/pygobject/3.nix
+++ pkgs/development/python-modules/pygobject/3.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "pygobject";
-  version = "3.56.3";
+  version = "3.58.0";
 
   outputs = [
     "out"
@@ -28,7 +28,7 @@ buildPythonPackage rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/pygobject/${lib.versions.majorMinor version}/pygobject-${version}.tar.gz";
-    hash = "sha256-EnYOSg49BLbrleBveifjYsgm1WfqYTNzqSwAO2xw0tY=";
+    hash = "sha256-RQaGl94//kaEDKNpcF8jEYs0209962P27/B5pnNN3Mo=";
   };
 
   depsBuildBuild = [ pkg-config ];
