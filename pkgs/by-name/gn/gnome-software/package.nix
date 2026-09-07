c3ca9737c89bb7f1a4424b4733b0f6cd570edf06
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 17:56:35 +0000

    gnome-software: 50.3 -> 51.rc
    https://gitlab.gnome.org/GNOME/gnome-software/-/compare/50.3...51.rc

--- pkgs/by-name/gn/gnome-software/package.nix
+++ pkgs/by-name/gn/gnome-software/package.nix
@@ -48,11 +48,11 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-software";
-  version = "50.3";
+  version = "51.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-software/${lib.versions.major finalAttrs.version}/gnome-software-${finalAttrs.version}.tar.xz";
-    hash = "sha256-sTGOaPArs5AvzY+QTVbwP1NOpQmPZeTGu5wskk2n+CM=";
+    hash = "sha256-EOIAWJus1VESHU6AUXTnsCcUlaFliRef1Nu1Bzjy6Tc=";
   };
 
   patches = [
