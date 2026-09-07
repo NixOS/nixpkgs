7d0909dfb0aec3c56048778c193551d357dc5a38
Che-0129 <yuto8664@gmail.com>
2026-09-07 17:16:39 +0900

    syshud: 0-unstable-2026-05-28 -> 0-unstable-2026-09-06

--- pkgs/by-name/sy/syshud/package.nix
+++ pkgs/by-name/sy/syshud/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "syshud";
-  version = "0-unstable-2026-05-28";
+  version = "0-unstable-2026-09-06";
 
   src = fetchFromGitHub {
     owner = "System64fumo";
     repo = "syshud";
-    rev = "4d0c4775fe38a14bd68fc654f8cc4ad624eed92a";
-    hash = "sha256-3DqBg3RUaEJCG5o/pt2B/+m/8X+Ifu9uHLJpjzLo9t8=";
+    rev = "f97c3314e7db340788d305907ca903aaaa9ded2f";
+    hash = "sha256-0ZfLuSKVe0q3KrdI+MOrBqGb9Bx+odc3D0f1xhP180o=";
   };
 
   postPatch = ''
