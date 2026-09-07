0ae4f2c3528563279db6cdf3d7c6fa7f92b6672d
Tristan Ross <tristan.ross@determinate.systems>
2026-09-06 21:09:47 -0700

    llvmPackages_git: 24.0.0-unstable-2026-08-30 -> 24.0.0-unstable-2026-09-06

--- pkgs/development/compilers/llvm/default.nix
+++ pkgs/development/compilers/llvm/default.nix
@@ -29,9 +29,9 @@ let
     "22.1.8".officialRelease.sha256 = "sha256-SF7wFuh4kXZTytpdgX7vUZItKtRobnVICm+ixze4iG0=";
     "23.1.0".officialRelease.sha256 = "sha256-Astfi1UDDcydyws3Q1sELqho/PxiNN/tvCtmCGj5FoE=";
     "24.0.0-git".gitRelease = {
-      rev = "b37a8a70cc98915d157cac2f049e9f46d6da8fe5";
-      rev-version = "24.0.0-unstable-2026-08-30";
-      sha256 = "sha256-V5CD9L9ParldV8cnxPX2HuFNv1tpL4EZy1pAqK82BL4=";
+      rev = "c6fd19f0fe4eb4f9ddd978eb9a6dbc8039acc3dd";
+      rev-version = "24.0.0-unstable-2026-09-06";
+      sha256 = "sha256-FzuV15Z0lg7srqOdztKM5vS4YJY5kLZW0rKScb/VG3c=";
     };
   }
   // llvmVersions;
