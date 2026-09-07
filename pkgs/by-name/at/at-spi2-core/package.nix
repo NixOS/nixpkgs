8c0974abaf3adff4db9477d6b31c3cdb7d0100d4
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 17:49:58 +0000

    at-spi2-core: 2.60.6 -> 2.61.90
    https://gitlab.gnome.org/GNOME/at-spi2-core/-/compare/2.60.6...2.61.90

--- pkgs/by-name/at/at-spi2-core/package.nix
+++ pkgs/by-name/at/at-spi2-core/package.nix
@@ -29,7 +29,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "at-spi2-core";
-  version = "2.60.6";
+  version = "2.61.90";
 
   outputs = [
     "out"
@@ -39,7 +39,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/at-spi2-core/${lib.versions.majorMinor finalAttrs.version}/at-spi2-core-${finalAttrs.version}.tar.xz";
-    hash = "sha256-qJtkqLIXqAQr3w41y/q2Kc7uNWQNunXfV4r96ap4nVc=";
+    hash = "sha256-8JnWINKkiZf807ikoES2AVblnBFoV6y8Z2IIVW71QMM=";
   };
 
   nativeBuildInputs = [
