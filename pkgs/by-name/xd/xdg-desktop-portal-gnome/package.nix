0c11e5ba316164b3c31860b6a9ec11c532c3c011
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 18:06:34 +0000

    xdg-desktop-portal-gnome: 50.0 -> 51.rc
    https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome/-/compare/50.0...51.rc

--- pkgs/by-name/xd/xdg-desktop-portal-gnome/package.nix
+++ pkgs/by-name/xd/xdg-desktop-portal-gnome/package.nix
@@ -23,11 +23,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xdg-desktop-portal-gnome";
-  version = "50.0";
+  version = "51.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/xdg-desktop-portal-gnome/${lib.versions.major finalAttrs.version}/xdg-desktop-portal-gnome-${finalAttrs.version}.tar.xz";
-    hash = "sha256-zu7y+2izSz9mo97wozKiKnCvJyZB+2xQBlt6L949V1k=";
+    hash = "sha256-Enw1nxA1T994vNqUZZc+Ibs1Co+7Ifk472EYHqkGzyc=";
   };
 
   nativeBuildInputs = [
