2c5480d4c466e8a421578cf3ed15907248d374f0
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 17:51:31 +0000

    gdm: 50.2 -> 51.rc
    https://gitlab.gnome.org/GNOME/gdm/-/compare/50.2...51.rc

--- pkgs/by-name/gd/gdm/package.nix
+++ pkgs/by-name/gd/gdm/package.nix
@@ -44,7 +44,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gdm";
-  version = "50.2";
+  version = "51.rc";
 
   outputs = [
     "out"
@@ -53,7 +53,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gdm/${lib.versions.major finalAttrs.version}/gdm-${finalAttrs.version}.tar.xz";
-    hash = "sha256-ikZ55Hzd/osKG9nTg2BC3raPs+Gw5+Yt/QYx3FtvxUE=";
+    hash = "sha256-oe20nWbQIldZfmtJ3hMW7mfuPMGdjcRkbVTBfHrhnbo=";
   };
 
   mesonFlags = [
