3a369626d2ffcc8350b2399dc7959e378420b174
Marco "Capypara" Köpcke <hello@capypara.de>
2026-09-07 17:55:13 +0000

    gnome-maps: 50.3 -> 51.rc
    https://gitlab.gnome.org/GNOME/gnome-maps/-/compare/50.3...51.rc

--- pkgs/by-name/gn/gnome-maps/package.nix
+++ pkgs/by-name/gn/gnome-maps/package.nix
@@ -31,11 +31,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-maps";
-  version = "50.3";
+  version = "51.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-maps/${lib.versions.major finalAttrs.version}/gnome-maps-${finalAttrs.version}.tar.xz";
-    hash = "sha256-CJyo4Vq9cqyD2zhs7xnOveBe74t26qyBkGpLBTVUTiw=";
+    hash = "sha256-e4rRxNaNZwL63LWuk1NTByRK6rsBXEosL4ZctFGtdLo=";
   };
 
   doCheck = !stdenv.hostPlatform.isDarwin;
