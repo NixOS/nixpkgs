82590127c1da39ef308c70aec4edd5dba5181c8c
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-08 09:31:48 +0200

    python3Packages.llama-index-llms-ollama: migrate to finalAttrs

--- pkgs/development/python-modules/llama-index-llms-ollama/default.nix
+++ pkgs/development/python-modules/llama-index-llms-ollama/default.nix
@@ -7,14 +7,14 @@
   ollama,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "llama-index-llms-ollama";
   version = "0.11.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "llama_index_llms_ollama";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-5rjKwgCp/2fK6lMf7OCmJyqJ41BrK5ZI7HCaz/5KeYA=";
   };
 
@@ -36,4 +36,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
