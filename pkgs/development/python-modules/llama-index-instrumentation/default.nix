d3e1a5c8e2c055511ea2ef90972c295de39397a6
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-08 09:30:54 +0200

    python3Packages.llama-index-instrumentation: migrate to finalAttrs

--- pkgs/development/python-modules/llama-index-instrumentation/default.nix
+++ pkgs/development/python-modules/llama-index-instrumentation/default.nix
@@ -7,14 +7,14 @@
   pydantic,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "llama-index-instrumentation";
   version = "0.6.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "llama_index_instrumentation";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-sYXE4op/MomcJ2ScwuLX1UJn+i/wzUPL4rUhK66Y/jo=";
   };
 
@@ -35,4 +35,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
