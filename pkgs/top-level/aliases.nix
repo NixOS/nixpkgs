e3d7bc3b834320fa7003edc746c56c2b074c1cc6
Maximilian Bosch <maximilian@mbosch.me>
2026-09-08 17:44:39 +0200

    ctx: drop
    Unmaintained and not updated in three years. Also doesn't build with
    latest glibc anymore.
    
    Failing Hydra build: https://hydra.nixos.org/build/344144642

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -640,6 +640,7 @@ mapAliases {
   csslint = throw "'csslint' has been removed as upstream considers it abandoned."; # Added 2025-11-07
   cstore_fdw = throw "'cstore_fdw' has been removed. Use 'postgresqlPackages.cstore_fdw' instead."; # Added 2025-07-19
   ctpp2 = throw "'ctpp2' has been removed due to lack of maintenance."; # Added 2025-12-31
+  ctx = throw "'ctx' was unmaintained and not updated for three years"; # Added 2026-09-08
   cudaPackages_11 = throw "CUDA 11 has been removed from Nixpkgs, as it is unmaintained upstream and depends on unsupported compilers"; # Added 2025-08-08
   cudaPackages_11_0 = throw "CUDA 11.0 has been removed from Nixpkgs, as it is unmaintained upstream and depends on unsupported compilers"; # Added 2025-08-08
   cudaPackages_11_1 = throw "CUDA 11.1 has been removed from Nixpkgs, as it is unmaintained upstream and depends on unsupported compilers"; # Added 2025-08-08
