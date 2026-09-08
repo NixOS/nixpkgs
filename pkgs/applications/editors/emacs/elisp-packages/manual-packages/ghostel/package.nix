c847d447776141a3ad259209f72e9576c925afb0
yvnth <yashupress@gmail.com>
2026-09-08 20:22:29 +0530

    emacsPackages.ghostel: 0.52.0 -> 0.53.0

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/ghostel/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/ghostel/package.nix
@@ -58,13 +58,13 @@ in
 melpaBuild (finalAttrs: {
   pname = "ghostel";
 
-  version = "0.52.0";
+  version = "0.53.0";
 
   src = fetchFromGitHub {
     owner = "dakra";
     repo = "ghostel";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Z+MPF/2d3JMx1KCExQ3giE3fHD9zY3alFj2QcVbtpWk=";
+    hash = "sha256-ZzRf/3QoG499S2bJhA+wnphqX0KDiY+Pe7yENKh8AV4=";
   };
 
   # these can be put into mkModule, but we put them here to ease user overrideAttrs
