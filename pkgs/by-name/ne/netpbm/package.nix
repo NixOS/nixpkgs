dc6a5e1d8799bbd99f1503ed5db52166fe3b980b
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-08 02:04:45 +0000

    netpbm: 11.15.3 -> 11.15.7

--- pkgs/by-name/ne/netpbm/package.nix
+++ pkgs/by-name/ne/netpbm/package.nix
@@ -21,7 +21,7 @@ stdenv.mkDerivation (finalAttrs: {
   # Determine version and revision from:
   # https://sourceforge.net/p/netpbm/code/HEAD/log/?path=/advanced
   pname = "netpbm";
-  version = "11.15.3";
+  version = "11.15.7";
 
   outputs = [
     "bin"
@@ -31,8 +31,8 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchsvn {
     url = "https://svn.code.sf.net/p/netpbm/code/advanced";
-    rev = "5264";
-    sha256 = "sha256-5G2OitW25ZNsdBcVkKfLpFJWjTm9VcB3ca0QllOSugI=";
+    rev = "5318";
+    sha256 = "sha256-LEsj3vSrq1Up8ZYtko6oWEytOU1KgsIJeZ1KHFbiJCg=";
   };
 
   nativeBuildInputs = [
