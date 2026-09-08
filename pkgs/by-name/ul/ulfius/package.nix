bae8a721d98b1b4ede0f6c04cec505dcf6a691ac
Maximilian Bosch <maximilian@mbosch.me>
2026-09-08 23:09:25 +0200

    ulfius: fix build w/ glibc-2.44
    Failing Hydra build: https://hydra.nixos.org/build/344467071

--- pkgs/by-name/ul/ulfius/package.nix
+++ pkgs/by-name/ul/ulfius/package.nix
@@ -40,6 +40,9 @@ stdenv.mkDerivation (finalAttrs: {
     cmake
   ];
 
+  # Fix build w/ glibc-2.44
+  env.NIX_CFLAGS_COMPILE = "-Wno-error=discarded-qualifiers";
+
   propagatedBuildInputs = [
     libmicrohttpd
     orcania
