100c0b7b2e7991eacdd8965798d8fd5e1a031d9f
Kaj Kowalski <info@kajkowalski.nl>
2026-09-09 21:53:34 +0200

    maintainers: add kjanat

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15212,6 +15212,12 @@
     githubId = 49951907;
     name = "Karl Zschiebsch";
   };
+  kjanat = {
+    email = "info@kajkowalski.nl";
+    github = "kjanat";
+    githubId = 6353477;
+    name = "Kaj Kowalski";
+  };
   kjeremy = {
     email = "kjeremy@gmail.com";
     name = "Jeremy Kolb";
