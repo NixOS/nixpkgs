84a72fdbd364d7fe84bea89b0e64278a1cf2e423
David McFarland <corngood@gmail.com>
2026-09-10 15:13:44 -0300

    dotnet-sdk_{8..11}: enable __structuredAttrs
    (cherry picked from commit ee4d12a81b8ef5edc9fd099dfd14cfc13a8a7361)

--- pkgs/development/compilers/dotnet/source/vmr.nix
+++ pkgs/development/compilers/dotnet/source/vmr.nix
@@ -69,6 +69,7 @@ stdenv.mkDerivation {
   inherit version;
 
   strictDeps = true;
+  __structuredAttrs = true;
 
   # TODO: fix this in the binary sdk packages
   preHook = lib.optionalString stdenv.hostPlatform.isDarwin ''
@@ -377,7 +378,7 @@ stdenv.mkDerivation {
       dotnet nuget add source "${bootstrapSdk.artifacts}"
     ''
     + ''
-      ${prepScript} $prepFlags
+      ${prepScript} "''${prepFlags[@]}"
     ''
     + lib.optionalString (!hasRuntime) ''
       mkdir .shared-components
@@ -385,7 +386,7 @@ stdenv.mkDerivation {
       chmod +w -R .shared-components/
       # zip dependencies unzipped in bootstrap installPhase, so they can be found
       find .shared-components/assets . -name \*.tar -exec gzip -f --fast {} \;
-      buildFlags+=\ --with-shared-components\ "$PWD"/.shared-components
+      buildFlags+=(--with-shared-components "$PWD"/.shared-components)
     ''
     + ''
 
@@ -450,7 +451,7 @@ stdenv.mkDerivation {
     version= \
     CLR_CC=$(command -v clang) \
     CLR_CXX=$(command -v clang++) \
-      ./build.sh $buildFlags
+      ./build.sh "''${buildFlags[@]}"
 
     runHook postBuild
   '';
