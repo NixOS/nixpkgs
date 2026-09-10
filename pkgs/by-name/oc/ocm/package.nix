9d3b0cf51a995332033fe8c9a29d4d64a6fa5dcf
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-10 18:38:43 +0000

    ocm: 1.0.15 -> 1.0.16

--- pkgs/by-name/oc/ocm/package.nix
+++ pkgs/by-name/oc/ocm/package.nix
@@ -10,16 +10,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "ocm";
-  version = "1.0.15";
+  version = "1.0.16";
 
   src = fetchFromGitHub {
     owner = "openshift-online";
     repo = "ocm-cli";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-/WSWifl5UH34d2/I/874ZTHzJwI0QyoXq5QGuz4dzZE=";
+    sha256 = "sha256-jIUpr1lzRWqilB5LISJb/hbtdZeyHAAZmTooHAxAPuo=";
   };
 
-  vendorHash = "sha256-1+H2DoSsPqg6JXWLVFyQ4eBTU3EqySTA2fe9BBWbjMg=";
+  vendorHash = "sha256-cIp5loQRzblChtJAhQNhsx4usHdgLW//N7FxYGEPJvw=";
 
   # Strip the final binary.
   ldflags = [
