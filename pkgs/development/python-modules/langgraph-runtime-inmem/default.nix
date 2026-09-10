beb2bb82b4ce97331b3252dcfd7c8e37d71d4c41
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-10 23:15:58 +0000

    python3Packages.langgraph-runtime-inmem: 0.32.3 -> 0.34.0

--- pkgs/development/python-modules/langgraph-runtime-inmem/default.nix
+++ pkgs/development/python-modules/langgraph-runtime-inmem/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "langgraph-runtime-inmem";
-  version = "0.32.3";
+  version = "0.34.0";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -22,7 +22,7 @@ buildPythonPackage (finalAttrs: {
   src = fetchPypi {
     pname = "langgraph_runtime_inmem";
     inherit (finalAttrs) version;
-    hash = "sha256-k5ATRuxkds7dDZx3lxGJUL+vUgVFHe8/BaAZqoaaxd4=";
+    hash = "sha256-CjFqR+ezMGcb+3mCcBoxiwyvJZBcmtBiQUY2WGX0yeU=";
   };
 
   build-system = [ hatchling ];
