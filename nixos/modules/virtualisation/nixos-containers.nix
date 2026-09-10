728063292e2e2547c494399201785b5055ff7c11
Robert Hensing <robert@roberthensing.nl>
2026-09-10 15:58:52 +0200

    nixos/tests/containers-eval: init + improve error messages

--- nixos/modules/virtualisation/nixos-containers.nix
+++ nixos/modules/virtualisation/nixos-containers.nix
@@ -1016,6 +1016,7 @@ in
             hostNixSocketIsDisabled
             ;
 
+          # Tested in: nixos/tests/containers-eval.nix
           mapper =
             name:
             { cfg, opt }:
@@ -1032,7 +1033,7 @@ in
                   && cfg.config.nix.enable
                   && cfg.config.nix.daemon.enable
                 )
-                "${options.containers}.${strings.escapeNixIdentifier name} requires a Nix daemon but the host does not provide it, as ${hostNixSocketIsDisabled}";
+                "${options.containers}.${strings.escapeNixIdentifier name} has nix.daemon.enable = true, but the host does not provide a nix daemon socket, as ${hostNixSocketIsDisabled}. Disable nix.daemon.enable in the container, or enable the daemon on the host.";
         in
         (lib.concatMap
           # This could be done in mapper but causes a reformat
