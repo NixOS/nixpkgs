3254cad473d7ac527cf79ef4a39d971116d3a00e
Kentaro Wuertele <ken@wuertele.com>
2026-09-09 20:46:46 -0700

    kanjivg: init at r20260714

---
+++ pkgs/by-name/ka/kanjivg/package.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "kanjivg";
+  version = "r20260714";
+  src = fetchFromGitHub {
+    owner = "KanjiVG";
+    repo = "kanjivg";
+    rev = "d95a97627fd9fe5b2c8d06ca81e38149609c0c1e";
+    hash = "sha256-gRQUu3PNFuGIhEAqNGiAcq50jZOdaHM7HQlhabubauk=";
+  };
+
+  buildPhase = ''
+    mkdir -p $out
+    cp kanji/* $out
+  '';
+
+  meta = {
+    description = "Stroke order diagrams for Japanese kanji";
+    homepage = "https://kanjivg.tagaini.net/";
+    license = lib.licenses.cc-by-sa-30;
+    maintainers = with lib.maintainers; [ kentario ];
+  };
+}
