99e0c102e0a1e4c9ed8696bbc5e98e35cbc8c1d0
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-11 03:25:02 +0000

    gearboy: 3.8.12 -> 3.8.15

--- pkgs/by-name/ge/gearboy/package.nix
+++ pkgs/by-name/ge/gearboy/package.nix
@@ -16,13 +16,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "gearboy";
-  version = "3.8.12";
+  version = "3.8.15";
 
   src = fetchFromGitHub {
     owner = "drhelius";
     repo = "Gearboy";
     tag = finalAttrs.version;
-    hash = "sha256-5R0ZMgXd/t9AgbaJJNHt71fLD9jiUX74xZ8mkvRsPRk=";
+    hash = "sha256-3djwosL254pfiNjMgIGDY8LtGcmV21s2hga0ILhCnbg=";
   };
 
   __structuredAttrs = true;
