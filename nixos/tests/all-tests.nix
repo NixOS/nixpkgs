57ae5acb53706824e64bcb2f1245d89b6346c35b
h7x4 <h7x4@nani.wtf>
2026-09-11 11:54:41 +0900

    nixos/tests/duplicity: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -583,6 +583,7 @@ in
   druid = handleTestOn [ "x86_64-linux" ] ./druid { };
   drupal = runTest ./drupal.nix;
   dublin-traceroute = runTest ./dublin-traceroute.nix;
+  duplicity = runTest ./duplicity.nix;
   dwl = runTestOn [ "x86_64-linux" "aarch64-linux" ] ./dwl.nix;
   e57inspector = import ./e57inspector.nix { inherit pkgs runTest; };
   early-mount-options = runTest ./early-mount-options.nix;
