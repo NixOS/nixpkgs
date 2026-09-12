19b34b398ea4723e2618c27bd4ea83dc7496fa79
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-12 18:28:14 +0000

    nginxModules.oidc: 0.6.0 -> 0.8.0

--- pkgs/servers/http/nginx/modules/oidc/package.nix
+++ pkgs/servers/http/nginx/modules/oidc/package.nix
@@ -7,14 +7,14 @@
 }:
 mkNginxPlugin (finalAttrs: {
   pname = "oidc";
-  version = "0.6.0";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "kjdev";
     repo = "nginx-oidc";
     rev = finalAttrs.version;
     fetchSubmodules = true;
-    hash = "sha256-/zvE28uVYOKNx7htlg5meiQegvFupbSkhhA6l+5KDHs=";
+    hash = "sha256-d1be3U7ybfYDCoLYdIDp9JEs29+NMFTr16SRj5vOX0w=";
   };
 
   buildInputs = [
