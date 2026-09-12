c012d5e35f3c04e49d7731c749d7891329156787
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-12 05:53:51 +0000

    mongoc: 1.30.9 -> 2.5.3

--- pkgs/by-name/mo/mongoc/package.nix
+++ pkgs/by-name/mo/mongoc/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mongoc";
-  version = "1.30.9";
+  version = "2.5.3";
 
   src = fetchFromGitHub {
     owner = "mongodb";
     repo = "mongo-c-driver";
     tag = finalAttrs.version;
-    hash = "sha256-5msXPEt4a/Q/LDSowf2Eu2HD7ktrTrB9Adi/PtPIs5o=";
+    hash = "sha256-USz0ZCU9ULSGxL+2CYfMOyejAH0TFdlF+DKC5+znceY=";
   };
 
   nativeBuildInputs = [
