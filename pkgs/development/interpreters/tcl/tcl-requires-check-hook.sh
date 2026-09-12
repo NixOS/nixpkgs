af9977e4c5208efe4f369383ae2e090fa27b3f7e
Francesco Gazzetta <fgaz@fgaz.me>
2026-09-12 12:05:51 +0200

    tcl.tclRequiresCheckHook: move tcl code from heredoc to a separate file
    A heredoc piped into tclsh makes tclsh run in interactive mode, which
    keeps running regardless of errors. This required us to `catch` the
    error and `exit` manually. By using a standalone script we avoid this
    hack and the error gets automatically printed out.
    
    With a standalone file we also get better editor support.

--- pkgs/development/interpreters/tcl/tcl-requires-check-hook.sh
+++ pkgs/development/interpreters/tcl/tcl-requires-check-hook.sh
@@ -6,27 +6,8 @@ tclRequiresCheckPhase () {
 
     if [ -n "$tclRequiresCheck" ]; then
         export TCLLIBPATH="$out/lib $TCLLIBPATH" # Redundant if tcl-package-hook is also used
-        tclsh <<'EOF'
-          if {[info exists env(NIX_ATTRS_JSON_FILE)]} {
-            set nix_attrs_json_file [open $env(NIX_ATTRS_JSON_FILE)]
-            set nix_attrs_json [read $nix_attrs_json_file]
-            close $nix_attrs_json_file
-            # Normally we'd use one of tcllib/tdom/rl_json/yajl-tcl to parse JSON,
-            # however we don't want to introduce a circular dependency,
-            # so we rely on the JSON capabilities of the builtin sqlite package
-            # https://wiki.tcl-lang.org/page/SQLite+extension+JSON1
-            package require sqlite3
-            sqlite3 db :memory: -readonly 1
-            set packages_to_check [db eval {
-              select value from json_each(jsonb_extract($nix_attrs_json, '$.tclRequiresCheck'))
-            }]
-            db close
-          } else {
-            set packages_to_check $env(tclRequiresCheck)
-          }
-          puts "Check whether the following packages can be required: $packages_to_check"
-          exit [catch {foreach pkg $packages_to_check {package require $pkg}}]
-EOF
+        # FIXME: For some reason the output gets swallowed unless we pipe it through cat‽
+        tclsh @tcl_hook@ 2>&1 | cat
     fi
 }
 
