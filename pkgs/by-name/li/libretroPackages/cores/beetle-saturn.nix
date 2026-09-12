1ad6f3efb4dade357565b70644cefabaf7ab1a68
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-12 14:05:13 +0000

    libretro.beetle-saturn: 0-unstable-2026-08-11 -> 0-unstable-2026-09-06

--- pkgs/by-name/li/libretroPackages/cores/beetle-saturn.nix
+++ pkgs/by-name/li/libretroPackages/cores/beetle-saturn.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "mednafen-saturn";
-  version = "0-unstable-2026-08-11";
+  version = "0-unstable-2026-09-06";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "beetle-saturn-libretro";
-    rev = "ed549bdac0e1a830bb794fa720e45c225a45355c";
-    hash = "sha256-uRmkeRuOM2JN5OvuImAxyRyRPjXxCHeAl828304hF0o=";
+    rev = "1382b85dcad2e98ef9a67426a775ba548eaf0c68";
+    hash = "sha256-Ps3GduOqKZYP9gLD7TkSM2EzbuuEsIFCiF2hmgn9D84=";
   };
 
   makefile = "Makefile";
