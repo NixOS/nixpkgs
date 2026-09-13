37d97728968a0cd3ee463341f5d9e7770f9a4dfa
Adam C. Stephens <adam@valkor.net>
2026-09-13 11:04:31 -0400

    rustfs: 1.0.0-rc.5 -> 1.0.0-rc.6
    Changelog: https://github.com/rustfs/rustfs/releases/tag/1.0.0-rc.6
    Co-authored-By: Ben Siraphob <bensiraphob@gmail.com>

--- pkgs/by-name/ru/rustfs/package.nix
+++ pkgs/by-name/ru/rustfs/package.nix
@@ -18,7 +18,7 @@
 let
   console = stdenv.mkDerivation (finalAttrs: {
     pname = "rustfs-console";
-    version = "0.1.25";
+    version = "0.1.26";
     __structuredAttrs = true;
     __darwinAllowLocalNetworking = true;
 
@@ -26,7 +26,7 @@ let
       owner = "rustfs";
       repo = "console";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-wPxexsOaZD+pmf1XldN8baa1f6tE0xj/B706m5uwlwc=";
+      hash = "sha256-1X7ZcprtVXybV58mdrqbvERNHfs8Y/3klGDPObhUt9o=";
     };
 
     pnpmDeps = fetchPnpmDeps {
@@ -54,24 +54,22 @@ let
 in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rustfs";
-  version = "1.0.0-rc.5";
+  version = "1.0.0-rc.6";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "rustfs";
     repo = "rustfs";
     tag = finalAttrs.version;
-    hash = "sha256-Xb9Lv+8BvHF089D5YwTp7DOMosXc8bUYYEV5F7V2gxU=";
+    hash = "sha256-+ZcLd6WlT5X48u4LHYQVw+QgaVotpEN4JLKy8N86ejM=";
   };
 
   postPatch = ''
     rm -rf ./rustfs/static
     cp -rL ${finalAttrs.console} ./rustfs/static
-
-    substituteInPlace Cargo.toml --replace-fail "1.98.0" "1.97.0"
   '';
 
-  cargoHash = "sha256-+PnEy6Z/ynNjgsgQz98Q/kGuyQ2+FgnJbh6Mk1/tohg=";
+  cargoHash = "sha256-0VMunv3UYMEwf6msTSbL/Eo1vRBLfo9zNixPkc0+kcU=";
 
   nativeBuildInputs = [
     protobuf
@@ -92,6 +90,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
   cargoBuildFlags = "-p rustfs";
 
   useNextest = true;
+  # Use debug mode to reduce test compilation time.
+  checkType = "debug";
   cargoTestFlags = [
     "--package"
     "rustfs"
@@ -110,8 +110,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
       name = "rustfs-update-script";
       runtimeInputs = [ nix-update ];
       text = ''
-        nix-update rustfs
-        nix-update rustfs.console
+        nix-update rustfs --version=unstable
+        nix-update rustfs.console --version=unstable
       '';
     });
   };
