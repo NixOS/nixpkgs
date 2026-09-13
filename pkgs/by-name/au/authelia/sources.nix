5ab7b89f06aa9e9128cc5c6a83f41788914f6615
Nicolas Mémeint <git@nicomem.com>
2026-09-13 09:51:37 +0200

    authelia: 4.39.22 -> 4.39.26

--- pkgs/by-name/au/authelia/sources.nix
+++ pkgs/by-name/au/authelia/sources.nix
@@ -1,14 +1,14 @@
 { fetchFromGitHub }:
 rec {
   pname = "authelia";
-  version = "4.39.22";
+  version = "4.39.26";
 
   src = fetchFromGitHub {
     owner = "authelia";
     repo = "authelia";
     rev = "v${version}";
-    hash = "sha256-6mKS+U0Leac2vcHRTMIAKfqr78NQUCMBiW76z4H/STw=";
+    hash = "sha256-CFgv7H8no8Z4gYXheEKPyi8C7FGRC77kNqGnIjra3bA=";
   };
-  vendorHash = "sha256-8ftsYIEMkoM3emW0d6E3cOv3hUQDLZcSBDEy8NvwNcY=";
-  pnpmDepsHash = "sha256-ngHVlFIQuUY+D54CDZ7FIlu13UjGr3zcdTvKryntVhQ=";
+  vendorHash = "sha256-Bi3cAkcVP1ZWFBuy0RfpqW7yqyV5DxSsa6gmtfLgxEA=";
+  pnpmDepsHash = "sha256-UpgS+6Wj88WyNE4JT9z0eSxYxKANWTfwL0+2xTlcWDQ=";
 }
