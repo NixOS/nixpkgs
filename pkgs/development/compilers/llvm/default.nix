106ac9603aa2cba4d8fb41b916445f9436c6c268
whispers <whispersofthedawn@duck.com>
2026-09-13 12:20:05 -0400

    llvmPackages_23: 23.1.0 -> 23.1.2
    https://discourse.llvm.org/t/llvm-23-1-1-released/91760
    https://discourse.llvm.org/t/lvm-23-1-2-released/91895

--- pkgs/development/compilers/llvm/default.nix
+++ pkgs/development/compilers/llvm/default.nix
@@ -27,7 +27,7 @@ let
     "20.1.8".officialRelease.sha256 = "sha256-ysyB/EYxi2qE9fD5x/F2zI4vjn8UDoo1Z9ukiIrjFGw=";
     "21.1.8".officialRelease.sha256 = "sha256-pgd8g9Yfvp7abjCCKSmIn1smAROjqtfZaJkaUkBSKW0=";
     "22.1.8".officialRelease.sha256 = "sha256-SF7wFuh4kXZTytpdgX7vUZItKtRobnVICm+ixze4iG0=";
-    "23.1.0".officialRelease.sha256 = "sha256-Astfi1UDDcydyws3Q1sELqho/PxiNN/tvCtmCGj5FoE=";
+    "23.1.2".officialRelease.sha256 = "sha256-7RkcTVAGsNCZGGRi/qZrKxhFGDGRoYlu/WYvnvYzwCU=";
     "24.0.0-git".gitRelease = {
       rev = "316d12deb7725d55f90c01b72799df18a7d71d81";
       rev-version = "24.0.0-unstable-2026-09-20";
