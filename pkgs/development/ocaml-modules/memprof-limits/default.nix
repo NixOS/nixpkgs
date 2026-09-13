1255244daa55aaba1d0d34d655bb320c69389b69
Anthony Wang <a@unnamed.website>
2026-09-13 15:49:35 -0400

    ocamlPackages.memprof-limits: 0.2.1 -> 0.3.0

--- pkgs/development/ocaml-modules/memprof-limits/default.nix
+++ pkgs/development/ocaml-modules/memprof-limits/default.nix
@@ -3,26 +3,30 @@
   buildDunePackage,
   fetchFromGitLab,
   ocaml,
+  cppo,
 }:
 
 buildDunePackage (finalAttrs: {
   pname = "memprof-limits";
-  version = "0.2.1";
+  version = "0.3.0";
 
   src = fetchFromGitLab {
     owner = "gadmm";
     repo = "memprof-limits";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Pmuln5TihPoPZuehZlqPfERif6lf7O+0454kW9y3aKc=";
+    hash = "sha256-k/uB1jDQtE/PkVPU8zg8cpOmlPttTWVpKerQ0HuWfuI=";
   };
 
   minimalOCamlVersion = "4.12";
+
+  nativeBuildInputs = [
+    cppo
+  ];
 
   meta = {
     homepage = "https://ocaml.org/p/memprof-limits/latest";
     description = "Memory limits, allocation limits, and thread cancellation for OCaml";
     license = lib.licenses.lgpl3;
     maintainers = with lib.maintainers; [ alizter ];
-    broken = !(lib.versionOlder ocaml.version "5.0.0");
   };
 })
