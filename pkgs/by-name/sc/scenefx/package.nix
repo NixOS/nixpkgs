81774c3c63f67e4855bdbeb9953073313374f58d
yvnth <yashupress@gmail.com>
2026-09-13 19:46:36 +0530

    scenefx: add yvnth as maintainer

--- pkgs/by-name/sc/scenefx/package.nix
+++ pkgs/by-name/sc/scenefx/package.nix
@@ -68,8 +68,11 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = finalAttrs.src.meta.homepage;
     license = lib.licenses.mit;
     mainProgram = "scenefx";
-    maintainers = with lib.maintainers; [ swarsel ];
+    maintainers = with lib.maintainers; [
+      swarsel
+      yvnth
+    ];
     pkgConfigModules = [ "scenefx-${lib.versions.majorMinor finalAttrs.version}" ];
-    platforms = lib.platforms.all;
+    platforms = lib.platforms.linux;
   };
 })
