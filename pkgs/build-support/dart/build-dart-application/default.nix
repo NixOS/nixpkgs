74a71bf3953517f47d570dd6dd45a104a5602367
Stefan Frijters <sfrijters@gmail.com>
2026-09-13 16:49:12 +0200

    buildDartApplication: enable strictDeps, use structuredAttrs instead of passAsFile

--- pkgs/build-support/dart/build-dart-application/default.nix
+++ pkgs/build-support/dart/build-dart-application/default.nix
@@ -201,20 +201,22 @@ lib.extendMkDerivation {
           # Ensure that we inherit the propagated build inputs from the dependencies.
           builtins.attrValues pubspecLockData.dependencySources;
 
+      strictDeps = true;
+
       preConfigure = args.preConfigure or "" + ''
-        ln -sf "$pubspecLockFilePath" pubspec.lock
+        printf "%s" "$pubspecLockFile" > pubspec.lock
       '';
 
       # When stripping, it seems some ELF information is lost and the dart VM cli
       # runs instead of the expected program. Don't strip if it's an exe output.
       dontStrip = args.dontStrip or (dartOutputType == "exe");
 
-      passAsFile = [ "pubspecLockFile" ];
-
       passthru = {
         pubspecLock = pubspecLockData;
       }
       // (args.passthru or { });
+
+      __structuredAttrs = true;
 
       meta = (args.meta or { }) // {
         platforms = args.meta.platforms or dart.meta.platforms;
