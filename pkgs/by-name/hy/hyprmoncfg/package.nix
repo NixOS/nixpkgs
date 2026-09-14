0c6ee60288df05615d38bd3228b43fa1d2b9a373
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-14 04:06:46 +0000

    hyprmoncfg: 1.9.1 -> 1.18.3

--- pkgs/by-name/hy/hyprmoncfg/package.nix
+++ pkgs/by-name/hy/hyprmoncfg/package.nix
@@ -11,7 +11,7 @@
 
 buildGoModule (finalAttrs: {
   pname = "hyprmoncfg";
-  version = "1.9.1";
+  version = "1.18.3";
 
   __structuredAttrs = true;
 
@@ -19,7 +19,7 @@ buildGoModule (finalAttrs: {
     owner = "crmne";
     repo = "hyprmoncfg";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-LCZ1F30Ix4NnWYPI3WkL03jke9XW1cB5CWxmVSrdYYI=";
+    hash = "sha256-wRdOp7o7ePbe+Cp7l+f9J+xBBPPxQflyHBPhbGhzfiY=";
   };
 
   vendorHash = "sha256-gQbjvdKtO0hCXrs9RnWo1s0YeHf5W9t+8AgS2ELXlPo=";
