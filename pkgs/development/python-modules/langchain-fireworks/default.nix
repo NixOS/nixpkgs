69c11057329a1e8a507f80a1aeb0d3b494be3240
Sarah Clark <seclark@nextquestion.net>
2026-09-14 21:31:49 +0000

    python3Packages.langchain-fireworks: 1.4.4 -> 1.6.1

--- pkgs/development/python-modules/langchain-fireworks/default.nix
+++ pkgs/development/python-modules/langchain-fireworks/default.nix
@@ -24,7 +24,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "langchain-fireworks";
-  version = "1.4.4";
+  version = "1.6.1";
   pyproject = true;
   __structuredAttrs = true;
   strictDeps = true;
@@ -33,7 +33,7 @@ buildPythonPackage (finalAttrs: {
     owner = "langchain-ai";
     repo = "langchain";
     tag = "langchain-fireworks==${finalAttrs.version}";
-    hash = "sha256-O63UohmChFeeQZH7G1iYDwNdvJapVEnHlkGHdGxIDjE=";
+    hash = "sha256-tJduBjY+JcG9bhZYsOi7+2TdR4DTi/ZCTIaNZ87R8JM=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/libs/partners/fireworks";
