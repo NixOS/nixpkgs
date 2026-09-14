eb2bb167f80d008b863a67f2324335867b375951
Keenan Weaver <keenanweaver@protonmail.com>
2026-09-13 21:08:46 -0500

    lgogdownloader: modernize

--- pkgs/by-name/lg/lgogdownloader/package.nix
+++ pkgs/by-name/lg/lgogdownloader/package.nix
@@ -9,15 +9,19 @@
   html-tidy,
   jsoncpp,
   ninja,
-  pkg-config,
+  nix-update-script,
   rhash,
   tinyxml-2,
+  versionCheckHook,
   zlib,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lgogdownloader";
   version = "3.19";
+
+  __structuredAttrs = true;
+  strictDeps = true;
 
   src = fetchFromGitHub {
     owner = "Sude-";
@@ -37,24 +41,22 @@ stdenv.mkDerivation (finalAttrs: {
     curl
     html-tidy
     jsoncpp
-    liboauth
     rhash
     tinyxml-2
     zlib
   ];
 
-  passthru.tests = {
-    version = testers.testVersion { package = finalAttrs.finalPackage; };
-  };
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  preVersionCheck = ''
+    export HOME=$TMPDIR
+  '';
+  versionCheckKeepEnvironment = [ "HOME" ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
     mainProgram = "lgogdownloader";
-    homepage = "https://github.com/Sude-/lgogdownloader";
-    license = lib.licenses.wtfpl;
-    # qtbase requires a sandbox profile with read access to /usr/share/icu.
-    # To prevent build failures in CI, we disable Darwin support when the GUI is enabled.
-    platforms = lib.platforms.linux ++ lib.optionals (!enableGui) lib.platforms.darwin;
-    maintainers = with lib.maintainers; [ _0x4A6F ];
   };
 })
