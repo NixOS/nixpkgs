7347fb5bb2737b1e0dcbae21896105c6e0a675e6
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-14 02:39:45 +0000

    tdarr-node: 2.86.01 -> 2.87.01

--- pkgs/tools/misc/tdarr/server.nix
+++ pkgs/tools/misc/tdarr/server.nix
@@ -5,10 +5,10 @@ callPackage ./common.nix { } {
   component = "server";
 
   hashes = {
-    linux_x64 = "sha256-KcFJjWX8+FVY66azStlzs/FRQRAHEMxjG1k7H9IPM3U=";
-    linux_arm64 = "sha256-BVy8O0xYG08Rl5imKB3xNQQGQAw0X5bB65wAdM+mgtc=";
-    darwin_x64 = "sha256-Up4CXyC4H7O56wp1xF7r6v6F8gElsiYjs+AqZbbAOQU=";
-    darwin_arm64 = "sha256-SjcTxrAYEj/lbYZePiGcw2Dp+FcvGzb91KdTrXRE2zw=";
+    linux_x64 = "sha256-dAXcBPJ8wOxx4gg50mN6nEgUcDqEOn17eByX2FvphrQ=";
+    linux_arm64 = "sha256-MDDhkSmMTiOEezKpInR86TItkVnX20IKblUf2JUKnKg=";
+    darwin_x64 = "sha256-faCNm8eeJzQq3Doyac/vtmx80dOTaWVMIErgNwz77xk=";
+    darwin_arm64 = "sha256-NNEvCFZvPJFy6Elh7voeSe847bGVJpnOt/ac5nLEy7Y=";
   };
 
   includeInPath = [ ccextractor ];
