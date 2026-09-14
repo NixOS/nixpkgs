9ca886717c949372dcacf1baec776ec4f26311c8
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-14 12:54:27 +0000

    python3Packages.azure-cosmos: 4.14.5 -> 4.17.0

--- pkgs/development/python-modules/azure-cosmos/default.nix
+++ pkgs/development/python-modules/azure-cosmos/default.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "azure-cosmos";
-  version = "4.14.5";
+  version = "4.17.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "azure_cosmos";
     inherit version;
-    hash = "sha256-MjmmBf4pyUt3ORgmdzqj0Nm75Lk5A/zltkNLDzJ0K6c=";
+    hash = "sha256-OFWfIJzhPRNcB7EmUtctNSusa21rMAcAbOj8F0yBWwc=";
   };
 
   build-system = [ setuptools ];
