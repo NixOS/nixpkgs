112fd11b21abbd75a765021bab0fba5c5ca59ae5
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-09-14 22:34:06 +0200

    ocamlPackages.jws: init at 0.0.2

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1053,6 +1053,8 @@ let
         junit_alcotest = callPackage ../development/ocaml-modules/junit/alcotest.nix { };
         junit_ounit = callPackage ../development/ocaml-modules/junit/ounit.nix { };
 
+        jws = callPackage ../development/ocaml-modules/jws { };
+
         jwto = callPackage ../development/ocaml-modules/jwto { };
 
         ### K ###
