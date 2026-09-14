8e39062f760fda4904e5aae78c46da8ae95243a7
teutat3s <10206665+teutat3s@users.noreply.github.com>
2026-09-14 17:17:03 +0200

    electron_44-bin: fix build
    electron 44+ no longer ships libGLESv2 / libEGL, so we no longer need to
    patchelf it. chromium statically links against libGL / vulkan-loader.
    
    See Breaking Changes in https://github.com/electron/electron/releases/tag/v44.0.0
    
    (cherry picked from commit b3041dc18afe5078fc99feeb7c2c3384447e809a)

--- pkgs/development/tools/electron/binary/generic.nix
+++ pkgs/development/tools/electron/binary/generic.nix
@@ -170,6 +170,12 @@ let
         $out/libexec/electron/electron \
         $out/libexec/electron/chrome_crashpad_handler
 
+      # replace bundled vulkan-loader
+      rm "$out/libexec/electron/libvulkan.so.1"
+      ln -s -t "$out/libexec/electron" "${lib.getLib vulkan-loader}/lib/libvulkan.so.1"
+
+    ''
+    + lib.optionalString (lib.versionOlder version "44") ''
       # patch libANGLE
       patchelf \
         --set-rpath "${
@@ -180,10 +186,6 @@ let
           ]
         }" \
         $out/libexec/electron/lib*GL*
-
-      # replace bundled vulkan-loader
-      rm "$out/libexec/electron/libvulkan.so.1"
-      ln -s -t "$out/libexec/electron" "${lib.getLib vulkan-loader}/lib/libvulkan.so.1"
     '';
 
     passthru.dist = finalAttrs.finalPackage + "/libexec/electron";
