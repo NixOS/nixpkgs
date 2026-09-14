8eb0079666578b45efbb34dfc87681c0c7b1813b
Grimmauld <Grimmauld@grimmauld.de>
2026-09-14 13:37:04 +0200

    Revert "libcap_ng: disable tests on static"
    This reverts commit 18219b32ad562755274e15f7c1797391931c3e30.

--- pkgs/by-name/li/libcap_ng/package.nix
+++ pkgs/by-name/li/libcap_ng/package.nix
@@ -101,9 +101,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   # assumption: build machine runs linux kernel 5.0 or newer
   # see https://github.com/stevegrubb/libcap-ng?tab=readme-ov-file#note-to-distributions
-  # disabled on static due to symbol collision in test file compilation
-  # see https://github.com/stevegrubb/libcap-ng/issues/85
-  doCheck = !stdenv.hostPlatform.isStatic;
+  doCheck = true;
 
   pythonImportsCheck = [
     "capng"
