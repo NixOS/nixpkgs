599afc857d23730af95a916fb78eaeb1b572eab9
Thomas Mühlbacher <tmuehlbacher@posteo.net>
2026-09-15 01:46:34 +0200

    llvmPackages.clang-tools: fix wrapper bash-isms
    Even if /bin/sh is bash, it will run in POSIX-compatible mode. The
    arithmetic evaluation, used namely with `while (( $# )); do`, is not
    part of POSIX.
    
    A minimal reproducer with a project that generally ought to pass
    clang-tidy checking may look like so:
    
    ```nix
    {
      pkgs ? import <nixpkgs> { },
    }:
    pkgs.fwupd.overrideAttrs (
      final: prev: {
        nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.clang-tools ];
        preCheck = ''
          ninja clang-tidy
        '';
      }
    )
    ```
    
    Before, you may see many errors about missing headers and some lines
    that suspiciously look like bash errors.
    
    ```
    fwupd> >>> /nix/store/gf6qdh329rc8lbby710r9rzhwqkkgv3h-clang-tools-21.1.8/bin/clang-tidy --use-color -quiet -p /build/source/build /build/source/plugins/uf2/fu-self-test.c
    fwupd> /nix/store/gf6qdh329rc8lbby710r9rzhwqkkgv3h-clang-tools-21.1.8/bin/clang-tidy: line 5: 129: not found
    fwupd> /nix/store/gf6qdh329rc8lbby710r9rzhwqkkgv3h-clang-tools-21.1.8/bin/clang-tidy: line 23: 129: not found
    fwupd> 276 warnings and 1 error generated.
    fwupd> Error while processing /build/source/plugins/uf2/fu-self-test.c.
    fwupd> /nix/store/dm7fg33sqnjxkwdpirnnbnx7wyi89m82-glib-2.88.3-dev/include/glib-2.0/glib/gtypes.h:41:10: error: 'time.h' file not found [clang-diagnostic-error]
    fwupd>    41 | #include <time.h>
    fwupd>       |          ^~~~~~~~
    ```
    
    With this change, these clang-tidy errors no longer appear.

--- pkgs/development/compilers/llvm/common/clang-tools/default.nix
+++ pkgs/development/compilers/llvm/common/clang-tools/default.nix
@@ -3,6 +3,7 @@
   stdenv,
   runCommand,
   writeText,
+  bash,
   clang-unwrapped,
   clang,
   libcxxClang,
@@ -19,6 +20,8 @@ stdenv.mkDerivation (finalAttrs: {
 
   strictDeps = true;
   __structuredAttrs = true;
+
+  buildInputs = [ bash ];
 
   installPhase = ''
     runHook preInstall
