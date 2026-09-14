09d5db710d20e0c19816cd04989eb47d990bc6b8
h7x4 <h7x4@nani.wtf>
2026-09-14 17:44:05 +0900

    nixos/tests/goeland: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -783,6 +783,7 @@ in
   gocd-server = runTest ./gocd-server.nix;
   gocron = runTest ./gocron.nix;
   gocryptfs = runTest ./gocryptfs.nix;
+  goeland = runTest ./goeland.nix;
   gokapi = runTest ./gokapi.nix;
   gollum = runTest ./gollum.nix;
   gonic = runTest ./gonic.nix;
