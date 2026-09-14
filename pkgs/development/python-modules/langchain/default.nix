2ad51ef94b04f2ea26431e73c4599dc29af17cb9
Sarah Clark <seclark@nextquestion.net>
2026-09-14 21:32:24 +0000

    python3Packages.langchain: 1.3.14 -> 1.4.0

--- pkgs/development/python-modules/langchain/default.nix
+++ pkgs/development/python-modules/langchain/default.nix
@@ -46,7 +46,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "langchain";
-  version = "1.3.14";
+  version = "1.4.0";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -54,7 +54,7 @@ buildPythonPackage (finalAttrs: {
     owner = "langchain-ai";
     repo = "langchain";
     tag = "langchain==${finalAttrs.version}";
-    hash = "sha256-SmbqK8/AmUYfp+hUEnuwl18K+A/6csLxMUTv0oQHhEs=";
+    hash = "sha256-p4YqDRa37QC0diLMUPUX1J8Rc3q+DFxjPU+w8M0EJtY=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/libs/langchain_v1";
