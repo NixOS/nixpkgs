8c5f0e83e7d8683bb4b2f45a6e01e66bfe632567
Sarah Clark <seclark@nextquestion.net>
2026-09-14 21:32:23 +0000

    python3Packages.langgraph-runtime-inmem: 0.32.3 -> 0.34.1

--- pkgs/development/python-modules/langgraph-runtime-inmem/default.nix
+++ pkgs/development/python-modules/langgraph-runtime-inmem/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "langgraph-runtime-inmem";
-  version = "0.32.3";
+  version = "0.34.1";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -22,7 +22,7 @@ buildPythonPackage (finalAttrs: {
   src = fetchPypi {
     pname = "langgraph_runtime_inmem";
     inherit (finalAttrs) version;
-    hash = "sha256-k5ATRuxkds7dDZx3lxGJUL+vUgVFHe8/BaAZqoaaxd4=";
+    hash = "sha256-DgyDoLsWDpnSF8CBPLRtQtpQ5LazZrSozkrbRIOX4xo=";
   };
 
   build-system = [ hatchling ];
