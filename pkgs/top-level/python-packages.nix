a12d4729fa91039fc6236b5690c91e21327c2896
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-09-14 20:46:14 +0200

    python3Packages.mjlab: init at 1.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10924,6 +10924,8 @@ self: super: with self; {
 
   mizani = callPackage ../development/python-modules/mizani { };
 
+  mjlab = callPackage ../development/python-modules/mjlab { };
+
   mjviser = callPackage ../development/python-modules/mjviser { };
 
   mkdocs = callPackage ../development/python-modules/mkdocs { };
