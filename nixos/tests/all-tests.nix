6b9764ec5af727db0c9da92c57742b79d7a3cd6a
h7x4 <h7x4@nani.wtf>
2026-09-15 13:36:26 +0900

    nixos/tests/speechd: init
    This test ensures that the speechd module evals correctly behind its
    `mkIf` guard, and verifies that configuration is picked up correctly.
    
    It unfortunately does not test integration with other systems like
    pipewire, but the test is better than nothing in it's current state.

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1717,6 +1717,7 @@ in
   sonic-server = runTest ./sonic-server.nix;
   spacecookie = runTest ./spacecookie.nix;
   spark = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./spark { };
+  speechd = runTest ./speechd.nix;
   speedtest-tracker = runTest ./speedtest-tracker.nix;
   spiped = runTest ./spiped.nix;
   spire = runTest ./spire.nix;
