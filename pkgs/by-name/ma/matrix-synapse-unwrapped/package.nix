c1a7daafa856d3e688b196333f13fb705facbeb4
transcaffeine <transcaffeine@finally.coffee>
2026-09-15 21:08:32 +0200

    matrix-synapse-unwrapped: 1.160.0 -> 1.161.0
    Release notes: https://github.com/element-hq/synapse/blob/release-v1.161/CHANGES.md
    Full changelog: https://github.com/element-hq/synapse/compare/v1.160.0...v1.161.0

--- pkgs/by-name/ma/matrix-synapse-unwrapped/package.nix
+++ pkgs/by-name/ma/matrix-synapse-unwrapped/package.nix
@@ -14,19 +14,19 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.160.0";
+  version = "1.161.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "element-hq";
     repo = "synapse";
     rev = "v${version}";
-    hash = "sha256-sf7YS3AKPnhQySGZD1uvHvpVdUWIV/hrzBLvmbvS3bA=";
+    hash = "sha256-gtTgILfGLYL0ASNz6m8MJhAidQ7kTGqdiMNP6R8bH5Q=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit pname version src;
-    hash = "sha256-154xfJLCCcPbjxknbm5HanJyMGnAJCTZKH8BYvMCvpo=";
+    hash = "sha256-PHaFsLVUpeMSgHMg05blJUNgycDLbZUiZkWSPjDT1CM=";
   };
 
   build-system =
