1d1d0799dcfe6d5ef08cd36dda5db91da28549b4
Gaetan Lepage <gaetan@glepage.com>
2026-09-15 07:14:45 +0000

    python3Packages.fastmcp: 3.4.7 -> 4.0.3
    Diff: https://github.com/PrefectHQ/fastmcp/compare/v3.4.7...v4.0.3
    
    Changelog: https://github.com/PrefectHQ/fastmcp/releases/tag/v4.0.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5964,6 +5964,8 @@ self: super: with self; {
 
   fastmcp-slim = callPackage ../development/python-modules/fastmcp-slim { };
 
+  fastmcp-tasks = callPackage ../development/python-modules/fastmcp-tasks { };
+
   fastmri = callPackage ../development/python-modules/fastmri { };
 
   fastnlo-toolkit = toPythonModule (
