e84650f82d68c355bb0f08031f9025a9e231e910
Colin <colin@uninsane.org>
2026-09-15 16:52:29 +0000

    tcl: move configure patching to tcl-8_6, make unconditional
    fixes `nix-build -A pkgsCross.aarch64-multiplatform.tcl-9_0`,
    previously failing because unix/configure.in is specific to tcl 8.6.
    
    making this unconditional ensures that type of error gets surfaced more
    quickly in the future.

--- pkgs/development/interpreters/tcl/8.6.nix
+++ pkgs/development/interpreters/tcl/8.6.nix
@@ -2,6 +2,7 @@
   callPackage,
   fetchurl,
   fetchpatch,
+  stdenv,
   ...
 }@args:
 
@@ -44,5 +45,10 @@ callPackage ./generic.nix (
       # Backport of upstream check-in `fd06472ef41e1d73`; see the patch.
       ./8.6-windows-disable-tzdata.patch
     ];
+
+    extraPatch = ''
+      substituteInPlace unix/configure unix/configure.in \
+        --replace-fail '`uname -s`' '${stdenv.hostPlatform.uname.system}'
+    '';
   }
 )
