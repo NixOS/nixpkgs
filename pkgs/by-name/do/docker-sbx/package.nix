df49e6787ad14a7b23b592d59eb39a6addc3011d
Craig Younkins <cyounkins@gmail.com>
2026-09-15 22:14:46 +0000

    docker-sbx: 0.39.0 -> 0.43.0

--- pkgs/by-name/do/docker-sbx/package.nix
+++ pkgs/by-name/do/docker-sbx/package.nix
@@ -15,9 +15,9 @@
 }:
 let
   hashes = {
-    "x86_64-linux" = "sha256-LsRbx5OMIML0Bv6MxyKUrVqVS9wEdgFIS4m/GhCDEdQ=";
-    "aarch64-linux" = "sha256-OcRwpfXgmRscI1iVLiqzKnsDCb+lesYra7xktGbQLBc=";
-    "aarch64-darwin" = "sha256-0S+gau7OKY2W2uBYEB8lZTd3CgDvRuZYR2Lg8nsGpaM=";
+    "x86_64-linux" = "sha256-PrFbhETpaaqo1jclC+8PK/kMsDCroKcBala45fzdJf4=";
+    "aarch64-linux" = "sha256-Ma46iMwIN0PllWn+3WofUns73WxWfVjlvGDGwK8ZpUA=";
+    "aarch64-darwin" = "sha256-6fybO66cVyilcm9JubNgkyhRnaV288mf2Ofl0TcAinw=";
   };
   platformName = {
     "x86_64-linux" = "linux-amd64";
@@ -27,7 +27,7 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "docker-sbx";
-  version = "0.39.0";
+  version = "0.43.0";
   src =
     let
       throwPlat = throw "Unsupported platform ${stdenvNoCC.hostPlatform.system}";
