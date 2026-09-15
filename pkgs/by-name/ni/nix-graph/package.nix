48b828d0ec08b1fb8efc8b462f417d88db682c37
AlexAntonik <antonikavv@gmail.com>
2026-09-15 23:48:16 +0300

    nix-graph: init at 0.0.6
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

---
+++ pkgs/by-name/ni/nix-graph/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "nix-graph";
+  version = "0.0.6";
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "AlexAntonik";
+    repo = "nix-graph";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-YyXtVTBExTTOMv1eqYlhzbAhPqkdHKqO3f7y9C/Sq9k=";
+  };
+
+  vendorHash = null;
+  env.CGO_ENABLED = "0";
+  ldflags = [
+    "-s"
+    "-X main.version=${finalAttrs.version}"
+  ];
+
+  meta = {
+    homepage = "https://github.com/AlexAntonik/nix-graph";
+    description = "Interactive TUI viewer for Nix dependency graphs";
+    license = lib.licenses.mit;
+    mainProgram = "nix-graph";
+    maintainers = with lib.maintainers; [ AlexAntonik ];
+    platforms = lib.platforms.unix;
+  };
+})
