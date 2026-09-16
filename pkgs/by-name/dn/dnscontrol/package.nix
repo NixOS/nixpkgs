23fa6a7edc89b799dc3863571991fc41019aba97
zowoq <59103226+zowoq@users.noreply.github.com>
2026-09-16 10:15:31 +1000

    dnscontrol: 5.0.4 -> 5.1.0
    Diff: https://github.com/DNSControl/dnscontrol/compare/v5.0.4...v5.1.0
    
    Changelog: https://github.com/DNSControl/dnscontrol/releases/tag/v5.1.0

--- pkgs/by-name/dn/dnscontrol/package.nix
+++ pkgs/by-name/dn/dnscontrol/package.nix
@@ -9,16 +9,16 @@
 
 buildGo127Module (finalAttrs: {
   pname = "dnscontrol";
-  version = "5.0.4";
+  version = "5.1.0";
 
   src = fetchFromGitHub {
     owner = "DNSControl";
     repo = "dnscontrol";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ihcFAsIrNX3pK6TyWcHaaYlqWj8geJvtdNoDa0z3rnI=";
+    hash = "sha256-StdVGROKsXJa5F7CEno3T516lVJbXToBxLx6AqKz9+I=";
   };
 
-  vendorHash = "sha256-1MtLq6DWvjvTyLoOARju5ZlW/Q9J24L+Lcy0BdsW40k=";
+  vendorHash = "sha256-qIb/hUn/ROWBaSAnEleeLDWQlCvFapBwKcZjdHlNpF8=";
 
   nativeBuildInputs = [ installShellFiles ];
 
