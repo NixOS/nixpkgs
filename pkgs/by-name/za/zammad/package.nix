cb1bb06e75230ab562743394af27a6251dfc2b95
Robert Schütz <nix@dotlambda.de>
2026-09-16 05:16:27 +0000

    zammad: 7.1.2 -> 7.1.2
    Diff: https://github.com/zammad/zammad/compare/7.1.2...7.1.3
    
    Changelog: https://zammad.com/en/product/releases/7-1-3

--- pkgs/by-name/za/zammad/package.nix
+++ pkgs/by-name/za/zammad/package.nix
@@ -23,7 +23,7 @@
 
 let
   pname = "zammad";
-  version = "7.1.2";
+  version = "7.1.3";
 
   src = applyPatches {
     src = fetchFromGitHub (lib.importJSON ./source.json);
@@ -134,6 +134,7 @@ stdenvNoCC.mkDerivation {
   };
 
   meta = {
+    changelog = "https://zammad.com/en/product/releases/${lib.replaceString "." "-" version}";
     description = "Web-based, open source user support/ticketing solution";
     homepage = "https://zammad.org";
     license = lib.licenses.agpl3Plus;
