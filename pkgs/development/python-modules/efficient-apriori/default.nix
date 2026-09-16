649cf125b850a6ca14a9ef7c830eb44572dfa30c
Rutwik Shrikant <rutwik1221@gmail.com>
2026-09-16 23:49:32 +0800

    python3Packages.efficient-apriori: replaced fetchPypi with fetchFomGitHub

--- pkgs/development/python-modules/efficient-apriori/default.nix
+++ pkgs/development/python-modules/efficient-apriori/default.nix
@@ -1,34 +1,45 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   setuptools,
+  nix-update-script,
+  pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "efficient-apriori";
   version = "2.0.6";
-
   pyproject = true;
 
-  src = fetchPypi {
-    pname = "efficient_apriori";
-    inherit version;
-    hash = "sha256-WtjOS0+K7lPMDvmagEFFBxvYvdZS/ft4zfLkODG6rW4=";
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "tommyod";
+    repo = "Efficient-Apriori";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-mWuSEAYxonIv/E703JFYE+ptPXMYKaK3gcRl9xVUic0=";
   };
 
   build-system = [
     setuptools
   ];
 
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
   pythonImportsCheck = [
     "efficient_apriori"
   ];
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "An efficient Python implementation of the Apriori algorithm";
     homepage = "https://github.com/tommyod/Efficient-Apriori";
+    changelog = "https://github.com/tommyod/Efficient-Apriori/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ rutwik1221 ];
   };
-}
+})
