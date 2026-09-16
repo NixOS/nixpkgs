b14808e380edc4039569a2545f55dc0f965dc5bb
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-16 05:52:51 +0000

    gpu-screen-recorder-ui: 1.13.7 -> 1.13.9

--- pkgs/by-name/gp/gpu-screen-recorder-ui/package.nix
+++ pkgs/by-name/gp/gpu-screen-recorder-ui/package.nix
@@ -34,12 +34,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gpu-screen-recorder-ui";
-  version = "1.13.7";
+  version = "1.13.9";
 
   src = fetchgit {
     url = "https://repo.dec05eba.com/gpu-screen-recorder-ui";
     tag = finalAttrs.version;
-    hash = "sha256-liBdZDHI32ElTTsHAZekYKFgZY1eP7rz4u7tM4pkpZ4=";
+    hash = "sha256-f4jTv3IS73NWStg1af20pGKlqwIN9K9ZyLjKUX9Tp3s=";
   };
 
   nativeBuildInputs = [
