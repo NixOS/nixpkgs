ec64bcf500054347aa02aa970c91955a412ac176
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-16 21:08:44 +0000

    vorta: 0.11.5 -> 0.11.6

--- pkgs/by-name/vo/vorta/package.nix
+++ pkgs/by-name/vo/vorta/package.nix
@@ -12,14 +12,14 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "vorta";
-  version = "0.11.5";
+  version = "0.11.6";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "borgbase";
     repo = "vorta";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-6WY1UAB5Vr+5Az6UYq2DwXwjabcQr7A3QAEQ2/aIzfg=";
+    hash = "sha256-7vV4jv8Gy+viZhQmWECJ02VjoWGWDKTMnzrYkA60OzE=";
   };
 
   nativeBuildInputs = [
