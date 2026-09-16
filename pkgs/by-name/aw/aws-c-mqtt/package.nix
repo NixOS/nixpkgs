a5a7fc89110bf35e5fd15a386c77f841f2b4f6a0
João Santos Reis <joao.reis@tweag.io>
2026-09-16 11:04:18 +0100

    aws-c-mqtt: 0.13.3 -> 1.0.0

--- pkgs/by-name/aw/aws-c-mqtt/package.nix
+++ pkgs/by-name/aw/aws-c-mqtt/package.nix
@@ -15,13 +15,13 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "aws-c-mqtt";
   # nixpkgs-update: no auto update
-  version = "0.13.3";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-c-mqtt";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Nf8c5iVl+NOPZFjsAPCMOGq2e7D8e7PafuMQh6t0DYw=";
+    hash = "sha256-6k3Ca25fRPqV+QW0+VEUdbB0JUFjqvZo8R7eaiEby0c=";
   };
 
   nativeBuildInputs = [
