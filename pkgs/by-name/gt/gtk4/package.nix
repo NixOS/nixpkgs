eec25e174d4a8fc6cea186c54a9989447b067263
Tom Hunze <dev@thunze.de>
2026-09-16 18:22:20 +0000

    gtk4: 4.23.4 -> 4.24.0
    https://gitlab.gnome.org/GNOME/gtk/-/compare/4.23.4...4.24.0

--- pkgs/by-name/gt/gtk4/package.nix
+++ pkgs/by-name/gt/gtk4/package.nix
@@ -76,7 +76,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gtk4";
-  version = "4.23.4";
+  version = "4.24.0";
 
   outputs = [
     "out"
@@ -92,7 +92,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtk/${lib.versions.majorMinor finalAttrs.version}/gtk-${finalAttrs.version}.tar.xz";
-    hash = "sha256-Wd9xJEzyBgG0JdGhOyp+gFuFTTteEa0l9/mOYTz8W60=";
+    hash = "sha256-KLpKwcBPhurAm3mhY8sWOkwrVEQtn37MwEZ5BipYEEQ=";
   };
 
   depsBuildBuild = [
