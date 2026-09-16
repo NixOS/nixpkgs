1cdec2d612a411a895c44158b96f6117a374a2a2
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-16 20:02:30 +0200

    python3Packages.mypy-boto3-directconnect: 1.43.76 -> 1.43.95

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -399,8 +399,8 @@ in
       "sha256-4VGCEZbno4w3H5+bc+2/f4ZkgefUDgWOOBkuGTVqvWk=";
 
   mypy-boto3-directconnect =
-    buildMypyBoto3Package "directconnect" "1.43.76"
-      "sha256-QjOdr+p3TniXH/86WJJm+QbePgDSe8iek1eq65g60E0=";
+    buildMypyBoto3Package "directconnect" "1.43.95"
+      "sha256-IZn+H2CbmgzdVl4BcZw0lG2CbUxoL2z9AW01R+T2ztY=";
 
   mypy-boto3-discovery =
     buildMypyBoto3Package "discovery" "1.43.0"
