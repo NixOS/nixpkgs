6750d8f5ad7468976f805863ac73387de438fa6b
Gaetan Lepage <gaetan@glepage.com>
2026-09-16 15:32:14 +0000

    python3Packages.lifelines: cleanup, fix

--- pkgs/development/python-modules/lifelines/default.nix
+++ pkgs/development/python-modules/lifelines/default.nix
@@ -1,35 +1,54 @@
 {
   lib,
+  stdenv,
+  buildPythonPackage,
+  fetchFromGitHub,
+  fetchpatch,
+
+  # build-system
+  setuptools,
+
+  # dependencies
   autograd,
   autograd-gamma,
-  buildPythonPackage,
-  dill,
-  fetchFromGitHub,
-  flaky,
   formulaic,
-  jinja2,
   matplotlib,
   numpy,
   pandas,
+  scipy,
+
+  # tests
+  dill,
+  flaky,
+  jinja2,
   psutil,
   pytestCheckHook,
   scikit-learn,
-  scipy,
-  setuptools,
   sybil,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "lifelines";
   version = "0.30.3";
   pyproject = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "CamDavidsonPilon";
     repo = "lifelines";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-A9MsQN/JGCQ4cYNIZI5LBKpRb44uI/SM8eT4/nKpsXQ=";
   };
+
+  patches = [
+    # pandas 3.0 compatibility
+    # https://github.com/CamDavidsonPilon/lifelines/pull/1684
+    (fetchpatch {
+      name = "pandas-3-compat.patch";
+      url = "https://github.com/CamDavidsonPilon/lifelines/commit/755db6f1e018056b14282885e010274d9f91755a.patch";
+      hash = "sha256-T9NLs3R1+G6gWam+DU/rCOYVyQpq9YjkoWgCh2KUxm4=";
+    })
+  ];
 
   build-system = [ setuptools ];
 
@@ -43,6 +62,8 @@ buildPythonPackage rec {
     scipy
   ];
 
+  pythonImportsCheck = [ "lifelines" ];
+
   nativeCheckInputs = [
     dill
     flaky
@@ -53,21 +74,17 @@ buildPythonPackage rec {
     sybil
   ];
 
-  pythonImportsCheck = [ "lifelines" ];
-
-  disabledTestPaths = [ "lifelines/tests/test_estimation.py" ];
-
-  disabledTests = [
-    "test_datetimes_to_durations_with_different_frequencies"
-    # AssertionError
-    "test_mice_scipy"
+  disabledTests = lib.optionals stdenv.hostPlatform.isDarwin [
+    # AssertionError: Not equal to tolerance rtol=0.01, atol=0
+    "test_score_method_returns_same_value_for_unpenalized_models"
+    "test_that_adding_strata_will_change_c_index"
   ];
 
   meta = {
     description = "Survival analysis in Python";
     homepage = "https://lifelines.readthedocs.io";
-    changelog = "https://github.com/CamDavidsonPilon/lifelines/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/CamDavidsonPilon/lifelines/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ swflint ];
   };
-}
+})
