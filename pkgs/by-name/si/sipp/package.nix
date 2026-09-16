9de4576453e7c18e2095a41d2a3250ba5ca4810b
sh0uv1 <mirantuten@yandex.com>
2026-09-16 17:55:58 +0000

    sipp: fix CVE-2026-90780 CVE-2026-90779 CVE-2026-90778
    Fixes: NixOS/nixpkgs#563484 NixOS/nixpkgs#563488 NixOS/nixpkgs#563490

--- pkgs/by-name/si/sipp/package.nix
+++ pkgs/by-name/si/sipp/package.nix
@@ -7,6 +7,7 @@
   cmake,
   openssl,
   lksctp-tools,
+  fetchpatch,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -19,6 +20,25 @@ stdenv.mkDerivation (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-1JKKD3rEddCzXrbNpbzvhFXlTlkTXlP7vmRXvlGzkgI=";
   };
+
+  # TODO: Remove this patches once SIPP releases a version newer than 3.7.7.
+  patches = [
+    (fetchpatch {
+      name = "CVE-2026-90780.patch";
+      url = "https://github.com/SIPp/sipp/commit/8ddfb43359703e665041a955543e07f504f80232.patch";
+      hash = "sha256-MYzy6C5yOwEJ7lo+GDdfTRlJ1t4ABtnm7ha+gAwoARo=";
+    })
+    (fetchpatch {
+      name = "CVE-2026-90779.patch";
+      url = "https://github.com/SIPp/sipp/commit/09916a92de768ab08056a78e4731f9edc3853e1c.patch";
+      hash = "sha256-MJe6SMT3yNzbep9/eC4ELCqxMKwe/v9ZkB4q/R0dpUg=";
+    })
+    (fetchpatch {
+      name = "CVE-2026-90778.patch";
+      url = "https://github.com/SIPp/sipp/commit/d913c75c4ac55fe6d2b79c6dc23d8aa36dbc5283.patch";
+      hash = "sha256-SEufhy4qB8PTWVwr5TE783P0yWjZk2/jcBGlqtJIbXc=";
+    })
+  ];
 
   postPatch = ''
     echo '#define SIPP_VERSION VERSION' > include/version.h
