87912b31e7024b8ee5500be0955b740fab844982
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-17 04:55:37 +0000

    ananicy-rules-cachyos: 0-unstable-2026-08-31 -> 0-unstable-2026-09-08

--- pkgs/by-name/an/ananicy-rules-cachyos/package.nix
+++ pkgs/by-name/an/ananicy-rules-cachyos/package.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "ananicy-rules-cachyos";
-  version = "0-unstable-2026-08-31";
+  version = "0-unstable-2026-09-08";
 
   src = fetchFromGitHub {
     owner = "CachyOS";
     repo = "ananicy-rules";
-    rev = "7870e04fb5b1dfc57dc3ed4e576459246f826149";
-    hash = "sha256-06sKcoJQoYvfGDKkAmlBhDiApg8IDXn8BUmihZsWr/4=";
+    rev = "03ef03fbf7e834385377432ccecaedd32e3414bb";
+    hash = "sha256-7aM+IsRctA/XIYVkqpljf4kHttoUtG9t/FO+Ium1XaM=";
   };
 
   dontConfigure = true;
