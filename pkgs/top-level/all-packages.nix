18371449108388f6d79a9cf0ae8560845909ca85
Thomas <thomas.erents@gmail.com>
2026-09-17 23:12:13 +0200

    Zabbix: Make default zabbix version 7.4
    With Zabbix 6.0 being in limited support (source:
    https://www.zabbix.com/life_cycle_and_release_policy)
    and currently being incompatible with the PHP that ships with nixpkgs,
    we should look into replacing it with a newer version that works OOTB,
    as well as receives full support from upstream.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7550,7 +7550,7 @@ with pkgs;
   zabbix70 = recurseIntoAttrs (zabbixFor "v70");
   zabbix60 = recurseIntoAttrs (zabbixFor "v60");
 
-  zabbix = zabbix60;
+  zabbix = zabbix74;
 
   ### OS-SPECIFIC
 
