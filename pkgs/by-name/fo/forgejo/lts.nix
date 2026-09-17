31520976171f9b4a70fdd78a99d6ef5269384133
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2026-09-17 12:20:06 -0400

    forgejo: 15.0.8 -> 15.0.9
    changelog: https://codeberg.org/forgejo/forgejo/milestone/142240
    (cherry picked from commit 0007391c9b6d45e4bb47175e6438e51d1b44040e)

--- pkgs/by-name/fo/forgejo/lts.nix
+++ pkgs/by-name/fo/forgejo/lts.nix
@@ -1,8 +1,8 @@
 import ./generic.nix {
-  version = "15.0.8";
-  hash = "sha256-tpgqa6RvQ/xesSv/THjlXiM0JQqzgMQJiuUAOFrgGpo=";
+  version = "15.0.9";
+  hash = "sha256-6EtUVoB8/PsgbWiB4j9hLYOYyjPKCykzILWezDpJnAs=";
   npmDepsHash = "sha256-g3ebO2W8lnHeS6T6bMJkC/UVDA3pa5O5v64QJG5irn0=";
-  vendorHash = "sha256-p9U/nvAHSC2Pj09gfDWQVKl+1TKB0PTqW2E+gKZ4/D4=";
+  vendorHash = "sha256-KW0WIfRm53pVOiWOYoSQaCz7BO+lFykAQdgdgXesXkY=";
   lts = true;
   nixUpdateExtraArgs = [
     "--override-filename"
