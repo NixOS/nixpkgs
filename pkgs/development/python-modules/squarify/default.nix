1c985de672d1586e0deb58ce55ec7396180969e1
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-17 15:16:10 +0000

    python3Packages.squarify: 0.4.3 -> 0.4.5

--- pkgs/development/python-modules/squarify/default.nix
+++ pkgs/development/python-modules/squarify/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "squarify";
-  version = "0.4.3";
+  version = "0.4.5";
   pyproject = true;
 
   __structuredAttrs = true;
@@ -18,7 +18,7 @@ buildPythonPackage (finalAttrs: {
     owner = "laserson";
     repo = "squarify";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zSv+6xT9H4WyShRnwjjcNMjY19AFlQ6bw9Mh9p2rL08=";
+    hash = "sha256-4c04HXtw5BcdKf+MhwKnYBoMqLlvaxQy2/qCZP6u8bg=";
   };
 
   build-system = [ setuptools ];
