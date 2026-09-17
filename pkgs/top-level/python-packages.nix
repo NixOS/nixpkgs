7a7b9a9d3594ac64cac50254d88368c72fa73f54
Ruben Børseth <rubbr@sundhedsdata.dk>
2026-09-17 16:39:37 +0200

    python3Packages.biocommons-seqrepo: init at 0.6.11

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2431,6 +2431,8 @@ self: super: with self; {
 
   biocframe = callPackage ../development/python-modules/biocframe { };
 
+  biocommons-seqrepo = callPackage ../development/python-modules/biocommons-seqrepo { };
+
   biocutils = callPackage ../development/python-modules/biocutils { };
 
   biom-format = callPackage ../development/python-modules/biom-format { };
