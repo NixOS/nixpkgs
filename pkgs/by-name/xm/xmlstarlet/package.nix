a940c632a579cad0ef87e80ab0169e2ef5a73c01
Evan Porter <evanwporter@gmail.com>
2026-09-16 17:53:30 -0700

    xmlstarlet: 1.6.1 -> 1.7.0
    first release in 12 years :O

--- pkgs/by-name/xm/xmlstarlet/package.nix
+++ pkgs/by-name/xm/xmlstarlet/package.nix
@@ -1,44 +1,35 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  fetchpatch,
+  fetchFromGitHub,
   autoreconfHook,
   pkg-config,
   libxml2,
   libxslt,
 }:
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "xmlstarlet";
-  version = "1.6.1";
+  version = "1.7.0";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/xmlstar/xmlstarlet-${finalAttrs.version}.tar.gz";
-    sha256 = "1jp737nvfcf6wyb54fla868yrr39kcbijijmjpyk4lrpyg23in0m";
+  src = fetchFromGitHub {
+    owner = "xmlstarlet";
+    repo = "xmlstarlet";
+    tag = "${finalAttrs.version}";
+    hash = "sha256-dnuDDr14ACB16r1/N9g71RxPaVjHbikjDef3OLqzMZQ=";
   };
 
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
   ];
+
   buildInputs = [
     libxml2
     libxslt
   ];
 
-  patches = [
-    (fetchpatch {
-      name = "0001-Fix-build-with-libxml2-2.12.patch";
-      url = "https://sourceforge.net/p/xmlstar/patches/_discuss/thread/890e29655a/66ca/attachment/0001-Fix-build-with-libxml2-2.12.patch";
-      hash = "sha256-XEk7aFOdrzdec1j2ffERJQbLH0AUNJA52QwA9jf4XWA=";
-    })
-    (fetchurl {
-      name = "libxml-2.14.patch";
-      url = "https://github.com/termux/termux-packages/raw/39135f3f1190268d127b998c2c6040d9af611ba5/packages/xmlstarlet/libxml2-2.14-attribute-unused.patch";
-      hash = "sha256-zHkUQsrhPLWI3kdfCITbcixpBmDRmxSM2Viz5R+8q5E=";
-    })
-  ];
+  # installing the docs requires internet
+  configureFlags = [ "--disable-install-docs" ];
 
   preConfigure = ''
     export LIBXSLT_PREFIX=${libxslt.dev}
@@ -53,7 +44,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Command line tool for manipulating and querying XML data";
-    homepage = "https://xmlstar.sourceforge.net/";
+    homepage = "https://xmlstarlet.github.io/";
     license = lib.licenses.mit;
     mainProgram = "xmlstarlet";
     platforms = lib.platforms.unix;
