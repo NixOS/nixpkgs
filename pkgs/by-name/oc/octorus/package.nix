912c7ab0b05c65ab4c020db20d75622d5234b37c
KangaZero <samuelyongw@gmail.com>
2026-09-17 14:52:03 +0900

    octorus: add kangazero as maintainer

--- pkgs/by-name/oc/octorus/package.nix
+++ pkgs/by-name/oc/octorus/package.nix
@@ -49,6 +49,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [
       matthiasbeyer
+      kangazero
     ];
     mainProgram = "or";
   };
