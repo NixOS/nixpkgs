d2d3eb058bd144e30135ba28d02694374d95ab38
Keir Lawson <keirlawson@gmail.com>
2026-09-17 16:49:14 +0100

    maintainers: add keirlawson

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14735,6 +14735,11 @@
     github = "keenanweaver";
     githubId = 37268985;
   };
+  keirlawson = {
+    name = "Keir Lawson";
+    github = "keirlawson";
+    githubId = 49213;
+  };
   keksgesicht = {
     name = "Jan Braun";
     email = "git@keksgesicht.de";
