1a8f8032368b813e19b8bed463bed47fd1023899
Robert Schütz <nix@dotlambda.de>
2026-09-18 16:57:35 -0700

    home-assistant-custom-components.waste_collection_schedule: 2.34.1 -> 2.34.2
    Diff: https://github.com/mampfes/hacs_waste_collection_schedule/compare/v2.34.1...v2.34.2
    
    Changelog: https://github.com/mampfes/hacs_waste_collection_schedule/releases/tag/v2.34.2

--- pkgs/servers/home-assistant/custom-components/waste_collection_schedule/package.nix
+++ pkgs/servers/home-assistant/custom-components/waste_collection_schedule/package.nix
@@ -20,13 +20,13 @@
 buildHomeAssistantComponent rec {
   owner = "mampfes";
   domain = "waste_collection_schedule";
-  version = "2.34.1";
+  version = "2.34.2";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "hacs_waste_collection_schedule";
     tag = "v${version}";
-    hash = "sha256-Eql14DO55xS3qwknmS7etp0a5zRMAfS2GRyfoUaqg1I=";
+    hash = "sha256-vGsyNR04aGwj/cp4m23kg+PS5bLGgUM6jI8rhnPHJjU=";
   };
 
   dependencies = [
