4fc0f659337b70ec5addaea8ee3683e67d8e0f1c
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-18 04:10:00 +0000

    ocamlPackages.miou: 0.10.0 -> 0.10.1

--- pkgs/development/ocaml-modules/miou/default.nix
+++ pkgs/development/ocaml-modules/miou/default.nix
@@ -7,13 +7,13 @@
 
 buildDunePackage (finalAttrs: {
   pname = "miou";
-  version = "0.10.0";
+  version = "0.10.1";
 
   minimalOCamlVersion = "5.2";
 
   src = fetchurl {
     url = "https://github.com/robur-coop/miou/releases/download/v${finalAttrs.version}/miou-${finalAttrs.version}.tbz";
-    hash = "sha256-vxlUxvZqydMGs7T5SLGfWdLDJyE6+TDhnZ+8c/E0+hg=";
+    hash = "sha256-Vuow/9CSTZQciv25Z0klLMSvYUZdOSF4xwR1p4WShNU=";
   };
 
   buildInputs = [ dune-configurator ];
