6c4310ba256c5abb0fdb1d5fa5c88ffbcf455cce
Defelo <mail@defelo.de>
2026-09-18 20:19:04 +0200

    radicle-node{,-unstable}: use nix-update-script

--- pkgs/by-name/ra/radicle-node-unstable/package.nix
+++ pkgs/by-name/ra/radicle-node-unstable/package.nix
@@ -4,5 +4,8 @@ radicle-node.override {
   version = "1.10.3";
   srcHash = "sha256-xbVu+s4TQMc6fA8iUNq01y5lDxfC76Pwe6Z2rqA6C5Q=";
   cargoHash = "sha256-zQ6XnDJt3pek0WzC0J8PY111LGuzZeSCZ1QxwYAhPWw=";
-  updateScript = ./update-unstable.sh;
+  updateScriptExtraArgs = [
+    "--version=unstable"
+    "--override-filename=pkgs/by-name/ra/radicle-node-unstable/package.nix"
+  ];
 }
