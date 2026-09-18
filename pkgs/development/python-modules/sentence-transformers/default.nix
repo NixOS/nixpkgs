724a48be73ad84b3202b6a75aec3fcb2abae6f3c
Gaetan Lepage <gaetan@glepage.com>
2026-09-18 16:20:40 +0000

    python3Packages.sentence-transformers: 6.0.1 -> 6.1.0
    Diff: https://github.com/huggingface/sentence-transformers/compare/v6.0.1...v6.1.0
    
    Changelog: https://github.com/huggingface/sentence-transformers/releases/tag/v6.1.0

--- pkgs/development/python-modules/sentence-transformers/default.nix
+++ pkgs/development/python-modules/sentence-transformers/default.nix
@@ -34,7 +34,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "sentence-transformers";
-  version = "6.0.1";
+  version = "6.1.0";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -42,7 +42,7 @@ buildPythonPackage (finalAttrs: {
     owner = "huggingface";
     repo = "sentence-transformers";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-SKN9n9rYku3VUzafSiDflkz02V0NV7pXPHZIbKURoTk=";
+    hash = "sha256-65ImwbtYh07BeztkXd/wbiYMTFRg8yYRmJcrLyjU1V0=";
   };
 
   build-system = [ setuptools ];
