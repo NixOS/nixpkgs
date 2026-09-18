4a5ba9e116b0b10b908bc11e994816ae997d757a
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-09-17 20:48:29 -0400

    openipmi: improve sim packaging
    This patch includes SDR files that are necessary to run the sim. It also
    fixes one ordering bug in the command file, as well as a bug in
    set_sensor_value command.
    
    These changes unlock the next patch in the series where we utilize the
    sim for prometheus ipmi exporter validation.

--- pkgs/by-name/op/openipmi/package.nix
+++ pkgs/by-name/op/openipmi/package.nix
@@ -1,6 +1,7 @@
 {
   stdenv,
   buildPackages,
+  fetchpatch,
   fetchurl,
   popt,
   ncurses,
@@ -18,6 +19,20 @@ stdenv.mkDerivation (finalAttrs: {
     url = "mirror://sourceforge/openipmi/OpenIPMI-${finalAttrs.version}.tar.gz";
     sha256 = "sha256-xi049dp99Cmaw6ZSUI6VlTd1JEAYHjTHayrs69fzAbk=";
   };
+
+  patches = [
+    # Fix broken command order in shipped ipmisim1:
+    (fetchpatch {
+      url = "https://github.com/cminyard/openipmi/commit/07adcedd824496d69be469101a7be1bbe371741a.patch";
+      hash = "sha256-zAbnyliZr5HV9dYlyse4ff1lYJty+/74aqmXHWJM13c=";
+    })
+
+    # Sensors not marked ready after set_sensor_value.
+    (fetchpatch {
+      url = "https://github.com/cminyard/openipmi/commit/2325cc63727bab6a0f29fca2db4ba7d69c11e3b9.patch";
+      hash = "sha256-ZbxV7ThFDbI5vxlLLavMrAZzCj3gPnR0sRcyHub5S30=";
+    })
+  ];
 
   postConfigure = lib.optionalString (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
     substituteInPlace lanserv/Makefile \
@@ -41,6 +56,13 @@ stdenv.mkDerivation (finalAttrs: {
   makeFlags = [
     "BUILD_CC=${stdenv.cc.targetPrefix}cc"
   ];
+
+  # Include SDR files; ipmi-sim requires .bsdr (installed as sdr.20.main) to
+  # advertise its sensor names.
+  postInstall = ''
+    install -Dm444 lanserv/ipmisim1.sdrs $out/share/openipmi/ipmisim1.sdrs
+    install -Dm444 lanserv/ipmisim1.bsdr $out/share/openipmi/ipmisim1.bsdr
+  '';
 
   outputs = [
     "out"
