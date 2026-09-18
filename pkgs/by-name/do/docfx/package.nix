a9147893a936ce0252084692097c89807ce7b480
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-18 17:44:34 +0000

    docfx: 2.78.5 -> 2.80.1

--- pkgs/by-name/do/docfx/package.nix
+++ pkgs/by-name/do/docfx/package.nix
@@ -6,12 +6,12 @@
 
 buildDotnetGlobalTool {
   pname = "docfx";
-  version = "2.78.5";
+  version = "2.80.1";
 
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
   dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-  nugetHash = "sha256-kkoQQHXkv36ulSAUfsEKgJ6CqEOOwzAQRYHSGUNZaMU=";
+  nugetHash = "sha256-v9cfkqa+GNiLyBVK+asQSHpbju5TzkcE35xZ3/QkFig=";
 
   meta = {
     description = "Build your technical documentation site with docfx, with landing pages, markdown, API reference docs for .NET, REST API and more";
