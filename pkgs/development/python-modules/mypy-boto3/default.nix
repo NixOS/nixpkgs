86975a6a0eee7f72f9a80f8bbaf310512f41d6d1
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-18 08:49:44 +0200

    python3Packages.mypy-boto3-connect: 1.43.91 -> 1.43.97

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -335,8 +335,8 @@ in
       "sha256-3JYcWKFk0dKJg/qn+EBvxeAO5xh5PXCU3dTEWDr1oXI=";
 
   mypy-boto3-connect =
-    buildMypyBoto3Package "connect" "1.43.91"
-      "sha256-PaYz2Zs2V9nVbCicx2Q951HCY8cCSHH3SmGiq6VdGp0=";
+    buildMypyBoto3Package "connect" "1.43.97"
+      "sha256-56zFmPmX5g+87mqd6VHHmMBiDyZPRCj3LWjWoPjd0hU=";
 
   mypy-boto3-connect-contact-lens =
     buildMypyBoto3Package "connect-contact-lens" "1.43.79"
