08a8f0ac2243439a8b29c7389da4df27c7a1cabc
Darren Rambaud <225436867+debtquity@users.noreply.github.com>
2026-09-17 22:26:57 -0500

    barman: add debtquity as maintainer
    * adopt package as maintainer
    * clean up `meta`

--- pkgs/by-name/ba/barman/package.nix
+++ pkgs/by-name/ba/barman/package.nix
@@ -80,11 +80,53 @@ python3Packages.buildPythonApplication (finalAttrs: {
 
   meta = {
     description = "Backup and Recovery Manager for PostgreSQL";
-    homepage = "https://www.pgbarman.org/";
+    longDescription = ''
+      **Barman** (Backup and Recovery Manager) is an open-source administration
+      tool for disaster recovery of PostgreSQL servers written in Python.
+
+      It allows your organisation to perform remote backups of multiple servers
+      in business critical environments and help DBAs during the recovery phase.
+
+      Barman’s most wanted features include backup catalogs, incremental backup,
+      retention policies, remote recovery, archiving and compression of WAL files
+      and backups.
+
+      Barman was originally developed by 2ndQuadrant, and is now maintained by
+      [EnterpriseDB](https://enterprisedb.com).
+
+      The main features and goals of Barman are:
+
+      * Full hot physical backup of a PostgreSQL server
+      * Point-In-Time-Recovery (PITR)
+      * Management of multiple PostgreSQL servers
+      * Remote backup via rsync/SSH or `pg_basebackup` (including a 9.2+ standby)
+      * Support for both local and remote (via SSH) recovery
+      * Support for both WAL archiving and streaming
+      * Support for synchronous WAL streaming (“zero data loss”, RPO=0)
+      * Incremental backup and recovery
+      * Incremental backup and recovery
+      * Parallel backup and recovery
+      * Hub of WAL files for enhanced integration with standby servers
+      * Management of retention policies for backups and WAL files
+      * Server status and information
+      * Compression of WAL files (bzip2, gzip or custom)
+      * Management of base backups and WAL files through a catalogue
+      * A simple INI configuration file
+      * Totally written in Python
+      * Relocation of PGDATA and tablespaces at recovery time
+      * General and disk usage information of backups
+      * Server diagnostics for backup
+      * Integration with standard archiving tools (e.g. tar)
+      * Pre/Post backup hook scripts
+      * Local storage of metadata
+    '';
+    homepage = "https://pgbarman.org";
     changelog = "https://github.com/EnterpriseDB/barman/blob/${finalAttrs.src.tag}/RELNOTES.md";
-    mainProgram = "barman";
     license = lib.licenses.gpl3Plus;
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [
+      debtquity
+    ];
+    mainProgram = "barman";
     platforms = lib.platforms.unix;
   };
 })
