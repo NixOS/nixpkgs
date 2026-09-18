0841100c28e1efed4100fc21edbca6b3ebf3fd85
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-18 13:07:32 +0000

    scipopt-scip: 10.0.3 -> 10.1.0

--- pkgs/by-name/sc/scipopt-scip/package.nix
+++ pkgs/by-name/sc/scipopt-scip/package.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "scipopt-scip";
-  version = "10.0.3";
+  version = "10.1.0";
 
   src = fetchFromGitHub {
     owner = "scipopt";
     repo = "scip";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dR2H18xLbLko3AQMiEwAL5mzLYcUD9U9g3AjWDT5Kz8=";
+    hash = "sha256-BCxR/CY3iT724VOJJKQgGq63xyuZkPrPjbM7RMDAXHA=";
   };
 
   nativeBuildInputs = [ cmake ];
