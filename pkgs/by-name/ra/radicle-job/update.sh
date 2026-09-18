b0ed5f9fa77743a275cde078bf6c01916bdde366
Defelo <mail@defelo.de>
2026-09-18 19:49:15 +0200

    radicle-job: use nix-update-script

--- pkgs/by-name/ra/radicle-job/update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p coreutils gnugrep common-updater-scripts nix-update
-
-version=$(list-git-tags | grep -oP '^releases/v\K\d+\.\d+\.\d+$' | sort -rV | head -1)
-nix-update --version="$version" radicle-job
