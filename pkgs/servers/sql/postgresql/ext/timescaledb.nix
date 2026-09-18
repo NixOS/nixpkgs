ad6210ea86fc9c587d958f8fc8e852fd4816c5d0
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-18 20:50:37 +0000

    postgresqlPackages.timescaledb-apache: 2.29.2 -> 2.30.1

--- pkgs/servers/sql/postgresql/ext/timescaledb.nix
+++ pkgs/servers/sql/postgresql/ext/timescaledb.nix
@@ -13,13 +13,13 @@
 
 postgresqlBuildExtension (finalAttrs: {
   pname = "timescaledb${lib.optionalString (!enableUnfree) "-apache"}";
-  version = "2.29.2";
+  version = "2.30.1";
 
   src = fetchFromGitHub {
     owner = "timescale";
     repo = "timescaledb";
     tag = finalAttrs.version;
-    hash = "sha256-pR68kA795hFX4aEC7UXyruigDNnudQVekDg+eIpdzJU=";
+    hash = "sha256-VkAkOpngiJ5Wzfk2goTfvXim66HID05UKkWmv3wryLA=";
   };
 
   nativeBuildInputs = [ cmake ];
