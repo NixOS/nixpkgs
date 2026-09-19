2f1e464c4c5d87e0187283efb33c1691bb00a505
Acid Bong <acidbong@tilde.club>
2026-09-19 10:08:31 +0300

    nixos/guix: make default substituter entries mergeable with user values

--- nixos/modules/services/misc/guix/default.nix
+++ nixos/modules/services/misc/guix/default.nix
@@ -141,16 +141,19 @@ in
     substituters = {
       urls = lib.mkOption {
         type = with lib.types; listOf str;
-        default = [
+        default = lib.warn ''
+          Default Guix substituter URLs now have the same priority as
+          user-defined values, which removes the necessity to access
+          `options.services.guix.substituters.urls.default`.
+        '' [ ];
+        defaultText = [
           "https://ci.guix.gnu.org"
           "https://bordeaux.guix.gnu.org"
         ];
-        example = lib.literalExpression ''
-          options.services.guix.substituters.urls.default ++ [
-            "https://guix.example.com"
-            "https://guix.example.org"
-          ]
-        '';
+        example = [
+          "https://guix.example.com"
+          "https://guix.example.org"
+        ];
         description = ''
           A list of substitute servers' URLs for the Guix daemon to download
           substitutes from.
@@ -159,15 +162,14 @@ in
 
       authorizedKeys = lib.mkOption {
         type = with lib.types; listOf path;
-        default = [
-          "${cfg.package}/share/guix/ci.guix.gnu.org.pub"
-          "${cfg.package}/share/guix/bordeaux.guix.gnu.org.pub"
-        ];
-        defaultText = ''
-          The packaged signing keys from {option}`services.guix.package`.
+        defaultText = lib.literalExpression ''
+          [
+            "''${config.services.guix.package}/share/guix/ci.guix.gnu.org.pub"
+            "''${config.services.guix.package}/share/guix/bordeaux.guix.gnu.org.pub"
+          ]
         '';
         example = lib.literalExpression ''
-          options.services.guix.substituters.authorizedKeys.default ++ [
+          [
             (pkgs.fetchurl {
               url = "https://guix.example.com/signing-key.pub";
               hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
@@ -266,225 +268,236 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      {
-        environment.systemPackages = [ package ];
-
-        users.users = guixBuildUsers cfg.nrBuildUsers;
-        users.groups.${cfg.group} = { };
-
-        # Guix uses Avahi (through guile-avahi) both for the auto-discovering and
-        # advertising substitute servers in the local network.
-        services.avahi.enable = lib.mkDefault true;
-        services.avahi.publish.enable = lib.mkDefault true;
-        services.avahi.publish.userServices = lib.mkDefault true;
-
-        # It's similar to Nix daemon so there's no question whether or not this
-        # should be sandboxed.
-        systemd.services.guix-daemon = {
-          environment = serviceEnv // config.networking.proxy.envVars;
-          serviceConfig = {
-            OOMPolicy = "continue";
-            RemainAfterExit = "yes";
-            Restart = "always";
-            TasksMax = 8192;
-            ExecStart = ''
-              ${lib.getExe' package "guix-daemon"} \
-                --build-users-group=${cfg.group} \
-                ${
-                  lib.optionalString (
-                    cfg.substituters.urls != [ ]
-                  ) "--substitute-urls='${lib.concatStringsSep " " cfg.substituters.urls}'"
-                } \
-                ${lib.escapeShellArgs cfg.extraArgs}
-            '';
-          };
-          unitConfig.RequiresMountsFor = [
-            cfg.storeDir
-            cfg.stateDir
-          ];
-          wantedBy = [ "multi-user.target" ];
-        };
-
-        # This is based from Nix daemon socket unit from upstream Nix package.
-        # Guix build daemon has support for systemd-style socket activation.
-        systemd.sockets.guix-daemon = {
-          description = "Guix daemon socket";
-          before = [ "multi-user.target" ];
-          listenStreams = [ "${cfg.stateDir}/guix/daemon-socket/socket" ];
-          unitConfig.RequiresMountsFor = [
-            cfg.storeDir
-            cfg.stateDir
-          ];
-          wantedBy = [ "sockets.target" ];
-        };
-
-        systemd.mounts = [
-          {
-            description = "Guix read-only store directory";
-            before = [ "guix-daemon.service" ];
-            what = cfg.storeDir;
-            where = cfg.storeDir;
-            type = "none";
-            options = "bind,ro";
-
-            unitConfig.DefaultDependencies = false;
-            wantedBy = [ "guix-daemon.service" ];
-          }
+  config = lib.mkMerge [
+    {
+      services.guix.substituters = {
+        urls = [
+          "https://ci.guix.gnu.org"
+          "https://bordeaux.guix.gnu.org"
         ];
+        authorizedKeys = [
+          "${cfg.package}/share/guix/ci.guix.gnu.org.pub"
+          "${cfg.package}/share/guix/bordeaux.guix.gnu.org.pub"
+        ];
+      };
+    }
 
-        # Make transferring files from one store to another easier with the usual
-        # case being of most substitutes from the official Guix CI instance.
-        environment.etc."guix/acl".source = aclFile;
+    (lib.mkIf cfg.enable {
+      environment.systemPackages = [ package ];
 
-        # Link the usual Guix profiles to the home directory. This is useful in
-        # ephemeral setups where only certain part of the filesystem is
-        # persistent (e.g., "Erase my darlings"-type of setup).
-        system.userActivationScripts.guix-activate-user-profiles.text =
-          let
-            guixProfile = profile: "${cfg.stateDir}/guix/profiles/per-user/\${USER}/${profile}";
-            linkProfile =
-              profile: location:
-              let
-                userProfile = guixProfile profile;
-              in
-              ''
-                [ -d "${userProfile}" ] && ln -sfn "${userProfile}" "${location}"
-              '';
-            linkProfileToPath =
-              acc: profile: location:
-              acc + (linkProfile profile location);
+      users.users = guixBuildUsers cfg.nrBuildUsers;
+      users.groups.${cfg.group} = { };
 
-            # This should contain export-only Guix user profiles. The rest of it is
-            # handled manually in the activation script.
-            guixUserProfiles' = lib.attrsets.removeAttrs guixUserProfiles [ "guix-home" ];
+      # Guix uses Avahi (through guile-avahi) both for the auto-discovering and
+      # advertising substitute servers in the local network.
+      services.avahi.enable = lib.mkDefault true;
+      services.avahi.publish.enable = lib.mkDefault true;
+      services.avahi.publish.userServices = lib.mkDefault true;
 
-            linkExportsScript = lib.foldlAttrs linkProfileToPath "" guixUserProfiles';
-          in
-          ''
-            # Don't export this please! It is only expected to be used for this
-            # activation script and nothing else.
-            XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
-
-            # Linking the usual Guix profiles into the home directory.
-            ${linkExportsScript}
-
-            # Activate all of the default Guix non-exports profiles manually.
-            ${linkProfile "guix-home" "$HOME/.guix-home"}
-            [ -L "$HOME/.guix-home" ] && "$HOME/.guix-home/activate"
+      # It's similar to Nix daemon so there's no question whether or not this
+      # should be sandboxed.
+      systemd.services.guix-daemon = {
+        environment = serviceEnv // config.networking.proxy.envVars;
+        serviceConfig = {
+          OOMPolicy = "continue";
+          RemainAfterExit = "yes";
+          Restart = "always";
+          TasksMax = 8192;
+          ExecStart = ''
+            ${lib.getExe' package "guix-daemon"} \
+              --build-users-group=${cfg.group} \
+              ${
+                lib.optionalString (
+                  cfg.substituters.urls != [ ]
+                ) "--substitute-urls='${lib.concatStringsSep " " cfg.substituters.urls}'"
+              } \
+              ${lib.escapeShellArgs cfg.extraArgs}
           '';
+        };
+        unitConfig.RequiresMountsFor = [
+          cfg.storeDir
+          cfg.stateDir
+        ];
+        wantedBy = [ "multi-user.target" ];
+      };
 
-        # GUIX_LOCPATH is basically LOCPATH but for Guix libc which in turn used by
-        # virtually every Guix-built packages. This is so that Guix-installed
-        # applications wouldn't use incompatible locale data and not touch its host
-        # system.
-        environment.sessionVariables.GUIX_LOCPATH = lib.makeSearchPath "lib/locale" guixProfiles;
+      # This is based from Nix daemon socket unit from upstream Nix package.
+      # Guix build daemon has support for systemd-style socket activation.
+      systemd.sockets.guix-daemon = {
+        description = "Guix daemon socket";
+        before = [ "multi-user.target" ];
+        listenStreams = [ "${cfg.stateDir}/guix/daemon-socket/socket" ];
+        unitConfig.RequiresMountsFor = [
+          cfg.storeDir
+          cfg.stateDir
+        ];
+        wantedBy = [ "sockets.target" ];
+      };
 
-        # What Guix profiles export is very similar to Nix profiles so it is
-        # acceptable to list it here. Also, it is more likely that the user would
-        # want to use packages explicitly installed from Guix so we're putting it
-        # first.
-        environment.profiles = lib.mkBefore guixProfiles;
-      }
+      systemd.mounts = [
+        {
+          description = "Guix read-only store directory";
+          before = [ "guix-daemon.service" ];
+          what = cfg.storeDir;
+          where = cfg.storeDir;
+          type = "none";
+          options = "bind,ro";
 
-      (lib.mkIf cfg.publish.enable {
-        systemd.services.guix-publish = {
-          description = "Guix remote store";
-          environment = serviceEnv;
+          unitConfig.DefaultDependencies = false;
+          wantedBy = [ "guix-daemon.service" ];
+        }
+      ];
 
-          # Mounts will be required by the daemon service anyways so there's no
-          # need add RequiresMountsFor= or something similar.
-          requires = [ "guix-daemon.service" ];
-          after = [ "guix-daemon.service" ];
-          partOf = [ "guix-daemon.service" ];
+      # Make transferring files from one store to another easier with the usual
+      # case being of most substitutes from the official Guix CI instance.
+      environment.etc."guix/acl".source = aclFile;
 
-          preStart = lib.mkIf cfg.publish.generateKeyPair ''
-            # Generate the keypair if it's missing.
-            [ -f "/etc/guix/signing-key.sec" ] && [ -f "/etc/guix/signing-key.pub" ] || \
-              ${lib.getExe' package "guix"} archive --generate-key || {
-                rm /etc/guix/signing-key.*;
-                ${lib.getExe' package "guix"} archive --generate-key;
-              }
-          '';
-
-          serviceConfig = {
-            Restart = "always";
-            RestartSec = 10;
-
-            ExecStart = ''
-              ${lib.getExe' package "guix"} publish \
-                --user=${cfg.publish.user} --port=${toString cfg.publish.port} \
-                ${lib.escapeShellArgs cfg.publish.extraArgs}
+      # Link the usual Guix profiles to the home directory. This is useful in
+      # ephemeral setups where only certain part of the filesystem is
+      # persistent (e.g., "Erase my darlings"-type of setup).
+      system.userActivationScripts.guix-activate-user-profiles.text =
+        let
+          guixProfile = profile: "${cfg.stateDir}/guix/profiles/per-user/\${USER}/${profile}";
+          linkProfile =
+            profile: location:
+            let
+              userProfile = guixProfile profile;
+            in
+            ''
+              [ -d "${userProfile}" ] && ln -sfn "${userProfile}" "${location}"
             '';
+          linkProfileToPath =
+            acc: profile: location:
+            acc + (linkProfile profile location);
 
-            ProtectClock = true;
-            ProtectHostname = true;
-            ProtectKernelTunables = true;
-            ProtectKernelModules = true;
-            ProtectControlGroups = true;
-            SystemCallFilter = [
-              "@system-service"
-              "@debug"
-              "@setuid"
-            ];
+          # This should contain export-only Guix user profiles. The rest of it is
+          # handled manually in the activation script.
+          guixUserProfiles' = lib.attrsets.removeAttrs guixUserProfiles [ "guix-home" ];
 
-            RestrictNamespaces = true;
-            RestrictAddressFamilies = [
-              "AF_UNIX"
-              "AF_INET"
-              "AF_INET6"
-            ];
+          linkExportsScript = lib.foldlAttrs linkProfileToPath "" guixUserProfiles';
+        in
+        ''
+          # Don't export this please! It is only expected to be used for this
+          # activation script and nothing else.
+          XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
 
-            # While the permissions can be set, it is assumed to be taken by Guix
-            # daemon service which it has already done the setup.
-            ConfigurationDirectory = "guix";
+          # Linking the usual Guix profiles into the home directory.
+          ${linkExportsScript}
 
-            AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
-            CapabilityBoundingSet = [
-              "CAP_NET_BIND_SERVICE"
-              "CAP_SETUID"
-              "CAP_SETGID"
-            ];
-          };
-          wantedBy = [ "multi-user.target" ];
+          # Activate all of the default Guix non-exports profiles manually.
+          ${linkProfile "guix-home" "$HOME/.guix-home"}
+          [ -L "$HOME/.guix-home" ] && "$HOME/.guix-home/activate"
+        '';
+
+      # GUIX_LOCPATH is basically LOCPATH but for Guix libc which in turn used by
+      # virtually every Guix-built packages. This is so that Guix-installed
+      # applications wouldn't use incompatible locale data and not touch its host
+      # system.
+      environment.sessionVariables.GUIX_LOCPATH = lib.makeSearchPath "lib/locale" guixProfiles;
+
+      # What Guix profiles export is very similar to Nix profiles so it is
+      # acceptable to list it here. Also, it is more likely that the user would
+      # want to use packages explicitly installed from Guix so we're putting it
+      # first.
+      environment.profiles = lib.mkBefore guixProfiles;
+    })
+
+    (lib.mkIf cfg.publish.enable {
+      systemd.services.guix-publish = {
+        description = "Guix remote store";
+        environment = serviceEnv;
+
+        # Mounts will be required by the daemon service anyways so there's no
+        # need add RequiresMountsFor= or something similar.
+        requires = [ "guix-daemon.service" ];
+        after = [ "guix-daemon.service" ];
+        partOf = [ "guix-daemon.service" ];
+
+        preStart = lib.mkIf cfg.publish.generateKeyPair ''
+          # Generate the keypair if it's missing.
+          [ -f "/etc/guix/signing-key.sec" ] && [ -f "/etc/guix/signing-key.pub" ] || \
+            ${lib.getExe' package "guix"} archive --generate-key || {
+              rm /etc/guix/signing-key.*;
+              ${lib.getExe' package "guix"} archive --generate-key;
+            }
+        '';
+
+        serviceConfig = {
+          Restart = "always";
+          RestartSec = 10;
+
+          ExecStart = ''
+            ${lib.getExe' package "guix"} publish \
+              --user=${cfg.publish.user} --port=${toString cfg.publish.port} \
+              ${lib.escapeShellArgs cfg.publish.extraArgs}
+          '';
+
+          ProtectClock = true;
+          ProtectHostname = true;
+          ProtectKernelTunables = true;
+          ProtectKernelModules = true;
+          ProtectControlGroups = true;
+          SystemCallFilter = [
+            "@system-service"
+            "@debug"
+            "@setuid"
+          ];
+
+          RestrictNamespaces = true;
+          RestrictAddressFamilies = [
+            "AF_UNIX"
+            "AF_INET"
+            "AF_INET6"
+          ];
+
+          # While the permissions can be set, it is assumed to be taken by Guix
+          # daemon service which it has already done the setup.
+          ConfigurationDirectory = "guix";
+
+          AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+          CapabilityBoundingSet = [
+            "CAP_NET_BIND_SERVICE"
+            "CAP_SETUID"
+            "CAP_SETGID"
+          ];
         };
+        wantedBy = [ "multi-user.target" ];
+      };
 
-        users.users.guix-publish = lib.mkIf (cfg.publish.user == "guix-publish") {
-          description = "Guix publish user";
-          group = config.users.groups.guix-publish.name;
-          isSystemUser = true;
+      users.users.guix-publish = lib.mkIf (cfg.publish.user == "guix-publish") {
+        description = "Guix publish user";
+        group = config.users.groups.guix-publish.name;
+        isSystemUser = true;
+      };
+      users.groups.guix-publish = { };
+    })
+
+    (lib.mkIf cfg.gc.enable {
+      # This service should be handled by root to collect all garbage by all
+      # users.
+      systemd.services.guix-gc = {
+        description = "Guix garbage collection";
+        startAt = cfg.gc.dates;
+        serviceConfig = {
+          Type = "oneshot";
+
+          ExecStart = "${lib.getExe' package "guix"} gc ${lib.escapeShellArgs cfg.gc.extraArgs}";
+
+          PrivateDevices = true;
+          PrivateNetwork = true;
+          ProtectControlGroups = true;
+          ProtectHostname = true;
+          ProtectKernelTunables = true;
+          SystemCallFilter = [
+            "@default"
+            "@file-system"
+            "@basic-io"
+            "@system-service"
+          ];
         };
-        users.groups.guix-publish = { };
-      })
+      };
 
-      (lib.mkIf cfg.gc.enable {
-        # This service should be handled by root to collect all garbage by all
-        # users.
-        systemd.services.guix-gc = {
-          description = "Guix garbage collection";
-          startAt = cfg.gc.dates;
-          serviceConfig = {
-            Type = "oneshot";
-
-            ExecStart = "${lib.getExe' package "guix"} gc ${lib.escapeShellArgs cfg.gc.extraArgs}";
-
-            PrivateDevices = true;
-            PrivateNetwork = true;
-            ProtectControlGroups = true;
-            ProtectHostname = true;
-            ProtectKernelTunables = true;
-            SystemCallFilter = [
-              "@default"
-              "@file-system"
-              "@basic-io"
-              "@system-service"
-            ];
-          };
-        };
-
-        systemd.timers.guix-gc.timerConfig.Persistent = true;
-      })
-    ]
-  );
+      systemd.timers.guix-gc.timerConfig.Persistent = true;
+    })
+  ];
 }
