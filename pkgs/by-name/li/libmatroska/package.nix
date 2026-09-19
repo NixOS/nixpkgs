c83485e7f4c76289608f5065b05cc9bbd3530a95
rnhmjoj <rnhmjoj@eurofusion.eu>
2026-09-19 17:10:00 +0200

    libmatroska: 1.7.1 -> 1.7.2
    (cherry picked from commit aa9befac202c2c92aa133627b867607c57dea836)

--- pkgs/by-name/li/libmatroska/package.nix
+++ pkgs/by-name/li/libmatroska/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   cmake,
   libebml,
   nix-update-script,
@@ -14,7 +13,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libmatroska";
-  version = "1.7.1";
+  version = "1.7.2";
 
   outputs = [
     "dev"
@@ -25,16 +24,8 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "Matroska-Org";
     repo = "libmatroska";
     rev = "release-${finalAttrs.version}";
-    hash = "sha256-hfu3Q1lIyMlWFWUM2Pu70Hie0rlQmua7Kq8kSIWnfHE=";
+    hash = "sha256-FedoJxUsRgKQn41PZGBaeF0O29PVQEOK20LsNMK3xHo=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "libmatroska-fix-cmake-4.patch";
-      url = "https://github.com/Matroska-Org/libmatroska/commit/dc80e194e93e6f0e25c8ad3e015d83aca2a99e10.patch";
-      hash = "sha256-2dKRJ6z5rOrLJ5agvXQ6k8TPi5rTMA3H1wCO2F5tBbc=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake
