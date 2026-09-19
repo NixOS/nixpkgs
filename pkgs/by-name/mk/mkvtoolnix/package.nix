769ced4b0f1ff27a2757fb68b6334c3981395e33
rnhmjoj <rnhmjoj@eurofusion.eu>
2026-09-19 17:12:03 +0200

    mkvtoolnix: 100.0 -> 102.0
    (cherry picked from commit 8d4b64a98432ed80562a7b1224d9189145a9156c)

--- pkgs/by-name/mk/mkvtoolnix/package.nix
+++ pkgs/by-name/mk/mkvtoolnix/package.nix
@@ -49,13 +49,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "mkvtoolnix";
-  version = "100.0";
+  version = "102.0";
 
   src = fetchFromCodeberg {
     owner = "mbunkus";
     repo = "mkvtoolnix";
     tag = "release-${finalAttrs.version}";
-    hash = "sha256-85mL3/x7SoTgOxU/YCFh58vcGzHLG3qPbbG4MD5dB9o=";
+    hash = "sha256-YsenzSOMwh+jDOgdYHQhiicdHEHIGzd3UqPyecdgJaI=";
   };
 
   passthru = {
