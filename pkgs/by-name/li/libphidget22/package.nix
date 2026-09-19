a6028620b5bf45cb9bf353aad1bf4dd0aa291da6
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-19 06:16:39 +0000

    libphidget22: 1.25.20260512 -> 1.26.20260821

--- pkgs/by-name/li/libphidget22/package.nix
+++ pkgs/by-name/li/libphidget22/package.nix
@@ -7,7 +7,7 @@
 }:
 let
   # This package should be updated together with libphidget22extra
-  version = "1.25.20260512";
+  version = "1.26.20260821";
 in
 stdenv.mkDerivation {
   pname = "libphidget22";
@@ -15,7 +15,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22/libphidget22-${version}.tar.gz";
-    hash = "sha256-wrLPP32gOjXsewdLUCe7lUUqfib3egrOONey9mcUfV8=";
+    hash = "sha256-kSb2x2Zc2bB+JCjRtVljl7A3RYhfMzpueT9Dlee+riY=";
   };
 
   nativeBuildInputs = [ automake ];
