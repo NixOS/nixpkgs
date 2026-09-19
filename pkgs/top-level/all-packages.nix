8d8dee4c13745aac6c772f00a4f08fab21667183
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-09-19 09:36:31 -0400

    uniscribe: move to by-name
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2009,8 +2009,6 @@ with pkgs;
 
   frostwire-bin = callPackage ../applications/networking/p2p/frostwire/frostwire-bin.nix { };
 
-  uniscribe = callPackage ../tools/text/uniscribe { };
-
   inherit (callPackages ../tools/filesystems/garage { })
     garage
     garage_1
