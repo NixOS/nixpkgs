783c5ec0a5c1ab3176f8aef8016cf0aa9537b94e
semka612 <smgmail987@gmail.com>
2026-09-19 19:06:01 +0300

    neuro-karaoke-wrapper: init at 1.8.1

---
+++ pkgs/by-name/ne/neuro-karaoke-wrapper/package.nix
@@ -0,0 +1,22 @@
+{ lib, appimageTools, fetchurl, nix-update-script }:
+appimageTools.wrapType2 rec {
+  pname = "neuro-karaoke-wrapper";
+  version = "1.8.1";
+
+  src = fetchurl {
+    url = "https://github.com/AferilVT/neuro-karaoke-wrapper/releases/download/v${version}/Neuro.Karaoke.Player-x86_64.AppImage";
+    hash = "sha256-2iZdZq2XkK2jlVsg1i23+9IHyV5QPNl1bxL33qk/s+E=";
+  };
+
+  meta = {
+    description = "Neuro Karaoke Player";
+    homepage = "https://github.com/AferilVT/neuro-karaoke-wrapper";
+    changelog = "https://github.com/AferilVT/neuro-karaoke-wrapper/releases/tag/v${version}";
+    license = lib.licenses.gpl3;
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    maintainers = [ lib.maintainers.semka612 ];
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "neuro-karaoke-wrapper";
+  };
+  passthru.updateScript = nix-update-script { };
+}
