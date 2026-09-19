62e8d968edd91784029caa56b54d58db930bf220
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-09-19 01:37:04 -0400

    haste-client: move to by-name
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>
    (cherry picked from commit 2efa23ab8576086cef10e4dcdb00b55dc5886f13)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2337,8 +2337,6 @@ with pkgs;
 
   hassil = with python3Packages; toPythonApplication hassil;
 
-  haste-client = callPackage ../tools/misc/haste-client { };
-
   hareThirdParty = recurseIntoAttrs (callPackage ./hare-third-party.nix { });
 
   hdf5 = callPackage ../tools/misc/hdf5 {
