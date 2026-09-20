90c93ca5fd88e1267a1ece756bcac1af0c0489eb
Jeffrey Allan <195243343+jeff-oomu@users.noreply.github.com>
2026-09-20 14:49:00 -0400

    maintainers: remove noreply email from jeff-oomu

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13203,7 +13203,6 @@
     name = "Jeffrey David Johnson";
   };
   jeff-oomu = {
-    email = "195243343+jeff-oomu@users.noreply.github.com";
     github = "jeff-oomu";
     githubId = 195243343;
     name = "Jeffrey Allan";
