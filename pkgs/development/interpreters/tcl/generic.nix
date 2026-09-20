50694c503708f940f522da79a4d916b6d30226f2
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 17:12:30 -0300

    tcl: fix homepage

--- pkgs/development/interpreters/tcl/generic.nix
+++ pkgs/development/interpreters/tcl/generic.nix
@@ -193,7 +193,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Tcl scripting language";
-    homepage = "https://www.tcl.tk/";
+    homepage = "https://www.tcl-lang.org/";
     license = lib.licenses.tcltk;
     platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ agbrooks ];
