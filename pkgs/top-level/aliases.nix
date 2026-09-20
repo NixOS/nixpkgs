65fe6e3a030810326050742fd4cd6f56a86111d5
Corey O'Connor <coreyoconnor@gmail.com>
2026-09-20 04:03:26 +0000

    nordpass: remove

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1868,6 +1868,7 @@ mapAliases {
   nodePackages_latest = throw "nodePackages has been removed. Many packages are now available at the top level (e.g. `pkgs.package-name`). Check on https://search.nixos.org to see if the package is still available."; # Added 2026-03-03
   nofi = throw "'nofi' has been removed due to being archived upstream."; # Added 2026-01-16
   nomacs-qt6 = nomacs; # Added 2025-08-30
+  nordpass = throw "nordpass has been removed."; # Added 2026-09-19
   norouter = throw "norouter has been removed because it has been marked as broken since at least November 2024."; # Added 2025-09-29
   notary = throw "'notary' has been removed due to being archived upstream. Consider using 'notation' instead."; # Added 2025-11-13
   notepadqq = throw "'notepadqq' has been removed due to upstream stopping maintenance in 2023."; # Added 2026-02-10
