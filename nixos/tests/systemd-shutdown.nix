c3c2644b50a29473ca04c05c1234050bce56fb45
Will Fancher <elvishjerricco@gmail.com>
2026-09-20 01:20:52 -0400

    nixos/tests/systemd-shutdown: Test pre-exitrd shutdown scripts

--- nixos/tests/systemd-shutdown.nix
+++ nixos/tests/systemd-shutdown.nix
@@ -13,24 +13,32 @@ in
 
   nodes.machine = {
     imports = [ ../modules/profiles/minimal.nix ];
+    systemd.shutdown.pre-exitrd = pkgs.writeShellScript "pre-exitrd" ''
+      echo pre-exitrd > /run/initramfs/test.txt
+    '';
     systemd.shutdownRamfs.contents."/etc/systemd/system-shutdown/shutdown-message".source =
       pkgs.writeShellScript "shutdown-message" ''
-        echo "${msg}" > /dev/kmsg
+        if test -e /test.txt; then
+          # Test should only pass if both scripts run.
+          echo "${msg}" > /dev/kmsg
+        fi
       '';
     boot.initrd.systemd.enable = systemdStage1;
   };
 
   testScript = ''
+    import datetime as dt
+
     # Check that 'generate-shutdown-ramfs.service' is started
     # automatically and that 'systemd-shutdown' runs our script.
     machine.wait_for_unit("multi-user.target")
     # .shutdown() would wait for the machine to power off
     machine.execute("systemctl poweroff", check_return=False)
     # Message printed by systemd-shutdown
-    machine.wait_for_console_text("Unmounting '/oldroot'")
-    machine.wait_for_console_text("${msg}")
+    machine.wait_for_console_text("Unmounting '/oldroot'", timeout=dt.timedelta(seconds=60))
+    machine.wait_for_console_text("${msg}", timeout=dt.timedelta(seconds=5))
     # Don't try to sync filesystems
-    machine.wait_for_shutdown()
+    machine.wait_for_shutdown(timeout=dt.timedelta(seconds=5))
 
     # In a separate boot, start 'generate-shutdown-ramfs.service'
     # manually in order to check the permissions on '/run/initramfs'.
