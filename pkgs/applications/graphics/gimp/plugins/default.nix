faa58d284a28a03d11f5872af0595f9a0bd5c2a9
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 14:42:04 -0300

    gimpPlugins.gimplensfun: fix homepage

--- pkgs/applications/graphics/gimp/plugins/default.nix
+++ pkgs/applications/graphics/gimp/plugins/default.nix
@@ -359,7 +359,7 @@ lib.makeScope pkgs.newScope (
         broken = gimp.apiVersion != "2.0";
         description = "GIMP plugin to correct lens distortion using the lensfun library and database";
 
-        homepage = "http://lensfun.sebastiankraft.net/";
+        homepage = "https://seebk.github.io/GIMP-Lensfun/";
 
         license = lib.licenses.gpl3Plus;
         maintainers = [ ];
