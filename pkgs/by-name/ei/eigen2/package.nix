9a3024b75a5dc7b5c423feb884c6ef501056999b
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 13:59:23 -0300

    eigen2: fix homepage

--- pkgs/by-name/ei/eigen2/package.nix
+++ pkgs/by-name/ei/eigen2/package.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation (finalAttrs: {
   patches = [ ./cmake-4-build.patch ];
 
   meta = {
-    homepage = "https://eigen.tuxfamily.org";
+    homepage = "https://libeigen.gitlab.io/";
     description = "C++ template library for linear algebra: vectors, matrices, and related algorithms";
     license = lib.licenses.lgpl3Plus;
     maintainers = with lib.maintainers; [
