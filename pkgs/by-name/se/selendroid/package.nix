438a0463538b6363154e218c16e12b1d7e1b088f
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-19 23:31:14 -0300

    selendroid: fix homepage

--- pkgs/by-name/se/selendroid/package.nix
+++ pkgs/by-name/se/selendroid/package.nix
@@ -46,7 +46,7 @@ stdenv.mkDerivation {
   '';
 
   meta = {
-    homepage = "https://selendroid.io/";
+    homepage = "https://github.com/selendroid/selendroid";
     description = "Test automation for native or hybrid Android apps and the mobile web";
     maintainers = [ ];
     platforms = lib.platforms.all;
