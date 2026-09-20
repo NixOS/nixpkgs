965ba6c3bf5614db5620e7a31370cac6264adfac
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-09-20 21:52:57 +0800

    styluslabs-write-bin: use copyDesktopItems for desktop entry installation

--- pkgs/by-name/st/styluslabs-write-bin/package.nix
+++ pkgs/by-name/st/styluslabs-write-bin/package.nix
@@ -7,21 +7,9 @@
   libxi,
   fetchurl,
   makeDesktopItem,
+  copyDesktopItems,
 }:
-let
-  desktopItem = makeDesktopItem {
-    name = "Write";
-    exec = "Write";
-    comment = "A word processor for handwriting";
-    icon = "write_stylus";
-    desktopName = "Write";
-    genericName = "Write";
-    categories = [
-      "Office"
-      "Graphics"
-    ];
-  };
-in
+
 stdenv.mkDerivation rec {
   pname = "styluslabs-write-bin";
   version = "300";
@@ -35,18 +23,37 @@ stdenv.mkDerivation rec {
 
   dontBuild = true;
 
+  nativeBuildInputs = [ copyDesktopItems ];
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "Write";
+      exec = "Write";
+      comment = "A word processor for handwriting";
+      icon = "write_stylus";
+      desktopName = "Write";
+      genericName = "Write";
+      categories = [
+        "Office"
+        "Graphics"
+      ];
+    })
+  ];
+
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     cp -R Write $out/
     # symlink the binary to bin/
     ln -s $out/Write/Write $out/bin/Write
 
-    # Create desktop item
-    mkdir -p $out/share/applications
-    ln -s ${desktopItem}/share/applications/* $out/share/applications/
     mkdir -p $out/share/icons
     ln -s $out/Write/Write144x144.png $out/share/icons/write_stylus.png
+
+    runHook postInstall
   '';
+
   preFixup =
     let
       libPath = lib.makeLibraryPath [
