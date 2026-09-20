5c044b086eab53c2cdf476f66669051b82be924e
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 14:21:17 -0300

    gimpPlugins.fourier: fix homepage

--- pkgs/applications/graphics/gimp/plugins/default.nix
+++ pkgs/applications/graphics/gimp/plugins/default.nix
@@ -208,7 +208,7 @@ lib.makeScope pkgs.newScope (
       meta = {
         broken = gimp.apiVersion != "2.0";
         description = "GIMP plug-in to do the fourier transform";
-        homepage = "https://people.via.ecp.fr/~remi/soft/gimp/gimp_plugin_en.php3#fourier";
+        homepage = "https://www.lprp.fr/gimp_plugin_en/";
         license = lib.licenses.gpl3Plus;
       };
     };
