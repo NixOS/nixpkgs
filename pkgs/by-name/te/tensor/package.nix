66953516b1bfbe0f624b0e39ef42a12a8c6e4a2b
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-09-20 22:33:24 +0800

    tensor: use copyDesktopItems for desktop entry installation, use relative exec path

--- pkgs/by-name/te/tensor/package.nix
+++ pkgs/by-name/te/tensor/package.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   libsForQt5,
   makeDesktopItem,
+  copyDesktopItems,
 }:
 
 # we now have libqmatrixclient so a future version of tensor that supports it
@@ -24,6 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [
     libsForQt5.qmake
     libsForQt5.wrapQtAppsHook
+    copyDesktopItems
   ];
 
   buildInputs = [
@@ -31,19 +33,21 @@ stdenv.mkDerivation (finalAttrs: {
     libsForQt5.qtquickcontrols
   ];
 
-  desktopItem = makeDesktopItem {
-    name = "tensor";
-    exec = "@bin@";
-    icon = "tensor.png";
-    comment = finalAttrs.meta.description;
-    desktopName = "Tensor Matrix Client";
-    genericName = finalAttrs.meta.description;
-    categories = [
-      "Chat"
-      "Utility"
-    ];
-    mimeTypes = [ "application/x-chat" ];
-  };
+  desktopItems = [
+    (makeDesktopItem {
+      name = "tensor";
+      exec = "tensor";
+      icon = "tensor.png";
+      comment = finalAttrs.meta.description;
+      desktopName = "Tensor Matrix Client";
+      genericName = finalAttrs.meta.description;
+      categories = [
+        "Chat"
+        "Utility"
+      ];
+      mimeTypes = [ "application/x-chat" ];
+    })
+  ];
 
   installPhase =
     if stdenv.hostPlatform.isDarwin then
@@ -62,11 +66,6 @@ stdenv.mkDerivation (finalAttrs: {
         install -Dm755 tensor $out/bin/tensor
         install -Dm644 client/logo.png \
                        $out/share/icons/hicolor/512x512/apps/tensor.png
-        install -Dm644 ${finalAttrs.desktopItem}/share/applications/tensor.desktop \
-                       $out/share/applications/tensor.desktop
-
-        substituteInPlace $out/share/applications/tensor.desktop \
-          --subst-var-by bin $out/bin/tensor
 
         runHook postInstall
       '';
