7689c88968d00f891b538ad6d6b96ce3c73d5d42
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-20 11:58:27 +0000

    everest: 6531 -> 6580

--- pkgs/by-name/ev/everest/package.nix
+++ pkgs/by-name/ev/everest/package.nix
@@ -11,8 +11,8 @@
 
 let
   pname = "everest";
-  version = "6531";
-  rev = "d72e94f4b9e62b91cbdea674587ed39d53de9550";
+  version = "6580";
+  rev = "082e21b0b6dd7ff7c96d65b2ca2c632f4fd8df75";
   phome = "$out/lib/Celeste";
 in
 buildDotnetModule {
@@ -25,7 +25,7 @@ buildDotnetModule {
     fetchSubmodules = true;
     # TODO: use leaveDotGit = true and modify external/MonoMod in postFetch to please SourceLink
     # Microsoft.SourceLink.Common.targets(53,5): warning : Source control information is not available - the generated source link is empty.
-    hash = "sha256-F7FTCJqVqvqS+OPPK2K03ZUixqU5p55d3SURYMHXUpk=";
+    hash = "sha256-SGcrNjKiLlUik7dsGN9JkBd0TCtKUYRBp+q9ET0v3oo=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
