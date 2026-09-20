bf376dedd4d74d55af8a4d03b0a04e7cb8862b29
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 14:11:45 -0300

    fastd: fix homepage

--- pkgs/by-name/fa/fastd/package.nix
+++ pkgs/by-name/fa/fastd/package.nix
@@ -52,7 +52,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Fast and Secure Tunneling Daemon";
-    homepage = "https://projects.universe-factory.net/projects/fastd/wiki";
+    homepage = "https://fastd.readthedocs.io/";
     license = with lib.licenses; [
       bsd2
       bsd3
