8dea9561c03a6853a69482e9810815f584734819
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 00:00:16 -0300

    nvrs: fix homepage

--- pkgs/by-name/nv/nvrs/package.nix
+++ pkgs/by-name/nv/nvrs/package.nix
@@ -46,7 +46,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   meta = {
     description = "Fast new version checker for software releases";
-    homepage = "https://nvrs.adamperkowski.dev";
+    homepage = "https://nvrs.koi.rip/";
     changelog = "https://github.com/koibtw/nvrs/blob/v${finalAttrs.version}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ koi ];
