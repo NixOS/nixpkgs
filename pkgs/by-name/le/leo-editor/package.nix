a5b25e8bc856b5c4e1c7806bc2db7ba56236f1b1
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-09-20 21:27:08 +0800

    leo-editor: use copyDesktopItems for desktop entry installation

--- pkgs/by-name/le/leo-editor/package.nix
+++ pkgs/by-name/le/leo-editor/package.nix
@@ -5,6 +5,7 @@
   fetchFromGitHub,
   makeWrapper,
   makeDesktopItem,
+  copyDesktopItems,
   libsForQt5,
 }:
 
@@ -25,6 +26,7 @@ stdenv.mkDerivation (finalAttrs: {
     libsForQt5.wrapQtAppsHook
     makeWrapper
     python3
+    copyDesktopItems
   ];
 
   propagatedBuildInputs = with python3.pkgs; [
@@ -32,64 +34,63 @@ stdenv.mkDerivation (finalAttrs: {
     docutils
   ];
 
-  desktopItem = makeDesktopItem {
-    name = "leo-editor";
-    exec = "leo %U";
-    icon = "leoapp32";
-    type = "Application";
-    comment = finalAttrs.meta.description;
-    desktopName = "Leo";
-    genericName = "Text Editor";
-    categories = [
-      "Application"
-      "Development"
-      "IDE"
-    ];
-    startupNotify = false;
-    mimeTypes = [
-      "text/plain"
-      "text/asp"
-      "text/x-c"
-      "text/x-script.elisp"
-      "text/x-fortran"
-      "text/html"
-      "application/inf"
-      "text/x-java-source"
-      "application/x-javascript"
-      "application/javascript"
-      "text/ecmascript"
-      "application/x-ksh"
-      "text/x-script.ksh"
-      "application/x-tex"
-      "text/x-script.rexx"
-      "text/x-pascal"
-      "text/x-script.perl"
-      "application/postscript"
-      "text/x-script.scheme"
-      "text/x-script.guile"
-      "text/sgml"
-      "text/x-sgml"
-      "application/x-bsh"
-      "application/x-sh"
-      "application/x-shar"
-      "text/x-script.sh"
-      "application/x-tcl"
-      "text/x-script.tcl"
-      "application/x-texinfo"
-      "application/xml"
-      "text/xml"
-      "text/x-asm"
-    ];
-  };
+  desktopItems = [
+    (makeDesktopItem {
+      name = "leo-editor";
+      exec = "leo %U";
+      icon = "leoapp32";
+      type = "Application";
+      comment = finalAttrs.meta.description;
+      desktopName = "Leo";
+      genericName = "Text Editor";
+      categories = [
+        "Application"
+        "Development"
+        "IDE"
+      ];
+      startupNotify = false;
+      mimeTypes = [
+        "text/plain"
+        "text/asp"
+        "text/x-c"
+        "text/x-script.elisp"
+        "text/x-fortran"
+        "text/html"
+        "application/inf"
+        "text/x-java-source"
+        "application/x-javascript"
+        "application/javascript"
+        "text/ecmascript"
+        "application/x-ksh"
+        "text/x-script.ksh"
+        "application/x-tex"
+        "text/x-script.rexx"
+        "text/x-pascal"
+        "text/x-script.perl"
+        "application/postscript"
+        "text/x-script.scheme"
+        "text/x-script.guile"
+        "text/sgml"
+        "text/x-sgml"
+        "application/x-bsh"
+        "application/x-sh"
+        "application/x-shar"
+        "text/x-script.sh"
+        "application/x-tcl"
+        "text/x-script.tcl"
+        "application/x-texinfo"
+        "application/xml"
+        "text/xml"
+        "text/x-asm"
+      ];
+    })
+  ];
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p "$out/share/icons/hicolor/32x32/apps"
     cp leo/Icons/leoapp32.png "$out/share/icons/hicolor/32x32/apps"
-
-    mkdir -p "$out/share/applications"
-    cp $desktopItem/share/applications/* $out/share/applications
 
     mkdir -p $out/share/leo-editor
     mv * $out/share/leo-editor
