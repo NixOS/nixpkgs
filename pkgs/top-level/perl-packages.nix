2a7bc7a70744161e3cbfac466b852fee50065ad2
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 16:14:56 -0300

    perlPackages.TestRunCmdLine: fix homepage

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -35314,7 +35314,7 @@ with self;
     doCheck = !stdenv.hostPlatform.isDarwin;
     meta = {
       description = "Analyze tests from the command line using Test::Run";
-      homepage = "http://web-cpan.berlios.de/modules/Test-Run";
+      homepage = "https://metacpan.org/pod/Test::Run::CmdLine";
       license = lib.licenses.mit;
       mainProgram = "runprove";
     };
