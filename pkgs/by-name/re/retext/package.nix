6eb09bc8c734c840fb079bda16efcd7d37c484b2
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-19 23:48:29 -0300

    aspellDicts.en-computers: drop

--- pkgs/by-name/re/retext/package.nix
+++ pkgs/by-name/re/retext/package.nix
@@ -8,10 +8,7 @@
   aspellDicts,
   # Use `lib.collect lib.isDerivation aspellDicts;` to make all dictionaries
   # available.
-  enchantAspellDicts ? with aspellDicts; [
-    en
-    en-computers
-  ],
+  enchantAspellDicts ? with aspellDicts; [ en ],
 }:
 
 python3Packages.buildPythonApplication rec {
