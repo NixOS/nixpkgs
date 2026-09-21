22c9f2ee21d9b5d315e114f9fda9a2fcd2ffd6ba
Grayson Tinker <graysontinker@gmail.com>
2026-09-21 16:42:49 -0600

    go_1_{26,27}: mark as supported on Windows

--- pkgs/development/compilers/go/1.26.nix
+++ pkgs/development/compilers/go/1.26.nix
@@ -182,7 +182,11 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.bsd3;
     teams = [ lib.teams.golang ];
     platforms =
-      lib.platforms.darwin ++ lib.platforms.linux ++ lib.platforms.wasi ++ lib.platforms.freebsd;
+      lib.platforms.darwin
+      ++ lib.platforms.linux
+      ++ lib.platforms.wasi
+      ++ lib.platforms.freebsd
+      ++ lib.platforms.windows;
     badPlatforms = [
       # Support for big-endian POWER < 8 was dropped in 1.9, but POWER8 users have less of a reason to run in big-endian mode than pre-POWER8 ones
       # So non-LE ppc64 is effectively unsupported, and Go SIGILLs on affordable ppc64 hardware
