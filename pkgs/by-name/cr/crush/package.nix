4082402787435b73c70fb61c98b8b935e0ee745c
Dan Xin <davinci42.cn@gmail.com>
2026-09-21 12:27:50 +0800

    crush: 0.92.0 -> 0.95.0
    Diff: https://github.com/charmbracelet/crush/compare/v0.92.0...v0.95.0
    
    Changelog: https://github.com/charmbracelet/crush/releases/tag/v0.95.0

--- pkgs/by-name/cr/crush/package.nix
+++ pkgs/by-name/cr/crush/package.nix
@@ -11,16 +11,16 @@
 
 buildGo127Module (finalAttrs: {
   pname = "crush";
-  version = "0.92.0";
+  version = "0.95.0";
 
   src = fetchFromGitHub {
     owner = "charmbracelet";
     repo = "crush";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-qAwbvPV1yxqQu7H6YyFYCth09Kg/c0akCTpQTfOXla8=";
+    hash = "sha256-4JbJOP5hIc1bpga84bium7QEzFJj9jjjojfpG/hbjCg=";
   };
 
-  vendorHash = "sha256-iq3EDdEwZEPzHrBqy0k8QLcLSCLGlezKxICWgo7vMqg=";
+  vendorHash = "sha256-HktSB71scE55BG3TzJyDbAV1j+T8+DDxMTAiomeLbgc=";
 
   ldflags = [
     "-s"
