5503afa4f2052655704680dea13048e758317bdc
Gaetan Lepage <gaetan@glepage.com>
2026-09-21 11:24:09 +0000

    nix-fast-build: 2.0.2 -> 2.0.3
    Diff: https://github.com/Mic92/nix-fast-build/compare/2.0.2...2.0.3
    
    Changelog: https://github.com/Mic92/nix-fast-build/releases/tag/2.0.3

--- pkgs/by-name/ni/nix-fast-build/package.nix
+++ pkgs/by-name/ni/nix-fast-build/package.nix
@@ -9,7 +9,7 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "nix-fast-build";
-  version = "2.0.2";
+  version = "2.0.3";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -17,7 +17,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
     owner = "Mic92";
     repo = "nix-fast-build";
     tag = finalAttrs.version;
-    hash = "sha256-bD5s8NbVSQevMEA7ewS4S5wkxnfat/QV0WRjGK1Kx6o=";
+    hash = "sha256-L4HfADUq4Imq1LnvmjBPFBEAZAIKD9Pnj6ExRkVqHC4=";
   };
 
   build-system = [ python3Packages.setuptools ];
