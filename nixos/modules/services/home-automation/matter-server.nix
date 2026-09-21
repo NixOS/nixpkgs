bf100780fb44b36c4816e642380dd5bf12cfc3c6
Matt Leon <ml@mattleon.com>
2026-09-20 20:12:25 -0400

    matter-server: migrate to matterjs-server compatibility layer
    Signed-off-by: Matt Leon <ml@mattleon.com>

--- nixos/modules/services/home-automation/matter-server.nix
+++ nixos/modules/services/home-automation/matter-server.nix
@@ -1,28 +1,38 @@
 {
   lib,
-  pkgs,
   config,
   ...
 }:
 let
   cfg = config.services.matter-server;
   storageDir = "matter-server";
-  storagePath = "/var/lib/${storageDir}";
-  vendorId = "4939"; # home-assistant vendor ID
 in
 
 {
+  imports = [
+    (lib.mkRemovedOptionModule [ "services" "matter-server" "package" ]
+      "This module now configures matterjs-server with matter-server compatible options. Please configure services.matterjs-server.package instead."
+    )
+    (lib.mkRemovedOptionModule [ "services" "matter-server" "extraArgs" ]
+      "This module now configures matterjs-server with matter-server compatible options. Please configure services.matterjs-server.extraArgs instead."
+    )
+  ];
+
   meta.maintainers = with lib.maintainers; [ leonm1 ];
 
   options.services.matter-server = {
-    enable = lib.mkEnableOption "Matter-server";
-
-    package = lib.mkPackageOption pkgs "python-matter-server" { };
+    enable = lib.mkEnableOption "matterjs-server (matter-server compatibility module)";
 
     port = lib.mkOption {
       type = lib.types.port;
       default = 5580;
       description = "Port to expose the matter-server service on.";
+    };
+
+    vendorId = lib.mkOption {
+      type = lib.types.int;
+      default = 4939; # home-assistant vendor ID
+      description = "Vendor ID to expose to the matter network.";
     };
 
     openFirewall = lib.mkOption {
@@ -42,121 +52,23 @@ in
       default = "info";
       description = "Verbosity of logs from the matter-server";
     };
-
-    extraArgs = lib.mkOption {
-      type = lib.types.attrs;
-      default = { };
-      description = ''
-        Attribute set of extra arguments to pass to the matter-server executable.
-        See <https://github.com/home-assistant-libs/python-matter-server?tab=readme-ov-file#running-the-development-server> for options.
-      '';
-    };
   };
 
   config = lib.mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.port ];
+    services.matterjs-server = {
+      enable = cfg.enable;
+      port = cfg.port;
+      openFirewall = cfg.openFirewall;
 
-    systemd.services.matter-server = {
-      after = [ "network-online.target" ];
-      before = [ "home-assistant.service" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-      description = "Matter Server";
-      environment = {
-        HOME = storagePath;
-        SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-      };
-      script = ''
-        # `python-matter-server` writes to /data even when a storage-path is
-        # specified. This symlinks /data at the systemd-managed
-        # /var/lib/matter-server, so all files get dropped into the state
-        # directory.
-        ln -s "$STATE_DIRECTORY" "$RUNTIME_DIRECTORY/data"
-
-        # Create directories to hold certificates and OTA updates.
-        CERT_DIR="$CACHE_DIRECTORY/certs"
-        mkdir -p "$CERT_DIR"
-        OTA_UPDATE_DIR="$CACHE_DIRECTORY/updates"
-        mkdir -p "$OTA_UPDATE_DIR"
-
-        "${lib.getExe cfg.package}" ${
-          lib.concatStringsSep " " (
-            lib.cli.toCommandLineGNU { } (
-              {
-                port = cfg.port;
-                vendorid = vendorId;
-                storage-path = storagePath;
-                log-level = cfg.logLevel;
-                paa-root-cert-dir = ''"$CERT_DIR"'';
-                ota-provider-dir = ''"$OTA_UPDATE_DIR"'';
-              }
-              // cfg.extraArgs
-            )
-          )
-        }
-      '';
-      serviceConfig = {
-        # Start with a clean root filesystem, and allowlist what the container
-        # is permitted to access.
-        # See https://discourse.nixos.org/t/hardening-systemd-services/17147/14.
-        RuntimeDirectory = [ "matter-server/root" ];
-        RootDirectory = "%t/matter-server/root";
-        CacheDirectory = [ "matter-server" ];
-
-        BindReadOnlyPaths = [
-          "/nix/store" # To allow the binary to find its dependencies.
-          "/run/dbus"
-          "/etc/resolv.conf" # For DNS resolution.
-        ];
-        # Let systemd manage `/var/lib/matter-server` for us inside the
-        # ephemeral TemporaryFileSystem.
-        StateDirectory = storageDir;
-
-        # Hardening bits
-        AmbientCapabilities = "";
-        CapabilityBoundingSet = "";
-        DevicePolicy = "closed";
-        DynamicUser = true;
-        LockPersonality = true;
-        MemoryDenyWriteExecute = true;
-        NoNewPrivileges = true;
-        PrivateDevices = true;
-        PrivateTmp = true;
-        PrivateUsers = true;
-        ProcSubset = "pid";
-        ProtectClock = true;
-        ProtectControlGroups = true;
-        ProtectHome = true;
-        ProtectHostname = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        ProtectProc = "invisible";
-        RestrictAddressFamilies = [
-          "AF_INET"
-          "AF_INET6"
-          "AF_NETLINK"
-          "AF_UNIX"
-        ];
-        RestrictNamespaces = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-        SystemCallFilter = lib.concatStringsSep " " [
-          "~" # Blocklist
-          "@clock"
-          "@cpu-emulation"
-          "@debug"
-          "@module"
-          "@mount"
-          "@obsolete"
-          "@privileged"
-          "@raw-io"
-          "@reboot"
-          "@resources"
-          "@swap"
-        ];
-        UMask = "0077";
-      };
+      # Patch the matterjs-server service to use existing users' config, which
+      # matterjs-server will migrate automatically.
+      stateDirectoryName = storageDir;
+      extraArgs = [
+        "--vendorid=${toString cfg.vendorId}"
+        "--log-level=${cfg.logLevel}"
+      ];
     };
+
+    systemd.services.matterjs-server.aliases = [ "matter-server.service" ];
   };
 }
