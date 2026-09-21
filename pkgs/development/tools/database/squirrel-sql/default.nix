5fade19889e4e452119cbb8b68e633b384207f94
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-09-21 11:36:22 +0800

    squirrel-sql: use copyDesktopItems for desktop entry installation

--- pkgs/development/tools/database/squirrel-sql/default.nix
+++ pkgs/development/tools/database/squirrel-sql/default.nix
@@ -5,6 +5,7 @@
   stdenv,
   fetchurl,
   makeDesktopItem,
+  copyDesktopItems,
   makeWrapper,
   unzip,
   jre,
@@ -22,6 +23,7 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [
     makeWrapper
     unzip
+    copyDesktopItems
   ];
   buildInputs = [ jre ];
 
@@ -61,20 +63,21 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/icons/hicolor/32x32/apps
     ln -s $out/share/squirrel-sql/icons/acorn.png \
       $out/share/icons/hicolor/32x32/apps/squirrel-sql.png
-    ln -s ${desktopItem}/share/applications $out/share
 
     runHook postInstall
   '';
 
-  desktopItem = makeDesktopItem {
-    name = "squirrel-sql";
-    exec = "squirrel-sql";
-    comment = meta.description;
-    desktopName = "SQuirreL SQL";
-    genericName = "SQL Client";
-    categories = [ "Development" ];
-    icon = "squirrel-sql";
-  };
+  desktopItems = [
+    (makeDesktopItem {
+      name = "squirrel-sql";
+      exec = "squirrel-sql";
+      comment = meta.description;
+      desktopName = "SQuirreL SQL";
+      genericName = "SQL Client";
+      categories = [ "Development" ];
+      icon = "squirrel-sql";
+    })
+  ];
 
   meta = {
     description = "Universal SQL Client";
