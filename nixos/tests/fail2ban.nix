b1c1f2b3c3661b6a48416b061cf585dfca2ab741
Eric Wolf <robo-eric@gmx.de>
2026-09-22 08:28:45 +0200

    nixos/tests/fail2ban: add subtest verifying the generated configuration

--- nixos/tests/fail2ban.nix
+++ nixos/tests/fail2ban.nix
@@ -6,6 +6,44 @@
     services.fail2ban = {
       enable = true;
       bantime-increment.enable = true;
+      jails.inline-filter-override = {
+        filter.Definition.failregex = "inline filter test: <HOST>";
+        settings = {
+          destemail = "example@localhost";
+          mta = "sendmail";
+          action_mwl = ''
+            %(action_)s
+              %(mta)s-whois-matches[sender="%(sender)s", dest="%(destemail)s", chain="%(chain)s"]
+          '';
+          action = "%(action_mwl)s";
+        };
+      };
+      jails.test-jail = {
+        filter = "nextcloud";
+        settings.action = "test-action";
+      };
+      actions.test-action.Definition = {
+        actionban = "echo 'Test action ban'";
+        actionunban = "echo 'Test action unban'";
+      };
+      # test examples
+      actions.sendmail-common.Definition = {
+        actionstart = "";
+        actionstop = "";
+      };
+      filters.nextcloud = {
+        INCLUDES.before = "common.conf";
+        Definition = {
+          _groupsre = ''(?:(?:,?\s*"\w+":(?:"[^"]+"|\w+))*)'';
+          failregex = ''
+            ^%(__prefix_line)s\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Login failed:
+              ^%(__prefix_line)s\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Two-factor challenge failed:
+              ^%(__prefix_line)s\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Trusted domain error.
+          '';
+          datepattern = '',?\s*"time"\s*:\s*"%%Y-%%m-%%d[T ]%%H:%%M:%%S(%%z)?"'';
+          journalmatch = "_SYSTEMD_UNIT=phpfpm-nextcloud.service";
+        };
+      };
     };
     services.openssh.enable = true;
     networking.nftables.enable = true;
@@ -35,6 +73,20 @@
     def fail2ban_running():
       machine.require_unit_state("fail2ban.service", "active")
 
+    def assertFailRegex(jail, *regex):
+      output = machine.succeed(f"fail2ban-client get {jail} failregex")
+      regex = "^The following regular expression are defined:\n"
+      for i in range(0, len(regex) - 1):
+        regex += f"|- \\[{i}\\]: {regex[i]}\n"
+      regex += f"`- \\[{len(regex) - 1}\\]: {regex[-1]}\n$"
+      t.assertRegex(output, regex)
+
+    def assertAction(jail, action, attr, expected):
+      output = machine.succeed(
+        f"fail2ban-client get {jail} action {action} {attr}"
+      ).rstrip()
+      t.assertEqual(output, expected)
+
     with fail2ban_running:
       with subtest("Verify reported versions"):
         clientVersion = machine.succeed("fail2ban-client -V").rstrip()
@@ -46,6 +98,15 @@
         machine.succeed("fail2ban-client ping")
         # Verify that the socket is reachable via the fail2ban group
         machine.succeed("sudo -u nobody -g fail2ban fail2ban-client ping")
+
+      with subtest("Verify the generated configuration"):
+        machine.succeed("fail2ban-client --test")
+        assertAction("test-jail", "test-action", "actionban", "echo 'Test action ban'")
+        assertAction("test-jail", "test-action", "actionunban", "echo 'Test action unban'")
+        assertAction("inline-filter-override", "sendmail-whois-matches", "actionstart", "")
+        assertAction("inline-filter-override", "sendmail-whois-matches", "actionstop", "")
+        assertFailRegex("test-jail", "[^\n]+", "[^\n]+", "[^\n]+")
+        assertFailRegex("inline-filter-override", "inline filter test: [^\n]+")
 
       with subtest("Verify there is not ban and the port is reachable from the client"):
         machine.succeed(f"test 0 -eq $(fail2ban-client get sshd banned {client_addr})")
