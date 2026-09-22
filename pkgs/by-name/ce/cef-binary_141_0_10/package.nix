9806a66c3bbfcde6b7bbc46240475a003c761ed3
Gavin John <gavinnjohn@gmail.com>
2026-09-22 17:13:00 -0400

    cef-binary_141_0_10: init at 141.0.10

---
+++ pkgs/by-name/ce/cef-binary_141_0_10/package.nix
@@ -0,0 +1,13 @@
+{
+  cef-binary,
+}:
+
+cef-binary.override {
+  version = "141.0.10";
+  gitRevision = "1d65b0d";
+  chromiumVersion = "141.0.7390.123";
+  srcHashes = {
+    aarch64-linux = "sha256-CJGCJRIMV8UBummf6uHe8NV0Y+HWRZTKZuNB08xD30Y=";
+    x86_64-linux = "sha256-17TxfH/kbrab/2huZoh+lXVPDU+Lm6AFzFFJhnDyKN8=";
+  };
+}
