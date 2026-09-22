7e5b777de75903feaf04b1d764f28df3a9a4f35c
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-22 09:45:20 +0000

    officecli: 1.0.146 -> 1.0.152

--- pkgs/by-name/of/officecli/package.nix
+++ pkgs/by-name/of/officecli/package.nix
@@ -8,7 +8,7 @@
 
 buildDotnetModule (finalAttrs: {
   pname = "officecli";
-  version = "1.0.146";
+  version = "1.0.152";
 
   strictDeps = true;
   __structuredAttrs = true;
@@ -17,7 +17,7 @@ buildDotnetModule (finalAttrs: {
     owner = "iOfficeAI";
     repo = "OfficeCLI";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-YS4aF2kDn3uJxFGsY4z/8pqm+UK9GYXYiCIRjFuBhBc=";
+    hash = "sha256-p+jwcgtRQJKSVWPxBO8F92xIAlLlDZxZ4fLwzJiZwZU=";
   };
 
   projectFile = "src/officecli/officecli.csproj";
