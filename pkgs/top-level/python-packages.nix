72de027eb03274dac9829f4710afc31d29fa7d99
Guillaume Fournier <me@guif.dev>
2026-09-22 13:08:00 +0200

    nixoscope: 0.1.0 -> 0.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10695,6 +10695,8 @@ self: super: with self; {
 
   merkletools = callPackage ../development/python-modules/merkletools { };
 
+  mermaid-py = callPackage ../development/python-modules/mermaid-py { };
+
   meross-iot = callPackage ../development/python-modules/meross-iot { };
 
   meshcat = callPackage ../development/python-modules/meshcat { };
