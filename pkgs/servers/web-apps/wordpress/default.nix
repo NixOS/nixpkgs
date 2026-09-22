7ad3ada80d4b569b6f106d326cc28f9dd5b107e6
Bart Oostveen <bart@bartoostveen.nl>
2026-09-22 16:46:56 +0200

    wordpress_6_9: 6.9.8 -> 6.9.9
    Announcement: https://wordpress.org/news/2026/09/wordpress-7-1-2-release/
    Release notes: https://wordpress.org/documentation/wordpress-version/version-7-1-2/
    Fixes: GHSA-7hp8-65ch-5whp / CVE-2026-87902

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -2,8 +2,8 @@
 builtins.mapAttrs (_: callPackage ./generic.nix) rec {
   wordpress = wordpress_7_1;
   wordpress_6_9 = {
-    version = "6.9.8";
-    hash = "sha256-QSJH6Jfg13yGmL5rKF+CMwRnhhDvulg6sWpBnUnVWvU=";
+    version = "6.9.9";
+    hash = "sha256-MiMbmsvnY6hhOOi7t7XE8/EjvsnWX8OJi8s7ZFBqc1g=";
   };
   wordpress_7_0 = {
     version = "7.0.5";
