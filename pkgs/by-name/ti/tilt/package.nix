b54d71d88f0bbc78caecdc29d99cac05fc4c6a4a
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-09-22 12:08:58 +0200

    tilt: 0.37.6 -> 0.37.7

--- pkgs/by-name/ti/tilt/package.nix
+++ pkgs/by-name/ti/tilt/package.nix
@@ -9,13 +9,13 @@ let
       running in development environment and try to serve assets from the
       source tree, which is not there once build completes.
     */
-    version = "0.37.6";
+    version = "0.37.7";
 
     src = fetchFromGitHub {
       owner = "tilt-dev";
       repo = "tilt";
       tag = "v${version}";
-      hash = "sha256-tMP3EYgwulyqmuL3yE3CtCBIs2QhoWzW8PnYF+//bgs=";
+      hash = "sha256-VoPTJDbg17xd20Ja8MT53H5+fuWJg5dJum8T3vt9ubY=";
     };
   };
 
