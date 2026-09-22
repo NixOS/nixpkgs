63185e59e36b89c3a712bedd6cf09ff2c82b040e
kinnrai <me@kinnrai.com>
2026-09-23 04:56:55 +0800

    python3Packages.mlx-{bin,metal}: 0.31.2 -> 0.32.0

--- pkgs/development/python-modules/mlx/wheel-sources.nix
+++ pkgs/development/python-modules/mlx/wheel-sources.nix
@@ -1,34 +1,34 @@
 {
-  version = "0.31.2";
+  version = "0.32.0";
   # Hash of the upstream source used for examples and benchmarks matching the wheel version.
-  testSourceHash = "sha256-0Oxacz61WGWZrpWw+fMQjEQfwOx1l1L2d0kWl54/LrQ=";
+  testSourceHash = "sha256-yHpTyRf9FOPbdyDWSM7b6VC72STnUpgCMLbDxLbdaqs=";
 
   mlx = {
     "3.13-aarch64-darwin" = {
       platform = "macosx_14_0_arm64";
       dist = "cp313";
-      hash = "sha256-Gz+w3alVsNVSzle91vQrMwmrIbBn5AWH1oSEQ9MH6R8=";
+      hash = "sha256-q7eG7h6WOHWb6CWDIi/H0JxWUO+QrSt8XafRkxqGdtw=";
     };
     "3.14-aarch64-darwin" = {
       platform = "macosx_14_0_arm64";
       dist = "cp314";
-      hash = "sha256-oTyc4jw97vaqWgkxXnlT4aXcMR6FH6Fvx0yB+yUJwLk=";
+      hash = "sha256-LuebH4wsKjKa/JXs59zgvnmNQ/PedxpjcNK5+XArvZo=";
     };
   };
 
   mlx-metal = {
     "14" = {
       platform = "macosx_14_0_arm64";
-      hash = "sha256-slOFvO4Y/BlAkiVbi1O5o9hInrZQ5ZFg8bV6rdB6otw=";
+      hash = "sha256-W2SyCsJLDEAfSJ3gHoIJ7cTTchJSAfGTFObznjhTIqo=";
     };
     "15" = {
       platform = "macosx_15_0_arm64";
-      hash = "sha256-6dTl/ObKEKh6DjiFl/mVGa1ZTQnmdHCLUxK9i9T1mX0=";
+      hash = "sha256-G9lKHOWwOgyJh3Gj51nwEkMAxqtRVRJ5BqHVCx8/zxk=";
     };
     # Includes NAX kernels for supported GPUs running macOS 26.2 or newer.
     "26" = {
       platform = "macosx_26_0_arm64";
-      hash = "sha256-hP+2DuUD8D62hPX7Fo1c/zHioWt/J8FzHq92Yr1um0Y=";
+      hash = "sha256-OvdqSY2EgE9mEZgASZ+dFD19/7CHig3Q18KEblhWX9c=";
     };
   };
 }
