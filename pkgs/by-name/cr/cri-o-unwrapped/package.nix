16fa945ddd9e3952db8431534cd93db1e06f0143
Sascha Grunert <sgrunert@redhat.com>
2026-09-22 08:36:00 +0200

    cri-o-unwrapped: 1.36.0 -> 1.36.6
    Fixes CVE-2026-92574 (checkpoint restore bypasses destination security
    context) and CVE-2026-15801 (insufficient validation during checkpoint
    restore).
    
    Not-cherry-picked-because: master tracks 1.37.x, this bumps the 1.36.x series
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- pkgs/by-name/cr/cri-o-unwrapped/package.nix
+++ pkgs/by-name/cr/cri-o-unwrapped/package.nix
@@ -17,13 +17,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "cri-o";
-  version = "1.36.0";
+  version = "1.36.6";
 
   src = fetchFromGitHub {
     owner = "cri-o";
     repo = "cri-o";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-I+oqb8TeI7dBX4ecmEEWNKM7EAdCfGWYv6dpz8emvuA=";
+    hash = "sha256-4Ven4XJilmSig+757hb9+arOSsJerQgNiTlfdLAiT3A=";
   };
   vendorHash = null;
 
