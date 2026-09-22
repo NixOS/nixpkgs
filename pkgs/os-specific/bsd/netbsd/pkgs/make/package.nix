818a5281d5399eae0e12a0b952729da31c72ae38
Richard Tichý <richard@tichy.io>
2026-09-22 02:45:26 +0200

    netbsd.make: fix broken build for linux
    Both latest aarch64 and x86_64 Linux builds failed:
    aarch64 - https://hydra.nixos.org/build/345307975
    x86_64 - https://hydra.nixos.org/build/345307974
    
    Signed-off-by: Richard Tichý <richard@tichy.io>

--- pkgs/os-specific/bsd/netbsd/pkgs/make/package.nix
+++ pkgs/os-specific/bsd/netbsd/pkgs/make/package.nix
@@ -3,9 +3,14 @@
 mkDerivation {
   path = "usr.bin/make";
 
+  patches = [
+    ./0001-Dont-use-sysctl-on-Linux.patch
+  ];
+
   postPatch = make-rules.postPatch + ''
     # make needs this to pick up our sys make files
     appendToVar NIX_CFLAGS_COMPILE "-D_PATH_DEFSYSPATH=\"$out/share/mk\""
+    appendToVar NIX_CFLAGS_COMPILE "-DHAVE_STRSEP"
   '';
 
   postInstall = ''
