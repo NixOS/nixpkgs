0dda12db378138344a5cd27e5c801873df60a81f
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-22 17:14:34 +0000

    linux_xanmod: 6.18.52 -> 6.18.53
    - Changelog: https://dl.xanmod.org/changelog/6.18/ChangeLog-6.18.53-xanmod1.gz
    - Diff: https://gitlab.com/xanmod/linux/-/compare/6.18.52-xanmod1..6.18.53-xanmod1?from_project_id=51590166
    
    (cherry picked from commit a5a49a32de71a9abe0bc85574d87ebe08c39a5db)

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -15,8 +15,8 @@ let
   variants = {
     # ./update-xanmod.sh lts
     lts = {
-      version = "6.18.52";
-      hash = "sha256-VbMijsO4Et7wYVXi2kBYt4ilnO87wrzRkje1BTv4c/U=";
+      version = "6.18.53";
+      hash = "sha256-+2DnSpnBekGDBeXMjwGNrBFexnYmqQCc93LjDaseD/8=";
       isLTS = true;
     };
     # ./update-xanmod.sh main
