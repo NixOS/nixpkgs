6761485483510338673a1af3feea4c5c34b40586
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-22 21:31:48 +0000

    wasmtime: 48.0.1 -> 49.0.0

--- pkgs/by-name/wa/wasmtime/package.nix
+++ pkgs/by-name/wa/wasmtime/package.nix
@@ -22,9 +22,9 @@ let
       cargoHash = "sha256-68EM8hDN5dFxcBnEkqDt0sX86BYYiUZXfRaXut4WYpI=";
     };
     main = {
-      version = "48.0.1";
-      hash = "sha256-nvDiKIyo0/BmwjPTfNGIX1kSVVJQbmjjT4qed0dr8cc=";
-      cargoHash = "sha256-NVea5PiuFTr1NrIcyEJSq0gFf2XkDZj/VKubHwO34U4=";
+      version = "49.0.0";
+      hash = "sha256-Brhv9qwNKtYTQpH4BnU32XDA5K3l/DZhby7lPZg8GK8=";
+      cargoHash = "sha256-bHJeuFoxmaFx92s86pkhL/gebSuL76BNr5izTg/isiU=";
     };
   };
   source = sources.${variant};
--- pkgs/by-name/wa/wasmtime/python.nix
+++ pkgs/by-name/wa/wasmtime/python.nix
@@ -18,14 +18,14 @@ let
 in
 buildPythonPackage (finalAttrs: {
   pname = "wasmtime";
-  version = "48.0.0";
+  version = "49.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = "wasmtime-py";
     tag = finalAttrs.version;
-    hash = "sha256-OpA/LCCYAYNULiIqcXY070wjO5vNQ/ek35wDwgjoKj0=";
+    hash = "sha256-OV5lT4k64P7ldbMH1SbfgU0ANnZDvXdw7FBe1NX5I2I=";
   };
 
   postPatch = ''
