0e3144ff3941701c1fee334bb9985cd4c7ad5179
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-23 07:03:42 +0000

    python3Packages.comfyui-workflow-templates-json: 0.1.72 -> 0.1.94

--- pkgs/development/python-modules/comfyui-workflow-templates-json/default.nix
+++ pkgs/development/python-modules/comfyui-workflow-templates-json/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "comfyui-workflow-templates-json";
-  version = "0.1.72";
+  version = "0.1.94";
   pyproject = true;
 
   src = fetchPypi {
     pname = "comfyui_workflow_templates_json";
     inherit (finalAttrs) version;
-    hash = "sha256-m35dJtD1KYDmGkhLG5I9/dXKd2Pq9U6pJLn655SmpiA=";
+    hash = "sha256-+zTz/vyGnOIuPOb8UihWr403fYCX1E9GeT8DGVQ2BEk=";
   };
 
   build-system = [ setuptools ];
