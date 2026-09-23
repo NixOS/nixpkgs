5243bc76c45a766eee7be33c3d72096b45148a1e
sh0uv1 <mirantuten@yandex.com>
2026-09-23 20:56:47 +0000

    zennotes-desktop: 2.45.0 -> 2.55.0, change app state to managed
    Update to newer version. Change app state from dev to managed.

--- pkgs/by-name/ze/zennotes-desktop/package.nix
+++ pkgs/by-name/ze/zennotes-desktop/package.nix
@@ -13,14 +13,14 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "zennotes-desktop";
-  version = "2.45.0";
-  npmDepsHash = "sha256-+5eIwbSTpRupYj4gR33MDeJEEDBlt+K4idnlZFWG4Z4=";
+  version = "2.55.0";
+  npmDepsHash = "sha256-G3e4cDdxyZVy6B9s48dTIpzv9IPgEvzoqO0vuDIdQiM=";
 
   src = fetchFromGitHub {
     owner = "ZenNotes";
     repo = "zennotes";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-/HoSAgt7INT5Hharc+ltb2ya8InAuZmhS8Tmftl0VOo=";
+    hash = "sha256-HqjxrdJSV8uBGA7IisPguSTvjbZM5/X/jdJQ+av5JXc=";
   };
 
   npmWorkspace = "apps/desktop";
@@ -43,9 +43,10 @@ buildNpmPackage (finalAttrs: {
   configurePhase = ''
     runHook preConfigure
 
-    # Allow getting information about latest releases
+    # Allow getting information about latest releases, change app state from development to managed
     substituteInPlace apps/desktop/src/main/updater.ts \
-    --replace-fail "let managedInstall = false" "let managedInstall = true"
+    --replace-fail "let managedInstall = false" "let managedInstall = true" \
+    --replace-fail "isPackaged: app.isPackaged," "isPackaged: true,"
 
     runHook postConfigure
   '';
