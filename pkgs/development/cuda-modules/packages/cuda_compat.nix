0230cc1baac1e9b3c2f729202c1875f318787125
Pratham Patel <prathampatel@thefossguy.com>
2026-09-24 00:21:31 +0530

    cudaPackages/cuda_compat: ignore `libcrypto` shared objects
    They aren't really necessary for the functionality that we need
    `cuda_compat` for.

--- pkgs/development/cuda-modules/packages/cuda_compat.nix
+++ pkgs/development/cuda-modules/packages/cuda_compat.nix
@@ -11,6 +11,9 @@ buildRedist {
     "libnvdla_runtime.so"
     "libnvrm_gpu.so"
     "libnvrm_mem.so"
+    # Not necessary for the core function
+    "libcrypto.so.1.1"
+    "libcrypto.so.3"
   ];
 
   meta = {
