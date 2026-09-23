a016eca301df62c132ed22ea98aafe119d2fde70
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-23 22:30:49 +0000

    recode: 3.7.15 -> 3.7.16

--- pkgs/by-name/re/recode/package.nix
+++ pkgs/by-name/re/recode/package.nix
@@ -12,12 +12,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "recode";
-  version = "3.7.15";
+  version = "3.7.16";
 
   # Use official tarball, avoid need to bootstrap/generate build system
   src = fetchurl {
     url = "https://github.com/rrthomas/recode/releases/download/v${finalAttrs.version}/recode-${finalAttrs.version}.tar.gz";
-    hash = "sha256-9ZBAf8UbrbNRlz/BMz7jMRHwXsg6j5VP2M8MXjBDmAY=";
+    hash = "sha256-w9QH9U90uudjYDEgluLtRmIvAchuULCe9Fstk8j8/y0=";
   };
 
   nativeBuildInputs = [
