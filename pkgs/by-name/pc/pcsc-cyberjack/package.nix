8d21bdb6b8b0f5b8c1ff308797b5e96ca8dbe55d
Florian Klink <flokli@flokli.de>
2026-09-23 10:39:18 +0300

    pcsc-cyberjack: add udev rules to account for non-root pcscd
    Closes #565927
    Closes #538378

--- pkgs/by-name/pc/pcsc-cyberjack/package.nix
+++ pkgs/by-name/pc/pcsc-cyberjack/package.nix
@@ -13,6 +13,13 @@ let
   suffix = "SP17";
   tarBall = "${version}final.${suffix}";
 
+  # upstream doesn't ship this udev file, but it's necessary for pcscd to be able to open the device,
+  # as we don't run pcscd as root anymore.
+  udevFile = fetchurl {
+    url = "https://src.fedoraproject.org/rpms/pcsc-cyberjack/raw/a32e58b6e3d124a1fc28648b01a0d62013d96aa1/f/libifd-cyberjack6.udev";
+    hash = "sha256-JnPc8xqCcbbriEeCjFCL/nQsXLg47NIuniba7vVtj2Y=";
+  };
+
 in
 stdenv.mkDerivation rec {
   pname = "pcsc-cyberjack";
@@ -47,7 +54,10 @@ stdenv.mkDerivation rec {
     "--bindir=${placeholder "tools"}/bin"
   ];
 
-  postInstall = "make -C tools/cjflash install";
+  postInstall = ''
+    make -C tools/cjflash install
+    install -Dm644 ${udevFile} $out/lib/udev/rules.d/93-cyberjack.rules
+  '';
 
   meta = {
     description = "REINER SCT cyberJack USB chipcard reader user space driver";
