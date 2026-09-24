5fb25c66e37ace50defb8fd7668735a259c8c9f4
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-24 11:30:14 +0000

    bitrise: 2.45.0 -> 3.0.0

--- pkgs/by-name/bi/bitrise/package.nix
+++ pkgs/by-name/bi/bitrise/package.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "bitrise";
-  version = "2.45.0";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "bitrise-io";
     repo = "bitrise";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-WqW0ryP29kmKtSk88bbZ2E+VwesJbqXwB3k+PM6iLb8=";
+    hash = "sha256-ANORdYNZ/7aV4ZRIO/2iNo9DoSyRqJEJmD6iEwxMbmw=";
   };
 
   # many tests rely on writable $HOME/.bitrise and require network access
