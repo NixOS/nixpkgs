164a5641d54d33588fde376f19cfda64fc841e8d
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-24 07:42:14 +0000

    tdarr-node: 2.87.01 -> 2.90.01

--- pkgs/tools/misc/tdarr/node.nix
+++ pkgs/tools/misc/tdarr/node.nix
@@ -5,10 +5,10 @@ callPackage ./common.nix { } {
   component = "node";
 
   hashes = {
-    linux_x64 = "sha256-F7P0DZ4J10cgwbME1qcWYnJcMlcsALJ+z6u/eVTv5g0=";
-    linux_arm64 = "sha256-oKapd+SjpercZecXCjojbsjHem5oWxctycj/woD0Bwc=";
-    darwin_x64 = "sha256-RmRngSwtU5ZMlE8yPy+Am6DzMmuiFihbeIk1yn/85Gc=";
-    darwin_arm64 = "sha256-muqjEYYTDckM8FesRrE8rAxPOIzMyLcnXOSGcfKfw5Y=";
+    linux_x64 = "sha256-ITL0Hwvg2+AqWA9tup0vPU6/2hMNFdLaWKKBfWPyWuU=";
+    linux_arm64 = "sha256-sIayVxJhod4AGaSEXjGLNGc7d0953X1NHXZ7zHDYSTU=";
+    darwin_x64 = "sha256-s3CJ9he2WvtA0rLAyrshUKHYa2W0H46L9XWd0t2x6ek=";
+    darwin_arm64 = "sha256-Jkhxir8Vdgo8mN1WWCLCRvKFgPEkToUzDNbnHbraz3c=";
   };
 
   includeInPath = [ ccextractor ];
--- pkgs/tools/misc/tdarr/server.nix
+++ pkgs/tools/misc/tdarr/server.nix
@@ -5,10 +5,10 @@ callPackage ./common.nix { } {
   component = "server";
 
   hashes = {
-    linux_x64 = "sha256-dAXcBPJ8wOxx4gg50mN6nEgUcDqEOn17eByX2FvphrQ=";
-    linux_arm64 = "sha256-MDDhkSmMTiOEezKpInR86TItkVnX20IKblUf2JUKnKg=";
-    darwin_x64 = "sha256-faCNm8eeJzQq3Doyac/vtmx80dOTaWVMIErgNwz77xk=";
-    darwin_arm64 = "sha256-NNEvCFZvPJFy6Elh7voeSe847bGVJpnOt/ac5nLEy7Y=";
+    linux_x64 = "sha256-VRTtCYWM7F3H4VL79jZIZSgfPdfveMulV6JwtbAE24Y=";
+    linux_arm64 = "sha256-V73BKbPpr5htkauhrxDVPbKZdLZMss3Y8IGYzYMeb3Q=";
+    darwin_x64 = "sha256-2scrFYx4mt3N+HjzJ43nB+fNIiuvy2prMv0odte/CXM=";
+    darwin_arm64 = "sha256-k42yA6Apq6YS7Bp+Ie4ve15XTZmkUGRhNYAnVMh5bEw=";
   };
 
   includeInPath = [ ccextractor ];
