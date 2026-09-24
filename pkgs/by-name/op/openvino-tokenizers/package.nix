e8fbae8eb474907dea9e9d1adf7973d03cbfaec0
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-09-24 14:09:58 +0200

    openvino-tokenizers: 2026.3.1.0 -> 2026.4.0.0
    https://github.com/openvinotoolkit/openvino_tokenizers/releases/tag/2026.4.0.0

--- pkgs/by-name/op/openvino-tokenizers/package.nix
+++ pkgs/by-name/op/openvino-tokenizers/package.nix
@@ -17,7 +17,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "openvino-tokenizers";
-  version = "2026.3.1.0";
+  version = "2026.4.0.0";
 
   __structuredAttrs = true;
 
@@ -27,7 +27,7 @@ stdenv.mkDerivation (finalAttrs: {
       owner = "openvinotoolkit";
       repo = "openvino_tokenizers";
       tag = finalAttrs.version;
-      hash = "sha256-vaNL/M6PaIi63Pty6b7SvZyQxHwg9jKejzLLvY8grCc=";
+      hash = "sha256-M+HPqxwCZCBxSmpcnaIPBtEniRD9H1lCFgOrqH/eAFQ=";
     };
 
   nativeBuildInputs = [
