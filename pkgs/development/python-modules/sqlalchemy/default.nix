4262cf9882cd6c4bde62bfb64a4d5cd7335db5ca
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-09-24 22:32:31 +0200

    python3Packages.sqlalchemy: 2.0.51 -> 2.0.54
    https://github.com/sqlalchemy/sqlalchemy/releases/tag/rel_2_0_54

--- pkgs/development/python-modules/sqlalchemy/default.nix
+++ pkgs/development/python-modules/sqlalchemy/default.nix
@@ -43,14 +43,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "sqlalchemy";
-  version = "2.0.51";
+  version = "2.0.54";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "sqlalchemy";
     repo = "sqlalchemy";
     tag = "rel_${lib.replaceStrings [ "." ] [ "_" ] finalAttrs.version}";
-    hash = "sha256-2t3NhfLiu/rLI2yvFPK9uQXGyzqNUj7ImDRx0EasdsI=";
+    hash = "sha256-sAVr3h7c+S30p4SVTmz/cWCKqMlDG+H33vPHIMMlEns=";
   };
 
   postPatch = ''
