f19fd61b360deb8f097251060046cbb47e82f096
Salva HG <salva.hg@proton.me>
2026-09-24 23:51:20 +0000

    cosmic-idle: 1.8.0 -> 1.9.0

--- pkgs/by-name/co/cosmic-idle/package.nix
+++ pkgs/by-name/co/cosmic-idle/package.nix
@@ -16,7 +16,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-idle";
-  version = "1.8.0";
+  version = "1.9.0";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
