4626bf64096638f00a27efbde7bf7baa12caf33a
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-25 16:05:44 +0000

    freescout: 1.8.235 -> 1.8.241

--- pkgs/by-name/fr/freescout/package.nix
+++ pkgs/by-name/fr/freescout/package.nix
@@ -7,13 +7,13 @@
 stdenv.mkDerivation (finalAttrs: {
   preferLocalBuild = true;
   pname = "freescout";
-  version = "1.8.235";
+  version = "1.8.241";
 
   src = fetchFromGitHub {
     owner = "freescout-help-desk";
     repo = "freescout";
     tag = finalAttrs.version;
-    hash = "sha256-PwooPCUqJydKLN8TDQRHMYv17nUiEqrbgPiWxbTwHkw=";
+    hash = "sha256-iHg9KSSksV4+ZbCn+aC0ubNDt91CbuZDK3K5Bf7qIi4=";
   };
 
   patches = [
