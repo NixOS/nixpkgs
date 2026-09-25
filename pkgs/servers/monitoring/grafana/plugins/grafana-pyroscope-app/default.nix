f8207d0c9489d703aae0051dd9edf66951b706bd
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-25 05:53:07 +0000

    grafanaPlugins.grafana-pyroscope-app: 2.3.0 -> 2.3.1

--- pkgs/servers/monitoring/grafana/plugins/grafana-pyroscope-app/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-pyroscope-app/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "grafana-pyroscope-app";
-  version = "2.3.0";
-  zipHash = "sha256-nz5jIoSLLhaKvH9PBliiyK7N9TEh3e1GRg6R94l8PX8=";
+  version = "2.3.1";
+  zipHash = "sha256-m9hkW4t+2LtZ9AnCS3qoPieRm0T+a0lfAhd8X45gsNk=";
   meta = {
     description = "Integrate seamlessly with Pyroscope, the open-source continuous profiling platform, providing a smooth, query-less experience for browsing and analyzing profiling data";
     license = lib.licenses.agpl3Only;
