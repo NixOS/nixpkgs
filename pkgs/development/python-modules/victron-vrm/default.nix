22eff44fd844dccc8be0126a396e2eff5e28961c
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-25 16:26:29 +0000

    python3Packages.victron-vrm: 0.1.12 -> 0.1.13

--- pkgs/development/python-modules/victron-vrm/default.nix
+++ pkgs/development/python-modules/victron-vrm/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "victron-vrm";
-  version = "0.1.12";
+  version = "0.1.13";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "KSoft-Si";
     repo = "vrm-client";
     tag = "v${version}";
-    hash = "sha256-In4yL5e6DZkP/8JeM1FhoMuhsqQ6uZE3fFLyfnLzgZQ=";
+    hash = "sha256-bkQeIlTXy0970+tw7BRK8tllaCulOlsbEmMWa8jR2S8=";
   };
 
   build-system = [ hatchling ];
