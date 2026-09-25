bd77871aba73759a0ff051fba5f9880c0e5047a0
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-25 22:11:12 +0000

    llmfit: 1.1.15 -> 1.1.16

--- pkgs/by-name/ll/llmfit/package.nix
+++ pkgs/by-name/ll/llmfit/package.nix
@@ -8,7 +8,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "llmfit";
-  version = "1.1.15";
+  version = "1.1.16";
 
   __structuredAttrs = true;
 
@@ -16,10 +16,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "AlexsJones";
     repo = "llmfit";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-rAlWEpoHuh03sU+Ma9LqvjNMq8/1x8e0MAQRmt4etk4=";
+    hash = "sha256-EMCtdgfR4y9+UY3byg+jYUhkcWpt1ZU8/CIOHnMY3UQ=";
   };
 
-  cargoHash = "sha256-RN5f0TGnhi2FrekmVmEYONU59g+akuewwCOj6kfcBjw=";
+  cargoHash = "sha256-aQEThRrqTh4m3KMJqCU4vcMGrQiIR23yiSibTIAywB8=";
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   doInstallCheck = true;
