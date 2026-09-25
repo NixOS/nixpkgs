c7f75f4f4335b574be2dae0db6bc8fe1e3ce8675
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-09-25 15:41:54 +0530

    maintainers: add kaezrr

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14660,6 +14660,12 @@
     github = "kaeeraa";
     githubId = 99148867;
   };
+  kaezrr = {
+     name = "Anjishnu Banerjee";
+     email = "kaezr.dev@gmail.com";
+     github = "kaezrr";
+     githubId = 107052359;
+  };
   kagehisa = {
     name = "Sven Woelfel";
     email = "woelfel.pub@gmail.com";
--- pkgs/by-name/qm/qmk-udev-rules/package.nix
+++ pkgs/by-name/qm/qmk-udev-rules/package.nix
@@ -40,6 +40,9 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Official QMK udev rules list";
     platforms = lib.platforms.linux;
     license = lib.licenses.gpl2Only;
-    maintainers = with lib.maintainers; [ miniharinn ];
+    maintainers = with lib.maintainers; [
+      miniharinn
+      kaezrr
+    ];
   };
 })
