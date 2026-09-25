b536ae06ff2924ea766959718458d840e7ab3b41
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-25 04:59:31 +0000

    flyctl: 0.4.102 -> 0.4.108

--- pkgs/by-name/fl/flyctl/package.nix
+++ pkgs/by-name/fl/flyctl/package.nix
@@ -12,7 +12,7 @@
 
 buildGoModule rec {
   pname = "flyctl";
-  version = "0.4.102";
+  version = "0.4.108";
 
   src = fetchFromGitHub {
     owner = "superfly";
@@ -22,11 +22,11 @@ buildGoModule rec {
       cd "$out"
       git rev-parse HEAD > COMMIT
     '';
-    hash = "sha256-TV+Ne1Pp5PBGtaofpIEX5alnafyBKgPR8r1IKDCbNQE=";
+    hash = "sha256-S8MzbGH4VPGTvP2NCvzocTNCnWbj3YdyhZTPePiNY18=";
   };
 
   proxyVendor = true;
-  vendorHash = "sha256-DivuislpNHmimSAN5oTKEEUt0/oBpTukxttEUzLSqyI=";
+  vendorHash = "sha256-AZ+SBYxYFffKKGrp3slxDyNdwCH/zUHurYYXv/ElG9U=";
 
   subPackages = [ "." ];
 
