26022c570bea8e70f328e6332d6406257884461f
xscriptor <preciado.oscar.osorio@gmail.com>
2026-09-25 17:39:57 +0200

    xfetch: init at 1.0.0

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -31857,6 +31857,12 @@
     githubId = 47603387;
     name = "Mihók Martin";
   };
+  xscriptor = {
+    email = "x@xscriptor.com";
+    github = "xscriptor";
+    githubId = 164530146;
+    name = "xscriptor";
+  };
   xtrayambak = {
     github = "xTrayambak";
     githubId = 59499552;
