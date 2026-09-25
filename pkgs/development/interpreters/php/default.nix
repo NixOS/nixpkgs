e6ce58c1d2d67e4d947b38978f7f6f9b724b31b7
Jared Biel <jbiel@bushelpowered.com>
2026-09-25 18:05:48 -0500

    php83: 8.3.33 -> 8.3.35
    (cherry picked from commit 71445f2a5cfff53f5be6b39cd8401462fa3d6756)

--- pkgs/development/interpreters/php/default.nix
+++ pkgs/development/interpreters/php/default.nix
@@ -73,8 +73,8 @@ in
     hash = "sha256-U2Lyp6DnFoznIv6gBIsaHSjg98wmXEF99nDGVnBpUBg=";
   };
   php83 = mkPhp {
-    version = "8.3.33";
-    hash = "sha256-+cn00M12ksbj2jY81MpyqCakmuGPXtQwtvyue8KIHhg=";
+    version = "8.3.35";
+    hash = "sha256-4nnZ7JDZvKuQDRRoFuWxHT6JJLgOK6NsfX2ufXHcvy0=";
   };
   php84 = mkPhp {
     version = "8.4.25";
