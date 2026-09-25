92e8f36e6784cf138bf4d65196aecc43c80a5c57
Baptiste Girardeau <contact@bgirardeau.me>
2026-09-25 14:54:34 +0200

    maintainers: add baptiste0928

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3249,6 +3249,12 @@
     githubId = 766221;
     name = "Ngoc Nguyen";
   };
+  baptiste0928 = {
+    email = "contact@bgirardeau.me";
+    github = "baptiste0928";
+    githubId = 22115890;
+    name = "Baptiste Girardeau";
+  };
   barab-i = {
     email = "barab_i@outlook.com";
     github = "barab-i";
