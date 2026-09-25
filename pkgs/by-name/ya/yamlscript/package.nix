fe64e52956bdea71b92a746018515ac8e4bf33ed
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-25 17:36:00 +0000

    yamlscript: 0.3.0 -> 0.3.1

--- pkgs/by-name/ya/yamlscript/package.nix
+++ pkgs/by-name/ya/yamlscript/package.nix
@@ -6,11 +6,11 @@
 
 buildGraalvmNativeImage (finalAttrs: {
   pname = "yamlscript";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchurl {
     url = "https://github.com/yaml/yamlscript/releases/download/${finalAttrs.version}/yamlscript.cli-${finalAttrs.version}-standalone.jar";
-    hash = "sha256-8eEj7nZW0XNc2exbz/XPXtQfqqypfEpTZw6NhLev3qU=";
+    hash = "sha256-FblgTEQdbE0tIXJLNgTm8CYhJKYipsC55kSkW/Ajh/g=";
   };
 
   extraNativeImageBuildArgs = [
