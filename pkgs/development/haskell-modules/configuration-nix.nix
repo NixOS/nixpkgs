5f58270b3c86c1d144afdf973e3ae075a48ad080
OPNA2608 <christoph.neidahl@gmail.com>
2026-09-25 11:55:38 +0200

    haskellPackages: Adjust to cudatoolkit being moved to alias

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -222,11 +222,11 @@ builtins.intersectAttrs super {
   cuda = overrideCabal (drv: {
     extraLibraries = (drv.extraLibraries or [ ]) ++ [ pkgs.linuxPackages.nvidia_x11 ];
     configureFlags = (drv.configureFlags or [ ]) ++ [
-      "--extra-lib-dirs=${pkgs.cudatoolkit.lib}/lib"
-      "--extra-include-dirs=${pkgs.cudatoolkit}/include"
+      "--extra-lib-dirs=${pkgs.cudaPackages.cudatoolkit.lib}/lib"
+      "--extra-include-dirs=${pkgs.cudaPackages.cudatoolkit}/include"
     ];
     preConfigure = ''
-      export CUDA_PATH=${pkgs.cudatoolkit}
+      export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
     '';
   }) super.cuda;
 
@@ -244,7 +244,7 @@ builtins.intersectAttrs super {
 
   nvvm = overrideCabal (drv: {
     preConfigure = ''
-      export CUDA_PATH=${pkgs.cudatoolkit}
+      export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
     '';
   }) super.nvvm;
 
@@ -296,7 +296,7 @@ builtins.intersectAttrs super {
 
   cufft = overrideCabal (drv: {
     preConfigure = ''
-      export CUDA_PATH=${pkgs.cudatoolkit}
+      export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
     '';
   }) super.cufft;
 
