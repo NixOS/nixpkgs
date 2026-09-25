c2d848e06f278cc14f931398dfb101f7aed2b1cf
Ingo Reitz <9l@9lo.re>
2026-09-25 16:06:55 +0200

    quassel: add myself as maintainer

--- pkgs/by-name/qu/quassel/package.nix
+++ pkgs/by-name/qu/quassel/package.nix
@@ -95,7 +95,7 @@ stdenv.mkDerivation rec {
       as WeeChat, but graphical (based on Qt4/KDE4 or Qt5/KF5).
     '';
     license = lib.licenses.gpl3;
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ ninelore ];
     mainProgram =
       if monolithic then
         "quassel"
