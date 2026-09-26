3cc85bb6584932ca90ba626771b6757e4b788e1f
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-26 23:44:40 +0000

    oxvg: 0.0.7 -> 0.0.8

--- pkgs/by-name/ox/oxvg/package.nix
+++ pkgs/by-name/ox/oxvg/package.nix
@@ -8,15 +8,15 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "oxvg";
-  version = "0.0.7";
+  version = "0.0.8";
   __structuredAttrs = true;
 
   src = fetchCrate {
     inherit (finalAttrs) pname version;
-    hash = "sha256-x2xK37GOQOu7tdOmxUxitdpDfEXzfrJveNnbjHCeuvs=";
+    hash = "sha256-eq2cihOJs+F4R+ZCL0k9QoJdW0YjJSNf52vhJzyfLy4=";
   };
 
-  cargoHash = "sha256-NYMf4enipgr1dsiJMsSRpWHRi3BVDH92WRmYDOfmXVU=";
+  cargoHash = "sha256-SMjNYRCCX4ZqP9vRTNmnVsYVp90l22N9HD05wDDfgZs=";
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   doInstallCheck = true;
