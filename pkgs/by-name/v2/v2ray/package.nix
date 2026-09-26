7d6d09ead24062ca5c92fb6c2275a2b3c5eea087
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-26 07:42:16 +0000

    v2ray: 5.49.0 -> 5.54.2

--- pkgs/by-name/v2/v2ray/package.nix
+++ pkgs/by-name/v2/v2ray/package.nix
@@ -16,18 +16,18 @@
 
 buildGoModule (finalAttrs: {
   pname = "v2ray-core";
-  version = "5.49.0";
+  version = "5.54.2";
 
   src = fetchFromGitHub {
     owner = "v2fly";
     repo = "v2ray-core";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-a1q/QJpXt3XJZwSteOf3+cQ2W7+KmMfknv1qaByg4Hc=";
+    hash = "sha256-mpNe+v2UrdZYU9N6lr+v+m8h1HQDiJnfGCcME+ePPk0=";
   };
 
   # `nix-update` doesn't support `vendorHash` yet.
   # https://github.com/Mic92/nix-update/pull/95
-  vendorHash = "sha256-A45fCy9Ytm/zVLQEn1QbUumBYZVG+jbmSFWkS/yAJn4=";
+  vendorHash = "sha256-/uik8v15VcrVBv/GHpuWgIKKro/xlzNsxnrgfLxU0o8=";
 
   ldflags = [
     "-s"
