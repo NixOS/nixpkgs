6634d105518c45c6762e51776d24d78610a3da30
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-26 08:06:03 +0000

    linux_xanmod_latest: 7.2.7 -> 7.2.8
    - Changelog: https://dl.xanmod.org/changelog/7.2/ChangeLog-7.2.8-xanmod1.gz
    - Diff: https://gitlab.com/xanmod/linux/-/compare/7.2.7-xanmod1..7.2.8-xanmod1?from_project_id=51590166
    
    (cherry picked from commit 1f1e5214c6bc70d6042ea4aaf83fc61155e9978e)

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -21,8 +21,8 @@ let
     };
     # ./update-xanmod.sh main
     main = {
-      version = "7.2.7";
-      hash = "sha256-QeEJiBJfQloehzZppWX8zvL43UGSMa4xUicA1VXN6f0=";
+      version = "7.2.8";
+      hash = "sha256-jHCIFm1xI3T/2Zl0h91Nf6oAr4ozIPQSYRFo92KmOfc=";
     };
   };
 
