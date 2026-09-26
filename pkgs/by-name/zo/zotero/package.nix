ec8492fde6cd3b5d4a8b5dbe6b3358746b9c6194
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-26 15:41:03 +0000

    zotero: 10.0.2 -> 10.0.4

--- pkgs/by-name/zo/zotero/package.nix
+++ pkgs/by-name/zo/zotero/package.nix
@@ -33,14 +33,14 @@ let
   nodejs = nodejs_22;
 
   pname = "zotero";
-  version = "10.0.2";
+  version = "10.0.4";
 
   src = fetchFromGitHub {
     owner = "zotero";
     repo = "zotero";
     tag = version;
     fetchSubmodules = true;
-    hash = "sha256-V0dYKFWr1ZjYNEWufslW/NqfjPZOb8W1pECz33PcCEQ=";
+    hash = "sha256-vevcTBWo4o2K7ibg+FOetx07EyH3k1V2I43UF2tQrRU=";
   };
 
   pdf-js = buildNpmPackage {
