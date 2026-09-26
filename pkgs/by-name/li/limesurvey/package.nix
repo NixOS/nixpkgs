1ae79528ec3a0864e7bb3fecae43acb47ca9d46e
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-26 01:07:44 +0000

    limesurvey: 6.15.14+250924 -> 7.1.2+260917

--- pkgs/by-name/li/limesurvey/package.nix
+++ pkgs/by-name/li/limesurvey/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "limesurvey";
-  version = "6.15.14+250924";
+  version = "7.1.2+260917";
 
   src = fetchFromGitHub {
     owner = "LimeSurvey";
     repo = "LimeSurvey";
     tag = version;
-    hash = "sha256-xxK6JEgeBVIj8CGb0qSzwfO1Se9+jMtGB9V3rsc9bBU=";
+    hash = "sha256-F75m131PpWPGXyPsA15N9LnCmSnHPH32/mgAMGn0/4I=";
   };
 
   phpConfig = writeText "config.php" ''
