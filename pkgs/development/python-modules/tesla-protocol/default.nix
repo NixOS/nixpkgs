bf6d9e9a57bd23cf88336c899d7bbe8149927276
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-26 18:38:35 +0000

    python3Packages.tesla-protocol: 2.0.0 -> 3.0.0

--- pkgs/development/python-modules/tesla-protocol/default.nix
+++ pkgs/development/python-modules/tesla-protocol/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "tesla-protocol";
-  version = "2.0.0";
+  version = "3.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Teslemetry";
     repo = "tesla-protocol";
     tag = "@teslemetry/tesla-protocol@${finalAttrs.version}";
-    hash = "sha256-Zii0Hq4ow7tyMD2Vsjj8wMF4xAHIAYUHeum3htWcRx4=";
+    hash = "sha256-q5GDWaxgxVH9ZUiu0dKVcKJUo7A/pK+YtVvMrM6Ys88=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/packages/python";
