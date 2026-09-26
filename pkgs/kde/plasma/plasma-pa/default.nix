27c7600bcbab920f8ff9532400cc1eab08e96faf
Arnout Engelen <arnout@bzzt.net>
2026-09-26 10:41:53 +0200

    kdePackages.plasma-pa: fix reproducibility
    fixes #566869

--- pkgs/kde/plasma/plasma-pa/default.nix
+++ pkgs/kde/plasma/plasma-pa/default.nix
@@ -7,6 +7,11 @@
 mkKdeDerivation {
   pname = "plasma-pa";
 
+  patches = [
+    # https://invent.kde.org/plasma/plasma-pa/-/merge_requests/431
+    ./reproducible-build.patch
+  ];
+
   extraNativeBuildInputs = [ pkg-config ];
   extraBuildInputs = [
     libcanberra
