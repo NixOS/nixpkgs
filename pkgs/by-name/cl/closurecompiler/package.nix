4ec5b105f98590efd60fe971da6e1425a3a16760
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-26 23:11:16 +0000

    closurecompiler: 20260804 -> 20260923

--- pkgs/by-name/cl/closurecompiler/package.nix
+++ pkgs/by-name/cl/closurecompiler/package.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "closure-compiler";
-  version = "20260804";
+  version = "20260923";
 
   src = fetchurl {
     url = "mirror://maven/com/google/javascript/closure-compiler/v${finalAttrs.version}/closure-compiler-v${finalAttrs.version}.jar";
-    sha256 = "sha256-DLhqS5Z2nGebT8LS3A5azQq9Df+TKUW5D74yckdGaik=";
+    sha256 = "sha256-kaexGCOp9c49LNs35DPZQzxeVgN3sDxLv+qDg8GtfqM=";
   };
 
   dontUnpack = true;
