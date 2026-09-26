18e4330d1867c153aff2a378680def8408023edd
phanirithvij <phanirithvij2000@gmail.com>
2026-09-26 10:34:02 +0530

    python3Packages.typesense: 1.1.1 -> 1.3.0
    https://github.com/typesense/typesense-python#compatibility
    
    Diff: https://github.com/typesense/typesense-python/compare/v1.1.1...v1.3.0
    Not-cherry-picked-because: patches are specific to 1.3.0 which is unavailable in unstable
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/development/python-modules/typesense/default.nix
+++ pkgs/development/python-modules/typesense/default.nix
@@ -14,22 +14,25 @@
   isort,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "typesense";
-  version = "1.1.1";
+  version = "1.3.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "typesense";
     repo = "typesense-python";
-    tag = "v${version}";
-    hash = "sha256-vo9DW4kinb00zWW4yX8ibyelQxW3eVabn+oMddPEd18=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-b3t4l02tOiSMrkqZACV6l5f+Kb5Wfcnq9ZZCld1SKBU=";
   };
 
   patches = [
-    # See <https://github.com/typesense/typesense-python/pull/103>.
-    ./linux-only-metrics.patch
-    ./generated-temp-path.patch
+    # See <https://github.com/typesense/typesense-python/pull/132>.
+    # See <https://github.com/typesense/typesense-python/pull/133>.
+    ./0001-linux-only-metrics.patch
+    ./0002-generated-temp-path.patch
+    ./0003-tests-fix-rule_id.patch
+    ./0004-test-gate-v30-collection-schema-expectations.patch
   ];
 
   build-system = [ setuptools ];
@@ -47,24 +50,36 @@ buildPythonPackage rec {
     isort
   ];
   disabledTestMarks = [ "open_ai" ];
-  disabledTests = [ "import_typing_extensions" ];
+  disabledTests = [
+    "import_typing_extensions"
+    # tests/operations_test.py::test_snapshot_async fails when tests are run
+    # in parallel due to writing to the shared /tmp directory on macOS
+    "test_snapshot_async"
+  ];
 
   __darwinAllowLocalNetworking = true;
 
+  # For darwin, the nix sandbox doesn't isolate loopback interfaces like linux does.
+  # So nixpkgs-review builds of python3.x will collide on the hardcoded port 8108.
+  # So for tests patch it to use random port.
   preCheck = ''
+    export TYPESENSE_PORT=$(( 10000 + $$ % 30000 ))
+    export PEERING_PORT=$(( TYPESENSE_PORT - 1 ))
+    find tests -type f -exec sed -i "s/8108/$TYPESENSE_PORT/g" {} +
+
     TYPESENSE_API_KEY="xyz" \
     TYPESENSE_DATA_DIR="$(mktemp -d)" \
-    typesense-server &
+    typesense-server --api-port=$TYPESENSE_PORT --peering-port=$PEERING_PORT &
 
     typesense_pid=$!
 
+    # Ensure typesense-server is killed even if tests fail
+    trap "kill $typesense_pid" EXIT
+
     # Wait for typesense to finish starting.
-    timeout 20 bash -c '
-      while ! curl -s --fail localhost:8108/health; do sleep 1; done
-    ' || false
-  '';
-  postCheck = ''
-    kill $typesense_pid
+    timeout 20 bash -c "
+      while ! curl -s --fail localhost:$TYPESENSE_PORT/health; do sleep 1; done
+    " || false
   '';
 
   pythonImportsCheck = [ "typesense" ];
@@ -74,5 +89,7 @@ buildPythonPackage rec {
     homepage = "https://github.com/typesense/typesense-python";
     license = lib.licenses.asl20;
     teams = [ lib.teams.ngi ];
+    # on x86_64-darwin the typesense server doesn't start
+    badPlatforms = [ "x86_64-darwin" ];
   };
-}
+})
