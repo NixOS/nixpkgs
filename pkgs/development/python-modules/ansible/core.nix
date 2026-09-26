599960ca391474b02b25a4311b244c2074886239
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-26 06:49:12 +0000

    ansible: 2.21.4 -> 2.22.0b1

--- pkgs/development/python-modules/ansible/core.nix
+++ pkgs/development/python-modules/ansible/core.nix
@@ -32,7 +32,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "ansible-core";
-  version = "2.21.4";
+  version = "2.22.0b1";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -41,7 +41,7 @@ buildPythonPackage (finalAttrs: {
     owner = "ansible";
     repo = "ansible";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-lUm7xhvF0CU37fnunLyAG4FLpjwAgEoLEY1ztua2m4s=";
+    hash = "sha256-xx9GRXdvh06oRAZ2kUdDA7JEFfolx1f5mEe2vbfKzvM=";
   };
 
   postPatch = ''
