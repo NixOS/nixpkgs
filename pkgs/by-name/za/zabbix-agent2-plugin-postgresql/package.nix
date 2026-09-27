14f8a0369ab50b3963155e447f3a80f8203f0857
Thomas <thomas.erents@gmail.com>
2026-09-27 11:32:51 +0200

    zabbix-agent2-plugin-postgresql: 7.4.14 -> 7.4.15

--- pkgs/by-name/za/zabbix-agent2-plugin-postgresql/package.nix
+++ pkgs/by-name/za/zabbix-agent2-plugin-postgresql/package.nix
@@ -6,11 +6,11 @@
 
 buildGoModule (finalAttrs: {
   pname = "zabbix-agent2-plugin-postgresql";
-  version = "7.4.14";
+  version = "7.4.15";
 
   src = fetchurl {
     url = "https://cdn.zabbix.com/zabbix-agent2-plugins/sources/postgresql/zabbix-agent2-plugin-postgresql-${finalAttrs.version}.tar.gz";
-    hash = "sha256-H+WwJ9RrITeWKErMW+vcEG/Z2rAjbznVg/dK3ZM7j0I=";
+    hash = "sha256-xofvvAc1Mn3r+KvziF6gWecGz5Ctalgb3THQNpSSMiA=";
   };
 
   vendorHash = null;
