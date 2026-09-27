2dea07f539bbb1e7fa70ce6fce3a4030d7c077e3
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-27 09:06:57 +0000

    python3Packages.aioftp: 0.28.0 -> 0.28.3

--- pkgs/development/python-modules/aioftp/default.nix
+++ pkgs/development/python-modules/aioftp/default.nix
@@ -15,12 +15,12 @@
 
 buildPythonPackage rec {
   pname = "aioftp";
-  version = "0.28.0";
+  version = "0.28.3";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-MR9m6f9yYFX3PnR2T7YuCXq4Btoxd8E/E1bI5+r5pl0=";
+    hash = "sha256-8FTzryhbSVhiEiDwTOdV5YkHFAGB6V76hZNpCRyS0EA=";
   };
 
   build-system = [ setuptools ];
