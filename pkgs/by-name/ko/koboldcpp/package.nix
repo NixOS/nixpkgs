c1e719f901d29b78c0d4a96ebfd3fe39f2492655
4evy <git@evy.pink>
2026-09-27 10:25:00 +0300

    koboldcpp: fix runtime and platform support

--- pkgs/by-name/ko/koboldcpp/package.nix
+++ pkgs/by-name/ko/koboldcpp/package.nix
@@ -1,13 +1,17 @@
 {
   lib,
   buildEnv,
+  buildPackages,
+  cacert,
+  cloudflared,
+  coreutils,
+  curl,
   fetchFromGitHub,
   stdenv,
-  makeWrapper,
+  runtimeShell,
+  replaceVars,
   python3Packages,
-  tk,
   autoAddDriverRunpath,
-  addDriverRunpath,
 
   koboldLiteSupport ? true,
 
@@ -23,8 +27,11 @@
   vulkanSupport ? false,
   vulkan-loader,
   shaderc,
+  vulkan-tools,
   metalSupport ? false,
   nix-update-script,
+  xdg-utils,
+  xterm,
 }:
 
 assert lib.assertMsg (!(cublasSupport && rocmSupport)) ''
@@ -32,48 +39,45 @@ assert lib.assertMsg (!(cublasSupport && rocmSupport)) ''
 '';
 
 let
-  version = "1.122";
-
-  src = fetchFromGitHub {
-    owner = "LostRuins";
-    repo = "koboldcpp";
-    tag = "v${version}";
-    hash = "sha256-9NW6cnlNgxRMvhyEE1Icydo5Mxii5rjfoUMNa7YZg6A=";
-  };
+  platformMakeFlags = [
+    "UNAME_S=${stdenv.hostPlatform.uname.system}"
+    "UNAME_M=${stdenv.hostPlatform.uname.processor}"
+    "UNAME_P=${stdenv.hostPlatform.uname.processor}"
+    "UNAME_O=${stdenv.hostPlatform.uname.system}"
+    # Do not specialize portable binaries for the build machine's POWER CPU
+    "POWER9_M="
+    "LLAMA_PORTABLE=1"
+  ];
 
   cudaMaxArch =
     let
-      cudaMaxCapability = lib.removeSuffix "a" (
-        cudaPackages.flags.dropDots (lib.last cudaPackages.flags.cudaCapabilities)
-      );
+      cudaArchitectures = map (
+        capability:
+        lib.toInt (cudaPackages.flags.dropDots (lib.removeSuffix "f" (lib.removeSuffix "a" capability)))
+      ) cudaPackages.flags.cudaCapabilities;
     in
-    "${cudaMaxCapability}0";
+    # KCPP_LIMIT_CUDA_MAX_ARCH uses CUDA's numeric architecture encoding
+    # (8.9 -> 89 -> 890). Take the numeric maximum because the capability
+    # list need not be sorted, then convert it to a Makefile flag value
+    toString (10 * lib.foldl' lib.max 0 cudaArchitectures);
 
-  libraryPathWrapperArgs =
-    lib.optionals (cublasSupport && stdenv.hostPlatform.isLinux) [
-      "--prefix"
-      "LD_LIBRARY_PATH"
-      ":"
-      (lib.makeLibraryPath [ addDriverRunpath.driverLink ])
-    ]
-    ++ lib.optionals (rocmSupport && stdenv.hostPlatform.isLinux) [
-      "--prefix"
-      "LD_LIBRARY_PATH"
-      ":"
-      "${rocmPath}/lib"
-      "--set-default"
-      "HIP_PATH"
-      "${rocmPath}"
-      "--set-default"
-      "ROCM_PATH"
-      "${rocmPath}"
-    ];
+  libraryPathWrapperArgs = lib.optionals (rocmSupport && stdenv.hostPlatform.isLinux) [
+    "--prefix"
+    "LD_LIBRARY_PATH"
+    ":"
+    "${rocmPath}/lib"
+    "--set-default"
+    "HIP_PATH"
+    "${rocmPath}"
+    "--set-default"
+    "ROCM_PATH"
+    "${rocmPath}"
+  ];
 
-  # The Makefile expects a single ROCM_PATH that provides hipconfig,
-  # hipcc, amdhip64, hipblas, and rocblas. clr ships hipconfig plus the
-  # HIP/LLVM compiler, but hipconfig is wrapped with ROCM_PATH=clr so
-  # `hipconfig -C` never emits hipblas/rocblas/hipblas-common include
-  # flags
+  # The Makefile expects a single ROCM_PATH that provides hipconfig, hipcc,
+  # amdhip64, hipblas, and rocblas. clr ships hipconfig plus the HIP/LLVM
+  # compiler, but hipconfig is wrapped with ROCM_PATH=clr so `hipconfig -C`
+  # never emits hipblas/rocblas/hipblas-common include flags
   rocmBuildInputs = with rocmPackages; [
     clr
     hipblas
@@ -86,13 +90,27 @@ let
     paths = rocmBuildInputs;
   };
 
-  runtimePath = [ tk ];
+  runtimePath = [
+    cloudflared
+    curl
+  ]
+  ++ lib.optionals rocmSupport [ rocmPackages.rocminfo ]
+  ++ lib.optionals vulkanSupport [
+    vulkan-tools
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    xdg-utils
+    xterm
+  ];
 
   makeWrapperArgs = [
     "--prefix"
     "PATH"
     ":"
     (lib.makeBinPath runtimePath)
+    "--set-default"
+    "SSL_CERT_FILE"
+    "${cacert}/etc/ssl/certs/ca-bundle.crt"
   ]
   ++ libraryPathWrapperArgs;
 
@@ -121,40 +139,78 @@ let
     else
       stdenv;
 
-  metalFailsafe = stdenv.mkDerivation {
-    pname = "koboldcpp-macos-failsafe";
-    inherit src version;
+  metalFailsafe =
+    { src, version }:
+    stdenv.mkDerivation {
+      pname = "koboldcpp-macos-failsafe";
+      inherit src version;
 
-    __structuredAttrs = true;
-    strictDeps = true;
+      __structuredAttrs = true;
+      strictDeps = true;
 
-    enableParallelBuilding = true;
-    makeFlags = [ "LLAMA_PORTABLE=1" ];
-    buildFlags = [ "koboldcpp_macos_failsafe" ];
+      enableParallelBuilding = true;
+      makeFlags = platformMakeFlags;
+      buildFlags = [ "koboldcpp_macos_failsafe" ];
 
-    installPhase = ''
-      runHook preInstall
+      installPhase = ''
+        runHook preInstall
 
-      install -Dm755 koboldcpp_macos_failsafe.so \
-        "$out/lib/koboldcpp_failsafe.so"
+        install -Dm755 koboldcpp_macos_failsafe.so \
+          "$out/lib/koboldcpp_failsafe.so"
 
-      runHook postInstall
-    '';
-  };
+        runHook postInstall
+      '';
+    };
 
   builtLibraries = if metalSupport then lib.remove "koboldcpp_failsafe" libraries else libraries;
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "koboldcpp";
-  inherit src version;
+  version = "1.122";
+
+  src = fetchFromGitHub {
+    owner = "LostRuins";
+    repo = "koboldcpp";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-9NW6cnlNgxRMvhyEE1Icydo5Mxii5rjfoUMNa7YZg6A=";
+  };
 
   __structuredAttrs = true;
   strictDeps = true;
+  patches = [
+    (replaceVars ./nixos-runtime-paths.patch {
+      shell = runtimeShell;
+      cat = lib.getExe' coreutils "cat";
+    })
+  ];
 
+  postPatch = lib.concatStringsSep "\n" [
+    ''
+      substituteInPlace kcpp_agent.py \
+        --replace-fail 'shutil.which("sh")' '"${runtimeShell}"'
+    ''
+    (lib.optionalString vulkanSupport ''
+      # Shader generators run on the build platform and need no target libraries
+      ${lib.concatMapStringsSep "\n"
+        (define: ''
+          substituteInPlace Makefile \
+            --replace-fail '$(CXX) $(CXXFLAGS) $(${define}) $(filter-out %.h,$^) -o $@ $(LDFLAGS)' \
+              '${lib.getExe' buildPackages.stdenv.cc "c++"} -std=c++17 -O3 -pthread $(${define}) $(filter-out %.h,$^) -o $@'
+        '')
+        [
+          "VKGEN_NOEXT_ADD"
+          "VKGEN_NOEXT_FORCE"
+        ]
+      }
+    '')
+    # Prefer nixpkgs shaderc's glslc over the bundled binary
+    ''
+      rm -f glslc-linux
+    ''
+  ];
   enableParallelBuilding = true;
 
   nativeBuildInputs = [
-    makeWrapper
     python3Packages.wrapPython
   ]
   ++ lib.optionals cublasSupport [
@@ -164,23 +220,29 @@ effectiveStdenv.mkDerivation (finalAttrs: {
   ++ lib.optionals rocmSupport [ rocmPackages.clr ]
   ++ lib.optionals vulkanSupport [ shaderc ];
 
-  pythonInputs = builtins.attrValues { inherit (python3Packages) tkinter customtkinter packaging; };
+  pythonPath = builtins.attrValues {
+    inherit (python3Packages)
+      customtkinter
+      darkdetect
+      jinja2
+      psutil
+      tkinter
+      ;
+  };
 
-  buildInputs = [
-    tk
-  ]
-  ++ finalAttrs.pythonInputs
-  ++ lib.optionals cublasSupport [
-    cudaPackages.libcublas
-    cudaPackages.cuda_cudart
-    cudaPackages.cccl
-  ]
-  ++ lib.optionals rocmSupport rocmBuildInputs
-  ++ lib.optionals vulkanSupport [
-    vulkan-loader
-  ];
+  buildInputs =
+    finalAttrs.pythonPath
+    ++ lib.optionals cublasSupport [
+      cudaPackages.libcublas
+      cudaPackages.cuda_cudart
+      cudaPackages.cccl
+    ]
+    ++ lib.optionals rocmSupport rocmBuildInputs
+    ++ lib.optionals vulkanSupport [
+      vulkan-loader
+    ];
 
-  pythonPath = finalAttrs.pythonInputs;
+  inherit makeWrapperArgs;
 
   env =
     lib.optionalAttrs rocmSupport {
@@ -193,57 +255,62 @@ effectiveStdenv.mkDerivation (finalAttrs: {
       LLAMA_USE_BUNDLED_GLSLC = "";
     };
 
-  makeFlags = [
-    "LLAMA_PORTABLE=1"
-  ]
-  ++ lib.optionals cublasSupport [
-    "LLAMA_CUBLAS=1"
-    "CUBLAS_FLAGS=-DGGML_USE_CUDA"
-    "CUBLASLD_FLAGS=-L${lib.getOutput "stubs" cudaPackages.cuda_cudart}/lib/stubs -lcuda -lcublas -lcudart -lcublasLt -lpthread -ldl -lrt"
-    "NVCCFLAGS=--forward-unknown-to-host-compiler -use_fast_math -extended-lambda -Wno-deprecated-gpu-targets -DKCPP_LIMIT_CUDA_MAX_ARCH=${cudaMaxArch} ${cudaPackages.flags.gencodeString}"
-  ]
-  ++ lib.optionals rocmSupport [
-    "LLAMA_HIPBLAS=1"
-    "ROCM_PATH=${rocmPath}"
-    "HCC=${rocmPath}/bin/hipcc"
-    "HCXX=${rocmPath}/bin/hipcc"
-    "GPU_TARGETS=${builtins.concatStringsSep " " rocmGpuTargets}"
-  ]
-  ++ lib.optionals vulkanSupport [
-    "LLAMA_VULKAN=1"
-    "LLAMA_USE_BUNDLED_GLSLC="
-  ]
-  ++ lib.optionals metalSupport [ "LLAMA_METAL=1" ];
+  makeFlags =
+    platformMakeFlags
+    ++ lib.optionals cublasSupport [
+      "LLAMA_CUBLAS=1"
+      "CUBLAS_FLAGS=-DGGML_USE_CUDA"
+      "CUBLASLD_FLAGS=-L${lib.getOutput "stubs" cudaPackages.cuda_cudart}/lib/stubs -lcuda -lcublas -lcudart -lcublasLt -lpthread -ldl -lrt"
+      "NVCCFLAGS=--forward-unknown-to-host-compiler -use_fast_math -extended-lambda -Wno-deprecated-gpu-targets -DKCPP_LIMIT_CUDA_MAX_ARCH=${cudaMaxArch} ${cudaPackages.flags.gencodeString}"
+    ]
+    ++ lib.optionals rocmSupport [
+      "LLAMA_HIPBLAS=1"
+      "ROCM_PATH=${rocmPath}"
+      "HCC=${rocmPath}/bin/hipcc"
+      "HCXX=${rocmPath}/bin/hipcc"
+      "GPU_TARGETS=${builtins.concatStringsSep " " rocmGpuTargets}"
+    ]
+    ++ lib.optionals vulkanSupport [
+      "LLAMA_VULKAN=1"
+      "LLAMA_USE_BUNDLED_GLSLC="
+    ]
+    ++ lib.optionals metalSupport [ "LLAMA_METAL=1" ];
 
   buildFlags = builtLibraries;
 
-  installPhase = ''
-    runHook preInstall
+  installPhase = lib.concatStringsSep "\n" [
+    ''
+      runHook preInstall
 
-    mkdir -p "$out/bin"
+      installDir="$out/libexec/koboldcpp"
+      mkdir -p "$installDir" "$out/bin"
 
-    install -Dm755 koboldcpp.py "$out/bin/koboldcpp.unwrapped"
-    install -Dm644 kcpp_agent.py "$out/bin/kcpp_agent.py"
-    install -Dm755 ${lib.escapeShellArgs (map (library: "${library}.so") builtLibraries)} "$out/bin"
-    cp -r embd_res "$out/bin"
+      install -Dm755 koboldcpp.py "$installDir/koboldcpp"
+      install -m644 json_to_gbnf.py kcpp_agent.py -t "$installDir"
+      install -Dm755 ${lib.escapeShellArgs (map (library: "${library}.so") builtLibraries)} "$installDir"
+      cp -r --no-preserve=mode embd_res kcpp_adapters -t "$installDir"
+    ''
 
-    ${lib.optionalString metalSupport ''
-      install -Dm755 ${metalFailsafe}/lib/koboldcpp_failsafe.so "$out/bin"
-      cp -r --no-preserve=mode kernels "$out/bin"
-    ''}
+    (lib.optionalString metalSupport ''
+      install -Dm755 ${
+        metalFailsafe { inherit (finalAttrs) src version; }
+      }/lib/koboldcpp_failsafe.so "$installDir"
+      cp -r --no-preserve=mode kernels "$installDir"
+    '')
 
-    ${lib.optionalString (!koboldLiteSupport) ''
-      rm "$out/bin/embd_res/kcpp_docs.embd"
-      rm "$out/bin/embd_res/klite.embd"
-    ''}
+    (lib.optionalString (!koboldLiteSupport) ''
+      rm "$installDir/embd_res/kcpp_docs.embd"
+      rm "$installDir/embd_res/klite.embd"
+    '')
 
-    runHook postInstall
-  '';
+    ''
+      runHook postInstall
+    ''
+  ];
 
   postFixup = ''
-    wrapPythonProgramsIn "$out/bin" "''${pythonPath[*]}"
-    makeWrapper "$out/bin/koboldcpp.unwrapped" "$out/bin/koboldcpp" \
-      ${lib.escapeShellArgs makeWrapperArgs}
+    wrapPythonProgramsIn "$out/libexec/koboldcpp" "''${pythonPath[*]}"
+    ln -s ../libexec/koboldcpp/koboldcpp "$out/bin/koboldcpp"
   '';
 
   requiredSystemFeatures = lib.optionals rocmSupport [ "big-parallel" ];
@@ -264,8 +331,13 @@ effectiveStdenv.mkDerivation (finalAttrs: {
     maintainers = with lib.maintainers; [
       _4evy
     ];
-    platforms = if metalSupport then lib.platforms.darwin else lib.platforms.unix;
-    badPlatforms = lib.optionals (cublasSupport || rocmSupport) lib.platforms.darwin;
+    platforms =
+      if cublasSupport || rocmSupport then
+        lib.platforms.linux
+      else if metalSupport then
+        lib.platforms.darwin
+      else
+        lib.platforms.unix;
     broken = metalSupport && !effectiveStdenv.hostPlatform.isDarwin;
   };
 })
