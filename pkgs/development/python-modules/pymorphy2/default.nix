238ec259a48cbb88516f86b9ea06258b6c5507f2
Eric Berquist <eric.berquist@gmail.com>
2026-09-26 23:16:30 -0400

    python3Packages.pymorphy2: fix runtime with setuptools 83 and Python 3.11+

--- pkgs/development/python-modules/pymorphy2/default.nix
+++ pkgs/development/python-modules/pymorphy2/default.nix
@@ -3,6 +3,7 @@
   fetchPypi,
   buildPythonPackage,
   isPy3k,
+  python,
   dawg-python,
   docopt,
   pymorphy2-dicts-ru,
@@ -20,6 +21,15 @@ buildPythonPackage rec {
     hash = "sha256-hsRHFX3uLrI0HvvkU44SgadUdWuhqjLad6iWFMWLVgw=";
   };
 
+  postPatch = ''
+    substituteInPlace pymorphy2/analyzer.py \
+      --replace-fail "import pkg_resources" "from importlib.metadata import entry_points" \
+      --replace-fail "ws = pkg_resources.WorkingSet()" "" \
+      --replace-fail "return ws.iter_entry_points(*args, **kwargs)" "return entry_points(group=args[0])"
+    substituteInPlace pymorphy2/units/base.py \
+      --replace-fail "args, varargs, kw, default = inspect.getargspec(cls.__init__)" "args = inspect.getfullargspec(cls.__init__).args"
+  '';
+
   propagatedBuildInputs = [
     dawg-python
     docopt
@@ -27,6 +37,14 @@ buildPythonPackage rec {
   ];
 
   pythonImportsCheck = [ "pymorphy2" ];
+
+  # Substitute for the sdist not packaging tests
+  installCheckPhase = ''
+    runHook preInstallCheck
+    PYTHONPATH="$out/${python.sitePackages}:$PYTHONPATH" \
+      python -c "import pymorphy2; pymorphy2.MorphAnalyzer()"
+    runHook postInstallCheck
+  '';
 
   meta = {
     description = "Morphological analyzer/inflection engine for Russian and Ukrainian";
