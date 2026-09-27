6e8a16a15008a478e47913980cb3bf8239690df2
Thomas <thomas.erents@gmail.com>
2026-09-27 11:24:27 +0200

    Zabbix70: 7.0.30 -> 7.0.31

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -4,8 +4,8 @@ generic: {
     hash = "sha256-795fbxmJbwIAu1JF44ZgNWZyccex6EYm0mCV8kpvu0I=";
   };
   v70 = generic {
-    version = "7.0.30";
-    hash = "sha256-wMpAeRGa3uxBNeM1HRuEkVet4HbDUsAGco5zLxcHIyw=";
+    version = "7.0.31";
+    hash = "sha256-nfEGmqHP32e3Ex6T8vX+bQrQBeQWyZAgFv2Mto8zDow=";
   };
   v60 = generic {
     version = "6.0.48";
